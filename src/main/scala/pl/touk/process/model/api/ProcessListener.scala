5189fab1783a25579a7625054181ea6111e4d61b
Arek Burdach <arek.burdach@gmail.com>
2016-07-20 13:28:25 +0200

    LoggingListener
    Change-Id: I4214926c2249f10a8b3f310cf0c3f8c6efa250d9

--- src/main/scala/pl/touk/process/model/api/ProcessListener.scala
+++ src/main/scala/pl/touk/process/model/api/ProcessListener.scala
@@ -1,10 +1,13 @@
 package pl.touk.process.model.api
 
+import pl.touk.process.model.graph.expression.Expression
 import pl.touk.process.model.graph.node.Node
 
 trait ProcessListener {
 
   def nodeEntered(ctx: Ctx, node: Node): Unit
+
+  def expressionEvaluated(expr: Expression, result: Any): Unit
 
   def serviceInvoked(id: String, result: Any): Unit
 
