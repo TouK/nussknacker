51ef59af3bcbfc3e0177a32dc85e4e0db0315418
Agnieszka Cieśla <acs@touk.pl>
2016-07-25 12:42:26 +0200

    Add stylus loader
    Change-Id: Ib846c5152d5ebe4200befb07e9da91b9f4a4d710

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { browserHistory, Router, Route, Link } from 'react-router'
 import { AppContainer } from 'react-hot-loader';
 import { App, Process, Visualization, TodoApp } from './containers/MainPage';
 
-import './app.css'
+import './app.styl'
 import 'todomvc-app-css/index.css'; /*fixme wyrzucic przy usuwanio todoapp*/
 import 'bootstrap/dist/css/bootstrap.css';
 
