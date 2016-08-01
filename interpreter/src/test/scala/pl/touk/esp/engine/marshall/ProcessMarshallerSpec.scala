b2f1fadde6f5cd0775d929cb8f067a6184916e93
Arek Burdach <abr@touk.pl>
2016-08-01 19:48:32 +0200

    backend ui moved to ui project
    Change-Id: If5f87e6b136fc879c2b4372c988ca0c5235142b6

---
+++ interpreter/src/test/scala/pl/touk/esp/engine/marshall/ProcessMarshallerSpec.scala
@@ -0,0 +1,43 @@
+package pl.touk.esp.engine.marshall
+
+import org.scalatest.{FlatSpec, Matchers}
+import pl.touk.esp.engine.api.MetaData
+import pl.touk.esp.engine.api.sink.SinkRef
+import pl.touk.esp.engine.build.GraphBuilder
+import pl.touk.esp.engine.graph.EspProcess
+import pl.touk.esp.engine._
+import pl.touk.esp.engine.graph.node._
+import pl.touk.esp.engine.graph.service.{Parameter, ServiceRef}
+import pl.touk.esp.engine.graph.variable.Field
+
+
+class ProcessMarshallerSpec extends FlatSpec with Matchers {
+
+  import spel.Implicits._
+
+  it should "marshall and unmarshall to same process" in {
+
+    def nestedGraph(id: String) =
+      GraphBuilder
+        .processor(id + "Processor", ServiceRef(id + "Service", List.empty))
+        .sink(id + "End", "")
+
+    val graph =
+      GraphBuilder
+        .source("a", "")
+        .filter("b", "alamakota == 'true'", Some(nestedGraph("b")))
+        .buildVariable("c", "fooVar", Field("f1", "expr1"), Field("f2", "expr2"))
+        .enricher("d", ServiceRef("dService", List(Parameter("p1", "expr3"))), "barVar")
+        .to(Switch("e", "expr4", "eVar", List(Case("e1", Sink("endE1", SinkRef("", List.empty)))), Some(nestedGraph("e"))))
+
+    val process = EspProcess(MetaData("process1"), graph)
+
+    val marshalled = ProcessMarshaller.toJson(process)
+    println(marshalled)
+
+    val unmarshalled = ProcessMarshaller.fromJson(marshalled).toOption
+
+    unmarshalled should equal (Some(process))
+  }
+
+}
