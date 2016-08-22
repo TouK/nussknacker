eeb7f8c29736e97d2047f495abbd98c9c36527d0
Maciek Próchniak <mpr@touk.pl>
2016-08-22 12:09:25 +0200

    key selection for kafka sinks, choosing topic in KafkaExceptionHandler
    Change-Id: Id7d73e1dad78b7e1920dbf34f7a98c71d569d270

--- kafka/src/main/scala/pl/touk/esp/engine/kafka/KafkaSinkFactory.scala
+++ kafka/src/main/scala/pl/touk/esp/engine/kafka/KafkaSinkFactory.scala
@@ -2,12 +2,12 @@ package pl.touk.esp.engine.kafka
 
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09
-import org.apache.flink.streaming.util.serialization.SerializationSchema
+import org.apache.flink.streaming.util.serialization.{KeyedSerializationSchema, SerializationSchema}
 import pl.touk.esp.engine.api.MetaData
 import pl.touk.esp.engine.api.process.{Sink, SinkFactory}
 
 class KafkaSinkFactory(kafkaAddress: String,
-                       serializationSchema: SerializationSchema[Any]) extends SinkFactory {
+                       serializationSchema: KeyedSerializationSchema[Any]) extends SinkFactory {
 
   override def create(processMetaData: MetaData, parameters: Map[String, String]): Sink = {
     val topic = parameters(KafkaSinkFactory.TopicParamName)
