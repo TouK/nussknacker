c130074cd1f6d2032525984f0f85728a011be485
Arek Burdach <abr@touk.pl>
2016-09-16 18:31:06 +0200

    Fixed IntelliJ Idea underscoring of cartesian syntax and sequenceU
    Change-Id: Iaa674c5369542b09e439cce92e63e144a7219a76

---
+++ util/src/main/scala/pl/touk/esp/engine/util/validated/ValidatedSyntax.scala
@@ -0,0 +1,28 @@
+package pl.touk.esp.engine.util.validated
+
+import cats.data.{NonEmptyList, _}
+import cats.{Applicative, SemigroupK, Traverse, _}
+
+import scala.language.{higherKinds, reflectiveCalls}
+
+class ValidatedSyntax[Err] {
+
+  implicit val nelSemigroup: Semigroup[NonEmptyList[Err]] =
+    SemigroupK[NonEmptyList].algebra[Err]
+
+  // Using Cartesian syntax causes IntelliJ idea errors
+  val A = Applicative[({type V[B] = ValidatedNel[Err, B]})#V]
+
+  // Using travers syntax causes IntelliJ idea errors
+  implicit class ValidationTraverseOps[T[_]: Traverse, B](traverse: T[ValidatedNel[Err, B]]) {
+    def sequence: ValidatedNel[Err, T[B]] =
+      Traverse[T].sequence[({type V[C] = ValidatedNel[Err, C]})#V, B](traverse)
+  }
+}
+
+object ValidatedSyntax {
+
+  def apply[Err]: ValidatedSyntax[Err] =
+    new ValidatedSyntax[Err]
+
+}
