7b377432fb612eaf7e90c602bb81e99068a46fc4
Michal Fijolek <mfi@touk.pl>
2016-09-23 16:38:55 +0200

    node details content component extracted
    Change-Id: I6ef29787822e878fb535aec31cb1be59536b3e47

---
+++ client/components/graph/NodeUtils.js
@@ -0,0 +1,7 @@
+
+export default {
+  nodeType: (node) => {
+    return node.type ? node.type : "Properties";
+  }
+
+}
--- client/components/graph/nodeDetailsModal.js
+++ client/components/graph/nodeDetailsModal.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import classNames from 'classnames';
 import Modal from 'react-modal';
 import _ from 'lodash';
@@ -9,309 +9,106 @@ import { ListGroupItem } from 'react-bootstrap';
 
 export default class NodeDetailsModal extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-          readOnly: true,
-          tempNodeData: props.node,
-          currentNodeId: props.node.id,
-          pendingRequest: false
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditMode: false,
+      editedNode: props.node,
+      currentNodeId: props.node.id,
+      pendingRequest: false
+    };
+  }
 
-    closeModal = () => {
-        this.setState({readOnly: true})
-        this.props.onClose()
-    }
+  closeModal = () => {
+    this.setState({isEditMode: true})
+    this.props.onClose()
+  }
 
-    createField = (fieldType, fieldLabel, fieldValue, handleChange) => {
-      switch (fieldType) {
-      case 'input':
-        return (
-          <div className="node-row">
-            <div className="node-label">{fieldLabel}</div>
-            <div className="node-value"><input type="text" className="node-input" value={fieldValue} onChange={handleChange} readOnly={this.state.readOnly}/></div>
-          </div>
-        )
-      case 'textarea':
-        return (
-          <div className="node-row">
-            <div className="node-label">{fieldLabel}</div>
-            <div className="node-value"><textarea rows="5" cols="50" className="node-input" value={fieldValue} onChange={handleChange}  readOnly={this.state.readOnly}/></div>
-          </div>
-        )
-      default:
-          return (
-              <div>
-                  Field type not known...
-              </div>
-          )
+  toggleEdition = () => {
+    this.setState({isEditMode: !this.state.isEditMode});
+  }
+
+  editNodeData = () => {
+    this.toggleEdition();
+  }
+
+  updateNodeData = () => {
+    this.setState({pendingRequest: true})
+    this.props.editUsing(this.props.processId, this.state.currentNodeId, this.state.editedNode).then((resp) => {
+      return this.props.onProcessEdit().then(() => {
+        this.setState({currentNodeId: this.state.editedNode.id, isEditMode: false})
+        if (!_.isEmpty(resp.invalidNodes)) {
+          console.error('Errors', resp.invalidNodes)
+        }
+      })
+    }).then(() =>
+      this.setState({pendingRequest: false})
+    ).catch( (error) => {
+      //todo globalna oblsuga bledow
+      this.setState({pendingRequest: false})
+      console.log(error)
+    })
+  }
+
+  nodeAttributes = () => {
+    var nodeAttributes = require('json!../../assets/json/nodeAttributes.json');
+    return nodeAttributes[NodeUtils.nodeType(this.props.node)];
+  }
+
+  updateNodeState = (newNodeState) => {
+    this.setState( { editedNode: newNodeState})
+  }
+
+  render() {
+    var isOpen = !(_.isEmpty(this.props.node))
+    var modalStyles = {
+      overlay: {
+        backgroundColor: 'rgba(63, 62, 61, 0.3)'
+      },
+      content: {
+        borderRadius: '0',
+        padding: '0',
+        left: '20%',
+        right: '20%',
+        top: '15%',
+        bottom: '15%',
+        border: 'none'
       }
-    }
+    };
 
-    customNode = () => {
-        switch (this.nodeType()) {
-            case 'Source':
-              return this.sourceSinkCommon()
-            case 'Sink':
-              return this.sourceSinkCommon()
-            case 'Filter':
-              return (
-                <div className="node-table-body">
-                  {this.createField("input",  "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-                  {this.createField("textarea", "Expression:", this.state.tempNodeData.expression.expression, ((event) => this.setNodeDataAt('expression.expression', event.target.value) ))}
-                </div>
-              )
-              case 'Enricher':
-              case 'Processor':
-                return (
-                  <div className="node-table-body">
-                    {this.createField("input",  "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-                    <div className="node-row">
-                      <div className="node-label">Service:</div>
-                      <div className="node-group">
-                        {this.createField("input", "Service Id:", this.state.tempNodeData.service.id, ((event) => this.setNodeDataAt('service.id', event.target.value) ))}
-                        <div className="node-row">
-                          <div className="node-label">Parameters:</div>
-                          <div className="node-group child-group">
-                            {this.state.tempNodeData.service.parameters.map ((params, index) => {
-                              return (
-                                <div className="node-block" key={index}>
-                                  {this.createField("input", "Name:", params.name, ((event) => this.setNodeDataAt(`service.parameters[${index}].name`, event.target.value) ))}
-                                  {this.createField("input", "Expression:", params.expression.expression, ((event) => this.setNodeDataAt(`service.parameters[${index}].expression.expression`, event.target.value) ))}
-                                </div>
-                              )
-                            })}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    {this.props.node.type == 'Enricher' ? this.createField("input", "Output:", this.state.tempNodeData.output, ((event) => this.setNodeDataAt('output', event.target.value) )) : null }
-                  </div>
-                )
-            case 'CustomNode':
-              return (
-                <div className="node-table-body">
-                  {this.createField("input",  "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-                  {this.createField("input",  "Ouput:", this.state.tempNodeData.outputVar, ((event) => this.setNodeDataAt('outputVar', event.target.value) ))}
-                  {this.createField("input",  "Node type:", this.state.tempNodeData.customNodeRef, ((event) => this.setNodeDataAt('customNodeRef', event.target.value) ))}
-                    <div className="node-row">
-                      <div className="node-label">Parameters:</div>
-                      <div className="node-group child-group">
-                        {this.state.tempNodeData.parameters.map ((params, index) => {
-                          return (
-                            <div className="node-block" key={index}>
-                              {this.createField("input", "Name:", params.name, ((event) => this.setNodeDataAt(`parameters[${index}].name`, event.target.value) ))}
-                              {this.createField("input", "Expression:", params.expression.expression, ((event) => this.setNodeDataAt(`parameters[${index}].expression.expression`, event.target.value) ))}
-                            </div>
-                          )
-                        })}
-                      </div>
-                    </div>
+    var buttonClasses = classNames('modalButton')
+    var editButtonClasses = classNames(buttonClasses, 'pull-left', {'hidden': this.state.isEditMode})
+    var saveButtonClasses = classNames(buttonClasses, 'pull-left', {'hidden': !this.state.isEditMode})
 
-                </div>
-              )
-            case 'VariableBuilder':
-                return (
-                  <div className="node-table-body">
-                    {this.createField("input",  "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-                    {this.createField("input", "Variable Name:", this.state.tempNodeData.varName, ((event) => this.setNodeDataAt('varName', event.target.value) ))}
-                    <div className="node-row">
-                      <div className="node-label">Fields:</div>
-                      <div className="node-group child-group">
-                        {this.state.tempNodeData.fields.map ((params, index) => {
-                          return (
-                            <div className="node-block" key={index}>
-                              {this.createField("input", "Name:", params.name, ((event) => this.setNodeDataAt(`fields[${index}].name`, event.target.value) ))}
-                              {this.createField("input", "Expression:", params.expression.expression, ((event) => this.setNodeDataAt(`fields[${index}].expression.expression`, event.target.value) ))}
-                            </div>
-                          )
-                        })}
-                      </div>
-                    </div>
-                  </div>
-                )
-            case 'Switch':
-              return (
-                <div className="node-table-body">
-                  {this.createField("input",  "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-                  {this.createField("input", "Expression:", this.state.tempNodeData.expression.expression, ((event) => this.setNodeDataAt('expression.expression', event.target.value) ))}
-                  {this.createField("input", "exprVal:", this.props.node.exprVal, ((event) => this.setNodeDataAt('exprVal', event.target.value) ))}
-                </div>
-              )
-            case 'Aggregate':
-              return (
-                <div className="node-table-body">
-                  {this.createField("input",  "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-                  {this.createField("textarea", "Key Expression:", this.state.tempNodeData.keyExpression.expression, ((event) => this.setNodeDataAt('keyExpression.expression', event.target.value) ))}
-                  {this.createField("textarea", "Trigger Expression:", this.state.tempNodeData.triggerExpression.expression, ((event) => this.setNodeDataAt('triggerExpression.expression', event.target.value) ))}
-                  {this.createField("input", "Folding function", this.state.tempNodeData.foldingFunRef, ((event) => this.setNodeDataAt('foldingFunRef', event.target.value) ))}
-                  {this.createField("input", "Duration (ms):", this.state.tempNodeData.durationInMillis, ((event) => this.setNodeDataAt('durationInMillis', event.target.value) ))}
-                  {this.createField("input", "Slide (ms):", this.state.tempNodeData.slideInMillis, ((event) => this.setNodeDataAt('slideInMillis', event.target.value) ))}
-                </div>
-              )
-            case 'Properties':
-              return (
-                <div className="node-table-body">
-                  {this.createField("input", "Parallelism:", this.state.tempNodeData.parallelism, ((event) => this.setNodeDataAt('parallelism', event.target.value) ))}
-                  <div className="node-row">
-                    <div className="node-label">Exception handler:</div>
-                    <div className="node-group child-group">
-                      {this.state.tempNodeData.exceptionHandler.parameters.map((params, index) => {
-                        return (
-                            <div className="node-block" key={index}>
-                              {this.createField("input", "Name:", params.name, ((event) => this.setNodeDataAt(`exceptionHandler.parameters[${index}].name`, event.target.value) ))}
-                              {this.createField("input", "Value:", params.value, ((event) => this.setNodeDataAt(`exceptionHandler.parameters[${index}].value`, event.target.value) ))}
-                            </div>
-                        )
-                      })}
-                    </div>
-                  </div>
-                </div>
-            )
-          default:
-            return (
-              <div>
-                Node type not known.
-                <NodeDetails node={this.props.node}/>
-              </div>
-            )
-        }
-    }
+    var headerStyles = {
+      backgroundColor: this.nodeAttributes().styles.fill,
+      color: this.nodeAttributes().styles.color
+    };
 
-    sourceSinkCommon() {
-      return (
-        <div className="node-table-body">
-          {this.createField("input", "Id:", this.state.tempNodeData.id, ((event) => this.setNodeDataAt('id', event.target.value) ))}
-          <div className="node-row">
-            <div className="node-label">Ref:</div>
-            <div className="node-group">
-              {this.createField("input", "Type:", this.state.tempNodeData.ref.typ, ((event) => this.setNodeDataAt('ref.typ', event.target.value) ))}
-              <div className="node-row">
-                <div className="node-label">Parameters:</div>
-                <div className="node-group child-group">
-                  {this.state.tempNodeData.ref.parameters.map((params, index) => {
-                    return (
-                      <div className="node-block" key={index}>
-                        {this.createField("input", "Name:", params.name, ((event) => this.setNodeDataAt(`ref.parameters[${index}].name`, event.target.value)))}
-                        {this.createField("input", "Value:", params.value, ((event) => this.setNodeDataAt(`ref.parameters[${index}].value`, event.target.value)))}
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
+    return (
+      <div className="objectModal">
+        <Modal isOpen={isOpen} style={modalStyles} onRequestClose={this.closeModal}>
+          <div id="modalHeader" style={headerStyles}>
+            <span>{NodeUtils.nodeType(this.props.node)}</span>
+          </div>
+          <div id="modalContent">
+            <NodeDetailsContent isEditMode={this.state.isEditMode} node={this.state.editedNode}
+                                validationErrors={this.props.validationErrors} onChange={this.updateNodeState}/>
+          </div>
+          <div id="modalFooter">
+            <div>
+              <LaddaButton title="Save node details" className={saveButtonClasses} loading={this.state.pendingRequest}
+                           buttonStyle='zoom-in' onClick={this.updateNodeData}>Save</LaddaButton>
+              <button type="button" title="Edit node details" className={editButtonClasses} onClick={this.editNodeData}>
+                Edit
+              </button>
+              <button type="button" title="Close node details" className={buttonClasses} onClick={this.closeModal}>
+                Close
+              </button>
             </div>
           </div>
-        </div>
-      )
-    }
-
-    setNodeDataAt = (propToMutate, newValue) => {
-      var newtempNodeData = _.cloneDeep(this.state.tempNodeData)
-      _.set(newtempNodeData, propToMutate, newValue)
-      this.setState( { tempNodeData: newtempNodeData} )
-    }
-
-    contentForNode = () => {
-        if (!_.isEmpty(this.props.node)) {
-          var nodeClass = classNames('node-table', { 'node-editable': !this.state.readOnly})
-          return (
-            <div className={nodeClass}>
-              {this.customNode()}
-              {!_.isEmpty(this.props.validationErrors) ? <ListGroupItem bsStyle="danger">Node is invalid</ListGroupItem> : null}
-            </div>
-          )
-        }
-    }
-
-    toggleReadOnly = () => {
-      this.setState({readOnly: !this.state.readOnly});
-    }
-
-    editNodeData = () => {
-      this.toggleReadOnly();
-    }
-
-    updateNodeData = () => {
-      this.setState({ pendingRequest: true})
-      this.props.editUsing(this.props.processId, this.state.currentNodeId, this.state.tempNodeData).then((resp) => {
-        return this.props.onProcessEdit().then(() => {
-          this.setState({tempNodeData: this.state.tempNodeData, currentNodeId: this.state.tempNodeData.id})
-          this.toggleReadOnly();
-          if (!_.isEmpty(resp.invalidNodes)) {
-            console.error('Errors', resp.invalidNodes)
-          }
-        })
-      }).then(() =>
-        this.setState({ pendingRequest: false})
-      )
-    }
-
-    nodeAttributes = () => {
-      var nodeAttributes = require('json!../../assets/json/nodeAttributes.json');
-      return nodeAttributes[this.nodeType()];
-    }
-
-    nodeType = () => {
-      return this.props.node.type ? this.props.node.type : "Properties";
-    }
-
-    render () {
-        var isOpen = !(_.isEmpty(this.props.node))
-        var modalStyles = {
-          overlay: {
-            backgroundColor: 'rgba(63, 62, 61, 0.3)'
-          },
-          content : {
-            borderRadius: '0',
-            padding: '0',
-            left: '20%',
-            right: '20%',
-            top: '15%',
-            bottom: '15%',
-            border: 'none'
-          }
-        };
-
-      var buttonClasses = classNames('modalButton')
-      var editButtonClasses = classNames(buttonClasses, 'pull-left', { 'hidden': !this.state.readOnly})
-      var saveButtonClasses = classNames(buttonClasses, 'pull-left', { 'hidden': this.state.readOnly})
-
-      if (!_.isEmpty(this.props.node)) {
-          var headerStyles = {
-            backgroundColor: this.nodeAttributes().styles.fill,
-            color: this.nodeAttributes().styles.color
-          };
-        };
-
-        return (
-            <div className="objectModal">
-                <Modal isOpen={isOpen} style={modalStyles} onRequestClose={this.closeModal}>
-                    <div id="modalHeader" style={headerStyles}>
-                        <span>{this.nodeType()}</span>
-                    </div>
-                    <div id="modalContent">
-                        {this.contentForNode()}
-                    </div>
-                    <div id="modalFooter">
-                      <div>
-                        <LaddaButton title="Save node details" className={saveButtonClasses} loading={this.state.pendingRequest} buttonStyle='zoom-in' onClick={this.updateNodeData}>Save</LaddaButton>
-                        <button type="button" title="Edit node details" className={editButtonClasses} onClick={this.editNodeData}>Edit</button>
-                        <button type="button" title="Close node details" className={buttonClasses} onClick={this.closeModal}>Close</button>
-                      </div>
-                    </div>
-                </Modal>
-            </div>
-        );
-    }
-}
-
-class NodeDetails extends React.Component {
-    render() {
-        return (
-            <div>
-                <pre>{JSON.stringify(this.props.node, null, 2)}</pre>
-            </div>
-        );
-    }
+        </Modal>
+      </div>
+    );
+  }
 }
