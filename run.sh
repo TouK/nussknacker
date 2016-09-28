0c697653a391653f6bb4ec88edb68a07ba791c9f
Maciek Próchniak <mpr@touk.pl>
2016-09-28 12:45:54 +0200

    fix importow
    Change-Id: Ic207d61e443cb01fa1af33372a86526859781a72

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-java -Dlogback.configurationFile=./logback.xml -Dconfig.file=./server/develConf/application.conf -jar ./server/target/scala-2.11/esp-ui-assembly-0.1-SNAPSHOT.jar 8080 ./server/develConf/jsons 
+java -Dlogback.configurationFile=./logback.xml -Dconfig.file=./server/develConf/application.conf -jar ./server/target/scala-2.11/esp-ui-assembly-0.1-SNAPSHOT.jar 8081 ./server/develConf/jsons 
 
