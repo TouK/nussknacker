82cfa1d160d712c36dba4793ac8b1665dd56237f
Maciek Próchniak <mpr@touk.pl>
2016-10-05 17:27:26 +0200

    first try of notification system
    Change-Id: I8cbc9e506ba36005adf3fc75b0bb8b5bc5652005

--- client/containers/Root.production.js
+++ client/containers/Root.production.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from '../store/configureStore.production';
+import NotificationSystem from 'react-notification-system';
+import HttpService from '../http/HttpService'
 
 import EspAppRouter from './EspAppRouter';
 
@@ -9,10 +11,13 @@ const store = configureStore();
 
 export default class Root extends React.Component {
 
+
+
   render() {
     return (
       <Provider store={store}>
         <div>
+          <NotificationSystem ref={(c) => HttpService.setNotificationSystem(c)} />
           <EspAppRouter/>
         </div>
       </Provider>
