e327d99c3f14b9e7eae55894b19cc94840a18a0c
Maciek Próchniak <mpr@touk.pl>
2016-12-11 16:41:43 +0100

    better error handling + node details fixes
    Change-Id: I2fa5a05371ce30b04c0c2fd73b1a0e3a260cc571

--- server/src/main/scala/pl/touk/esp/ui/api/ProcessValidation.scala
+++ server/src/main/scala/pl/touk/esp/ui/api/ProcessValidation.scala
@@ -2,9 +2,10 @@ package pl.touk.esp.ui.api
 
 import cats.data.NonEmptyList
 import pl.touk.esp.engine.canonicalgraph.CanonicalProcess
+import pl.touk.esp.engine.compile.ProcessCompilationError.ExpressionParseError
 import pl.touk.esp.engine.compile.{ProcessCompilationError, ProcessValidator}
 import pl.touk.esp.engine.util.ReflectUtils
-import pl.touk.esp.ui.api.ProcessValidation.ValidationResult
+import pl.touk.esp.ui.api.ProcessValidation.{NodeValidationError, ValidationResult}
 
 class ProcessValidation(processValidator: ProcessValidator) {
 
@@ -27,18 +28,27 @@ class ProcessValidation(processValidator: ProcessValidator) {
     )
   }
 
-  private def formatErrorMessage(error: ProcessCompilationError): String = {
-    ReflectUtils.fixedClassSimpleNameWithoutParentModule(error.getClass) // TODO: better messages
+  private def formatErrorMessage(error: ProcessCompilationError): NodeValidationError = {
+    val typ = ReflectUtils.fixedClassSimpleNameWithoutParentModule(error.getClass)
+    error match {
+      case ExpressionParseError(message, _, fieldName, _) => NodeValidationError(typ, message, fieldName)
+
+
+      case _ => NodeValidationError(typ, typ, None)
+
+    }
   }
 
 }
 
 object ProcessValidation {
 
-  case class ValidationResult(invalidNodes: Map[String, List[String]]) {
+  case class ValidationResult(invalidNodes: Map[String, List[NodeValidationError]]) {
     def filterNode(nodeId: String) =
       copy(invalidNodes = invalidNodes.filterKeys(_ == nodeId))
   }
+
+  case class NodeValidationError(typ: String, message: String, fieldName: Option[String])
 
   object ValidationResult {
     val success = ValidationResult(Map.empty)
