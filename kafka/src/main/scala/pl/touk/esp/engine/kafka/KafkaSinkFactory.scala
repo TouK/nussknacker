8320a9f715d53640795ad0f60ed44b7c6efc9900
Michal Fijolek <mfi@touk.pl>
2016-12-16 12:57:45 +0100

    partition by key flink kafka producer
    Change-Id: I9968bb4a0be1db532bd6d3cf5b21f0f5f61ea3f8

--- kafka/src/main/scala/pl/touk/esp/engine/kafka/KafkaSinkFactory.scala
+++ kafka/src/main/scala/pl/touk/esp/engine/kafka/KafkaSinkFactory.scala
@@ -1,34 +1,21 @@
 package pl.touk.esp.engine.kafka
 
-import java.util.Properties
-
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09
 import org.apache.flink.streaming.util.serialization.KeyedSerializationSchema
-import pl.touk.esp.engine.api.{MetaData, ParamName}
 import pl.touk.esp.engine.api.process.{Sink, SinkFactory}
-import KafkaSinkFactory._
-import pl.touk.esp.engine.flink.api.process.{FlinkSink, FlinkSourceFactory}
-
-import collection.JavaConverters._
+import pl.touk.esp.engine.api.{MetaData, ParamName}
+import pl.touk.esp.engine.flink.api.process.FlinkSink
+import pl.touk.esp.engine.kafka.KafkaSinkFactory._
 
 class KafkaSinkFactory(config: KafkaConfig,
                        serializationSchema: KeyedSerializationSchema[Any]) extends SinkFactory {
 
   def create(processMetaData: MetaData, @ParamName(`TopicParamName`) topic: String): Sink = {
     new FlinkSink {
-      //we give null as partitioner to use default kafka partition behaviour... 
-      //default behaviour should partition by key
-      override def toFlinkFunction: SinkFunction[Any] = new FlinkKafkaProducer09(
-        topic, serializationSchema, kafkaProperties(), null)
+      override def toFlinkFunction: SinkFunction[Any] = {
+        PartitionByKeyFlinkKafkaProducer09(config.kafkaAddress, topic, serializationSchema, config.kafkaProperties)
+      }
     }
-  }
-
-  private def kafkaProperties(): Properties = {
-    val props = new Properties()
-    props.setProperty("bootstrap.servers", config.kafkaAddress)
-    config.kafkaProperties.map(_.asJava).foreach(props.putAll)
-    props
   }
 }
 
