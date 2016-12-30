bbb35cc3de01d22cfb96fe5b69228aae8adb480d
Maciek Próchniak <mpr@touk.pl>
2016-12-30 16:37:00 +0100

    fixes
    Change-Id: If785fd30b1a9e108414f1c7016567d7f8ab5d4a9

--- client/common/TestResultUtils.js
+++ client/common/TestResultUtils.js
@@ -4,11 +4,11 @@ class TestResultUtils {
 
 
   _nodeResults = (testResults, nodeId) => {
-    return (testResults._nodeResults || {})[nodeId] || []
+    return (testResults.nodeResults || {})[nodeId] || []
   }
 
   _invocationResults = (testResults, nodeId) => {
-    return (testResults._invocationResults || {})[nodeId] || []
+    return (testResults.invocationResults || {})[nodeId] || []
   }
 
   resultsForNode = (testResults, nodeId) => {
@@ -23,9 +23,9 @@ class TestResultUtils {
   }
 
   nodeResultsForContext = (nodeTestResults, contextId) => {
-    var context = (nodeTestResults._nodeResults.find(result => result.context.id == contextId) || {}).context
+    var context = (nodeTestResults.nodeResults.find(result => result.context.id == contextId) || {}).context
     var expressionResults = _.fromPairs(nodeTestResults
-      ._invocationResults
+      .invocationResults
       .filter(result => result.context.id == contextId)
       .map(result => [result.name, result.value]))
     return {
@@ -36,7 +36,7 @@ class TestResultUtils {
 
   availableContexts = (testResults) => {
     var varToInclude = "input"
-    return _.uniq(testResults._nodeResults.map(nr => ({id: nr.context.id, [varToInclude]: nr.context.variables[varToInclude]})))
+    return _.uniq(testResults.nodeResults.map(nr => ({id: nr.context.id, [varToInclude]: nr.context.variables[varToInclude]})))
 
   }
 }
