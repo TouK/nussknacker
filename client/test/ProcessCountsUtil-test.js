c2c2dae11cb1bc36a567e2433f7b01b202282312
Michal Fijolek <mfi@touk.pl>
2017-05-09 11:56:32 +0200

    test process counts bug fixed
    Change-Id: Ib6b5167048aff681f3f1e27a5230a7e5c4034db0

--- client/test/ProcessCountsUtil-test.js
+++ client/test/ProcessCountsUtil-test.js
@@ -1,6 +1,15 @@
 import ProcessCountsUtil from '../common/ProcessCountsUtil'
 
 describe("process counts", () => {
+  const nodesWithGroups = [
+    { "id": "node3"},
+    {
+      "id": "node1-node2",
+      "type": "_group",
+      "nodes": [{ "id": "node1",}, { "id": "node2",}],
+      "ids": ["node1", "node2"]
+    }
+  ]
 
   it("should find counts for groups", () => {
     const processCountsWithoutGroups = {
@@ -8,15 +17,6 @@ describe("process counts", () => {
       "node2": { "all": 2, "errors": 0},
       "node3": { "all": 2, "errors": 0}
     }
-    const nodesWithGroups = [
-      { "id": "node3"},
-      {
-        "id": "node1-node2",
-        "type": "_group",
-        "nodes": [{ "id": "node1",}, { "id": "node2",}],
-        "ids": ["node1", "node2"]
-      }
-    ]
 
     expect(ProcessCountsUtil.processCounts(processCountsWithoutGroups, nodesWithGroups)).toEqual({
       "node1-node2": { "all": 2, "errors": 0},
@@ -36,19 +36,28 @@ describe("process counts", () => {
         "node2": [{ "context": {}}, { "context": {}}]
       }
     }
-    const nodesWithGroups = [
-      { "id": "node3"},
-      {
-        "id": "node1-node2",
-        "type": "_group",
-        "nodes": [{ "id": "node1",}, { "id": "node2",}],
-        "ids": ["node1", "node2"]
-      }
-    ]
-    
+
     expect(ProcessCountsUtil.processCountsForTests(testResults, nodesWithGroups)).toEqual({
       "node1-node2": { "all": 2, "errors": 0},
       "node3": { "all": 2, "errors": 0},
+      "node1": { "all": 2, "errors": 0},
+      "node2": { "all": 2, "errors": 0}
+    })
+  })
+
+  it("should find counts for uncompleted test results", () => {
+    const testResults = {
+      "mockedResults": {},
+      "invocationResults": {},
+      "nodeResults": {
+        "node1": [{ "context": {}}, { "context": {}}],
+        "node2": [{ "context": {}}, { "context": {}}]
+      }
+    }
+
+    expect(ProcessCountsUtil.processCountsForTests(testResults, nodesWithGroups)).toEqual({
+      "node1-node2": { "all": 2, "errors": 0},
+      "node3": { "all": 0, "errors": 0},
       "node1": { "all": 2, "errors": 0},
       "node2": { "all": 2, "errors": 0}
     })
