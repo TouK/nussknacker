7e501d1a88b85953ebe73aa87a3366e040652690
Dominik Przybysz <dpr@touk.pl>
2017-06-27 09:52:40 +0200

    Allow for custom args for sbt build
    Change-Id: I31a968fb5557511b6da375d34a4bdeadd7a485e2

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+EXTRA_ARGS=$@
 
 cd client
 npm install
@@ -11,4 +12,4 @@ cp client/main.html server/src/main/resources/web
 cp -r client/dist server/src/main/resources/web/static
 
 cd server
-./sbtwrapper 'set test in assembly := {}' clean assembly
+./sbtwrapper $EXTRA_ARGS 'set test in assembly := {}' clean assembly
