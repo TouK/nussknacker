2171f2bc4a8beea430a9b3521c0fba0366ebdc86
Maciek Próchniak <mpr@touk.pl>
2017-07-03 14:15:02 +0200

    jenkins build script - temporary fix
    Change-Id: I26f287cb6451339836842e667c5331b94b49b681

---
+++ ciBuildOnMaster.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+runAndExitOnFail() {
+    command=$1
+    $command
+    result=${PIPESTATUS[0]}
+    if [[ ${result} -eq 0 ]]
+    then
+        echo "$command SUCCESS!"
+    else
+        echo "$command FAILURE!"
+        exit ${result}
+    fi
+}
+
+#TODO: only temporary, we should have just one sbt project
+cd engine
+runAndExitOnFail "./ciBuildOnMaster.sh"
+cd ui
+runAndExitOnFail "./ciBuild.sh"
+
+
