b3f313ed49b274d8b27e088f72c3c8ed3c50a99a
Maciek Próchniak <mpr@touk.pl>
2017-07-03 14:15:02 +0200

    jenkins build script - temporary fix
    Change-Id: I26f287cb6451339836842e667c5331b94b49b681

---
+++ ciBuild.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+espEngineToukVersion=$1
+
+runAndExitOnFail() {
+    command=$1
+    $command
+    result=${PIPESTATUS[0]}
+    if [[ ${result} -eq 0 ]]
+    then
+        echo "$command SUCCESS!"
+    else
+        echo "$command FAILURE!"
+        exit ${result}
+    fi
+}
+
+#TODO: only temporary, we should have just one sbt project
+cd engine
+runAndExitOnFail "./ciBuild.sh $espEngineToukVersion"
+cd ui
+runAndExitOnFail "./ciBuild.sh $espEngineToukVersion"
