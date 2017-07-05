be2233f6e747314665341747c74a537d539c673e
Piotr Monarski <pmn@touk.pl>
2017-07-05 11:24:22 +0200

    Simplify application root
    Change-Id: I88f550734e3473127dd1304a216e9f47c20abbe9

--- ui/client/index.js
+++ ui/client/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Root from './containers/Root'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import NotificationSystem from 'react-notification-system';
+import $ from 'jquery';
 
-render((
-  <Root/>
-), document.getElementById('rootApp'));
+import configureStore from './store/configureStore';
+import HttpService from './http/HttpService'
+import Settings from './http/Settings'
+import EspAppRouter from './containers/EspAppRouter';
+
+import "./stylesheets/notifications.styl";
+
+const store = configureStore();
+Settings.updateSettings(store);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <div>
+        <NotificationSystem ref={(c) => HttpService.setNotificationSystem(c)} style={false} />
+        <EspAppRouter store={store}/>
+      </div>
+    </Provider>
+  ,
+  document.getElementById('rootApp')
+);
---
+++ ui/client/store/configureStore.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers';
+
+export default function configureStore() {
+
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    ),
+  );
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () =>
+      store.replaceReducer(require('../reducers').default)
+    );
+  }
+
+  return store;
+}
