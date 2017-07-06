313e628161999372f8133a257e76ccb1da373e07
Maciek Próchniak <mpr@touk.pl>
2017-07-06 09:46:56 +0200

    ui build scripts
    Change-Id: Ifdf065f717e0fb7dd73795db302a4d20a3a002c3

--- ui/run.sh
+++
@@ -1,2 +0,0 @@
-cd server
-java -Dlogback.configurationFile=./logback.xml -Dconfig.file=./develConf/application.conf -jar ./target/scala-2.11/esp-ui-assembly-0.1-SNAPSHOT.jar 8081 ./develConf/jsons
--- ui/runServer.sh
+++ ui/runServer.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/env bash
-
-cd ..
-./sbtwrapper -DincludeFlinkAndScala=true 'set test in assembly := {}' ui/assembly
-cd -
-./run.sh
+cd server
+java -Dlogback.configurationFile=./logback.xml -Dconfig.file=./develConf/application.conf -jar ./target/scala-2.11/esp-ui-assembly-0.1-SNAPSHOT.jar 8081 ./develConf/jsons
