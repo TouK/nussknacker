8de16047566f6d76eb3edc35b779a1dc9de534d0
Maciek Próchniak <mpr@touk.pl>
2017-07-18 10:59:11 +0200

    HA configuration in docker tests
    Change-Id: I817152dd8916a9c2dee49cd3a4c8cea64b11669b

--- engine/management/src/it/resources/docker/entrypointWithIP.sh
+++ engine/management/src/it/resources/docker/entrypointWithIP.sh
@@ -4,6 +4,6 @@ if [[ -z "$JOB_MANAGER_RPC_ADDRESS" && -n "$JOB_MANAGER_RPC_ADDRESS_COMMAND" ]];
 fi
 echo "Using JOB_MANAGER_RPC_ADDRESS: $JOB_MANAGER_RPC_ADDRESS"
 
-echo -e "\nstate.savepoints.dir: file:///tmp/savepoints" >> $FLINK_HOME/conf/flink-conf.yaml
+cat /conf.yml >> $FLINK_HOME/conf/flink-conf.yaml
 
 /docker-entrypoint.sh "$@"
