7d9c2fb6102fda67eb4a9b97ec6780938b72cdf8
Michal Fijolek <mfi@touk.pl>
2017-09-06 14:48:19 +0200

    ciBuildOnMaster.sh - publish snapshot with unique name after every build
    Change-Id: I4b854c24cdf5edb0702f7bce100a57b24529c4e8

--- ciBuildOnMaster.sh
+++ ciBuildOnMaster.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-./sbtwrapper  -DnexusPassword=$1 clean test management/it:test publish
+nexusPassword=$1
+nexusHost=$2
+
+currentHashCommit=`git rev-parse HEAD`
+formattedDate=`date '+%Y-%m-%d-%H-%M'`
+currentVersion=`cat version.sbt | grep "version in ThisBuild :=" | grep -Po '= "\K[^"]*'`
+version=${formattedDate}-${currentHashCommit}-${currentVersion}
+
+if [ -z "$nexusPassword" ]; then
+    echo "nexusPassword missing"; exit -1
+fi
+if [ -z "$nexusHost" ]; then
+    echo "nexusHost missing"; exit -1
+fi
+
+echo publishing nussknacker version: $version
+./sbtwrapper clean test management/it:test
+./sbtwrapper -DnexusPassword=${nexusPassword} -DnexusHost=${nexusHost} "set version in ThisBuild := \"$version\"" publish
