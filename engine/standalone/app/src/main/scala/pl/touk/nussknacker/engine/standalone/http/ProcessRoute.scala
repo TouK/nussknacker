6725992b31ba7307f8c0726920a8005dbdb4f120
Maciek Próchniak <mpr@touk.pl>
2017-12-21 09:03:34 +0100

    fix for handling null messages in standalone errors
    Change-Id: I097c0053ba293e7a5c60042ad382160c63bd90a3

--- engine/standalone/app/src/main/scala/pl/touk/nussknacker/engine/standalone/http/ProcessRoute.scala
+++ engine/standalone/app/src/main/scala/pl/touk/nussknacker/engine/standalone/http/ProcessRoute.scala
@@ -4,8 +4,10 @@ import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.{Directives, Route}
 import argonaut.Argonaut._
 import argonaut.ArgonautShapeless._
+import cats.data.NonEmptyList
 import com.typesafe.scalalogging.LazyLogging
 import pl.touk.http.argonaut.Argonaut62Support
+import pl.touk.nussknacker.engine.api.exception.EspExceptionInfo
 import pl.touk.nussknacker.engine.standalone.StandaloneRequestHandler
 import pl.touk.nussknacker.engine.standalone.deployment.DeploymentService
 
@@ -23,7 +25,8 @@ class ProcessRoute(deploymentService: DeploymentService) extends Directives with
           }
         case Some(processInterpreter) => new StandaloneRequestHandler(processInterpreter).invoke {
           case Left(errors) => complete {
-            (StatusCodes.InternalServerError, errors.toList.map(info => EspError(info.nodeId, info.throwable.getMessage)).asJson)
+            logErrors(processPath, errors)
+            (StatusCodes.InternalServerError, errors.toList.map(info => EspError(info.nodeId, Option(info.throwable.getMessage))).asJson)
           }
           case Right(results) => complete {
             (StatusCodes.OK, results)
@@ -33,6 +36,13 @@ class ProcessRoute(deploymentService: DeploymentService) extends Directives with
     }
 
 
-  case class EspError(nodeId: Option[String], message: String)
+  private def logErrors(processPath: JsonField, errors: NonEmptyList[EspExceptionInfo[_ <: Throwable]]) = {
+    logger.warn(s"Failed to invoke: $processPath with errors: ${errors.map(_.throwable.getMessage)}")
+    errors.toList.foreach { error =>
+      logger.info(s"Invocation failed $processPath, error in ${error.nodeId}: ${error.throwable.getMessage}", error.throwable)
+    }
+  }
+
+  case class EspError(nodeId: Option[String], message: Option[String])
 
 }
