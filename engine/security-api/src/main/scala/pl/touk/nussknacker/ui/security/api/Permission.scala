573d24602779becfc5028fe9a4c1e7d7c1c59409
Wojciech Decker <wdk@touk.pl>
2018-02-14 12:25:08 +0100

    Permission enum has string values
    Change-Id: Ia77abb4f8043709bd99181b006946b1ca76c89bb

--- engine/security-api/src/main/scala/pl/touk/nussknacker/ui/security/api/Permission.scala
+++ engine/security-api/src/main/scala/pl/touk/nussknacker/ui/security/api/Permission.scala
@@ -2,5 +2,8 @@ package pl.touk.nussknacker.ui.security.api
 
 object Permission extends Enumeration {
   type Permission = Value
-  val Read, Write, Deploy, Admin = Value
+  val Read = Value("Read")
+  val Write = Value("Write")
+  val Deploy = Value("Deploy")
+  val Admin = Value("Admin")
 }
