dcebf2ecd0e3a97b1be80cd50884ee9da58d6baa
Maciek Próchniak <mpr@touk.pl>
2018-02-22 08:45:01 +0100

    demo fixes for new version
    Change-Id: I4da5999d930a20a7d86e8c81de5f262cd0b7cddc

--- engine/example/src/main/scala/pl/touk/nussknacker/engine/example/Model.scala
+++ engine/example/src/main/scala/pl/touk/nussknacker/engine/example/Model.scala
@@ -1,7 +1,10 @@
 package pl.touk.nussknacker.engine.example
 
-import pl.touk.nussknacker.engine.api.DisplayableAsJson
+import pl.touk.nussknacker.engine.api.{DisplayableAsJson, Documentation}
 import argonaut.ArgonautShapeless._
 
-case class Transaction(clientId: String, amount: Int, eventDate: Long = System.currentTimeMillis()) extends DisplayableAsJson[Transaction]
+import scala.annotation.meta.getter
+
+case class Transaction(@(Documentation @getter)(description = "Client id, should be in format: 'Client1'")
+  clientId: String, amount: Int, eventDate: Long = System.currentTimeMillis()) extends DisplayableAsJson[Transaction]
 case class Client(id: String, name: String, cardNumber: String) extends DisplayableAsJson[Client]
--- engine/example/src/main/scala/pl/touk/nussknacker/engine/example/UtilProcessHelper.scala
+++ engine/example/src/main/scala/pl/touk/nussknacker/engine/example/UtilProcessHelper.scala
@@ -1,13 +1,23 @@
 package pl.touk.nussknacker.engine.example
 
+import pl.touk.nussknacker.engine.api.{Documentation, ParamName}
+
+import scala.util.Random
+
 object UtilProcessHelper {
 
   import argonaut.Argonaut._
 
   import scala.collection.JavaConversions._
 
+  @Documentation(description = "Convert map to JSON")
   def mapToJson(map: java.util.Map[String, String]) = {
     map.toMap.asJson.nospaces
   }
 
+  @Documentation(description = "Get random number")
+  def random(@ParamName("To (exclusive)") to: Int): Unit = {
+    Random.nextInt(to)
+
+  }
 }
