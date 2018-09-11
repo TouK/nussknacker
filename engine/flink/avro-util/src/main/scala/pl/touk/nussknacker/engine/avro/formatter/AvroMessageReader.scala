a05b1168933df85bc37d9e8afe427828391d9dfe
Arek Burdach <arek.burdach@gmail.com>
2018-09-11 12:28:23 +0200

    avroFlinkUtil module failing compilation fix - removed dependency to kafka.utils.VerifiableProperties
    Change-Id: I537c84f0b23a156ce8a38587f46b86925aed6810

--- engine/flink/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/formatter/AvroMessageReader.scala
+++ engine/flink/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/formatter/AvroMessageReader.scala
@@ -1,12 +1,15 @@
 package pl.touk.nussknacker.engine.avro.formatter
 
-import java.io.IOException
+import java.io.{ByteArrayOutputStream, IOException}
+import java.nio.ByteBuffer
 
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerializer
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException
+import io.confluent.kafka.serializers.{AbstractKafkaAvroSerDe, NonRecordContainer}
 import org.apache.avro.Schema.Type
-import org.apache.avro.generic.GenericDatumReader
-import org.apache.avro.io.DecoderFactory
+import org.apache.avro.generic.{GenericDatumReader, GenericDatumWriter}
+import org.apache.avro.io.{DecoderFactory, EncoderFactory}
+import org.apache.avro.specific.{SpecificDatumWriter, SpecificRecord}
 import org.apache.avro.util.Utf8
 import org.apache.avro.{AvroRuntimeException, Schema}
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -14,7 +17,7 @@ import org.apache.kafka.common.errors.SerializationException
 
 /**
   * This class is mainly copy-paste of Confluent's AvroMessageReader but with better constructor handling
-  * both passing schemaRegistryClient and keySeparator.
+  * both passing schemaRegistryClient and keySeparator and without dependency to kafka.utils.VerifiableProperties.
   *
   * @param schemaRegistryClient schema registry client
   * @param topic topic
@@ -23,13 +26,15 @@ import org.apache.kafka.common.errors.SerializationException
   */
 private[formatter] class AvroMessageReader(schemaRegistryClient: SchemaRegistryClient, topic: String,
                                            parseKey: Boolean, keySeparator: String)
-  extends AbstractKafkaAvroSerializer {
+  extends AbstractKafkaAvroSerDe {
 
   schemaRegistry = schemaRegistryClient
 
   private val keySubject = topic + "-key"
 
   private val valueSubject = topic + "-value"
+
+  private val encoderFactory = EncoderFactory.get
 
   private val decoderFactory = DecoderFactory.get
 
@@ -75,6 +80,44 @@ private[formatter] class AvroMessageReader(schemaRegistryClient: SchemaRegistryC
       case ex: AvroRuntimeException =>
         throw new SerializationException(
           String.format("Error deserializing json %s to Avro of schema %s", jsonString, schema), ex)
+    }
+  }
+
+  private def serializeImpl(subject: String, obj: AnyRef): Array[Byte] = {
+    if (obj == null) {
+      null
+    } else {
+      var schema: Schema = null
+      try {
+        schema = getSchema(obj)
+        val id = schemaRegistry.getId(subject, schema)
+        val out = new ByteArrayOutputStream
+        out.write(0)
+        out.write(ByteBuffer.allocate(4).putInt(id).array)
+        obj match {
+          case bytes: Array[Byte] => out.write(bytes.asInstanceOf[Array[Byte]])
+          case _ =>
+            val encoder = encoderFactory.directBinaryEncoder(out, null)
+            val value = obj match {
+              case container: NonRecordContainer => container.getValue
+              case _ => obj
+            }
+            val writer = if (value.isInstanceOf[SpecificRecord])
+              new SpecificDatumWriter[AnyRef](schema)
+            else
+              new GenericDatumWriter[AnyRef](schema)
+            writer.write(value, encoder)
+            encoder.flush()
+        }
+        val bytes = out.toByteArray
+        out.close()
+        bytes
+      } catch {
+        case ex@(_: RuntimeException | _: IOException) =>
+          throw new SerializationException("Error serializing Avro message", ex)
+        case ex: RestClientException =>
+          throw new SerializationException("Error retrieving Avro schema: " + schema, ex)
+      }
     }
   }
 
