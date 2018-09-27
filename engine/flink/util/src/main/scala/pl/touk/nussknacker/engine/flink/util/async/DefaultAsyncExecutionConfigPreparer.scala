f9afab5d53bd0bab8c9b6b5a8142bd3143b58597
Maciek Próchniak <mpr@touk.pl>
2018-09-27 16:01:47 +0200

    Some more logging for async, let async threads timeout after inactivity
    Change-Id: I80e2fc9c39e476b4499506e72fa927bad74952f6

--- engine/flink/util/src/main/scala/pl/touk/nussknacker/engine/flink/util/async/DefaultAsyncExecutionConfigPreparer.scala
+++ engine/flink/util/src/main/scala/pl/touk/nussknacker/engine/flink/util/async/DefaultAsyncExecutionConfigPreparer.scala
@@ -3,6 +3,7 @@ package pl.touk.nussknacker.engine.flink.util.async
 import java.util.concurrent._
 import java.util.concurrent.atomic.AtomicLong
 
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.commons.lang3.concurrent.BasicThreadFactory
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import pl.touk.nussknacker.engine.api.process.AsyncExecutionContextPreparer
@@ -10,41 +11,50 @@ import pl.touk.nussknacker.engine.api.process.AsyncExecutionContextPreparer
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService}
 
 //TODO: this is somewhat experimental - how should we behave??
-object DefaultAsyncExecutionConfigPreparer {
+object DefaultAsyncExecutionConfigPreparer extends LazyLogging {
 
-  private final var asyncExecutionContext : Option[ExecutionContextExecutorService] = None
+  private final var asyncExecutionContext : Option[(String, ExecutionContextExecutorService)] = None
 
   private final val counter = new AtomicLong(0)
 
   private val executorServiceCreator: (Int, ThreadFactory) => ExecutorService =
-    (workers, threadFactory) => new ThreadPoolExecutor(workers, workers, 1, TimeUnit.MINUTES, new LinkedBlockingQueue[Runnable], threadFactory)
+    (workers, threadFactory) => {
+      val ex = new ThreadPoolExecutor(workers, workers, 1, TimeUnit.MINUTES, new LinkedBlockingQueue[Runnable], threadFactory)
+      //it seems with async timeout this pool can be not closed during process restart. To avoid thread leak we want to timeout also core threads
+      ex.allowCoreThreadTimeOut(true)
+      ex
+    }
 
 
   private[DefaultAsyncExecutionConfigPreparer] def getExecutionContext(workers: Int, process: String) = synchronized {
     counter.incrementAndGet()
+    logger.info(s"Creating asyncExecutor for $process, with $workers, counter is ${counter.get()}")
     asyncExecutionContext match {
-      case Some(ec) => ec
+      case Some((_, ec)) => ec
       case None =>
         val threadFactory = new BasicThreadFactory.Builder().namingPattern(s"asyncWorkerThread-$process-%d").build()
         val ec = ExecutionContext.fromExecutorService(executorServiceCreator(workers, threadFactory))
-        asyncExecutionContext = Some(ec)
+        asyncExecutionContext = Some((process, ec))
         ec
     }
   }
 
   private[DefaultAsyncExecutionConfigPreparer] def close(): Unit = {
+    logger.info(s"Closing asyncExecutor for ${asyncExecutionContext.map(_._1)} counter is ${counter.get()}")
     if (counter.decrementAndGet() == 0) {
-      asyncExecutionContext.foreach(_.shutdownNow())
+      asyncExecutionContext.foreach(_._2.shutdownNow())
       asyncExecutionContext = None
     }
   }
 
 }
 
-case class DefaultAsyncExecutionConfigPreparer(bufferSize: Int, parallelismMultiplier: Int) extends AsyncExecutionContextPreparer {
+case class DefaultAsyncExecutionConfigPreparer(bufferSize: Int, parallelismMultiplier: Int) extends AsyncExecutionContextPreparer with LazyLogging {
 
   def prepareExecutionContext(processId: String) : ExecutionContext = {
-    DefaultAsyncExecutionConfigPreparer.getExecutionContext(StreamExecutionEnvironment.getExecutionEnvironment.getParallelism * parallelismMultiplier, processId)
+    val parallelism = StreamExecutionEnvironment.getExecutionEnvironment.getParallelism
+    logger.info(s"Creating asyncExecutor for $processId, parallelism: $parallelism, multiplier: $parallelismMultiplier")
+    DefaultAsyncExecutionConfigPreparer.getExecutionContext(parallelism * parallelismMultiplier, processId)
   }
 
   def close() : Unit = {
