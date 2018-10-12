a787fc452abc93ccf1777c4bc817eb9440eb6892
Maciek Próchniak <mpr@touk.pl>
2018-10-12 15:40:42 +0200

    [ITDEVESP-664] - fixed corner cases for counts
    Change-Id: I704d1d94407f256ac81fd5f0e6ffa3abaf7e3b0e

--- engine/processReports/src/main/scala/pl/touk/process/report/influxdb/InfluxGenerator.scala
+++ engine/processReports/src/main/scala/pl/touk/process/report/influxdb/InfluxGenerator.scala
@@ -8,52 +8,33 @@ import com.typesafe.scalalogging.LazyLogging
 import dispatch._
 import pl.touk.nussknacker.engine.dispatch.LoggingDispatchClient
 import pl.touk.nussknacker.engine.dispatch.utils._
+import pl.touk.process.report.influxdb.InfluxGenerator.{InfluxResponse, InfluxSerie, PointInTimeQuery}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 private[influxdb] class InfluxGenerator(url: String, user: String, password: String, dbName: String, env: String = "test") extends LazyLogging {
 
   import argonaut.ArgonautShapeless._
+  import InfluxGenerator._
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
   val httpClient = LoggingDispatchClient(classOf[InfluxGenerator].getSimpleName, Http)
 
-  implicit val numberOrStringDecoder = DecodeJson.apply[Any] { cursor =>
-    val focused = cursor.focus
-    val bigDecimalDecoder = implicitly[DecodeJson[BigDecimal]].asInstanceOf[DecodeJson[Any]]
-    val stringDecoder = implicitly[DecodeJson[String]].asInstanceOf[DecodeJson[Any]]
-    DecodeResult.ok(focused.as(bigDecimalDecoder).toOption.getOrElse(focused.as(stringDecoder).toOption.getOrElse("")))
-  }
-
+  //TODO: query below should work better even in case of restarts, however for large processes/time ranges it can be *really* slow (at least on influx we use...)
+  //select sum(diff) from (select non_negative_difference(value) as diff from "$metricName.count" where process = '$processName' and env = '$env' and time > $from and time < $to group by slot) group by action
   def query(processName: String, metricName: String, dateFrom: LocalDateTime, dateTo: LocalDateTime): Future[Map[String, Long]] = {
-    val start = dateFrom
-    val stop = dateTo
-
-    def queryForDate(date: LocalDateTime) = {
-      //we use epoch seconds to avoid time zone problems... in influx
-      val from = toEpochSeconds(date)
-      //two hour window is for possible delays in sending metrics from taskmanager to jobmanager (or upd sending problems...)
-      //it's VERY unclear how large it should be. If it's too large, we may overlap with end and still generate
-      //bad results...
-      val queryString =
-      s"""select action, first(value) as value from "$metricName.count" where process = '$processName' """ +
-        s"and time >= ${from}s and time < ${from}s + 2h and env = '$env' group by slot, action"
-
-      query(queryString).map(
-        _.map(r => (r.values.head.lift(1).getOrElse("UNKNOWN"), r.values.head.lift(2).getOrElse(0L)))
-          .groupBy(_._1.asInstanceOf[String]).mapValues(_.map(_._2.asInstanceOf[Number].longValue()).sum))
-    }
+    val pointInTimeQuery = new PointInTimeQuery(rawQuery, processName, metricName, env)
 
     for {
-      valuesAtEnd <- queryForDate(stop)
-      valuesAtStart <- queryForDate(start)
+      valuesAtEnd <- pointInTimeQuery.query(dateTo)
+      valuesAtStart <- pointInTimeQuery.query(dateFrom)
     } yield valuesAtEnd.map {
       case (key, value) => key -> (value - valuesAtStart.getOrElse(key, 0L))
     }
   }
 
-  private def query(query: String): Future[List[InfluxSerie]] = {
+  private def rawQuery(query: String): Future[List[InfluxSerie]] = {
     httpClient {
       dispatch
         .url(url) <<? Map("db" -> dbName, "q" -> query) as_!(user, password) OK
@@ -61,10 +42,6 @@ private[influxdb] class InfluxGenerator(url: String, user: String, password: Str
     }.map { qr =>
       qr.results.head.series
     }
-  }
-
-  private def toEpochSeconds(d: LocalDateTime): Long = {
-    d.atZone(ZoneId.systemDefault()).toInstant.toEpochMilli / 1000
   }
 
   def detectRestarts(processName: String, dateFrom: LocalDateTime, dateTo: LocalDateTime): Future[List[LocalDateTime]] = {
@@ -75,7 +52,7 @@ private[influxdb] class InfluxGenerator(url: String, user: String, password: Str
       s"""SELECT derivative(value) FROM "source.count" WHERE
          |"process" = '$processName' AND env = '$env'
          | AND time >= ${from}s and time < ${to}s GROUP BY slot""".stripMargin
-    query(queryString).map { series =>
+    rawQuery(queryString).map { series =>
       series.headOption.map(readRestartsFromSourceCounts).getOrElse(List())
     }
   }
@@ -94,9 +71,62 @@ private[influxdb] class InfluxGenerator(url: String, user: String, password: Str
     httpClient.shutdown()
   }
 
+
+}
+
+object InfluxGenerator {
+
   case class InfluxResponse(results: List[InfluxResult] = List())
 
   case class InfluxResult(series: List[InfluxSerie] = List())
 
-  case class InfluxSerie(name: String, columns: List[String], values: List[List[Any]] = List())
+  case class InfluxSerie(name: String, tags: Map[String, String], columns: List[String], values: List[List[Any]] = List()) {
+    val toMap: List[Map[String, Any]] = values.map(value => columns.zip(value).toMap)
+  }
+
+  private[influxdb] implicit val numberOrStringDecoder: DecodeJson[Any] = DecodeJson.apply[Any] { cursor =>
+    val focused = cursor.focus
+    val bigDecimalDecoder = implicitly[DecodeJson[BigDecimal]].asInstanceOf[DecodeJson[Any]]
+    val stringDecoder = implicitly[DecodeJson[String]].asInstanceOf[DecodeJson[Any]]
+    DecodeResult.ok(focused.as(bigDecimalDecoder).toOption.getOrElse(focused.as(stringDecoder).toOption.getOrElse("")))
+  }
+
+  private[influxdb] def toEpochSeconds(d: LocalDateTime): Long = {
+    d.atZone(ZoneId.systemDefault()).toInstant.toEpochMilli / 1000
+  }
+
+  //influx cannot give us result for "give me value nearest in time to t1", so we try to do it by looking for
+  //last point before t1 and first after t1.
+  // TODO: probably we should just take one of them, but the one which is closer to t1?
+  class PointInTimeQuery(query: String => Future[List[InfluxSerie]], processName: String, metricName: String, env: String)(implicit ec: ExecutionContext) extends LazyLogging {
+
+    //two hour window is for possible delays in sending metrics from taskmanager to jobmanager (or upd sending problems...)
+    //it's VERY unclear how large it should be. If it's too large, we may overlap with end and still generate
+    //bad results...
+    def query(date: LocalDateTime): Future[Map[String, Long]] = {
+      val around = toEpochSeconds(date)
+      for {
+        valuesBefore <- retrieveOnlyResultFromActionValueQuery(s"""select action, last(value) as value from "$metricName.count" where process = '$processName' """ +
+                    s"and time <= ${around}s and time > ${around}s - 1h and env = '$env' group by slot, action")
+        valuesAfter <- retrieveOnlyResultFromActionValueQuery(s"""select action, first(value) as value from "$metricName.count" where process = '$processName' """ +
+                    s"and time >= ${around}s and time < ${around}s + 1h and env = '$env' group by slot, action")
+      } yield valuesBefore ++ valuesAfter
+    }
+
+    //see InfluxGeneratorSpec for influx return format...
+    def retrieveOnlyResultFromActionValueQuery(queryString: String): Future[Map[String, Long]] = {
+      val groupedResults = query(queryString).map { seriesList =>
+        seriesList.map { oneSeries =>
+          //in case of our queries we know there will be only one result (we use only first/last aggregations), rest will be handled by aggregations
+          val firstResult = oneSeries.toMap.headOption.getOrElse(Map())
+          (firstResult.getOrElse("action", "UNKNOWN").asInstanceOf[String], firstResult.getOrElse("value", 0L).asInstanceOf[Number].longValue())
+        }.groupBy(_._1).mapValues(_.map(_._2).sum)
+      }
+      groupedResults.onSuccess {
+        case evaluated => logger.debug(s"Query: $queryString retrieved grouped results: $evaluated")
+      }
+      groupedResults
+    }
+  }
+
 }
---
+++ engine/processReports/src/test/scala/pl/touk/process/report/influxdb/InfluxGeneratorSpec.scala
@@ -0,0 +1,113 @@
+package pl.touk.process.report.influxdb
+
+import java.time.LocalDateTime
+
+import org.scalatest.{FunSuite, Matchers}
+import pl.touk.process.report.influxdb.InfluxGenerator.{InfluxSerie, PointInTimeQuery}
+import argonaut._
+import argonaut.Argonaut._
+import argonaut.ArgonautShapeless._
+import org.scalatest.concurrent.ScalaFutures
+
+import scala.concurrent.Future
+
+class InfluxGeneratorSpec extends FunSuite with Matchers with ScalaFutures {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import InfluxGenerator._
+
+  //TODO: test generated query, not just shape of output
+  test("Point in time query returns correct results") {
+
+    val pointInTimeQuery = new PointInTimeQuery(_ => Future.successful(sampleInfluxOutput), "process1", "nodeCount", "test")
+
+    pointInTimeQuery.query(LocalDateTime.now()).futureValue shouldBe Map(
+      "start" -> (552855221L + 557871409L),
+      "end" -> (412793677L + 414963365L)
+    )
+  }
+
+  val sampleInfluxOutputRaw: String = """
+    |    [
+    |        {
+    |          "name": "nodeCount.count",
+    |          "tags": {
+    |            "action": "end",
+    |            "slot": "0"
+    |          },
+    |          "columns": [
+    |            "time",
+    |            "action",
+    |            "value"
+    |          ],
+    |          "values": [
+    |            [
+    |              "2018-10-15T06:17:35Z",
+    |              "end",
+    |              412793677
+    |            ]
+    |          ]
+    |        },
+    |        {
+    |          "name": "nodeCount.count",
+    |          "tags": {
+    |            "action": "end",
+    |            "slot": "1"
+    |          },
+    |          "columns": [
+    |            "time",
+    |            "action",
+    |            "value"
+    |          ],
+    |          "values": [
+    |            [
+    |              "2018-10-15T06:17:35Z",
+    |              "end",
+    |              414963365
+    |            ]
+    |          ]
+    |        },
+    |        {
+    |          "name": "nodeCount.count",
+    |          "tags": {
+    |            "action": "start",
+    |            "slot": "0"
+    |          },
+    |          "columns": [
+    |            "time",
+    |            "action",
+    |            "value"
+    |          ],
+    |          "values": [
+    |            [
+    |              "2018-10-15T06:17:35Z",
+    |              "start",
+    |              552855221
+    |            ]
+    |          ]
+    |        },
+    |        {
+    |          "name": "nodeCount.count",
+    |          "tags": {
+    |            "action": "start",
+    |            "slot": "1"
+    |          },
+    |          "columns": [
+    |            "time",
+    |            "action",
+    |            "value"
+    |          ],
+    |          "values": [
+    |            [
+    |              "2018-10-15T06:17:35Z",
+    |              "start",
+    |              557871409
+    |            ]
+    |          ]
+    |        }
+    |      ]
+  """.stripMargin
+
+  val sampleInfluxOutput: List[InfluxSerie] = sampleInfluxOutputRaw.decodeOption[List[InfluxSerie]].get
+
+}
