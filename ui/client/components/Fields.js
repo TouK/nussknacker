65c7612dde26a5c32875eceb98e1c36c1c5cd596
Adrian Bednarz <abd@touk.pl>
2019-04-23 09:03:53 +0200

    Subprocess parameters reordering
    Change-Id: I6eace9d2a770295ecd281b68199ea2d548cbd794

--- ui/client/components/Fields.js
+++ ui/client/components/Fields.js
@@ -1,6 +1,94 @@
 import React from "react";
 import _ from "lodash";
+import { DragSource, DropTarget } from "react-dnd";
+import dragHandleIcon from "../assets/img/drag-handle.png";
+import update from "immutability-helper";
+import ReactDOM from "react-dom";
 
+
+class RawField extends React.Component {
+  render() {
+    const markedClass = this.props.isMarked(this.props.index) ? " marked" : "";
+    const index = this.props.index;
+    const field = this.props.field;
+    const opacity = this.props.isDragging ? 0 : 1;
+
+    return this.props.connectDropTarget(this.props.connectDragSource(
+      <div className="node-row" key={field.name} style={{opacity}}>
+        <img src={dragHandleIcon} />
+        <div className={"node-value fieldName" + markedClass}>
+          <input className="node-input" type="text" value={field.name} placeholder="Field name"
+                 onChange={(e) => this.props.changeName(index, e.target.value)}/>
+        </div>
+        <div className={"node-value field" + markedClass}>
+          {this.props.fieldCreator(field, (value) => this.props.changeValue(index, field.name, value))}
+        </div>
+        <div className="node-value fieldRemove">
+          {/* TODO: add nicer buttons. Awesome font? */}
+          <button className="addRemoveButton" title="Remove field" onClick={() => this.props.removeField(index)}>-
+          </button>
+        </div>
+      </div>
+    ));
+  }
+}
+
+const Field =
+  new DropTarget(
+    "field", {
+      // http://react-dnd.github.io/react-dnd/examples/sortable/simple
+      hover(props, monitor, component) {
+        if (!component) {
+          return null;
+        }
+        // node = HTML Div element from imperative API
+        const node = ReactDOM.findDOMNode(component);
+        if (!node) {
+          return null;
+        }
+        const dragIndex = monitor.getItem().index;
+        const hoverIndex = props.index;
+        // Don't replace items with themselves
+        if (dragIndex === hoverIndex) {
+          return;
+        }
+        // Determine rectangle on screen
+        const hoverBoundingRect = node.getBoundingClientRect();
+        // Get vertical middle
+        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+        // Determine mouse position
+        const clientOffset = monitor.getClientOffset();
+        // Get pixels to the top
+        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+        // Only perform the move when the mouse has crossed half of the items height
+        // When dragging downwards, only move when the cursor is below 50%
+        // When dragging upwards, only move when the cursor is above 50%
+        // Dragging downwards
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+          return;
+        }
+        // Dragging upwards
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+          return;
+        }
+
+        props.moveItem(dragIndex, hoverIndex);
+        monitor.getItem().index = hoverIndex;
+      },
+    }, (connect) => ({
+      connectDropTarget: connect.dropTarget()
+    }),
+  )(
+  new DragSource(
+    "field", {
+    beginDrag: (props) => ({
+      index: props.index
+    })
+  }, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  }))(RawField)
+);
 
 export default class Fields extends React.Component {
 
@@ -24,35 +112,31 @@ export default class Fields extends React.Component {
   }
 
   render() {
-    return (<div className="fieldsControl">
-        {
-          this.state.fields.map((field, index) => {
-            const markedClass = this.props.isMarked(index) ? " marked" : ""
-            return (
-              <div className="node-row" key={index}>
-                <div className={"node-value fieldName" + markedClass}>
-                  <input className="node-input" type="text" value={field.name} placeholder="Field name"
-                         onChange={e => this.changeName(index, e.target.value)}/>
-                </div>
-                <div className={"node-value field" + markedClass}>
-                  {this.props.fieldCreator(field, (value) => this.changeValue(index, field.name, value))}
-                </div>
-                <div className="node-value fieldRemove">
-                  {/* TODO: add nicer buttons. Awesome font? */}
-                  <button className="addRemoveButton" title="Remove field" onClick={() => this.removeField(index)}>-
-                  </button>
-                </div>
-              </div>
-            )
+    const moveItem = (dragIndex, hoverIndex) => {
+      this.edit(previous => {
+        return update(previous, {
+          $splice: [[dragIndex, 1], [hoverIndex, 0, previous[dragIndex]]],
+        })
+      })
+    };
 
-            }
-          )
-        }
+    return (<div className="fieldsControl">
+      {
+        this.state.fields.map((field, index) =>
+          <Field key={field.name}
+                 field={field}
+                 index={index}
+                 changeName={this.changeName.bind(this)}
+                 changeValue={this.changeValue.bind(this)}
+                 removeField={this.removeField.bind(this)}
+                 moveItem={moveItem}
+                 {...this.props} />
+        )
+      }
       <div>
         <button className="addRemoveButton"  title="Add field"  onClick={() => this.addField()}>+</button>
       </div>
-    </div>)
-
+    </div>);
   }
 
   changeName(index, name) {
