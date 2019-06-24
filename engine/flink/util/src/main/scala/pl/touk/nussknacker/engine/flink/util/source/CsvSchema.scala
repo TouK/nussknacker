e7b7f93fad4818e13f8595346b5803dbefb6a4c8
Adrian Bednarz <abd@touk.pl>
2019-06-24 14:10:59 +0200

    Configurable CSV separator in CsvSchema util

--- engine/flink/util/src/main/scala/pl/touk/nussknacker/engine/flink/util/source/CsvSchema.scala
+++ engine/flink/util/src/main/scala/pl/touk/nussknacker/engine/flink/util/source/CsvSchema.scala
@@ -5,12 +5,12 @@ import java.nio.charset.StandardCharsets
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.common.serialization.DeserializationSchema
 
-class CsvSchema[T: TypeInformation](constructor: List[String] => T) extends DeserializationSchema[T] {
+class CsvSchema[T: TypeInformation](constructor: List[String] => T, separator: Char) extends DeserializationSchema[T] {
   override def isEndOfStream(t: T): Boolean = false
 
   override def deserialize(bytes: Array[Byte]): T = constructor(toFields(bytes))
 
-  def toFields(bytes: Array[Byte]) = new String(bytes, StandardCharsets.UTF_8).split("\\|").toList
+  def toFields(bytes: Array[Byte]) = new String(bytes, StandardCharsets.UTF_8).split(separator).toList
 
   override def getProducedType: TypeInformation[T] = implicitly[TypeInformation[T]]
 }
