509ac3ec627d8ffdfebd9fdc5342f07a7a753100
Łukasz Ciołecki <lcl@touk.pl>
2019-07-29 11:47:16 +0200

    [Fx] Cleaning comment input after add comment

--- ui/client/components/CommentInput.js
+++ ui/client/components/CommentInput.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 const CommentInput = (props) => {
   return (
     <textarea
+      value={props.value || ""}
       className="add-comment-on-save"
       placeholder="Write a comment..."
       onChange={props.onChange}
