890a201b3326c82975185311c3568ced3e40758d
Maciek Próchniak <mpr@touk.pl>
2019-09-04 10:32:56 +0200

    Feature: standalone sink can create response based on parameters

--- engine/standalone/util/src/main/scala/pl/touk/nussknacker/engine/standalone/utils/StandaloneSinkFactory.scala
+++ engine/standalone/util/src/main/scala/pl/touk/nussknacker/engine/standalone/utils/StandaloneSinkFactory.scala
@@ -1,13 +1,23 @@
 package pl.touk.nussknacker.engine.standalone.utils
 
-import pl.touk.nussknacker.engine.api.MethodToInvoke
+import pl.touk.nussknacker.engine.api.{Context, LazyParameterInterpreter, MethodToInvoke}
 import pl.touk.nussknacker.engine.api.process.{Sink, SinkFactory}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class StandaloneSinkFactory extends SinkFactory {
 
   @MethodToInvoke
   def invoke(): Sink = new Sink {
-    override def testDataOutput: Option[(Any) => String] = Some(_.toString)
+    override def testDataOutput: Option[Any => String] = Some(_.toString)
   }
+
+}
+
+//TODO: this is not so easy to use...
+//to make it easier we have to make LazyParameter sth like monad (probably depending on interpreter - like EC)
+trait StandaloneSinkWithParameters extends Sink {
+
+  def prepareResponse(evaluateLazyParameter: LazyParameterInterpreter): (Context, ExecutionContext) => Future[Any]
 
 }
