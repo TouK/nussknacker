015895e9307744e11521f06d1c31393461c6b668
Łukasz Jędrzejewski <ljd@touk.pl>
2019-10-07 09:42:54 +0200

    Deserialize process name as value in standalone http client

--- engine/api/src/main/scala/pl/touk/nussknacker/engine/api/process/package.scala
+++ engine/api/src/main/scala/pl/touk/nussknacker/engine/api/process/package.scala
@@ -1,5 +1,7 @@
 package pl.touk.nussknacker.engine.api
 
+import argonaut.Argonaut._
+import argonaut.{CodecJson, _}
 import io.circe.{Decoder, Encoder}
 
 package object process {
@@ -7,6 +9,9 @@ package object process {
   object ProcessName {
     implicit val encoder: Encoder[ProcessName] = Encoder.encodeString.contramap(_.value)
     implicit val decoder: Decoder[ProcessName] = Decoder.decodeString.map(ProcessName(_))
+
+    // argonaut codec for backward compatibility
+    implicit val codec: CodecJson[ProcessName] = CodecJson.derived[String].xmap(ProcessName(_))(_.value)
   }
 
   final case class ProcessName(value: String) extends AnyVal
