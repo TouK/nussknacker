22cf0da2efb88310dea36f8818cb7b7116084540
Łukasz Jędrzejewski <ljd@touk.pl>
2019-10-16 18:17:29 +0200

    Reorganize private methods and extract api calls

--- ui/server/src/main/scala/pl/touk/nussknacker/ui/process/migrate/RemoteEnvironment.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/process/migrate/RemoteEnvironment.scala
@@ -87,6 +87,102 @@ trait StandardRemoteEnvironment extends FailFastCirceSupport with RemoteEnvironm
 
   implicit def materializer: Materializer
 
+  override def processVersions(processName: ProcessName)(implicit ec: ExecutionContext): Future[List[ProcessHistoryEntry]] =
+    invokeJson[ProcessDetails](HttpMethods.GET, List("processes", processName.value), Query(("businessView", true.toString))).map { result =>
+      result.fold(_ => List(), _.history)
+    }
+
+  protected def request(path: Uri, method: HttpMethod, request: MessageEntity): Future[HttpResponse]
+
+  override def compare(localProcess: DisplayableProcess, remoteProcessVersion: Option[Long], businessView: Boolean = false)(implicit ec: ExecutionContext) : Future[Either[EspError, Map[String, Difference]]] = {
+    val id = localProcess.id
+    (for {
+      process <- EitherT(fetchProcessVersion(id, remoteProcessVersion, businessView))
+      compared <- EitherT.fromEither[Future](compareProcess(id, localProcess)(process))
+    } yield compared).value
+  }
+
+  private def compareProcess(id: String, localProcess: DisplayableProcess)(remoteProcessDetails: ProcessDetails) : Either[EspError, Map[String, Difference]] = remoteProcessDetails.json match {
+    case Some(remoteProcess) => Right(ProcessComparator.compare(localProcess, remoteProcess))
+    case None => Left(InvalidProcessTypeError(id))
+  }
+
+  override def migrate(localProcess: DisplayableProcess, category: String)
+                      (implicit ec: ExecutionContext, loggedUser: LoggedUser) : Future[Either[EspError, Unit]] = {
+    (for {
+      validation <- EitherT(validateProcess(localProcess))
+      _ <- EitherT.fromEither[Future](if (validation.errors != ValidationErrors.success) Left[EspError, Unit](MigrationValidationError(validation.errors)) else Right(()))
+      _ <- createRemoteProcessIfNotExist(localProcess, category)
+      _ <- EitherT.right[EspError](saveProcess(localProcess, comment = s"Process migrated from $environmentId by ${loggedUser.id}"))
+    } yield ()).value
+  }
+
+  private def createRemoteProcessIfNotExist(localProcess: DisplayableProcess, category: String)
+                                           (implicit ec: ExecutionContext): EitherT[Future, EspError, Unit] = {
+    EitherT {
+      invokeStatus(HttpMethods.GET, List("processes", localProcess.id)).flatMap { status =>
+        if (status == StatusCodes.NotFound)
+          invokeForSuccess(HttpMethods.POST, List("processes", localProcess.id, category), Query(("isSubprocess", localProcess.metaData.isSubprocess.toString)))
+        else
+          Future.successful(().asRight)
+      }
+    }
+  }
+
+  override def testMigration(implicit ec: ExecutionContext): Future[Either[EspError, List[TestMigrationResult]]] = {
+    (for {
+      basicProcesses <- EitherT(fetchProcesses)
+      processes      <- fetchGroupByGroup(basicProcesses)
+      subProcesses   <- EitherT(fetchSubProcessesDetails)
+    } yield testModelMigrations.testMigrations(processes, subProcesses)).value
+  }
+
+  private def fetchGroupByGroup[T](basicProcesses: List[BasicProcess])
+                                  (implicit ec: ExecutionContext): EitherT[Future, EspError, List[ValidatedProcessDetails]] = {
+    basicProcesses.map(_.name).grouped(config.batchSize)
+      .foldLeft(EitherT.rightT[Future, EspError](List.empty[ValidatedProcessDetails])) { case (acc, processesGroup) =>
+        for {
+          current <- acc
+          fetched <- fetchProcessesDetails(processesGroup)
+        } yield current ::: fetched
+      }
+  }
+
+  private def fetchProcesses(implicit ec: ExecutionContext): Future[Either[EspError, List[BasicProcess]]] = {
+    invokeJson[List[BasicProcess]](HttpMethods.GET, List("processes"))
+  }
+
+  private def fetchProcessVersion(id: String, remoteProcessVersion: Option[Long], businessView: Boolean)
+                                 (implicit ec: ExecutionContext): Future[Either[EspError, ProcessDetails]] = {
+    invokeJson[ProcessDetails](HttpMethods.GET, List("processes", id) ++ remoteProcessVersion.map(_.toString).toList, Query(("businessView", businessView.toString)))
+  }
+
+  private def fetchProcessesDetails(names: List[String])(implicit ec: ExecutionContext) = EitherT {
+    invokeJson[List[ValidatedProcessDetails]](
+      HttpMethods.GET,
+      "processesDetails" :: Nil,
+      Query(("names", names.map(URLEncoder.encode(_, StandardCharsets.UTF_8.displayName())).mkString(",")))
+    )
+  }
+
+  private def fetchSubProcessesDetails(implicit ec: ExecutionContext): Future[Either[EspError, List[ValidatedProcessDetails]]] = {
+    invokeJson[List[ValidatedProcessDetails]](HttpMethods.GET, List("subProcessesDetails"))
+  }
+
+  private def validateProcess(process: DisplayableProcess)(implicit ec: ExecutionContext): Future[Either[EspError, ValidationResult]] = {
+    for {
+      processToValidate <- Marshal(process).to[MessageEntity]
+      validation <- invokeJson[ValidationResult](HttpMethods.POST, List("processValidation"), requestEntity = processToValidate)
+    } yield validation
+  }
+
+  private def saveProcess(process: DisplayableProcess, comment: String)(implicit ec: ExecutionContext): Future[Unit] = {
+    for {
+      processToSave <- Marshal(ProcessToSave(process, comment)).to[MessageEntity](marshaller, ec)
+      _ <- invokeJson[ValidationResult](HttpMethods.PUT, List("processes", process.id), requestEntity = processToSave)
+    } yield ()
+  }
+
   private def invoke[T](method: HttpMethod, pathParts: List[String], query: Query = Query.Empty, requestEntity: RequestEntity = HttpEntity.Empty)
                        (f: HttpResponse => Future[T])(implicit ec: ExecutionContext): Future[T] = {
     val pathEncoded = pathParts.foldLeft[Path](baseUri.path)(_ / _)
@@ -114,7 +210,7 @@ trait StandardRemoteEnvironment extends FailFastCirceSupport with RemoteEnvironm
 
   private def invokeJson[T: Decoder](method: HttpMethod, pathParts: List[String],
                                      query: Query = Query.Empty, requestEntity: RequestEntity = HttpEntity.Empty)
-                                       (implicit ec: ExecutionContext): Future[Either[EspError, T]] = {
+                                    (implicit ec: ExecutionContext): Future[Either[EspError, T]] = {
     invoke(method, pathParts, query, requestEntity) { response =>
       if (response.status.isSuccess()) {
         Unmarshal(response.entity).to[T].map(Either.right)
@@ -123,80 +219,5 @@ trait StandardRemoteEnvironment extends FailFastCirceSupport with RemoteEnvironm
           .map(error => Either.left(RemoteEnvironmentCommunicationError(response.status, error)))
       }
     }
-  }
-
-  override def processVersions(processName: ProcessName)(implicit ec: ExecutionContext): Future[List[ProcessHistoryEntry]] =
-    invokeJson[ProcessDetails](HttpMethods.GET, List("processes", processName.value), Query(("businessView", true.toString))).map { result =>
-      result.fold(_ => List(), _.history)
-    }
-
-  protected def request(path: Uri, method: HttpMethod, request: MessageEntity): Future[HttpResponse]
-
-  override def compare(localProcess: DisplayableProcess, remoteProcessVersion: Option[Long], businessView: Boolean = false)(implicit ec: ExecutionContext) : Future[Either[EspError, Map[String, Difference]]] = {
-    val id = localProcess.id
-
-    (for {
-      //TODO: move urls to some constants...
-      process <- EitherT(invokeJson[ProcessDetails](HttpMethods.GET, List("processes", id) ++ remoteProcessVersion.map(_.toString).toList, Query(("businessView", businessView.toString))))
-      compared <- EitherT.fromEither[Future](compareProcess(id, localProcess)(process))
-    } yield compared).value
-
-  }
-
-  override def migrate(localProcess: DisplayableProcess, category: String)
-                      (implicit ec: ExecutionContext, loggedUser: LoggedUser) : Future[Either[EspError, Unit]]= {
-
-    val comment = s"Process migrated from $environmentId by ${loggedUser.id}"
-    (for {
-      processToValidate <- EitherT.right(Marshal(localProcess).to[MessageEntity])
-      validation <- EitherT(invokeJson[ValidationResult](HttpMethods.POST, List("processValidation"), requestEntity = processToValidate))
-      _ <- EitherT.fromEither[Future](if (validation.errors != ValidationErrors.success) Left[EspError, Unit](MigrationValidationError(validation.errors)) else Right(()))
-      _ <- createRemoteProcessIfNotExist(localProcess, category)
-      processToSave <- EitherT.right(Marshal(ProcessToSave(localProcess, comment)).to[MessageEntity](marshaller, ec))
-      _ <- EitherT(invokeJson[ValidationResult](HttpMethods.PUT, List("processes", localProcess.id), requestEntity = processToSave))
-    } yield ()).value
-  }
-
-  private def createRemoteProcessIfNotExist(localProcess: DisplayableProcess, category: String)
-                                           (implicit ec: ExecutionContext): EitherT[Future, EspError, Unit] = {
-    EitherT {
-      invokeStatus(HttpMethods.GET, List("processes", localProcess.id)).flatMap { status =>
-        if (status == StatusCodes.NotFound)
-          invokeForSuccess(HttpMethods.POST, List("processes", localProcess.id, category), Query(("isSubprocess", localProcess.metaData.isSubprocess.toString)))
-        else
-          Future.successful(().asRight)
-      }
-    }
-  }
-
-  private def compareProcess(id: String, localProcess: DisplayableProcess)(remoteProcessDetails: ProcessDetails) : Either[EspError, Map[String, Difference]] = remoteProcessDetails.json match {
-    case Some(remoteProcess) => Right(ProcessComparator.compare(localProcess, remoteProcess))
-    case None => Left(InvalidProcessTypeError(id))
-  }
-
-  override def testMigration(implicit ec: ExecutionContext): Future[Either[EspError, List[TestMigrationResult]]] = {
-    (for {
-      basicProcesses <- EitherT(invokeJson[List[BasicProcess]](HttpMethods.GET, List("processes")))
-      processes      <- fetchGroupByGroup(basicProcesses)
-      subprocesses   <- EitherT(invokeJson[List[ValidatedProcessDetails]](HttpMethods.GET, List("subProcessesDetails")))
-    } yield testModelMigrations.testMigrations(processes, subprocesses)).value
-  }
-
-  private def fetchGroupByGroup[T](basicProcesses: List[BasicProcess])
-                                  (implicit ec: ExecutionContext): EitherT[Future, EspError, List[ValidatedProcessDetails]] = {
-    def getMany(processes: List[BasicProcess]) = EitherT {
-      invokeJson[List[ValidatedProcessDetails]](
-        HttpMethods.GET,
-        "processesDetails" :: Nil,
-        Query(("names", processes.map(process => URLEncoder.encode(process.name, StandardCharsets.UTF_8.displayName())).mkString(",")))
-      )
-    }
-
-    basicProcesses.grouped(config.batchSize)
-      .foldLeft(EitherT.rightT[Future, EspError](List.empty[ValidatedProcessDetails])) { case (acc, batch) => for {
-          current <- acc
-          fetched <- getMany(batch)
-        } yield current ::: fetched
-      }
   }
 }
