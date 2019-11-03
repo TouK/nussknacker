a9e30d4b6883ada729cc70efd9b39b5b480f09f8
Grzegorz Skrobisz <gskrobisz@touk.pl>
2019-11-03 18:45:53 +0100

    Fix default value decoding error while getting counts report

--- engine/processReports/src/main/scala/pl/touk/nussknacker/processCounts/influxdb/SimpleInfluxClient.scala
+++ engine/processReports/src/main/scala/pl/touk/nussknacker/processCounts/influxdb/SimpleInfluxClient.scala
@@ -3,8 +3,6 @@ package pl.touk.nussknacker.processCounts.influxdb
 import sttp.client._
 import sttp.client.circe._
 import io.circe.Decoder
-import io.circe.generic.JsonCodec
-import io.circe.generic.semiauto._
 import pl.touk.nussknacker.engine.sttp.SttpJson
 import sttp.model.Uri
 
@@ -44,11 +42,25 @@ class SimpleInfluxClient(config: InfluxConfig)(implicit backend: SttpBackend[Fut
   }
 }
 
-@JsonCodec(decodeOnly = true) case class InfluxResponse(results: List[InfluxResult] = List())
+case class InfluxResponse(results: List[InfluxResult] = List())
 
-@JsonCodec(decodeOnly = true) case class InfluxResult(series: List[InfluxSerie] = List())
+object InfluxResponse {
+  import io.circe.generic.semiauto._
+  implicit val decoder: Decoder[InfluxResponse] = deriveDecoder
+}
+
+case class InfluxResult(series: List[InfluxSerie] = List())
+
+object InfluxResult {
+  import io.circe.generic.extras.Configuration
+  import io.circe.generic.extras.semiauto._
+  implicit val config: Configuration = Configuration.default.withDefaults
+  implicit val decoder: Decoder[InfluxResult] = deriveDecoder
+}
 
 object InfluxSerie {
+
+  import io.circe.generic.semiauto._
 
   private implicit val numberOrStringDecoder: Decoder[Any] =
     Decoder.decodeBigDecimal.asInstanceOf[Decoder[Any]] or Decoder.decodeString.asInstanceOf[Decoder[Any]] or Decoder.const[Any]("")
