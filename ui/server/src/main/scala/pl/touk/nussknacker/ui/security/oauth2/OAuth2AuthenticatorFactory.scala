3cb497bbec5fce990a6dc93c8565957dab4d99b4
Łukasz Ciołecki <lcl@touk.pl>
2019-11-04 17:05:33 +0100

    Improvement at OAuth2ServiceFactory Trait

--- ui/server/src/main/scala/pl/touk/nussknacker/ui/security/oauth2/OAuth2AuthenticatorFactory.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/security/oauth2/OAuth2AuthenticatorFactory.scala
@@ -4,16 +4,17 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.SecurityDirectives
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
+import pl.touk.nussknacker.ui.process.ProcessTypesForCategories
 import pl.touk.nussknacker.ui.security.api.AuthenticatorFactory
 import pl.touk.nussknacker.ui.security.api.AuthenticatorFactory.{AuthenticatorData, LoggedUserAuth}
-import pl.touk.nussknacker.ui.security.oauth2.OAuth2ServiceProvider.OAuth2Service
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class OAuth2AuthenticatorFactory extends AuthenticatorFactory with LazyLogging {
   override def createAuthenticator(config: Config, classLoader: ClassLoader): AuthenticatorData = {
     val configuration = OAuth2Configuration.create(config)
-    val service = OAuth2ServiceProvider(configuration, classLoader)
+    val typesForCategories = new ProcessTypesForCategories(config)
+    val service = OAuth2ServiceProvider(configuration, classLoader, typesForCategories.getAllCategories)
 
     AuthenticatorData(
       createDirective(configuration, service),
--- ui/server/src/main/scala/pl/touk/nussknacker/ui/security/oauth2/OAuth2ServiceFactory.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/security/oauth2/OAuth2ServiceFactory.scala
@@ -1,9 +1,21 @@
 package pl.touk.nussknacker.ui.security.oauth2
 
 import pl.touk.nussknacker.ui.security.api.GlobalPermission.GlobalPermission
-import pl.touk.nussknacker.ui.security.api.{GlobalPermission, Permission}
+import pl.touk.nussknacker.ui.security.api.{GlobalPermission, LoggedUser, Permission}
 import pl.touk.nussknacker.ui.security.api.Permission.Permission
 import pl.touk.nussknacker.ui.security.oauth2.OAuth2Configuration.OAuth2ConfigRule
+import pl.touk.nussknacker.ui.security.oauth2.OAuth2ServiceProvider.OAuth2AuthenticateData
+
+import scala.concurrent.Future
+
+trait OAuth2Service {
+  def authenticate(code: String): Future[OAuth2AuthenticateData]
+  def authorize(token: String): Future[LoggedUser]
+}
+
+trait OAuth2ServiceFactory {
+  def create(configuration: OAuth2Configuration, allCategories: List[String]): OAuth2Service
+}
 
 object OAuth2ServiceFactory {
   import cats.instances.all._
