610a87945c18cbb146dd9fb15d3b0ac5422d2543
Maciek Próchniak <mpr@touk.pl>
2019-11-14 15:13:28 +0100

    cleanup

--- ciBackendTest.sh
+++ ciBackendTest.sh
@@ -6,12 +6,9 @@ scala_cross_build="+"
 if ! [[ -z "$1" ]]; then
     scala_version="++$1"
     scala_cross_build=""
-
 fi
 
 set -e
-echo $scala_version
-echo $scala_cross_build
 
 if [ "$COVERAGE" = true ]; then
     sbt $scala_version ${scala_cross_build}clean ${scala_cross_build}coverage ${scala_cross_build}test ${scala_cross_build}coverageReport
