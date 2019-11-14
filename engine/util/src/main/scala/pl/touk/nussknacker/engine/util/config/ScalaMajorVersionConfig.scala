31b2ce8eee492215abea8e99acb7b83e5bcae891
Maciek Próchniak <mpr@touk.pl>
2019-11-14 15:19:58 +0100

    one more binary fixed

--- engine/util/src/main/scala/pl/touk/nussknacker/engine/util/config/ScalaMajorVersionConfig.scala
+++ engine/util/src/main/scala/pl/touk/nussknacker/engine/util/config/ScalaMajorVersionConfig.scala
@@ -9,8 +9,8 @@ object ScalaMajorVersionConfig {
   val scalaMajorVersion: String = util.Properties.versionNumberString.replaceAll("(\\d+\\.\\d+)\\..*$", "$1")
 
   def configWithScalaMajorVersion(config: Config = ConfigFactory.defaultApplication()): Config = {
-    val withBinary = ConfigFactory.parseMap(Collections.singletonMap("scala.major.version", scalaMajorVersion))
-    ConfigFactory.load(config.withFallback(withBinary))
+    val withMajor = ConfigFactory.parseMap(Collections.singletonMap("scala.major.version", scalaMajorVersion))
+    ConfigFactory.load(config.withFallback(withMajor))
 
   }
 
