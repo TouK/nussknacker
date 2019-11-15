ef5db5ae8fcaf3fa535a7ac7be4542e2bec871a9
Maciek Próchniak <mpr@touk.pl>
2019-11-15 07:39:42 +0100

    Travis: cross build in tests only in staging/master

--- ciBackendTest.sh
+++ ciBackendTest.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-
-scala_version=""
-scala_cross_build="+"
-
-if ! [[ -z "$1" ]]; then
-    scala_version="++$1"
-    scala_cross_build=""
-fi
-
 set -e
 
-if [ "$COVERAGE" = true ]; then
-    sbt $scala_version ${scala_cross_build}clean ${scala_cross_build}coverage ${scala_cross_build}test ${scala_cross_build}coverageReport
-    sbt $scala_version ${scala_cross_build}coverageAggregate
+if [[ "$COVERAGE" = true ]]; then
+    ./ciRunSbt.sh clean coverage test coverageReport
+    ./ciRunSbt.sh coverageAggregate
 else
-    sbt $scala_version ${scala_cross_build}clean ${scala_cross_build}test
+    ./ciRunSbt.sh clean test
 fi
---
+++ ciRunSbt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+#if $CROSS_BUILD is true - we do cross build, otherwise we use default scala version
+if [[ $CROSS_BUILD == 'true' ]]
+then
+   #for crossbuild we prepend each arg with +...
+   ARGS=`echo " $*" | sed "s/ \+/ \+/g"`
+else
+   ARGS="$*"
+fi
+sbt $ARGS
