54527320b5143c64c88ec18f7942408bf0b51041
Łukasz Ciołecki <lukasz.ciolecki@gmail.com>
2019-11-15 08:45:57 +0100

    Improvements: Normalize users config (#336)
    Normalize users config

--- engine/security/src/main/scala/pl/touk/nussknacker/ui/security/api/AuthenticationConfiguration.scala
+++ engine/security/src/main/scala/pl/touk/nussknacker/ui/security/api/AuthenticationConfiguration.scala
@@ -4,16 +4,22 @@ import java.io.File
 import java.net.URI
 
 import com.typesafe.config.{Config, ConfigFactory}
-import com.typesafe.scalalogging.LazyLogging
 import net.ceedubs.ficus.readers.ValueReader
+import pl.touk.nussknacker.ui.security.api.AuthenticationConfiguration.{ConfigRule, ConfigUser}
 import pl.touk.nussknacker.ui.security.api.AuthenticationMethod.AuthenticationMethod
-import pl.touk.nussknacker.ui.security.api.DefaultAuthenticationConfiguration.DefaultConfigUser
 import pl.touk.nussknacker.ui.security.api.GlobalPermission.GlobalPermission
 import pl.touk.nussknacker.ui.security.api.Permission.Permission
 
 trait AuthenticationConfiguration {
   def authorizeUrl: Option[URI] = Option.empty
   def method: AuthenticationMethod
+  def usersFile: String
+
+  val userConfig: Config = ConfigFactory.parseFile(new File(usersFile))
+
+  lazy val users: List[ConfigUser] = AuthenticationConfiguration.getUsers(userConfig)
+
+  lazy val rules: List[ConfigRule] = AuthenticationConfiguration.getRules(userConfig)
 }
 
 object AuthenticationMethod extends Enumeration {
@@ -27,6 +33,7 @@ object AuthenticationMethod extends Enumeration {
 object AuthenticationConfiguration {
   import net.ceedubs.ficus.Ficus._
   import net.ceedubs.ficus.readers.EnumerationReader._
+  import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 
   implicit val uriValueReader: ValueReader[URI] = new ValueReader[URI] {
     def read(config: Config, path: String): URI = new URI(config.getString(path))
@@ -38,11 +45,24 @@ object AuthenticationConfiguration {
   val rulesConfigurationPath = "rules"
 
   def parseMethod(config: Config): AuthenticationMethod = config.as[AuthenticationMethod](methodConfigPath)
+
+  def getUsers(config: Config): List[ConfigUser] = config.as[List[ConfigUser]](usersConfigurationPath)
+
+  def getRules(config: Config): List[ConfigRule] = config.as[List[ConfigRule]](rulesConfigurationPath)
+
+  case class ConfigUser(identity: String,
+                        password: Option[String],
+                        encryptedPassword: Option[String],
+                        roles: List[String])
+
+  case class ConfigRule(role: String,
+                        isAdmin: Boolean = false,
+                        categories: List[String] = List.empty,
+                        permissions: List[Permission] = List.empty,
+                        globalPermissions: List[GlobalPermission] = List.empty)
 }
 
-case class DefaultAuthenticationConfiguration(method: AuthenticationMethod = AuthenticationMethod.Other, usersFile: String) extends AuthenticationConfiguration with LazyLogging {
-  lazy val users: List[DefaultConfigUser] = DefaultAuthenticationConfiguration.getUsers(ConfigFactory.parseFile(new File(usersFile)))
-}
+case class DefaultAuthenticationConfiguration(method: AuthenticationMethod = AuthenticationMethod.Other, usersFile: String) extends AuthenticationConfiguration
 
 object DefaultAuthenticationConfiguration {
   import net.ceedubs.ficus.Ficus._
@@ -50,13 +70,6 @@ object DefaultAuthenticationConfiguration {
   import net.ceedubs.ficus.readers.EnumerationReader._
   import AuthenticationConfiguration._
 
-  def create(config: Config): DefaultAuthenticationConfiguration = config.as[DefaultAuthenticationConfiguration](authenticationConfigPath)
-
-  def getUsers(config: Config): List[DefaultConfigUser] = config.as[List[DefaultConfigUser]](usersConfigurationPath)
-
-  case class DefaultConfigUser(id: String, password: Option[String],
-                               encryptedPassword: Option[String],
-                               categoryPermissions: Map[String, Set[Permission]] = Map.empty,
-                               globalPermissions: List[GlobalPermission] = List.empty,
-                               isAdmin: Boolean = false)
+  def create(config: Config): DefaultAuthenticationConfiguration =
+    config.as[DefaultAuthenticationConfiguration](authenticationConfigPath)
 }
