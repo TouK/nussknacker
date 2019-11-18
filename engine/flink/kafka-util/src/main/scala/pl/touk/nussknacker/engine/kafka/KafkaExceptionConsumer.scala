364f5e0c86c6df12d0806617f8193a44545d86b6
Maciek Próchniak <mpr@touk.pl>
2019-11-18 08:49:39 +0100

    cleanup

--- engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/KafkaExceptionConsumer.scala
+++ engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/KafkaExceptionConsumer.scala
@@ -1,15 +1,16 @@
 package pl.touk.nussknacker.engine.kafka
 
 import org.apache.flink.api.common.serialization.SerializationSchema
+import pl.touk.nussknacker.engine.api.MetaData
 import pl.touk.nussknacker.engine.api.exception.{EspExceptionInfo, NonTransientException}
 import pl.touk.nussknacker.engine.flink.api.exception.FlinkEspExceptionConsumer
 
 class KafkaExceptionConsumer(val kafkaConfig: KafkaConfig,
                              topic: String,
-                             serializationSchema: SerializationSchema[EspExceptionInfo[NonTransientException]])
+                             serializationSchema: SerializationSchema[EspExceptionInfo[NonTransientException]])(implicit metaData: MetaData)
   extends FlinkEspExceptionConsumer {
 
-  lazy val producer = KafkaEspUtils.createProducer(kafkaConfig, "exception-"+topic)
+  private lazy val producer = KafkaEspUtils.createProducer(kafkaConfig, s"exception-${metaData.id}")
 
   def consume(exceptionInfo: EspExceptionInfo[NonTransientException]): Unit = {
     val toSend = serializationSchema.serialize(exceptionInfo)
