c61a9fd86464a4ce2140d9f32f77cfca73bcea06
Krzysztof Gadomski <kgd@touk.pl>
2019-11-26 09:18:19 +0100

    use user passed from listener plugin

--- ui/listener-api/src/main/scala/pl/touk/nussknacker/ui/listener/services/PullProcessRepository.scala
+++ ui/listener-api/src/main/scala/pl/touk/nussknacker/ui/listener/services/PullProcessRepository.scala
@@ -2,13 +2,14 @@ package pl.touk.nussknacker.ui.listener.services
 
 import pl.touk.nussknacker.restmodel.process.ProcessId
 import pl.touk.nussknacker.restmodel.processdetails.BaseProcessDetails
+import pl.touk.nussknacker.ui.security.api.LoggedUser
 
 import scala.concurrent.{ExecutionContext, Future}
 
 trait PullProcessRepository {
   def fetchLatestProcessDetailsForProcessId(id: ProcessId)
-                                           (implicit ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]]
+                                           (implicit loggedUser: LoggedUser, ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]]
 
   def fetchProcessDetailsForId(processId: ProcessId, versionId: Long)
-                              (implicit ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]]
+                              (implicit loggedUser: LoggedUser, ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]]
 }
--- ui/server/src/main/scala/pl/touk/nussknacker/ui/process/repository/PullProcessRepository.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/process/repository/PullProcessRepository.scala
@@ -2,20 +2,18 @@ package pl.touk.nussknacker.ui.process.repository
 
 import pl.touk.nussknacker.restmodel.processdetails.BaseProcessDetails
 import pl.touk.nussknacker.restmodel.process
-import pl.touk.nussknacker.ui.security.api.ReadOnlyTechUser
 import pl.touk.nussknacker.ui.listener.services.{PullProcessRepository => ListenerPullProcessRepository}
+import pl.touk.nussknacker.ui.security.api.LoggedUser
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class PullProcessRepository(fetchingProcessRepository: FetchingProcessRepository[Future]) extends ListenerPullProcessRepository {
 
-  private implicit val user = ReadOnlyTechUser
-
   override def fetchLatestProcessDetailsForProcessId(id: process.ProcessId)
-                                                    (implicit ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]] =
+                                                    (implicit loggedUser: LoggedUser, ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]] =
     fetchingProcessRepository.fetchLatestProcessDetailsForProcessId(id = id, businessView = false)
 
   override def fetchProcessDetailsForId(processId: process.ProcessId, versionId: Long)
-                                       (implicit ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]] =
+                                       (implicit loggedUser: LoggedUser, ec: ExecutionContext): Future[Option[BaseProcessDetails[Unit]]] =
     fetchingProcessRepository.fetchProcessDetailsForId(processId, versionId, businessView = false)
 }
