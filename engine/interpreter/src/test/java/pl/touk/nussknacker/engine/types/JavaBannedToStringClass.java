01466f08b8d3e076794eb887a3af4a939678ae7b
Krzysztof Gadomski <kgd@touk.pl>
2019-12-03 08:52:39 +0100

    add marker interface for hiding toString method in code completion

---
+++ engine/interpreter/src/test/java/pl/touk/nussknacker/engine/types/JavaBannedToStringClass.java
@@ -0,0 +1,6 @@
+package pl.touk.nussknacker.engine.types;
+
+import pl.touk.nussknacker.engine.api.HideToString;
+
+public class JavaBannedToStringClass extends JavaSampleClass implements HideToString {
+}
