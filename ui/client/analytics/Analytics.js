8ab9aa913bb8cc4376457a169f8b1669e25d7fb7
Paweł Piędel <pawelpiedel@yahoo.pl>
2019-12-10 09:30:19 +0100

    Rename matomo const

--- ui/client/analytics/Analytics.js
+++ ui/client/analytics/Analytics.js
@@ -1,14 +1,14 @@
 import MatomoAnalytics from "./MatomoAnalytics"
 
+const MATOMO_ENGINE = 'Matomo'
+
 export default class Analytics {
 
   constructor(analyticsSettings) {
-    if (analyticsSettings.engine === matomoEngine) {
+    if (analyticsSettings.engine === MATOMO_ENGINE) {
       this.engine = new MatomoAnalytics(analyticsSettings)
     }
   }
 
   sendEvent = (event) => this.engine.sendEvent(event)
 }
-
-const matomoEngine = 'Matomo'
