00f918d699d261e9e44166919b2ccfcf4e01d31c
Maciek Próchniak <mpr@touk.pl>
2019-12-18 13:53:20 +0100

    [FIX] Kafka signal connector emits watermarks

--- engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/flink/util/signal/KafkaSignalStreamConnector.scala
+++ engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/flink/util/signal/KafkaSignalStreamConnector.scala
@@ -2,6 +2,7 @@ package pl.touk.nussknacker.engine.flink.util.signal
 
 import org.apache.flink.api.common.serialization.DeserializationSchema
 import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.streaming.api.functions.IngestionTimeExtractor
 import org.apache.flink.streaming.api.scala.{ConnectedStreams, DataStream}
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
 import pl.touk.nussknacker.engine.kafka.{KafkaConfig, KafkaEspUtils}
@@ -13,7 +14,11 @@ trait KafkaSignalStreamConnector {
   def connectWithSignals[A, B: TypeInformation](start: DataStream[A], processId: String, nodeId: String, schema: DeserializationSchema[B]): ConnectedStreams[A, B] = {
     val signalsSource = new FlinkKafkaConsumer011[B](signalsTopic, schema,
       KafkaEspUtils.toProperties(kafkaConfig, Some(s"$processId-$nodeId-signal")))
-    val signals = start.executionEnvironment.addSource(signalsSource).name(s"signals-$processId-$nodeId")
+    val signals = start.executionEnvironment
+      .addSource(signalsSource).name(s"signals-$processId-$nodeId")
+      //We use ingestion time here, to advance watermarks in connected streams
+      //TODO: this is not always optimal solution, as e.g. in tests periodic watermarks are not the best option
+      .assignTimestampsAndWatermarks(new IngestionTimeExtractor[B])
     start.connect(signals)
   }
 }
---
+++ engine/flink/process/src/test/scala/pl/touk/nussknacker/engine/process/functional/KafkaSignalsSpec.scala
@@ -0,0 +1,56 @@
+package pl.touk.nussknacker.engine.process.functional
+
+import java.util.Date
+
+import com.typesafe.config.ConfigFactory
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
+import pl.touk.nussknacker.engine.api.ProcessVersion
+import pl.touk.nussknacker.engine.build.EspProcessBuilder
+import pl.touk.nussknacker.engine.flink.test.{FlinkTestConfiguration, StoppableExecutionEnvironment}
+import pl.touk.nussknacker.engine.kafka.KafkaSpec
+import pl.touk.nussknacker.engine.process.compiler.FlinkStreamingProcessCompiler
+import pl.touk.nussknacker.engine.process.helpers.ProcessTestHelpers.processInvoker
+import pl.touk.nussknacker.engine.process.helpers.SampleNodes.{MockService, SimpleRecord}
+import pl.touk.nussknacker.engine.spel.Implicits._
+import pl.touk.nussknacker.test.VeryPatientScalaFutures
+
+class KafkaSignalsSpec extends FunSuite with Matchers with BeforeAndAfterAll with KafkaSpec with VeryPatientScalaFutures {
+
+  private val env: StoppableExecutionEnvironment = StoppableExecutionEnvironment(FlinkTestConfiguration.configuration())
+
+  override protected def afterAll(): Unit = {
+    env.stop()
+    super.afterAll()
+  }
+
+  test("signals don't cause watermarks to stop") {
+    val process =
+      EspProcessBuilder
+        .id("proc1")
+        .exceptionHandler()
+        .source("id", "input")
+        .customNodeNoOutput("signal", "signalReader")
+        .customNode("cid", "count", "transformWithTime", "seconds" -> "1")
+        .processorEnd("out", "logService", "all" -> "#count")
+
+    def record(time: Long) = SimpleRecord(time.toString, 0, "", new Date(time))
+
+    val creator = processInvoker.prepareCreator(List(
+      record(1000),
+      record(1200),
+      record(2000)
+    ), kafkaConfig)
+
+    new FlinkStreamingProcessCompiler(creator, ConfigFactory.load()).createFlinkProcessRegistrar()
+      .register(new StreamExecutionEnvironment(env), process, ProcessVersion.empty)
+
+    env.withJobRunning(process.id) {
+      eventually {
+        MockService.data shouldBe List(2, 1)
+      }
+    }
+
+  }
+
+}
