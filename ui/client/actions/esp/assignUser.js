cb0e1ff6864d8d6b368a93992f6f33db6f6d027a
Julian Wielga <j@touk.pl>
2020-01-09 14:39:12 +0100

    refactor User

--- ui/client/actions/esp/assignUser.js
+++ ui/client/actions/esp/assignUser.js
@@ -1,6 +1,14 @@
+// @flow
+import type {UserData} from "../../common/models/User"
 import User from "../../common/models/User"
 
-export function assignUser(data) {
+export type LoggedUserAction = {
+  type: "LOGGED_USER",
+  user: User,
+}
+
+
+export function assignUser(data: UserData): LoggedUserAction {
   return {
     type: "LOGGED_USER",
     user: new User(data),
