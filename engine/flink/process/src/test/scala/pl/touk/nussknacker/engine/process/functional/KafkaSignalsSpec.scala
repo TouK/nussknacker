221d8d4dc97ee5dc0ec14fc4934eb66fe53a1b84
Maciek Próchniak <mpr@touk.pl>
2020-01-10 07:14:17 +0100

    Compile fix

--- engine/flink/process/src/test/scala/pl/touk/nussknacker/engine/process/functional/KafkaSignalsSpec.scala
+++ engine/flink/process/src/test/scala/pl/touk/nussknacker/engine/process/functional/KafkaSignalsSpec.scala
@@ -9,11 +9,13 @@ import pl.touk.nussknacker.engine.api.ProcessVersion
 import pl.touk.nussknacker.engine.build.EspProcessBuilder
 import pl.touk.nussknacker.engine.flink.test.{FlinkTestConfiguration, StoppableExecutionEnvironment}
 import pl.touk.nussknacker.engine.kafka.KafkaSpec
-import pl.touk.nussknacker.engine.process.compiler.FlinkStreamingProcessCompiler
+import pl.touk.nussknacker.engine.process.FlinkStreamingProcessRegistrar
+import pl.touk.nussknacker.engine.process.compiler.FlinkProcessCompiler
 import pl.touk.nussknacker.engine.process.helpers.ProcessTestHelpers
 import pl.touk.nussknacker.engine.process.helpers.ProcessTestHelpers.processInvoker
 import pl.touk.nussknacker.engine.process.helpers.SampleNodes.{MockService, SimpleRecord}
 import pl.touk.nussknacker.engine.spel.Implicits._
+import pl.touk.nussknacker.engine.testing.LocalModelData
 import pl.touk.nussknacker.test.VeryPatientScalaFutures
 
 class KafkaSignalsSpec extends FunSuite with Matchers with BeforeAndAfterAll with KafkaSpec with VeryPatientScalaFutures {
@@ -46,7 +48,8 @@ class KafkaSignalsSpec extends FunSuite with Matchers with BeforeAndAfterAll wit
       record(2000)
     ), kafkaConfig)
 
-    new FlinkStreamingProcessCompiler(creator, ConfigFactory.load()).createFlinkProcessRegistrar()
+    val modelData = LocalModelData(ConfigFactory.load(), creator)
+    FlinkStreamingProcessRegistrar(new FlinkProcessCompiler(modelData), modelData.processConfig)
       .register(new StreamExecutionEnvironment(env), process, ProcessVersion.empty)
 
     env.withJobRunning(process.id) {
