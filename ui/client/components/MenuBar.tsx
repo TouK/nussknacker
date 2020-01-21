4d7b23bb4324530758e5cefa3fbd154003bc6698
Julian Wielga <j@touk.pl>
2020-01-21 16:15:22 +0100

    extracted MenuBar, added rwd

---
+++ ui/client/components/MenuBar.tsx
@@ -0,0 +1,82 @@
+import _ from "lodash"
+import React, {ReactNode, useEffect, useState} from "react"
+import {useTranslation} from "react-i18next"
+import {NavLink} from "react-router-dom"
+import {$TodoType} from "../actions/migrationTypes"
+import AdminPage from "../containers/AdminPage"
+import Archive from "../containers/Archive"
+import EspApp from "../containers/EspApp"
+import Metrics from "../containers/Metrics"
+import Processes from "../containers/Processes"
+import Search from "../containers/Search"
+import Signals from "../containers/Signals"
+import SubProcesses from "../containers/SubProcesses"
+import {Flex} from "./common/Flex"
+
+function useStateWithRevertTimeout<T>(startValue: T, time = 10000): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [defaultValue] = useState<T>(startValue)
+  const [value, setValue] = useState<T>(defaultValue)
+  useEffect(() => {
+    let t: NodeJS.Timeout
+    if (value) {
+      t = setTimeout(() => {
+        setValue(defaultValue)
+      }, time)
+    }
+    return () => clearTimeout(t)
+  }, [value, time])
+  return [value, setValue]
+}
+
+type Props = {
+  app: typeof EspApp;
+  rightElement?: ReactNode;
+  leftElement?: ReactNode;
+  loggedUser: $TodoType;
+  featuresSettings: $TodoType;
+}
+
+const Spacer = () => <Flex flex={1}/>
+
+export function MenuBar({rightElement = null, leftElement = null, ...props}: Props) {
+  const {app: {path, header}, loggedUser, featuresSettings} = props
+  const showMetrics = !_.isEmpty(featuresSettings.metrics)
+  const showSearch = !_.isEmpty(featuresSettings.search)
+  const showSignals = featuresSettings.signals
+  const showAdmin = loggedUser.globalPermissions.adminTab
+
+  const [expanded, setExpanded] = useStateWithRevertTimeout(false)
+  const {t} = useTranslation()
+
+  return (
+      <header>
+        <nav id="main-menu" className={`navbar navbar-default ${expanded ? "expanded" : "collapsed"}`}>
+          <Flex>
+            {leftElement}
+            <NavLink id="brand-name" className="navbar-brand" to={path}>
+              <span id="app-logo" className="vert-middle">{header}</span>
+            </NavLink>
+            {rightElement}
+            <Spacer/>
+            <button className="expand-button" onClick={() => setExpanded(!expanded)}>
+              <span className={`glyphicon glyphicon-menu-${expanded ? "up" : "down"}`}/>
+            </button>
+            <ul id="menu-items" onClick={() => setExpanded(false)}>
+              <MenuItem path={Processes.path} title={t("menu.processes", "Processes")}/>
+              <MenuItem path={SubProcesses.path} title={t("menu.subProcesses", "Subprocesses")}/>
+              {showMetrics && <MenuItem path={Metrics.basePath} title={t("menu.metrics", "Metrics")}/>}
+              {showSearch && <MenuItem path={Search.path} title={t("menu.search", "Search")}/>}
+              {showSignals && <MenuItem path={Signals.path} title={t("menu.signals", "Signals")}/>}
+              <MenuItem path={Archive.path} title={t("menu.archive", "Archive")}/>
+              {showAdmin && <MenuItem path={AdminPage.path} title={t("menu.adminPage", "Admin")}/>}
+            </ul>
+          </Flex>
+        </nav>
+      </header>
+  )
+}
+
+function MenuItem({title, path}: { title: string; path: string }) {
+  return <li key={title}><NavLink to={path}>{title}</NavLink></li>
+}
+
--- ui/client/containers/EspApp.js
+++ ui/client/containers/EspApp.js
@@ -1,8 +1,9 @@
 import React from "react"
-import {matchPath, NavLink, Route, Switch, withRouter} from "react-router-dom"
-import _ from "lodash"
-import Processes from "./Processes"
 import {Redirect} from "react-router"
+import {matchPath, Route, Switch, withRouter} from "react-router-dom"
+import _ from "lodash"
+import {MenuBar} from "../components/MenuBar"
+import Processes from "./Processes"
 import SubProcesses from "./SubProcesses"
 import NotFound from "./errors/NotFound"
 import {nkPath} from "../config";
@@ -71,9 +72,7 @@ class EspApp extends React.Component {
   environmentAlert(params) {
     if (params && params.content)
       return (
-        <span className="navbar-brand vert-middle ">
-          <span className={`indicator ${  params.cssClass}`}>{params.content}</span>
-        </span>
+          <span className={`indicator ${params.cssClass}`} title={params.content}>{params.content}</span>
       )
   }
 
@@ -81,42 +80,13 @@ class EspApp extends React.Component {
     const AllDialogs = Dialogs.AllDialogs
     return this.props.resolved ? (
       <div id="app-container">
-        <nav id="main-menu" className="navbar navbar-default">
-          <div id="git" className="hide">{JSON.stringify(__GIT__)}</div>
-          <div className="container-fluid">
-            <div className="navbar-header">
-              {this.renderTopLeftButton()}
-              <NavLink id="brand-name" className="navbar-brand" to={EspApp.path}>
-                <span id="app-logo" className="vert-middle">{EspApp.header}</span>
-              </NavLink>
-              {this.environmentAlert(this.props.featuresSettings.environmentAlert)}
-            </div>
-
-            <div className="collapse navbar-collapse">
-              <ul id="menu-items" className="nav navbar-nav navbar-right nav-pills nav-stacked">
-                <li><NavLink to={Processes.path}>{Processes.header}</NavLink></li>
-                <li><NavLink to={SubProcesses.path}>{SubProcesses.header}</NavLink></li>
-                {
-                  !_.isEmpty(this.props.featuresSettings.metrics) ?
-                    <li><NavLink to={Metrics.basePath}>{Metrics.header}</NavLink></li> : null
-                }
-                {
-                  !_.isEmpty(this.props.featuresSettings.search) ?
-                    <li><NavLink to={Search.path}>{Search.header}</NavLink></li> : null
-                }
-                {
-                  this.props.featuresSettings.signals ?
-                    <li><NavLink to={Signals.path}>{Signals.header}</NavLink></li> : null
-                }
-                <li><NavLink to={Archive.path}>{Archive.header}</NavLink></li>
-                {
-                  this.props.loggedUser.globalPermissions.adminTab ?
-                    <li><NavLink to={AdminPage.path}>{AdminPage.header}</NavLink></li> : null
-                }
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <div className="hide">{JSON.stringify(__GIT__)}</div>
+        <MenuBar
+            {...this.props}
+            app={EspApp}
+            leftElement={this.renderTopLeftButton()}
+            rightElement={this.environmentAlert(this.props.featuresSettings.environmentAlert)}
+        />
         <main>
           <DragArea>
             <AllDialogs/>
