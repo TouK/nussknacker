fb9f99f841ab5166d585d32f761e72ec75892166
Julian Wielga <j@touk.pl>
2020-02-05 09:27:08 +0100

    revert SwitchIcon.js

---
+++ ui/client/components/graph/node-modal/editors/expression/SwitchIcon.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types"
+import React from "react"
+import * as LoaderUtils from "../../../../../common/LoaderUtils"
+
+export default function SwitchIcon(props) {
+
+  const {switchable, readOnly, hint, onClick, displayRawEditor, shouldShowSwitch} = props
+
+  const title = () => readOnly ? "Switching to basic mode is disabled. You are in read-only mode" : hint
+
+  return (
+    shouldShowSwitch ?
+      <button className={`inlined switch-icon${displayRawEditor ? " active " : ""}${readOnly ? " read-only " : ""}`}
+              onClick={onClick}
+              disabled={!switchable || readOnly}
+              title={title()}>
+        <div dangerouslySetInnerHTML={{__html: LoaderUtils.loadSvgContent("buttons/switch.svg")}}/>
+      </button> : null
+  )
+}
+
+SwitchIcon.propTypes = {
+  switchable: PropTypes.bool,
+  hint: PropTypes.string,
+  onClick: PropTypes.func,
+  shouldShowSwitch: PropTypes.bool,
+  displayRawEditor: PropTypes.bool,
+  readOnly: PropTypes.bool,
+}
