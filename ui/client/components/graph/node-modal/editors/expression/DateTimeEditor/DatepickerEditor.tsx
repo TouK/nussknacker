ae8bbfc10c90e786ef6d83213fb271a9c25aab65
Julian Wielga <j@touk.pl>
2020-02-10 16:58:14 +0100

    removed debugger

--- ui/client/components/graph/node-modal/editors/expression/DateTimeEditor/DatepickerEditor.tsx
+++ ui/client/components/graph/node-modal/editors/expression/DateTimeEditor/DatepickerEditor.tsx
@@ -9,7 +9,7 @@ import moment from "moment"
 import ValidationLabels from "../../../../../modals/ValidationLabels"
 import i18next from "i18next"
 import * as ExpressionFormatter from "./dateExpresionFormats"
-import {allValid, Validator} from "../../Validators";
+import {allValid, Validator} from "../../Validators"
 
 /* eslint-disable i18next/no-literal-string */
 export enum JavaTimeTypes {
@@ -38,15 +38,14 @@ const parse = ({expression}: ExpressionObj, expressionType: JavaTimeTypes): mome
   const parseRegExp = ExpressionFormatter.getParseRegExp()
   const [fullString, date] = parseRegExp.exec(expression) || []
   const formats = expressionType === JavaTimeTypes.LOCAL_TIME ?
-      ExpressionFormatter.getTimeOnlyFormat() :
-      ExpressionFormatter.getDateTimeFormat()
+    ExpressionFormatter.getTimeOnlyFormat() :
+    ExpressionFormatter.getDateTimeFormat()
 
   return moment(date, formats) || null
 }
 
 function format(value: string | moment.Moment, expressionType: JavaTimeTypes): string {
   const m = moment(value)
-  debugger
   if (m.isValid()) {
     switch (expressionType) {
       case JavaTimeTypes.LOCAL_DATE_TIME:
@@ -77,18 +76,18 @@ export function DatepickerEditor(props: DatepickerEditorProps) {
   const [value, setValue] = useState<string | moment.Moment>(moment(parse(expressionObj, expressionType)))
   const {expression} = expressionObj
   const [onChange] = useDebouncedCallback(
-      value => {
-        const date = format(value, expressionType)
-        onValueChange(date)
-      },
-      200,
+    value => {
+      const date = format(value, expressionType)
+      onValueChange(date)
+    },
+    200,
   )
 
   useEffect(
-      () => {
-        onChange(value)
-      },
-      [value],
+    () => {
+      onChange(value)
+    },
+    [value],
   )
 
   const isValid = allValid(validators, [expression])
@@ -98,36 +97,36 @@ export function DatepickerEditor(props: DatepickerEditorProps) {
   ]
 
   return (
-      <div
-          className={className}
-      >
-        <DateTimePicker
-            onChange={setValue}
-            value={value}
-            inputProps={{
-              className: classNames([
-                "node-input",
-                showValidation && !isValid && "node-input-with-error",
-                isMarked && "marked",
-                editorFocused && "focused",
-                readOnly && "read-only",
-              ]),
-              readOnly,
-              disabled: readOnly,
-            }}
-            locale={i18n.language}
-            {...other}
+    <div
+      className={className}
+    >
+      <DateTimePicker
+        onChange={setValue}
+        value={value}
+        inputProps={{
+          className: classNames([
+            "node-input",
+            showValidation && !isValid && "node-input-with-error",
+            isMarked && "marked",
+            editorFocused && "focused",
+            readOnly && "read-only",
+          ]),
+          readOnly,
+          disabled: readOnly,
+        }}
+        locale={i18n.language}
+        {...other}
+      />
+      {showValidation && (
+        <ValidationLabels
+          validators={[
+            ...localValidators,
+            ...validators,
+          ]}
+          values={[expression]}
         />
-        {showValidation && (
-            <ValidationLabels
-                validators={[
-                  ...localValidators,
-                  ...validators,
-                ]}
-                values={[expression]}
-            />
-        )}
-      </div>
+      )}
+    </div>
   )
 }
 
