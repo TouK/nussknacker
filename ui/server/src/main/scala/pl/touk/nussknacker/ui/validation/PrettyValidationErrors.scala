154319a88206c91f81bfb1002e59344082b7d0ba
Paweł Piędel <pawelpiedel@yahoo.pl>
2020-02-11 12:14:01 +0100

    Validate empty expressions

--- ui/server/src/main/scala/pl/touk/nussknacker/ui/validation/PrettyValidationErrors.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/validation/PrettyValidationErrors.scala
@@ -29,6 +29,8 @@ object PrettyValidationErrors {
         node(s"Global process parameters not filled", s"Please fill process properties ${params.mkString(", ")} by clicking 'Properties button'")
       case MissingParameters(params, _) =>
         node(s"Node parameters not filled", s"Please fill missing node parameters: : ${params.mkString(", ")}")
+      case EmptyMandatoryParameter(paramName, _) =>
+        node(s"Empty expression for mandatory parameter", s"Please fill expression for this parameter", fieldName = Some(paramName))
       //exceptions below should not really happen (unless services change and process becomes invalid)
       case MissingCustomNodeExecutor(id, _) => node(s"Missing custom executor: $id", s"Please check the name of custom executor, $id is not available")
       case MissingService(id, _) => node(s"Missing processor/enricher: $id", s"Please check the name of processor/enricher, $id is not available")
