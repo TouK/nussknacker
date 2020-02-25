66f232cbc6e05367be0ac5ec5d8628900bc560c8
Paweł Piędel <pawelpiedel@yahoo.pl>
2020-02-25 13:18:01 +0100

    Refactor validation test

--- ui/client/test/ValidationLabels-test.js
+++ ui/client/test/ValidationLabels-test.js
@@ -5,48 +5,40 @@ import ValidationLabels from "../components/modals/ValidationLabels"
 
 describe("test validation labels", () => {
   const backendErrorDescription = "test"
-  const fieldName = "fieldName"
-  const emptyValue = ""
-  const validators = (backendError) => [mandatoryValueValidator, errorValidator([backendError], fieldName)]
+  const backendError = (errorType) => ({
+    message: "Test",
+    description: backendErrorDescription,
+    typ: errorType,
+    fieldName: "fieldName"
+  })
 
   const testCases = [
     {
       description: "display only fe validation label when both be and fe validators available for the same error type",
-      error: backendError(mandatoryValueValidator.handledErrorType),
-      expectedFeValidationLabels: 1,
+      errorType: mandatoryValueValidator.handledErrorType,
       expectedBackendValidationLabels: 0,
     },
     {
       description: "display both validation labels for different error type",
-      error: backendError("AnotherErrorType"),
-      expectedFeValidationLabels: 1,
+      errorType: "AnotherErrorType",
       expectedBackendValidationLabels: 1,
     },
   ]
 
-  testCases.forEach((testCase) => {
-    it(testCase.description, () => {
+  testCases.forEach(({description, errorType, expectedBackendValidationLabels}) => {
+    it(description, () => {
       //given
       const validators = [
         mandatoryValueValidator,
-        errorValidator([testCase.error], "fieldName"),
+        errorValidator([backendError(errorType)], "fieldName"),
       ]
 
       //when
-      render(<ValidationLabels validators={validators} values={[emptyValue]}/>)
+      render(<ValidationLabels validators={validators} values={[""]}/>)
 
       //then
-      expect(screen.findAllByText(mandatoryValueValidator.description).length).toBe(testCase.expectedFeValidationLabels)
-      expect(screen.findAllByText(backendErrorDescription).length).toBe(testCase.expectedBackendValidationLabels)
+      expect(screen.findAllByText(mandatoryValueValidator.description).length).toBe(1)
+      expect(screen.findAllByText(backendErrorDescription).length).toBe(expectedBackendValidationLabels)
     })
   })
-
-  function backendError(errorType) {
-    return {
-      message: "Test",
-      description: backendErrorDescription,
-      typ: errorType,
-      fieldName: "fieldName"
-    }
-  }
 })
