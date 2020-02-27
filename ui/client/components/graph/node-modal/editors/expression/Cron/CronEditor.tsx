928971736100c6722bcec30f607ccdefd1313f11
PPiedel <pawelpiedel@yahoo.pl>
2020-02-27 12:30:45 +0100

    Fix cron editor in read only mode appearance (#727)

--- ui/client/components/graph/node-modal/editors/expression/Cron/CronEditor.tsx
+++ ui/client/components/graph/node-modal/editors/expression/Cron/CronEditor.tsx
@@ -5,7 +5,7 @@ import Cron from "react-cron-generator"
 import "react-cron-generator/dist/cron-builder.css"
 import Input from "../../field/Input"
 import "./cronEditorStyle.styl"
-import i18next from "i18next";
+import i18next from "i18next"
 
 type CronExpression = string
 
@@ -26,19 +26,20 @@ const CRON_SPEL_EXPRESSION = (value) => `T(com.cronutils.parser.CronParser).pars
 
 // we have to pass some value to <Cron/> component
 // when expression is empty - this component sets some default cron value and trigger onValueChange - we don't want that
-const NOT_EXISTING_CRON_EXPRESSION = "-1 -1 -1 -1 -1 -1 -1";
+const NOT_EXISTING_CRON_EXPRESSION = "-1 -1 -1 -1 -1 -1 -1"
 
 export default function CronEditor(props: Props) {
   const node = useRef(null)
 
-  const {expressionObj, validators, isMarked, editorFocused, onValueChange, className, showValidation} = props
+  const {expressionObj, validators, isMarked, onValueChange, showValidation, readOnly} = props
 
   function encode(value) {
     return value == "" ? "" : CRON_SPEL_EXPRESSION(value)
   }
 
   function decode(expression: string): CronExpression {
-    return expression === "" ? "" : CRON_DECODE_REGEX.exec(expression)[1]
+    const cronRegexExec = CRON_DECODE_REGEX.exec(expression)
+    return cronRegexExec == null ? "" : cronRegexExec[1]
   }
 
   const [value, setValue] = useState(decode(expressionObj.expression))
@@ -46,23 +47,22 @@ export default function CronEditor(props: Props) {
 
   const handleClickOutside = e => {
     if (node.current.contains(e.target)) {
-      return;
+      return
     }
-    setOpen(false);
+    setOpen(false)
   }
 
   useEffect(() => {
-      if (open) {
-        document.addEventListener("mousedown", handleClickOutside);
-      } else {
-        document.removeEventListener("mousedown", handleClickOutside);
-      }
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      }
-    },
-    [open],
-  )
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside)
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  },
+  [open])
 
   useEffect(
     () => {
@@ -71,6 +71,12 @@ export default function CronEditor(props: Props) {
     [value],
   )
 
+  const onInputFocus = () => {
+    if (!readOnly) {
+      setOpen(true)
+    }
+  }
+
   return (
     <div ref={node} className={"cron-editor-container"}>
       <Input
@@ -78,28 +84,29 @@ export default function CronEditor(props: Props) {
         formattedValue={expressionObj.expression}
         validators={validators}
         isMarked={isMarked}
-        onFocus={() => setOpen(true)}
+        onFocus={onInputFocus}
         showValidation={showValidation}
+        readOnly={readOnly}
+        inputClassName={readOnly ? "read-only" : ""}
       />
       {
-        open &&
-        <Cron
-          onChange={(e) => {
-            setValue(e)
-          }}
-          value={value === "" ? NOT_EXISTING_CRON_EXPRESSION : value}
-          showResultText={true}
-          showResultCron={false}
-        />
-      }
+        open && (
+          <Cron
+            onChange={(e) => {
+              setValue(e)
+            }}
+            value={value === "" ? NOT_EXISTING_CRON_EXPRESSION : value}
+            showResultText={true}
+            showResultCron={false}
+          />
+        )}
     </div>
   )
 }
 
-CronEditor.switchableTo = (expressionObj: ExpressionObj) =>
-  CRON_DECODE_REGEX.test(expressionObj.expression) || expressionObj.expression === ""
+CronEditor.switchableTo = (expressionObj: ExpressionObj) => CRON_DECODE_REGEX.test(expressionObj.expression) || expressionObj.expression === ""
 
-CronEditor.switchableToHint = () => i18next.t("editors.cron..switchableToHint","Switch to basic mode")
+CronEditor.switchableToHint = () => i18next.t("editors.cron..switchableToHint", "Switch to basic mode")
 
 CronEditor.notSwitchableToHint = () => i18next.t("editors.cron.notSwitchableToHint",
-  "Expression must match pattern T(com.cronutils.parser.CronParser).parse('* * * * * * *') to switch to basic mode" )
+  "Expression must match pattern T(com.cronutils.parser.CronParser).parse('* * * * * * *') to switch to basic mode")
