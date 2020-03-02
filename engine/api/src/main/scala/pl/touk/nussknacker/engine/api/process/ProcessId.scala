7450ee93fd8d03d36187dffa05ca3c3aedd2a788
Arek Burdach <arek.burdach@gmail.com>
2020-03-02 14:53:54 +0100

    Java interop fix: replaced package objects with classes on the same package level (#734)

---
+++ engine/api/src/main/scala/pl/touk/nussknacker/engine/api/process/ProcessId.scala
@@ -0,0 +1,17 @@
+package pl.touk.nussknacker.engine.api.process
+
+import io.circe.{Decoder, Encoder}
+
+import scala.util.{Failure, Success, Try}
+
+final case class ProcessId(value: Long) extends AnyVal
+
+object ProcessId {
+  implicit val encoder: Encoder[ProcessId] = Encoder.encodeLong.contramap(_.value)
+  implicit val decoder: Decoder[ProcessId] = Decoder.decodeLong.map(ProcessId(_))
+
+  def apply(value: String): ProcessId = Try(value.toLong) match {
+    case Success(id) => ProcessId(id)
+    case Failure(_) => throw new IllegalArgumentException(s"Value '$value' is not valid ProcessId.")
+  }
+}
