ba3c241e7c664626b5db9061f466ca480a45c778
Piotr Przybylski <ppr@touk.pl>
2020-03-16 08:33:27 +0100

    Demo update (#800)

--- demo/docker/testData/sendToKafka.sh
+++ demo/docker/testData/sendToKafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #TODO: is it always this?
 CONTAINER_NAME=nussknacker_kafka
-docker exec -i $CONTAINER_NAME "/opt/kafka/bin/kafka-console-producer.sh" --topic $1 --broker-list localhost:9092
+docker exec -i $CONTAINER_NAME kafka-console-producer.sh --topic $1 --broker-list localhost:9092
