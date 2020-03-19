be7a5574c0a91ea49fea673baf9dfa08236ef859
Maciek Próchniak <mpr@touk.pl>
2020-03-19 12:30:33 +0100

    Review

--- ui/client/components/graph/node-modal/NodeAdditionalInfoBox.tsx
+++ ui/client/components/graph/node-modal/NodeAdditionalInfoBox.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import HttpService from "../../../http/HttpService";
-import ReactMarkdown from 'react-markdown/with-html';
+import React, {useState, useEffect} from "react"
+import HttpService from "../../../http/HttpService"
+import ReactMarkdown from "react-markdown/with-html"
 import "../../../stylesheets/markdown.styl"
 
 type Props = {
   processId: string,
-  node: any
+  node: any,
 }
 
 //Types should match implementations of NodeAdditionalInfo on Backend!
@@ -13,34 +13,34 @@ type NodeAdditionalInfo = MarkdownNodeAdditionalInfo
 
 type MarkdownNodeAdditionalInfo = {
     type: "MarkdownNodeAdditionalInfo",
-    content: string
+    content: string,
 }
-
 
 export default function NodeAdditionalInfoBox(props: Props) {
 
-    const [additionalData, setAdditionalData] = useState<NodeAdditionalInfo>(null)
-    const {
-      processId, node
-    } = props
+  const [additionalData, setAdditionalData] = useState<NodeAdditionalInfo>(null)
+  const {
+    processId, node,
+  } = props
 
-    useEffect(() => {
-        if (node?.type) {
-            HttpService.getNodeAdditionalData(processId, node).then(res => setAdditionalData(res.data))
-        }
-    }, [processId, node])
-
-    // eslint-disable-next-line i18next/no-literal-string
-    const linkTarget = "_blank"
-    switch (additionalData?.type){
-        case "MarkdownNodeAdditionalInfo":
-            return <ReactMarkdown source={additionalData.content} className="markdownDisplay" linkTarget={linkTarget} />
-        case null:
-            return null
-        case undefined:
-            return null
-        default:
-            console.log("Unknown type:", additionalData.type)
-            return null
+  useEffect(() => {
+    if (node?.type) {
+      HttpService.getNodeAdditionalData(processId, node).then(res => setAdditionalData(res.data))
     }
+  }, [processId, node])
+
+  const type = additionalData?.type
+  if (!type) {
+    return null
+  }
+  switch (type){
+    case "MarkdownNodeAdditionalInfo":
+      // eslint-disable-next-line i18next/no-literal-string
+      const linkTarget = "_blank"
+      return <ReactMarkdown source={additionalData.content} className="markdownDisplay" linkTarget={linkTarget}/>
+    default:
+      // eslint-disable-next-line i18next/no-literal-string
+      console.warn("Unknown type:", additionalData.type)
+      return null
+  }
 }
