3c521dbf5bc62518a984d29737bd6def0df50d44
Maciej Próchniak <mpr@touk.pl>
2020-04-06 10:23:18 +0200

    [Performance] first benchmarks, ScalaPropertyAccessor performance fix, more performant EvaluationContext initialization, small refactoring (#847)

--- engine/interpreter/src/main/scala/pl/touk/nussknacker/engine/expression/ExpressionEvaluator.scala
+++ engine/interpreter/src/main/scala/pl/touk/nussknacker/engine/expression/ExpressionEvaluator.scala
@@ -81,6 +81,8 @@ class ExpressionEvaluator(globalVariablesPreparer: GlobalVariablesPreparer,
   def evaluate[R](expr: Expression, expressionId: String, nodeId: String, ctx: Context)
                  (implicit ec: ExecutionContext, metaData: MetaData): Future[ValueWithContext[R]] = {
     val lazyValuesProvider = lazyValuesProviderCreator(ec, metaData, nodeId)
+    //FIXME: this is *not* performant when we have some global variables, we should not add to map here, but e.g. push
+    //lookup logic down to expressions
     val ctxWithGlobals = ctx.withVariables(globalVariablesPreparer.prepareGlobalVariables(metaData).mapValues(_.obj))
 
     expr.evaluate[R](ctxWithGlobals, lazyValuesProvider).map { valueWithLazyContext =>
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -15,3 +15,5 @@ addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.5.1")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
 
 addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.2.7")
+
+addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.3.7")
