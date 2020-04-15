f3897b7fdef85ad7498aa3e660b67352219230fb
Marta Pacuszka <mpz@touk.pl>
2020-04-15 13:27:02 +0200

    Make ObjectNaming serializable

--- engine/api/src/main/scala/pl/touk/nussknacker/engine/api/namespaces/ObjectNaming.scala
+++ engine/api/src/main/scala/pl/touk/nussknacker/engine/api/namespaces/ObjectNaming.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.engine.api.namespaces
 
 import com.typesafe.config.Config
 
-trait ObjectNaming {
+trait ObjectNaming extends Serializable {
   def prepareName(originalName: String, config: Config, namingContext: NamingContext): String
 }
 
