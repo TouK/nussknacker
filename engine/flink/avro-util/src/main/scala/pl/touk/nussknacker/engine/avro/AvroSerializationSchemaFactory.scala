039b3ff5ad5fda8500e007260482e0d2e538ba61
Łukasz Ciołecki <lcl@touk.pl>
2020-04-20 14:50:22 +0200

    Refactor avro serializer and deserializer

--- engine/flink/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/AvroSerializationSchemaFactory.scala
+++ engine/flink/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/AvroSerializationSchemaFactory.scala
@@ -5,35 +5,10 @@ import org.apache.kafka.common.serialization.Serializer
 import pl.touk.nussknacker.engine.kafka.KafkaConfig
 import pl.touk.nussknacker.engine.kafka.serialization.{KafkaKeyValueSerializationSchemaFactoryBase, KafkaSerializationSchemaFactoryBase}
 
-class AvroSerializationSchemaFactory(schemaRegistryClientFactory: SchemaRegistryClientFactory) extends KafkaSerializationSchemaFactoryBase[Any]  {
-
+trait AvroSerializer {
   import collection.JavaConverters._
 
-  override protected def createValueSerializer(topic: String, kafkaConfig: KafkaConfig): Serializer[Any] = {
-    // TODO: this client is never destroyed and it is potential leak of resources
-    val schemaRegistryClient = schemaRegistryClientFactory.createSchemaRegistryClient(kafkaConfig)
-    val serializer = new KafkaAvroSerializer(schemaRegistryClient)
-    val props = kafkaConfig.kafkaProperties.getOrElse(Map.empty)
-    serializer.configure(props.asJava, false)
-    serializer.asInstanceOf[Serializer[Any]]
-  }
-
-}
-
-abstract class AvroKeyValueSerializationSchemaFactory(schemaRegistryClientFactory: SchemaRegistryClientFactory)
-  extends KafkaKeyValueSerializationSchemaFactoryBase[Any] {
-
-  import collection.JavaConverters._
-
-  override protected def createKeySerializer(topic: String, kafkaConfig: KafkaConfig): Serializer[K] = {
-    createSerializer(kafkaConfig, isKey = true).asInstanceOf[Serializer[K]]
-  }
-
-  override protected def createValueSerializer(topic: String, kafkaConfig: KafkaConfig): Serializer[V] = {
-    createSerializer(kafkaConfig, isKey = false).asInstanceOf[Serializer[V]]
-  }
-
-  private def createSerializer(kafkaConfig: KafkaConfig, isKey: Boolean) = {
+  protected def createSerializer(schemaRegistryClientFactory: SchemaRegistryClientFactory, kafkaConfig: KafkaConfig, isKey: Boolean) = {
     // TODO: this client is never destroyed and it is potential leak of resources
     val schemaRegistryClient = schemaRegistryClientFactory.createSchemaRegistryClient(kafkaConfig)
     val serializer = new KafkaAvroSerializer(schemaRegistryClient)
@@ -41,5 +16,21 @@ abstract class AvroKeyValueSerializationSchemaFactory(schemaRegistryClientFactor
     serializer.configure(props.asJava, isKey)
     serializer
   }
+}
 
+class AvroSerializationSchemaFactory(schemaRegistryClientFactory: SchemaRegistryClientFactory)
+  extends KafkaSerializationSchemaFactoryBase[Any] with AvroSerializer {
+
+  override protected def createValueSerializer(topic: String, kafkaConfig: KafkaConfig): Serializer[Any] =
+    createSerializer(schemaRegistryClientFactory, kafkaConfig, isKey = false).asInstanceOf[Serializer[Any]]
+}
+
+abstract class AvroKeyValueSerializationSchemaFactory(schemaRegistryClientFactory: SchemaRegistryClientFactory)
+  extends KafkaKeyValueSerializationSchemaFactoryBase[Any] with AvroSerializer {
+
+  override protected def createKeySerializer(topic: String, kafkaConfig: KafkaConfig): Serializer[K] =
+    createSerializer(schemaRegistryClientFactory, kafkaConfig, isKey = true).asInstanceOf[Serializer[K]]
+
+  override protected def createValueSerializer(topic: String, kafkaConfig: KafkaConfig): Serializer[V] =
+    createSerializer(schemaRegistryClientFactory, kafkaConfig, isKey = false).asInstanceOf[Serializer[V]]
 }
