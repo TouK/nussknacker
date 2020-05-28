99fb786ac4c71e61870dfc7a61e211d1996c3fb8
Marcin Abramowicz <mab@touk.pl>
2020-05-28 13:05:26 +0200

    new json editor

--- ui/client/components/graph/node-modal/editors/expression/ace.ts
+++ ui/client/components/graph/node-modal/editors/expression/ace.ts
@@ -2,6 +2,7 @@ import AceEditor from "react-ace"
 import "ace-builds/src-noconflict/mode-jsx"
 import "ace-builds/src-noconflict/ext-language_tools"
 import "ace-builds/src-noconflict/ext-searchbox"
+import "ace-builds/src-noconflict/mode-json"
 
 import "../../../../../brace/mode/spel"
 import "../../../../../brace/mode/sql"
