caeea001209d5a6acdb53a8b7f75b1fed7dccac1
Marcin Abramowicz <mab@touk.pl>
2020-06-26 11:53:24 +0200

    json validator extractor update

--- engine/interpreter/src/main/scala/pl/touk/nussknacker/engine/definition/validator/JsonValidatorExtractor.scala
+++ engine/interpreter/src/main/scala/pl/touk/nussknacker/engine/definition/validator/JsonValidatorExtractor.scala
@@ -1,13 +1,12 @@
 package pl.touk.nussknacker.engine.definition.validator
 
-import pl.touk.nussknacker.engine.api.definition.{JsonValidator, ParameterValidator}
+import pl.touk.nussknacker.engine.api.definition.{JsonParameterEditor, JsonValidator, ParameterValidator}
 
 object JsonValidatorExtractor extends ValidatorExtractor {
 
   override def extract(params: ValidatorExtractorParameters): Option[ParameterValidator] = {
     params.extractedEditor match {
-      // TODO add json editor case
-      // case _ => Some(JsonValidator)
+      case Some(JsonParameterEditor) => Some(JsonValidator)
       case _ => None
     }
   }
