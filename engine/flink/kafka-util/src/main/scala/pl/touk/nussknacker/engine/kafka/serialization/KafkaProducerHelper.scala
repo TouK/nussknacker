8fa880140f4ced6fb602d5a14bc8974a147b3650
Maciej Próchniak <mpr@touk.pl>
2020-07-06 22:40:35 +0200

    [FIX] handling nulls in scala 2.11 (#1038)
    * [FIX] handling nulls in scala 2.11

--- engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/serialization/KafkaProducerHelper.scala
+++ engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/serialization/KafkaProducerHelper.scala
@@ -1,12 +1,13 @@
 package pl.touk.nussknacker.engine.kafka.serialization
 
 import org.apache.kafka.clients.producer.ProducerRecord
+import java.lang
 
 object KafkaProducerHelper {
 
-  def createRecord(topic: String, key: Array[Byte], value: Array[Byte], timestamp: Long): ProducerRecord[Array[Byte], Array[Byte]] = {
+  def createRecord(topic: String, key: Array[Byte], value: Array[Byte], timestamp: lang.Long): ProducerRecord[Array[Byte], Array[Byte]] = {
     //Kafka timestamp has to be >= 0, while Flink can use Long.MinValue
-    val timestampToSerialize: java.lang.Long = Math.max(0L, timestamp)
+    val timestampToSerialize: lang.Long = Option(timestamp).map(Math.max(0L, _): lang.Long).orNull
     new ProducerRecord[Array[Byte], Array[Byte]](topic, null,
       timestampToSerialize,
       key,
