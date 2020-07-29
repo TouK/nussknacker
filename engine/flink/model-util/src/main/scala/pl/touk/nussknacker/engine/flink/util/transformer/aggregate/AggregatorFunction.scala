47548c8b98040b680206994bab4862d9de8f2722
Arek Burdach <arek.burdach@gmail.com>
2020-07-29 14:24:50 +0200

    LatelyEvictableStateCoFunction and AggregatorFunction split to Mixin for purpose of more advanced usages (like outer join with aggregation)

--- engine/flink/model-util/src/main/scala/pl/touk/nussknacker/engine/flink/util/transformer/aggregate/AggregatorFunction.scala
+++ engine/flink/model-util/src/main/scala/pl/touk/nussknacker/engine/flink/util/transformer/aggregate/AggregatorFunction.scala
@@ -1,10 +1,11 @@
 package pl.touk.nussknacker.engine.flink.util.transformer.aggregate
 
 import org.apache.flink.api.common.state.ValueStateDescriptor
+import org.apache.flink.streaming.api.TimerService
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
 import pl.touk.nussknacker.engine.api.{ValueWithContext, Context => NkContext}
-import pl.touk.nussknacker.engine.flink.api.state.LatelyEvictableStateFunction
+import pl.touk.nussknacker.engine.flink.api.state.{LatelyEvictableStateFunction, StateHolder}
 import pl.touk.nussknacker.engine.flink.util.keyed.StringKeyedValue
 
 import scala.collection.immutable.TreeMap
@@ -15,30 +16,50 @@ import scala.collection.immutable.TreeMap
 // Flinks SlidingWindows are something that is often called HoppingWindow. They consume a lot of memory because each element
 // is stored in multiple windows with different offsets.
 class AggregatorFunction(protected val aggregator: Aggregator, protected val timeWindowLengthMillis: Long)
-  extends LatelyEvictableStateFunction[ValueWithContext[StringKeyedValue[AnyRef]], ValueWithContext[AnyRef], TreeMap[Long, AnyRef]] {
+  extends LatelyEvictableStateFunction[ValueWithContext[StringKeyedValue[AnyRef]], ValueWithContext[AnyRef], TreeMap[Long, AnyRef]]
+  with AggregatorFunctionMixin {
 
   type FlinkCtx = KeyedProcessFunction[String, ValueWithContext[StringKeyedValue[AnyRef]], ValueWithContext[AnyRef]]#Context
 
-  //TODO make it configurable
-  private val minimalResolutionMs = 60000L
-
   override def processElement(value: ValueWithContext[StringKeyedValue[AnyRef]], ctx: FlinkCtx, out: Collector[ValueWithContext[AnyRef]]): Unit = {
+    handleNewElementAdded(value, ctx.timestamp(), ctx.timerService(), out)
+  }
 
-    moveEvictionTime(timeWindowLengthMillis, ctx)
+}
 
-    val newElementInStateTimestamp = computeTimestampToStore(ctx)
-    val newElement = value.value.value.asInstanceOf[aggregator.Element]
-    val newState: TreeMap[Long, aggregator.Aggregate] = computeNewState(newElementInStateTimestamp, newElement, ctx)
+trait AggregatorFunctionMixin { self: StateHolder[TreeMap[Long, AnyRef]] =>
 
-    state.update(newState)
-    handleElementAddedToState(newElementInStateTimestamp, newElement, value.context, ctx, out)
+  //TODO make it configurable
+  protected val minimalResolutionMs = 60000L
 
-    val finalVal: AnyRef = computeFinalValue(newState)
+  protected val aggregator: Aggregator
+
+  protected def timeWindowLengthMillis: Long
+
+
+  protected def handleNewElementAdded(value: ValueWithContext[StringKeyedValue[AnyRef]],
+                                      timestamp: Long, timeService: TimerService,
+                                      out: Collector[ValueWithContext[AnyRef]]): Unit = {
+    val newState = addElementToState(value, timestamp, timeService, out)
+    val finalVal = computeFinalValue(newState)
     out.collect(ValueWithContext(finalVal, value.context))
   }
 
+  protected def addElementToState(value: ValueWithContext[StringKeyedValue[AnyRef]],
+                                  timestamp: Long, timeService: TimerService,
+                                  out: Collector[ValueWithContext[AnyRef]]): TreeMap[Long, aggregator.Aggregate] = {
+    val newElementInStateTimestamp = computeTimestampToStore(timestamp)
+    val newElement = value.value.value.asInstanceOf[aggregator.Element]
+    val newState = computeNewState(newElementInStateTimestamp, newElement)
+
+    updateState(newState, newElementInStateTimestamp + timeWindowLengthMillis, timeService)
+    handleElementAddedToState(newElementInStateTimestamp, newElement, value.context, timeService, out)
+    newState
+  }
+
+  // for extending classes purpose
   protected def handleElementAddedToState(newElementInStateTimestamp: Long, newElement: aggregator.Element, nkCtx: NkContext,
-                                          ctx: FlinkCtx, out: Collector[ValueWithContext[AnyRef]]): Unit = {
+                                          timeService: TimerService, out: Collector[ValueWithContext[AnyRef]]): Unit = {
   }
 
   protected def computeFinalValue(newState: TreeMap[Long, aggregator.Aggregate]): AnyRef = {
@@ -50,8 +71,8 @@ class AggregatorFunction(protected val aggregator: Aggregator, protected val tim
     }
   }
 
-  private def computeNewState(newElementInStateTimestamp: Long, newValue: aggregator.Element, ctx: FlinkCtx): TreeMap[Long, aggregator.Aggregate] = {
-    val current: TreeMap[Long, aggregator.Aggregate] = stateForTimestamp(currentState(), ctx.timestamp())
+  private def computeNewState(newElementInStateTimestamp: Long, newValue: aggregator.Element): TreeMap[Long, aggregator.Aggregate] = {
+    val current: TreeMap[Long, aggregator.Aggregate] = stateForTimestamp(readStateOrInitial(), newElementInStateTimestamp)
 
     val currentAggregate = current.getOrElse(newElementInStateTimestamp, aggregator.createAccumulator())
     val newAggregate = aggregator.add(newValue, currentAggregate).asInstanceOf[aggregator.Aggregate]
@@ -59,17 +80,18 @@ class AggregatorFunction(protected val aggregator: Aggregator, protected val tim
     current.updated(newElementInStateTimestamp, newAggregate)
   }
 
-  private def computeTimestampToStore(ctx: FlinkCtx): Long = {
-    (ctx.timestamp() / minimalResolutionMs) * minimalResolutionMs
+  private def computeTimestampToStore(timestamp: Long): Long = {
+    (timestamp / minimalResolutionMs) * minimalResolutionMs
   }
 
   protected def stateForTimestamp[T](stateValue: TreeMap[Long, T], timestamp: Long): TreeMap[Long, T] = {
     stateValue.from(timestamp - timeWindowLengthMillis + 1) // we must have exclusive range like Flink's sliding/tumbling have
   }
 
-  protected def currentState(): TreeMap[Long, aggregator.Aggregate] =
-    Option(state.value().asInstanceOf[TreeMap[Long, aggregator.Aggregate]]).getOrElse(TreeMap.empty(Ordering.Long))
+  protected def readStateOrInitial(): TreeMap[Long, aggregator.Aggregate] =
+    Option(readState().asInstanceOf[TreeMap[Long, aggregator.Aggregate]]).getOrElse(TreeMap.empty(Ordering.Long))
 
   override protected def stateDescriptor: ValueStateDescriptor[TreeMap[Long, AnyRef]] =
     new ValueStateDescriptor[TreeMap[Long, AnyRef]]("state", classOf[TreeMap[Long, AnyRef]])
+
 }
