6376733b085ce7dae707225bb2be403d9ba1f633
Julian Wielga <j@touk.pl>
2020-08-10 15:14:57 +0200

    disable "deploy" & "metrics" for subprocess

--- ui/client/components/toolbars/status/buttons/MetricsButton.tsx
+++ ui/client/components/toolbars/status/buttons/MetricsButton.tsx
@@ -1,35 +1,24 @@
 import React from "react"
-import {RootState} from "../../../../reducers/index"
-import {connect} from "react-redux"
-import {showMetrics} from "../../../../actions/nk/showMetrics"
-import ToolbarButton from "../../../toolbarComponents/ToolbarButton"
-import {getProcessId} from "../../../../reducers/selectors/graph"
 import {useTranslation} from "react-i18next"
+import {useDispatch, useSelector} from "react-redux"
+import {showMetrics} from "../../../../actions/nk/showMetrics"
 import {ReactComponent as Icon} from "../../../../assets/img/toolbarButtons/migrate.svg"
+import {getProcessId, isSubprocess} from "../../../../reducers/selectors/graph"
+import ToolbarButton from "../../../toolbarComponents/ToolbarButton"
 
-type Props = StateProps
+export default function MetricsButton() {
+  const dispatch = useDispatch()
+  const processId = useSelector(getProcessId)
+  const subprocess = useSelector(isSubprocess)
 
-function MetricsButton(props: Props) {
-  const {showMetrics, processId} = props
   const {t} = useTranslation()
 
   return (
     <ToolbarButton
       name={t("panels.actions.deploy-metrics.button", "metrics")}
-      onClick={() => showMetrics(processId)}
+      onClick={() => dispatch(showMetrics(processId))}
+      disabled={subprocess}
       icon={<Icon/>}
     />
   )
 }
-
-const mapState = (state: RootState) => ({
-  processId: getProcessId(state),
-})
-
-const mapDispatch = {
-  showMetrics,
-}
-
-type StateProps = typeof mapDispatch & ReturnType<typeof mapState>
-
-export default connect(mapState, mapDispatch)(MetricsButton)
