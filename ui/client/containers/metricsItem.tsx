37acf60a9e4519361da6992c55914e7c847ec90f
JulianWielga <j@touk.pl>
2020-08-12 11:54:10 +0200

    link to process instead of onClick (#1141)

--- ui/client/containers/metricsItem.tsx
+++ ui/client/containers/metricsItem.tsx
@@ -1,15 +1,17 @@
+import {useTranslation} from "react-i18next"
 import TableRowIcon from "../components/table/TableRowIcon"
 import {showMetrics} from "../actions/nk"
 import {useDispatch} from "react-redux"
 import React, {useCallback} from "react"
 
 export function MetricsItem({process}: { process: $TodoType }) {
+  const {t} = useTranslation()
   const dispatch = useDispatch()
   const clickHandler = useCallback(
     () => dispatch(showMetrics(process.name)),
     [process],
   )
   return (
-    <TableRowIcon glyph={"stats"} title="Show metrics" onClick={clickHandler}/>
+    <TableRowIcon glyph={"stats"} title={t("tableRowIcon-show-metrics","Show metrics")} onClick={clickHandler}/>
   )
 }
