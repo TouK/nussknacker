a62edb4d66e8dd6bdfe6a0705c5cd1cf6af11241
Krzysztof Piesiewicz <kps@touk.pl>
2020-09-10 17:56:06 +0200

    extracting PEM from textual representation with regex

--- engine/security/src/main/scala/pl/touk/nussknacker/ui/security/CertificatesAndKeys.scala
+++ engine/security/src/main/scala/pl/touk/nussknacker/ui/security/CertificatesAndKeys.scala
@@ -1,7 +1,7 @@
 package pl.touk.nussknacker.ui.security
 
 import java.io.ByteArrayInputStream
-import java.nio.charset.{Charset, StandardCharsets}
+import java.nio.charset.Charset
 import java.security.cert.CertificateFactory
 import java.security.spec.X509EncodedKeySpec
 import java.security.{KeyFactory, PublicKey}
@@ -9,22 +9,21 @@ import java.util.Base64
 
 object CertificatesAndKeys {
   def publicKeyFromString(keyStr: String, charset: Charset): PublicKey = {
-    val publicKey64 = keyStr
-      .replace("-----BEGIN PUBLIC KEY-----", "")
-      .replaceAll(System.lineSeparator, "")
-      .replace("-----END PUBLIC KEY-----", "")
-
-    val keySpec = new X509EncodedKeySpec(Base64.getDecoder.decode(publicKey64.getBytes(charset)))
+    val keySpec = new X509EncodedKeySpec(decodeBase64(extractBase64EncodedPEM(keyStr), charset))
     KeyFactory.getInstance("RSA").generatePublic(keySpec)
   }
 
   def publicKeyFromStringCertificate(certStr: String, charset: Charset): PublicKey = {
-    val cert64 = certStr
-      .replace("-----BEGIN CERTIFICATE-----", "")
-      .replaceAll(System.lineSeparator, "")
-      .replace("-----END CERTIFICATE-----", "")
-
-    val certStream = new ByteArrayInputStream(Base64.getDecoder.decode(cert64.getBytes(charset)))
+    val certStream = new ByteArrayInputStream(decodeBase64(extractBase64EncodedPEM(certStr), charset))
     CertificateFactory.getInstance("X.509").generateCertificate(certStream).getPublicKey
   }
+
+  def extractBase64EncodedPEM(pem: String): String = {
+    // referring to https://tools.ietf.org/html/rfc7468#section-5.1
+    val beginningPattern = "^-----BEGIN.*-----".r
+    val endingPattern = "-----END(.)*$".r
+    beginningPattern.replaceFirstIn(endingPattern.replaceFirstIn(pem.replaceAll(System.lineSeparator, ""), ""), "")
+  }
+
+  def decodeBase64(decoded: String, charset: Charset): Array[Byte] = Base64.getDecoder.decode(decoded.getBytes(charset))
 }
