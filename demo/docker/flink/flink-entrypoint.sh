c42d68b5101fba08467b539eaec1c3fb0d6c3d33
Maciek Próchniak <mpr@touk.pl>
2020-09-15 15:02:43 +0200

    Flink config

--- demo/docker/flink/flink-entrypoint.sh
+++ demo/docker/flink/flink-entrypoint.sh
@@ -4,7 +4,6 @@ DATA_DIR=${FLINK_DATA-"$FLINK_HOME/data"}
 GROUP=${DAEMON_GROUP-"flink"}
 USER=${DAEMON_USER-"flink"}
 
-cp ${FLINK_HOME}/opt/flink-metrics-influxdb-${FLINK_VERSION}.jar ${FLINK_HOME}/lib/flink-metrics-influxdb-${FLINK_VERSION}.jar
 cp ${FLINK_HOME}/opt/flink-queryable-state-runtime_${SCALA_VERSION}-${FLINK_VERSION}.jar ${FLINK_HOME}/lib/flink-queryable-state-runtime_${SCALA_VERSION}-${FLINK_VERSION}.jar
 
 mkdir -p "$DATA_DIR/checkpoints" "$DATA_DIR/savepoints" "$DATA_DIR/storage" "$DATA_DIR/logs"
