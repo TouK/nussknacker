327858034b246c97523536b4c0d515b9f0415f25
Maciek Próchniak <mpr@touk.pl>
2020-09-17 12:12:49 +0200

    fix

--- ui/buildServer.sh
+++ ui/buildServer.sh
@@ -4,5 +4,5 @@ set -e
 
 cd ..
 #assemblySamples is needed to use models, assemblyEngines - to access process managers, ui/assembly - to be able to use FE
-./sbtwrapper ;assemblySamples;assemblyEngines;ui/assembly
+./sbtwrapper ";assemblySamples;assemblyEngines;ui/assembly"
 cd -
