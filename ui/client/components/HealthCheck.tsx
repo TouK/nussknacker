a08f4a7a313aa8cecfc11a22c02414d55faabe6a
Julian Wielga <j@touk.pl>
2020-09-21 15:39:31 +0200

    cleanup

--- ui/client/components/HealthCheck.tsx
+++ ui/client/components/HealthCheck.tsx
@@ -1,63 +1,33 @@
-import React from "react"
+import React, {useCallback, useState} from "react"
 import {hot} from "react-hot-loader"
-import {withTranslation} from "react-i18next"
-import {WithTranslation} from "react-i18next/src"
-import {connect} from "react-redux"
-import {compose} from "redux"
-import {EspActionsProps, mapDispatchWithEspActions} from "../actions/ActionsUtils"
+import {useTranslation} from "react-i18next"
+import {useSelector} from "react-redux"
 import InlinedSvgs from "../assets/icons/InlinedSvgs"
+import {useInterval} from "../containers/Interval"
 import HttpService, {HealthCheckResponse} from "../http/HttpService"
-import {getFeatureSettings} from "../reducers/selectors/settings"
+import {getHealthcheckIntervalTime} from "../reducers/selectors/settings"
 import styles from "./healthCheck.styl"
 
-type State = {
-  healthCheck?: HealthCheckResponse,
+const STATE_OK = "ok"
+
+function HealthCheck(): JSX.Element {
+  const {t} = useTranslation()
+  const [{error, state}, setState] = useState<HealthCheckResponse>({state: STATE_OK})
+
+  const updateState = useCallback(async () => {
+    setState(await HttpService.fetchHealthCheckProcessDeployment())
+  }, [])
+
+  const refreshTime = useSelector(getHealthcheckIntervalTime)
+  useInterval(updateState, refreshTime)
+
+  if (state === STATE_OK) {return null}
+  return (
+    <div className={styles.healthCheck}>
+      <div className={styles.icon} title="Warning" dangerouslySetInnerHTML={{__html: InlinedSvgs.tipsWarning}}/>
+      <span className={styles.errorText}>{error || t("healthCheck.unknownState", "State unknown")}</span>
+    </div>
+  )
 }
 
-const stateOk = "ok"
-
-class HealthCheck extends React.Component<Props, State> {
-  private intervalId
-  state = {healthCheck: null}
-
-  componentDidMount() {
-    const {healthCheckInterval = 4000} = this.props
-    this.intervalId = setInterval(this.updateState, healthCheckInterval)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId)
-  }
-
-  private async updateState() {
-    const check = await HttpService.fetchHealthCheckProcessDeployment()
-    this.setState({healthCheck: check})
-  }
-
-  render() {
-    const {t} = this.props
-    const {healthCheck} = this.state
-
-    return !healthCheck || healthCheck.state === stateOk ?
-      null :
-      (
-        <div className={styles.healthCheck}>
-          <div className={styles.icon} title="Warning" dangerouslySetInnerHTML={{__html: InlinedSvgs.tipsWarning}}/>
-          <span className={styles.errorText}>{healthCheck.error || t("healthCheck.unknownState", "State unknown")}</span>
-        </div>
-      )
-  }
-}
-
-const mapState = state => ({
-  healthCheckInterval: getFeatureSettings(state)?.intervalSettings?.healthCheck,
-})
-
-type Props = ReturnType<typeof mapState> & WithTranslation
-
-const enhance = compose(
-  connect(mapState),
-  withTranslation(),
-)
-
-export default hot(module)(enhance(HealthCheck))
+export default hot(module)(HealthCheck)
