9534dd7ddf103bbfd673261ec1cf30a926bdbfc3
Julian Wielga <j@touk.pl>
2020-09-23 17:15:27 +0200

    fixed dynamic rows calc

---
+++ ui/client/containers/processesTable/FillCheck.tsx
@@ -0,0 +1,41 @@
+import {css} from "emotion"
+import React, {PropsWithChildren, useLayoutEffect, useRef} from "react"
+import {useDebouncedCallback} from "use-debounce"
+import {useSize} from "../hooks/useSize"
+
+type Props = {
+  onChange: (freeSpace: number, currentHeight: number) => void,
+}
+
+export function FillCheck({children, onChange}: PropsWithChildren<Props>): JSX.Element {
+  const wrapperRef = useRef()
+  const childRef = useRef()
+  const wrapper = useSize({ref: wrapperRef})
+  const child = useSize({ref: childRef})
+
+  const availableHeight = wrapper.height
+  const currentHeight = child.height
+
+  const [callback] = useDebouncedCallback((freeSpace, currentHeight) => onChange(freeSpace, currentHeight), 25)
+
+  useLayoutEffect(() => {
+    if (availableHeight && currentHeight) {
+      const freeSpace = availableHeight - currentHeight
+      callback(freeSpace, currentHeight)
+    }
+  }, [callback, availableHeight, currentHeight])
+
+  const wrapperStyles = css({
+    overflowY: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "stretch",
+    flex: 1,
+  })
+
+  return (
+    <div ref={wrapperRef} className={wrapperStyles}>
+      <div ref={childRef}>{children}</div>
+    </div>
+  )
+}
