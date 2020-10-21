5761597c2059e6bd2921a71924cd01f5b1623e46
JulianWielga <j@touk.pl>
2020-10-21 08:15:30 +0200

    ff68 polyfill (#1274)

--- ui/client/containers/hooks/useSize.tsx
+++ ui/client/containers/hooks/useSize.tsx
@@ -3,10 +3,8 @@ import useDimensions from "react-cool-dimensions"
 (async () => {
   if (!("ResizeObserver" in window)) {
     const module = await import("@juggle/resize-observer")
-    // eslint-disable-next-line i18next/no-literal-string
     window["ResizeObserver"] = module.ResizeObserver
-    // Only use it when you have this trouble: https://github.com/wellyshen/react-cool-dimensions/issues/45
-    // window.ResizeObserverEntry = module.ResizeObserverEntry;
+    window["ResizeObserverEntry"] = module.ResizeObserverEntry // Only use it when you have this trouble: https://github.com/wellyshen/react-cool-dimensions/issues/45
   }
 })()
 
