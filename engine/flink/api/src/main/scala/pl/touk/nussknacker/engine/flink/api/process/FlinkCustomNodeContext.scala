d318ea9955dfef08a545121ad64a6b5c5712d0de
Łukasz Jędrzejewski <ljd@touk.pl>
2020-10-23 08:15:14 +0200

    Access job data from custom nodes
    Job data is already available from services via RuntimeInjectedJobData trait.

--- engine/flink/api/src/main/scala/pl/touk/nussknacker/engine/flink/api/process/FlinkCustomNodeContext.scala
+++ engine/flink/api/src/main/scala/pl/touk/nussknacker/engine/flink/api/process/FlinkCustomNodeContext.scala
@@ -1,19 +1,21 @@
 package pl.touk.nussknacker.engine.flink.api.process
 
-import pl.touk.nussknacker.engine.api.MetaData
+import pl.touk.nussknacker.engine.api.{JobData, MetaData}
 import pl.touk.nussknacker.engine.flink.api.NkGlobalParameters
 import pl.touk.nussknacker.engine.flink.api.signal.FlinkProcessSignalSender
 
 import scala.concurrent.duration.FiniteDuration
 import scala.reflect.ClassTag
 
-case class FlinkCustomNodeContext(metaData: MetaData,
+case class FlinkCustomNodeContext(jobData: JobData,
                                   // TODO: it can be used in state recovery - make sure that it won't change during renaming of nodes on gui
                                   nodeId: String,
                                   timeout: FiniteDuration,
                                   lazyParameterHelper: FlinkLazyParameterFunctionHelper,
                                   signalSenderProvider: FlinkProcessSignalSenderProvider,
-                                  globalParameters: Option[NkGlobalParameters])
+                                  globalParameters: Option[NkGlobalParameters]) {
+  def metaData: MetaData = jobData.metaData
+}
 
 case class SignalSenderKey(id: String, klass: Class[_])
 
@@ -26,5 +28,3 @@ class FlinkProcessSignalSenderProvider(signalSenders: Map[SignalSenderKey, Flink
       .getOrElse(throw new IllegalArgumentException(s"Unknown signal class: ${clazz.getName}, please check ProcessConfigCreator"))._2
   }
 }
-
-case class FlinkProcessConfig()
