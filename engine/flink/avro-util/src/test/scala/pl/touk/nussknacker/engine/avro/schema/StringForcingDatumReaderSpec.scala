95628be6012aeef5a4d0919236f8bb13aa2888b4
Krzysztof Gadomski <kgd@touk.pl>
2020-12-21 16:07:14 +0100

    test for default value

--- engine/flink/avro-util/src/test/scala/pl/touk/nussknacker/engine/avro/schema/StringForcingDatumReaderSpec.scala
+++ engine/flink/avro-util/src/test/scala/pl/touk/nussknacker/engine/avro/schema/StringForcingDatumReaderSpec.scala
@@ -1,28 +1,49 @@
 package pl.touk.nussknacker.engine.avro.schema
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
-import org.apache.avro.generic.{GenericDatumWriter, GenericRecord}
+import org.apache.avro.generic.{GenericData, GenericDatumWriter, GenericRecord}
 import org.apache.avro.io.{DecoderFactory, EncoderFactory}
 import org.apache.avro.Schema
+import org.apache.avro.util.Utf8
+import org.apache.flink.formats.avro.typeutils.LogicalTypesGenericRecordBuilder
 import org.scalatest.{EitherValues, FunSpec, Matchers}
 import pl.touk.nussknacker.engine.avro.{AvroStringSettingsInTests, AvroUtils}
-import pl.touk.nussknacker.engine.avro.encode.{BestEffortAvroEncoder, ValidationMode}
 
 
 class StringForcingDatumReaderSpec extends FunSpec with Matchers with EitherValues {
 
-  import collection.JavaConverters._
-
-  final protected val avroEncoder = BestEffortAvroEncoder(ValidationMode.strict)
-
-  it("should create simple record") {
+  it("should encode & decode") {
     val schema = wrapWithRecordSchema(
       """[
         |  { "name": "foo", "type": "string" }
         |]""".stripMargin)
+    val builder = new LogicalTypesGenericRecordBuilder(schema)
+    builder.set("foo", "bar")
+    val givenRecord = builder.build()
 
-    roundTripVerifyWhenStringTypeIsForced(avroEncoder.encodeRecord(Map("foo" -> "bar").asJava, schema).toEither.right.value)
-    roundTripVerifyWhenStringTypeIsForced(avroEncoder.encodeRecord(Map("foo" -> Some("bar")), schema).toEither.right.value)
+    val readRecord = roundTripWriteRead(givenRecord)
+    readRecord.get("foo") shouldBe a[Utf8]
+
+    val readWhenStringForced = AvroStringSettingsInTests.whenEnabled {
+      roundTripWriteRead(readRecord)
+    }
+    readWhenStringForced.get("foo") shouldBe a[String]
+    readWhenStringForced shouldEqual givenRecord
+  }
+
+  it("should use correct type in provided default value") {
+    val schema = wrapWithRecordSchema(
+      """[
+        |  { "name": "foo", "type": "string", "default": "bar" }
+        |]""".stripMargin)
+
+    val record1 = AvroStringSettingsInTests.whenEnabled {
+      new LogicalTypesGenericRecordBuilder(schema).build()
+    }
+    record1.get("foo") shouldBe a[String]
+
+    val record2 = new LogicalTypesGenericRecordBuilder(schema).build()
+    record2.get("foo") shouldBe a[Utf8]
   }
 
   private def wrapWithRecordSchema(fieldsDefinition: String) =
@@ -33,21 +54,6 @@ class StringForcingDatumReaderSpec extends FunSpec with Matchers with EitherValu
          |  "fields": $fieldsDefinition
          |}""".stripMargin)
 
-  private def roundTripVerifyWhenStringTypeIsForced(givenRecord: GenericRecord): GenericRecord = {
-    val read = roundTripVerifyWriteRead(givenRecord)
-    val readWhenStringForced = AvroStringSettingsInTests.whenEnabled {
-      roundTripVerifyWriteRead(read)
-    }
-    readWhenStringForced shouldEqual givenRecord
-    readWhenStringForced
-  }
-
-  private def roundTripVerifyWriteRead(givenRecord: GenericRecord): GenericRecord = {
-    val readRecord = roundTripWriteRead(givenRecord)
-    readRecord shouldEqual givenRecord
-    readRecord
-  }
-
   private def roundTripWriteRead(givenRecord: GenericRecord): GenericRecord = {
     val bos = new ByteArrayOutputStream()
     val encoder = EncoderFactory.get().binaryEncoder(bos, null)
@@ -57,6 +63,7 @@ class StringForcingDatumReaderSpec extends FunSpec with Matchers with EitherValu
     val decoder = DecoderFactory.get().binaryDecoder(new ByteArrayInputStream(bos.toByteArray), null)
     val readRecord = StringForcingDatumReaderProvider.genericDatumReader[GenericRecord](schema, schema, AvroUtils.genericData)
       .read(null, decoder)
+    readRecord shouldBe givenRecord
     readRecord
   }
 
