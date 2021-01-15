11cf581ad72018f9661f9bfca591f02b0d7be826
Łukasz Jędrzejewski <ljd@touk.pl>
2021-01-15 12:20:10 +0100

    typo

--- engine/interpreter/src/main/scala/pl/touk/nussknacker/engine/ModelConfigToLoad.scala
+++ engine/interpreter/src/main/scala/pl/touk/nussknacker/engine/ModelConfigToLoad.scala
@@ -4,9 +4,10 @@ import com.typesafe.config.Config
 import pl.touk.nussknacker.engine.api.ModelConfigLoader
 
 /**
-  * Used by a running process (e.g. on Flink) to recreate full process config passed to ProcessConfigCreator
-  * in execution. This class is serialized in Flink graph. We separate it from the rest of config to keep it small
-  * (see e.g. FlinkProcessCompiler). as it's only part that has to be passed to Flink.
+  * Used by a running process on Flink to recreate full process config passed to ProcessConfigCreator
+  * using only config passed during process deployment. This class with this config is serialized in Flink graph
+  * so we separate it from the rest of configs to keep it small (see e.g. FlinkProcessCompiler), as it's only part
+  * that has to be passed to Flink.
   */
 case class ModelConfigToLoad(inputConfig: Config, modelConfigLoader: ModelConfigLoader) {
 
