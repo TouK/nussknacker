7b51a8f97b9e2d20c83793271dd28e52bc266460
Maciek Próchniak <mpr@touk.pl>
2021-01-20 12:46:13 +0100

    Fix circe problems in scala 2.11

--- engine/flink/management/sample/src/main/scala/pl/touk/nussknacker/engine/management/sample/signal/Signals.scala
+++ engine/flink/management/sample/src/main/scala/pl/touk/nussknacker/engine/management/sample/signal/Signals.scala
@@ -10,12 +10,14 @@ import pl.touk.nussknacker.engine.flink.util.source.EspDeserializationSchema
 object Signals {
   @JsonCodec case class SampleProcessSignal(processId: String, timestamp: Long, action: SignalAction)
 
-  @ConfiguredJsonCodec sealed trait SignalAction {
-    def key: String
-  }
-
+  //Note: due to some problems with circe (https://circe.github.io/circe/codecs/known-issues.html#knowndirectsubclasses-error)
+  // and scala 2.11 this definition should be *before* SignalAction. This problem occurs during doc generation...
   case class RemoveLock(lockId: String) extends SignalAction {
     override def key: String = lockId
+  }
+
+  @ConfiguredJsonCodec sealed trait SignalAction {
+    def key: String
   }
 
 }
