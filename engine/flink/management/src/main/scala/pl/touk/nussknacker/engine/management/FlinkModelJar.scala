894e0252343b74bf9b52ba8d1644e2c34e79865b
Maciek Próchniak <mpr@touk.pl>
2021-01-25 16:55:27 +0100

    [Workaround] Temporary workaround for JobManager leak

--- engine/flink/management/src/main/scala/pl/touk/nussknacker/engine/management/FlinkModelJar.scala
+++ engine/flink/management/src/main/scala/pl/touk/nussknacker/engine/management/FlinkModelJar.scala
@@ -3,12 +3,13 @@ package pl.touk.nussknacker.engine.management
 import java.io.{File, FileOutputStream}
 import java.net.URL
 import java.nio.file.Files
-import java.util.jar.{JarEntry, JarOutputStream}
+import java.util.jar.{JarEntry, JarInputStream, JarOutputStream}
 
-import org.apache.commons.io.IOUtils
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.commons.io.{FileUtils, IOUtils}
 import pl.touk.nussknacker.engine.ModelData
 
-class FlinkModelJar {
+class FlinkModelJar extends LazyLogging {
 
   //TODO: handle multiple models?
   private var modelFile: Option[File] = None
@@ -28,13 +29,26 @@ class FlinkModelJar {
     val tempFile = Files.createTempFile("tempModelJar", ".jar").toFile
     tempFile.deleteOnExit()
 
+    //It seems that Flink leaks file descriptors (ZipEntries) when using file upload + jars embedded in lib folder...
+    modelData.modelClassLoader.urls match {
+      case single :: Nil if single.getPath.endsWith(".jar") =>
+        logger.info("Single jar file detected, using directly to upload to Flink")
+        FileUtils.copyInputStreamToFile(single.openStream(), tempFile)
+      case other =>
+        logger.warn("Multiple URL detected in classpath, embedding in lib folder, this can lead to memory leaks...")
+        copyEntriesToLib(tempFile, other)
+    }
+    tempFile
+  }
+
+  private def copyEntriesToLib(tempFile: File, other: List[URL]): Unit = {
     val output = new FileOutputStream(tempFile)
     val jarOutput = new JarOutputStream(output)
-
-    modelData.modelClassLoader.urls.foreach(putToJar(jarOutput))
-    jarOutput.close()
-
-    tempFile
+    try {
+      other.foreach(putToJar(jarOutput))
+    } finally {
+      jarOutput.close()
+    }
   }
 
   private def putToJar(jarOutputStream: JarOutputStream)(url: URL) = {
