baba520d09e4a11b6c94cc49b95146a3d227d605
Juliusz Nadbereżny <nadberezny@gmail.com>
2021-02-09 05:49:39 +0100

    cr1: sink value constructor better error

--- engine/flink/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/sink/AvroSinkValue.scala
+++ engine/flink/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/sink/AvroSinkValue.scala
@@ -3,18 +3,27 @@ package pl.touk.nussknacker.engine.avro.sink
 import pl.touk.nussknacker.engine.api.LazyParameter
 
 object AvroSinkValue {
+  case class InvalidSinkValue(parameterName: String)
+    extends Exception(s"Parameter: $parameterName must be a LazyParameter[AnyRef] instance.")
 
   def applyUnsafe(sinkParameter: AvroSinkValueParameter, parameterValues: Map[String, Any]): AvroSinkValue =
     sinkParameter match {
       case AvroSinkPrimitiveValueParameter(param) =>
-        val value = parameterValues(param.name).asInstanceOf[LazyParameter[AnyRef]]
-        AvroSinkPrimitiveValue(value)
+        val value = parameterValues(param.name)
+        AvroSinkPrimitiveValue(toLazyParameter(value, param.name))
 
       case AvroSinkRecordParameter(paramFields) =>
         val fields = paramFields.map { case (fieldName, sinkParam) =>
           (fieldName, applyUnsafe(sinkParam, parameterValues))
         }
         AvroSinkRecordValue(fields)
+    }
+
+  private def toLazyParameter(a: Any, paramName: String): LazyParameter[AnyRef] =
+    try {
+      a.asInstanceOf[LazyParameter[AnyRef]]
+    } catch {
+      case _: ClassCastException => throw InvalidSinkValue(paramName)
     }
 }
 
