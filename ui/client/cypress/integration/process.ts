34fc22d603809f643c0857e8af0c1cc5f13b179d
Julian Wielga <j@touk.pl>
2021-02-12 02:10:03 +0100

    test

--- ui/client/cypress/integration/process.ts
+++ ui/client/cypress/integration/process.ts
@@ -61,5 +61,15 @@ describe("Process", () => {
       cy.get("@button").click()
       cy.get("[data-testid=modal]").should("be.visible").toMatchImageSnapshot()
     })
+
+    it("should have \"latest deploy\" button", () => {
+      cy.contains(/^deploy$/i).click()
+      cy.intercept("POST", "/api/processManagement/deploy/*").as("deploy")
+      cy.contains(/^ok$/i).should("be.enabled").click()
+      cy.wait("@deploy").its("response.statusCode").should("eq", 200)
+      cy.contains(/process is running/i).should("exist")
+      cy.contains(/^counts$/i).click()
+      cy.get("[data-testid=modal]").should("be.visible").toMatchImageSnapshot()
+    })
   })
 })
