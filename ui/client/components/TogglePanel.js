8d71a638a6afb61b5adb7f3f826502647fd404e3
JulianWielga <j@touk.pl>
2021-02-15 09:45:09 +0100

    React fast refresh (#1417)
    * enabled react fast refresh
    
    * fixed fetch status error handling
    
    * updated previous tests
    
    * sorted dependencies

--- ui/client/components/TogglePanel.js
+++ ui/client/components/TogglePanel.js
@@ -1,24 +1,26 @@
 import cn from "classnames"
 import PropTypes from "prop-types"
 import React from "react"
+import {useTranslation} from "react-i18next"
 import "../stylesheets/togglePanel.styl"
 
 import SvgDiv from "./SvgDiv"
 
-export default class TogglePanel extends React.Component {
+export default function TogglePanel(props) {
+  const {t} = useTranslation()
+  const {isOpened, onToggle, type} = props
+  const left = type === "LEFT" ? isOpened : !isOpened
+  const iconFile = `arrows/arrow-${left ? "left" : "right"}.svg`
+  const title = type === "LEFT" ?
+    t("panel.toggle.left", "toggle left panel") :
+    t("panel.toggle.right", "toggle right panel")
+  return (
+    <SvgDiv title={title} className={cn("togglePanel", type, {"is-opened": isOpened})} onClick={onToggle} svgFile={iconFile}/>
+  )
+}
 
-  static propTypes = {
-    type: PropTypes.oneOf(["RIGHT", "LEFT"]).isRequired,
-    isOpened: PropTypes.bool.isRequired,
-    onToggle: PropTypes.func.isRequired,
-  }
-
-  render() {
-    const {isOpened, onToggle, type} = this.props
-    const left = type === "LEFT" ?  isOpened : !isOpened
-    const iconFile = `arrows/arrow-${left ? "left" : "right"}.svg`
-    return (
-      <SvgDiv className={cn("togglePanel", type, {"is-opened": isOpened})} onClick={onToggle} svgFile={iconFile}/>
-    )
-  }
+TogglePanel.propTypes = {
+  type: PropTypes.oneOf(["RIGHT", "LEFT"]).isRequired,
+  isOpened: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
 }
