0780494bb79c978b79f798b6961db6e72edd25f2
Krzysztof Gadomski <kgd@touk.pl>
2021-03-19 13:33:23 +0100

    [FIX] DelegatingFlinkEspExceptionHandler: call open(jobData) on delagate

--- engine/flink/api/src/main/scala/pl/touk/nussknacker/engine/flink/api/exception/FlinkEspExceptionHandler.scala
+++ engine/flink/api/src/main/scala/pl/touk/nussknacker/engine/flink/api/exception/FlinkEspExceptionHandler.scala
@@ -2,6 +2,7 @@ package pl.touk.nussknacker.engine.flink.api.exception
 
 import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.api.common.restartstrategy.RestartStrategies.RestartStrategyConfiguration
+import pl.touk.nussknacker.engine.api.JobData
 import pl.touk.nussknacker.engine.api.exception.EspExceptionHandler
 import pl.touk.nussknacker.engine.flink.api.RuntimeContextLifecycle
 
@@ -17,6 +18,8 @@ trait FlinkEspExceptionHandler extends EspExceptionHandler with RuntimeContextLi
 abstract class DelegatingFlinkEspExceptionHandler(protected val delegate: FlinkEspExceptionHandler) extends FlinkEspExceptionHandler {
 
   override def open(runtimeContext: RuntimeContext): Unit = delegate.open(runtimeContext)
+
+  override def open(jobData: JobData): Unit = delegate.open(jobData)
 
   override def close(): Unit = delegate.close()
 
