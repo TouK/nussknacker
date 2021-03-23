fdc97e878d175565b9a965d0c6a2df3929a56d3e
Maciek Próchniak <mpr@touk.pl>
2021-03-23 07:18:23 +0100

    chars range

--- ui/client/common/regexpCompat.ts
+++ ui/client/common/regexpCompat.ts
@@ -1,4 +1,5 @@
 //Older browsers (pre ES2018) do not support 's' (dotAll) flag.
 //In regex literals this is handled by https://babeljs.io/docs/en/babel-plugin-transform-dotall-regex,
 //but if we construct RegExp object by hand (e.g. by concatenating) we have to use replacement
-export const dotAllReplacement = "[\\s\\S]"
+//NOTE: it does *not* handle unicode >2 bytes correctly (with u flag)
+export const dotAllReplacement = "[\\0-\uFFFF]"
