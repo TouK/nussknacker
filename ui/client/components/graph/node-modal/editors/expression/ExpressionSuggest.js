c439c68533b1f075dd08b28744f12beb33af5e5a
Julian Wielga <j@touk.pl>
2021-03-26 17:59:53 +0100

    fixed bad tokens making suggestions not working

--- ui/client/components/graph/node-modal/editors/expression/ExpressionSuggest.js
+++ ui/client/components/graph/node-modal/editors/expression/ExpressionSuggest.js
@@ -27,10 +27,10 @@ const identifierRegexpsIncludingDot = [/[#a-zA-Z0-9-_.]/]
 function isSqlTokenAllowed(iterator, modeId) {
   if (modeId === "ace/mode/sql") {
     let token = iterator.getCurrentToken()
-    while (token && (token.type !== "spel.open" && token.type !== "spel.close")) {
+    while (token && (token.type !== "spel.start" && token.type !== "spel.end")) {
       token = iterator.stepBackward()
     }
-    return token?.type === "spel.open"
+    return token?.type === "spel.start"
   }
   return true
 }
