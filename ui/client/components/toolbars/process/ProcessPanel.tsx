6710e05bbc91bd8372bde87303db80e845e85b30
Julian Wielga <j@touk.pl>
2021-04-13 13:52:37 +0200

    cleanup buttons props (process)

--- ui/client/components/toolbars/process/ProcessPanel.tsx
+++ ui/client/components/toolbars/process/ProcessPanel.tsx
@@ -1,7 +1,4 @@
-import React, {memo} from "react"
-import {RootState} from "../../../reducers/index"
-import {connect} from "react-redux"
-import {isEmpty} from "lodash"
+import React from "react"
 import {CollapsibleToolbar} from "../../toolbarComponents/CollapsibleToolbar"
 import {ArchiveToggleButton} from "./buttons/ArchiveToggleButton"
 import PDFButton from "./buttons/PDFButton"
@@ -9,23 +6,17 @@ import JSONButton from "./buttons/JSONButton"
 import ImportButton from "./buttons/ImportButton"
 import CompareButton from "./buttons/CompareButton"
 import MigrateButton from "./buttons/MigrateButton"
-import {getFeatureSettings} from "../../../reducers/selectors/settings"
 import {useTranslation} from "react-i18next"
-import {getCapabilities} from "../../../reducers/selectors/other"
 import Properties from "../status/buttons/PropertiesButton"
-import {isSubprocess} from "../../../reducers/selectors/graph"
 import {ToolbarButtons} from "../../toolbarComponents/ToolbarButtons"
 
-type Props = StateProps
-
-function ProcessPanel(props: Props) {
-  const {capabilities, featuresSettings, isSubprocess} = props
+function ProcessPanel(): JSX.Element {
   const {t} = useTranslation()
 
   return (
     <CollapsibleToolbar id="PROCESS-PANEL" title={t("panels.process.title", "Process")}>
       <ToolbarButtons>
-        {!isSubprocess ? <Properties/> : null}
+        <Properties/>
         <CompareButton/>
         <MigrateButton/>
         <ImportButton/>
@@ -37,12 +28,4 @@ function ProcessPanel(props: Props) {
   )
 }
 
-const mapState = (state: RootState) => ({
-  featuresSettings: getFeatureSettings(state),
-  capabilities: getCapabilities(state),
-  isSubprocess: isSubprocess(state),
-})
-
-type StateProps = ReturnType<typeof mapState>
-
-export default connect(mapState)(memo(ProcessPanel))
+export default ProcessPanel
