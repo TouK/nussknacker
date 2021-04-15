4fb8f64d17f573b08a2e4adebcb5f86ba79c42d2
Juliusz Nadbereżny <nadberezny@gmail.com>
2021-04-15 21:22:37 +0200

    TypedObjectTypingResult orderd fields

--- engine/api/src/main/scala/pl/touk/nussknacker/engine/api/typed/TypedObjectDefinition.scala
+++ engine/api/src/main/scala/pl/touk/nussknacker/engine/api/typed/TypedObjectDefinition.scala
@@ -2,4 +2,12 @@ package pl.touk.nussknacker.engine.api.typed
 
 import pl.touk.nussknacker.engine.api.typed.typing.TypingResult
 
-case class TypedObjectDefinition(fields: Map[String, TypingResult])
+import scala.collection.immutable.ListMap
+
+object TypedObjectDefinition {
+
+  def apply(fields: List[(String, TypingResult)]): TypedObjectDefinition =
+    TypedObjectDefinition(ListMap(fields: _*))
+}
+
+case class TypedObjectDefinition(fields: ListMap[String, TypingResult])
