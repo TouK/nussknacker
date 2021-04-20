e785850c92544f58a9c7c10624046da73cc443bb
Arek Burdach <arek.burdach@gmail.com>
2021-04-20 17:26:11 +0200

    Default InfluxDb reporter for standalone-app metrics

---
+++ engine/standalone/util/src/main/scala/pl/touk/nussknacker/engine/standalone/utils/metrics/dropwizard/influxdb/StandaloneInfluxDbReporter.scala
@@ -0,0 +1,28 @@
+package pl.touk.nussknacker.engine.standalone.utils.metrics.dropwizard.influxdb
+
+import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
+import io.dropwizard.metrics5.MetricRegistry
+import io.dropwizard.metrics5.influxdb.InfluxDbReporter
+
+import java.util.concurrent.TimeUnit
+
+object StandaloneInfluxDbReporter extends LazyLogging {
+
+  import net.ceedubs.ficus.Ficus._
+  import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+
+  def createAndRunReporterIfConfigured(metricRegistry: MetricRegistry, config: Config): Option[InfluxDbReporter] = {
+    config.getAs[InfluxSenderConfig]("metrics.influx").map { influxSenderConfig =>
+      logger.info("Found Influxdb metrics reporter config, starting reporter")
+      val reporter = InfluxDbHttpReporter.build(metricRegistry, influxSenderConfig)
+      reporter.start(influxSenderConfig.reporterPolling.toSeconds, TimeUnit.SECONDS)
+      reporter
+    } orElse {
+      logger.info("Influxdb metrics reporter config not found")
+      None
+    }
+  }
+
+}
+
