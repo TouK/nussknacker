ea4511c91e997ffc09e653c719c189c5652cb7b5
Juliusz Nadbereżny <nadberezny@gmail.com>
2021-04-22 11:50:43 +0200

    Avro sink: nullable type as optional param

--- engine/flink/avro-util/src/test/scala/pl/touk/nussknacker/engine/avro/sink/AvroSinkValueTest.scala
+++ engine/flink/avro-util/src/test/scala/pl/touk/nussknacker/engine/avro/sink/AvroSinkValueTest.scala
@@ -1,23 +1,28 @@
 package pl.touk.nussknacker.engine.avro.sink
 
+import org.apache.avro.SchemaBuilder
 import org.scalatest.{FunSuite, Matchers}
 import pl.touk.nussknacker.engine.api.LazyParameter
 import pl.touk.nussknacker.engine.api.context.ProcessCompilationError.NodeId
 import pl.touk.nussknacker.engine.api.typed.typing
 import pl.touk.nussknacker.engine.api.typed.typing.Typed
 import pl.touk.nussknacker.engine.avro.KafkaAvroBaseTransformer.SinkValueParamName
-import pl.touk.nussknacker.engine.util.typing.TypingUtils
 
 
 class AvroSinkValueTest extends FunSuite with Matchers {
   private implicit val nodeId = NodeId("")
 
   test("sink params to AvroSinkRecordValue") {
-    val typ = TypingUtils.typeMapDefinition(
-      Map(
-        "a" -> "Long",
-        "b" -> Map("c" -> "Long")
-      ))
+    val recordSchema = SchemaBuilder
+      .record("A")
+      .fields()
+      .name("a").`type`().longType().noDefault()
+      .name("b").`type`()
+        .record("B")
+        .fields()
+          .name("c").`type`().longType().noDefault()
+        .endRecord().noDefault()
+      .endRecord()
 
     val value = new LazyParameter[AnyRef] {
       override def returnType: typing.TypingResult = Typed[java.lang.Long]
@@ -27,7 +32,7 @@ class AvroSinkValueTest extends FunSuite with Matchers {
       "a" -> value,
       "b.c" -> value)
 
-    val sinkParam = AvroSinkValueParameter(typ).valueOr(e => fail(e.toString))
+    val sinkParam = AvroSinkValueParameter(recordSchema).valueOr(e => fail(e.toString))
 
     val fields = AvroSinkValue.applyUnsafe(sinkParam, parameterValues)
       .asInstanceOf[AvroSinkRecordValue]
@@ -38,11 +43,12 @@ class AvroSinkValueTest extends FunSuite with Matchers {
   }
 
   test("sink params to AvroSinkSingleValue") {
+    val longSchema = SchemaBuilder.builder().longType()
     val value = new LazyParameter[AnyRef] {
       override def returnType: typing.TypingResult = Typed[java.lang.Long]
     }
     val parameterValues = Map(SinkValueParamName -> value)
-    val sinkParam = AvroSinkValueParameter(typing.Typed[java.lang.Long]).valueOr(e => fail(e.toString))
+    val sinkParam = AvroSinkValueParameter(longSchema).valueOr(e => fail(e.toString))
 
     AvroSinkValue
       .applyUnsafe(sinkParam, parameterValues)
