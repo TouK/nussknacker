2a06e3417e637c892d3bdf5688222b2868f47f57
dswiecki <dswiecki9@gmail.com>
2021-06-22 15:33:33 +0200

    Add schema registry model

---
+++ demo/docker/testData/schema/createSchemas.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd $(dirname $0)
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @alerts-value.json http://localhost:3082/subjects/alerts-value/versions -v
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @transactions-value.json http://localhost:3082/subjects/transactions-value/versions -v
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @processedEvents-value.json http://localhost:3082/subjects/processedEvents-value/versions -v
