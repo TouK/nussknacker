360bcb1b775e384de9cf2d11af15fcbbca72eb02
gskrobisz <gsk@touk.pl>
2021-07-09 15:55:36 +0200

    Add delayed sources (with preview branch) (#1894)
    * Add delayed sources (generic and avro)
    
    * Fix scala 2.11 compilation error
    
    * CR fixes and tests for delayed avro source.
    
    * Docs and CR fixes: consolidate extractTimestamp and timestampAsigner.
    
    * CR fixes
    
    * Fix legacy timestampAssigner
    
    * Add timestampField validation
    
    * Revert exception throwing inside the source.

--- engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/generic/sources.scala
+++ engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/generic/sources.scala
@@ -1,5 +1,6 @@
 package pl.touk.nussknacker.engine.kafka.generic
 
+import cats.data.Validated.{Invalid, Valid}
 import io.circe.{Decoder, Json, JsonObject}
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.scala._
@@ -9,17 +10,21 @@ import pl.touk.nussknacker.engine.api.CirceUtil
 import pl.touk.nussknacker.engine.api.context.ProcessCompilationError.NodeId
 import pl.touk.nussknacker.engine.api.context.ValidationContext
 import pl.touk.nussknacker.engine.api.context.transformation.{DefinedEagerParameter, NodeDependencyValue}
-import pl.touk.nussknacker.engine.api.definition.{JsonParameterEditor, MandatoryParameterValidator, NotBlankParameterValidator, Parameter}
+import pl.touk.nussknacker.engine.api.definition.Parameter
+import pl.touk.nussknacker.engine.api.exception.NonTransientException
 import pl.touk.nussknacker.engine.api.process.ProcessObjectDependencies
 import pl.touk.nussknacker.engine.api.test.{TestDataSplit, TestParsingUtils}
 import pl.touk.nussknacker.engine.api.typed._
+import pl.touk.nussknacker.engine.flink.api.process.{FlinkContextInitializer, FlinkSource}
+import pl.touk.nussknacker.engine.flink.api.timestampwatermark.TimestampWatermarkHandler
 import pl.touk.nussknacker.engine.flink.util.source.EspDeserializationSchema
 import pl.touk.nussknacker.engine.kafka.consumerrecord.FixedValueDeserializationSchemaFactory
+import pl.touk.nussknacker.engine.kafka.generic.KafkaDelayedSourceFactory._
+import pl.touk.nussknacker.engine.kafka.generic.KafkaTypedSourceFactory._
 import pl.touk.nussknacker.engine.kafka.source.KafkaSourceFactory
 import pl.touk.nussknacker.engine.kafka.source.KafkaSourceFactory.TopicParamName
-import pl.touk.nussknacker.engine.kafka.{BasicRecordFormatter, KafkaConfig, RecordFormatter, RecordFormatterFactory}
+import pl.touk.nussknacker.engine.kafka.{BasicRecordFormatter, KafkaConfig, PreparedKafkaTopic, RecordFormatter, RecordFormatterFactory}
 import pl.touk.nussknacker.engine.util.Implicits._
-import pl.touk.nussknacker.engine.util.typing.TypingUtils
 
 import java.nio.charset.StandardCharsets
 import java.util
@@ -37,22 +42,93 @@ object sources {
   class GenericTypedJsonSourceFactory(processObjectDependencies: ProcessObjectDependencies) extends KafkaSourceFactory[String, TypedMap](
     new FixedValueDeserializationSchemaFactory(JsonTypedMapDeserialization), None, FixedRecordFormatterFactoryWrapper(JsonRecordFormatter), processObjectDependencies) {
 
-    protected val TypeDefinitionParamName = "type"
-
-    protected val TypeParameter: Parameter = Parameter[java.util.Map[String, _]](TypeDefinitionParamName).copy(
-      validators = List(MandatoryParameterValidator, NotBlankParameterValidator),
-      editor = Some(JsonParameterEditor)
+    override protected def prepareInitialParameters: List[Parameter] = super.prepareInitialParameters ++ List(
+      TypeParameter
     )
-
-    override protected def prepareInitialParameters: List[Parameter] = super.prepareInitialParameters ++ List(TypeParameter)
 
     override protected def nextSteps(context: ValidationContext, dependencies: List[NodeDependencyValue])(implicit nodeId: NodeId): NodeTransformationDefinition = {
       case step@TransformationStep((TopicParamName, DefinedEagerParameter(topic: String, _)) ::
         (TypeDefinitionParamName, DefinedEagerParameter(definition: Any, _)) :: Nil, _) =>
-        val typingResult = TypingUtils.typeMapDefinition(definition.asInstanceOf[java.util.Map[String, _]])
-        prepareSourceFinalResults(context, dependencies, step.parameters, keyTypingResult, typingResult, Nil)
+        val topicValidationErrors = topicsValidationErrors(topic)
+        calculateTypingResult(definition) match {
+          case Valid((_, typingResult)) =>
+            prepareSourceFinalResults(context, dependencies, step.parameters, keyTypingResult, typingResult, topicValidationErrors)
+          case Invalid(exc) =>
+            val errors = topicValidationErrors ++ List(exc.toCustomNodeError(nodeId))
+            prepareSourceFinalErrors(context, dependencies, step.parameters, errors)
+        }
       case step@TransformationStep((TopicParamName, top) :: (TypeDefinitionParamName, typ) :: Nil, _) =>
         prepareSourceFinalErrors(context, dependencies, step.parameters, errors = Nil)
+    }
+  }
+
+  class DelayedGenericTypedJsonSourceFactory(formatterFactory: RecordFormatterFactory,
+                                             processObjectDependencies: ProcessObjectDependencies,
+                                             timestampAssigner: Option[TimestampWatermarkHandler[TypedJson]])
+    extends KafkaSourceFactory[String, TypedMap](
+      new FixedValueDeserializationSchemaFactory(JsonTypedMapDeserialization),
+      timestampAssigner,
+      formatterFactory,
+      processObjectDependencies
+    ) with BaseKafkaDelayedSourceFactory {
+
+    override protected def prepareInitialParameters: List[Parameter] = super.prepareInitialParameters ++ List(
+      TypeParameter, TimestampFieldParameter, DelayParameter
+    )
+
+    override def nextSteps(context: ValidationContext, dependencies: List[NodeDependencyValue])(implicit nodeId: NodeId): NodeTransformationDefinition = {
+      case step@TransformationStep(
+      (TopicParamName, DefinedEagerParameter(topic: String, _)) ::
+        (TypeDefinitionParamName, DefinedEagerParameter(definition: TypeDefinition, _)) ::
+        (TimestampFieldParamName, DefinedEagerParameter(field, _)) ::
+        (DelayParameterName, DefinedEagerParameter(delay, _)) :: Nil, _
+      ) =>
+        val topicValidationErrors = topicsValidationErrors(topic)
+        calculateTypingResult(definition) match {
+          case Valid((definition, typingResult)) =>
+            val delayValidationErrors = Option(delay.asInstanceOf[java.lang.Long]).map(d => validateDelay(d)).getOrElse(Nil)
+            val timestampValidationErrors = Option(field.asInstanceOf[String]).map(f => validateTimestampField(f, typingResult)).getOrElse(Nil)
+            val errors = topicValidationErrors ++ timestampValidationErrors ++ delayValidationErrors
+            prepareSourceFinalResults(context, dependencies, step.parameters, keyTypingResult, typingResult, errors)
+          case Invalid(exc) =>
+            val errors = topicValidationErrors ++ List(exc.toCustomNodeError(nodeId))
+            prepareSourceFinalErrors(context, dependencies, step.parameters, errors = errors)
+        }
+      case step@TransformationStep((TopicParamName, _) :: (TypeDefinitionParamName, _) :: (TimestampFieldParamName, _) :: (DelayParameterName, _) :: Nil, _) =>
+        prepareSourceFinalErrors(context, dependencies, step.parameters, errors = Nil)
+    }
+
+    override protected def createSource(params: Map[String, Any],
+                                        dependencies: List[NodeDependencyValue],
+                                        finalState: Option[State],
+                                        preparedTopics: List[PreparedKafkaTopic],
+                                        kafkaConfig: KafkaConfig,
+                                        deserializationSchema: KafkaDeserializationSchema[TypedJson],
+                                        timestampAssigner: Option[TimestampWatermarkHandler[TypedJson]],
+                                        formatter: RecordFormatter,
+                                        flinkContextInitializer: FlinkContextInitializer[TypedJson]): FlinkSource[TypedJson] = {
+      extractDelayInMillis(params) match {
+        case millis if millis > 0 =>
+          val timestampFieldName = extractTimestampField(params)
+          val timestampAssignerWithExtract: Option[TimestampWatermarkHandler[TypedJson]] =
+            Option(timestampFieldName)
+              .map(fieldName => {
+                prepareTimestampAssigner(
+                  kafkaConfig,
+                  extractTimestampFromField(fieldName)
+                )
+              }).orElse(timestampAssigner)
+          createDelayedKafkaSource[String, TypedMap](preparedTopics, kafkaConfig, deserializationSchema, timestampAssignerWithExtract, formatter, flinkContextInitializer, millis)
+        case _ =>
+          super.createSource(params, dependencies, finalState, preparedTopics, kafkaConfig, deserializationSchema, timestampAssigner, formatter, flinkContextInitializer)
+      }
+    }
+
+    def extractTimestampFromField(fieldName: String)(element: TypedJson, kafkaEventTimestamp: Long): Long = {
+      // TODO: Handle exceptions thrown within sources (now the whole process fails)
+      Option(element.value().get(fieldName))
+        .map(_.asInstanceOf[Long])
+        .getOrElse(0L) // explicit null to 0L conversion (instead of implicit unboxing)
     }
   }
 
