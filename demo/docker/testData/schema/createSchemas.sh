17d395f819db1cb9d31c37c4d1c7ed39535e62eb
Arek Burdach <arek.burdach@gmail.com>
2021-07-23 10:20:37 +0200

    Demo test scripts fix: added change dir to make relative paths works correctly

--- demo/docker/testData/schema/createSchemas.sh
+++ demo/docker/testData/schema/createSchemas.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cd $(dirname $0)
+
+set -e
+
+cd "$(dirname $0)"
+
 curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @transactions-value.json http://localhost:3082/subjects/transactions-value/versions -v
 curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @processedEvents-value.json http://localhost:3082/subjects/processedEvents-value/versions -v
 curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @alerts-value.json http://localhost:3082/subjects/alerts-value/versions -v
--- demo/docker/testData/sendToKafka.sh
+++ demo/docker/testData/sendToKafka.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+set -e
+
+cd "$(dirname $0)"
+
 #TODO: is it always this?
 CONTAINER_NAME=nussknacker_kafka
 docker exec -i $CONTAINER_NAME kafka-console-producer.sh --topic $1 --broker-list localhost:9092
