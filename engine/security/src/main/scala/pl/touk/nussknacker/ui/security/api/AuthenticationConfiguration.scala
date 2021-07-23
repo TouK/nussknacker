2365faa9225a04e61d8cf2efc491763099305c96
Tomasz Wielga <tw@touk.pl>
2021-07-23 11:53:46 +0200

    An authorization rejection maps to the Unauthorized status code on anonymous access.

--- engine/security/src/main/scala/pl/touk/nussknacker/ui/security/api/AuthenticationConfiguration.scala
+++ engine/security/src/main/scala/pl/touk/nussknacker/ui/security/api/AuthenticationConfiguration.scala
@@ -11,7 +11,6 @@ import pl.touk.nussknacker.ui.security.api.Permission.Permission
 trait AuthenticationConfiguration {
   def name: String
   def usersFile: URI
-  def anonymousUserRole: Option[String]
 
   val userConfig: Config = ConfigFactoryExt.parseUri(usersFile, getClass.getClassLoader)
 
--- engine/security/src/main/scala/pl/touk/nussknacker/ui/security/api/AuthenticationResources.scala
+++ engine/security/src/main/scala/pl/touk/nussknacker/ui/security/api/AuthenticationResources.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.ui.security.api
 
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.server.directives.AuthenticationDirective
-import akka.http.scaladsl.server.{Directive1, Directives, Route}
+import akka.http.scaladsl.server.{AuthorizationFailedRejection, Directive1, Directives, RejectionHandler, Route}
 import com.typesafe.config.Config
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import sttp.client.{NothingT, SttpBackend}
@@ -24,13 +24,27 @@ trait AuthenticationResources extends Directives with FailFastCirceSupport {
   protected lazy val additionalRoute: Route = Directives.reject
 }
 
-trait AnonymousAccess {
+trait AnonymousAccess extends Directives {
   val anonymousUserRole: Option[String]
 
   def authenticateReally(): AuthenticationDirective[AuthenticatedUser]
 
+  def authenticateOrPermitAnonymously(anonymousUser: AuthenticatedUser): AuthenticationDirective[AuthenticatedUser] = {
+    def handleAuthorizationFailedRejection = handleRejections(RejectionHandler.newBuilder()
+      // If the authorization rejection was caused by anonymous access,
+      // we issue the Unauthorized status code with a challenge instead of the Forbidden
+      .handle { case AuthorizationFailedRejection => authenticateReally() { _ => reject } }
+      .result())
+    authenticateReally().optional.flatMap(_.map(provide).getOrElse(
+      handleAuthorizationFailedRejection.tmap(_ => anonymousUser)
+    ))
+  }
+
   def authenticate(): Directive1[AuthenticatedUser] = {
-    anonymousUserRole.map(role => AuthenticatedUser("anonymous", "anonymous", List(role))).map(authenticateReally().withAnonymousUser(_)).getOrElse(authenticateReally())
+    anonymousUserRole.map(role => AuthenticatedUser("anonymous", "anonymous", List(role)))
+      .map(authenticateOrPermitAnonymously)
+      .getOrElse(authenticateReally())
+
   }
 }
 
