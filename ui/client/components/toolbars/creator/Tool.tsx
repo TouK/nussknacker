867a7d06eac07bbf59014f3543c1e976664cd8ea
Arek Burdach <arek.burdach@gmail.com>
2021-07-28 14:47:24 +0200

    Component drag: icon instead of tool panel

--- ui/client/components/toolbars/creator/Tool.tsx
+++ ui/client/components/toolbars/creator/Tool.tsx
@@ -1,6 +1,6 @@
 import {cloneDeep, memoize} from "lodash"
-import React, {useMemo} from "react"
-import {useDrag} from "react-dnd"
+import React, {useEffect, useMemo} from "react"
+import {DragPreviewImage, useDrag} from "react-dnd"
 import Highlighter from "react-highlight-words"
 import {useSelector} from "react-redux"
 import ProcessUtils from "../../../common/ProcessUtils"
@@ -23,10 +23,27 @@ type OwnProps = {
 export default function Tool(props: OwnProps): JSX.Element {
   const {label, nodeModel, highlight} = props
   const icon = useToolIcon(nodeModel)
-  const [collectedProps, drag] = useDrag({
+  const [collectedProps, drag, preview] = useDrag({
     item: {...cloneDeep(nodeModel), id: label, type: DndTypes.ELEMENT},
     begin: () => ({...cloneDeep(nodeModel), id: label}),
   })
+
+  useEffect(() => {
+    const canvas = document.createElement('canvas')
+
+    const img = new Image()
+    img.onload = function () {
+      canvas.width = img.width * 0.4
+      canvas.height = img.height * 0.4
+      canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height)
+
+      const img2 = new Image()
+      img2.src = canvas.toDataURL()
+
+      preview(img2)
+    }
+    img.src = icon
+  }, [preview])
 
   const {theme} = useNkTheme()
 
