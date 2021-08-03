632c83a2955561c2efc91557cd51c3a68440b685
Arek Burdach <arek.burdach@gmail.com>
2021-08-03 19:04:54 +0200

    Demo moved to https://github.com/TouK/nussknacker-quickstart

--- demo/docker/testData/sendToKafka.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname $0)"
-
-#TODO: is it always this?
-CONTAINER_NAME=nussknacker_kafka
-docker exec -i $CONTAINER_NAME kafka-console-producer.sh --topic $1 --broker-list localhost:9092
