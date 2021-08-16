a78326434790913fb43b285069416800df7393a8
Krzysztof Gadomski <kgd@touk.pl>
2021-08-16 11:08:13 +0200

    fix saving properties for scenarios with fragments

--- ui/client/components/graph/SubprocessSchemaAligner.js
+++ ui/client/components/graph/SubprocessSchemaAligner.js
@@ -3,7 +3,7 @@ import fp from "lodash/fp"
 export function alignSubprocessWithSchema(processDefinitionData, subprocessNode) {
   const subprocessId = subprocessNode.ref.id
   const subprocessSchema = processDefinitionData.nodesToAdd
-    .find((nodesToAdd) => {return nodesToAdd.name === "subprocesses"}).possibleNodes
+    .find((nodesToAdd) => {return nodesToAdd.name === "fragments"}).possibleNodes
     .find((obj) => obj.node.ref.id === subprocessId)
   const subprocessSchemaParameters = subprocessSchema.node.ref.parameters
   const mergedParameters = subprocessSchemaParameters
