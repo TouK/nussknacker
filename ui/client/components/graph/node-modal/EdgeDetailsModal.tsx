8d9d8f4946038b9be18271cbd00da42b4ee9b156
Julian Wielga <j@touk.pl>
2021-08-23 13:47:45 +0200

    refactor: types & formatting

--- ui/client/components/graph/node-modal/EdgeDetailsModal.tsx
+++ ui/client/components/graph/node-modal/EdgeDetailsModal.tsx
@@ -1,26 +1,48 @@
+/* eslint-disable i18next/no-literal-string */
 import "ladda/dist/ladda.min.css"
-import React from "react"
-import PropTypes from "prop-types"
-import {connect} from "react-redux"
-import Modal from "react-modal"
 import _ from "lodash"
-import LaddaButton from "react-ladda"
+import React from "react"
 import Draggable from "react-draggable"
+import LaddaButton from "react-ladda"
+import Modal from "react-modal"
+import {connect} from "react-redux"
 import ActionsUtils from "../../../actions/ActionsUtils"
 import {isEdgeEditable} from "../../../common/EdgeUtils"
 import NkModalStyles from "../../../common/NkModalStyles"
-import {getEdgeToDisplay} from "../../../reducers/selectors/graph"
+import ProcessUtils from "../../../common/ProcessUtils"
+import {getEdgeToDisplay, getProcessCategory, getProcessToDisplay} from "../../../reducers/selectors/graph"
+import {getCapabilities} from "../../../reducers/selectors/other"
+import {getProcessDefinitionData} from "../../../reducers/selectors/settings"
 import {isEdgeDetailsModalVisible} from "../../../reducers/selectors/ui"
 import {ButtonWithFocus} from "../../withFocus"
 import NodeUtils from "../NodeUtils"
 import EdgeDetailsContent from "./EdgeDetailsContent"
-import ProcessUtils from "../../../common/ProcessUtils"
 
 //TODO: this is still pretty switch-specific.
-class EdgeDetailsModal extends React.Component {
-
-  static propTypes = {
-    edge: PropTypes.object.isRequired,
+class EdgeDetailsModal extends React.Component<StateProps, {editedEdge, pendingRequest: boolean}> {
+  closeModal = () => {
+    this.props.actions.closeModals()
+  }
+  performEdgeEdit = async () => {
+    this.setState({pendingRequest: true})
+    await this.props.actions.editEdge(this.props.processToDisplay, this.props.edge, this.state.editedEdge)
+    this.setState({pendingRequest: false})
+    this.closeModal()
+  }
+  updateEdgeProp = (prop, value) => {
+    const editedEdge = _.cloneDeep(this.state.editedEdge)
+    const newEdge = _.set(editedEdge, prop, value)
+    this.setState({editedEdge: newEdge})
+  }
+  changeEdgeTypeValue = (edgeTypeValue) => {
+    const fromNode = NodeUtils.getNodeById(this.props.edge.from, this.props.processToDisplay)
+    const defaultEdgeType = NodeUtils
+      .edgesForNode(fromNode, this.props.processDefinitionData).edges.find(e => e.type === edgeTypeValue)
+    const newEdge = {
+      ...this.state.editedEdge,
+      edgeType: defaultEdgeType,
+    }
+    this.setState({editedEdge: newEdge})
   }
 
   constructor(props) {
@@ -38,57 +60,26 @@ class EdgeDetailsModal extends React.Component {
     }
   }
 
-  closeModal = () => {
-    this.props.actions.closeModals()
-  }
-
-  performEdgeEdit = () => {
-    this.setState({pendingRequest: true})
-    this.props.actions.editEdge(this.props.processToDisplay, this.props.edge, this.state.editedEdge).then(() => {
-      this.setState({pendingRequest: false})
-      this.closeModal()
-    })
-  }
-
   renderModalButtons() {
     return [
       <ButtonWithFocus key="2" type="button" title="Cancel node details" className="modalButton" onClick={this.closeModal}>
         Cancel
       </ButtonWithFocus>,
-      !this.props.readOnly ? (
-        <LaddaButton
-          key="1"
-          title="Apply edge details"
-          className="modalButton pull-right modalConfirmButton"
-          loading={this.state.pendingRequest}
-          data-style="zoom-in"
-          onClick={this.performEdgeEdit}
-        >
-          Apply
-        </LaddaButton>
-      ) : null,
+      !this.props.readOnly ?
+        (
+          <LaddaButton
+            key="1"
+            title="Apply edge details"
+            className="modalButton pull-right modalConfirmButton"
+            loading={this.state.pendingRequest}
+            data-style="zoom-in"
+            onClick={this.performEdgeEdit}
+          >
+            Apply
+          </LaddaButton>
+        ) :
+        null,
     ]
-  }
-
-  updateEdgeProp = (prop, value) => {
-    const editedEdge = _.cloneDeep(this.state.editedEdge)
-    const newEdge = _.set(editedEdge, prop, value)
-    this.setState({editedEdge: newEdge})
-  }
-
-  changeEdgeTypeValue = (edgeTypeValue) => {
-    const fromNode = NodeUtils.getNodeById(this.props.edge.from, this.props.processToDisplay)
-    const defaultEdgeType = NodeUtils
-      .edgesForNode(fromNode, this.props.processDefinitionData).edges.find(e => e.type === edgeTypeValue)
-    const newEdge = {
-      ...this.state.editedEdge,
-      edgeType: {
-        type: defaultEdgeType.type,
-        //we want to preserve previously edited (in state) value of expression condition
-        condition: this.state.editedEdge.edgeType.condition,
-      },
-    }
-    this.setState({editedEdge: newEdge})
   }
 
   render() {
@@ -137,22 +128,28 @@ class EdgeDetailsModal extends React.Component {
 
 function mapState(state) {
   const edge = getEdgeToDisplay(state)
+  const processToDisplay = getProcessToDisplay(state)
+  const processDefinitionData = getProcessDefinitionData(state)
+  const processCategory = getProcessCategory(state)
+
   const nodeId = edge.from
-  const errors = _.get(state.graphReducer.processToDisplay, `validationResult.errors.invalidNodes[${nodeId}]`, [])
-  const processCategory = state.graphReducer.fetchedProcessDetails.processCategory
-  const variableTypes = ProcessUtils.findAvailableVariables(state.settings.processDefinitionData,
+  const errors = _.get(processToDisplay, `validationResult.errors.invalidNodes[${nodeId}]`, [])
+  const variableTypes = ProcessUtils.findAvailableVariables(
+    processDefinitionData,
     processCategory,
-    state.graphReducer.processToDisplay)(nodeId)
+    processToDisplay,
+  )(nodeId, undefined)
   return {
     edge,
-    processToDisplay: state.graphReducer.processToDisplay,
+    processToDisplay,
+    processDefinitionData,
+    variableTypes,
     edgeErrors: errors,
-    readOnly: !state.settings.loggedUser.canWrite(processCategory) ||
-      _.get(state, "graphReducer.fetchedProcessDetails.isArchived"),
-    processDefinitionData: state.settings.processDefinitionData,
+    readOnly: !getCapabilities(state).write,
     showModal: isEdgeDetailsModalVisible(state),
-    variableTypes: variableTypes,
   }
 }
+
+type StateProps = ReturnType<(typeof ActionsUtils.mapDispatchWithEspActions)> & ReturnType<typeof mapState>
 
 export default connect(mapState, ActionsUtils.mapDispatchWithEspActions)(EdgeDetailsModal)
