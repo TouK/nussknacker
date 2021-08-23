c73759710577d8b8f0999462cc29a213a209a120
Julian Wielga <j@touk.pl>
2021-08-23 14:38:43 +0200

    refactor: FC & hooks

--- ui/client/components/graph/node-modal/EdgeDetailsModal.tsx
+++ ui/client/components/graph/node-modal/EdgeDetailsModal.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable i18next/no-literal-string */
 import "ladda/dist/ladda.min.css"
-import _ from "lodash"
-import React from "react"
+import {cloneDeep, set} from "lodash"
+import React, {useCallback, useEffect, useMemo, useState} from "react"
 import Draggable from "react-draggable"
 import LaddaButton from "react-ladda"
 import Modal from "react-modal"
-import {connect} from "react-redux"
-import ActionsUtils from "../../../actions/ActionsUtils"
+import {useDispatch, useSelector} from "react-redux"
+import {closeModals, editEdge} from "../../../actions/nk"
 import {isEdgeEditable} from "../../../common/EdgeUtils"
 import NkModalStyles from "../../../common/NkModalStyles"
 import ProcessUtils from "../../../common/ProcessUtils"
@@ -19,137 +19,125 @@ import NodeUtils from "../NodeUtils"
 import EdgeDetailsContent from "./EdgeDetailsContent"
 
 //TODO: this is still pretty switch-specific.
-class EdgeDetailsModal extends React.Component<StateProps, {editedEdge, pendingRequest: boolean}> {
-  closeModal = () => {
-    this.props.actions.closeModals()
-  }
-  performEdgeEdit = async () => {
-    this.setState({pendingRequest: true})
-    await this.props.actions.editEdge(this.props.processToDisplay, this.props.edge, this.state.editedEdge)
-    this.setState({pendingRequest: false})
-    this.closeModal()
-  }
-  updateEdgeProp = (prop, value) => {
-    const editedEdge = _.cloneDeep(this.state.editedEdge)
-    const newEdge = _.set(editedEdge, prop, value)
-    this.setState({editedEdge: newEdge})
-  }
-  changeEdgeTypeValue = (edgeTypeValue) => {
-    const fromNode = NodeUtils.getNodeById(this.props.edge.from, this.props.processToDisplay)
+function EdgeDetailsModal() {
+  const edge = useSelector(getEdgeToDisplay)
+  const processToDisplay = useSelector(getProcessToDisplay)
+  const processDefinitionData = useSelector(getProcessDefinitionData)
+  const processCategory = useSelector(getProcessCategory)
+  const {write} = useSelector(getCapabilities)
+  const showModal = useSelector(isEdgeDetailsModalVisible)
+
+  const nodeId = edge.from
+  const readOnly = !write
+
+  const variableTypes = useMemo(() => {
+    const findAvailableVariables = ProcessUtils.findAvailableVariables(
+      processDefinitionData,
+      processCategory,
+      processToDisplay,
+    )
+    return findAvailableVariables(nodeId, undefined)
+  }, [nodeId, processCategory, processDefinitionData, processToDisplay])
+
+  const dispatch = useDispatch()
+
+  const [state, setState] = useState({
+    pendingRequest: false,
+    editedEdge: edge,
+  })
+
+  useEffect(() => {
+    setState(s => ({...s, editedEdge: edge}))
+  }, [edge])
+
+  const closeModal = useCallback(() => dispatch(closeModals()), [dispatch])
+
+  const performEdgeEdit = useCallback(async () => {
+    setState(s => ({...s, pendingRequest: true}))
+    await dispatch(editEdge(processToDisplay, edge, state.editedEdge))
+    setState(s => ({...s, pendingRequest: false}))
+    closeModal()
+  }, [closeModal, dispatch, edge, processToDisplay, state.editedEdge])
+
+  const updateEdgeProp = useCallback((prop, value) => {
+    const editedEdge = cloneDeep(state.editedEdge)
+    const newEdge = set(editedEdge, prop, value)
+    setState(s => ({...s, editedEdge: newEdge}))
+  }, [state.editedEdge])
+
+  const changeEdgeTypeValue = useCallback((edgeTypeValue) => {
+    const fromNode = NodeUtils.getNodeById(edge.from, processToDisplay)
     const defaultEdgeType = NodeUtils
-      .edgesForNode(fromNode, this.props.processDefinitionData).edges.find(e => e.type === edgeTypeValue)
+      .edgesForNode(fromNode, processDefinitionData).edges.find(e => e.type === edgeTypeValue)
     const newEdge = {
       ...this.state.editedEdge,
       edgeType: defaultEdgeType,
     }
-    this.setState({editedEdge: newEdge})
-  }
+    setState(s => ({...s, editedEdge: newEdge}))
+  }, [edge.from, processDefinitionData, processToDisplay])
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      pendingRequest: false,
-      editedEdge: props.edge,
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const {edge} = this.props
-    if (!_.isEqual(prevProps.edge, edge)) {
-      this.setState({editedEdge: edge})
-    }
-  }
-
-  renderModalButtons() {
+  const renderModalButtons = useCallback(() => {
     return [
-      <ButtonWithFocus key="2" type="button" title="Cancel node details" className="modalButton" onClick={this.closeModal}>
+      <ButtonWithFocus key="2" type="button" title="Cancel node details" className="modalButton" onClick={closeModal}>
         Cancel
       </ButtonWithFocus>,
-      !this.props.readOnly ?
+      !readOnly ?
         (
           <LaddaButton
             key="1"
             title="Apply edge details"
             className="modalButton pull-right modalConfirmButton"
-            loading={this.state.pendingRequest}
+            loading={state.pendingRequest}
             data-style="zoom-in"
-            onClick={this.performEdgeEdit}
+            onClick={performEdgeEdit}
           >
             Apply
           </LaddaButton>
         ) :
         null,
     ]
-  }
+  }, [closeModal, performEdgeEdit, readOnly, state.pendingRequest])
 
-  render() {
-    const {edge, showModal, readOnly} = this.props
-    const isOpen = isEdgeEditable(edge) && showModal
-    const titleStyles = NkModalStyles.headerStyles("#2D8E54", "white")
-    return (
-      <div className="objectModal">
-        <Modal
-          isOpen={isOpen}
-          shouldCloseOnOverlayClick={false}
-          onRequestClose={this.closeModal}
-        >
-          <div className="draggable-container">
-            <Draggable bounds="parent" handle=".modal-draggable-handle">
-              <div className="espModal">
-                <div className="modalHeader">
-                  <div className="edge-modal-title modal-draggable-handle" style={titleStyles}>
-                    <span>edge</span>
-                  </div>
-                </div>
-                <div className="modalContentDark edge-details">
-                  <EdgeDetailsContent
-                    changeEdgeTypeValue={this.changeEdgeTypeValue}
-                    updateEdgeProp={this.updateEdgeProp}
-                    readOnly={readOnly}
-                    edge={this.state.editedEdge}
-                    showValidation={true}
-                    showSwitch={true}
-                    variableTypes={this.props.variableTypes}
-                  />
-                </div>
-                <div className="modalFooter">
-                  <div className="footerButtons">
-                    {this.renderModalButtons()}
-                  </div>
+  const isOpen = useMemo(() => isEdgeEditable(edge) && showModal, [edge, showModal])
+  const titleStyles = NkModalStyles.headerStyles("#2D8E54", "white")
+
+  return (
+    <div className="objectModal">
+      <Modal
+        isOpen={isOpen}
+        shouldCloseOnOverlayClick={false}
+        onRequestClose={closeModal}
+      >
+        <div className="draggable-container">
+          <Draggable bounds="parent" handle=".modal-draggable-handle">
+            <div className="espModal">
+              <div className="modalHeader">
+                <div className="edge-modal-title modal-draggable-handle" style={titleStyles}>
+                  <span>edge</span>
                 </div>
               </div>
-            </Draggable>
-          </div>
-        </Modal>
-      </div>
-    )
-  }
+              <div className="modalContentDark edge-details">
+                <EdgeDetailsContent
+                  changeEdgeTypeValue={changeEdgeTypeValue}
+                  updateEdgeProp={updateEdgeProp}
+                  readOnly={readOnly}
+                  edge={state.editedEdge}
+                  showValidation={true}
+                  showSwitch={true}
+                  variableTypes={variableTypes}
+                />
+              </div>
+              <div className="modalFooter">
+                <div className="footerButtons">
+                  {renderModalButtons()}
+                </div>
+              </div>
+            </div>
+          </Draggable>
+        </div>
+      </Modal>
+    </div>
+  )
 }
 
-function mapState(state) {
-  const edge = getEdgeToDisplay(state)
-  const processToDisplay = getProcessToDisplay(state)
-  const processDefinitionData = getProcessDefinitionData(state)
-  const processCategory = getProcessCategory(state)
-
-  const nodeId = edge.from
-  const errors = _.get(processToDisplay, `validationResult.errors.invalidNodes[${nodeId}]`, [])
-  const variableTypes = ProcessUtils.findAvailableVariables(
-    processDefinitionData,
-    processCategory,
-    processToDisplay,
-  )(nodeId, undefined)
-  return {
-    edge,
-    processToDisplay,
-    processDefinitionData,
-    variableTypes,
-    edgeErrors: errors,
-    readOnly: !getCapabilities(state).write,
-    showModal: isEdgeDetailsModalVisible(state),
-  }
-}
-
-type StateProps = ReturnType<(typeof ActionsUtils.mapDispatchWithEspActions)> & ReturnType<typeof mapState>
-
-export default connect(mapState, ActionsUtils.mapDispatchWithEspActions)(EdgeDetailsModal)
+export default EdgeDetailsModal
