27d5e79dcc325d1ec8bda27423834dfbf7eed584
Tomasz Wielga <tw@touk.pl>
2021-09-01 00:22:47 +0200

    After-review changes

--- engine/security/src/main/scala/pl/touk/nussknacker/ui/security/oidc/OidcDiscovery.scala
+++ engine/security/src/main/scala/pl/touk/nussknacker/ui/security/oidc/OidcDiscovery.scala
@@ -1,9 +1,7 @@
 package pl.touk.nussknacker.ui.security.oidc
 
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.Decoder
 import io.circe.generic.extras.{Configuration, ConfiguredJsonCodec, JsonKey}
-import net.ceedubs.ficus.readers.URIReaders
 import sttp.client.circe.asJson
 import sttp.client.{NothingT, SttpBackend, UriContext, basicRequest}
 import sttp.model.MediaType
@@ -11,20 +9,20 @@ import sttp.model.MediaType
 import java.net.URI
 import scala.concurrent.duration.{Duration, SECONDS}
 import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.Try
 
 object OidcDiscovery extends LazyLogging {
-
-  implicit val uriDecoder: Decoder[URI] = Decoder.decodeString.map(URI.create)
+  import pl.touk.nussknacker.engine.api.CirceUtil.codecs._
   implicit val config: Configuration = Configuration.default
 
   def apply(issuer: URI)(implicit ec: ExecutionContext, sttpBackend: SttpBackend[Future, Nothing, NothingT]): Option[OidcDiscovery] =
-    Await.result(
+    Try(Await.result(
       basicRequest
         .contentType(MediaType.ApplicationJson)
         .get(uri"$issuer/.well-known/openid-configuration")
         .response(asJson[OidcDiscovery]).send(),
       Duration(30, SECONDS)
-    ).body match {
+    ).body).fold(Left(_), identity) match {
       case Right(v) => Some(v)
       case Left(err) =>
         logger.warn(s"Unable to retrieve the OpenID Provider's configuration: ${err.getMessage}")
@@ -32,7 +30,7 @@ object OidcDiscovery extends LazyLogging {
     }
 }
 
-@ConfiguredJsonCodec(decodeOnly = true)
+@ConfiguredJsonCodec
 case class OidcDiscovery
 (
   issuer: URI,
