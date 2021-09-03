be6e1e224ee7f214b71204f6677b01a2fef0fc62
JulianWielga <j@touk.pl>
2021-09-03 17:11:02 +0200

    testing

--- ui/client/init.js
+++ ui/client/init.js
@@ -1,4 +1,7 @@
 //config *has* to be loaded before other imports, see Warning at https://webpack.js.org/guides/public-path/#on-the-fly
 import "./config"
 import "./styles"
+
+
+
 import("./bootstrap")
