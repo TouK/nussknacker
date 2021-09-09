2d31502041480c810f5aa320486e3a444a1de2d2
Arek Burdach <arek.burdach@gmail.com>
2021-09-09 16:15:41 +0200

    Discard dialog with easier decision message and buttons

--- ui/client/components/ProcessHistory.tsx
+++ ui/client/components/ProcessHistory.tsx
@@ -28,7 +28,7 @@ export function ProcessHistoryComponent(props: {isReadOnly?: boolean}): JSX.Elem
   const changeVersion = useCallback(
     (version: ProcessVersionType) => props.isReadOnly || nothingToSave ?
       doChangeVersion(version) :
-      confirm({text: unsavedProcessChanges(), onConfirmCallback: () => doChangeVersion(version), confirmText: "DISCARD", denyText: "NO"}),
+      confirm({text: unsavedProcessChanges(), onConfirmCallback: () => doChangeVersion(version), confirmText: "DISCARD", denyText: "CANCEL"}),
     [confirm, doChangeVersion, nothingToSave, props.isReadOnly],
   )
 
