ca1881aa2e6507e1e8169fbce13147e44519af02
Julian Wielga <j@touk.pl>
2021-09-14 15:17:32 +0200

    fix: global event handlers called from windows

--- ui/client/containers/useDocumentListeners.tsx
+++ ui/client/containers/useDocumentListeners.tsx
@@ -1,23 +1,34 @@
-import {useEffect} from "react"
-import {useDispatch} from "react-redux"
+import {useEffect, useMemo} from "react"
 
 type Listener<K extends keyof DocumentEventMap> = (event: DocumentEventMap[K]) => void
 export type DocumentListeners = {
   [K in keyof DocumentEventMap]?: Listener<K> | false
 }
 
+const filterEventsByContainer = (container: HTMLElement) => <K extends keyof DocumentEventMap>(listener: Listener<K>): Listener<K> => (event) => {
+  const path = event.composedPath() || []
+  const rootElements = [window, document, document.documentElement, document.body]
+  const parents = path.filter(t => ![event.target, ...rootElements].includes(t))
+  // allow events from root
+  // ignore all events from outside of container (modals, overlays, etc)
+  if (!parents.length || parents.includes(container)) {
+    return listener(event)
+  }
+}
+
 export function useDocumentListeners(listeners: DocumentListeners): void {
-  const dispatch = useDispatch()
+  const filterByRoot = useMemo(() => filterEventsByContainer(document.getElementById("root")), [])
   useEffect(
     () => {
       const entries = Object.entries(listeners).map(([type, listener]) => {
         if (listener) {
-          document.addEventListener(type, listener)
-          return () => document.removeEventListener(type, listener)
+          const wrapped = filterByRoot(listener)
+          document.addEventListener(type, wrapped)
+          return () => document.removeEventListener(type, wrapped)
         }
       })
       return () => entries.forEach(unbind => unbind())
     },
-    [listeners],
+    [filterByRoot, listeners],
   )
 }
