4888af95f78e6ede33c199378ac2c45d7677c5f1
Maciej Próchniak <mpr@touk.pl>
2021-09-16 10:39:12 +0200

    Drop legacy metrics mode (#2165)

--- engine/flink/util/src/main/scala/pl/touk/nussknacker/engine/flink/util/metrics/MetricUtils.scala
+++ engine/flink/util/src/main/scala/pl/touk/nussknacker/engine/flink/util/metrics/MetricUtils.scala
@@ -5,14 +5,6 @@ import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.metrics.{Counter, Gauge, Histogram, Meter, MetricGroup}
 import pl.touk.nussknacker.engine.flink.api.{NkGlobalParameters, RuntimeContextLifecycle}
 
-/*
-  IMPORTANT: PLEASE keep Metrics.md up to date
-
-  Handling Flink metrics is a bit tricky. For long time we parsed tags directly in Influx, via graphite plugin
-  This is complex and error prone, so we'd like to switch to passing flink metric variables as tags via native influx API
-  Unfortunately, current Flink Influx report doesn't allow for elastic configuration, so for now
-  we translate by default to `old` way in groupsWithNameForLegacyMode
- */
 class MetricUtils(runtimeContext: RuntimeContext) {
 
   def counter(nameParts: NonEmptyList[String], tags: Map[String, String]): Counter = {
@@ -36,16 +28,9 @@ class MetricUtils(runtimeContext: RuntimeContext) {
     group.histogram(name, histogram)
   }
 
-  private val useLegacyMetricsMode: Boolean =
-    NkGlobalParameters.readFromContext(runtimeContext.getExecutionConfig).flatMap(_.configParameters.flatMap(_.useLegacyMetrics)).getOrElse(false)
-
   private def groupsWithName(nameParts: NonEmptyList[String], tags: Map[String, String]): (MetricGroup, String) = {
-    if (useLegacyMetricsMode) {
-      groupsWithNameForLegacyMode(nameParts, tags)
-    } else {
-      val namespaceTags = extractTags(NkGlobalParameters.readFromContext(runtimeContext.getExecutionConfig))
-      tagMode(nameParts, tags ++ namespaceTags)
-    }
+    val namespaceTags = extractTags(NkGlobalParameters.readFromContext(runtimeContext.getExecutionConfig))
+    tagMode(nameParts, tags ++ namespaceTags)
   }
 
   private def tagMode(nameParts: NonEmptyList[String], tags: Map[String, String]): (MetricGroup, String) = {
@@ -58,40 +43,6 @@ class MetricUtils(runtimeContext: RuntimeContext) {
       case (group, (tag, tagValue)) => group.addGroup(tag, tagValue)
     }
     (finalGroup, lastName)
-  }
-
-  private def groupsWithNameForLegacyMode(nameParts: NonEmptyList[String], tags: Map[String, String]): (MetricGroup, String) = {
-    def insertTag(tagId: String)(nameParts: NonEmptyList[String]): (MetricGroup, String)
-      = tagMode(NonEmptyList(nameParts.head, tags(tagId)::nameParts.tail), Map.empty)
-    val insertNodeId = insertTag("nodeId") _
-
-    nameParts match {
-
-      //RateMeterFunction, nodeId tag
-      case l@NonEmptyList("source", _) => insertNodeId(l)
-      //EventTimeDelayMeterFunction, nodeId tag
-      case l@NonEmptyList("eventtimedelay", _) => insertNodeId(l)
-
-      //EndRateMeterFunction, nodeId tag
-      case l@NonEmptyList("end", _) => insertNodeId(l)
-      case l@NonEmptyList("dead_end", _) => insertNodeId(l)
-
-      //NodeCountMetricListener nodeId tag
-      case l@NonEmptyList("nodeCount", _) =>insertNodeId(l)
-
-      //GenericTimeMeasuringService
-      case l@NonEmptyList("service", name :: "instantRate" :: Nil) => tagMode(NonEmptyList("serviceInstant",  tags("serviceName") :: name :: Nil), Map.empty)
-      case l@NonEmptyList("service", name :: "histogram" :: Nil) => tagMode(NonEmptyList("serviceTimes", tags("serviceName") :: name :: Nil), Map.empty)
-
-      case l@NonEmptyList("error", "instantRate" :: "instantRate" :: Nil) => tagMode(l, Map.empty)
-      case l@NonEmptyList("error", "instantRateByNode" :: "instantRate" :: Nil) => insertNodeId(l)
-
-      case l@NonEmptyList("error", "instantRate" :: "count" :: Nil) => tagMode(l, Map.empty)
-      case l@NonEmptyList("error", "instantRateByNode" :: "count" :: Nil) => insertNodeId(l)
-        
-      //we resort to default mode...
-      case _ => tagMode(nameParts, tags)
-    }
   }
 
   private def extractTags(nkGlobalParameters: Option[NkGlobalParameters]): Map[String, String] = {
