9c4d602d5737e9b4c81a313b211663c6da5b42dc
Arek Burdach <arek.burdach@gmail.com>
2021-09-24 18:49:37 +0200

    Added sample node server serving production build

---
+++ ui/client/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const fs = require('fs')
+
+const app = express();
+let proxy = createProxyMiddleware({
+    target: process.env.BACKEND_DOMAIN,
+    changeOrigin: true
+});
+
+app.use('/api', proxy);
+app.use('/static', express.static(path.join(__dirname, 'dist')));
+
+const mainHtmlFile = path.resolve("/tmp", 'main-dev.html');
+
+fs.readFile(path.resolve(__dirname, 'dist', 'main.html'), 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    const result = data.replace(/__publicPath__/g, '');
+
+    fs.writeFile(mainHtmlFile, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(mainHtmlFile);
+});
+app.use(proxy);
+
+app.listen(3000);
