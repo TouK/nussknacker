339d55e8b867ac00715d4499f099a816e5f52017
Maciej Brzeziński <mab816813@gmail.com>
2021-09-30 22:44:50 +0200

    fixed ReflectUtils not parsing correctly + added tests for ReflectUtils parsing scala class names

--- engine/api/src/main/scala/pl/touk/nussknacker/engine/api/util/ReflectUtils.scala
+++ engine/api/src/main/scala/pl/touk/nussknacker/engine/api/util/ReflectUtils.scala
@@ -3,6 +3,6 @@ package pl.touk.nussknacker.engine.api.util
 object ReflectUtils {
 
   def simpleNameWithoutSuffix(clazz: Class[_]): String = {
-    clazz.getSimpleName.stripSuffix("\\$")
+    clazz.getSimpleName.stripSuffix("$")
   }
 }
