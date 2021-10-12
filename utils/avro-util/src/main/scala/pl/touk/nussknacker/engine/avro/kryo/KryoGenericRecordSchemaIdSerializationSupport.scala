be5bdace58e8f275e4130643923c63de423e00e5
dswiecki <dswiecki9@gmail.com>
2021-10-12 14:13:26 +0200

    Move avro serializer and deserializer

--- utils/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/kryo/KryoGenericRecordSchemaIdSerializationSupport.scala
+++ utils/avro-util/src/main/scala/pl/touk/nussknacker/engine/avro/kryo/KryoGenericRecordSchemaIdSerializationSupport.scala
@@ -1,11 +1,11 @@
 package pl.touk.nussknacker.engine.avro.kryo
 
-import org.apache.flink.annotation.PublicEvolving
+import org.apache.kafka.common.annotation.InterfaceStability.Evolving
 import pl.touk.nussknacker.engine.kafka.KafkaConfig
 
 object KryoGenericRecordSchemaIdSerializationSupport {
 
-  @PublicEvolving // default behaviour will be switched to true in some future
+  @Evolving // default behaviour will be switched to true in some future
   def schemaIdSerializationEnabled(kafkaConfig: KafkaConfig): Boolean =
     Option(kafkaConfig)
       .flatMap(_.avroKryoGenericRecordSchemaIdSerialization)
