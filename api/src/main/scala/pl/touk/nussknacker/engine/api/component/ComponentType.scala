5a82498f075f406030c15e543faa7cc0113a3ede
Łukasz Ciołecki <lcl@touk.pl>
2021-10-19 11:36:15 +0200

    Refactor: component definition preparer

---
+++ api/src/main/scala/pl/touk/nussknacker/engine/api/component/ComponentType.scala
@@ -0,0 +1,30 @@
+package pl.touk.nussknacker.engine.api.component
+
+import io.circe.{Decoder, Encoder}
+
+object ComponentType extends Enumeration {
+
+  implicit val typeEncoder: Encoder[ComponentType.Value] = Encoder.encodeEnumeration(ComponentType)
+  implicit val typeDecoder: Decoder[ComponentType.Value] = Decoder.decodeEnumeration(ComponentType)
+
+  type ComponentType = Value
+
+  //Basic's component types
+  val Filter: Value = Value("filter")
+  val Split: Value = Value("split")
+  val Switch: Value = Value("switch")
+  val Variable: Value = Value("variable")
+  val MapVariable: Value = Value("mapVariable")
+
+  //Generic's component types
+  val Processor: Value = Value("processor")
+  val Enricher: Value = Value("enricher")
+  val Sink: Value = Value("sink")
+  val Source: Value = Value("source")
+  val Fragments: Value = Value("fragments")
+  val CustomNode: Value = Value("customNode")
+
+  //Fragment's component types
+  val FragmentInput: Value = Value("input")
+  val FragmentOutput: Value = Value("output")
+}
