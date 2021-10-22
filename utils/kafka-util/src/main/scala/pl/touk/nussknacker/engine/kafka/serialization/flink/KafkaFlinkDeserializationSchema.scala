3132c9390054acce89c98036e19e985da3c5f94a
dswiecki <dswiecki9@gmail.com>
2021-10-22 14:43:43 +0200

    Review 2

--- utils/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/serialization/flink/KafkaFlinkDeserializationSchema.scala
+++
@@ -1,5 +0,0 @@
-package pl.touk.nussknacker.engine.kafka.serialization.flink
-
-import pl.touk.nussknacker.engine.kafka.serialization.KafkaDeserializationSchema
-
-//trait KafkaDeserializationSchema[T] extends KafkaDeserializationSchema[T]
