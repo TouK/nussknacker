d4d399e76aa4a1006741498e76e625a1365e71ba
Maciej Próchniak <mpr@touk.pl>
2021-10-25 17:50:12 +0200

    Better jar hint (#2387)

--- utils/util/src/test/scala/pl/touk/nussknacker/engine/util/loader/ScalaServiceLoaderSpec.scala
+++ utils/util/src/test/scala/pl/touk/nussknacker/engine/util/loader/ScalaServiceLoaderSpec.scala
@@ -3,6 +3,9 @@ package pl.touk.nussknacker.engine.util.loader
 import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor3}
 import org.scalatest.{FlatSpec, Matchers}
 
+import java.net.{URL, URLClassLoader}
+import java.nio.file.Files
+
 class ScalaServiceLoaderSpec extends FlatSpec with Matchers with TableDrivenPropertyChecks {
 
   behavior of "ScalaServiceLoader.chooseClass"
@@ -11,6 +14,18 @@ class ScalaServiceLoaderSpec extends FlatSpec with Matchers with TableDrivenProp
   case object DummyAuthenticatorFactory extends DummyFactoryTrait
   case object DummyAuthenticatorFactory2 extends DummyFactoryTrait
   case object DummyAuthenticatorFactory3 extends DummyFactoryTrait
+
+  it should "give proper hint" in {
+
+    val tempFile = Files.createTempFile("test", ".jar")
+    val loader = new URLClassLoader(Array(new URL("http://example.com"),
+      tempFile.toUri.toURL, new URL("file:///shouldNotExist.jar")))
+
+    val exception = intercept[IllegalArgumentException] {
+      ProcessConfigCreatorLoader.justOne(loader)
+    }
+    exception.getMessage shouldBe s"ProcessConfigCreator not found. Jar URLs configured: http://example.com, file:${tempFile.toFile.getAbsolutePath}, file:/shouldNotExist.jar, missing files: /shouldNotExist.jar"
+  }
 
   it should "Load class from implementations" in {
     val table: TableFor3[List[DummyFactoryTrait], DummyFactoryTrait, DummyFactoryTrait] = Table(
