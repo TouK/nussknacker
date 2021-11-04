4f558b0aa0b6a4690381db2f4454eaefa55c7d43
JulianWielga <j@touk.pl>
2021-11-04 13:30:25 +0100

    prepare for web-submodules (#2413)

--- ui/client/containers/useMuiTheme.tsx
+++ ui/client/containers/useMuiTheme.tsx
@@ -1,12 +1,11 @@
-import {colors} from "@material-ui/core"
-import {createTheme, Theme as MuiTheme} from "@material-ui/core/styles"
+import {createTheme, Theme as MuiTheme} from "@mui/material/styles"
 import {defaultsDeep} from "lodash"
 import {useMemo} from "react"
 import {useNkTheme} from "./theme"
 
 const paletteTheme = createTheme({
   palette: {
-    type: `dark`,
+    mode: `dark`,
     primary: {
       main: `#93BB6C`,
     },
@@ -26,38 +25,17 @@ const paletteTheme = createTheme({
   },
 })
 
-const overridesTheme = createTheme(
-  {
-    overrides: {
-      MuiTableBody: {
-        root: {
-          backgroundColor: colors.grey["700"],
-        },
-      },
-      MuiTableCell: {
-        stickyHeader: {
-          backgroundColor: colors.grey["800"],
-        },
-        head: {
-          color: paletteTheme.palette.text.secondary,
-        },
-      },
-    },
-  },
-  paletteTheme,
-)
-
 // translate emotion (nk) theme to mui theme
 export function useMuiTheme(): MuiTheme {
   const {theme} = useNkTheme()
 
   const isDark = useMemo(
-    () => theme.themeClass.toLocaleLowerCase().includes("dark"),
+    () => theme.themeClass.toLowerCase().includes("dark"),
     [theme.themeClass],
   )
 
   return useMemo(
-    () => defaultsDeep(isDark ? overridesTheme : {}, theme),
+    () => defaultsDeep(isDark ? paletteTheme : {}, theme),
     [isDark, theme],
   )
 }
