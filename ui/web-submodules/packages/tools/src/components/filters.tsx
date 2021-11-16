a9e5f5245bcb779eec0e650e6b5707595b62b63f
Julian Wielga <j@touk.pl>
2021-11-16 09:45:18 +0100

    cypress tests & cleanup

--- ui/web-submodules/packages/tools/src/components/filters.tsx
+++ ui/web-submodules/packages/tools/src/components/filters.tsx
@@ -24,114 +24,107 @@ export function Filters(props: PropsWithChildren<FiltersProps>): JSX.Element {
 
     const setFilter = useCallback(
         <T extends any>(filter: FilterType<T>) =>
-            onFilterChange((model: FilterModel) => {
-                return [...model.filter((m) => m.id !== filter.id), filter];
-            }),
+            onFilterChange((model: FilterModel) => [...model.filter((m) => m.id !== filter.id), filter].filter(({ value }) => !!value)),
         [onFilterChange],
     );
     const getFilterValue = useCallback((name: string) => model.find(({ id }) => id === name)?.value, [model]);
-    const getStringValue = useCallback((name: string) => getFilterValue(name)?.toString() || "", [getFilterValue]);
 
     return (
-        <Grid
-            component={"form"}
-            noValidate
-            autoComplete="off"
-            container
-            alignItems="center"
-            spacing={2}
-            direction="row"
-            justifyContent="flex-end"
-        >
-            <Grid item xs={12} md={5} lg xl>
-                <TextField
-                    label="Name"
-                    variant="outlined"
-                    fullWidth
-                    value={getStringValue("name")}
-                    onChange={(e) =>
-                        setFilter({
-                            id: "name",
-                            value: e.target.value,
-                            check: (row, value) => !value.length || row["name"]?.includes(value),
-                        })
-                    }
-                />
-            </Grid>
-            <Grid item xs={12} md lg={3} xl>
-                <SelectFilter
-                    label="Type"
-                    options={values["componentType"]}
-                    value={getStringValue("componentType")}
-                    onChange={(value) =>
-                        setFilter({
-                            id: "componentType",
-                            value,
-                            check: (row, value) => {
-                                const filters = value.length ? value.split("|") : [];
-                                return !filters.length || filters.some((f) => row["componentType"]?.includes(f));
-                            },
-                        })
-                    }
-                />
-            </Grid>
-            <Grid item xs={12} md lg={3} xl>
-                <SelectFilter
-                    label="Categories"
-                    options={values["categories"]}
-                    value={getStringValue("categories")}
-                    onChange={(value) =>
-                        setFilter({
-                            id: "categories",
-                            value,
-                            check: (row, value) => {
-                                const filters = value.length ? value.split("|") : [];
-                                return !filters.length || filters.some((f) => row["categories"]?.includes(f));
-                            },
-                        })
-                    }
-                />
-            </Grid>
-            <Grid item>
-                <Box sx={{ display: "flex", flexWrap: "wrap", columnGap: 2 }}>
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                checked={getFilterValue("UNUSED_ONLY") === true}
-                                onChange={(e) => {
-                                    setFilter({
-                                        id: "UNUSED_ONLY",
-                                        check: (row) => row["usageCount"] === 0,
-                                        value: e.target.checked,
-                                    });
-                                    setFilter({
-                                        id: "USED_ONLY",
-                                    });
-                                }}
-                            />
+        <>
+            <Grid
+                component={"form"}
+                noValidate
+                autoComplete="off"
+                container
+                alignItems="center"
+                spacing={2}
+                direction="row"
+                justifyContent="flex-end"
+            >
+                <Grid item xs={12} md={4} lg xl>
+                    <TextField
+                        label={t("table.filter.NAME", "Name")}
+                        variant="outlined"
+                        fullWidth
+                        value={getFilterValue("NAME")}
+                        onChange={(e) =>
+                            setFilter({
+                                id: "NAME",
+                                value: e.target.value.toLowerCase(),
+                                check: (row, value) => !value.length || row["name"]?.toLowerCase().includes(value),
+                            })
                         }
-                        label={t("table.filter.UNUSED_ONLY", "Show unused only")}
                     />
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                checked={getFilterValue("USED_ONLY") === true}
-                                onChange={(e) => {
-                                    setFilter({
-                                        id: "USED_ONLY",
-                                        check: (row) => row["usageCount"] > 0,
-                                        value: e.target.checked,
-                                    });
-                                    setFilter({
-                                        id: "UNUSED_ONLY",
-                                    });
-                                }}
-                            />
+                </Grid>
+                <Grid item xs={12} md lg={3} xl>
+                    <SelectFilter
+                        label={t("table.filter.GROUP", "Type")}
+                        options={values["componentGroupName"]}
+                        value={[].concat(getFilterValue("GROUP")).filter(Boolean)}
+                        onChange={(value) =>
+                            setFilter({
+                                id: "GROUP",
+                                value,
+                                check: (row, value = []) => !value.length || value.some((f) => row["componentGroupName"]?.includes(f)),
+                            })
                         }
-                        label={t("table.filter.USED_ONLY", "Show used only")}
                     />
-                </Box>
+                </Grid>
+                <Grid item xs={12} md lg={3} xl>
+                    <SelectFilter
+                        label={t("table.filter.CATEGORY", "Category")}
+                        options={values["categories"]}
+                        value={[].concat(getFilterValue("CATEGORY")).filter(Boolean)}
+                        onChange={(value) =>
+                            setFilter({
+                                id: "CATEGORY",
+                                value,
+                                check: (row, value = []) => !value.length || value.some((f) => row["categories"]?.includes(f)),
+                            })
+                        }
+                    />
+                </Grid>
+                <Grid item>
+                    <Box sx={{ display: "flex", flexWrap: "wrap", columnGap: 2 }}>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={getFilterValue("UNUSED_ONLY") === true}
+                                    onChange={(e) => {
+                                        setFilter({
+                                            id: "UNUSED_ONLY",
+                                            check: (row) => row["usageCount"] === 0,
+                                            value: e.target.checked,
+                                        });
+                                        setFilter({
+                                            id: "USED_ONLY",
+                                        });
+                                    }}
+                                />
+                            }
+                            label={t("table.filter.UNUSED_ONLY", "Show unused only")}
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={getFilterValue("USED_ONLY") === true}
+                                    onChange={(e) => {
+                                        setFilter({
+                                            id: "USED_ONLY",
+                                            check: (row) => row["usageCount"] > 0,
+                                            value: e.target.checked,
+                                        });
+                                        setFilter({
+                                            id: "UNUSED_ONLY",
+                                        });
+                                    }}
+                                />
+                            }
+                            label={t("table.filter.USED_ONLY", "Show used only")}
+                        />
+                    </Box>
+                </Grid>
             </Grid>
-        </Grid>
+        </>
     );
 }
--- ui/web-submodules/packages/tools/src/components/table.tsx
+++ ui/web-submodules/packages/tools/src/components/table.tsx
@@ -1,4 +1,4 @@
-import { Box, Chip, Paper, useTheme } from "@mui/material";
+import { Box, Chip, Paper } from "@mui/material";
 import { DataGrid, GridColDef, GridRow, GridRowProps } from "@mui/x-data-grid";
 import React, { PropsWithChildren, useCallback, useMemo } from "react";
 import { useTranslation } from "react-i18next";
@@ -33,7 +33,13 @@ export function Table(props: ScenariosTableViewProps): JSX.Element {
     const { data = [], isLoading, filter = [], ...passProps } = props;
     const { t } = useTranslation();
 
-    const getFilterValue = useCallback((field) => filter.find(({ id }) => id === field)?.value.toString() || null, [filter]);
+    const getFilterValue = useCallback(
+        (field: string) => {
+            const value = filter.find(({ id }) => id === field)?.value;
+            return value ? [].concat(value) : [];
+        },
+        [filter],
+    );
 
     const columns = useMemo(
         (): Columns<typeof data> => [
@@ -42,35 +48,26 @@ export function Table(props: ScenariosTableViewProps): JSX.Element {
                 minWidth: 200,
                 headerName: t("table.title.NAME", "Name"),
                 flex: 1,
-                renderCell: ({ value }) => {
-                    const filterValue = getFilterValue("name");
-                    return <Highlighter textToHighlight={value.toString()} searchWords={[filterValue]} highlightTag={Highlight} />;
-                },
-            },
-            {
-                field: "componentType",
-                minWidth: 150,
-                headerName: t("table.title.TYPE", "Type"),
-                renderCell: ({ row, value }) => (
-                    <>
-                        <img style={{ height: "1.5em", marginRight: ".25em" }} src={row.icon} />
-                        {value}
-                    </>
+                renderCell: ({ value }) => (
+                    <Highlighter textToHighlight={value.toString()} searchWords={getFilterValue("NAME")} highlightTag={Highlight} />
                 ),
             },
             {
                 field: "usageCount",
                 type: "number",
                 headerName: t("table.title.USAGE_COUNT", "Usage count"),
-                renderCell: ({ value }) => (
-                    <>
-                        <Box sx={{ fontWeight: value ? "bold" : "light" }}>{value}</Box>
-                    </>
-                ),
+                renderCell: ({ value }) => <Box sx={{ fontWeight: value ? "bold" : "light" }}>{value}</Box>,
             },
             {
                 field: "componentGroupName",
-                headerName: t("table.title.GROUP_NAME", "Group"),
+                minWidth: 150,
+                headerName: t("table.title.TYPE", "Type"),
+                renderCell: ({ row, value }) => (
+                    <>
+                        <img title={row.componentType} style={{ height: "1.5em", marginRight: ".25em" }} src={row.icon} />
+                        {value}
+                    </>
+                ),
             },
             {
                 field: "categories",
@@ -78,34 +75,31 @@ export function Table(props: ScenariosTableViewProps): JSX.Element {
                 minWidth: 150,
                 flex: 2,
                 sortable: false,
-                renderCell: ({ row }) => {
-                    const filterValue = getFilterValue("categories");
-                    return (
-                        <Box
-                            sx={{
-                                flex: 1,
-                                overflow: "hidden",
-                                display: "flex",
-                                gap: 0.5,
-                                position: "relative",
-                                maskImage: "linear-gradient(to right, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0))",
-                            }}
-                        >
-                            {row.categories.map((name) => {
-                                const isSelected = filterValue?.includes(name);
-                                return (
-                                    <Chip
-                                        key={name}
-                                        label={name}
-                                        size="small"
-                                        variant={isSelected ? "outlined" : "filled"}
-                                        color={isSelected ? "primary" : "default"}
-                                    />
-                                );
-                            })}
-                        </Box>
-                    );
-                },
+                renderCell: ({ row }) => (
+                    <Box
+                        sx={{
+                            flex: 1,
+                            overflow: "hidden",
+                            display: "flex",
+                            gap: 0.5,
+                            position: "relative",
+                            maskImage: "linear-gradient(to right, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0))",
+                        }}
+                    >
+                        {row.categories.map((name) => {
+                            const isSelected = getFilterValue("CATEGORY").includes(name);
+                            return (
+                                <Chip
+                                    key={name}
+                                    label={name}
+                                    size="small"
+                                    variant={isSelected ? "outlined" : "filled"}
+                                    color={isSelected ? "primary" : "default"}
+                                />
+                            );
+                        })}
+                    </Box>
+                ),
             },
         ],
         [getFilterValue, t],
