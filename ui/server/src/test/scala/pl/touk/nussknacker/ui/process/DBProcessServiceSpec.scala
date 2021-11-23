0e287d61bca95f46e8056c4b337724169a2b7214
Łukasz Ciołecki <lcl@touk.pl>
2021-11-23 07:52:32 +0100

    Add tests

--- ui/server/src/test/scala/pl/touk/nussknacker/ui/process/DBProcessServiceSpec.scala
+++ ui/server/src/test/scala/pl/touk/nussknacker/ui/process/DBProcessServiceSpec.scala
@@ -2,9 +2,15 @@ package pl.touk.nussknacker.ui.process
 
 import org.scalatest.{FlatSpec, Matchers}
 import pl.touk.nussknacker.engine.api.deployment.ProcessActionType.Deploy
+import pl.touk.nussknacker.engine.api.{FragmentSpecificData, MetaData}
+import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
+import pl.touk.nussknacker.engine.graph.exceptionhandler.ExceptionHandlerRef
+import pl.touk.nussknacker.restmodel.processdetails.{BaseProcessDetails, ProcessShapeFetchStrategy}
 import pl.touk.nussknacker.test.PatientScalaFutures
-import pl.touk.nussknacker.ui.api.helpers.TestFactory.{StubSubprocessRepository, processResolving}
+import pl.touk.nussknacker.ui.api.helpers.TestFactory.StubSubprocessRepository
 import pl.touk.nussknacker.ui.api.helpers.{MockFetchingProcessRepository, TestFactory}
+import pl.touk.nussknacker.ui.process.ProcessCategoryService.Category
+import pl.touk.nussknacker.ui.process.subprocess.SubprocessDetails
 import pl.touk.nussknacker.ui.security.api.LoggedUser
 import pl.touk.nussknacker.ui.util.ConfigWithScalaVersion
 
@@ -28,27 +34,23 @@ class DBProcessServiceSpec extends FlatSpec with Matchers with PatientScalaFutur
   private val testSubProcess = createBasicProcess("testSubProcess", isSubprocess = true, isArchived = false, category = TESTCAT)
   private val reqRespArchivedSubProcess = createBasicProcess("reqRespArchivedSubProcess", isSubprocess = true, isArchived = true, category = ReqRes)
 
-  private val processes: List[ProcessWithoutJson] = List(
-    category1Process, category2ArchivedProcess, testSubProcess, reqRespArchivedSubProcess,
+  private val processes: List[BaseProcessDetails[Unit]] = List(
+    category1Process, category2ArchivedProcess, testSubProcess, reqRespArchivedSubProcess
+  )
+
+  private val subprocessCategory1 = createSubprocess("subprocessCategory1", Category1)
+  private val subprocessCategory2 = createSubprocess("subprocessCategory2", Category2)
+  private val subprocessTest = createSubprocess("subprocessTest", TESTCAT)
+  private val subprocessReqResp = createSubprocess("subprocessReqResp", ReqRes)
+
+  private val subprocesses: Set[SubprocessDetails] = Set(
+    subprocessCategory1, subprocessCategory2, subprocessTest, subprocessReqResp
   )
 
   private val processCategoryService = new ConfigProcessCategoryService(ConfigWithScalaVersion.config)
-  private val newProcessPreparer = TestFactory.createNewProcessPreparer()
-
-  private val DefaultRequestTimeLimit = Duration.ofMinutes(1)
-  private val dummyManagerActor = TestFactory.newDummyManagerActor()
-  private val dummyWriteProcessRepository = TestFactory.newDummyWriteProcessRepository()
-  private val dummyActionRepository = TestFactory.newDummyActionRepository()
-  private val dummyRepositoryManager = TestFactory.newDummyRepositoryManager()
-  private val dummySubprocessRepository = StubSubprocessRepository(Set.empty)
 
   it should "return user processes" in {
-    val mockRepository = MockFetchingProcessRepository(processes)
-
-    val dBProcessService = new DBProcessService(
-      dummyManagerActor, DefaultRequestTimeLimit, newProcessPreparer, processCategoryService, processResolving,
-      dummyRepositoryManager, mockRepository, dummyActionRepository, dummyWriteProcessRepository, dummySubprocessRepository,
-    )
+    val dBProcessService = createDbProcessService(processes)
 
     val testingData = Table(
       ("user", "expected"),
@@ -63,4 +65,41 @@ class DBProcessServiceSpec extends FlatSpec with Matchers with PatientScalaFutur
       result shouldBe expected
     }
   }
+
+  it should "return user subprocesses" in {
+    val dBProcessService = createDbProcessService[Unit](Nil, subprocesses)
+
+    val testingData = Table(
+      ("user", "subprocesses"),
+      (adminUser, subprocesses),
+      (categoriesUser, Set(subprocessCategory1, subprocessCategory2)),
+      (testUser, Set(subprocessTest)),
+      (testReqRespUser, Set(subprocessTest, subprocessReqResp)),
+    )
+
+    forAll(testingData) { (user: LoggedUser, expected: Set[SubprocessDetails] ) =>
+      val result = dBProcessService.getSubProcesses(user)
+      result shouldBe expected
+    }
+  }
+
+  private def createSubprocess(name: String, category: Category) = {
+    val metaData = MetaData(name, FragmentSpecificData())
+    val exceptionHandler = ExceptionHandlerRef(List())
+    SubprocessDetails(CanonicalProcess(metaData, exceptionHandler, Nil, Nil), category)
+  }
+
+  private def createDbProcessService[T: ProcessShapeFetchStrategy](processes: List[BaseProcessDetails[T]] = Nil, subprocesses: Set[SubprocessDetails] = Set.empty): DBProcessService =
+    new DBProcessService(
+      managerActor = TestFactory.newDummyManagerActor(),
+      requestTimeLimit = Duration.ofMinutes(1),
+      newProcessPreparer = TestFactory.createNewProcessPreparer(),
+      processCategoryService = processCategoryService,
+      processResolving = TestFactory.processResolving,
+      repositoryManager = TestFactory.newDummyRepositoryManager(),
+      fetchingProcessRepository = MockFetchingProcessRepository(processes),
+      processActionRepository = TestFactory.newDummyActionRepository(),
+      processRepository = TestFactory.newDummyWriteProcessRepository(),
+      subprocessRepository = new StubSubprocessRepository(subprocesses)
+    )
 }
