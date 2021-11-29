aa1e13b401242095f358125031f7c3ce4fce487c
JulianWielga <j@touk.pl>
2021-11-29 15:37:57 +0100

    Component filters in query (#2556)
    * move filters to query string
    
    * create pr for missing snapshots
    
    * Updated snapshots (#2559)
    
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>
    
    * change category filter (#2557)
    
    * change category filter
    
    * set category filter by category click (#2558)
    
    * create pr for missing snapshots
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>

--- ui/submodules/packages/components/src/components/table.tsx
+++ ui/submodules/packages/components/src/components/table.tsx
@@ -1,13 +1,14 @@
 import { Box, Paper } from "@mui/material";
 import { DataGrid, GridActionsColDef, GridColDef } from "@mui/x-data-grid";
-import React, { useCallback, useMemo } from "react";
+import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { FilterModel } from "./filters";
 import { ScenariosTableProps } from "./listWithFilters";
 import type { ComponentType } from "nussknackerUi/HttpService";
 import { NameCell } from "./cellRenderers/nameCell";
 import { UsageCountCell } from "./cellRenderers/usageCountCell";
 import { CategoriesCell } from "./cellRenderers/categoriesCell";
+import { FILTER_RULES } from "./filters/filterRules";
+import { useFilterContext } from "./filters/filtersContext";
 
 type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
@@ -21,20 +22,13 @@ type Columns<R extends Array<unknown>> = Array<
 export interface ScenariosTableViewProps extends ScenariosTableProps {
     data: ComponentType[];
     isLoading?: boolean;
-    filter?: FilterModel;
 }
 
 export function Table(props: ScenariosTableViewProps): JSX.Element {
-    const { data = [], isLoading, filter = [], ...passProps } = props;
-    const { t } = useTranslation();
+    const { data = [], isLoading, ...passProps } = props;
+    const { model, getFilter } = useFilterContext();
 
-    const getFilterValue = useCallback(
-        (field: string) => {
-            const value = filter.find(({ id }) => id === field)?.value;
-            return value ? [].concat(value) : [];
-        },
-        [filter],
-    );
+    const { t } = useTranslation();
 
     const columns = useMemo(
         (): Columns<typeof data> => [
@@ -43,7 +37,7 @@ export function Table(props: ScenariosTableViewProps): JSX.Element {
                 minWidth: 200,
                 headerName: t("table.title.NAME", "Name"),
                 flex: 1,
-                renderCell: (props) => <NameCell {...props} filterValue={getFilterValue("NAME")} />,
+                renderCell: (props) => <NameCell {...props} filterValue={getFilter("NAME", true)} />,
                 sortComparator: (v1, v2) => v1.toString().toLowerCase().localeCompare(v2.toString().toLowerCase()),
             },
             {
@@ -64,19 +58,20 @@ export function Table(props: ScenariosTableViewProps): JSX.Element {
                 minWidth: 150,
                 flex: 2,
                 sortable: false,
-                renderCell: (props) => <CategoriesCell {...props} filterValue={getFilterValue("CATEGORY")} />,
+                renderCell: (props) => <CategoriesCell {...props} filterValue={getFilter("CATEGORY", true)} />,
             },
         ],
-        [getFilterValue, t],
+        [getFilter, t],
     );
 
     const filtered = useMemo(() => {
         return data.filter((row) => {
-            return filter.every(({ check, value }) => {
+            return Object.entries(model).every(([id, value]) => {
+                const check = FILTER_RULES[id];
                 return value && check ? check(row, value) : true;
             });
         });
-    }, [data, filter]);
+    }, [data, model]);
 
     return (
         <Box flexDirection="column" display="flex" width="100%" flex={1}>
