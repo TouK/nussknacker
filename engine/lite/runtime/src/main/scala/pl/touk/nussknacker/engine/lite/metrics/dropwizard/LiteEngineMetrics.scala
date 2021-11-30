1ed26ee11d4205b2ac1ef49d023ede5a3790b865
Maciek Próchniak <mpr@touk.pl>
2021-11-30 13:22:25 +0100

    [Refactor] Lite metrics configuration separated from InfluxDB

--- engine/lite/runtime/src/main/scala/pl/touk/nussknacker/engine/lite/metrics/dropwizard/LiteEngineMetrics.scala
+++ engine/lite/runtime/src/main/scala/pl/touk/nussknacker/engine/lite/metrics/dropwizard/LiteEngineMetrics.scala
@@ -2,25 +2,43 @@ package pl.touk.nussknacker.engine.lite.metrics.dropwizard
 
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import io.dropwizard.metrics5.MetricRegistry
+import io.dropwizard.metrics5.{MetricName, MetricRegistry}
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader.arbitraryTypeValueReader
 import pl.touk.nussknacker.engine.lite.metrics.dropwizard.influxdb.LiteEngineInfluxDbReporter
 import pl.touk.nussknacker.engine.util.loader.ScalaServiceLoader
 
+import scala.jdk.CollectionConverters.mapAsJavaMapConverter
 import scala.util.control.NonFatal
 
 object LiteEngineMetrics extends LazyLogging {
 
+  val metricsConfigPath = "metrics"
+
+  case class CommonMetricConfig(prefix: Option[String],
+                                host: String,
+                                environment: String,
+                                additionalTags: Map[String, String] = Map.empty)
+
   def prepareRegistry(config: Config): MetricRegistry = {
     val metricRegistry = new MetricRegistry
+    def prefix() = preparePrefix(config.as[CommonMetricConfig](metricsConfigPath))
     val metricReporters = loadMetricsReporters()
     if (metricReporters.nonEmpty) {
       metricReporters.foreach { reporter =>
-        reporter.createAndRunReporter(metricRegistry, config)
+        reporter.createAndRunReporter(metricRegistry, prefix(), config.getConfig(metricsConfigPath))
       }
     } else {
-      LiteEngineInfluxDbReporter.createAndRunReporterIfConfigured(metricRegistry, config)
+      LiteEngineInfluxDbReporter.createAndRunReporterIfConfigured(metricRegistry, prefix, config)
     }
     metricRegistry
+  }
+
+  private def preparePrefix(conf: CommonMetricConfig): MetricName = {
+    conf.prefix.map(MetricName.build(_)).getOrElse(MetricName.empty())
+      .tagged("host", conf.host)
+      .tagged("env", conf.environment)
+      .tagged(conf.additionalTags.asJava)
   }
 
   private def loadMetricsReporters(): List[MetricsReporter] = {
