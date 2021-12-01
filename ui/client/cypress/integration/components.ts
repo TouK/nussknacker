5a5a71924f91a11802a7caee2d1478ad57ccc882
Maciek Próchniak <mpr@touk.pl>
2021-12-01 16:20:55 +0100

    cypress...

--- ui/client/cypress/integration/components.ts
+++ ui/client/cypress/integration/components.ts
@@ -1,7 +1,7 @@
 describe("Components list", () => {
   const seed = "components"
 
-  const totalComponents = 90
+  const totalComponents = 100
   const totalComponentsNamedSource = 9
   const totalCategories = 6
 
