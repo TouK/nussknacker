cac74e987de5610aa2e3c2154a3376fd37527bd1
Julian Wielga <j@touk.pl>
2021-12-02 01:19:40 +0100

    better auth errors

--- ui/client/containers/errors/InitializeError.tsx
+++ ui/client/containers/errors/InitializeError.tsx
@@ -1,48 +1,71 @@
-import React, {useCallback} from "react"
+import React, {useMemo} from "react"
 import {useTranslation} from "react-i18next"
+import {AuthErrorCodes} from "../Auth/AuthErrorCodes"
+import {InitErrorComponentProps} from "../Auth/InitErrorComponent"
 
-export type ErrorProps = {
-  message?: string,
-  description?: string,
-  buttonLabel?: string,
-  showButton?: boolean,
-  buttonOnClick?: () => void,
+interface ErrorProps {
+  message: string,
+  description: string,
+  button?: string,
 }
 
-function InitializeError(props: ErrorProps): JSX.Element {
-  const defaultOnClick = useCallback(() => window.location.reload(), [])
+export function InitializeError({error, retry}: InitErrorComponentProps): JSX.Element {
   const {t} = useTranslation()
-  const title = t("InitializeError.title", "Oops!")
-  const {
-    message = t("InitializeError.message", "Application Unexpected Error"),
-    description = t(
-      "InitializeError.description",
-      "An unexpected error seems to have occurred. Please contact with system administrators.",
-    ),
-    buttonLabel = t("InitializeError.buttonLabel", "Try to refresh page"),
-    buttonOnClick = defaultOnClick,
-    showButton = true,
-  } = props
+  const errorProps = useMemo<ErrorProps>(() => {
+    switch (error) {
+      case AuthErrorCodes.HTTP_UNAUTHORIZED_CODE:
+        return {
+          message: t("auth.StrategyInitializer.errors.401.message", "Unauthorized Error"),
+          description: t(
+            "auth.StrategyInitializer.errors.401.description",
+            "It seems you are not authenticated... Why not try to authenticate again?",
+          ),
+          button: t("auth.StrategyInitializer.errors.401.buttonLabel", "Try authenticate again"),
+        }
+      case AuthErrorCodes.HTTP_TIMEOUT_CODE:
+        return {
+          message: t("auth.StrategyInitializer.errors.504.message", "504 Gateway Timeout Error"),
+          description: t(
+            "auth.StrategyInitializer.errors.504.description",
+            "It seems server has some problems... Why not to try refreshing your page? Or you can contact with system administrators.",
+          ),
+          button: t("InitializeError.buttonLabel", "Try to refresh page"),
+        }
+      case AuthErrorCodes.HTTP_APPLICATION_CODE:
+        return {
+          message: t("InitializeError.message", "Application Unexpected Error"),
+          description: t(
+            "InitializeError.description",
+            "An unexpected error seems to have occurred. Please contact with system administrators.",
+          ),
+        }
+      case AuthErrorCodes.ACCESS_TOKEN_CODE:
+        return {
+          message: t("auth.StrategyInitializer.errors.accessToken.message", "Authentication Error"),
+          description: t(
+            "auth.StrategyInitializer.errors.1024.description",
+            "It seems application has some problem with authentication. Please contact with system administrators.",
+          ),
+          button: t("auth.StrategyInitializer.errors.1024.buttonLabel", "Go to authentication page"),
+        }
+    }
+  }, [error, t])
+
   return (
     <div className="error-template center-block">
-      <h1>{title}</h1>
-      <h2>{message}</h2>
+      <h1>{t("InitializeError.title", "Oops!")}</h1>
+      <h2>{errorProps.message}</h2>
       <div className="error-details">
-        <br/>
-        <p>{description}</p>
-        {showButton ?
-          (
-            <div
-              className="big-blue-button"
-              role="button"
-              style={{margin: "0 auto"}}
-              onClick={buttonOnClick}
-            >
-              {buttonLabel}
-            </div>
-          ) :
-          null
-        }
+        <p>{errorProps.description}</p>
+        {errorProps.button && (
+          <button
+            className="big-blue-button"
+            style={{border: "none"}}
+            onClick={() => retry()}
+          >
+            {errorProps.button}
+          </button>
+        )}
       </div>
     </div>
   )
