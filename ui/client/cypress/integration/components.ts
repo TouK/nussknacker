ffd293d8f13a39cd24d6363c87cde535f0c29250
JulianWielga <j@touk.pl>
2021-12-03 08:46:31 +0100

    replace text matching with screenshots

--- ui/client/cypress/integration/components.ts
+++ ui/client/cypress/integration/components.ts
@@ -1,12 +1,8 @@
 describe("Components list", () => {
   const seed = "components"
 
-  const baseGroupComponents = 5
-  const totalGroups = 7
-
-  function components(number: number) {
-    return new RegExp(`of ${number}$`)
-  }
+  const totalComponentsNamedSource = 9
+  const totalCategories = 6
 
   before(() => {
     cy.deleteAllTestProcesses({filter: seed, force: true})
@@ -22,19 +18,14 @@ describe("Components list", () => {
     cy.visit("/customtabs/components")
   })
 
-  // We filter by Default category in almost all test cases because in other categories there are sandbox components which
-  // will often change and this changes won't have influence on our official distribution
-
   it("should display component", () => {
-    filterByDefaultCategory()
     cy.contains(/^name$/i).should("be.visible")
     cy.contains(/^categories$/i).should("be.visible")
-    cy.contains(/^filter$/).should("be.visible")
+    cy.contains(/^accountService$/).should("be.visible")
     cy.get("#app-container").toMatchImageSnapshot()
   })
 
   it("should have dynamic page size", () => {
-    filterByDefaultCategory()
     cy.contains(/^1-12 of \d+$/i).should("be.visible")
     cy.get("[role=row]").should("have.lengthOf", 13)
     cy.viewport(1400, 500)
@@ -43,9 +34,8 @@ describe("Components list", () => {
   })
 
   it("should allow filtering by name", () => {
-    filterByDefaultCategory()
-    cy.contains(/^name$/i).parent().find("input").type("filt")
-    cy.contains(/^filter$/i).should("be.visible")
+    cy.contains(/^name$/i).parent().find("input").type("response")
+    cy.contains(/^response-sink$/i).should("be.visible")
     cy.get("[role=row]").should("have.lengthOf", 2)
     cy.contains(/^name$/i).parent().find("input").type("-dummy")
     cy.get("[role=row]").should("have.lengthOf", 1)
@@ -53,35 +43,48 @@ describe("Components list", () => {
   })
 
   it("should allow filtering by group", () => {
-    filterByDefaultCategory()
     cy.contains(/^group$/i).parent().as("select")
     cy.get("[role=row]").should("have.length.greaterThan", 11)
     cy.get("@select").click()
     cy.get("[role=option]").as("options")
-    cy.get("@options").should("have.lengthOf", totalGroups)
+    cy.get("@options").should("have.lengthOf", 7)
     cy.get("@options").contains(/^base/i).click()
-    cy.get("@options").contains(/^source/i).click()
+    cy.get("@options").contains(/^types/i).click()
     cy.matchQuery()
     cy.get("[role=row]").should("have.lengthOf", 11)
     cy.get("body").click()
     cy.get("@select").contains(/^base/).dblclick()
-    cy.get("[role=row]").should("have.lengthOf", baseGroupComponents + 1).contains(/^filter/).should("be.visible")
+    cy.get("[role=row]").should("have.lengthOf", 6).contains(/^filter/).should("be.visible")
     cy.get("@select").find("[data-testid=CancelIcon]").click()
     cy.get("[role=row]").should("have.length.greaterThan", 2)
   })
 
+  it("should allow filtering by category", () => {
+    cy.contains(/^category/i).parent().as("select")
+    cy.contains(/^name$/i).parent().find("input").type("source")
+    cy.get("[role=row]").should("have.lengthOf", totalComponentsNamedSource + 1)
+    cy.get("@select").click()
+    cy.get("[role=option]").should("have.lengthOf", totalCategories).as("options")
+    cy.get("@options").contains(/^demo/i).click()
+    cy.get("[role=row]").should("have.lengthOf", 2)
+    cy.get("[role=row]").filter(`:contains("DemoFeatures")`).should("have.lengthOf", 1)
+    cy.get("@options").contains(/^server/i).click()
+    cy.get("[role=row]").should("have.lengthOf", 1)
+    cy.matchQuery()
+  })
+
   it("should allow filtering by usage", () => {
-    filterByDefaultCategory()
+    cy.contains(/\d+ of \d+/).should("be.visible").toMatchImageSnapshot()
     cy.contains(/^Show used only$/).click()
     cy.matchQuery()
-    cy.get("#app-container").toMatchImageSnapshot()
+    cy.contains(/\d+ of \d+/).should("be.visible").toMatchImageSnapshot()
     cy.contains(/^Show unused only$/).click()
     cy.matchQuery()
-    cy.get("#app-container").toMatchImageSnapshot()
+    cy.contains(/\d+ of \d+/).should("be.visible").toMatchImageSnapshot()
   })
 
   it("should apply filters from query", () => {
-    cy.visit("/customtabs/components?NAME=split&GROUP=base&CATEGORY=Default&UNUSED_ONLY=true")
+    cy.visit("/customtabs/components?NAME=split&GROUP=base&CATEGORY=Default&CATEGORY=DemoFeatures&UNUSED_ONLY=true")
     cy.contains(/^name$/i).should("be.visible")
     cy.get("[role=row]").should("have.length.above", 1)
     cy.get("[role=row]").contains(/^Default$/).should("be.visible")
@@ -89,31 +92,12 @@ describe("Components list", () => {
   })
 
   it("should apply category filters by row click", () => {
-    filterByBaseGroup()
     cy.contains(/^category$/i).should("be.visible")
     cy.get("[role=row]").should("have.length.above", 1)
+    cy.get("[role=row]").contains(/^Default$/).click()
+    cy.get("[role=row]").contains(/^Category1$/).click()
+    cy.matchQuery()
     cy.get("[role=row]").contains(/^Default$/).click()
     cy.matchQuery()
-    cy.get("#app-container").toMatchImageSnapshot()
   })
-
-  function filterByDefaultCategory() {
-    // we filter by Default category to make sure that snapshots won't be made on our sandbox components li
-    cy.contains(/^category$/i).should("be.visible")
-    cy.get("[role=row]").should("have.length.above", 1)
-    cy.get("[role=row]").contains(/^Default$/).click()
-  }
-
-  function filterByBaseGroup() {
-    cy.contains(/^group$/i).parent().as("select")
-    cy.get("[role=row]").should("have.length.greaterThan", 11)
-    cy.get("@select").click()
-    cy.get("[role=option]").as("options")
-    cy.get("@options").should("have.lengthOf", totalGroups)
-    cy.get("@options").contains(/^base/i).click()
-    cy.get("[role=row]").should("have.lengthOf", baseGroupComponents + 1)
-    cy.get("body").click()
-  }
-
-
 })
