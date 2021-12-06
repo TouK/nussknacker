17a08e355b5b0fe7d6aa4b4cb4ecec19ff20cf84
Arek Burdach <arek.burdach@gmail.com>
2021-12-06 17:40:11 +0100

    static parameters - passing OutputVariableNameValue

--- api/src/main/scala/pl/touk/nussknacker/engine/api/context/transformation/WithLegacyStaticParameters.scala
+++ api/src/main/scala/pl/touk/nussknacker/engine/api/context/transformation/WithLegacyStaticParameters.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.engine.api.context.transformation
 
 import pl.touk.nussknacker.engine.api.context.ProcessCompilationError.NodeId
 import pl.touk.nussknacker.engine.api.context.ValidationContext
-import pl.touk.nussknacker.engine.api.definition.Parameter
+import pl.touk.nussknacker.engine.api.definition.{OutputVariableNameDependency, Parameter}
 
 // TODO: Deprecated API - remove it after clean up
 // The purpose of this trait is to give ability to return static parameters next to existing dynamic one.
@@ -15,7 +15,10 @@ trait WithLegacyStaticParameters { self: GenericNodeTransformation[_] =>
 
 trait SingleInputLegacyStaticParametersBasedOnDynamicParameters extends WithLegacyStaticParameters { self: SingleInputGenericNodeTransformation[_] =>
   override def staticParameters: List[Parameter] = {
-    self.contextTransformation(ValidationContext.empty, List.empty)(NodeId("fakeId"))(TransformationStep(List.empty, None))  match {
+    val nodeDependencyValues = nodeDependencies.collect {
+      case OutputVariableNameDependency => OutputVariableNameValue("fakeOutputVariable")
+    }
+    self.contextTransformation(ValidationContext.empty, nodeDependencyValues)(NodeId("fakeNodeId"))(TransformationStep(List.empty, None))  match {
       case NextParameters(params, _, _) =>
         params
       case FinalResults(_, _, _) =>
@@ -26,7 +29,10 @@ trait SingleInputLegacyStaticParametersBasedOnDynamicParameters extends WithLega
 
 trait  JoinLegacyStaticParametersBasedOnDynamicParameters extends WithLegacyStaticParameters { self: JoinGenericNodeTransformation[_] =>
   override def staticParameters: List[Parameter] = {
-    self.contextTransformation(Map.empty, List.empty)(NodeId("fakeId"))(TransformationStep(List.empty, None))  match {
+    val nodeDependencyValues = nodeDependencies.collect {
+      case OutputVariableNameDependency => OutputVariableNameValue("fakeOutputVariable")
+    }
+    self.contextTransformation(Map.empty, nodeDependencyValues)(NodeId("fakeNodeId"))(TransformationStep(List.empty, None))  match {
       case NextParameters(params, _, _) =>
         params
       case FinalResults(_, _, _) =>
