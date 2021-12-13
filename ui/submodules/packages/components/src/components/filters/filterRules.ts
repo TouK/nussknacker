35e7d1735b0e1408c73bbf9f32b717c996272bc1
Julian Wielga <j@touk.pl>
2021-12-13 12:39:19 +0100

    fixed name filter

--- ui/submodules/packages/components/src/components/filters/filterRules.ts
+++ ui/submodules/packages/components/src/components/filters/filterRules.ts
@@ -20,7 +20,7 @@ export interface FiltersModel {
 }
 
 export const FILTER_RULES: FilterRules<ComponentType> = {
-    TEXT: (row, value) => !value.length || row["name"]?.toLowerCase().includes(value),
+    NAME: (row, value) => !value.length || row["name"]?.toLowerCase().includes(value),
     GROUP: (row, value = []) => !value.length || [].concat(value).some((f) => row["componentGroupName"]?.includes(f)),
     CATEGORY: (row, value = []) => !value.length || [].concat(value).every((f) => row["categories"]?.includes(f)),
     UNUSED_ONLY: (row, value) => value && row["usageCount"] === 0,
