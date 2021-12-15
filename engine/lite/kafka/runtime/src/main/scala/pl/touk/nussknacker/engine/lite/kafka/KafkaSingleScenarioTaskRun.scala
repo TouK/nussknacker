0b4275628a0e75e1414cddb46789374ddbacccf4
Maciej Brzeziński <mab816813@gmail.com>
2021-12-15 15:18:02 +0100

    cleanup

--- engine/lite/kafka/runtime/src/main/scala/pl/touk/nussknacker/engine/lite/kafka/KafkaSingleScenarioTaskRun.scala
+++ engine/lite/kafka/runtime/src/main/scala/pl/touk/nussknacker/engine/lite/kafka/KafkaSingleScenarioTaskRun.scala
@@ -126,7 +126,7 @@ class KafkaSingleScenarioTaskRun(taskId: String,
 
   private def sendOutputToKafka(output: ResultType[interpreterTypes.EndResult[ProducerRecord[Array[Byte], Array[Byte]]]]): Future[_] = {
 
-    val resultsWithTimestamp = output.value.map(endResult => {
+    val resultsWithContextTimestamp = output.value.map(endResult => {
       val contextEventTimestamp = endResult.context.variables(VariableConstants.EventTimestampVariableName).asInstanceOf[Long]
       val producerRecord = endResult.result
       new ProducerRecord[Array[Byte], Array[Byte]](
@@ -139,7 +139,7 @@ class KafkaSingleScenarioTaskRun(taskId: String,
     })
 
     val errors = output.written.map(serializeError)
-    (resultsWithTimestamp ++ errors).map(KafkaUtils.sendToKafka(_)(producer)).sequence
+    (resultsWithContextTimestamp ++ errors).map(KafkaUtils.sendToKafka(_)(producer)).sequence
   }
 
   //TODO: test behaviour on transient exceptions
