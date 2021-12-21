67f87f0c46c42b84ed3f8a5bb6a4c78b7f2ea446
Damian Święcki <dswiecki9@gmail.com>
2021-12-21 14:16:46 +0100

    Refactor lite union (#2646)
    * Refactor lite union
    
    * Evaluate interpreter once per branch
    
    Co-authored-by: Damian Święcki <dsw@touk.pl>

--- engine/lite/components/base/src/main/scala/pl/touk/nussknacker/engine/lite/components/Union.scala
+++ engine/lite/components/base/src/main/scala/pl/touk/nussknacker/engine/lite/components/Union.scala
@@ -1,12 +1,14 @@
 package pl.touk.nussknacker.engine.lite.components
 
 import cats.Monad
-import cats.data.Validated.Valid
+import cats.data.Validated
+import pl.touk.nussknacker.engine.api.context.ProcessCompilationError.{CannotCreateObjectError, NodeId}
 import pl.touk.nussknacker.engine.api.context.{ContextTransformation, JoinContextTransformation, ValidationContext}
-import pl.touk.nussknacker.engine.api.typed.typing.Typed
-import pl.touk.nussknacker.engine.api.{CustomStreamTransformer, MethodToInvoke}
+import pl.touk.nussknacker.engine.api.typed.typing
+import pl.touk.nussknacker.engine.api.typed.typing.TypingResult
+import pl.touk.nussknacker.engine.api.{BranchParamName, Context, CustomStreamTransformer, LazyParameter, MethodToInvoke, OutputVariableName}
 import pl.touk.nussknacker.engine.lite.api.commonTypes.{DataBatch, ResultType}
-import pl.touk.nussknacker.engine.lite.api.customComponentTypes.{CustomComponentContext, LiteJoinCustomComponent, JoinDataBatch}
+import pl.touk.nussknacker.engine.lite.api.customComponentTypes.{CustomComponentContext, JoinDataBatch, LiteJoinCustomComponent}
 
 import scala.language.higherKinds
 
@@ -14,26 +16,33 @@ import scala.language.higherKinds
 object Union extends CustomStreamTransformer {
 
   @MethodToInvoke
-  def execute(): JoinContextTransformation = {
+  def execute(@BranchParamName("Output expression") outputExpressionByBranchId: Map[String, LazyParameter[AnyRef]],
+              @OutputVariableName variableName: String)(implicit nodeId: NodeId): JoinContextTransformation = {
     ContextTransformation
       .join
       .definedBy { contexts =>
-        Valid(computeIntersection(contexts))
+        val branchReturnTypes: Iterable[typing.TypingResult] = outputExpressionByBranchId.values.map(_.returnType)
+        if (branchReturnTypes.toSet.size == 1) unionValidationContext(variableName, contexts, branchReturnTypes.head)
+        else Validated.invalidNel(CannotCreateObjectError("All branch values must be of the same type", nodeId.id))
       }
       .implementedBy(new LiteJoinCustomComponent {
-        override def createTransformation[F[_] : Monad, Result](continuation: DataBatch => F[ResultType[Result]], context: CustomComponentContext[F]): JoinDataBatch => F[ResultType[Result]] =
-          (inputs: JoinDataBatch) => continuation(DataBatch(inputs.value.map(_._2)))
+        override def createTransformation[F[_] : Monad, Result](continuation: DataBatch => F[ResultType[Result]], context: CustomComponentContext[F]): JoinDataBatch => F[ResultType[Result]] = {
+          val interpreterByBranchId = outputExpressionByBranchId.mapValues(context.interpreter.syncInterpretationFunction)
+          (inputs: JoinDataBatch) => {
+            val contextWithNewValue = inputs.value.map {
+              case (branchId, branchContext) =>
+                val branchNewValue = interpreterByBranchId(branchId.value)(branchContext)
+                branchContext.withVariable(variableName, branchNewValue)
+            }
+            continuation(DataBatch(contextWithNewValue))
+          }
+        }
       })
   }
 
-  private def computeIntersection(contexts: Map[String, ValidationContext]): ValidationContext = {
-    contexts.values.toList match {
-      case Nil => ValidationContext.empty
-      case one :: rest =>
-        val commonVariables = one.variables
-          .filter(v => rest.forall(_.contains(v._1)))
-          .map(v => v._1 -> Typed(v._2 :: rest.map(_ (v._1)): _*))
-        one.copy(localVariables = commonVariables)
+  private def unionValidationContext(variableName: String, contexts: Map[String, ValidationContext], branchReturnType: TypingResult)(implicit nodeId: NodeId) = {
+    ContextTransformation.findUniqueParentContext(contexts).map { parent =>
+      ValidationContext(Map(variableName -> branchReturnType), Map.empty, parent)
     }
   }
 
