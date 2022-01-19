1e230b8f33e0bf57fdeb64ce940a4d6d882db60a
Maciej Próchniak <mpr@touk.pl>
2022-01-19 20:59:39 +0100

    SQL component classloader fix (#2757)

--- components/sql/src/main/scala/pl/touk/nussknacker/sql/db/schema/MetaDataProviderFactory.scala
+++ components/sql/src/main/scala/pl/touk/nussknacker/sql/db/schema/MetaDataProviderFactory.scala
@@ -1,10 +1,13 @@
 package pl.touk.nussknacker.sql.db.schema
 
+import com.zaxxer.hikari.util.DriverDataSource
+import pl.touk.nussknacker.engine.util.ThreadUtils
 import pl.touk.nussknacker.sql.db.ignite.IgniteMetaDataProvider
 import pl.touk.nussknacker.sql.db.pool.DBPoolConfig
 import pl.touk.nussknacker.sql.db.schema.MetaDataProviderFactory.igniteDriverPrefix
 
-import java.sql.{Connection, DriverManager}
+import java.sql.Connection
+import java.util.Properties
 
 object MetaDataProviderFactory {
   private val igniteDriverPrefix = "org.apache.ignite.IgniteJdbc"
@@ -13,8 +16,14 @@ object MetaDataProviderFactory {
 class MetaDataProviderFactory {
 
   def create(dbPoolConfig: DBPoolConfig): JdbcMetaDataProvider = {
-    val getConnection: () => Connection = () => DriverManager.getConnection(dbPoolConfig.url, dbPoolConfig.username, dbPoolConfig.password)
-
+    val props = new Properties()
+    dbPoolConfig.connectionProperties.foreach {
+      case (k, v) => props.put(k, v)
+    }
+    val ds = ThreadUtils.withThisAsContextClassLoader(getClass.getClassLoader) {
+      new DriverDataSource(dbPoolConfig.url, dbPoolConfig.driverClassName, props, dbPoolConfig.username, dbPoolConfig.password)
+    }
+    val getConnection: () => Connection = () => ds.getConnection
     dbPoolConfig.driverClassName match {
       case className if className.startsWith(igniteDriverPrefix) => new IgniteMetaDataProvider(getConnection)
       case _  => new JdbcMetaDataProvider(getConnection)
