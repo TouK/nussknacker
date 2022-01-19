8cf931e5308638619a28a5b7cb479b73b51bad69
Krzysztof Gadomski <kgd@touk.pl>
2022-01-19 14:19:20 +0100

    Fix for pasting (#2754)

--- ui/client/containers/Visualization.js
+++ ui/client/containers/Visualization.js
@@ -100,7 +100,8 @@ class Visualization extends React.Component {
 
   getPastePosition = () => {
     const paper = this.getGraphInstance()?.processGraphPaper
-    return paper?.getArea()?.center() || {x: 300, y: 100}
+    const {x, y} = paper?.getArea()?.center() || {x: 300, y: 100}
+    return {x: Math.floor(x), y: Math.floor(y)}
   }
 
   getGraphInstance = () => this.graphRef.current?.getDecoratedComponentInstance()
