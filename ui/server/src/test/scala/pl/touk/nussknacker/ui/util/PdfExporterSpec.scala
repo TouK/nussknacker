57d2a76221d6e13bed491196d140a8494fb07fc7
Łukasz Ciołecki <lcl@touk.pl>
2022-01-21 11:25:16 +0100

    Improvement: Change modified information at BaseProcessDetails

--- ui/server/src/test/scala/pl/touk/nussknacker/ui/util/PdfExporterSpec.scala
+++ ui/server/src/test/scala/pl/touk/nussknacker/ui/util/PdfExporterSpec.scala
@@ -1,51 +1,48 @@
 package pl.touk.nussknacker.ui.util
 
-import java.io.FileOutputStream
-import java.nio.charset.StandardCharsets
-import java.time.LocalDateTime
 import org.apache.commons.io.IOUtils
 import org.scalatest.FlatSpec
 import pl.touk.nussknacker.engine.api.StreamMetaData
 import pl.touk.nussknacker.engine.canonize.ProcessCanonizer
 import pl.touk.nussknacker.engine.graph.node.{Filter, UserDefinedAdditionalNodeFields}
-import pl.touk.nussknacker.ui.api.helpers.{SampleProcess, TestProcessingTypes}
 import pl.touk.nussknacker.restmodel.displayedgraph.{DisplayableProcess, ProcessProperties}
+import pl.touk.nussknacker.restmodel.processdetails.ProcessVersion
+import pl.touk.nussknacker.ui.api.helpers.{SampleProcess, TestProcessUtil, TestProcessingTypes}
 import pl.touk.nussknacker.ui.process.marshall.ProcessConverter
 import pl.touk.nussknacker.ui.process.repository.ProcessActivityRepository.{Comment, ProcessActivity}
-import pl.touk.nussknacker.restmodel.processdetails.{BaseProcessDetails, ProcessVersion}
-import pl.touk.nussknacker.engine.api.process.ProcessId
 
+import java.io.FileOutputStream
+import java.nio.charset.StandardCharsets
+import java.time.LocalDateTime
 import scala.io.Source
 
 class PdfExporterSpec extends FlatSpec {
 
+  private val history = List(ProcessVersion(1, LocalDateTime.now(), "Zenon Wojciech", Option.empty, List.empty))
+
   it should "export process to " in {
     val process: DisplayableProcess = ProcessConverter.toDisplayable(ProcessCanonizer.canonize(SampleProcess.process), TestProcessingTypes.Streaming)
-    val displayable = process.copy(nodes = process.nodes.map {
+    val displayable: DisplayableProcess = process.copy(nodes = process.nodes.map {
         case a:Filter => a.copy(additionalFields = Some(UserDefinedAdditionalNodeFields(Some("mój wnikliwy komętaż"), None)))
         case a => a
     })
 
-    val details = BaseProcessDetails("My process", "My process", ProcessId(1), 11, true,
-      Some("My fancy description, which is quite, quite, quite looooooooong. \n And it contains maaaany, maaany strange features..."),false, false,
-      TestProcessingTypes.Streaming, "Category 22", LocalDateTime.now(), LocalDateTime.now(), "user", List(), None, None, Some(displayable),
-      List(ProcessVersion(11, LocalDateTime.now(), "Zenon Wojciech", Option.empty, List.empty)), Option.empty
-    )
+    val details = createDetails(displayable)
 
     val activities = ProcessActivity(List(
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
-      Comment(1L, "aa", 11, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now())
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now()),
+      Comment(1L, "aa", details.processId.value, "Jakiś taki dziwny ten proces??", "Wacław Wójcik", LocalDateTime.now())
     ), List())
 
     val svg: String = Source.fromInputStream(getClass.getResourceAsStream("/svgTest.svg"), StandardCharsets.UTF_8.name()).getLines().mkString("")
@@ -58,11 +55,7 @@ class PdfExporterSpec extends FlatSpec {
     val displayable: DisplayableProcess = DisplayableProcess(
       "Proc11", ProcessProperties(StreamMetaData(), subprocessVersions = Map.empty), List(), List(), TestProcessingTypes.Streaming)
 
-    val details = BaseProcessDetails("My process", "My process", ProcessId(1), 11, true,
-      Some("My fancy description, which is quite, quite, quite looooooooong. \n And it contains maaaany, maaany strange features..."), false, false,
-      TestProcessingTypes.Streaming, "Category 22", LocalDateTime.now(), LocalDateTime.now(), "user", List(), None, None, Some(displayable),
-      List(ProcessVersion(11, LocalDateTime.now(), "Zenon Wojciech", Option.empty, List.empty)), Option.empty
-    )
+    val details = createDetails(displayable)
 
     val activities = ProcessActivity(List(), List())
     val svg: String = Source.fromInputStream(getClass.getResourceAsStream("/svgTest.svg"), StandardCharsets.UTF_8.name()).getLines().mkString("")
@@ -70,4 +63,11 @@ class PdfExporterSpec extends FlatSpec {
 
     IOUtils.write(exported, new FileOutputStream("/tmp/empty.pdf"))
   }
+
+  private def createDetails(displayable: DisplayableProcess) = TestProcessUtil.toDetails(
+    "My process",
+    json = Some(displayable),
+    description = Some("My fancy description, which is quite, quite, quite looooooooong. \n And it contains maaaany, maaany strange features..."),
+    history = Some(history)
+  )
 }
