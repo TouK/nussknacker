ef25c40ed6bbd5c4944d0005eb9ded087df3a925
gskrobisz <gsk@touk.pl>
2022-02-18 11:35:08 +0100

    Allow KafkaExceptionConsumerProvider to overwrite serializationSchema (#2873)

--- engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/exception/KafkaExceptionConsumer.scala
+++ engine/flink/kafka-util/src/main/scala/pl/touk/nussknacker/engine/kafka/exception/KafkaExceptionConsumer.scala
@@ -4,9 +4,9 @@ import com.typesafe.config.Config
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 import net.ceedubs.ficus.Ficus._
 import pl.touk.nussknacker.engine.api.MetaData
-import pl.touk.nussknacker.engine.api.exception.{NuExceptionInfo, NonTransientException}
-import pl.touk.nussknacker.engine.api.runtimecontext.EngineRuntimeContext
+import pl.touk.nussknacker.engine.api.exception.{NonTransientException, NuExceptionInfo}
 import pl.touk.nussknacker.engine.flink.api.exception.{FlinkEspExceptionConsumer, FlinkEspExceptionConsumerProvider}
+import pl.touk.nussknacker.engine.kafka.serialization.KafkaSerializationSchema
 import pl.touk.nussknacker.engine.kafka.sharedproducer.WithSharedKafkaProducer
 import pl.touk.nussknacker.engine.kafka.{DefaultProducerCreator, KafkaConfig, KafkaProducerCreator, KafkaUtils}
 import pl.touk.nussknacker.engine.util.SynchronousExecutionContext
@@ -18,13 +18,16 @@ class KafkaExceptionConsumerProvider extends FlinkEspExceptionConsumerProvider {
     val kafkaConfig = KafkaConfig.parseConfig(additionalConfig)
     val consumerConfig = additionalConfig.rootAs[KafkaExceptionConsumerConfig]
     val producerCreator = kafkaProducerCreator(kafkaConfig)
-    val serialization = KafkaJsonExceptionSerializationSchema(metaData, consumerConfig)
+    val serializationSchema = createSerializationSchema(metaData, consumerConfig)
     if (consumerConfig.useSharedProducer) {
-      SharedProducerKafkaExceptionConsumer(metaData, serialization, producerCreator)
+      SharedProducerKafkaExceptionConsumer(metaData, serializationSchema, producerCreator)
     } else {
-      TempProducerKafkaExceptionConsumer(serialization, producerCreator)
+      TempProducerKafkaExceptionConsumer(serializationSchema, producerCreator)
     }
   }
+
+  protected def createSerializationSchema(metaData: MetaData, consumerConfig: KafkaExceptionConsumerConfig): KafkaSerializationSchema[NuExceptionInfo[NonTransientException]] =
+    new KafkaJsonExceptionSerializationSchema(metaData, consumerConfig)
 
   //visible for testing
   private[exception] def kafkaProducerCreator(kafkaConfig: KafkaConfig): KafkaProducerCreator.Binary = DefaultProducerCreator(kafkaConfig)
@@ -33,20 +36,20 @@ class KafkaExceptionConsumerProvider extends FlinkEspExceptionConsumerProvider {
 
 }
 
-case class TempProducerKafkaExceptionConsumer(serializationSchema: KafkaJsonExceptionSerializationSchema,
+case class TempProducerKafkaExceptionConsumer(serializationSchema: KafkaSerializationSchema[NuExceptionInfo[NonTransientException]],
                                               kafkaProducerCreator: KafkaProducerCreator.Binary) extends FlinkEspExceptionConsumer {
 
   override def consume(exceptionInfo: NuExceptionInfo[NonTransientException]): Unit = {
-    KafkaUtils.sendToKafkaWithTempProducer(serializationSchema.serialize(exceptionInfo))(kafkaProducerCreator)
+    KafkaUtils.sendToKafkaWithTempProducer(serializationSchema.serialize(exceptionInfo, System.currentTimeMillis()))(kafkaProducerCreator)
   }
 
 }
 
 case class SharedProducerKafkaExceptionConsumer(metaData: MetaData,
-                                                serializationSchema: KafkaJsonExceptionSerializationSchema,
+                                                serializationSchema: KafkaSerializationSchema[NuExceptionInfo[NonTransientException]],
                                                 kafkaProducerCreator: KafkaProducerCreator.Binary) extends FlinkEspExceptionConsumer with WithSharedKafkaProducer {
   override def consume(exceptionInfo: NuExceptionInfo[NonTransientException]): Unit = {
-    sendToKafka(serializationSchema.serialize(exceptionInfo))(SynchronousExecutionContext.ctx)
+    sendToKafka(serializationSchema.serialize(exceptionInfo, System.currentTimeMillis()))(SynchronousExecutionContext.ctx)
   }
   
 }
