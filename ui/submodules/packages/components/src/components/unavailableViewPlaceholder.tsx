9354b599f049eed743876859f852d7e046f13a5f
Julian Wielga <j@touk.pl>
2022-03-02 21:43:32 +0100

    UnavailableViewPlaceholder visual tweak

--- ui/submodules/packages/components/src/components/unavailableViewPlaceholder.tsx
+++ ui/submodules/packages/components/src/components/unavailableViewPlaceholder.tsx
@@ -1,4 +1,4 @@
-import { Link, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { useBackHref } from "../common";
@@ -6,11 +6,13 @@ import { useBackHref } from "../common";
 export function UnavailableViewPlaceholder(): JSX.Element {
     const to = useBackHref();
     return (
-        <Typography variant="h2">
-            {`not yet... `}
-            <Link component={RouterLink} to={to} replace color="secondary">
-                go back!
-            </Link>
-        </Typography>
+        <Box flex={1} display="flex" flexDirection="row" justifyContent="center" alignItems="center">
+            <Typography variant="h2" sx={{ marginBottom: ".8em" }}>
+                {`not yet... `}
+                <Link component={RouterLink} to={to} replace color="secondary">
+                    go back!
+                </Link>
+            </Typography>
+        </Box>
     );
 }
