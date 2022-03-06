8185cb27469236a3bd675e0bfe84abe95f6f4645
Łukasz Jędrzejewski <ljd@touk.pl>
2022-03-06 15:51:10 +0100

    Close periodic engine actors

--- engine/flink/management/periodic/src/main/scala/pl/touk/nussknacker/engine/management/periodic/RescheduleFinishedActor.scala
+++ engine/flink/management/periodic/src/main/scala/pl/touk/nussknacker/engine/management/periodic/RescheduleFinishedActor.scala
@@ -2,31 +2,53 @@ package pl.touk.nussknacker.engine.management.periodic
 
 import akka.actor.{Actor, Props, Timers}
 import com.typesafe.scalalogging.LazyLogging
-import pl.touk.nussknacker.engine.management.periodic.RescheduleFinishedActor.CheckStates
+import pl.touk.nussknacker.engine.management.periodic.RescheduleFinishedActor.{CheckStates, CheckStatesCompleted}
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 object RescheduleFinishedActor {
   def props(service: PeriodicProcessService, interval: FiniteDuration): Props = {
-    Props(new RescheduleFinishedActor(service, interval))
+    props(service.handleFinished, interval)
+  }
+
+  private[periodic] def props(handleFinished: => Future[Unit], interval: FiniteDuration): Props = {
+    Props(new RescheduleFinishedActor(handleFinished, interval))
   }
 
   private case object CheckStates
+
+  private case object CheckStatesCompleted
 }
 
-class RescheduleFinishedActor(service: PeriodicProcessService, interval: FiniteDuration) extends Actor
+class RescheduleFinishedActor(handleFinished: => Future[Unit], interval: FiniteDuration) extends Actor
   with Timers
   with LazyLogging {
 
+  import context.dispatcher
+
   override def preStart(): Unit = {
     logger.info(s"Initializing with $interval interval")
-    timers.startPeriodicTimer(key = "checkStates", msg = CheckStates, interval = interval)
+    scheduleCheckStates()
   }
 
   override def receive: Receive = {
     case CheckStates =>
       logger.debug("Checking scenarios to be rescheduled or marked as failed")
-      service.handleFinished
+      handleFinished onComplete {
+        case Success(_) =>
+          self ! CheckStatesCompleted
+        case Failure(exception) =>
+          logger.error("Checking scenarios to be rescheduled or marked as failed finished with error", exception)
+          self ! CheckStatesCompleted
+      }
+    case CheckStatesCompleted =>
+      scheduleCheckStates()
+  }
+
+  private def scheduleCheckStates(): Unit = {
+    timers.startSingleTimer(key = "checkStates", msg = CheckStates, timeout = interval)
   }
 
 }
