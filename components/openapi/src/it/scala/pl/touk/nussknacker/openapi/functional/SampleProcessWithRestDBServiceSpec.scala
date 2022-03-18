607e2d85aa37098fd666548473ea9d192140fe32
Damian Święcki <dswiecki9@gmail.com>
2022-03-18 09:40:09 +0100

    Stubbed open api test

--- components/openapi/src/it/scala/pl/touk/nussknacker/openapi/functional/SampleProcessWithRestDBServiceSpec.scala
+++ components/openapi/src/it/scala/pl/touk/nussknacker/openapi/functional/SampleProcessWithRestDBServiceSpec.scala
@@ -1,11 +1,12 @@
 package pl.touk.nussknacker.openapi.functional
 
-import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory.fromAnyRef
+import com.typesafe.config.{ConfigFactory, ConfigRenderOptions}
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.commons.io.IOUtils
 import org.scalatest._
-import pl.touk.nussknacker.engine.api.ProcessVersion
 import pl.touk.nussknacker.engine.api.typed.TypedMap
+import pl.touk.nussknacker.engine.api.{CirceUtil, ProcessVersion}
 import pl.touk.nussknacker.engine.build.ScenarioBuilder
 import pl.touk.nussknacker.engine.deployment.DeploymentData
 import pl.touk.nussknacker.engine.flink.test.FlinkSpec
@@ -18,7 +19,20 @@ import pl.touk.nussknacker.engine.process.helpers.SampleNodes.MockService
 import pl.touk.nussknacker.engine.process.registrar.FlinkProcessRegistrar
 import pl.touk.nussknacker.engine.spel
 import pl.touk.nussknacker.engine.testing.LocalModelData
+import pl.touk.nussknacker.engine.util.config.ConfigEnrichments.RichConfig
+import pl.touk.nussknacker.engine.util.config.CustomFicusInstances.{toFicusConfig, urlValueReader}
+import pl.touk.nussknacker.openapi.OpenAPIsConfig.openAPIServicesConfigVR
+import pl.touk.nussknacker.openapi.{OpenAPIServicesConfig, SwaggerService}
+import pl.touk.nussknacker.openapi.enrichers.SwaggerEnricher
+import pl.touk.nussknacker.openapi.http.backend.HttpBackendProvider
+import pl.touk.nussknacker.openapi.parser.SwaggerParser
 import pl.touk.nussknacker.test.VeryPatientScalaFutures
+import sttp.client.Response
+import sttp.client.testing.SttpBackendStub
+
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import scala.concurrent.{ExecutionContext, Future}
 
 class SampleProcessWithRestDBServiceSpec extends fixture.FunSuite with BeforeAndAfterAll with Matchers with FlinkSpec with LazyLogging with VeryPatientScalaFutures {
 
@@ -27,12 +41,26 @@ class SampleProcessWithRestDBServiceSpec extends fixture.FunSuite with BeforeAnd
 
   type FixtureParam = Int
 
+  def rootUrl(port: Int): String = s"http://localhost:$port/customers"
+
   def withFixture(test: OneArgTest): Outcome = StubService.withCustomerService { port =>
     withFixture(test.toNoArgTest(port))
   }
 
-  test("should enrich scenario with data") { port =>
+  val stubbedBackend: SttpBackendStub[Future, Nothing, Nothing] = SttpBackendStub.asynchronousFuture[Nothing]
+  val stubbedBackedProvider: HttpBackendProvider = (_: ExecutionContext) => stubbedBackend
 
+  test("should enrich scenario with data") { port =>
+    val finalConfig = ConfigFactory.load()
+      .withValue("components.openAPI.url", fromAnyRef(s"http://localhost:$port/swagger"))
+      .withValue("components.openAPI.rootUrl", fromAnyRef(rootUrl(port)))
+    val openAPIsConfig = config.rootAs[OpenAPIServicesConfig]
+    val definition = IOUtils.toString(finalConfig.as[URL]("components.openAPI.url"), StandardCharsets.UTF_8)
+    val services = SwaggerParser.parse(definition, openAPIsConfig)
+
+    val getCustomer = new SwaggerEnricher(Some(new URL(rootUrl(port))), services.head, Map.empty, stubbedBackedProvider)
+
+    //when
     val scenario =
       ScenarioBuilder
         .streaming("opeanapi-test")
@@ -41,8 +69,9 @@ class SampleProcessWithRestDBServiceSpec extends fixture.FunSuite with BeforeAnd
         .enricher("customer", "customer", "getCustomer", ("customer_id", "#input"))
         .processorEnd("end", "mockService", "all" -> "#customer")
 
-    val data = List("10")
-    run(scenario, data, port)
+    run(scenario, List("10"), port)
+    //then
+
     MockService.data shouldBe List(TypedMap(Map("name" -> "Robert Wright", "id" -> 10L, "category" -> "GOLD")))
   }
 
