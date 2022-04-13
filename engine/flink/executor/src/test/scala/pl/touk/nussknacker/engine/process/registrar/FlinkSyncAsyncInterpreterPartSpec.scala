bac2d904978e1d87e479f969ffd5ec895d0397e0
Łukasz Jędrzejewski <ljd@touk.pl>
2022-04-13 08:45:50 +0200

    join

--- engine/flink/executor/src/test/scala/pl/touk/nussknacker/engine/process/registrar/FlinkSyncAsyncInterpreterPartSpec.scala
+++ engine/flink/executor/src/test/scala/pl/touk/nussknacker/engine/process/registrar/FlinkSyncAsyncInterpreterPartSpec.scala
@@ -1,9 +1,12 @@
 package pl.touk.nussknacker.engine.process.registrar
 
+import cats.data.NonEmptyList
 import com.typesafe.config.ConfigValueFactory.fromAnyRef
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.flink.streaming.api.graph.{StreamGraph, StreamNode}
-import pl.touk.nussknacker.engine.build.{GraphBuilder, ScenarioBuilder}
+import pl.touk.nussknacker.engine.api.{MetaData, StreamMetaData}
+import pl.touk.nussknacker.engine.build.GraphBuilder
+import pl.touk.nussknacker.engine.graph.EspProcess
 import pl.touk.nussknacker.engine.process.registrar.FlinkProcessRegistrar.{BranchInterpretationName, CustomNodeInterpretationName, InterpretationName}
 import pl.touk.nussknacker.engine.spel.Implicits.asSpelExpression
 
@@ -18,7 +21,7 @@ class FlinkSyncAsyncInterpreterPartSpec extends FlinkStreamGraphSpec {
     val graph = streamGraph(prepareProcess(useAsyncInterpretation = false))
 
     val interpretationNodes = getInterpretationNodes(graph)
-    interpretationNodes should have size 5 // TODO should be 4
+    interpretationNodes should have size 7 // TODO should be 6
     every(interpretationNodes.map(_.getOperatorName)) should endWith("Sync")
   }
 
@@ -26,7 +29,7 @@ class FlinkSyncAsyncInterpreterPartSpec extends FlinkStreamGraphSpec {
     val graph = streamGraph(prepareProcess(useAsyncInterpretation = true))
 
     val interpretationNodes = getInterpretationNodes(graph)
-    interpretationNodes should have size 5 // TODO should be 4
+    interpretationNodes should have size 7 // TODO should be 6
     every(interpretationNodes.map(_.getOperatorName)) should endWith("Async")
   }
 
@@ -34,7 +37,7 @@ class FlinkSyncAsyncInterpreterPartSpec extends FlinkStreamGraphSpec {
     val graph = streamGraph(prepareProcess(useAsyncInterpretation = true), config = prepareConfig(detectSyncPart = Some(true)))
 
     val interpretationNodes = getInterpretationNodes(graph)
-    interpretationNodes should have size 5 // TODO should be 4
+    interpretationNodes should have size 7 // TODO should be 6
     val operatorNames = interpretationNodes.map(_.getOperatorName)
     exactly(3, operatorNames) should endWith("Async")
     exactly(1, operatorNames) should endWith("Sync")
@@ -46,44 +49,54 @@ class FlinkSyncAsyncInterpreterPartSpec extends FlinkStreamGraphSpec {
     graph.getStreamNodes.asScala.filter(node => interpretationNodeNames.exists(node.getOperatorName.contains))
   }
 
-  private def prepareProcess(useAsyncInterpretation: Boolean) = ScenarioBuilder
-    .streaming(scenarioId)
-    .useAsyncInterpretation(useAsyncInterpretation)
-    // Source part contains services.
-    // TODO: change to join.
-    .source("sourceId", "input")
-    .enricher("enricherId", "outputValue", "enricherWithOpenService")
-    .filter("filterId", "#input.value1 > 1")
-    .processor("processorId", "logService", "all" -> "123")
-    .buildSimpleVariable("varId", "var1", "42L")
-    .split("split",
-      // Only sink.
+  private def prepareProcess(useAsyncInterpretation: Boolean) = EspProcess(
+    MetaData(id = scenarioId, typeSpecificData = StreamMetaData(useAsyncInterpretation = Some(useAsyncInterpretation))),
+    NonEmptyList.of(
       GraphBuilder
-        .customNode("customId2", "customOutput2", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
-        .emptySink("sinkId2", "monitor"),
-      // Only sync components.
+        // Source part contains services.
+        .source("sourceId1", "input")
+        .enricher("enricherId1", "outputValue", "enricherWithOpenService")
+        .filter("filterId1", "#input.value1 > 1")
+        .processor("processorId1", "logService", "all" -> "123")
+        .branchEnd("end1", "joinId"),
       GraphBuilder
-        .customNode("customId3", "customOutput3", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
-        .buildSimpleVariable("varId3", "var3", "'xyz'")
-        .filter("filterId3", "#input.value1 > 1")
-        .emptySink("sinkId3", "monitor"),
-      // Contains a service.
+        // Source part does not contain services.
+        .source("sourceId2", "input")
+        .buildSimpleVariable("varId2", "var2", "42L")
+        .branchEnd("end2", "joinId"),
       GraphBuilder
-        .customNode("customId4", "customOutput4", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
-        .processorEnd("processorId4", "logService", "all" -> "123"),
-      // Contains a service.
-      GraphBuilder
-        .customNode("customId5", "customOutput5", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
-        .buildSimpleVariable("varId5", "var5", "'XYZ'")
-        .enricher("enricherId5", "outputValue5", "enricherWithOpenService")
-        .processorEnd("processorId5", "logService", "all" -> "123"),
-      // Only ending custom node.
-      GraphBuilder
-        .endingCustomNode("customEnding6", None, "optionalEndingCustom", "param" -> "'param'"),
-      // Only ending sink.
-      GraphBuilder
-        .emptySink("sinkId7", "monitor"),
+        .join("joinId", "sampleJoin", Some("joinInput"), Nil)
+        .buildSimpleVariable("varId3", "var3", "5L")
+        .split("split",
+          // Only sink.
+          GraphBuilder
+            .customNode("customId4", "customOutput4", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
+            .emptySink("sinkId4", "monitor"),
+          // Only sync components.
+          GraphBuilder
+            .customNode("customId5", "customOutput5", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
+            .buildSimpleVariable("varId5", "var5", "'xyz'")
+            .filter("filterId5", "true")
+            .emptySink("sinkId5", "monitor"),
+          // Contains a service.
+          GraphBuilder
+            .customNode("customId6", "customOutput6", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
+            .processorEnd("processorId6", "logService", "all" -> "123"),
+          // Contains a service.
+          GraphBuilder
+            .customNode("customId7", "customOutput7", "stateCustom", "groupBy" -> "''", "stringVal" -> "''")
+            .buildSimpleVariable("varId7", "var7", "'XYZ'")
+            .enricher("enricherId7", "outputValue7", "enricherWithOpenService")
+            .processorEnd("processorId7", "logService", "all" -> "123"),
+          // Only ending custom node.
+          GraphBuilder
+            .endingCustomNode("customEnding8", None, "optionalEndingCustom", "param" -> "'param'"),
+          // Only ending sink.
+          GraphBuilder
+            .emptySink("sinkId9", "monitor"),
+        ),
     )
+  )
 
   private def prepareConfig(detectSyncPart: Option[Boolean] = None): Config = {
     val baseConfig = ConfigFactory.load()
