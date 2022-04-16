1ef40b34b7477408eb35cf222fe10cc02418c4c0
JulianWielga <j@touk.pl>
2022-04-16 17:40:33 +0200

    fixed links to nu in submodules (#3016)
    * fixed links to nu in submodules
    
    * fixed links to nu in submodules

--- ui/submodules/packages/components/src/common/scenarioHref.tsx
+++ ui/submodules/packages/components/src/common/scenarioHref.tsx
@@ -5,7 +5,7 @@ function nuHref(path: string, scenarioId: string): string {
     // , and / allowed in scenarioId
     const sid = encodeURIComponent(scenarioId);
     const root = urljoin(BASE_ORIGIN, BASE_PATH);
-    return urljoin(window.location.href.startsWith(root) ? "" : root, path, sid);
+    return urljoin(window.location.href.startsWith(root) ? "/" : root, path, sid);
 }
 
 export function scenarioHref(scenarioId: string): string {
