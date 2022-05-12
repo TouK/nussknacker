7b0342544958ebe2950838ee5bd5018a7f03b74a
Julian Wielga <j@touk.pl>
2022-05-12 23:06:38 +0200

    revert to default splitchunks config (module federation problems)

--- ui/client/webpack.config.js
+++ ui/client/webpack.config.js
@@ -48,9 +48,6 @@ const outputPath = path.join(process.cwd(), "dist")
 module.exports = {
   mode: NODE_ENV,
   optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
     minimizer: [new TerserPlugin({
       parallel: true,
       //Reactable bug: https://github.com/abdulrahman-khankan/reactable/issues/3
