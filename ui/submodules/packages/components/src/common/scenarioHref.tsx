c026845a06af7b22bf77b685503d99d1e4229f09
JulianWielga <j@touk.pl>
2022-05-16 09:57:40 +0200

    fix(components): better public path in links (#3038)

--- ui/submodules/packages/components/src/common/scenarioHref.tsx
+++ ui/submodules/packages/components/src/common/scenarioHref.tsx
@@ -1,11 +1,16 @@
 import { BASE_ORIGIN, BASE_PATH } from "nussknackerUi/config";
 import urljoin from "url-join";
 
+export const BASE_HREF = urljoin(BASE_ORIGIN, BASE_PATH);
+
+export function makeRelative(href: string): string {
+    return href.replace(BASE_HREF, (match, offset) => (offset > 0 ? match : "/"));
+}
+
 function nuHref(path: string, scenarioId: string): string {
     // , and / allowed in scenarioId
     const sid = encodeURIComponent(scenarioId);
-    const root = urljoin(BASE_ORIGIN, BASE_PATH);
-    return urljoin(window.location.href.startsWith(root) ? "/" : root, path, sid);
+    return urljoin(BASE_HREF, path, sid);
 }
 
 export function scenarioHref(scenarioId: string): string {
