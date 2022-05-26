ee9bb7321decc2e50836f778b332666098e126d9
Julian Wielga <j@touk.pl>
2022-05-26 12:07:02 +0200

    test: corrected legacy test & added new

--- ui/client/cypress/integration/processes.ts
+++ ui/client/cypress/integration/processes.ts
@@ -12,6 +12,45 @@ describe("Processes list", () => {
 
   beforeEach(() => {
     cy.visit("/")
+    cy.url().should("match", /scenarios/)
+    cy.get("[placeholder='Search...']", {timeout: 60000}).should("be.visible")
+  })
+
+  it("should have no process matching filter", () => {
+    cy.get("[placeholder='Search...']").type(NAME)
+    cy.contains(/^None of the/i).should("be.visible")
+  })
+
+  it("should allow creating new process", function() {
+    cy.contains(/^new scenario$/i).should("be.visible").click()
+    cy.get("#newProcessId", {timeout: 30000}).type(this.processName)
+    cy.contains(/^create$/i).should("be.enabled").click()
+    cy.url().should("contain", `visualization\/${this.processName}`)
+  })
+
+  it("should have test process on list", function() {
+    cy.get("[placeholder='Search...']").type(NAME)
+    cy.contains(/^1 of the/i).should("be.visible")
+    cy.contains(this.processName).should("be.visible").toMatchImageSnapshot()
+    cy.contains(this.processName).click({x: 10, y: 10})
+    cy.url().should("contain", `visualization\/${this.processName}`)
+  })
+})
+
+describe("Processes list (legacy)", () => {
+  const NAME = "process-list"
+
+  before(() => {
+    cy.deleteAllTestProcesses({filter: NAME, force: true})
+    cy.createTestProcessName(NAME).as("processName")
+  })
+
+  after(() => {
+    cy.deleteAllTestProcesses({filter: NAME})
+  })
+
+  beforeEach(() => {
+    cy.visit("/processes")
     cy.url().should("match", /processes/)
   })
 
