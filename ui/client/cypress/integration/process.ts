e7103570fb0ee78d6b458461e131e16214dce141
Julian Wielga <j@touk.pl>
2022-05-30 11:35:59 +0200

    test: rename

--- ui/client/cypress/integration/process.ts
+++ ui/client/cypress/integration/process.ts
@@ -170,7 +170,7 @@ describe("Process", () => {
   })
 
   //TODO: bugfix not available yet
-  it.skip("should merge validation (node & process) errors", () => {
+  it.skip("should display correct filter errors", () => {
     cy.viewport(1400, 800)
     cy.visitNewProcess(seed, "filter")
     cy.intercept("POST", "/api/nodes/*/validation").as("validation")
