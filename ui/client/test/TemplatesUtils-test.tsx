c6fa12fcccbf264f7a81c233ee0e3c0107f3c079
Julian Wielga <j@touk.pl>
2022-06-13 19:12:13 +0200

    fix: escape characters for spel inside sql

--- ui/client/test/TemplatesUtils-test.tsx
+++ ui/client/test/TemplatesUtils-test.tsx
@@ -2,17 +2,17 @@ import * as TemplatesUtils from "../components/graph/node-modal/editors/expressi
 import {QuotationMark} from "../components/graph/node-modal/editors/expression/SpelQuotesUtils"
 
 const templates = `
-Lorem ipsum #{dolor} sit amet enim. Etiam #{
+Lorem ipsum #{dolor}# sit amet enim. Etiam #{
 ullamcorper.
 Suspendisse  
   } a pellentesque dui, non felis. Maecenas malesuada elit lectus
 `
 
 const templatesEscaped = `
-Lorem ipsum #\\{dolor\\} sit amet enim. Etiam #\\{
+Lorem ipsum #\\{dolor\\}# sit amet enim. Etiam #\\{
 ullamcorper.
 Suspendisse  
-  \\} a pellentesque dui, non felis. Maecenas malesuada elit lectus
+  \\}# a pellentesque dui, non felis. Maecenas malesuada elit lectus
 `
 
 const concats = `
@@ -21,28 +21,28 @@ Suspendisse+' a pellentesque dui, non felis. Maecenas malesuada elit lectus
 `
 
 const templatesTrimmed = `
-Lorem ipsum #{dolor} sit amet enim. Etiam #{ullamcorper.
-Suspendisse} a pellentesque dui, non felis. Maecenas malesuada elit lectus
+Lorem ipsum #{dolor}# sit amet enim. Etiam #{ullamcorper.
+Suspendisse}# a pellentesque dui, non felis. Maecenas malesuada elit lectus
 `
 
 describe("TemplatesUtils", () => {
   describe("templatesToConcats", () => {
-    it("should replace #{value} with '+value+' ", () => {
+    it("should replace #{value}# with '+value+' ", () => {
       expect(TemplatesUtils.templatesToConcats(QuotationMark.single, templates)).toBe(concats)
     })
   })
   describe("concatsToTemplates", () => {
-    it("should replace #{value} with '+value+' ", () => {
+    it("should replace #{value}# with '+value+' ", () => {
       expect(TemplatesUtils.concatsToTemplates(QuotationMark.single, concats)).toBe(templatesTrimmed)
     })
   })
   describe("escapeTemplates", () => {
-    it("should replace #{value} with #\\{value\\}", () => {
+    it("should replace #{value}# with #\\{value\\}", () => {
       expect(TemplatesUtils.escapeTemplates(templates)).toBe(templatesEscaped)
     })
   })
   describe("unescapeTemplates", () => {
-    it("should replace #\\{value\\} with #{value}", () => {
+    it("should replace #\\{value\\}# with #{value}#", () => {
       expect(TemplatesUtils.unescapeTemplates(templatesEscaped)).toBe(templates)
     })
   })
