26c378b4101b78101bda31658849b1cd76237493
Julian Wielga <j@touk.pl>
2022-06-14 14:32:30 +0200

    fix: more typescript, more (placeholder) types

--- ui/client/components/graph/GraphUtils.ts
+++ ui/client/components/graph/GraphUtils.ts
@@ -1,19 +1,22 @@
 import NodeUtils from "./NodeUtils"
-import _ from "lodash"
+import {cloneDeep, filter, isEqual, map, reject} from "lodash"
+import {Edge, Process} from "../../types"
 
 export function mapProcessWithNewNode(process, before, after) {
   return {
     ...process,
-    edges: _.map(process.edges, (e) => {
-      if (_.isEqual(e.from, before.id)) {
+    edges: map(process.edges, (e) => {
+      if (isEqual(e.from, before.id)) {
         return {...e, from: after.id, to: e.to}
-      } else if (_.isEqual(e.to, before.id)) {
+      } else if (isEqual(e.to, before.id)) {
         return {...e, from: e.from, to: after.id}
       } else {
         return e
       }
     }),
-    nodes: _.map(process.nodes, (n) => { return _.isEqual(n, before) ? after : mapBranchParametersWithNewNode(before.id, after.id, n) }),
+    nodes: map(process.nodes, (n) => {
+      return isEqual(n, before) ? after : mapBranchParametersWithNewNode(before.id, after.id, n)
+    }),
     properties: NodeUtils.nodeIsProperties(before) ? after : process.properties,
   }
 }
@@ -21,7 +24,7 @@ export function mapProcessWithNewNode(process, before, after) {
 //we do mapping here, because we validate changed process before closing modal, and before applying state change in reducer.
 function mapBranchParametersWithNewNode(beforeId, afterId, node) {
   if (beforeId !== afterId && node.branchParameters?.find(bp => bp.branchId === beforeId)) {
-    const newNode = _.cloneDeep(node)
+    const newNode = cloneDeep(node)
     const branchParameter = newNode.branchParameters.find(bp => bp.branchId === beforeId)
     if (branchParameter) {
       branchParameter.branchId = afterId
@@ -32,11 +35,11 @@ function mapBranchParametersWithNewNode(beforeId, afterId, node) {
   }
 }
 
-export function mapProcessWithNewEdge(process, before, after) {
+export function mapProcessWithNewEdge(process: Process, before: Edge, after: Edge): Process {
   return {
     ...process,
-    edges: _.map(process.edges, (e) => {
-      if (_.isEqual(e.from, before.from) && _.isEqual(e.to, before.to)) {
+    edges: map(process.edges, (e) => {
+      if (isEqual(e.from, before.from) && isEqual(e.to, before.to)) {
         return after
       } else {
         return e
@@ -48,8 +51,8 @@ export function mapProcessWithNewEdge(process, before, after) {
 export function deleteNode(process, id) {
   return {
     ...process,
-    edges: _.filter(process.edges, (e) => !_.isEqual(e.from, id) && !_.isEqual(e.to, id)),
-    nodes: _.filter(process.nodes, (n) => !_.isEqual(n.id, id)),
+    edges: filter(process.edges, (e) => !isEqual(e.from, id) && !isEqual(e.to, id)),
+    nodes: filter(process.nodes, (n) => !isEqual(n.id, id)),
   }
 }
 
@@ -64,7 +67,7 @@ export function canInjectNode(process, sourceId, middleManId, targetId, processD
 function deleteEdge(process, fromId, toId) {
   return {
     ...process,
-    edges: _.reject(process.edges, (e) => e.from === fromId && e.to === toId),
+    edges: reject(process.edges, (e) => e.from === fromId && e.to === toId),
   }
 }
 
