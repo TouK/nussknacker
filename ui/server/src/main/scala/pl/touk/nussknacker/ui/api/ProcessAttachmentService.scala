41f63f0ecd91fd2d2fed1d98c1528a557300abd8
Maciej Próchniak <mpr@touk.pl>
2022-06-22 10:45:45 +0200

    Fix add attachment hang problem (#3185)

--- ui/server/src/main/scala/pl/touk/nussknacker/ui/api/ProcessAttachmentService.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/api/ProcessAttachmentService.scala
@@ -1,7 +1,7 @@
 package pl.touk.nussknacker.ui.api
 
 import akka.stream.{Materializer, StreamLimitReachedException}
-import akka.stream.scaladsl.{Source, StreamConverters}
+import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 import pl.touk.nussknacker.engine.api.process.{ProcessId, VersionId}
@@ -11,25 +11,20 @@ import pl.touk.nussknacker.ui.process.repository.ProcessActivityRepository
 import pl.touk.nussknacker.ui.security.api.LoggedUser
 import pl.touk.nussknacker.ui.util.CatsSyntax
 
-import java.io.IOException
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success, Try, Using}
 
 class ProcessAttachmentService(config: AttachmentsConfig, processActivityRepository: ProcessActivityRepository) extends LazyLogging {
 
   def saveAttachment(processId: ProcessId, processVersionId: VersionId, originalFileName: String, byteSource: Source[ByteString, Any])
                     (implicit ec: ExecutionContext, loggedUser: LoggedUser, mat: Materializer): Future[Unit] = {
-
-    val inputStream = byteSource
+    byteSource
       .limitWeighted(config.maxSizeInBytes)(_.size)
-      .runWith(StreamConverters.asInputStream())
-
-    Try(Using.resource(inputStream)(_.readAllBytes())) match {
-      case Success(data) => processActivityRepository.addAttachment(AttachmentToAdd(processId, processVersionId, originalFileName, data))
-      case Failure(e: IOException) if e.getCause.isInstanceOf[StreamLimitReachedException] =>
+      //using StreamConverters.toInputStream hangs
+      .runFold(Array[Byte]()) { _ ++ _ }
+      .flatMap(bytes => processActivityRepository.addAttachment(AttachmentToAdd(processId, processVersionId, originalFileName, bytes)))
+      .recoverWith { case _: StreamLimitReachedException =>
         Future.failed(new IllegalArgumentException(s"Maximum (${config.maxSizeInBytes} bytes) attachment size exceeded."))
-      case Failure(exception) => Future.failed(exception)
-    }
+      }
   }
 
   def readAttachment(attachmentId: Long)
