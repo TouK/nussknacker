a753a4b93b5b57b0345a90654f7f53ae79b467ef
Dawid Sula <dsl@touk.pl>
2022-07-01 11:44:24 +0200

    Added added missing handling for primitives + tests

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/typing.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/typing.scala
@@ -215,8 +215,13 @@ object typing {
         case javaList: java.util.List[_] =>
           typedClass(obj.getClass, List(unionOfElementTypes(javaList.asScala.toList)))
         case typeFromInstance: TypedFromInstance => typeFromInstance.typingResult
-        case other =>
-          Typed(other.getClass)
+        case int: Int => Typed.typedValue(int)
+        case long: Long => Typed.typedValue(long)
+        case float: Float => Typed.typedValue(float)
+        case double: Double => Typed.typedValue(double)
+        case bool: Boolean => Typed.typedValue(bool)
+        case string: String => Typed.typedValue(string)
+        case other => Typed(other.getClass)
       }
     }
 
