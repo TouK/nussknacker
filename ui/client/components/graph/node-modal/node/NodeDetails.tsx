61bb0c0658c97fec5571b90bd02fe0201489aeea
Julian Wielga <j@touk.pl>
2022-07-02 00:57:52 +0200

    lock apply button for empty id

--- ui/client/components/graph/node-modal/node/NodeDetails.tsx
+++ ui/client/components/graph/node-modal/node/NodeDetails.tsx
@@ -56,6 +56,7 @@ export function NodeDetails(props: WindowContentProps<WindowKind, NodeType> & {r
       {
         title: t("dialog.button.apply", "apply"),
         action: () => performNodeEdit(),
+        disabled: !editedNode.id?.length,
         classname: css({
           //increase (x4) specificity over ladda
           "&&&&": {
