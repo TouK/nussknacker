9cde3a24eef990e707b18aa03da3af3dcc89108e
Arek Burdach <arek.burdach@gmail.com>
2022-07-05 20:07:52 +0200

    Switch component renamed to Choice

--- ui/server/src/main/scala/pl/touk/nussknacker/ui/component/ComponentDefinitionPreparer.scala
+++ ui/server/src/main/scala/pl/touk/nussknacker/ui/component/ComponentDefinitionPreparer.scala
@@ -60,7 +60,7 @@ object ComponentDefinitionPreparer {
     val base = ComponentGroup(BaseGroupName, List(
       ComponentTemplate.create(ComponentType.Filter, Filter("", Expression("spel", "true")), userProcessingTypeCategories),
       ComponentTemplate.create(ComponentType.Split, Split(""), userProcessingTypeCategories),
-      ComponentTemplate.create(ComponentType.Switch, Switch("", Expression("spel", "true"), "output"), userProcessingTypeCategories),
+      ComponentTemplate.create(ComponentType.Switch, Switch("", Expression("spel", "true"), "output"), userProcessingTypeCategories).copy(label = "choice"),
       ComponentTemplate.create(ComponentType.Variable, Variable("", "varName", Expression("spel", "'value'")), userProcessingTypeCategories),
       ComponentTemplate.create(ComponentType.MapVariable, VariableBuilder("", "mapVarName", List(Field("varName", Expression("spel", "'value'")))), userProcessingTypeCategories),
     ))
