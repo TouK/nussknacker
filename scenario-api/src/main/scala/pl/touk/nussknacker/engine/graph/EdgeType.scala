2a81d84d77dc936c26f568586e773b8ff64a6887
Maciek Próchniak <mpr@touk.pl>
2022-07-06 08:09:28 +0200

    Do not require switch edge uniqueness

--- scenario-api/src/main/scala/pl/touk/nussknacker/engine/graph/EdgeType.scala
+++ scenario-api/src/main/scala/pl/touk/nussknacker/engine/graph/EdgeType.scala
@@ -5,13 +5,17 @@ import pl.touk.nussknacker.engine.graph.expression.Expression
 import pl.touk.nussknacker.engine.api.CirceUtil._
 
 //unstable, may change in the future...
-@ConfiguredJsonCodec sealed abstract class EdgeType
+@ConfiguredJsonCodec sealed abstract class EdgeType {
+  def mustBeUnique: Boolean = true
+}
 object EdgeType {
   sealed trait FilterEdge extends EdgeType
-  sealed trait SwitchEdge extends EdgeType
   case object FilterTrue extends FilterEdge
   case object FilterFalse extends FilterEdge
-  case class NextSwitch(condition: Expression) extends SwitchEdge
+  sealed trait SwitchEdge extends EdgeType
+  case class NextSwitch(condition: Expression) extends SwitchEdge {
+    override def mustBeUnique: Boolean = false
+  }
   case object SwitchDefault extends SwitchEdge
   case class SubprocessOutput(name: String) extends EdgeType
 }
