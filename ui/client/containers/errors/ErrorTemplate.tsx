fd2b7f311c62c00f7425858ef22eaac68719409c
Julian Wielga <j@touk.pl>
2022-07-18 16:38:53 +0200

    rewrite to typescript & cleanup

--- ui/client/containers/errors/ErrorTemplate.tsx
+++ ui/client/containers/errors/ErrorTemplate.tsx
@@ -1,4 +1,5 @@
 import React, {PropsWithChildren} from "react"
+import {useTranslation} from "react-i18next"
 
 export interface ErrorTemplateProps {
   description?: string,
@@ -10,9 +11,11 @@ export function ErrorTemplate({
   message = "",
   children,
 }: PropsWithChildren<ErrorTemplateProps>): JSX.Element {
+  const {t} = useTranslation()
+
   return (
     <div className="error-template center-block">
-      <h1>{`Oops!`}</h1>
+      <h1>{t("error.title", "Oops!")}</h1>
       <h2>{message}</h2>
       <div className="error-details">
         <p>{description}</p>
