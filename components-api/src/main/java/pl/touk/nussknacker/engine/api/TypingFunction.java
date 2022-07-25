d2ee4e3cf48b92d36d55020e8905c6331445dd95
Dawid Sula <dsl@touk.pl>
2022-07-25 14:50:01 +0200

    Changed TypingFunction interface

--- components-api/src/main/java/pl/touk/nussknacker/engine/api/TypingFunction.java
+++ components-api/src/main/java/pl/touk/nussknacker/engine/api/TypingFunction.java
@@ -3,13 +3,18 @@ package pl.touk.nussknacker.engine.api;
 import cats.data.NonEmptyList;
 import cats.data.Validated;
 import pl.touk.nussknacker.engine.api.typed.typing.TypingResult;
+import scala.Option;
 import scala.Tuple2;
 import scala.collection.immutable.List;
 
-public interface TypingFunction {
-    List<Tuple2<String, TypingResult>> staticParameters();
+public abstract class TypingFunction {
+    public Option<List<Tuple2<String, TypingResult>>> staticParameters() {
+        return Option.apply(null);
+    }
 
-    TypingResult staticResult();
+    public Option<TypingResult> staticResult() {
+        return Option.apply(null);
+    }
 
-    Validated<NonEmptyList<String>, TypingResult> apply(List<TypingResult> arguments);
+    public abstract Validated<NonEmptyList<String>, TypingResult> apply(List<TypingResult> arguments);
 }
