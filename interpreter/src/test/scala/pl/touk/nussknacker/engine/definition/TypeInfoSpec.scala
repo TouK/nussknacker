b10466088316a3ff82b692f63d5212dd2731c1e7
Dawid Sula <dsl@touk.pl>
2022-07-26 13:27:03 +0200

    Added serialization test

--- interpreter/src/test/scala/pl/touk/nussknacker/engine/definition/TypeInfoSpec.scala
+++ interpreter/src/test/scala/pl/touk/nussknacker/engine/definition/TypeInfoSpec.scala
@@ -1,8 +1,10 @@
 package pl.touk.nussknacker.engine.definition
 
+import cats.data.ValidatedNel
 import org.scalatest.{FunSuite, Matchers}
-import pl.touk.nussknacker.engine.api.typed.typing.{Typed, Unknown}
-import pl.touk.nussknacker.engine.definition.TypeInfo.{MethodInfo, NoVarArgMethodInfo, Parameter, VarArgsMethodInfo}
+import pl.touk.nussknacker.engine.api.expression.ExpressionParseError
+import pl.touk.nussknacker.engine.api.typed.typing.{Typed, TypingResult, Unknown}
+import pl.touk.nussknacker.engine.definition.TypeInfo.{FunctionalMethodInfo, MethodInfo, NoVarArgMethodInfo, Parameter, SerializableMethodInfo, VarArgsMethodInfo}
 
 class TypeInfoSpec extends FunSuite with Matchers {
   test("should create methodInfos without varArgs") {
@@ -12,5 +14,24 @@ class TypeInfoSpec extends FunSuite with Matchers {
   test("should create methodInfos with varArgs") {
     MethodInfo(List(Parameter("", Typed[Array[Object]])), Unknown, "", None, varArgs = true) shouldBe
       VarArgsMethodInfo(List(), Parameter("", Unknown), Unknown, "", None)
+  }
+
+  test("should throw errors when creating illegal method") {
+    intercept[AssertionError] { MethodInfo(List(Parameter("", Typed[Int])), Unknown, "", None, varArgs = true) }
+    intercept[AssertionError] { MethodInfo(List(), Unknown, "", None, varArgs = true) }
+  }
+
+  test("should generate serializable method info") {
+    val paramX = Parameter("x", Typed[Int])
+    val paramY = Parameter("y", Typed[String])
+    val paramYArray = Parameter("y", Typed.genericTypeClass[Array[Object]](List(Typed[String])))
+    def f(x: List[TypingResult]): ValidatedNel[ExpressionParseError, TypingResult] = ???
+
+    NoVarArgMethodInfo(List(paramX), Typed[Double], "b", Some("c")).serializable shouldBe
+      SerializableMethodInfo(List(paramX), Typed[Double], Some("c"), varArgs = false)
+    VarArgsMethodInfo(List(paramX), paramY, Typed[Long], "d", Some("e")).serializable shouldBe
+      SerializableMethodInfo(List(paramX, paramYArray), Typed[Long], Some("e"), varArgs = true)
+    FunctionalMethodInfo(f, List(paramX, paramY), Typed[String], "f", Some("g")).serializable shouldBe
+      SerializableMethodInfo(List(paramX, paramY), Typed[String], Some("g"), varArgs = false)
   }
 }
