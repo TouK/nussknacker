8c770430543d9831e3b0f21d69dc29aaf20b37d0
Maciek Próchniak <mpr@touk.pl>
2022-07-26 17:41:57 +0200

    fe review

--- ui/client/actions/nk/notifications.ts
+++ ui/client/actions/nk/notifications.ts
@@ -1,20 +1,16 @@
-import {ThunkAction} from "../reduxTypes";
-
 export type NotificationActions =
-  | UpdateBackendNotifications
-  | MarkBackendNotificationRead
+    | UpdateBackendNotifications
+    | MarkBackendNotificationRead
 
 export type UpdateBackendNotifications = {
     type: "UPDATE_BACKEND_NOTIFICATIONS",
     notificationIds: string[]
 }
 
-export function updateBackendNotifications(notificationIds: string[]): ThunkAction {
-    return (dispatch) => {
-        return dispatch({
-            type: "UPDATE_BACKEND_NOTIFICATIONS",
-            notificationIds: notificationIds
-        })
+export function updateBackendNotifications(notificationIds: string[]): NotificationActions {
+    return {
+        type: "UPDATE_BACKEND_NOTIFICATIONS",
+        notificationIds: notificationIds
     }
 }
 
@@ -23,11 +19,9 @@ export type MarkBackendNotificationRead = {
     notificationId: string
 }
 
-export function markBackendNotificationRead(notificationId: string): ThunkAction {
-    return (dispatch) => {
-        return dispatch({
-            type: "MARK_BACKEND_NOTIFICATION_READ",
-            notificationId: notificationId
-        })
+export function markBackendNotificationRead(notificationId: string): NotificationActions {
+    return {
+        type: "MARK_BACKEND_NOTIFICATION_READ",
+        notificationId: notificationId
     }
 }
