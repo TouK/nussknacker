e8825ee5064c16135502a92da1dc1cf585c5fd06
Łukasz Ciołecki <lcl@touk.pl>
2022-08-02 08:50:05 +0200

    Remove Unknown support

--- utils/avro-components-utils/src/main/scala/pl/touk/nussknacker/engine/avro/encode/AvroSchemaOutputValidator.scala
+++ utils/avro-components-utils/src/main/scala/pl/touk/nussknacker/engine/avro/encode/AvroSchemaOutputValidator.scala
@@ -55,7 +55,6 @@ class AvroSchemaOutputValidator(validationMode: ValidationMode) extends LazyLogg
 
   final private def validateTypingResult(typingResult: TypingResult, schema: Schema, path: Option[String]): ValidatedNel[OutputValidatorError, Unit] = {
     (typingResult, schema.getType) match {
-      case (Unknown, _) => valid //TODO: It's should be probably removed - it can blow up in runtime, but we want to allow pass result of SpEL with Generics
       case (tc@TypedClass(cl, _), _) if AvroUtils.isSpecificRecord(cl) =>
         validateSpecificRecord(tc, schema, path)
       case (typingResult: TypedObjectTypingResult, Type.RECORD) =>
