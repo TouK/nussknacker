531db8bc849a2e65353c71d79442dce7574529c3
Damian Święcki <dswiecki9@gmail.com>
2022-08-11 09:24:04 +0200

    Fix state compatibility

--- engine/flink/tests/src/test/scala/pl/touk/nussknacker/defaultmodel/StateCompatibilityTest.scala
+++ engine/flink/tests/src/test/scala/pl/touk/nussknacker/defaultmodel/StateCompatibilityTest.scala
@@ -2,14 +2,15 @@ package pl.touk.nussknacker.defaultmodel
 
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.JsonCodec
+import org.apache.avro.Schema
 import org.apache.flink.api.common.JobExecutionResult
 import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.scalatest.concurrent.Eventually
-import pl.touk.nussknacker.defaultmodel.MockSchemaRegistry.RecordSchemaV1
+import pl.touk.nussknacker.defaultmodel.MockSchemaRegistry.{RecordSchemaStringV1, RecordSchemaV1}
 import pl.touk.nussknacker.defaultmodel.StateCompatibilityTest.{InputEvent, OutputEvent}
 import pl.touk.nussknacker.engine.api.ProcessVersion
-import pl.touk.nussknacker.engine.schemedkafka.KafkaUniversalComponentTransformer
+import pl.touk.nussknacker.engine.schemedkafka.{AvroUtils, KafkaUniversalComponentTransformer}
 import pl.touk.nussknacker.engine.schemedkafka.schemaregistry.{ExistingSchemaVersion, SchemaVersionOption}
 import pl.touk.nussknacker.engine.build.ScenarioBuilder
 import pl.touk.nussknacker.engine.deployment.DeploymentData
@@ -35,14 +36,14 @@ object StateCompatibilityTest {
 }
 
 /**
-  * Verifies whether a scenario can be run from savepoint created earlier, e.g. by older Nussknacker or older process config creator.
-  *
-  * Important:
-  * A compliance with previos-snapshot strongly depends on serialVersionUID (all snapshot classes implement Serializable).
-  * and this serialVersionUID should be explicitly provided in all savepoint-able classes.
-  * @see description in [[pl.touk.nussknacker.engine.process.util.Serializers]]
-  * @see https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html
-  */
+ * Verifies whether a scenario can be run from savepoint created earlier, e.g. by older Nussknacker or older process config creator.
+ *
+ * Important:
+ * A compliance with previos-snapshot strongly depends on serialVersionUID (all snapshot classes implement Serializable).
+ * and this serialVersionUID should be explicitly provided in all savepoint-able classes.
+ * @see description in [[pl.touk.nussknacker.engine.process.util.Serializers]]
+ * @see https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html
+ */
 class StateCompatibilityTest extends FlinkWithKafkaSuite with Eventually with LazyLogging {
   import spel.Implicits._
 
@@ -50,6 +51,42 @@ class StateCompatibilityTest extends FlinkWithKafkaSuite with Eventually with La
 
   private val inTopic = "state.compatibility.input"
   private val outTopic = "state.compatibility.output"
+
+  val outputSchemaString: String =
+    """{
+      |    "name": "person",
+      |    "type": "record",
+      |    "fields": [
+      |        {
+      |            "name": "input",
+      |            "type": {
+      |                     "type": "record",
+      |                     "namespace": "pl.touk.nussknacker.engine.schemedkafka",
+      |                     "name": "FullName",
+      |                     "fields": [
+      |                       { "name": "first", "type": "string" },
+      |                       { "name": "last", "type": "string" }
+      |                     ]
+      |                   }
+      |
+      |        },
+      |        {
+      |            "name": "previousInput",
+      |            "type": {
+      |                     "type": "record",
+      |                     "namespace": "pl.touk.nussknacker.engine.schemedkafka",
+      |                     "name": "FullName2",
+      |                     "fields": [
+      |                       { "name": "first", "type": "string" },
+      |                       { "name": "last", "type": "string" }
+      |                     ]
+      |                   }
+      |        }
+      |    ]
+      |}""".stripMargin
+
+  val outputSchema: Schema = AvroUtils.parseSchema(outputSchemaString)
+
 
   private val savepointDir = {
     val resourcesDir = Paths.get("src/test/resources/state-compatibility")
@@ -63,7 +100,7 @@ class StateCompatibilityTest extends FlinkWithKafkaSuite with Eventually with La
     }
   }
 
-  private def stateCompatibilityProcess(inTopic: String) = ScenarioBuilder
+  private def stateCompatibilityProcess(inTopic: String, outTopic: String) = ScenarioBuilder
     .streaming("stateCompatibilityTest")
     .parallelism(1)
     .source("start", "kafka",
@@ -84,18 +121,19 @@ class StateCompatibilityTest extends FlinkWithKafkaSuite with Eventually with La
   private val event2 = InputEvent("Zenon", "Nowak")
 
   /**
-    * When previous snapshot compatibility breaks - (read 'should restore from snapshot' fails):
-    * 1. un-ignore this test and run manually to create new savepoint used in the other test
-    * 2. remove old snapshot (tests in this class require ONLY ONE savepoint)
-    * 3. go back to ignore :)
-    */
+   * When previous snapshot compatibility breaks - (read 'should restore from snapshot' fails):
+   * 1. un-ignore this test and run manually to create new savepoint used in the other test
+   * 2. remove old snapshot (tests in this class require ONLY ONE savepoint)
+   * 3. go back to ignore :)
+   */
   ignore("should create savepoint and save to disk") {
-    val topicConfig = createAndRegisterTopicConfig(inTopic, RecordSchemaV1)
+    val inputConfig = createAndRegisterTopicConfig(inTopic, RecordSchemaV1)
+    val outputConfig = createAndRegisterTopicConfig(outTopic, RecordSchemaV1)
 
     val clusterClient = flinkMiniCluster.asInstanceOf[FlinkMiniClusterHolderImpl].getClusterClient
-    sendAvro(givenMatchingAvroObj, topicConfig.input)
+    sendAvro(givenMatchingAvroObj, inputConfig.input)
 
-    run(stateCompatibilityProcess(topicConfig.input), { jobExecutionResult =>
+    run(stateCompatibilityProcess(inputConfig.input, outputConfig.input), { jobExecutionResult =>
       verifyOutputEvent(input = event1, previousInput = event1)
 
       val savepointLocation = eventually {
@@ -107,27 +145,29 @@ class StateCompatibilityTest extends FlinkWithKafkaSuite with Eventually with La
   }
 
   test("should restore from snapshot") {
-    val topicConfig = createAndRegisterTopicConfig(inTopic, RecordSchemaV1)
+    val inputConfig = createAndRegisterTopicConfig(inTopic, RecordSchemaV1)
+    val outputConfig = createAndRegisterTopicConfig(outTopic, outputSchema)
 
     val existingSavepointLocation = Files.list(savepointDir).iterator().asScala.toList.head
     val env = flinkMiniCluster.createExecutionEnvironment()
-    val process1 = stateCompatibilityProcess(topicConfig.input)
+    val process1 = stateCompatibilityProcess(inputConfig.input, outputConfig.input)
     registrar.register(new StreamExecutionEnvironment(env), process1, ProcessVersion.empty, DeploymentData.empty)
     val streamGraph = env.getStreamGraph
     val allowNonRestoredState = false
     streamGraph.setSavepointRestoreSettings(SavepointRestoreSettings.forPath(existingSavepointLocation.toString, allowNonRestoredState))
-    // Send one artificial message to mimic offsets saved in savepoint from the above test because kafka commit cannot be performed.
-    sendAvro(givenMatchingAvroObj, topicConfig.input)
+    // Send one artificial message to mimic offsets saved in savepoint from the above test because kafka commit cannot be performed
+    sendAvro(givenMatchingAvroObj, inputConfig.input)
 
     val jobExecutionResult = env.execute(streamGraph)
     env.waitForStart(jobExecutionResult.getJobID, process1.id)()
-    sendAvro(givenNotMatchingAvroObj, topicConfig.input)
+    sendAvro(givenNotMatchingAvroObj, inputConfig.input)
 
     verifyOutputEvent(input = event2, previousInput = event1)
     env.stopJob(process1.id, jobExecutionResult)
   }
 
   private def verifyOutputEvent(input: InputEvent, previousInput: InputEvent): Unit = {
+    //todo read avro
     val rawOutputEvent = kafkaClient.createConsumer().consume(outTopic).take(1).head.msg
     val outputEvent = io.circe.parser.decode[OutputEvent](new String(rawOutputEvent)).right.get
     outputEvent.input shouldBe input
