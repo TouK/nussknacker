a801f26adaffc12c007d10809746a3421c86b4d7
Michal Hamuda <mhm@touk.pl>
2022-08-18 12:47:39 +0200

    Add DelayedKafkaAvroSourceFactory

---
+++ utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/source/delayed/DelayedUniversalKafkaSourceFactory.scala
@@ -0,0 +1,52 @@
+package pl.touk.nussknacker.engine.schemedkafka.source.delayed
+
+import cats.data.Validated.{Invalid, Valid}
+import pl.touk.nussknacker.engine.api.NodeId
+import pl.touk.nussknacker.engine.api.context.ValidationContext
+import pl.touk.nussknacker.engine.api.context.transformation.{DefinedEagerParameter, NodeDependencyValue}
+import pl.touk.nussknacker.engine.api.definition.Parameter
+import pl.touk.nussknacker.engine.api.process.ProcessObjectDependencies
+import pl.touk.nussknacker.engine.kafka.source.KafkaSourceFactory.KafkaSourceImplFactory
+import pl.touk.nussknacker.engine.kafka.source.delayed.DelayedKafkaSourceFactory.{DelayParameter, DelayParameterName, TimestampFieldParamName, TimestampFieldParameter, validateDelay, validateTimestampField}
+import pl.touk.nussknacker.engine.schemedkafka.KafkaUniversalComponentTransformer.SchemaVersionParamName
+import pl.touk.nussknacker.engine.schemedkafka.schemaregistry.{SchemaBasedSerdeProvider, SchemaRegistryClientFactory}
+import pl.touk.nussknacker.engine.schemedkafka.source.UniversalKafkaSourceFactory
+
+import scala.reflect.ClassTag
+
+class DelayedUniversalKafkaSourceFactory[K: ClassTag, V: ClassTag](schemaRegistryClientFactory: SchemaRegistryClientFactory,
+                                                                   schemaBasedMessagesSerdeProvider: SchemaBasedSerdeProvider,
+                                                                   processObjectDependencies: ProcessObjectDependencies,
+                                                                   implProvider: KafkaSourceImplFactory[K, V])
+  extends UniversalKafkaSourceFactory[K, V](schemaRegistryClientFactory, schemaBasedMessagesSerdeProvider, processObjectDependencies, implProvider) {
+
+  override def paramsDeterminedAfterSchema: List[Parameter] = super.paramsDeterminedAfterSchema ++ List(
+    TimestampFieldParameter, DelayParameter
+  )
+
+  override protected def nextSteps(context: ValidationContext, dependencies: List[NodeDependencyValue])
+                                  (implicit nodeId: NodeId): NodeTransformationDefinition = {
+    case step@TransformationStep(
+    (`topicParamName`, DefinedEagerParameter(topic: String, _)) ::
+      (SchemaVersionParamName, DefinedEagerParameter(version: String, _)) ::
+      (TimestampFieldParamName, DefinedEagerParameter(field, _)) ::
+      (DelayParameterName, DefinedEagerParameter(delay, _)) :: Nil, _) =>
+      val preparedTopic = prepareTopic(topic)
+      val versionOption = parseVersionOption(version)
+      val valueValidationResult = determineSchemaAndType(prepareUniversalValueSchemaDeterminer(preparedTopic, versionOption), Some(SchemaVersionParamName))
+
+      valueValidationResult match {
+        case Valid((valueRuntimeSchema, typingResult)) =>
+          val delayValidationErrors = Option(delay.asInstanceOf[java.lang.Long]).map(d => validateDelay(d)).getOrElse(Nil)
+          val timestampValidationErrors = Option(field.asInstanceOf[String]).map(f => validateTimestampField(f, typingResult)).getOrElse(Nil)
+          val errors = delayValidationErrors ++ timestampValidationErrors
+          prepareSourceFinalResults(preparedTopic, valueValidationResult, context, dependencies, step.parameters, errors)
+        case Invalid(exc) =>
+          prepareSourceFinalErrors(context, dependencies, step.parameters, List(exc))
+      }
+    case step@TransformationStep((`topicParamName`, _) :: (SchemaVersionParamName, _) :: (TimestampFieldParamName, _) :: (DelayParameterName, _) :: Nil, _) =>
+      prepareSourceFinalErrors(context, dependencies, step.parameters, errors = Nil)
+  }
+
+}
+
