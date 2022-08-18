0707ca3a8ecd99a33b67375de30ec5af39124541
Dawid Sula <82097893+dawidsula26@users.noreply.github.com>
2022-08-18 14:25:31 +0200

    Generic function documentation (#3422)
    * Added base for documentation
    
    * Started docs
    
    * More documentation
    
    * Some work on docs - plus function
    
    * First version of docs
    
    * Updated devCreator.json

---
+++ engine/flink/management/dev-model/src/main/scala/pl/touk/nussknacker/engine/management/sample/global/DocumentationFunctions.scala
@@ -0,0 +1,115 @@
+package pl.touk.nussknacker.engine.management.sample.global
+
+import cats.data.{NonEmptyList, ValidatedNel}
+import cats.implicits.catsSyntaxValidatedId
+import pl.touk.nussknacker.engine.api.generics.GenericFunctionTypingError.{ArgumentTypeError, OtherError}
+import pl.touk.nussknacker.engine.api.generics.{GenericFunctionTypingError, GenericType, MethodTypeInfo, Parameter, TypingFunction}
+import pl.touk.nussknacker.engine.api.typed.supertype.{CommonSupertypeFinder, NumberTypesPromotionStrategy, SupertypeClassResolutionStrategy}
+import pl.touk.nussknacker.engine.api.typed.typing.{Typed, TypedClass, TypedObjectTypingResult, TypedObjectWithValue, TypingResult, Unknown}
+
+import scala.annotation.varargs
+import scala.jdk.CollectionConverters.seqAsJavaListConverter
+
+object DocumentationFunctions {
+  @GenericType(typingFunction = classOf[HeadGenericFunction])
+  def head[T](list: java.util.List[T]): T =
+    list.get(0)
+
+  private class HeadGenericFunction extends TypingFunction {
+    private val listClass = classOf[java.util.List[_]]
+
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = {
+      arguments match {
+        case TypedClass(`listClass`, t :: Nil) :: Nil => t.validNel
+        case _ => ArgumentTypeError.invalidNel
+      }
+    }
+  }
+
+  @GenericType(typingFunction = classOf[PlusGenericFunction])
+  def plus(left: Any, right: Any): Any = (left, right) match {
+    case (left: Int, right: Int) => left + right
+    case (left: Double, right: Double) => left + right
+    case (left: String, right: String) => left + right
+    case _ => throw new AssertionError("should not be reached")
+  }
+
+  private class PlusGenericFunction() extends TypingFunction {
+    private val numberType = Typed.typedClass[Number]
+    private val intType = Typed.typedClass[Int]
+    private val doubleType = Typed.typedClass[Double]
+    private val stringType = Typed.typedClass[String]
+
+    override def signatures: Option[NonEmptyList[MethodTypeInfo]] = Some(NonEmptyList.of(
+      MethodTypeInfo.withoutVarargs(Parameter("left", numberType) :: Parameter("right", numberType) :: Nil, numberType),
+      MethodTypeInfo.withoutVarargs(Parameter("left", stringType) :: Parameter("right", stringType) :: Nil, stringType)
+    ))
+
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case left :: right :: Nil =>
+        (left.withoutValue, right.withoutValue) match {
+          case (`intType`, `intType`) => intType.validNel
+          case (`doubleType`, `doubleType`) => doubleType.validNel
+          case (l, r) if List(l, r).forall(_.canBeSubclassOf(numberType)) =>
+            OtherError(s"Addition of ${l.display} and ${r.display} is not supported").invalidNel
+          case (`stringType`, `stringType`) => stringType.validNel
+          case _ => ArgumentTypeError.invalidNel
+        }
+      case _ => ArgumentTypeError.invalidNel
+    }
+  }
+
+  @GenericType(typingFunction = classOf[UnionGenericFunction])
+  def union(x: java.util.Map[String, Any], y: java.util.Map[String, Any]): java.util.Map[String, Any] = {
+    val res = new java.util.HashMap[String, Any](x)
+    res.putAll(y)
+    res
+  }
+
+  private class UnionGenericFunction extends TypingFunction {
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case TypedObjectTypingResult(x, _, infoX) :: TypedObjectTypingResult(y, _, infoY) :: Nil =>
+        if (x.keys.exists(y.keys.toSet.contains))
+          OtherError("Argument maps have common field").invalidNel
+        else
+          TypedObjectTypingResult(x ++ y, Typed.typedClass[java.util.HashMap[_, _]], infoX ++ infoY).validNel
+      case _ =>
+        ArgumentTypeError.invalidNel
+    }
+  }
+
+  @GenericType(typingFunction = classOf[GetFieldGenericFunction])
+  def getField(obj: java.util.Map[String, Any], name: String): Any =
+    obj.get(name)
+
+  private class GetFieldGenericFunction extends TypingFunction {
+    private val stringType = Typed.typedClass[String]
+
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case TypedObjectTypingResult(fields, _, _) :: TypedObjectWithValue(`stringType`, name: String) :: Nil =>
+        fields.get(name) match {
+          case Some(v) => v.validNel
+          case None => OtherError("No field with given name").invalidNel
+        }
+      case TypedObjectTypingResult(_, _, _) :: x :: Nil if x.canBeSubclassOf(stringType) =>
+        OtherError("Expected string with known value").invalidNel
+      case _ =>
+        ArgumentTypeError.invalidNel
+    }
+  }
+
+  @GenericType(typingFunction = classOf[ToListGenericFunction])
+  @varargs
+  def toList[T](elems: T*): java.util.List[T] =
+    elems.asJava
+
+  private class ToListGenericFunction extends TypingFunction {
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = {
+      val supertypeFinder = new CommonSupertypeFinder(SupertypeClassResolutionStrategy.AnySuperclass, true)
+      val commonSupertype = arguments
+        .reduceOption(supertypeFinder.commonSupertype(_, _)(NumberTypesPromotionStrategy.ToSupertype))
+        .getOrElse(Unknown)
+      Typed.genericTypeClass[java.util.List[_]](commonSupertype :: Nil).validNel
+    }
+  }
+}
---
+++ engine/flink/management/dev-model/src/main/scala/pl/touk/nussknacker/engine/management/sample/global/ExampleFunctions.scala
@@ -0,0 +1,115 @@
+package pl.touk.nussknacker.engine.management.sample.global
+
+import cats.data.{NonEmptyList, ValidatedNel}
+import cats.implicits.catsSyntaxValidatedId
+import pl.touk.nussknacker.engine.api.Documentation
+import pl.touk.nussknacker.engine.api.generics.GenericFunctionTypingError.ArgumentTypeError
+import pl.touk.nussknacker.engine.api.generics.{GenericFunctionTypingError, GenericType, MethodTypeInfo, Parameter, TypingFunction}
+import pl.touk.nussknacker.engine.api.typed.typing.{Typed, TypedClass, TypedObjectTypingResult, TypedObjectWithValue, TypingResult}
+
+import scala.annotation.varargs
+import scala.jdk.CollectionConverters.collectionAsScalaIterableConverter
+
+object ExampleFunctions {
+  @Documentation(description = "returns first element of list")
+  @GenericType(typingFunction = classOf[HeadHelper])
+  def head[T >: Null](list: java.util.List[T]): T =
+    list.asScala.headOption.orNull
+
+  private class HeadHelper extends TypingFunction {
+    private val listClass = classOf[java.util.List[_]]
+
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case TypedClass(`listClass`, t :: Nil) :: Nil => t.validNel
+      case TypedClass(`listClass`, _) :: Nil => throw new AssertionError("Lists must have one parameter")
+      case _ => GenericFunctionTypingError.ArgumentTypeError.invalidNel
+    }
+  }
+
+
+  @Documentation(description = "returns example of given type")
+  @GenericType(typingFunction = classOf[ExampleOfTypeHelper])
+  def exampleOfType(typeName: String): Object = ???
+
+  private class ExampleOfTypeHelper extends TypingFunction {
+    private val stringClass = classOf[String]
+    private val expectedArgument = Typed(Typed.fromInstance("String"), Typed.fromInstance("Double"), Typed.fromInstance("Int"))
+    private val expectedResult = Typed(Typed[String], Typed[Double], Typed[Int])
+
+    override def signatures: Option[NonEmptyList[MethodTypeInfo]] =
+      Some(NonEmptyList.one(MethodTypeInfo(List(Parameter("typeName", expectedArgument)), None, expectedResult)))
+
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case TypedObjectWithValue(TypedClass(`stringClass`, Nil), typ: String) :: Nil => typ match {
+        case "String" => Typed[String].validNel
+        case "Int" => Typed[Int].validNel
+        case "Double" => Typed[Double].validNel
+        case _ => GenericFunctionTypingError.ArgumentTypeError.invalidNel
+      }
+      case a@TypedObjectWithValue(TypedClass(`stringClass`, _), _) :: Nil =>
+        throw new AssertionError(s"Found illegal type $a")
+      case TypedClass(`stringClass`, Nil) :: Nil =>
+        GenericFunctionTypingError.OtherError("Expected string with known value").invalidNel
+      case a@TypedClass(`stringClass`, _) :: Nil =>
+        throw new AssertionError(s"Found illegal type $a")
+      case _ =>
+        GenericFunctionTypingError.ArgumentTypeError.invalidNel
+    }
+  }
+
+
+  @Documentation(description = "fails to compile when given object without field 'a'")
+  @GenericType(typingFunction = classOf[RequiresFiledAHelper])
+  def getFieldA(obj: Any): Any = ???
+
+  private class RequiresFiledAHelper extends TypingFunction {
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case TypedObjectTypingResult(fields, _, _) :: Nil => fields.get("a") match {
+        case Some(x) => x.validNel
+        case None => GenericFunctionTypingError.OtherError("Given object does not have field 'a'").invalidNel
+      }
+      case _ =>
+        GenericFunctionTypingError.ArgumentTypeError.invalidNel
+    }
+  }
+
+
+  @Documentation(description = "adds field 'a' to given object")
+  @GenericType(typingFunction = classOf[AddFieldAHelper])
+  def addFieldA(obj: Any): Any = ???
+
+  private class AddFieldAHelper extends TypingFunction {
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = arguments match {
+      case TypedObjectTypingResult(fields, obj, info) :: Nil => fields.get("a") match {
+        case Some(_) => GenericFunctionTypingError.OtherError("Given object already has field 'a'").invalidNel
+        case None => TypedObjectTypingResult(fields + ("a" -> Typed[Int]), obj, info).validNel
+      }
+      case _ =>
+        GenericFunctionTypingError.ArgumentTypeError.invalidNel
+    }
+  }
+
+  @Documentation(description = "zips up to 3 numbers")
+  @GenericType(typingFunction = classOf[ZipHelper])
+  @varargs
+  def zip(x: Number*): Any = ???
+
+  private class ZipHelper extends TypingFunction {
+    override def signatures: Option[NonEmptyList[MethodTypeInfo]] =
+      Some(NonEmptyList.of(
+        MethodTypeInfo(Parameter("arg", Typed[Number]) :: Nil, None, Typed[Number]),
+        MethodTypeInfo(Parameter("left", Typed[Number]) :: Parameter("right", Typed[Number]) :: Nil, None, Typed[Number]),
+        MethodTypeInfo(Parameter("left", Typed[Number]) :: Parameter("mid", Typed[Number]) :: Parameter("right", Typed[Number]) :: Nil, None, Typed[Number])
+      ))
+
+    override def computeResultType(arguments: List[TypingResult]): ValidatedNel[GenericFunctionTypingError, TypingResult] = {
+      if (arguments.exists(!_.canBeSubclassOf(Typed[Number]))) return ArgumentTypeError.invalidNel
+      arguments match {
+        case t :: Nil => t.validNel
+        case l :: r :: Nil => TypedObjectTypingResult(List("left" -> l, "right" -> r)).validNel
+        case l :: m :: r :: Nil => TypedObjectTypingResult(List("left" -> l, "mid" -> m, "right" -> r)).validNel
+        case _ => ArgumentTypeError.invalidNel
+      }
+    }
+  }
+}
