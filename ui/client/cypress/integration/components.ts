a997800e8648e9097913165593a64b22cec9c1a7
Krzysztof Gadomski <kgd@touk.pl>
2022-08-29 09:20:37 +0200

    fix cypress

--- ui/client/cypress/integration/components.ts
+++ ui/client/cypress/integration/components.ts
@@ -129,19 +129,15 @@ describe("Components list", () => {
     cy.contains(/^≥ 1$/i).click()
     cy.get("body").click()
 
-    cy.get("#app-container>main").toMatchImageSnapshot({
-      screenshotConfig: {clip: {x: 0, y: 0, width: 1400, height: 300}},
-    })
-    
     cy.get("[role=row] a")
       // this number is two times larger than number of components with some usages because it handles also links to documentation
-      .should("have.length", 4)
+      .should("have.length", 6)
       .as("links")
 
     // we are clicking filter component because it has many usages and we are able to test usages list expansion
     cy.get("@links")
       .filter((i, e) => /^\d+$/.test(e.innerText))
-      .its(1)
+      .its(2)
       .click()
 
     // we are clicking "X more" on list of places of usages to test usages list expansion
