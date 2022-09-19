9a27a93b0292f1dc82d21595df2ef194cfacdfcf
Maciek Próchniak <mpr@touk.pl>
2022-09-19 08:32:49 +0200

    Ouput names on FE

---
+++ ui/client/components/graph/node-modal/ParameterOutputList.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import {NodeField} from "./NodeField";
+import {FieldType} from "./editors/field/Field";
+import {errorValidator} from "./editors/Validators";
+import {NodeType, NodeValidationError, ProcessDefinitionData} from "../../../types";
+import ProcessUtils from "../../../common/ProcessUtils";
+
+export default function ParameterOutputList({
+                                                editedNode,
+                                                processDefinitionData,
+                                                fieldErrors,
+                                                isEditMode,
+                                                showValidation,
+                                                renderFieldLabel,
+                                                setProperty,
+                                            }: {
+    editedNode: NodeType,
+    processDefinitionData: ProcessDefinitionData,
+    renderFieldLabel: (paramName: string) => JSX.Element,
+    setProperty: <K extends keyof NodeType>(property: K, newValue: NodeType[K], defaultValue?: NodeType[K]) => void,
+    fieldErrors?: NodeValidationError[],
+    showValidation?: boolean,
+    isEditMode?: boolean,
+}) {
+    const parameters = ProcessUtils.findNodeObjectTypeDefinition(editedNode, processDefinitionData.processDefinition)?.outputParameters
+    return parameters && parameters.length === 0 ? null : (
+        <div className="node-row" key="outputParameters">
+            <div className="node-label" title="Fragment outputs names">Outputs names:</div>
+            <div className="node-value">
+                <div className="fieldsControl">
+                    {
+                        parameters.map(paramName => {
+                            const paramProperty = `ref.outputParameters.${paramName}`
+                            return (<NodeField
+                                    key={paramName}
+                                    isEditMode={isEditMode}
+                                    showValidation={showValidation}
+                                    node={editedNode}
+                                    renderFieldLabel={renderFieldLabel}
+                                    setProperty={setProperty}
+                                    fieldType={FieldType.input}
+                                    fieldLabel={paramName}
+                                    fieldProperty={paramProperty}
+                                    validators={[errorValidator(fieldErrors || [], paramProperty)]}
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
