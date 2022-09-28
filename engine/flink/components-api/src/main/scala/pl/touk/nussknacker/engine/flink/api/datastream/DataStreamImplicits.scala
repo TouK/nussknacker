3f267cb7bba32a78c1c64b1cdfe1adb13f0e6b24
Dawid Sula <dsl@touk.pl>
2022-09-28 09:19:38 +0200

    Corrected error in mapWithState

--- engine/flink/components-api/src/main/scala/pl/touk/nussknacker/engine/flink/api/datastream/DataStreamImplicits.scala
+++ engine/flink/components-api/src/main/scala/pl/touk/nussknacker/engine/flink/api/datastream/DataStreamImplicits.scala
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.datastream.DataStream
 object DataStreamImplicits {
   implicit class DataStreamExtension[T](stream: DataStream[T]) {
     def mapWithState[R: TypeInformation, S: TypeInformation](fun: (T, Option[S]) => (R, Option[S])): DataStream[R] = {
+      val cleanFun = stream.getExecutionEnvironment.clean(fun)
       val stateTypeInfo: TypeInformation[S] = implicitly[TypeInformation[S]]
       val serializer: TypeSerializer[S] = stateTypeInfo.createSerializer(stream.getExecutionConfig)
 
@@ -16,11 +17,11 @@ object DataStreamImplicits {
         override val stateSerializer: TypeSerializer[S] = serializer
 
         override def map(in: T): R = {
-          applyWithState(in, fun)
+          applyWithState(in, cleanFun)
         }
       }
 
-      stream.map(mapper)
+      stream.map(mapper).returns(implicitly[TypeInformation[R]])
     }
   }
 }
