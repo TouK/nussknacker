32031e61bb485748063dafe506881daac6cc8d4e
Maciek Próchniak <mpr@touk.pl>
2022-10-06 11:00:27 +0200

    Fix testing from file with DisplayJsonWithEncoder

--- engine/flink/components-utils/src/main/scala/pl/touk/nussknacker/engine/flink/util/source/CollectionSource.scala
+++ engine/flink/components-utils/src/main/scala/pl/touk/nussknacker/engine/flink/util/source/CollectionSource.scala
@@ -2,7 +2,6 @@ package pl.touk.nussknacker.engine.flink.util.source
 
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.functions.source.FromElementsFunction
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import pl.touk.nussknacker.engine.api.typed.ReturningType
 import pl.touk.nussknacker.engine.api.typed.typing.TypingResult
 import pl.touk.nussknacker.engine.flink.api.process.BasicFlinkSource
@@ -14,7 +13,7 @@ case class CollectionSource[T: TypeInformation](list: List[T],
                                                 timestampAssigner: Option[TimestampWatermarkHandler[T]],
                                                 returnType: TypingResult
                                                ) extends BasicFlinkSource[T] with ReturningType {
-  override def flinkSourceFunction = new FromElementsFunction[T](typeInformation.createSerializer(StreamExecutionEnvironment.getExecutionEnvironment.getConfig), list.filterNot(_ == null).asJava)
+  override def flinkSourceFunction = new FromElementsFunction[T](list.filterNot(_ == null).asJava)
 
   override val typeInformation: TypeInformation[T] = implicitly[TypeInformation[T]]
 
