c1a0b5c3ddd280fd75ca5d1f3dba5532e1d60732
Krzysztof Gadomski <kgd@touk.pl>
2022-10-07 14:46:55 +0200

    do not cache main.html

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/WebResources.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/WebResources.scala
@@ -1,39 +1,41 @@
 package pl.touk.nussknacker.ui.api
 
 import akka.http.scaladsl.model.headers.{CacheDirectives, `Cache-Control`}
-
-import java.nio.file.Files
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.{Directives, Route}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.commons.io.{FileUtils, IOUtils}
+import org.apache.commons.io.IOUtils
 import pl.touk.nussknacker.engine.version.BuildInfo
 import pl.touk.nussknacker.ui.config.{UsageStatisticsReportsConfig, UsageStatisticsUrl}
 
 class WebResources(publicPath: String, usageStatisticsReports: UsageStatisticsReportsConfig) extends Directives with LazyLogging {
 
   //see config.js comment
-  private lazy val mainContentFile = {
-    val tempMainContentFile = Files.createTempFile("nussknacker", "main.html").toFile
-    tempMainContentFile.deleteOnExit()
+  private lazy val mainContent = {
     val data = getClass.getClassLoader.getResourceAsStream("web/static/main.html")
     val content = Option(data).map(IOUtils.toString).getOrElse {
       logger.error("Failed to find web/static/main.html - probably frontend resources are not packaged in jar. Frontend won't work properly!")
       ""
     }
-    FileUtils.writeStringToFile(tempMainContentFile, withUsageStatisticsReporting(content.replace("__publicPath__", publicPath)))
-    tempMainContentFile
+    content.replace("__publicPath__", publicPath)
   }
 
   val route: Route = handleAssets("submodules") ~ handleAssets("static") ~ get {
     //main.html instead of index.html to not interfere with flink's static resources...
-    getFromFile(mainContentFile) //return UI page by default to make links work
+    noCache {
+      val data = ByteString(withUsageStatisticsReporting(mainContent))
+      complete(HttpEntity.Default(ContentTypes.`text/html(UTF-8)`, data.length, Source.single(data)))
+    }
   }
+
 
   private def handleAssets(webSubfolder: String) = {
     pathPrefix(webSubfolder) {
       get {
         encodeResponse {
-          respondWithHeader(`Cache-Control`(List(CacheDirectives.public, CacheDirectives.`max-age`(0)))) {
+          noCache {
             extractRequest { matched =>
               logger.debug(s"Try to get static data from $webSubfolder for:  ${matched.uri.path}.")
               getFromResourceDirectory(s"web/$webSubfolder")
@@ -43,6 +45,8 @@ class WebResources(publicPath: String, usageStatisticsReports: UsageStatisticsRe
       }
     }
   }
+
+  private def noCache = respondWithHeader(`Cache-Control`(List(CacheDirectives.public, CacheDirectives.`max-age`(0))))
 
   private def withUsageStatisticsReporting(html: String) = {
     if(usageStatisticsReports.enabled) html.replace("</body>", s"""<img src="${UsageStatisticsUrl(usageStatisticsReports.fingerprint, BuildInfo.version)}" alt="" hidden /></body>""")
