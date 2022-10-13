2c4dfe88c895659ee8dc8c0d7eef36319472e972
Dawid Sula <dsl@touk.pl>
2022-10-13 11:31:02 +0200

    Bugfix in ContextType

--- engine/flink/components-utils/src/main/scala/pl/touk/nussknacker/engine/flink/typeinformation/ContextType.scala
+++ engine/flink/components-utils/src/main/scala/pl/touk/nussknacker/engine/flink/typeinformation/ContextType.scala
@@ -1,9 +1,13 @@
 package pl.touk.nussknacker.engine.flink.typeinformation
 
 import org.apache.flink.api.common.typeinfo.{TypeHint, TypeInformation}
+import org.apache.flink.api.common.typeutils.{SimpleTypeSerializerSnapshot, TypeSerializer, TypeSerializerSnapshot}
 import org.apache.flink.api.scala.typeutils.OptionTypeInfo
 import pl.touk.nussknacker.engine.api.Context
 import pl.touk.nussknacker.engine.flink.api.process.FlinkCustomNodeContext
+import pl.touk.nussknacker.engine.flink.typeinformation.FixedValueTypeInformationHelper.NullSerializer
+
+import java.util.function.Supplier
 
 object ContextType {
   def infoFromVariablesAndParent(variables: TypeInformation[Map[String, Any]], parentCtx: TypeInformation[Option[Context]]): TypeInformation[Context] =
@@ -14,9 +18,8 @@ object ContextType {
     )
 
   def infoFromVariablesAndParentOption(variables: TypeInformation[Map[String, Any]], parentOpt: Option[TypeInformation[Context]]): TypeInformation[Context] = {
-    val parentOptionInfo: Option[TypeInformation[Option[Context]]] = parentOpt.map(new OptionTypeInfo(_))
-    val parentInfo: TypeInformation[Option[Context]] = parentOptionInfo.getOrElse(FixedValueTypeInformationHelper.nullValueTypeInfo[Option[Context]])
-    infoFromVariablesAndParent(variables, parentInfo)
+    val parentCtx = new OptionTypeInfo[Context, Option[Context]](parentOpt.getOrElse(FixedValueTypeInformationHelper.nullValueTypeInfo))
+    infoFromVariablesAndParent(variables, parentCtx)
   }
 
   def infoFromVariables(variables: TypeInformation[Map[String, Any]]): TypeInformation[Context] =
