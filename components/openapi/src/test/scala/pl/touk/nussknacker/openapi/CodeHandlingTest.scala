3cd9130539cbb6c9d89b2ec0bf2942a7a93fc900
Maciek Próchniak <mpr@touk.pl>
2022-11-21 14:15:47 +0100

    more tests

---
+++ components/openapi/src/test/scala/pl/touk/nussknacker/openapi/CodeHandlingTest.scala
@@ -0,0 +1,46 @@
+package pl.touk.nussknacker.openapi
+
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.test.PatientScalaFutures
+import sttp.client.Response
+import sttp.client.testing.SttpBackendStub
+import sttp.model.StatusCode
+import pl.touk.nussknacker.engine.api.test.EmptyInvocationCollector.Instance
+import pl.touk.nussknacker.engine.api.typed.TypedMap
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class CodeHandlingTest extends  AnyFunSuite with BeforeAndAfterAll with Matchers with LazyLogging with PatientScalaFutures with BaseOpenAPITest {
+
+  private val codeParameter = "code"
+
+  private val backend = SttpBackendStub.asynchronousFuture.whenRequestMatchesPartial {
+    case request =>
+      val code = request.uri.params.get(codeParameter).get.toInt
+      Response("{}", StatusCode(code))
+  }
+
+  test("should handle configured response codes") {
+    val customEmptyCode = 204
+    val config = OpenAPIServicesConfig(codesToInterpretAsEmpty = List(customEmptyCode))
+    val service = parseToEnrichers("custom-codes.yml", backend, config)(ServiceName("code"))
+
+    def invokeWithCode(code: Int) =
+      service.invoke(Map(codeParameter -> code)).futureValue.asInstanceOf[AnyRef]
+
+    invokeWithCode(customEmptyCode) shouldBe TypedMap(Map.empty)
+    invokeWithCode(200) shouldBe TypedMap(Map.empty)
+
+    intercept[Exception] {
+      invokeWithCode(404)
+    }
+    intercept[Exception] {
+      invokeWithCode(503)
+    }
+
+  }
+
+}
--- components/openapi/src/test/scala/pl/touk/nussknacker/openapi/SecurityTest.scala
+++ components/openapi/src/test/scala/pl/touk/nussknacker/openapi/SecurityTest.scala
@@ -1,35 +1,20 @@
-package pl.touk.nussknacker.openapi.functional
+package pl.touk.nussknacker.openapi
 
-import cats.data.Validated.Valid
 import com.typesafe.scalalogging.LazyLogging
-import org.scalatest.{Assertion, BeforeAndAfterAll}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import pl.touk.nussknacker.engine.api._
-import pl.touk.nussknacker.engine.api.process.ComponentUseCase
+import org.scalatest.{Assertion, BeforeAndAfterAll}
 import pl.touk.nussknacker.engine.api.test.EmptyInvocationCollector.Instance
 import pl.touk.nussknacker.engine.api.typed.TypedMap
-import pl.touk.nussknacker.engine.util.runtimecontext.TestEngineRuntimeContext
-import pl.touk.nussknacker.engine.util.service.EagerServiceWithStaticParametersAndReturnType
-import pl.touk.nussknacker.openapi.enrichers.{SwaggerEnricherCreator, SwaggerEnrichers}
-import pl.touk.nussknacker.openapi.parser.SwaggerParser
-import pl.touk.nussknacker.openapi.{ApiKeyConfig, OpenAPIServicesConfig, ServiceName}
 import pl.touk.nussknacker.test.PatientScalaFutures
 import sttp.client.testing.SttpBackendStub
 import sttp.client.{Request, Response}
 import sttp.model.{HeaderNames, StatusCode}
 
-import java.net.URL
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
-import scala.io.Source
+import scala.concurrent.Future
 
-class SecurityTest extends AnyFunSuite with BeforeAndAfterAll with Matchers with LazyLogging with PatientScalaFutures {
-
-  implicit val componentUseCase: ComponentUseCase = ComponentUseCase.EngineRuntime
-  implicit val metaData: MetaData = MetaData("testProc", StreamMetaData())
-  implicit val contextId: ContextId = ContextId("testContextId")
-  private val context = TestEngineRuntimeContext(JobData(metaData, ProcessVersion.empty))
+class SecurityTest extends AnyFunSuite with BeforeAndAfterAll with Matchers with LazyLogging with PatientScalaFutures with BaseOpenAPITest {
 
   case class Config(path: String,
                     securityName: String,
@@ -66,21 +51,9 @@ class SecurityTest extends AnyFunSuite with BeforeAndAfterAll with Matchers with
         }
       }
     }
-
-
   }
 
   private def enrichersForSecurityConfig(backend: SttpBackendStub[Future, Nothing, Nothing], securities: Map[String, ApiKeyConfig]) = {
-    val definition = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("service-security.yml")).mkString
-    val config = OpenAPIServicesConfig(security = Some(securities))
-    val services = SwaggerParser.parse(definition, config).collect {
-      case Valid(service) => service
-    }
-
-    val enrichers = new SwaggerEnrichers(new URL("http://foo"), None,
-      new SwaggerEnricherCreator((_: ExecutionContext) => backend))
-      .enrichers(services, Nil, Map.empty).map(ed => ed.name -> ed.service.asInstanceOf[EagerServiceWithStaticParametersAndReturnType]).toMap
-    enrichers.foreach(_._2.open(context))
-    enrichers
+    parseToEnrichers("service-security.yml", backend, OpenAPIServicesConfig(security = Some(securities)))
   }
 }
