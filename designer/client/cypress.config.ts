588207b83c3a6157459907cadbd21baec890a6a9
Julian Wielga <j@touk.pl>
2022-12-01 15:33:50 +0100

    fixed update snapshots

--- designer/client/cypress.config.ts
+++ designer/client/cypress.config.ts
@@ -15,9 +15,33 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.isHeadless) {
+          if (browser.name === "chrome") {
+            launchOptions.args.push("--window-size=1400,1200")
+          }
+          if (browser.name === "electron") {
+            launchOptions.preferences.width = 1400
+            launchOptions.preferences.height = 1200
+          }
+          if (browser.name === "firefox") {
+            launchOptions.args.push("--width=1400")
+            launchOptions.args.push("--height=1200")
+          }
+        } else {
+          if (browser.name === "electron") {
+            launchOptions.preferences.fullscreen = true
+          } else if (browser.family === "chromium") {
+            launchOptions.args.push("--start-fullscreen")
+          }
+        }
+
+        return launchOptions
+      })
       return require("./cypress/plugins/index.js")(on, config)
     },
     baseUrl: "http://localhost:3000",
     excludeSpecPattern: ["**/__snapshots__/*", "**/__image_snapshots__/*"],
+
   },
 })
