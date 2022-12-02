e60fde13d1250a9529e96cbadcf4de4f414601e9
Maciek Próchniak <mpr@touk.pl>
2022-12-02 08:13:39 +0100

    fix

--- designer/restmodel/src/main/scala/pl/touk/nussknacker/restmodel/validation/PrettyValidationErrors.scala
+++ designer/restmodel/src/main/scala/pl/touk/nussknacker/restmodel/validation/PrettyValidationErrors.scala
@@ -68,7 +68,7 @@ object PrettyValidationErrors {
       case UnknownProperty(propertyName, _) => unknownProperty(typ, propertyName)
       case InvalidPropertyFixedValue(fieldName, label, value, values, _) => invalidPropertyFixedValue(typ, fieldName, label, value, values)
       case CustomNodeError(_, message, paramName) => NodeValidationError(typ, message, message, paramName, NodeValidationErrorType.SaveAllowed)
-      case MultipleOutputsForName(name, _) => node(s"There is more than one output with $name name defined in the fragment, currently this is not allowed", "Please check fragment definition")
+      case MultipleOutputsForName(name, _) => node(s"There is more than one output with '$name' name defined in the fragment, currently this is not allowed", "Please check fragment definition")
     }
   }
 
