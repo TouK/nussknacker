ce37911d52e48c86771c46aa497db888bbcfd27e
Maciej Próchniak <mpr@touk.pl>
2022-12-13 08:50:42 +0100

    JSON Schema: handle anyOf/oneOf for object schemas (#3807)
    * JSON Schema: handle anyOf/oneOf for object schemas
    
    * review

--- utils/json-utils/src/test/scala/pl/touk/nussknacker/engine/json/SwaggerBasedJsonSchemaTypeDefinitionExtractorTest.scala
+++ utils/json-utils/src/test/scala/pl/touk/nussknacker/engine/json/SwaggerBasedJsonSchemaTypeDefinitionExtractorTest.scala
@@ -362,6 +362,27 @@ class SwaggerBasedJsonSchemaTypeDefinitionExtractorTest extends AnyFunSuite with
     result shouldBe TypedObjectTypingResult.apply(results)
   }
 
+  test("should support anyOf/oneOf for object schemas") {
+    //TODO: handle case when we have fields common for both versions on "main" level
+    def schema(compositionType: String) = JsonSchemaBuilder.parseSchema(
+      s"""{
+        |   "type":"object",
+        |   "$compositionType": [
+        |       { "type": "object", "properties": {"passport": {"type": "string" }} },
+        |       { "type": "object", "properties": {"identityCard": {"type": "string" }} },
+        |   ]
+        |}""".stripMargin)
+
+    Table("type", "anyOf", "oneOf").forEvery { compositionType =>
+      val result = SwaggerBasedJsonSchemaTypeDefinitionExtractor.swaggerType(schema(compositionType)).typingResult
+
+      result shouldBe Typed(
+        TypedObjectTypingResult(List(("passport", Typed[String]))),
+        TypedObjectTypingResult(List(("identityCard", Typed[String]))),
+      )
+    }
+  }
+
   test("should support support multiple schemas but of the same type") {
     val schema = JsonSchemaBuilder.parseSchema(
       """{
