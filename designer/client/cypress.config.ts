41e1b3c5228186a2361b47cbb51a68529308b6c2
Tomasz Kalkosiński <tkl@touk.pl>
2022-12-29 13:16:52 +0100

    Set pluginVisualRegressionUpdateImages to true

--- designer/client/cypress.config.ts
+++ designer/client/cypress.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from "cypress"
 export default defineConfig({
   env: {
     pluginVisualRegressionMaxDiffThreshold: 0.008,
-    pluginVisualRegressionUpdateImages: false,
+    pluginVisualRegressionUpdateImages: true,
     pluginVisualRegressionForceDeviceScaleFactor: false,
   },
   reporter: "junit",
