92d4e10f426786a0dbcd3d759bbae779e1b0c507
JulianWielga <j@touk.pl>
2023-01-02 15:28:00 +0100

    fixing/tweaking cypress (#3802)
    * replaced wait with sth more reasonable
    
    * tweak snapshot diff threshold
    
    * tweak snapshot diff threshold
    
    * fixed watch build crash
    
    * update cypress snapshots on fail (overwrite matchImage)
    
    * update snapshots on first run
    
    * Switch Docker Based test to self-hosted runner
    
    * verbose logging for define_schema
    
    * Wait for schema registry to go up
    
    * Slow and K8s tests on self-hosted
    
    * Revert "Slow and K8s tests on self-hosted"
    
    This reverts commit 4d3e1c4df21da0db52f0c167861523f4e5060840.
    
    * disable npm cache for self-hosted
    
    * disable ivy cache for self-hosted
    
    * consistent fonts in aceeditor
    better fonts package
    
    * fixed deps
    
    * run be on some random port in docker tests
    
    * run be on some random port in docker tests
    
    * Updated snapshots
    
    * settle graph after layout
    
    * settle graph after layout
    
    * Updated snapshots
    
    * save videos from tests
    
    * tweak threshold
    
    * Updated snapshots
    
    * fail on snapshots update pr created
    
    * fixing tests
    
    * Updated snapshots
    
    * fixing tests
    
    * crop suggester snapshots
    
    * tweak waits to give graph time to settle
    
    * Updated snapshots
    
    * fixing tests
    
    * fixing tests
    
    * consistent fonts in aceeditor
    
    * Updated snapshots
    
    Co-authored-by: Damian Święcki <dswiecki9@gmail.com>
    Co-authored-by: Tomasz Kalkosiński <tkl@touk.pl>
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>

--- designer/client/cypress/e2e/processSelection.cy.ts
+++ designer/client/cypress/e2e/processSelection.cy.ts
@@ -1,7 +1,7 @@
-describe("Process", () => {
+describe("Process mouse drag", () => {
   const NAME = "processSelection"
   const snapshotParams: Cypress.MatchImageOptions = {
-    maxDiffThreshold: 0.00001,
+    maxDiffThreshold: 0.0001,
     screenshotConfig: {
       blackout: [
         "> :not(#nk-graph-main) > div",
@@ -19,66 +19,61 @@ describe("Process", () => {
 
   beforeEach(() => {
     cy.visitNewProcess(NAME, "testProcess")
-    cy.contains(/^layout$/).click()
     cy.get(".graphPage", {timeout: 20000}).as("graph").within(() => {
       cy.get("#nk-graph-main svg", {timeout: 20000}).as("canvas")
     })
+    cy.get("[title='toggle left panel']").click()
+    cy.layoutScenario()
   })
 
-  describe("mouse drag", () => {
-    beforeEach(() => {
-      cy.get("[title='toggle left panel']").click()
-    })
+  it("should allow pan view", () => {
+    cy.get("@canvas")
+      .trigger("mousedown", 10, 10, {force: true})
+      .trigger("mousemove", 200, 100, {force: true})
+      .trigger("mouseup", {force: true})
+      .wait(200)
+    cy.get("@graph").matchImage(snapshotParams)
+  })
 
-    it("should allow pan view", () => {
-      cy.get("@canvas")
-        .trigger("mousedown", 10, 10, {force: true})
-        .trigger("mousemove", 200, 100, {force: true})
-        .trigger("mouseup", {force: true})
-        .wait(200)
-      cy.get("@graph").matchImage(snapshotParams)
-    })
+  it("should select only fully covered (to right)", () => {
+    cy.get("@canvas")
+      .trigger("keydown", {key: "Meta"})
+      .trigger("mousedown", 300, 100, {metaKey: true, force: true})
+      .trigger("mousemove", 700, 500, {metaKey: true, force: true})
+    cy.get("@graph").matchImage(snapshotParams)
+    cy.get("@canvas")
+      .trigger("mouseup", {force: true})
+    cy.get("@graph").matchImage(snapshotParams)
+  })
 
-    it("should select only fully covered (to right)", () => {
-      cy.get("@canvas")
-        .trigger("keydown", {key: "Meta"})
-        .trigger("mousedown", 300, 100, {metaKey: true, force: true})
-        .trigger("mousemove", 700, 500, {metaKey: true, force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-      cy.get("@canvas")
-        .trigger("mouseup", {force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-    })
+  it("should select partially covered (to left)", () => {
+    cy.get("@canvas")
+      .trigger("keydown", {key: "Meta"})
+      .trigger("mousedown", 700, 100, {metaKey: true, force: true})
+      .trigger("mousemove", 500, 500, {metaKey: true, force: true})
+    cy.get("@graph").matchImage(snapshotParams)
+    cy.get("@canvas")
+      .trigger("mouseup", {force: true})
+    cy.get("@graph").matchImage(snapshotParams)
+  })
 
-    it("should select partially covered (to left)", () => {
-      cy.get("@canvas")
-        .trigger("keydown", {key: "Meta"})
-        .trigger("mousedown", 700, 100, {metaKey: true, force: true})
-        .trigger("mousemove", 500, 500, {metaKey: true, force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-      cy.get("@canvas")
-        .trigger("mouseup", {force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-    })
-
-    it("should switch modes, append and inverse select with shift", () => {
-      cy.get("@canvas")
-        .trigger("keydown", {key: "Meta"})
-        .trigger("mousedown", 700, 100, {metaKey: true, force: true})
-        .trigger("mousemove", 500, 400, {metaKey: true, force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-      cy.get("@canvas")
-        .trigger("mouseup", {force: true})
-        .trigger("keyup", {key: "Meta"})
-      cy.get("@graph").matchImage(snapshotParams)
-      cy.get("@canvas")
-        .trigger("keydown", {key: "Shift"})
-        .trigger("mousedown", 700, 150, {shiftKey: true, force: true})
-        .trigger("mousemove", 500, 550, {shiftKey: true, force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-      cy.get("@canvas")
-        .trigger("mouseup", {force: true})
-      cy.get("@graph").matchImage(snapshotParams)
-    })
+  it("should switch modes, append and inverse select with shift", () => {
+    cy.get("@canvas")
+      .trigger("keydown", {key: "Meta"})
+      .trigger("mousedown", 700, 100, {metaKey: true, force: true})
+      .trigger("mousemove", 500, 400, {metaKey: true, force: true})
+    cy.get("@graph").matchImage(snapshotParams)
+    cy.get("@canvas")
+      .trigger("mouseup", {force: true})
+      .trigger("keyup", {key: "Meta"})
+    cy.get("@graph").matchImage(snapshotParams)
+    cy.get("@canvas")
+      .trigger("keydown", {key: "Shift"})
+      .trigger("mousedown", 700, 150, {shiftKey: true, force: true})
+      .trigger("mousemove", 500, 550, {shiftKey: true, force: true})
+    cy.get("@graph").matchImage(snapshotParams)
+    cy.get("@canvas")
+      .trigger("mouseup", {force: true})
+    cy.get("@graph").matchImage(snapshotParams)
   })
 })
