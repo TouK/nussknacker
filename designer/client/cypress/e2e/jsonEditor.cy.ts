3da8367cf23ab7fb8e77046e07ec7f1f6a898918
Julian Wielga <j@touk.pl>
2023-01-02 15:14:01 +0100

    json editor test (properties of rrk8s scenario)

---
+++ designer/client/cypress/e2e/jsonEditor.cy.ts
@@ -0,0 +1,31 @@
+describe("JSON editor", () => {
+  const seed = "json"
+
+  before(() => {
+    // cy.deleteAllTestProcesses({filter: seed, force: true})
+  })
+
+  after(() => {
+    // cy.deleteAllTestProcesses({filter: seed})
+  })
+
+  it("should display colorfull json", () => {
+    cy.visitNewProcess(seed, "rr", "RequestResponseK8s")
+    cy.contains(/^properties/i).should("be.enabled").click()
+    cy.get("[data-testid=window]").as("modal")
+    cy.get("[title='Input schema']").next().find(".ace_editor").should("be.visible").as("editor")
+
+    cy.get("@editor").matchImage({
+      maxDiffThreshold: 0.0025,
+      screenshotConfig: {padding: [1]},
+    })
+
+    cy.get("@editor").click("center")
+      .type("{enter}false")
+      .wait(300)
+      .matchImage({
+        maxDiffThreshold: 0.0025,
+        screenshotConfig: {padding: [1, 1, 24, 1]},
+      })
+  })
+})
