3d3d84319dee9c41a872c9488bd4f97d455afc88
Arek Burdach <arek.burdach@gmail.com>
2023-01-24 20:09:19 +0100

    credentials fix

--- utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/azure/AzureTokenCredentialFactory.scala
+++ utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/azure/AzureTokenCredentialFactory.scala
@@ -5,27 +5,25 @@ import com.azure.core.util.{Configuration, ConfigurationPropertyBuilder}
 import com.azure.identity.{ChainedTokenCredentialBuilder, ClientSecretCredentialBuilder, DefaultAzureCredentialBuilder}
 import scala.jdk.CollectionConverters._
 
-// This class create credential chain that in the first place uses properties from configuration
-// and when they are absent, fallback to default credentials chain which check many source of credentials:
+// This class in the first place uses properties from configuration and when they are absent, fallback to
+// default credentials chain which check many other sources of credentials:
 // https://github.com/Azure/azure-sdk-for-java/wiki/Azure-Identity-Examples#authenticating-with-defaultazurecredential
 object AzureTokenCredentialFactory {
 
   def createCredential(configuration: Configuration): TokenCredential = {
     def getConfigValueOpt(name: String) = Option(configuration.get(ConfigurationPropertyBuilder.ofString(name).build()))
-    val clientSecretCredentialOpt = (
-      getConfigValueOpt(Configuration.PROPERTY_AZURE_TENANT_ID),
+    (getConfigValueOpt(Configuration.PROPERTY_AZURE_TENANT_ID),
       getConfigValueOpt(Configuration.PROPERTY_AZURE_CLIENT_ID),
       getConfigValueOpt(Configuration.PROPERTY_AZURE_CLIENT_SECRET)) match {
       case (Some(tenantId), Some(clientId), Some(clientSecret)) =>
-        Some(new ClientSecretCredentialBuilder()
+        new ClientSecretCredentialBuilder()
           .tenantId(tenantId)
           .clientId(clientId)
           .clientSecret(clientSecret)
-          .build())
+          .build()
       case _ =>
-        None
+        new DefaultAzureCredentialBuilder().configuration(configuration).build()
     }
-    val chain = clientSecretCredentialOpt.toList :+ new DefaultAzureCredentialBuilder().configuration(configuration).build()
-    new ChainedTokenCredentialBuilder().addAll(chain.asJava).build
   }
+
 }
