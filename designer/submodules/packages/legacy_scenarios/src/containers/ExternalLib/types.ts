09647703de6a40ad183dd9c2088c97f867fa8301
Julian Wielga <j@touk.pl>
2023-01-29 20:26:44 +0100

    move to submodules

--- designer/submodules/packages/legacy_scenarios/src/containers/ExternalLib/types.ts
+++ designer/submodules/packages/legacy_scenarios/src/containers/ExternalLib/types.ts
@@ -1,43 +1,35 @@
-import {ComponentType} from "react"
-
-declare global {
-  let __webpack_init_sharing__: (name: string) => unknown
-  let __webpack_share_scopes__: {
-    [name: string]: unknown,
-    default: unknown,
-  }
-}
+import { ComponentType } from "react";
 
 export interface Container {
-  init(scope: unknown): Promise<unknown>,
+    init(scope: unknown): Promise<unknown>;
 
-  get<M = { default: ComponentType<any> }>(module: PathString): Promise<() => M>,
+    get<M = { default: ComponentType<any> }>(module: PathString): Promise<() => M>;
 }
 
 /**
  * remote name from ModuleFederationPlugin
  */
-export type ScopeString = string
+export type ScopeString = string;
 
 /**
  * remote exposed module "path" from ModuleFederationPlugin
  */
-export type PathString = string
+export type PathString = string;
 
 /**
  * url to remote entry .js file
  */
-export type ScriptUrl = string
+export type ScriptUrl = string;
 
 /**
  * `${ScopeString}/${PathString}`
  */
-export type ModuleString = string
+export type ModuleString = string;
 
 /**
  * `${ModuleString}@${ScriptUrl}`
  */
-export type ModuleUrl = string
+export type ModuleUrl = string;
 
-type Hooks = Record<`use${Capitalize<string>}`, ((...args: any[]) => any)>
-export type Module = {default?: ComponentType<any>} & Hooks
+type Hooks = Record<`use${Capitalize<string>}`, (...args: any[]) => any>;
+export type Module = { default?: ComponentType<any> } & Hooks;
--- designer/submodules/packages/legacy_scenarios/src/types/common.ts
+++ designer/submodules/packages/legacy_scenarios/src/types/common.ts
@@ -1,9 +1,4 @@
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  type $TodoType = any
-}
-
-export type UnknownRecord = Record<string, unknown>
+export type UnknownRecord = Record<string, unknown>;
 
 //from BE comes as date in zulu time
-export type Instant = string
+export type Instant = string;
