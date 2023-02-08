bb14a33a90286ab70023f41adcf1f8a25bc7b9f4
Julian Wielga <j@touk.pl>
2023-02-09 00:07:29 +0100

    fixed mf

--- designer/submodules/packages/legacy_scenarios/src/components/tabs/Tabs.tsx
+++ designer/submodules/packages/legacy_scenarios/src/components/tabs/Tabs.tsx
@@ -1,11 +1,10 @@
 //TODO: move all colors to theme
 import { css, cx } from "@emotion/css";
 import React, { ComponentType, PropsWithChildren } from "react";
-import { Routes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import { useNkTheme } from "../../containers/theme";
 import styles from "./processTabs.styl";
 import { TabLink } from "./TabLink";
-import { Route } from "react-router";
 import { TabContent } from "./TabContent";
 
 type TabData = { path: string; header: string; Component: ComponentType };
@@ -14,6 +13,16 @@ type Props = { tabs: TabData[]; className?: string };
 
 export function Tabs({ tabs, children, className }: PropsWithChildren<Props>) {
     const { theme } = useNkTheme();
+    const routes = useRoutes(
+        tabs.map(({ path, Component, ...t }) => ({
+            path,
+            element: (
+                <TabContent>
+                    <Component />
+                </TabContent>
+            ),
+        })),
+    );
     return (
         <div className={cx(styles.tabsRoot, theme.themeClass, css({ backgroundColor: theme.colors.canvasBackground }), className)}>
             <div className={cx(styles.tabsWrap)}>
@@ -23,21 +32,7 @@ export function Tabs({ tabs, children, className }: PropsWithChildren<Props>) {
                         <TabLink key={data.path} {...data} />
                     ))}
                 </div>
-                <div className={styles.contentWrap}>
-                    <Routes>
-                        {tabs.map(({ path, Component }) => (
-                            <Route
-                                key={path}
-                                path={path}
-                                element={
-                                    <TabContent>
-                                        <Component />
-                                    </TabContent>
-                                }
-                            ></Route>
-                        ))}
-                    </Routes>
-                </div>
+                <div className={styles.contentWrap}>{routes}</div>
             </div>
         </div>
     );
