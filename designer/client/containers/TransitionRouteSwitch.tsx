b062c90243d6d4fde09e93f9bf42eb7ffa058ea7
Julian Wielga <j@touk.pl>
2023-02-18 00:33:14 +0100

    migrate routes & cleanup

--- designer/client/containers/TransitionRouteSwitch.tsx
+++ designer/client/containers/TransitionRouteSwitch.tsx
@@ -1,21 +1,21 @@
 import React, {PropsWithChildren} from "react"
-import {useRouteMatch} from "react-router"
-import {Switch, useLocation} from "react-router-dom"
+import {useLocation, useRouteMatch} from "react-router-dom"
 import {CSSTransition} from "react-transition-group"
 import TransitionGroup from "react-transition-group/TransitionGroup"
 import ErrorBoundary from "../components/common/ErrorBoundary"
 import animations from "../stylesheets/animations.styl"
+import {Routes} from "react-router-dom-v5-compat"
 
 export function TransitionRouteSwitch(props: PropsWithChildren<unknown>): JSX.Element {
   const location = useLocation()
-  const match = useRouteMatch<{root: string}>(`/:root/:rest?`)
+  const match = useRouteMatch<{ root: string }>(`/:root/:rest?`)
   return (
     <TransitionGroup className={animations.group}>
       <CSSTransition key={match?.params?.root} classNames={animations.fade} timeout={{enter: 300, exit: 300}}>
         <ErrorBoundary>
-          <Switch location={location}>
+          <Routes location={location}>
             {props.children}
-          </Switch>
+          </Routes>
         </ErrorBoundary>
       </CSSTransition>
     </TransitionGroup>
