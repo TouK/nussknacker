42b2070ae6f3703a8b4e104c191d0f5c0dbd8470
Arek Burdach <arek.burdach@gmail.com>
2023-02-22 12:23:28 +0100

    Fix for: generate test data mechanism didn't work for json messages with defined schema id (#4018)

--- utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/SchemaWithMetadata.scala
+++ utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/SchemaWithMetadata.scala
@@ -1,10 +1,24 @@
 package pl.touk.nussknacker.engine.schemedkafka.schemaregistry
 
 import io.confluent.kafka.schemaregistry.ParsedSchema
+import pl.touk.nussknacker.engine.schemedkafka.AvroUtils
 
 /**
  * This class holds information that are stored next to schema in registry.
  * It is lightened version of Confluent's SchemaMetadata. We don't want to use their class, because our SchemaRegistryClient
  * is not coupled with concrete schema registry implementation.
+ * This class ensures that ParsedSchema will be in the form expected by other mechanisms (see AvroUtils.adjustParsedSchema)
  */
-case class SchemaWithMetadata(schema: ParsedSchema, id: SchemaId)
+case class SchemaWithMetadata private(schema: ParsedSchema, id: SchemaId)
+
+object SchemaWithMetadata {
+
+  def apply(schema: ParsedSchema, id: SchemaId): SchemaWithMetadata = {
+    new SchemaWithMetadata(AvroUtils.adjustParsedSchema(schema), id)
+  }
+
+  def fromRawSchema(schemaType: String, schemaContent: String, id: SchemaId): SchemaWithMetadata = {
+    new SchemaWithMetadata(AvroUtils.toParsedSchema(schemaType, schemaContent), id)
+  }
+
+}
