31d7365c33a1ba297398acbfde62cd8b016cc60c
Julian Wielga <j@touk.pl>
2023-03-01 17:08:44 +0100

    revert publicPath use

--- designer/client/bootstrap.tsx
+++ designer/client/bootstrap.tsx
@@ -1,18 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import ErrorBoundary from "./components/common/ErrorBoundary"
 import NussknackerInitializer from "./containers/NussknackerInitializer"
 import {SettingsProvider} from "./containers/SettingsInitializer"
 import "./i18n"
 import {StoreProvider} from "./store/provider"
 import rootRoutes from "./containers/RootRoutes"
+import {BASE_PATH} from "./config"
 
 const rootContainer = document.createElement(`div`)
 rootContainer.id = "root"
 document.body.appendChild(rootContainer)
 
-const router = createBrowserRouter(rootRoutes)
+const router = createBrowserRouter(rootRoutes, {basename: BASE_PATH.replace(/\/$/, "")})
 
 const Root = () => {
   return (
