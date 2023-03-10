84d1216e4692558f64dde3aa6e07c0a64089f6d0
Julian Wielga <j@touk.pl>
2023-03-10 13:44:47 +0100

    fix & cleanup test

--- designer/client/test/Process/ProcessStateIcon-test.tsx
+++ designer/client/test/Process/ProcessStateIcon-test.tsx
@@ -3,43 +3,27 @@ import ProcessStateIcon from "../../components/Process/ProcessStateIcon"
 import {describe, expect, it, jest} from "@jest/globals"
 import {render, waitFor} from "@testing-library/react"
 
-//TODO: In future we shoulde convert it to ts - now, we have some problems with this..
-
 const processState = {
   allowedActions: ["DEPLOY"],
-  attributes: null,
-  deploymentId: null,
-  errorMessage: null,
   icon: "/states/stopping-success.svg",
-  startTime: null,
   status: {type: "StoppedStateStatus", name: "CANCELED"},
   tooltip: "The scenario has been successfully cancelled."
 }
 
 const noDataProcessState = {
   allowedActions: ["DEPLOY"],
-  attributes: null,
-  deploymentId: null,
-  errorMessage: null,
-  icon: null,
-  startTime: null,
   status: {type: "StoppedStateStatus", name: "CANCELED"}
 }
 
-// @ts-ignore
-global.fetch = jest.fn(async url => {
-  return await ({
-    async text() {
-      return await (`<span data-testid="svg">${url}</span>`)
-    }
-  })
-})
+global.fetch = jest.fn(url => Promise.resolve({
+  text: () => Promise.resolve(`<span data-testid="svg">${url}</span>`)
+})) as any
 
 describe("ProcessStateIcon tests", () => {
   it("should show defaults for missing process.state and stateProcess", async () => {
     const process = {processingType: "streaming"}
     const {getByTestId, container} = render(
-      <ProcessStateIcon process={process}/>
+      <ProcessStateIcon process={process as any}/>
     )
     expect(fetch).toHaveBeenLastCalledWith("/be-static/assets/states/status-unknown.svg")
     await waitFor(() => getByTestId("svg"))
@@ -49,7 +33,7 @@ describe("ProcessStateIcon tests", () => {
   it("should show defaults for loaded process.state without data", async () => {
     const process = {processingType: "streaming", state: noDataProcessState}
     const {getByTestId, container} = render(
-      <ProcessStateIcon process={process}/>
+      <ProcessStateIcon process={process as any}/>
     )
     await waitFor(() => getByTestId("svg"))
     expect(fetch).not.toHaveBeenCalled() //cached
@@ -59,7 +43,7 @@ describe("ProcessStateIcon tests", () => {
   it("should show data from loaded process.state", async () => {
     const process = {processingType: "streaming", state: processState}
     const {getByTestId, container} = render(
-      <ProcessStateIcon process={process}/>
+      <ProcessStateIcon process={process as any}/>
     )
     expect(fetch).toHaveBeenLastCalledWith("/be-static/states/stopping-success.svg")
     await waitFor(() => getByTestId("svg"))
@@ -69,7 +53,7 @@ describe("ProcessStateIcon tests", () => {
   it("should show defaults if loadedProcess is null", async () => {
     const process = {processingType: "streaming", state: processState}
     const {getByTestId, container} = render(
-      <ProcessStateIcon process={process} isStateLoaded={true}/>
+      <ProcessStateIcon process={process as any} isStateLoaded/>
     )
     await waitFor(() => getByTestId("svg"))
     expect(fetch).not.toHaveBeenCalled() //cached
@@ -79,7 +63,7 @@ describe("ProcessStateIcon tests", () => {
   it("should show defaults if loadedProcess is empty", async () => {
     const process = {processingType: "streaming", state: processState}
     const {getByTestId, container} = render(
-      <ProcessStateIcon process={process} processState={noDataProcessState}
+      <ProcessStateIcon process={process as any} processState={noDataProcessState as any}
                         isStateLoaded={true}/>
     )
     await waitFor(() => getByTestId("svg"))
@@ -89,7 +73,7 @@ describe("ProcessStateIcon tests", () => {
 
   it("should show loadedProcess data", async () => {
     const {getByTestId, container} = render(
-      <ProcessStateIcon process={noDataProcessState} processState={processState}
+      <ProcessStateIcon process={noDataProcessState as any} processState={processState as any}
                         isStateLoaded={true}/>
     )
     await waitFor(() => getByTestId("svg"))
