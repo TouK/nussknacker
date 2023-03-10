122ad711708297faaa0f6e60545dbbc59cbd8602
Arek Burdach <arek.burdach@gmail.com>
2023-03-10 16:18:45 +0100

    notifications fixes: - (1.9 regression) states wasn't refreshed after cancel finish/fail - (was in 1.8) "state" state wasn't refreshed after finish (was only after fail) - (was in 1.8) Notification "Deployment of ... failed ..." was presented even for "cance"

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/notifications/Notification.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/notifications/Notification.scala
@@ -2,6 +2,8 @@ package pl.touk.nussknacker.ui.notifications
 
 import io.circe.generic.JsonCodec
 import io.circe.{Decoder, Encoder}
+import pl.touk.nussknacker.engine.api.deployment.ProcessActionType
+import pl.touk.nussknacker.engine.api.deployment.ProcessActionType.ProcessActionType
 import pl.touk.nussknacker.engine.api.process.ProcessName
 import pl.touk.nussknacker.ui.notifications.DataToRefresh.DataToRefresh
 
@@ -13,16 +15,22 @@ import pl.touk.nussknacker.ui.notifications.DataToRefresh.DataToRefresh
                                    toRefresh: List[DataToRefresh])
 
 object Notification {
-  def deploymentFailedNotification(id: String, name: ProcessName, failureMessageOpt: Option[String]): Notification = {
-    Notification(id, Some(name), s"Deployment of ${name.value} failed" + failureMessageOpt.map(" with reason: " + _).getOrElse(""),
+  def deploymentActionFailedNotification(id: String, actionType: ProcessActionType, name: ProcessName, failureMessageOpt: Option[String]): Notification = {
+    Notification(id, Some(name), s"${displayableActionName(actionType)} of ${name.value} failed" + failureMessageOpt.map(" with reason: " + _).getOrElse(""),
       Some(NotificationType.error), List(DataToRefresh.state))
   }
 
-  def deploymentFinishedNotification(id: String, name: ProcessName): Notification = {
+  def deploymentActionFinishedNotification(id: String, actionType: ProcessActionType, name: ProcessName): Notification = {
     //We don't want to display this notification, not to confuse user, as
     //deployment may proceed asynchronously (e.g. in streaming-lite)
-    Notification(id, Some(name), s"Deployment finished", None, List(DataToRefresh.versions, DataToRefresh.activity))
+    Notification(id, Some(name), s"${displayableActionName(actionType)} finished", None, List(DataToRefresh.versions, DataToRefresh.activity, DataToRefresh.state))
   }
+
+  private def displayableActionName(actionType: ProcessActionType): String =
+    actionType match {
+      case ProcessActionType.Deploy => "Deployment"
+      case ProcessActionType.Cancel => "Cancel"
+    }
 
 }
 
