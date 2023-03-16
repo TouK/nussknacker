03f19b33a6a308b097f3383dc6f7283520aecb3c
Tomasz Kalkosiński <tkl@touk.pl>
2023-03-16 15:54:58 +0100

    Release workflow 46

--- designer/buildClient.sh
+++ designer/buildClient.sh
@@ -5,7 +5,6 @@ set -e
 cd "$(dirname -- "$0")"
 cd client && npm ci && npm run build && cd -
 cp -r client/.federated-types/nussknackerUi submodules/types/@remote
-
 cd submodules && npm ci && CI=true npm run build && cd -
 # Copy designer dist for purpose of further usage in server ran from Idea
 cd ..
