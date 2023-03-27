1a9a2727740a0dc61fe62416f409b2f1fab186d9
Bartłomiej Tartanus <btr@touk.pl>
2023-03-27 12:32:54 +0200

    test fix?

--- engine/flink/scala-utils/src/test/scala/org/apache/flink/runtime/types/FlinkScalaKryoInstantiatorSpec.scala
+++ engine/flink/scala-utils/src/test/scala/org/apache/flink/runtime/types/FlinkScalaKryoInstantiatorSpec.scala
@@ -1,4 +1,5 @@
 package org.apache.flink.runtime.types
+
 import com.esotericsoftware.kryo.io.{Input, Output}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -11,7 +12,9 @@ class FlinkScalaKryoInstantiatorSpec extends AnyFlatSpec with Matchers {
   it should "serialize and deserialize records properly" in {
     val kryo = new FlinkScalaKryoInstantiator().newKryo
 
-    val record = Record(5, Map("a"->1, "b"->2), List("123", "abc"), mutable.Map("a"-> 123).asJava)
+    val record = Record(true, 5, "abc",
+      Map("a" -> 1, "b" -> 2), List("123", "abc"), Set("abc"),
+      mutable.Map("a" -> 123).asJava, mutable.Buffer("abc").asJava, mutable.Set("abc").asJava)
     val output = new Output(1024)
     kryo.writeClassAndObject(output, record)
     val input = new Input(output.toBytes)
@@ -21,4 +24,6 @@ class FlinkScalaKryoInstantiatorSpec extends AnyFlatSpec with Matchers {
   }
 }
 
-case class Record(int: Int, map: Map[String, Int], list: List[String], javaMap: java.util.Map[String, Int])
+case class Record(boolean: Boolean, int: Int, string: String,
+                  map: Map[String, Int], list: List[String], set: Set[String],
+                  javaMap: java.util.Map[String, Int], javaList: java.util.List[String], javaSet: java.util.Set[String])
