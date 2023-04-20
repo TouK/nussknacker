7484f59a232fad0665c49b7408bfac8b34447fec
Krzysztof Gadomski <kgd@touk.pl>
2023-04-20 14:28:35 +0200

    Update AggregateWindowsConfig.scala

--- engine/flink/components/base/src/main/scala/pl/touk/nussknacker/engine/flink/util/transformer/aggregate/AggregateWindowsConfig.scala
+++ engine/flink/components/base/src/main/scala/pl/touk/nussknacker/engine/flink/util/transformer/aggregate/AggregateWindowsConfig.scala
@@ -12,7 +12,7 @@ object AggregateWindowsConfig {
 
   import net.ceedubs.ficus.Ficus._
   import net.ceedubs.ficus.readers.ArbitraryTypeReader._
-  private implicit val zoneIdValueReader: ValueReader[java.time.Duration] = ValueReader[String].map(value => java.time.Duration.parse(value))
+  private implicit val durationValueReader: ValueReader[java.time.Duration] = ValueReader[String].map(value => java.time.Duration.parse(value))
 
   def loadOrDefault(config: Config): AggregateWindowsConfig = {
     config.getAs[AggregateWindowsConfig]("aggregateWindowsConfig").getOrElse(AggregateWindowsConfig.Default)
