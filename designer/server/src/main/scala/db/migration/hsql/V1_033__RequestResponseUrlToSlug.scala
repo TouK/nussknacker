38674923e0d9ef53076e0a21960c23abbdabaaf2
Łukasz Jędrzejewski <ljd@touk.pl>
2023-04-22 18:50:27 +0200

    Remove restored migrations

--- designer/server/src/main/scala/db/migration/hsql/V1_033__RequestResponseUrlToSlug.scala
+++
@@ -1,8 +0,0 @@
-package db.migration.hsql
-
-import db.migration.{V1_033__RequestResponseUrlToSlug => V1_033__RequestResponseUrlToSlugDefinition}
-import slick.jdbc.{HsqldbProfile, JdbcProfile}
-
-class V1_033__RequestResponseUrlToSlug extends V1_033__RequestResponseUrlToSlugDefinition{
-  override protected lazy val profile: JdbcProfile = HsqldbProfile
-}
--- designer/server/src/main/scala/db/migration/postgres/V1_033__RequestResponseUrlToSlug.scala
+++
@@ -1,8 +0,0 @@
-package db.migration.postgres
-
-import db.migration.{V1_033__RequestResponseUrlToSlug => V1_033__RequestResponseUrlToSlugDefinition}
-import slick.jdbc.{JdbcProfile, PostgresProfile}
-
-class V1_033__RequestResponseUrlToSlug extends V1_033__RequestResponseUrlToSlugDefinition{
-  override protected lazy val profile: JdbcProfile = PostgresProfile
-}
