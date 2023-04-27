77e0be31402120206162f621786bc04554e7f58a
Julian Wielga <j@touk.pl>
2023-04-27 17:12:52 +0200

    list files in directory

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/WebResources.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/WebResources.scala
@@ -5,10 +5,10 @@ import com.typesafe.scalalogging.LazyLogging
 import org.apache.commons.io.FileUtils
 import pl.touk.nussknacker.engine.util.ResourceLoader
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path, Paths}
-import scala.util.Try
 import java.io.File
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path}
+import scala.util.Try
 
 class WebResources(publicPath: String) extends Directives with LazyLogging {
 
@@ -16,29 +16,34 @@ class WebResources(publicPath: String) extends Directives with LazyLogging {
   private lazy val mainContentFile = {
     val tempMainContentFile = Files.createTempFile("nussknacker", "main.html").toFile
     tempMainContentFile.deleteOnExit()
-    val data = Try(ResourceLoader.load("/web/static/main.html"))
+    val staticRoot = "/web/static"
+    val data = Try(ResourceLoader.load(Path.of(staticRoot, "/main.html").toString))
 
     val content = data.toOption.getOrElse {
       logger.error("Failed to find web/static/main.html - probably frontend resources are not packaged in jar. Frontend won't work properly!")
       ""
     }
 
-    val builder = new StringBuilder
-    Files.newDirectoryStream(Paths.get("/Users/j/Work/projects/nussknacker/designer")).forEach { file =>
-      builder.append(file.getFileName)
-      builder.append("\n")
-    }
+    val extraScriptsDir = "/extra"
+    val path = getClass.getResource(Path.of(staticRoot, extraScriptsDir).toString)
+    val files = Option(new File(path.getFile).list((_, name) => name.endsWith(".js")))
+    val extraScripts = files
+      .getOrElse(Array.empty)
+      .sorted
+      .map(file => s"<script src=\"${Path.of(publicPath, extraScriptsDir, file)}\"></script>")
+      .mkString("\n")
 
-    val withComment = content.replace("</body>", "<!-- " + builder.toString() + " //-->\n</body>")
-    val withPublicPathSubstituted = withComment.replace("__publicPath__", publicPath)
+    val withPublicPathSubstituted = content
+      .replace("</body>", s"<!--\n $extraScripts //-->\n</body>")
+      .replace("__publicPath__", publicPath)
 
     FileUtils.writeStringToFile(tempMainContentFile, withPublicPathSubstituted, StandardCharsets.UTF_8)
     tempMainContentFile
   }
 
   val route: Route = handleAssets("submodules") ~ handleAssets("static") ~ get {
-    //main.html instead of index.html to not interfere with flink's static resources...
-    getFromFile(mainContentFile) //return UI page by default to make links work
+    // main.html instead of index.html to not interfere with flink's static resources...
+    getFromFile(mainContentFile) // return UI page by default to make links work
   }
 
   private def handleAssets(webSubfolder: String) = {
