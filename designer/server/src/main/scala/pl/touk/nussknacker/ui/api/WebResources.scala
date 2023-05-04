993f2c6920bfb92dc5c81b480a4746a40f3f59ac
JulianWielga <j@touk.pl>
2023-05-04 20:05:15 +0200

    Tag manager (#4269)
    * list files in directory
    
    * list files in directory
    
    * Added listing preparer + tests
    
    * Handling extra from files and from jar
    
    * Handling public path
    
    * scala 2.13 fix
    
    * listing fixes
    
    * switch to Path also for resources
    
    * switch to Path also for resources - fix
    
    * removed redundant code
    
    * handling listing file
    
    * Better log message
    
    * remove comment
    
    * Documentation added
    
    * test fixes after package change
    
    * expose userId
    
    * test fix
    
    ---------
    
    Co-authored-by: Arek Burdach <arek.burdach@gmail.com>

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/WebResources.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/WebResources.scala
@@ -4,32 +4,42 @@ import akka.http.scaladsl.server.{Directives, Route}
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.commons.io.FileUtils
 import pl.touk.nussknacker.engine.util.ResourceLoader
+import pl.touk.nussknacker.ui.extrajs.ExtraScriptsListingPreparer
 
 import java.nio.charset.StandardCharsets
-import java.nio.file.Files
+import java.nio.file.{Files, Path}
 import scala.util.Try
 
 class WebResources(publicPath: String) extends Directives with LazyLogging {
 
   //see config.js comment
   private lazy val mainContentFile = {
-    val tempMainContentFile = Files.createTempFile("nussknacker", "main.html").toFile
-    tempMainContentFile.deleteOnExit()
-    val data = Try(ResourceLoader.load("/web/static/main.html"))
-
+    val mainPath = Path.of("/", "web", "static", "main.html").toString
+    val data = Try(ResourceLoader.load(mainPath))
     val content = data.toOption.getOrElse {
-      logger.error("Failed to find web/static/main.html - probably frontend resources are not packaged in jar. Frontend won't work properly!")
+      logger.error(s"Failed to find $mainPath - probably frontend resources are not packaged in jar. Frontend won't work properly!")
       ""
     }
-    val withPublicPathSubstituted = content.replace("__publicPath__", publicPath)
 
+    val extraScripts = {
+      val extraStaticRoot = Path.of("static", "extra")
+      val webResourcesRoot = Path.of(Option(publicPath).filterNot(_.isBlank).getOrElse("/")).resolve(extraStaticRoot)
+      new ExtraScriptsListingPreparer(getClass.getClassLoader, Path.of("web").resolve(extraStaticRoot), webResourcesRoot).scriptsListing
+    }
+
+    val withPublicPathSubstituted = content
+      .replace("</body>", s"\n$extraScripts\n</body>")
+      .replace("__publicPath__", publicPath)
+
+    val tempMainContentFile = Files.createTempFile("nussknacker", "main.html").toFile
+    tempMainContentFile.deleteOnExit()
     FileUtils.writeStringToFile(tempMainContentFile, withPublicPathSubstituted, StandardCharsets.UTF_8)
     tempMainContentFile
   }
 
   val route: Route = handleAssets("submodules") ~ handleAssets("static") ~ get {
-    //main.html instead of index.html to not interfere with flink's static resources...
-    getFromFile(mainContentFile) //return UI page by default to make links work
+    // main.html instead of index.html to not interfere with flink's static resources...
+    getFromFile(mainContentFile) // return UI page by default to make links work
   }
 
   private def handleAssets(webSubfolder: String) = {
@@ -37,7 +47,7 @@ class WebResources(publicPath: String) extends Directives with LazyLogging {
       get {
         encodeResponse {
           extractRequest { matched =>
-            logger.debug(s"Try to get static data from $webSubfolder for:  ${matched.uri.path}.")
+            logger.debug(s"Try to get static data from $webSubfolder for: ${matched.uri.path}.")
             getFromResourceDirectory(s"web/$webSubfolder")
           }
         }
---
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/extrajs/ExtraScriptsListingPreparerTest.scala
@@ -0,0 +1,58 @@
+package pl.touk.nussknacker.ui.extrajs
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import java.io.File
+import java.nio.file.Path
+import scala.reflect.internal.util.ScalaClassLoader.URLClassLoader
+
+class ExtraScriptsListingPreparerTest extends AnyFunSuite with Matchers {
+
+  private val classLoader = {
+    val classpathEntry = new File(getClass.getResource("/extra-scripts-test").getFile).toURI.toURL
+    new URLClassLoader(Seq(classpathEntry), getClass.getClassLoader)
+  }
+
+  test("should prepare correct listing based on classpath files") {
+    val listingPreparer = new ExtraScriptsListingPreparer(
+      classLoader, Path.of("web-extra"), Path.of("/resources-root"))
+
+    listingPreparer.webResourcesListing shouldEqual Seq("/resources-root/a.js", "/resources-root/b.js")
+  }
+
+  test("should return empty list for not existing directory") {
+    val listingPreparer = new ExtraScriptsListingPreparer(
+      classLoader, Path.of("not-existing"), Path.of("/"))
+
+    listingPreparer.webResourcesListing shouldBe empty
+  }
+
+  test("should return empty list for root as which is not a directory") {
+    val listingPreparer = new ExtraScriptsListingPreparer(
+      classLoader, Path.of("a.js"), Path.of("/"))
+
+    listingPreparer.webResourcesListing shouldBe empty
+  }
+
+  test("should use scripts.lst file when available") {
+    val classLoader = {
+      val classpathEntry = new File(getClass.getResource("/extra-scripts-test-lst").getFile).toURI.toURL
+      new URLClassLoader(Seq(classpathEntry), getClass.getClassLoader)
+    }
+    val listingPreparer = new ExtraScriptsListingPreparer(
+      classLoader, Path.of("web-extra"), Path.of("/"))
+
+    listingPreparer.webResourcesListing shouldBe Seq("/a.js", "/b.js")
+  }
+
+  test("should list resources as of html scripts") {
+    val listingPreparer = new ExtraScriptsListingPreparer(
+      classLoader, Path.of("web-extra"), Path.of("/"))
+
+    listingPreparer.scriptsListing shouldEqual
+      """<script defer src="/a.js"></script>
+        |<script defer src="/b.js"></script>""".stripMargin
+  }
+
+}
