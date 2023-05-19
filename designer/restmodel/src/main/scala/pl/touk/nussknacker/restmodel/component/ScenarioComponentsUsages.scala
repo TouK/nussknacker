c131cef01c6af90c62fb6f3a11082f3e004ee936
Łukasz Jędrzejewski <ljd@touk.pl>
2023-05-19 10:15:01 +0200

    Remove discuss comment

--- designer/restmodel/src/main/scala/pl/touk/nussknacker/restmodel/component/ScenarioComponentsUsages.scala
+++ designer/restmodel/src/main/scala/pl/touk/nussknacker/restmodel/component/ScenarioComponentsUsages.scala
@@ -2,7 +2,6 @@ package pl.touk.nussknacker.restmodel.component
 
 import pl.touk.nussknacker.engine.api.component.ComponentType.ComponentType
 
-// TODO components-usages: discuss changing type to: List[ComponentUsages] where ComponentUsages is ComponentIdParts, List[NodeId].
 case class ScenarioComponentsUsages(value: Map[ComponentIdParts, List[NodeId]])
 
 object ScenarioComponentsUsages {
