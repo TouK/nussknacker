702d6d80aace085ed711640df5eee2cb739acf27
Mateusz Słabek <mslb@touk.pl>
2023-05-23 17:07:19 +0200

    Fetch only process versions on cancel and deploy (#4312)
    * fix: load only process versions on notification
    
    * review: remove debugging log
    
    * review: add type to version loading action
    
    * docs: update changelog
    
    * review: add formatting and remove remaining function

--- designer/client/src/actions/reduxTypes.ts
+++ designer/client/src/actions/reduxTypes.ts
@@ -1,49 +1,51 @@
-import {AnyAction, Reducer as ReduxReducer} from "redux"
-import {ThunkAction as TA, ThunkDispatch as TD} from "redux-thunk"
+import { AnyAction, Reducer as ReduxReducer } from "redux";
+import { ThunkAction as TA, ThunkDispatch as TD } from "redux-thunk";
 
-import {ActionTypes} from "./actionTypes"
+import { ActionTypes } from "./actionTypes";
 import {
-  DisplayProcessActivityAction,
-  DisplayProcessCountsAction,
-  HandleHTTPErrorAction,
-  NodeActions,
-  SelectionActions,
-} from "./nk"
-import {FeatureFlagsActions} from "./nk/featureFlags"
-import {UserSettingsActions} from "./nk/userSettings"
-import {UiActions} from "./nk/ui/uiActions"
-import {SettingsActions} from "./settingsActions"
-import {ToolbarActions} from "./nk/toolbars"
-import {RootState} from "../reducers"
-import {UndoRedoActions} from "./undoRedoActions"
-import {NodeDetailsActions} from "./nk/nodeDetails"
-import {NotificationActions} from "./nk/notifications"
-import {DisplayTestResultsDetailsAction} from "./nk/displayTestResults"
+    DisplayProcessActivityAction,
+    DisplayProcessCountsAction,
+    HandleHTTPErrorAction,
+    NodeActions,
+    SelectionActions,
+} from "./nk";
+import { FeatureFlagsActions } from "./nk/featureFlags";
+import { UserSettingsActions } from "./nk/userSettings";
+import { UiActions } from "./nk/ui/uiActions";
+import { SettingsActions } from "./settingsActions";
+import { ToolbarActions } from "./nk/toolbars";
+import { RootState } from "../reducers";
+import { UndoRedoActions } from "./undoRedoActions";
+import { NodeDetailsActions } from "./nk/nodeDetails";
+import { NotificationActions } from "./nk/notifications";
+import { DisplayTestResultsDetailsAction } from "./nk/displayTestResults";
+import { LoadProcessVersionsAction } from "./nk/loadProcessVersions";
 
 type TypedAction =
-  | HandleHTTPErrorAction
-  | UiActions
-  | SettingsActions
-  | DisplayProcessActivityAction
-  | NodeActions
-  | ToolbarActions
-  | NodeDetailsActions
-  | UndoRedoActions
-  | FeatureFlagsActions
-  | UserSettingsActions
-  | SelectionActions
-  | NotificationActions
-  | DisplayTestResultsDetailsAction
-  | DisplayProcessCountsAction
+    | HandleHTTPErrorAction
+    | UiActions
+    | SettingsActions
+    | DisplayProcessActivityAction
+    | NodeActions
+    | ToolbarActions
+    | NodeDetailsActions
+    | UndoRedoActions
+    | FeatureFlagsActions
+    | UserSettingsActions
+    | SelectionActions
+    | NotificationActions
+    | DisplayTestResultsDetailsAction
+    | DisplayProcessCountsAction
+    | LoadProcessVersionsAction;
 
 interface UntypedAction extends AnyAction {
-  type: Exclude<ActionTypes, TypedAction["type"]>,
+    type: Exclude<ActionTypes, TypedAction["type"]>;
 }
 
-export type Action = UntypedAction | TypedAction
+export type Action = UntypedAction | TypedAction;
 
-type State = RootState
+type State = RootState;
 
-export type ThunkAction<R = void, S = State> = TA<R, S, undefined, Action>
-export type ThunkDispatch<S = State> = TD<S, undefined, Action>
-export type Reducer<S> = ReduxReducer<S, Action>
+export type ThunkAction<R = void, S = State> = TA<R, S, undefined, Action>;
+export type ThunkDispatch<S = State> = TD<S, undefined, Action>;
+export type Reducer<S> = ReduxReducer<S, Action>;
