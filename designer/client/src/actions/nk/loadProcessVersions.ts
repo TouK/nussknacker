d87f75018df898e1cbb3ace44f59688a7f0ba25c
Mateusz Słabek <mslb@touk.pl>
2023-05-23 16:16:05 +0200

    review: add formatting and remove remaining function

--- designer/client/src/actions/nk/loadProcessVersions.ts
+++ designer/client/src/actions/nk/loadProcessVersions.ts
@@ -1,22 +1,23 @@
 import HttpService from "../../http/HttpService";
-import {ProcessId} from "../../types";
-import {ProcessActionType, ProcessVersionType} from "../../components/Process/types";
-import {ThunkAction} from "../reduxTypes";
+import { ProcessId } from "../../types";
+import { ProcessActionType, ProcessVersionType } from "../../components/Process/types";
+import { ThunkAction } from "../reduxTypes";
 
 export type LoadProcessVersionsAction = {
-  type: "PROCESS_VERSIONS_LOADED",
-  history: ProcessVersionType[],
-  lastAction: ProcessActionType,
-  lastDeployedAction: ProcessActionType,
-}
+    type: "PROCESS_VERSIONS_LOADED";
+    history: ProcessVersionType[];
+    lastAction: ProcessActionType;
+    lastDeployedAction: ProcessActionType;
+};
 
 export function loadProcessVersions(processId: ProcessId): ThunkAction<Promise<LoadProcessVersionsAction>> {
-  return (dispatch) => HttpService.fetchProcessDetails(processId).then(response => {
-    return dispatch({
-      type: "PROCESS_VERSIONS_LOADED",
-      history: response.data.history,
-      lastDeployedAction: response.data.lastDeployedAction,
-      lastAction: response.data.lastAction
-    })
-  })
+    return (dispatch) =>
+        HttpService.fetchProcessDetails(processId).then((response) => {
+            return dispatch({
+                type: "PROCESS_VERSIONS_LOADED",
+                history: response.data.history,
+                lastDeployedAction: response.data.lastDeployedAction,
+                lastAction: response.data.lastAction,
+            });
+        });
 }
