efe427bf14bbbc7f26f32a81e7f728ace478751a
Piotr Rudnicki <prn@touk.pl>
2023-05-26 16:36:49 +0200

    [ITDEVESP-4676] Add mergeFragments function

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/component/ComponentsUsageHelper.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/component/ComponentsUsageHelper.scala
@@ -4,6 +4,8 @@ import pl.touk.nussknacker.engine.api.component.ComponentId
 import pl.touk.nussknacker.restmodel.component.{ComponentIdParts, NodeId, ScenarioComponentsUsages}
 import pl.touk.nussknacker.restmodel.processdetails.BaseProcessDetails
 
+import scala.annotation.tailrec
+
 object ComponentsUsageHelper {
 
   import pl.touk.nussknacker.engine.util.Implicits._
@@ -25,11 +27,41 @@ object ComponentsUsageHelper {
       }
     }
 
-    processesDetails
+    @tailrec
+    def mergeFragments(fragmentsProcessesDetails: List[BaseProcessDetails[ScenarioComponentsUsages]],
+                       acc: Map[ComponentId, List[(BaseProcessDetails[Unit], List[NodeId])]]): Map[ComponentId, List[(BaseProcessDetails[Unit], List[NodeId])]] =
+      fragmentsProcessesDetails match {
+        case Nil => acc
+        case fragmentProcessesDetails :: t => {
+          val componentIdUsages = toComponentIdUsages(fragmentProcessesDetails)
+          val additionalElems = componentIdUsages.map {
+            case (componentId, (baseProcessDetails, fragmentLevelNodeIds)) =>
+              (componentId, (baseProcessDetails, fragmentLevelNodeIds.map(nodeId => "<<fragment>> " ++ nodeId)))
+          }.groupBy(_._1).toList.map {
+            case (componentId, l) => (componentId, l.map(_._2))
+          }
+
+          //TODO correct newAcc to be proper transformation
+          val newAcc = (acc.toList ++ additionalElems).groupBy(_._1).mapValuesNow(_.flatMap(_._2).toList).toMap
+
+          mergeFragments(t, newAcc)
+        }
+      }
+
+    val scenariosProcessesDetails = processesDetails.filter(_.isSubprocess == false)
+    val fragmentsProcessesDetails = processesDetails.filter(_.isSubprocess == true)
+
+    val scenarioComponentsUsages = scenariosProcessesDetails
       .flatMap(toComponentIdUsages)
       // Can be replaced with .groupMap from Scala 2.13.
       .groupBy { case (componentId, _) => componentId }
       .transform { case (_, usages) => usages.map { case (_, processDetails) => processDetails } }
+
+    mergeFragments(fragmentsProcessesDetails, scenarioComponentsUsages)
   }
+
+
+
+
 
 }
