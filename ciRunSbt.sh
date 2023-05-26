977b42ce0b63af0d5be5829c0220051cd6f9dd90
Krzysztof Gadomski <kgd@touk.pl>
2023-05-26 09:23:34 +0200

    scala version matrix

--- ciRunSbt.sh
+++ ciRunSbt.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 #if $CROSS_BUILD is true - we do cross build, otherwise we use default scala version
-if [[ $CROSS_BUILD == 'true' ]]
-then
+if [[ -n "$FORCE_SCALA_VERSION" ]]; then
+   ARGS="++$FORCE_SCALA_VERSION $*"
+elif [[ $CROSS_BUILD == 'true' ]]; then
    #for crossbuild we prepend each arg with +...
    ARGS=`echo " $*" | sed "s/ \+/ \+/g"`
 else
@@ -11,5 +12,5 @@ fi
 # Tuning of sbt test to prevent travis OOM from killing java. Be aware that using JAVA_OPTS won't work on travis because
 # it has own alias for sbt: https://www.scala-sbt.org/1.x/docs/Travis-CI-with-sbt.html#Custom+JVM+options
 # todo: fix classloader leak during tests - for now, we increase max metaspace size.
-echo "Executing: sbt -J-Xss6M -J-Xms3g -J-Xmx3g -J-XX:ReservedCodeCacheSize=256M -J-XX:MaxMetaspaceSize=5g $ARGS"
-sbt -J-Xss6M -J-Xms3g -J-Xmx3g -J-XX:ReservedCodeCacheSize=256M -J-XX:MaxMetaspaceSize=5g $ARGS
+echo "Executing: sbt -J-Xss6M -J-Xms2g -J-Xmx2g -J-XX:ReservedCodeCacheSize=256M -J-XX:MaxMetaspaceSize=5g $ARGS"
+sbt -J-Xss6M -J-Xms2g -J-Xmx2g -J-XX:ReservedCodeCacheSize=256M -J-XX:MaxMetaspaceSize=5g $ARGS
