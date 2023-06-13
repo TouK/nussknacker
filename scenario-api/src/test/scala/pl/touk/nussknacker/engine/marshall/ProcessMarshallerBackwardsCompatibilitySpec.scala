69133f272b320427c02acc7c27dc86b2164df0a6
Mateusz Słabek <mslb@touk.pl>
2023-06-13 12:21:24 +0200

    change decoder to decode optional property field

--- scenario-api/src/test/scala/pl/touk/nussknacker/engine/marshall/ProcessMarshallerBackwardsCompatibilitySpec.scala
+++ scenario-api/src/test/scala/pl/touk/nussknacker/engine/marshall/ProcessMarshallerBackwardsCompatibilitySpec.scala
@@ -5,10 +5,10 @@ import org.scalatest.matchers.should.Matchers
 
 class ProcessMarshallerBackwardsCompatibilitySpec extends AnyFlatSpec with Matchers{
 
-  val oldJson =
+  private val legacyJsonWithNoFields =
     """{
       |  "metaData" : {
-      |    "id" : "testa",
+      |    "id" : "testId",
       |    "typeSpecificData" : {
       |      "parallelism" : 10,
       |      "spillStateToDisk" : true,
@@ -18,16 +18,14 @@ class ProcessMarshallerBackwardsCompatibilitySpec extends AnyFlatSpec with Match
       |    },
       |    "additionalFields" : null
       |  },
-      |  "nodes" : [
-      |  ],
-      |  "additionalBranches" : [
-      |  ]
+      |  "nodes" : [],
+      |  "additionalBranches" : []
       |}""".stripMargin
 
-  val newJson =
+  private val newJsonWithNoFields =
     """{
       |  "metaData" : {
-      |    "id" : "testa",
+      |    "id" : "testId",
       |    "additionalFields" : {
       |       "description": null,
       |       "properties" : {
@@ -39,25 +37,132 @@ class ProcessMarshallerBackwardsCompatibilitySpec extends AnyFlatSpec with Match
       |       "metaDataType": "StreamMetaData"
       |    }
       |  },
-      |  "nodes" : [
-      |  ],
-      |  "additionalBranches" : [
-      |  ]
+      |  "nodes" : [],
+      |  "additionalBranches" : []
+      |}""".stripMargin
+
+  private val legacyJsonWithDescriptionNoProperties =
+    """{
+      |  "metaData" : {
+      |    "id" : "testId",
+      |    "typeSpecificData" : {
+      |      "parallelism" : 10,
+      |      "spillStateToDisk" : true,
+      |      "useAsyncInterpretation" : null,
+      |      "checkpointIntervalInSeconds" : 11,
+      |      "type" : "StreamMetaData"
+      |    },
+      |    "additionalFields" : {
+      |      "description": "someDescription"
+      |    }
+      |  },
+      |  "nodes" : [],
+      |  "additionalBranches" : []
+      |}""".stripMargin
+
+  private val newJsonWithDescriptionNoProperties =
+    """{
+      |  "metaData" : {
+      |    "id" : "testId",
+      |    "additionalFields" : {
+      |       "description": "someDescription",
+      |       "properties" : {
+      |         "parallelism" : "10",
+      |         "spillStateToDisk" : "true",
+      |         "useAsyncInterpretation" : "",
+      |         "checkpointIntervalInSeconds" : "11"
+      |       },
+      |       "metaDataType": "StreamMetaData"
+      |    }
+      |  },
+      |  "nodes" : [],
+      |  "additionalBranches" : []
+      |}""".stripMargin
+
+  private val legacyJsonWithAdditionalProperties =
+    """{
+      |  "metaData" : {
+      |    "id" : "testId",
+      |    "typeSpecificData" : {
+      |      "parallelism" : 10,
+      |      "spillStateToDisk" : true,
+      |      "useAsyncInterpretation" : null,
+      |      "checkpointIntervalInSeconds" : 11,
+      |      "type" : "StreamMetaData"
+      |    },
+      |    "additionalFields" : {
+      |      "description": null,
+      |      "properties": {
+      |        "someProperty1": "",
+      |        "someProperty2": "someValue2"
+      |      }
+      |    }
+      |  },
+      |  "nodes" : [],
+      |  "additionalBranches" : []
+      |}""".stripMargin
+
+  private val newJsonWithAdditionalProperties =
+    """{
+      |  "metaData" : {
+      |    "id" : "testId",
+      |    "additionalFields" : {
+      |       "description": null,
+      |       "properties" : {
+      |         "someProperty1": "",
+      |         "someProperty2": "someValue2",
+      |         "parallelism" : "10",
+      |         "spillStateToDisk" : "true",
+      |         "useAsyncInterpretation" : "",
+      |         "checkpointIntervalInSeconds" : "11"
+      |       },
+      |       "metaDataType": "StreamMetaData"
+      |    }
+      |  },
+      |  "nodes" : [],
+      |  "additionalBranches" : []
       |}""".stripMargin
 
 
-  it should "decode old json" in {
-    ProcessMarshaller.fromJson(oldJson).isValid shouldBe true
+  it should "decode legacy json with no additional fields" in {
+    ProcessMarshaller.fromJson(legacyJsonWithNoFields).isValid shouldBe true
   }
 
-  it should "decode new json" in {
-      ProcessMarshaller.fromJson(newJson).isValid shouldBe true
+  it should "decode new json with no additional fields" in {
+    ProcessMarshaller.fromJson(newJsonWithNoFields).isValid shouldBe true
   }
 
-  it should "decode new json and old to the same structure" in {
-    val oldCP = ProcessMarshaller.fromJson(oldJson).getOrElse(throw new AssertionError())
-    val newCP = ProcessMarshaller.fromJson(newJson).getOrElse(throw new AssertionError())
+  it should "decode new json and old with no additional fields to the same structure" in {
+    val oldCP = ProcessMarshaller.fromJson(legacyJsonWithNoFields).getOrElse(throw new AssertionError())
+    val newCP = ProcessMarshaller.fromJson(newJsonWithNoFields).getOrElse(throw new AssertionError())
     oldCP shouldBe newCP
   }
 
+  it should "decode legacy json with description and no additional properties" in {
+    ProcessMarshaller.fromJson(legacyJsonWithDescriptionNoProperties).isValid shouldBe true
+  }
+
+  it should "decode new json with description and no additional properties" in {
+    ProcessMarshaller.fromJson(newJsonWithDescriptionNoProperties).isValid shouldBe true
+  }
+
+  it should "decode new json and old with description and no additional properties to the same structure" in {
+    val oldCP = ProcessMarshaller.fromJson(legacyJsonWithDescriptionNoProperties).getOrElse(throw new AssertionError())
+    val newCP = ProcessMarshaller.fromJson(newJsonWithDescriptionNoProperties).getOrElse(throw new AssertionError())
+    oldCP shouldBe newCP
+  }
+
+  it should "decode legacy json with additional properties" in {
+    ProcessMarshaller.fromJson(legacyJsonWithAdditionalProperties).isValid shouldBe true
+  }
+
+  it should "decode new json with additional properties" in {
+    ProcessMarshaller.fromJson(newJsonWithAdditionalProperties).isValid shouldBe true
+  }
+
+  it should "decode new json and old with additional properties to the same structure" in {
+    val oldCP = ProcessMarshaller.fromJson(legacyJsonWithAdditionalProperties).getOrElse(throw new AssertionError())
+    val newCP = ProcessMarshaller.fromJson(newJsonWithAdditionalProperties).getOrElse(throw new AssertionError())
+    oldCP shouldBe newCP
+  }
 }
