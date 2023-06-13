a1cc6942d9c68830da1f713f492b387a9c1e074a
Mateusz Słabek <mslb@touk.pl>
2023-06-13 13:21:50 +0200

    update process preparer

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/NewProcessPreparer.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/NewProcessPreparer.scala
@@ -3,7 +3,7 @@ package pl.touk.nussknacker.ui.process
 import pl.touk.nussknacker.engine.{ProcessingTypeData, TypeSpecificInitialData}
 import pl.touk.nussknacker.engine.api.component.AdditionalPropertyConfig
 import pl.touk.nussknacker.engine.api.process.ProcessName
-import pl.touk.nussknacker.engine.api.{MetaData, ProcessAdditionalFields, TypeSpecificData}
+import pl.touk.nussknacker.engine.api.{FragmentSpecificData, MetaData, ProcessAdditionalFields, RequestResponseMetaData, ScenarioSpecificData, TypeSpecificData}
 import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
 import pl.touk.nussknacker.restmodel.process.ProcessingType
 import pl.touk.nussknacker.ui.process.processingtypedata.ProcessingTypeDataProvider
@@ -41,41 +41,16 @@ class NewProcessPreparer(emptyProcessCreate: ProcessingTypeDataProvider[TypeSpec
 
   private def defaultProperties(processingType: ProcessingType,
                                 typeSpecificData: TypeSpecificData): Map[String, String] = {
-    // TODO: check if this is correct handling of fragments
-    if (typeSpecificData.isSubprocess) {
-      return typeSpecificData.toMap
-    }
-
-    val additionalFieldsConfig = additionalFields.forTypeUnsafe(processingType)
-
-    validateRequiredConfigPresent(typeSpecificData, additionalFieldsConfig)
-
-    additionalFieldsConfig.collect {
-      case (name, parameterConfig) if !typeSpecificData.toMap.contains(name) =>
-        name -> parameterConfig.defaultValue.getOrElse("")
-      case (name, parameterConfig) if typeSpecificData.toMap.contains(name) =>
-        val typeSpecificDefault = typeSpecificData.toMap(name)
-        val parameterDefault = parameterConfig.defaultValue
-        // In case when the parameter default is None, we get the default from type specific initial data. This is necessary
-        // when defaults are dynamic - for example slug in request-response
-        if (parameterDefault.isDefined && parameterDefault.get != typeSpecificDefault) {
-          throw new IllegalStateException(
-            s"""Property with name: $name has inconsistent default configuration. For properties that are also present
-               | in TypeSpecificData the defaults have to match. The default value for property was: ${parameterDefault.get}
-               | and for corresponding property in TypeSpecificData was ${typeSpecificDefault}""".stripMargin)
+    val configOutsideProviderConfig: Map[String, String] = typeSpecificData match {
+      case data: ScenarioSpecificData =>
+        data match {
+          case RequestResponseMetaData(_) => typeSpecificData.toMap
+          case _ => Map.empty
         }
-        name -> typeSpecificDefault
+      case FragmentSpecificData(_) => typeSpecificData.toMap
     }
+    val configFromConfigProvider = additionalFields.forTypeUnsafe(processingType).map(s => s._1 -> s._2.defaultValue.getOrElse(""))
+    configFromConfigProvider ++ configOutsideProviderConfig
   }
 
-  private def validateRequiredConfigPresent(typeSpecificData: TypeSpecificData,
-                                            propertyConfig: Map[String, AdditionalPropertyConfig]): Unit = {
-    typeSpecificData.toMap.collect {
-      case (key, _) if !propertyConfig.contains(key) =>
-        throw new IllegalStateException(
-          s"""Configuration for property with name: $key is not present. Type specific properties have to have explicit
-             | configuration (AdditionalPropertyConfig)""".stripMargin
-        )
-    }
-  }
 }
