e414c1684359c17e7565b6301e5aee1fab644555
Grzegorz Skrobisz <gsk@touk.pl>
2023-06-13 17:16:58 +0200

    CR fix (lastStateAction)

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/ManagementResourcesSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/ManagementResourcesSpec.scala
@@ -122,7 +122,7 @@ class ManagementResourcesSpec extends AnyFunSuite with ScalatestRouteTest with F
     deployProcess(SampleProcess.process.id, Some(DeploymentCommentSettings.unsafe("deploy.*", Some("deployComment"))), comment = Some("deployComment")) ~> checkThatEventually {
       getProcess(processName) ~> check {
         val processDetails = responseAs[ProcessDetails]
-        processDetails.lastAction.exists(_.isDeployed) shouldBe true
+        processDetails.lastStateAction.exists(_.isDeployed) shouldBe true
       }
       cancelProcess(SampleProcess.process.id, Some(DeploymentCommentSettings.unsafe("cancel.*", Some("cancelComment"))), comment = Some("cancelComment")) ~> check {
         status shouldBe StatusCodes.OK
@@ -163,7 +163,7 @@ class ManagementResourcesSpec extends AnyFunSuite with ScalatestRouteTest with F
       getProcess(processName) ~> check {
         val processDetails = responseAs[ProcessDetails]
         processDetails.lastStateAction shouldBe deployedWithVersions(1)
-        processDetails.lastAction.exists(_.isDeployed) shouldBe true
+        processDetails.lastStateAction.exists(_.isDeployed) shouldBe true
       }
     }
   }
@@ -177,7 +177,7 @@ class ManagementResourcesSpec extends AnyFunSuite with ScalatestRouteTest with F
         cancelProcess(SampleProcess.process.id) ~> check {
           getProcess(processName) ~> check {
             decodeDetails.lastStateAction should not be None
-            decodeDetails.lastAction.exists(_.isCanceled) shouldBe  true
+            decodeDetails.lastStateAction.exists(_.isCanceled) shouldBe  true
           }
         }
       }
