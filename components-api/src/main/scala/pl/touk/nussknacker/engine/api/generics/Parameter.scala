0f1bddf8a2193704c781929bc59cb22a76211a0c
Bartłomiej Tartanus <btr@touk.pl>
2023-06-14 16:49:19 +0200

    Add description and parameters to expression suggestions

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/generics/Parameter.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/generics/Parameter.scala
@@ -1,7 +1,6 @@
 package pl.touk.nussknacker.engine.api.generics
 
-import io.circe.generic.JsonCodec
-import pl.touk.nussknacker.engine.api.typed.typing.{Typed, TypedClass, TypingResult}
+import pl.touk.nussknacker.engine.api.typed.typing.TypingResult
 
 case class Parameter(name: String, refClazz: TypingResult)
 
