2b678bd76c5178f09675ab82ce66cf0d583ac30b
Julian Wielga <j@touk.pl>
2023-06-14 11:36:22 +0200

    fixed test

--- designer/client/__mocks__/svgComponentMock.ts
+++ designer/client/__mocks__/svgComponentMock.ts
@@ -1,5 +1,3 @@
-import React from "react"
-
-const content = "<svg></svg>"
-export const ReactComponent = React.Fragment
-export default content
+import React from "react";
+const ReactComponent = React.Fragment;
+export default ReactComponent;
