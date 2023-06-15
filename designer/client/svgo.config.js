1ec586cfcd7decfe865ed4732ee8397df081f4b8
JulianWielga <j@touk.pl>
2023-06-15 19:56:58 +0200

    svgo config fix (#4337)

--- designer/client/svgo.config.js
+++ designer/client/svgo.config.js
@@ -1,36 +1,37 @@
 module.exports = {
-  multipass: true,
-  pretty: true,
-  plugins: [
-    {
-      name: "preset-default",
-      params: {
-        overrides: {
-          //We inline styles because otherwise those styles are globally visible and can cause some unexpected behaviour
-          inlineStyles: {
-            onlyMatchedOnce: true,
-          },
-          removeUselessStrokeAndFill: {
-            removeNone: true,
-          },
-          removeViewBox: false,
+    multipass: true,
+    js2svg: {
+        pretty: true,
+    },
+    plugins: [
+        {
+            name: "preset-default",
+            params: {
+                overrides: {
+                    //We inline styles because otherwise those styles are globally visible and can cause some unexpected behaviour
+                    inlineStyles: {
+                        onlyMatchedOnce: true,
+                    },
+                    removeUselessStrokeAndFill: {
+                        removeNone: true,
+                    },
+                    removeViewBox: false,
+                },
+            },
         },
-      },
-    },
-    "removeOffCanvasPaths",
-    "convertStyleToAttrs",
-    {
-      name: "removeAttrs",
-      params: {
-        attrs: "(,*vectornator.*)",
-      },
-    },
-    {
-      name: "convertColors",
-      params: {
-        currentColor: /#ccc|#fff/,
-      },
-    },
-  ],
-}
-
+        "removeOffCanvasPaths",
+        "convertStyleToAttrs",
+        {
+            name: "removeAttrs",
+            params: {
+                attrs: "(,*vectornator.*)",
+            },
+        },
+        {
+            name: "convertColors",
+            params: {
+                currentColor: /#ccc|#fff/,
+            },
+        },
+    ],
+};
