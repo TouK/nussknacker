994fa57fbf712c1ce36023e98d8227e55d318a63
Mateusz Słabek <mslb@touk.pl>
2023-06-16 16:29:54 +0200

    reorder files and add todo to set initial property values in one place

--- engine/lite/deploymentManager/src/main/scala/pl/touk/nussknacker/lite/manager/LiteDeploymentManagerProvider.scala
+++ engine/lite/deploymentManager/src/main/scala/pl/touk/nussknacker/lite/manager/LiteDeploymentManagerProvider.scala
@@ -4,13 +4,39 @@ import com.typesafe.config.Config
 import pl.touk.nussknacker.engine.api.component.AdditionalPropertyConfig
 import pl.touk.nussknacker.engine.api.definition.{LiteralIntegerValidator, MinimalNumberValidator, StringParameterEditor}
 import pl.touk.nussknacker.engine.api.process.ProcessName
-import pl.touk.nussknacker.engine.api.{LiteStreamMetaData, RequestResponseMetaData}
 import pl.touk.nussknacker.engine.requestresponse.api.openapi.RequestResponseOpenApiSettings
 import pl.touk.nussknacker.engine.{DeploymentManagerProvider, MetaDataInitializer}
 
 trait LiteDeploymentManagerProvider extends DeploymentManagerProvider {
 
-  private val streamingInitialMetaData = MetaDataInitializer("LiteStreamMetaData", Map("parallelism" -> "1"))
+  override def metaDataInitializer(config: Config): MetaDataInitializer = {
+    forMode(config)(
+      LitePropertiesConfig.streamingInitialMetaData,
+      MetaDataInitializer("RequestResponseMetaData", scenarioName => Map("slug" -> defaultRequestResponseSlug(scenarioName, config)))
+    )
+  }
+
+  protected def defaultRequestResponseSlug(scenarioName: ProcessName, config: Config): String
+
+  override def additionalPropertiesConfig(config: Config): Map[String, AdditionalPropertyConfig] = forMode(config)(
+    LitePropertiesConfig.streamProperties,
+    LitePropertiesConfig.requestResponseProperties
+  )
+
+  // TODO: Lite DM will be able to handle both streaming and rr, without mode, when we add scenarioType to
+  //       TypeSpecificInitialData.forScenario and add scenarioType -> mode mapping with reasonable defaults to configuration
+  protected def forMode[T](config: Config)(streaming: => T, requestResponse: => T): T = {
+    config.getString("mode") match {
+      case "streaming" => streaming
+      case "request-response" => requestResponse
+      case other => throw new IllegalArgumentException(s"Unsupported mode: $other")
+    }
+  }
+
+}
+
+object LitePropertiesConfig {
+  val streamingInitialMetaData: MetaDataInitializer = MetaDataInitializer("LiteStreamMetaData", Map("parallelism" -> "1"))
 
   private val parallelismConfig: (String, AdditionalPropertyConfig) = "parallelism" ->
     AdditionalPropertyConfig(
@@ -26,32 +52,9 @@ trait LiteDeploymentManagerProvider extends DeploymentManagerProvider {
       label = Some("Slug")
     )
 
-  private val liteStreamProperties = Map(parallelismConfig)
-  private val requestResponseProperties = RequestResponseOpenApiSettings.additionalPropertiesConfig ++ Map(slugConfig)
+  val streamProperties: Map[String, AdditionalPropertyConfig] = Map(parallelismConfig)
 
-  override def metaDataInitializer(config: Config): MetaDataInitializer = {
-    forMode(config)(
-      streamingInitialMetaData,
-      MetaDataInitializer("RequestResponseMetaData", scenarioName => Map("slug" -> defaultRequestResponseSlug(scenarioName, config)))
-    )
-
-  }
-
-  protected def defaultRequestResponseSlug(scenarioName: ProcessName, config: Config): String
-
-  override def additionalPropertiesConfig(config: Config): Map[String, AdditionalPropertyConfig] = forMode(config)(
-    liteStreamProperties,
-    requestResponseProperties
-  )
-
-  // TODO: Lite DM will be able to handle both streaming and rr, without mode, when we add scenarioType to
-  //       TypeSpecificInitialData.forScenario and add scenarioType -> mode mapping with reasonable defaults to configuration
-  protected def forMode[T](config: Config)(streaming: => T, requestResponse: => T): T = {
-    config.getString("mode") match {
-      case "streaming" => streaming
-      case "request-response" => requestResponse
-      case other => throw new IllegalArgumentException(s"Unsupported mode: $other")
-    }
-  }
+  val requestResponseProperties: Map[String, AdditionalPropertyConfig] =
+    RequestResponseOpenApiSettings.additionalPropertiesConfig ++ Map(slugConfig)
 
 }
