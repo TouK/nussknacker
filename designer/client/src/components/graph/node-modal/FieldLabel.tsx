d67555e20e7be82eb8ebec937dc822ddc303f44d
Mateusz Słabek <mslb@touk.pl>
2023-06-22 16:27:57 +0200

    review

--- designer/client/src/components/graph/node-modal/FieldLabel.tsx
+++ designer/client/src/components/graph/node-modal/FieldLabel.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { getProcessDefinitionData } from "../../../reducers/selectors/settings";
 import ProcessUtils from "../../../common/ProcessUtils";
 import React from "react";
+import { css, cx } from "@emotion/css";
 
 export function findParamDefinitionByName(definitions: UIParameter[], paramName: string): UIParameter {
     return definitions?.find((param) => param.name === paramName);
@@ -24,21 +25,24 @@ export function FieldLabel({
     const processDefinitionData = useSelector(getProcessDefinitionData);
     const params = getNodeParams(processDefinitionData, nodeId);
     const parameter = findParamDefinitionByName(parameterDefinitions, paramName);
-    const label = (params?.[paramName]?.label ?? paramName) + ":";
+    const label = params?.[paramName]?.label ?? paramName;
+    const readableType = ProcessUtils.humanReadableType(parameter?.typ || null);
+
+    const lineLimitStyle = css({
+        display: "-webkit-box",
+        WebkitLineClamp: 3,
+        WebkitBoxOrient: "vertical",
+        overflow: "hidden",
+    });
 
     return (
         <div className="node-label" title={paramName}>
-            {label}
-            {parameter && <LabelFooter parameterType={parameter.typ} />}
-        </div>
-    );
-}
-
-function LabelFooter({ parameterType }: { parameterType: TypingResult }): JSX.Element {
-    const readableType = ProcessUtils.humanReadableType(parameterType);
-    return (
-        <div className="labelFooter line-cut" title={readableType}>
-            {readableType}
+            {label + ":"}
+            {parameter && (
+                <div className={cx("labelFooter", lineLimitStyle)} title={readableType}>
+                    {readableType}
+                </div>
+            )}
         </div>
     );
 }
