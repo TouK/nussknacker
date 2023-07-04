e1fe51725f9ac66f28ac3dc4e466646357d92f57
Arek Burdach <arek.burdach@gmail.com>
2023-07-04 17:16:40 +0200

    test for javadocs presentation

--- designer/client/cypress/e2e/suggester.cy.ts
+++ designer/client/cypress/e2e/suggester.cy.ts
@@ -30,6 +30,21 @@ describe("Expression suggester", () => {
             });
     });
 
+    it("should display javadocs", () => {
+        cy.visitNewProcess(seed, "variables");
+        cy.get("[title='toggle left panel']").click();
+        cy.layoutScenario();
+        cy.get("[model-id=kafka-string]").trigger("dblclick");
+        cy.get("[data-testid=window]").as("modal");
+        cy.get("[title=Value]").next().find(".ace_editor").click().type("{selectall}#DATE.parseDat");
+        cy.get(".ace_autocomplete").should("be.visible");
+        cy.get("[data-testid=graphPage]").matchImage({
+            screenshotConfig: {
+                blackout: ["> :not(#nk-graph-main) > div"],
+            },
+        });
+    });
+
     it("should display completions for second line (bugfix)", () => {
         cy.visitNewProcess(seed, "variables");
         cy.layoutScenario();
