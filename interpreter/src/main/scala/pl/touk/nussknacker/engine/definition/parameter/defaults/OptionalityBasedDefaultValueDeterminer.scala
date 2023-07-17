21edfb6b0f0c7aaf2baf82bdca623482c28f41ee
Piotr Rudnicki <prn@touk.pl>
2023-07-17 12:02:45 +0200

    collect substitutions for SqlSpelTemplate

--- interpreter/src/main/scala/pl/touk/nussknacker/engine/definition/parameter/defaults/OptionalityBasedDefaultValueDeterminer.scala
+++ interpreter/src/main/scala/pl/touk/nussknacker/engine/definition/parameter/defaults/OptionalityBasedDefaultValueDeterminer.scala
@@ -1,17 +1,17 @@
 package pl.touk.nussknacker.engine.definition.parameter.defaults
 
-import pl.touk.nussknacker.engine.api.definition.SpelTemplateParameterEditor
+import pl.touk.nussknacker.engine.api.definition.{SpelTemplateParameterEditor, SqlParameterEditor}
 import pl.touk.nussknacker.engine.graph.expression.Expression
 
 protected object OptionalityBasedDefaultValueDeterminer extends ParameterDefaultValueDeterminer {
 
   override def determineParameterDefaultValue(parameters: DefaultValueDeterminerParameters): Option[Expression] = {
     Option(parameters).filter(_.isOptional).map { _ =>
-      val lang = if (parameters.determinedEditor.contains(SpelTemplateParameterEditor)) {
-        Expression.Language.SpelTemplate
-      } else {
-        Expression.Language.Spel
-      }
+      val lang = parameters.determinedEditor.collect {
+        case SpelTemplateParameterEditor => Expression.Language.SpelTemplate
+        case SqlParameterEditor => Expression.Language.SqlSpelTemplate
+      }.getOrElse(Expression.Language.Spel)
+
       Expression(lang, "")
     }
   }
