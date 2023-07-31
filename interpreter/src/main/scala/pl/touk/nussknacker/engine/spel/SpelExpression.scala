fcf3d09316440a14aed0a6e6d8e5a5eae863b743
Piotr Rudnicki <prn@touk.pl>
2023-07-31 15:00:51 +0200

    Remove sql spel template temporary language

--- interpreter/src/main/scala/pl/touk/nussknacker/engine/spel/SpelExpression.scala
+++ interpreter/src/main/scala/pl/touk/nussknacker/engine/spel/SpelExpression.scala
@@ -174,10 +174,6 @@ object SpelExpressionParser extends LazyLogging {
   //TODO: should we enable other prefixes/suffixes?
   object Template extends Flavour(GraphExpression.Language.SpelTemplate, Some(ParserContext.TEMPLATE_EXPRESSION))
 
-  object SqlSpelTemplateParserContext extends TemplateParserContext("#{", "}#")
-
-  object SqlTemplate extends Flavour(GraphExpression.Language.SqlSpelTemplate, Some(SqlSpelTemplateParserContext))
-
   private[spel] final val LazyValuesProviderVariableName: String = "$lazy"
   private[spel] final val LazyContextVariableName: String = "$lazyContext"
 
