ba530e0350ad4e0d2dd2f1c7320af9d00b654bb7
Arek Burdach <arek.burdach@gmail.com>
2023-08-01 11:18:17 +0200

    Reverted handling spel templates by dict substitution mechanism

--- interpreter/src/main/scala/pl/touk/nussknacker/engine/spel/ast/SpelSubstitutionsCollector.scala
+++ interpreter/src/main/scala/pl/touk/nussknacker/engine/spel/ast/SpelSubstitutionsCollector.scala
@@ -17,7 +17,7 @@ class SpelSubstitutionsCollector(typeForNode: SpelNode => Option[TypingResult],
 
   override def collectSubstitutions(expression: Expression): List[ExpressionSubstitution] = {
     // TODO: handle other languages, especially spel template
-    if ((expression.language == Expression.Language.Spel || expression.language == Expression.Language.SpelTemplate) && !StringUtils.isBlank(expression.expression))
+    if (expression.language == Expression.Language.Spel && !StringUtils.isBlank(expression.expression))
       collectSubstitutions(expression.expression)
     else
       List.empty
