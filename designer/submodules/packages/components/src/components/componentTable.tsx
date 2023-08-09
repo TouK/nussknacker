bab2a3df20adb74be5e0e137abcb3e35e358a2d4
Dawid Poliszak <dpl@touk.pl>
2023-08-09 12:06:29 +0200

    fix content menu items are not links issue

--- designer/submodules/packages/components/src/components/componentTable.tsx
+++ designer/submodules/packages/components/src/components/componentTable.tsx
@@ -54,14 +54,16 @@ export function ComponentTable(props: TableViewData<ComponentType>): JSX.Element
                 getActions: ({ row }) =>
                     row.links.map((link, i) => (
                         <GridActionsCellItem
+                            // From @mui/x-data-grid:5.10.0 version it's a problem with a type definition, but we are able to pass component prop
+                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                            // @ts-ignore
+                            component={ExternalLink}
                             key={link.id}
-                            icon={
-                                <ExternalLink key={link.id} href={link.url} target="_blank">
-                                    <NuIcon src={link.icon} title={link.title} sx={{ fontSize: "1.5rem", verticalAlign: "middle" }} />
-                                </ExternalLink>
-                            }
+                            icon={<NuIcon src={link.icon} title={link.title} sx={{ fontSize: "1.5rem", verticalAlign: "middle" }} />}
                             label={link.title}
                             showInMenu={i > 0}
+                            href={link.url}
+                            target="_blank"
                         ></GridActionsCellItem>
                     )),
             },
