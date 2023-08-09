aadc7e010cc1e4fa222ecffd8328bb19977f760b
Julian Wielga <j@touk.pl>
2023-08-09 16:14:10 +0200

    get data for notification actions in time

--- designer/client/src/actions/notificationActions.tsx
+++ designer/client/src/actions/notificationActions.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Notifications from "react-notification-system-redux";
-import { v4 as uuid4 } from "uuid";
 import TipsError from "../assets/img/icons/tipsError.svg";
 import TipsInfo from "../assets/img/icons/tipsInfo.svg";
 import TipsSuccess from "../assets/img/icons/tipsWarning.svg";
@@ -10,7 +9,7 @@ import { Action } from "./reduxTypes";
 export function success(message: string): Action {
     return Notifications.success({
         autoDismiss: 10,
-        children: [<Notification icon={<TipsSuccess />} message={message} key={uuid4()} />],
+        children: <Notification icon={<TipsSuccess />} message={message} />,
     });
 }
 
@@ -18,13 +17,13 @@ export function error(message: string, error?: string, showErrorText?: boolean):
     const details = showErrorText && error ? error : null;
     return Notifications.error({
         autoDismiss: 10,
-        children: [<Notification icon={<TipsError />} message={message} details={details} key={uuid4()} />],
+        children: <Notification icon={<TipsError />} message={message} details={details} />,
     });
 }
 
 export function info(message: string): Action {
     return Notifications.info({
         autoDismiss: 10,
-        children: [<Notification icon={<TipsInfo />} message={message} key={uuid4()} />],
+        children: <Notification icon={<TipsInfo />} message={message} />,
     });
 }
