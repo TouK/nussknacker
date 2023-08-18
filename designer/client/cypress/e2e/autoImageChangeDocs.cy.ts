1e3001afcdf8da61720c19b4905724f13b23ac5e
Filip <fgl@touk.pl>
2023-08-18 15:00:00 +0200

    Auto Image Change Docs done

---
+++ designer/client/cypress/e2e/autoImageChangeDocs.cy.ts
@@ -0,0 +1,144 @@
+describe("Auto Image Change Docs - ", () => {
+    const seed = "autoImageChangeDocs";
+
+    before(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+    });
+
+    afterEach(() => {
+        cy.deleteAllTestProcesses({ filter: seed });
+    });
+
+    it("basic components - variable", () => {
+        cy.visitNewProcess(seed, "docsBasicComponentsVariable#0");
+        cy.layoutScenario();
+        takeGraphImage();
+
+        cy.get('[model-id="My first variable declaration"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+        cy.visitNewProcess(seed, "docsBasicComponentsVariable#1");
+        cy.get('[model-id="only financial ops"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it("basic components - mapmariable", () => {
+        cy.visitNewProcess(seed, "docsBasicComponentsMapVariable#0");
+        cy.layoutScenario();
+        cy.get('[model-id="node label goes here"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+        cy.get("[data-testid=window]")
+            .contains(/^cancel$/i)
+            .click();
+
+        cy.get('[model-id="variable"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it("basic components - filter", () => {
+        cy.visitNewProcess(seed, "docsBasicComponentsFilter#0");
+        cy.layoutScenario();
+        takeGraphImage();
+
+        cy.visitNewProcess(seed, "docsBasicComponentsFilter#1");
+        cy.layoutScenario();
+        takeGraphImage();
+
+        cy.get('[model-id="conditional filter"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it.skip("basic components - choice", () => {
+        //skip
+        cy.visitNewProcess(seed, "docsBasicComponentsChoice#0");
+        cy.layoutScenario();
+        takeGraphImage();
+
+        cy.get('[model-id="choice"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it("basic components - split", () => {
+        cy.visitNewProcess(seed, "docsBasicComponentsSplit#0");
+        cy.layoutScenario();
+        takeGraphImage();
+    });
+
+    it("basic components - foreach", () => {
+        cy.visitNewProcess(seed, "docsBasicComponentsForEach#0");
+        cy.layoutScenario();
+        cy.get('[model-id="for-each"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it.skip("basic components - union", () => {
+        //skip
+        cy.visitNewProcess(seed, "docsBasicComponentsUnion#0");
+        cy.layoutScenario();
+        takeGraphImage();
+
+        cy.get('[model-id="union"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it.skip("aggregates - Single Side Join", () => {
+        //skip
+        cy.visitNewProcess(seed, "docsAggregatesSingleSideJoin#0");
+        cy.layoutScenario();
+        takeGraphImage();
+
+        cy.get('[model-id="single-side-join"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it.skip("aggregates - Full Outer Join", () => {
+        //skip
+        cy.visitNewProcess(seed, "docsAggregatesFullOuterJoin#0");
+        cy.layoutScenario();
+        cy.get('[model-id="full-outer-join"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it("fragments - Inputs", () => {
+        cy.visitNewProcess(seed, "docsFragmentsInputs#0");
+        cy.layoutScenario();
+        cy.get('[model-id="input"]').dblclick();
+        cy.get('[title="Name"]').click();
+        takeWindowImage();
+    });
+
+    it("fragments - Outputs", () => {
+        cy.visitNewProcess(seed, "docsFragmentsOutputs#0");
+        cy.layoutScenario();
+        takeGraphImage();
+    });
+});
+
+const snapshotOptions = {
+    maxDiffThreshold: 0.00001,
+    imagesPath: "../../docs/scenarios_authoring/img/autoImageChangeDocs",
+};
+
+function takeGraphImage() {
+    cy.get('[joint-selector="layers"]').matchImage({
+        ...snapshotOptions,
+        screenshotConfig: { padding: 16 },
+    });
+}
+
+function takeWindowImage() {
+    cy.get('[data-testid="window-frame"]').matchImage(snapshotOptions);
+}
