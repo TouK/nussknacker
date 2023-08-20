af4e71c806a4b250d3efa2e413ce1f3bea4eb938
Julian Wielga <j@touk.pl>
2023-08-20 17:21:35 +0200

    NU-975 remove dependency to react-bootstrap/lib/Panel

--- designer/client/src/components/toolbarComponents/ToolbarWrapper.tsx
+++ designer/client/src/components/toolbarComponents/ToolbarWrapper.tsx
@@ -1,5 +1,4 @@
-import React, { Children, PropsWithChildren, useCallback, useMemo, useState } from "react";
-import Panel from "react-bootstrap/lib/Panel";
+import React, { PropsWithChildren, useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleToolbar } from "../../actions/nk/toolbars";
 import CollapseIcon from "../../assets/img/arrows/panel-hide-arrow.svg";
@@ -7,29 +6,27 @@ import CloseIcon from "../../assets/img/close.svg";
 import { getIsCollapsed, getToolbarsConfigId } from "../../reducers/selectors/toolbars";
 import ErrorBoundary from "../common/ErrorBoundary";
 import styleVariables from "../../stylesheets/_variables.styl";
+import { styled } from "@mui/material";
 import { useDragHandler } from "./DragHandle";
-import styled from "@emotion/styled";
-import { css } from "@emotion/css";
-import { getContrastColor, getDarkenContrastColor } from "../../containers/theme";
+import { CollapsiblePanelContent, Panel, PanelHeader } from "./Panel";
 
-const { panelBackground, panelHeaderTextSize, sidebarWidth } = styleVariables;
+const { panelBackground, sidebarWidth } = styleVariables;
 
 export type ToolbarWrapperProps = PropsWithChildren<{
     id?: string;
     title?: string;
-    noTitle?: boolean;
     onClose?: () => void;
     color?: string;
 }>;
 
-const Title = styled.div({
+const Title = styled("div")({
     padding: "0 .25em",
     overflow: "hidden",
     textOverflow: "ellipsis",
     flex: 1,
 });
 
-const IconWrapper = styled.div({
+const IconWrapper = styled("div")({
     padding: 0,
     flexShrink: 0,
     border: 0,
@@ -40,10 +37,10 @@ const IconWrapper = styled.div({
 
 const StyledCollapseIcon = styled(CollapseIcon, {
     shouldForwardProp: (name) => name !== "collapsed",
-})(({ collapsed }: { collapsed?: boolean }) => ({
+})<{ collapsed?: boolean }>(({ collapsed, theme }) => ({
     padding: "0 .25em",
     height: "1em",
-    transition: "all .3s",
+    transition: theme.transitions.create("transform", { duration: theme.transitions.duration.standard }),
     transform: `rotate(${collapsed ? 180 : 90}deg)`,
 }));
 
@@ -52,122 +49,51 @@ const StyledCloseIcon = styled(CloseIcon)({
     width: "1em",
 });
 
-const StyledPanel = styled(Panel)(({ expanded }) => ({
-    opacity: expanded ? 1 : 0.86,
-    transition: `all ${expanded ? 0.3 : 0.2}s ease-in-out`,
-}));
-
-const bsClass = css({
-    pointerEvents: "auto",
-    minWidth: sidebarWidth,
-    maxWidth: sidebarWidth,
-
-    "&-title": {
-        textTransform: "uppercase",
-        fontSize: panelHeaderTextSize,
-        fontFamily: "Open Sans",
-        fontWeight: 600,
-
-        "& > a": {
-            display: "flow-root",
-            overflow: "hidden",
-        },
-
-        "& > a, & > a:focus, & > a:hover": {
-            textDecoration: "none",
-            color: "inherit",
-        },
-    },
-
-    "&-body": {
-        userSelect: "text",
-        display: "flow-root",
-    },
-});
-
-const Line = styled.div({
-    display: "flex",
-    height: "2em",
-    justifyContent: "space-between",
-    lineHeight: "2em",
-    padding: "0 .5em",
-    flexGrow: 0,
-});
-
-export function ToolbarWrapper(props: ToolbarWrapperProps): JSX.Element | null {
-    const { title, noTitle, children, id, onClose, color = panelBackground } = props;
-    const dispatch = useDispatch();
-    const isCollapsed = useSelector(getIsCollapsed);
-    const [collapsed, setCollapsed] = useState(isCollapsed(id));
-    const configId = useSelector(getToolbarsConfigId);
-
-    const onToggle = useCallback(
-        () => id && dispatch(toggleToolbar(id, configId, !isCollapsed(id))),
-        [configId, dispatch, id, isCollapsed],
-    );
-
-    const collapseCallbacks = useMemo(
-        () => ({
-            onEnter: () => setCollapsed(false),
-            onExit: () => setCollapsed(true),
-        }),
-        [],
-    );
-
+export function ToolbarWrapper(props: ToolbarWrapperProps): React.JSX.Element | null {
+    const { title, children, id, onClose, color = panelBackground } = props;
     const handlerProps = useDragHandler();
 
-    const themeClassName = useMemo(() => {
-        const panelHeaderBackground = getDarkenContrastColor(color, 1.25);
-        const panelText = getContrastColor(color);
-        const panelHeaderText = getContrastColor(panelHeaderBackground);
-
-        return css({
-            borderColor: panelHeaderBackground,
-            background: color,
-
-            [`.${bsClass}-title`]: {
-                background: panelHeaderBackground,
-                color: panelHeaderText,
-            },
-
-            [`.${bsClass}-body`]: {
-                color: panelText,
-            },
-        });
-    }, [color]);
-
-    if (!Children.count(children)) {
-        return null;
-    }
+    const dispatch = useDispatch();
+    const toolbarsConfigId = useSelector(getToolbarsConfigId);
 
     const isCollapsible = !!id && !!title;
 
-    return (
-        <StyledPanel expanded={!isCollapsed(id)} onToggle={onToggle} bsClass={bsClass} className={themeClassName}>
-            {!noTitle && (
-                <Panel.Heading {...handlerProps} tabIndex={-1}>
-                    <Panel.Title toggle={isCollapsible}>
-                        <Line>
-                            <Title>{title}</Title>
-                            {isCollapsible && (
-                                <IconWrapper>
-                                    <StyledCollapseIcon collapsed={collapsed} />
-                                </IconWrapper>
-                            )}
-                            {onClose && (
-                                <IconWrapper as="button" onClick={onClose}>
-                                    <StyledCloseIcon />
-                                </IconWrapper>
-                            )}
-                        </Line>
-                    </Panel.Title>
-                </Panel.Heading>
-            )}
-            <Panel.Collapse {...collapseCallbacks}>
-                <Panel.Body>
-                    <ErrorBoundary>{children}</ErrorBoundary>
-                </Panel.Body>
-            </Panel.Collapse>
-        </StyledPanel>
+    const isCollapsedStored = useSelector(getIsCollapsed);
+    const storeIsCollapsed = useCallback(
+        (isCollapsed: boolean) => id && dispatch(toggleToolbar(id, toolbarsConfigId, isCollapsed)),
+        [dispatch, id, toolbarsConfigId],
     );
+
+    const [isCollapsedLocal, setIsCollapsedLocal] = useState(isCollapsedStored(id));
+
+    const toggleCollapsed = useCallback(() => {
+        setIsCollapsedLocal((s) => isCollapsible && !s);
+    }, [isCollapsible]);
+
+    return children ? (
+        <Panel expanded={!isCollapsedLocal} color={color} width={sidebarWidth}>
+            <PanelHeader {...handlerProps} onClick={() => toggleCollapsed()} onKeyDown={(e) => e.key === "Enter" && toggleCollapsed()}>
+                <Title>{title}</Title>
+                {isCollapsible && (
+                    <IconWrapper>
+                        <StyledCollapseIcon collapsed={isCollapsedLocal} />
+                    </IconWrapper>
+                )}
+                {onClose && (
+                    <IconWrapper as="button" onClick={onClose}>
+                        <StyledCloseIcon />
+                    </IconWrapper>
+                )}
+            </PanelHeader>
+            <CollapsiblePanelContent
+                in={!isCollapsedLocal}
+                unmountOnExit
+                mountOnEnter
+                onEntered={() => storeIsCollapsed(false)}
+                onExited={() => storeIsCollapsed(true)}
+            >
+                <ErrorBoundary>{children}</ErrorBoundary>
+            </CollapsiblePanelContent>
+        </Panel>
+    ) : null;
 }
