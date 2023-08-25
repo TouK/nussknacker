fd0940fab4f58c863d3d11a3a34168471f8033c0
Julian Wielga <j@touk.pl>
2023-08-25 08:52:35 +0200

    fixed buttons updates

--- designer/client/src/common/userSettings.tsx
+++ designer/client/src/common/userSettings.tsx
@@ -2,15 +2,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSettings, toggleSettings } from "../actions/nk/userSettings";
 import { getUserSettings } from "../reducers/selectors/userSettings";
 import { UserSettings } from "../reducers/userSettings";
+import { useCallback, useMemo } from "react";
 
 export const useUserSettings: () => [UserSettings, (keys: Array<keyof UserSettings>) => void, (value: UserSettings) => void] = () => {
     const dispatch = useDispatch();
     const current = useSelector(getUserSettings);
-    const toggle = (keys: Array<keyof UserSettings>) => {
-        dispatch(toggleSettings(keys));
-    };
-    const reset = (value: UserSettings) => {
-        dispatch(setSettings(value));
-    };
-    return [current, toggle, reset];
+
+    const toggle = useCallback(
+        (keys: Array<keyof UserSettings>) => {
+            dispatch(toggleSettings(keys));
+        },
+        [dispatch],
+    );
+
+    const reset = useCallback(
+        (value: UserSettings) => {
+            dispatch(setSettings(value));
+        },
+        [dispatch],
+    );
+    return useMemo(() => [current, toggle, reset], [current, reset, toggle]);
 };
--- designer/client/src/components/toolbars/edit/buttons/UndoButton.tsx
+++ designer/client/src/components/toolbars/edit/buttons/UndoButton.tsx
@@ -1,19 +1,18 @@
-/* eslint-disable i18next/no-literal-string */
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import Icon from "../../../../assets/img/toolbarButtons/undo.svg";
-import { getHistory } from "../../../../reducers/selectors/graph";
+import { getHistoryPast } from "../../../../reducers/selectors/graph";
 import { useSelectionActions } from "../../../graph/SelectionContextProvider";
 import { CapabilitiesToolbarButton } from "../../../toolbarComponents/CapabilitiesToolbarButton";
 import { ToolbarButtonProps } from "../../types";
 
 function UndoButton(props: ToolbarButtonProps): JSX.Element {
     const { undo } = useSelectionActions();
-    const history = useSelector(getHistory);
+    const past = useSelector(getHistoryPast);
     const { t } = useTranslation();
     const { disabled } = props;
-    const available = !disabled && history.past.length > 0 && undo;
+    const available = !disabled && past.length > 0 && undo;
 
     return (
         <CapabilitiesToolbarButton
