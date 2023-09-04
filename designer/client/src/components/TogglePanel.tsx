1db3f02f0b7e4f3c53a9d2aae0e24e7932191469
Bohdan Artiukhov <bar@touk.pl>
2023-09-04 14:37:00 +0200

    NU-1020 change toggle panel style, remove using styl

--- designer/client/src/components/TogglePanel.tsx
+++ designer/client/src/components/TogglePanel.tsx
@@ -1,24 +1,62 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import "../stylesheets/togglePanel.styl";
 import LeftIcon from "../assets/img/arrows/arrow-left.svg";
 import RightIcon from "../assets/img/arrows/arrow-right.svg";
-import { cx } from "@emotion/css";
+import styled from "@emotion/styled";
 
+const panelWidth = 298;
+type TogglePanelType = "RIGHT" | "LEFT";
+
+interface IconWrapper {
+    type: TogglePanelType;
+    isOpened: boolean;
+}
 interface Props {
     isOpened: boolean;
     onToggle: () => void;
-    type: "RIGHT" | "LEFT";
+    type: TogglePanelType;
 }
+
+function validRightProps(props: IconWrapper, panelWidth: number) {
+    if (props.type === "RIGHT" && !props.isOpened) {
+        return 0;
+    } else if (props.type === "RIGHT" && props.isOpened) {
+        return panelWidth;
+    }
+}
+
+function validLeftProps(props: IconWrapper, panelWidth: number) {
+    if (props.type === "LEFT" && !props.isOpened) {
+        return 0;
+    } else if (props.type === "LEFT" && props.isOpened) {
+        return panelWidth;
+    }
+}
+
+const IconWrapper = styled.div((props: IconWrapper) => ({
+    position: "absolute",
+    bottom: 0,
+    fontSize: 0,
+    lineHeight: 0,
+    height: "25px",
+    width: "25px",
+    transition: "left 0.5s ease, right 0.5s ease",
+    zIndex: 100,
+    cursor: "pointer",
+    color: "black",
+    right: validRightProps(props, panelWidth),
+    left: validLeftProps(props, panelWidth),
+}));
 
 export default function TogglePanel(props: Props): JSX.Element {
     const { t } = useTranslation();
     const { isOpened, onToggle, type } = props;
     const left = type === "LEFT" ? isOpened : !isOpened;
     const title = type === "LEFT" ? t("panel.toggle.left", "toggle left panel") : t("panel.toggle.right", "toggle right panel");
+
     return (
-        <div title={title} className={cx("togglePanel", type, { "is-opened": isOpened })} onClick={onToggle}>
+        <IconWrapper title={title} type={type} isOpened={isOpened} onClick={onToggle}>
             {left ? <LeftIcon /> : <RightIcon />}
-        </div>
+        </IconWrapper>
     );
 }
