ed1637d3a6ab84f66113aac9f18e2d54c24780ba
Adrian Matwiejuk <amt@touk.pl>
2023-09-05 14:31:41 +0200

    remove heuristic timestamp normalization

--- utils/utils/src/main/scala/pl/touk/nussknacker/engine/util/TimestampUtils.scala
+++ utils/utils/src/main/scala/pl/touk/nussknacker/engine/util/TimestampUtils.scala
@@ -7,17 +7,9 @@ object TimestampUtils {
 
   val supportedTimestampTypes: List[TypingResult] = List(Typed[java.lang.Long], Typed[Long], Typed[Int], Typed[Instant], Typed[LocalDate], Typed[ZonedDateTime])
 
-  // some timestamps (direct input from user or in data sources) can be in seconds
-  def normalizeTimestampToMillis(l: Long): Long =
-    if (l > 31536000000L) { // 1971.01.01 00:00:00 timestamp in milliseconds
-      l
-    } else { // l is nearly certainly given in seconds
-      l * 1000
-    }
-
   def supportedTypeToMillis(value: Any, errorMsg: String): Long = value match {
-    case v: Long => normalizeTimestampToMillis(v)
-    case v: Int => normalizeTimestampToMillis(v)
+    case v: Long => v
+    case v: Int => v
     case v: Instant => v.toEpochMilli
     case v: LocalDate => v.atStartOfDay(ZoneId.systemDefault()).toInstant.toEpochMilli
     case v: ZonedDateTime => v.toInstant.toEpochMilli
