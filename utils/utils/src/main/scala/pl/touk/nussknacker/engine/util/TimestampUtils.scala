2db8288a2f25f04cd495acfa6c899d8d31068d26
Piotr Przybylski <piotrprz@gmail.com>
2023-09-06 12:15:50 +0200

    Add invalid timestamp field type to thrown exception

--- utils/utils/src/main/scala/pl/touk/nussknacker/engine/util/TimestampUtils.scala
+++ utils/utils/src/main/scala/pl/touk/nussknacker/engine/util/TimestampUtils.scala
@@ -5,14 +5,23 @@ import pl.touk.nussknacker.engine.api.typed.typing.{Typed, TypingResult}
 
 object TimestampUtils {
 
-  val supportedTimestampTypes: List[TypingResult] = List(Typed[java.lang.Long], Typed[Long], Typed[Int], Typed[Instant], Typed[OffsetDateTime], Typed[ZonedDateTime])
+  val supportedTimestampTypes: Set[TypingResult] = Set(
+    Typed[java.lang.Long],
+    Typed[Long],
+    Typed[Int],
+    Typed[Instant],
+    Typed[ZonedDateTime],
+    Typed[OffsetDateTime],
+  )
 
-  def supportedTypeToMillis(value: Any, errorMsg: String): Long = value match {
+  def supportedTypeToMillis(value: Any, fieldName: String): Long = value match {
     case v: Long => v
     case v: Int => v
     case v: Instant => v.toEpochMilli
     case v: ZonedDateTime => v.toInstant.toEpochMilli
     case v: OffsetDateTime => v.toInstant.toEpochMilli
-    case _ => throw new IllegalArgumentException(errorMsg)
+    case other => throw new IllegalArgumentException(
+      s"Field $fieldName is of an invalid type for a timestamp field: ${if (other == null) "null" else other.getClass}"
+    )
   }
 }
