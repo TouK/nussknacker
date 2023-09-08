fd0dd2b98d0c41c4a0f8165f7985c7995a977deb
Julian Wielga <j@touk.pl>
2023-09-08 09:10:26 +0200

    use defineMarker

--- designer/client/src/components/graph/arrowMarker.ts
+++ designer/client/src/components/graph/arrowMarker.ts
@@ -2,22 +2,18 @@
 import { dia, V } from "jointjs";
 import arrow from "!raw-loader!./arrow.svg";
 
-const arrowMarker = V(
-    "marker",
-    {
-        viewBox: "0 0 10 10",
-        refX: 8,
-        refY: 5,
-        markerWidth: 6,
-        markerHeight: 20,
-        orient: "auto-start-reverse",
-        class: "arrow-marker",
-    },
-    V(arrow).children(),
-);
-
 export function createUniqueArrowMarker(paper: dia.Paper) {
-    const uniqueArrowMarker = arrowMarker.clone();
-    V(paper.defs).append(uniqueArrowMarker);
-    return uniqueArrowMarker.attr("id");
+    return paper.defineMarker({
+        markup: V(arrow).node.innerHTML,
+        attrs: {
+            viewBox: "0 0 10 10",
+            refX: 8,
+            refY: 5,
+            markerUnits: "strokeWidth",
+            markerWidth: 6,
+            markerHeight: 20,
+            orient: "auto-start-reverse",
+            class: "arrow-marker",
+        },
+    });
 }
