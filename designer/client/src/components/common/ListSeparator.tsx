707660ff0460246279238927851d6ea0b942ce2f
Bohdan Artiukhov <bar@touk.pl>
2023-09-08 12:26:42 +0200

    NU-1044 use divider from MIU

--- designer/client/src/components/common/ListSeparator.tsx
+++ designer/client/src/components/common/ListSeparator.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import styled from "@emotion/styled";
+import { Divider } from "@mui/material";
 import { variables } from "../../stylesheets/variables";
 
-const StyledSeparator = styled.hr(({ dark }: { dark?: boolean }) => ({
-    border: "none",
-    borderBottom: "1px solid",
-    margin: "10px 0",
-    color: dark ? variables.panelHeaderBackground : variables.panelTitleTextColor,
-}));
-
 export function ListSeparator(props: { dark?: boolean }): JSX.Element {
-    return <StyledSeparator dark={props.dark} />;
+    return (
+        <Divider sx={{ background: props.dark ? variables.panelHeaderBackground : variables.panelTitleTextColor, margin: "10px 0px" }} />
+    );
 }
