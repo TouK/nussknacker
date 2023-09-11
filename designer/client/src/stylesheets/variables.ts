e4fa743520cd12d85b074fca13fe5bec9dd2148d
Bohdan Artiukhov <bar@touk.pl>
2023-09-11 09:54:49 +0200

    NU-1047 remove stylus

--- designer/client/src/stylesheets/variables.ts
+++ designer/client/src/stylesheets/variables.ts
@@ -1,4 +1,7 @@
 export const variables = {
     panelTitleTextColor: "#808080",
     panelHeaderBackground: "#3e3e3e",
+    buttonBorderColor: "#666666",
+    menuButtonBorderColor: "#666666",
+    menuButtonActiveBKColor: "#5d5d5d",
 };
