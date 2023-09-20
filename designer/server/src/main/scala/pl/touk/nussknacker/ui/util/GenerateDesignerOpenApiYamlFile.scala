6a68b785e4ea05522755e74f51a2cf50f6bce867
Mateusz Kołodziejczyk <mkl@touk.pl>
2023-09-20 16:01:50 +0200

    fix

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/util/GenerateDesignerOpenApiYamlFile.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/util/GenerateDesignerOpenApiYamlFile.scala
@@ -1,13 +1,26 @@
 package pl.touk.nussknacker.ui.util
 
 import better.files.Dsl._
+import cats.effect.{ExitCode, IO, IOApp}
 import pl.touk.nussknacker.ui.api.NuDesignerAvailableToExposeApi
 
-object GenerateDesignerOpenApiYamlFile extends App {
+object GenerateDesignerOpenApiYamlFile extends IOApp {
 
-  (pwd / "docs" / "api" / "internal" / "nu-designer-openapi.yaml")
-    .createFileIfNotExists(createParents = true)
-    .overwrite(
-      NuDesignerAvailableToExposeApi.generateOpenApiYaml
-    )
+  override def run(args: List[String]): IO[ExitCode] =
+    generateAndSave()
+
+  private def generateAndSave() = IO {
+    val file = (pwd / "docs" / "api" / "internal" / "nu-designer-openapi.yaml")
+      .createFileIfNotExists(createParents = true)
+
+    val savedNuDesignerOpenApiYaml = file.contentAsString
+    val newNuDesignerOpenApiYaml = NuDesignerAvailableToExposeApi.generateOpenApiYaml
+
+    if (savedNuDesignerOpenApiYaml != newNuDesignerOpenApiYaml) {
+      file.overwrite(newNuDesignerOpenApiYaml)
+      ExitCode.Error
+    } else {
+      ExitCode.Success
+    }
+  }
 }
