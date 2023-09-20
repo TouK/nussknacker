7f5a9f58561e914da1193f2a452cfa5486cbd028
Mateusz Kołodziejczyk <mkl@touk.pl>
2023-09-20 15:48:48 +0200

    generating nu designer openapi yaml file on git push

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/util/GenerateDesignerOpenApiYamlFile.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/util/GenerateDesignerOpenApiYamlFile.scala
@@ -5,9 +5,9 @@ import pl.touk.nussknacker.ui.api.NuDesignerAvailableToExposeApi
 
 object GenerateDesignerOpenApiYamlFile extends App {
 
-  (pwd / "docs" / "api" / "internal" / "nu-designer-openapi.yaml")
-    .createFileIfNotExists(createParents = true)
-    .overwrite(
-      NuDesignerAvailableToExposeApi.generateOpenApiYaml
-    )
+//  (pwd / "docs" / "api" / "internal" / "nu-designer-openapi.yaml")
+//    .createFileIfNotExists(createParents = true)
+//    .overwrite(
+//      NuDesignerAvailableToExposeApi.generateOpenApiYaml
+//    )
 }
