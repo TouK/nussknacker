5ea9d021ca2daccf4f27d7c1cfcea664bd043d9c
Bohdan Artiukhov <bar@touk.pl>
2023-09-22 12:30:17 +0200

    NU-1091 fix problem with styles and snapshot

--- designer/client/src/components/modals/CalculateCounts/CountsStyled.tsx
+++ designer/client/src/components/modals/CalculateCounts/CountsStyled.tsx
@@ -9,22 +9,23 @@ export const PredefinedRangeButton = styled(ButtonWithFocus)`
     font-size: 12px;
     padding: 5px;
     font-weight: 400;
+    margin: 10px;
 `;
 
 export const PredefinedDropdownButton = styled(DropdownButton)`
     ${espButtonBaseStyle};
-    min-width: 80px;
     width: 100%;
-    font-size: 12px;
-    padding: 5px;
+    font-size: 12px !important;
+    padding: 5px !important;
     font-weight: 400;
+    margin: 10px;
 `;
 
 export const StyledRangesWrapper = styled("div")`
-    display: flex;
     flex-wrap: wrap;
+    display: flex;
     justify-content: center;
-    max-width: 600;
+    max-width: 600px;
     margin: 0 -10px;
     button: {
         margin: 10px;
