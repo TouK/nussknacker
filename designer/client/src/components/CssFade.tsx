d0853e87fdaea09fb1e0e48506d387706505d868
Bohdan Artiukhov <bar@touk.pl>
2023-09-22 11:38:15 +0200

    NU-1091 fix problem with federated-types

--- designer/client/src/components/CssFade.tsx
+++ designer/client/src/components/CssFade.tsx
@@ -1,15 +1,51 @@
 import { isFunction } from "lodash";
-import React, { useCallback, useMemo } from "react";
+import React, { ReactNode, useCallback, useMemo } from "react";
 import { CSSTransition } from "react-transition-group";
-import { CSSTransitionProps } from "react-transition-group/CSSTransition";
-import animations from "../stylesheets/animations.styl";
+import { styled } from "@mui/material";
 
-export function CssFade(props: Partial<CSSTransitionProps>) {
+const CSSTransactionStyled = styled(CSSTransition)`
+    opacity: inherit;
+    .group {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+    &-exit,
+    &-enter {
+        .group > & {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    }
+
+    &-enter > * {
+        opacity: 0;
+    }
+
+    &-enter-active > * {
+        opacity: 1;
+        transition: opacity 300ms ease-in;
+    }
+
+    &-exit {
+        opacity: 1;
+    }
+
+    &-exit-active > * {
+        opacity: 0;
+        transition: opacity 300ms ease-in;
+    }
+`;
+
+export function CssFade(props: { key: string; children: ReactNode }) {
     const addEndListener = useCallback((nodeOrDone: HTMLElement | (() => void), done?: () => void) => {
         !isFunction(nodeOrDone) ? nodeOrDone.addEventListener("transitionend", done, false) : nodeOrDone();
     }, []);
 
     const timeout = useMemo(() => ({ enter: 500, appear: 500, exit: 500 }), []);
 
-    return <CSSTransition classNames={animations.fade} timeout={timeout} addEndListener={addEndListener} {...props} />;
+    return <CSSTransactionStyled timeout={timeout} addEndListener={addEndListener} {...props} />;
 }
