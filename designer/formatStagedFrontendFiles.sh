91ca87014742fa2e907a3878014972db1dc06fc2
Piotr Przybylski <piotrprz@gmail.com>
2023-10-05 14:34:22 +0200

    Fix pre-commit hook to work with SBT_OPTS and real sbt launcher is present

--- designer/formatStagedFrontendFiles.sh
+++ designer/formatStagedFrontendFiles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+set -eu
 
-echo "Formatting frontend files ..."
+echo "Formatting frontend files..."
 
 npm run commit --if-present --prefix client
 npm run commit --if-present --prefix submodules
