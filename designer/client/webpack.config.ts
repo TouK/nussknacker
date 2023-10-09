a5cc4deff2fd3c6b0ccdccf3a39ba9fdfdd6faac
Dawid Poliszak <dpl@touk.pl>
2023-10-09 12:40:43 +0200

    NU-1083 fix build attempt

--- designer/client/webpack.config.ts
+++ designer/client/webpack.config.ts
@@ -294,6 +294,10 @@ module.exports = withModuleFederationPlugins(config, {
             singleton: true,
             requiredVersion: dependencies["@emotion/react"],
         },
+        "@emotion/styles": {
+            singleton: true,
+            requiredVersion: dependencies["@mui/styles"],
+        },
         "@mui/material": {
             singleton: true,
             requiredVersion: dependencies["@mui/material"],
