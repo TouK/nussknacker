80b49aefc6c1418705808dbe7088a758b6abb4fd
Dawid Poliszak <dpl@touk.pl>
2023-10-11 09:40:25 +0200

    NU-1108 provide styled Toolbar

--- designer/client/src/components/toolbars/creator/ToolBox.tsx
+++ designer/client/src/components/toolbars/creator/ToolBox.tsx
@@ -4,12 +4,93 @@ import "react-treeview/react-treeview.css";
 import { filterComponentsByLabel, getCategoryComponentGroups } from "../../../common/ProcessDefinitionUtils";
 import { getProcessCategory } from "../../../reducers/selectors/graph";
 import { getProcessDefinitionData } from "../../../reducers/selectors/settings";
-import "../../../stylesheets/toolBox.styl";
 import { ComponentGroup } from "../../../types";
 import { ToolboxComponentGroup } from "./ToolboxComponentGroup";
 import Tool from "./Tool";
 import { useTranslation } from "react-i18next";
+import { styled } from "@mui/material";
+import { alpha } from "../../../containers/theme/helpers";
 
+const StyledToolbox = styled("div")`
+    font-size: 14px;
+    font-weight: 600;
+    padding: 0;
+    padding-bottom: 0.5em;
+    min-height: 2.5em;
+
+    .tree-view {
+        background-color: #3e3e3e;
+    }
+
+    .tree-view_item {
+        background-color: #4d4d4d; //TODO: change me to MUI theme
+        border: 1px solid ${alpha("#434343", 50)}; //TODO: change me to MUI theme
+        cursor: pointer;
+        display: flex;
+        padding: 0 5px;
+        height: 28px;
+        line-height: 28px;
+
+        &:hover {
+            background-color: #444444; //TODO: change me to MUI theme
+            color: #ccc; //TODO: change me to MUI theme
+        }
+    }
+
+    .tree-view_arrow {
+        cursor: inherit;
+    }
+
+    .toolWrapper {
+        background-color: #464646;
+        font-weight: 400;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .tool {
+        margin-bottom: -1px;
+        height: 28px;
+        padding: 0 5px;
+        line-height: 28px;
+        border: 1px solid #434343; //TODO: change me to MUI theme
+
+        &.disabled {
+            opacity: 0.4;
+            cursor: not-allowed !important;
+        }
+
+        &:not(.disabled) {
+            cursor: -moz-grab;
+            cursor: -webkit-grab;
+            cursor: grab;
+            &:active {
+                cursor: -moz-grabbing;
+                cursor: -webkit-grabbing;
+                cursor: grabbing;
+            }
+            color: #b3b3b3;
+
+            &:hover {
+                background-color: darken(toolHoverBkgColor, 7%);
+                color: #ccc; //TODO: change me to MUI theme
+            }
+        }
+    }
+    .toolIcon {
+        height: 16px;
+        width: 16px;
+        display: inline-flex;
+        vertical-align: middle;
+        margin-right: 5px;
+        margin-bottom: 2px;
+    }
+
+    .group-label {
+        width: 95%;
+    }
+`;
 export default function ToolBox(props: { filter: string }): JSX.Element {
     const processDefinitionData = useSelector(getProcessDefinitionData);
     const processCategory = useSelector(getProcessCategory);
@@ -28,7 +109,7 @@ export default function ToolBox(props: { filter: string }): JSX.Element {
     );
 
     return (
-        <div id="toolbox">
+        <StyledToolbox id="toolbox">
             {groups.length ? (
                 groups.map((componentGroup) => (
                     <ToolboxComponentGroup
@@ -41,6 +122,6 @@ export default function ToolBox(props: { filter: string }): JSX.Element {
             ) : (
                 <Tool nodeModel={null} label={t("panels.creator.filter.noMatch", "no matching components")} disabled />
             )}
-        </div>
+        </StyledToolbox>
     );
 }
