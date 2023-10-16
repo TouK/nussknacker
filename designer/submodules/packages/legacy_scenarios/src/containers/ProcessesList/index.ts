e39df105515d863130559b74f6721143c6c8ee2f
JulianWielga <j@touk.pl>
2023-10-16 11:07:48 +0200

    remove legacy scenarios table (#4866)
    * remove legacy_scenarios
    
    * remove legacy_scenarios
    
    * remove legacy_scenarios
    
    * remove legacy_scenarios
    
    * remove legacy_scenarios
    
    * remove legacy_scenarios

--- designer/submodules/packages/legacy_scenarios/src/containers/ProcessesList/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ProcessesList";
-export { useTextFilter } from "./UseTextFilter";
-export type { RowsRenderer, RowRendererProps, BaseProcessesOwnProps, Filterable, Queries } from "./types";
-export { useFiltersState } from "./UseFiltersState";
