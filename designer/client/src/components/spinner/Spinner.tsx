bc640488b070b9cbac39b5bf252d1d6ba5e55ac0
Bohdan Artiukhov <bar@touk.pl>
2023-10-16 12:31:56 +0200

    NU-1085 fix loader position

--- designer/client/src/components/spinner/Spinner.tsx
+++ designer/client/src/components/spinner/Spinner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, CircularProgress, ThemeProvider, circularProgressClasses, createTheme } from "@mui/material";
+import { Box, CircularProgress, circularProgressClasses } from "@mui/material";
 
 type Props = {
     show: boolean;
@@ -12,33 +12,30 @@ function LoaderSpinner(props: Props) {
 export default LoaderSpinner;
 
 function CircularProgressWrapper() {
-    const theme = createTheme({});
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{ position: "absolute", left: "50%", top: "50% " }}>
-                <CircularProgress
-                    variant="determinate"
-                    sx={{ color: (theme) => theme.palette.grey[200] }}
-                    size={40}
-                    thickness={4}
-                    value={100}
-                />
-                <CircularProgress
-                    variant="indeterminate"
-                    disableShrink
-                    sx={{
-                        color: (theme) => theme.palette.grey[500],
-                        animationDuration: "550ms",
-                        position: "absolute",
-                        left: 0,
-                        [`& .${circularProgressClasses.circle}`]: {
-                            strokeLinecap: "round",
-                        },
-                    }}
-                    size={40}
-                    thickness={4}
-                />
-            </Box>
-        </ThemeProvider>
+        <Box sx={{ position: "fixed", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
+            <CircularProgress
+                variant="determinate"
+                sx={{ color: (theme) => theme.palette.grey[200] }}
+                size={40}
+                thickness={4}
+                value={100}
+            />
+            <CircularProgress
+                variant="indeterminate"
+                disableShrink
+                sx={{
+                    color: (theme) => theme.palette.grey[500],
+                    animationDuration: "550ms",
+                    position: "absolute",
+                    left: 0,
+                    [`& .${circularProgressClasses.circle}`]: {
+                        strokeLinecap: "round",
+                    },
+                }}
+                size={40}
+                thickness={4}
+            />
+        </Box>
     );
 }
