43068a465e7e4a29421a35d9a4a1b197d5c2cbfb
Bohdan Artiukhov <bar@touk.pl>
2023-10-17 14:30:13 +0200

    NU-1085 fix problem with provider

--- designer/client/src/components/spinner/Spinner.tsx
+++ designer/client/src/components/spinner/Spinner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, CircularProgress, circularProgressClasses } from "@mui/material";
+import { Box, CircularProgress, createTheme, ThemeProvider, circularProgressClasses } from "@mui/material";
 
 type Props = {
     show: boolean;
@@ -12,30 +12,33 @@ function LoaderSpinner(props: Props) {
 export default LoaderSpinner;
 
 function CircularProgressWrapper() {
+    const theme = createTheme({});
     return (
-        <Box sx={{ position: "fixed", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
-            <CircularProgress
-                variant="determinate"
-                sx={{ color: (theme) => theme.palette.grey[200] }}
-                size={40}
-                thickness={4}
-                value={100}
-            />
-            <CircularProgress
-                variant="indeterminate"
-                disableShrink
-                sx={{
-                    color: (theme) => theme.palette.grey[500],
-                    animationDuration: "550ms",
-                    position: "absolute",
-                    left: 0,
-                    [`& .${circularProgressClasses.circle}`]: {
-                        strokeLinecap: "round",
-                    },
-                }}
-                size={40}
-                thickness={4}
-            />
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ position: "fixed", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
+                <CircularProgress
+                    variant="determinate"
+                    sx={{ color: (theme) => theme.palette.grey[200] }}
+                    size={40}
+                    thickness={4}
+                    value={100}
+                />
+                <CircularProgress
+                    variant="indeterminate"
+                    disableShrink
+                    sx={{
+                        color: (theme) => theme.palette.grey[500],
+                        animationDuration: "550ms",
+                        position: "absolute",
+                        left: 0,
+                        [`& .${circularProgressClasses.circle}`]: {
+                            strokeLinecap: "round",
+                        },
+                    }}
+                    size={40}
+                    thickness={4}
+                />
+            </Box>
+        </ThemeProvider>
     );
 }
