28c02b16fdb4c4926368f9d895fd03161be411cc
Dawid Poliszak <dpl@touk.pl>
2023-10-18 14:22:50 +0200

    NU-1108 switch TimeRange colors to MUI

--- designer/client/src/components/graph/node-modal/editors/expression/Duration/TimeRangeStyled.ts
+++ designer/client/src/components/graph/node-modal/editors/expression/Duration/TimeRangeStyled.ts
@@ -1,46 +1,49 @@
-import { styled } from "@mui/material";
+import { css, styled } from "@mui/material";
+import { alpha } from "../../../../../../containers/theme/helpers";
 
-export const TimeRangeStyled = styled("div")`
-    min-width: 60%;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-
-    .time-range-component {
-        display: inline-flex;
-        margin-right: 1em;
-        margin-bottom: 0.5em;
-    }
-
-    .time-range-component:after {
-        clear: both;
-    }
-
-    .time-range-input {
-        width: 45px !important;
-        border: none !important;
-        background-color: #333;
-        color: #ccc;
-        text-align: center;
-        padding-left: 12px;
-        outline: 1px solid rgba(255, 255, 255, 0.075);
-        height: 35px;
-    }
-
-    .time-range-components {
+export const TimeRangeStyled = styled("div")(
+    ({ theme }) => css`
+        min-width: 60%;
+        flex: 1;
         display: flex;
-        flex-wrap: wrap;
-    }
+        flex-direction: column;
 
-    .time-range-component-label {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0.2em 0.6em 0.3em;
-        font-weight: 700;
-        line-height: 1;
-        text-align: center;
-        vertical-align: baseline;
-        border-radius: 0.25em;
-    }
-`;
+        .time-range-component {
+            display: inline-flex;
+            margin-right: 1em;
+            margin-bottom: 0.5em;
+        }
+
+        .time-range-component:after {
+            clear: both;
+        }
+
+        .time-range-input {
+            width: 45px !important;
+            border: none !important;
+            background-color: ${theme.custom.colors.secondaryBackground};
+            color: ${theme.custom.colors.secondaryColor};
+            text-align: center;
+            padding-left: 12px;
+            outline: 1px solid ${alpha(theme.custom.colors.primaryColor, 0.075)};
+            height: 35px;
+        }
+
+        .time-range-components {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .time-range-component-label {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0.2em 0.6em 0.3em;
+            font-weight: 700;
+            line-height: 1;
+            text-align: center;
+            vertical-align: baseline;
+            border-radius: 0.25em;
+        }
+    `,
+);
