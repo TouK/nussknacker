526edf2ac981c7351b39d8e5961cec9b022d4282
Piotr Fałdrowicz <pfl@touk.pl>
2023-10-20 18:29:46 +0200

    using Map interface instead of HashMap class

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/package.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/package.scala
@@ -2,13 +2,19 @@ package pl.touk.nussknacker.engine.api
 
 package object typed {
 
-  type TypedMap = java.util.HashMap[String, Any]
+  /**
+    * deprecated - introduced to easily remove TypeMap class
+    * use explicitly java.util.Map[String, Any] instead
+    */
+  type TypedMap = java.util.Map[String, Any]
 
   object TypedMap {
 
     import scala.jdk.CollectionConverters._
 
-    def apply(scalaFields: Map[String, Any]): TypedMap = new TypedMap(scalaFields.asJava)
+    def apply(scalaFields: Map[String, Any]): TypedMap = new java.util.HashMap[String, Any](scalaFields.asJava)
+
+    def apply(): TypedMap = new java.util.HashMap[String, Any]()
   }
 
 }
