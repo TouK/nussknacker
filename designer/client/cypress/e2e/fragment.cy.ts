9c655ca87d593f901b7d07e689a43d94ae37fe20
JulianWielga <j@touk.pl>
2023-10-30 13:45:57 +0100

    [NU-1145] fragment outputs (#4933)
    * cleanup
    
    * remove out port from dead-ended fragments
    remove invalid links from view
    
    resolves [NU-1145]
    resolves [NU-1147]
    
    * allow injection of input-only or output-only node
    
    * dead-ended fragments test
    
    * dead-ended fragments test fix
    
    * refine node selection and image matching in fragments tests
    
    * Updated snapshots (#4963)
    
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>

--- designer/client/cypress/e2e/fragment.cy.ts
+++ designer/client/cypress/e2e/fragment.cy.ts
@@ -53,11 +53,14 @@ describe("Fragment", () => {
             .click();
 
         cy.wait(750);
-        cy.get("[data-testid=graphPage]").matchImage({
-            screenshotConfig: {
-                blackout: ["> :not(#nk-graph-main) > div"],
-            },
-        });
+        cy.getNode("sendSms")
+            .parent()
+            .matchImage({
+                screenshotConfig: {
+                    padding: 16,
+                    blackout: ["> :not(#nk-graph-main) > div"],
+                },
+            });
 
         cy.get("[model-id=sendSms]").should("be.visible").trigger("dblclick");
         cy.intercept("POST", "/api/nodes/*/validation", (request) => {
@@ -161,5 +164,49 @@ describe("Fragment", () => {
         cy.get("@window").wait(200).matchImage();
 
         cy.deleteAllTestProcesses({ filter: seed2 });
+    });
+
+    it("should display dead-ended fragment correct", () => {
+        const fragmentName = "fragmentOutput";
+        const deadEndFragmentName = "fragmentDeadEnd";
+        cy.createTestFragment(fragmentName, "fragment").as("fragmentName");
+        cy.visitNewProcess(seed, "testProcess").as("scenarioName");
+        cy.createTestFragment(deadEndFragmentName, "deadEndFragment");
+        cy.layoutScenario();
+
+        cy.contains("fragments").should("be.visible").click();
+        cy.getNode("enricher").as("enricher");
+        cy.contains(`${fragmentName}-test`)
+            .last()
+            .should("be.visible")
+            .drag("@enricher", { target: { x: 250, y: -20 }, force: true });
+        cy.layoutScenario();
+        cy.contains(/^save\*$/i).click();
+        cy.contains(/^ok$/i).click();
+
+        cy.get<string>("@fragmentName").then((name) => cy.visitProcess(name));
+        cy.contains("sinks").should("be.visible").click();
+        cy.getNode("output").as("output");
+        cy.contains("dead-end")
+            .first()
+            .should("be.visible")
+            .drag("@output", { target: { x: 0, y: 0 }, force: true });
+        cy.get("@output").click().type("{backspace}");
+        cy.contains(/^save\*$/i).click();
+        cy.contains(/^ok$/i).click();
+        cy.contains(/^save$/i).should("be.disabled");
+
+        cy.viewport(2000, 800);
+        cy.get<string>("@scenarioName").then((name) => cy.visitProcess(name));
+        cy.getNode("sendSms").as("sendSms");
+        cy.contains(`${deadEndFragmentName}-test`)
+            .last()
+            .should("be.visible")
+            .drag("@sendSms", { target: { x: 250, y: -20 }, force: true });
+        cy.layoutScenario();
+
+        cy.get("@sendSms")
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 16 } });
     });
 });
