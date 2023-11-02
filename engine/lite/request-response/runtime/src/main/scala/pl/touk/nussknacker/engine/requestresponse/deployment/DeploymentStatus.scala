bb60fb779bc8452b5cd5bac0cad79f4222965137
Arek Burdach <arek.burdach@gmail.com>
2023-11-02 19:46:41 +0100

    Obsolete request-response-manager removal

--- engine/lite/request-response/runtime/src/main/scala/pl/touk/nussknacker/engine/requestresponse/deployment/DeploymentStatus.scala
+++
@@ -1,6 +0,0 @@
-package pl.touk.nussknacker.engine.requestresponse.deployment
-
-import io.circe.generic.JsonCodec
-import pl.touk.nussknacker.engine.api.ProcessVersion
-
-@JsonCodec final case class DeploymentStatus(processVersion: ProcessVersion, deploymentTime: Long)
