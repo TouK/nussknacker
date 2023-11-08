f634c0c02ee724230ba44a5742c8da75911e9431
Piotr Fałdrowicz <pfl@touk.pl>
2023-11-08 10:12:31 +0100

    more serialization benchmarks

--- benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/serialization/json/MapSerialization.scala
+++ benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/serialization/json/MapSerialization.scala
@@ -1,5 +1,6 @@
 package pl.touk.nussknacker.engine.benchmarks.serialization.json
 
+import io.circe.JsonObject
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.everit.json.schema.Schema
 import org.openjdk.jmh.annotations._
@@ -9,18 +10,27 @@ import pl.touk.nussknacker.engine.json.JsonSchemaBuilder
 import pl.touk.nussknacker.engine.json.serde.CirceJsonDeserializer
 import pl.touk.nussknacker.engine.json.swagger.extractor.JsonTypedMap
 import pl.touk.nussknacker.engine.process.util.Serializers
+import pl.touk.nussknacker.engine.schemedkafka.schemaregistry.SchemaId
 
 import java.util.concurrent.TimeUnit
 import scala.io.Source
 
+case class RawJsonObjectAndSchemaId(json: String, schemaId: SchemaId)
+case class MyJsonObject(jsonObject: JsonObject, schemaId: SchemaId)
+
 @State(Scope.Thread)
 class MapSerialization {
 
-  val schema: Schema        = JsonSchemaBuilder.parseSchema(Source.fromResource("pgw.schema.json").getLines().mkString)
-  val deserializer          = new CirceJsonDeserializer(schema)
-  val json: String          = Source.fromResource("pgw.record.json").getLines().mkString
-  val lazyMap: JsonTypedMap = deserializer.deserialize(json).asInstanceOf[JsonTypedMap]
+  val schemaString                        = Source.fromResource("pgw.schema.json").getLines().mkString
+  val schema: Schema                      = JsonSchemaBuilder.parseSchema(schemaString)
+  val deserializer                        = new CirceJsonDeserializer(schema)
+  val json: String                        = Source.fromResource("pgw.record.json").getLines().mkString
+  val lazyMap: JsonTypedMap               = deserializer.deserialize(json).asInstanceOf[JsonTypedMap]
   val typeMap: java.util.Map[String, Any] = lazyMap.materialize
+
+  val rawJsonSchemaId: RawJsonObjectAndSchemaId = RawJsonObjectAndSchemaId(json, SchemaId.fromInt(333))
+
+  val jsonWithSchemaId = MyJsonObject(lazyMap.jsonObject, SchemaId.fromInt(333))
 
   val lazyMapSetup = new SerializationBenchmarkSetup(
     TypeInformation.of(classOf[JsonTypedMap]),
@@ -33,16 +43,40 @@ class MapSerialization {
     typeMap
   )
 
+  val rawJsonAndSchemaIdMapSetup = new SerializationBenchmarkSetup(
+    TypeInformation.of(classOf[RawJsonObjectAndSchemaId]),
+    rawJsonSchemaId,
+    config => Serializers.CaseClassSerializer.registerIn(config)
+  )
+
+  val jsonWithSchemaIdSetup = new SerializationBenchmarkSetup(
+    TypeInformation.of(classOf[MyJsonObject]),
+    jsonWithSchemaId,
+    config => Serializers.CaseClassSerializer.registerIn(config)
+  )
+
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  def lazyMapSerialization() =
-    lazyMapSetup.roundTripSerialization()
+  def rawJsonAndSchemaIdSerialization() =
+    rawJsonAndSchemaIdMapSetup.roundTripSerialization()
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def jsonWithSchemaIdSerialization() =
+    jsonWithSchemaIdSetup.roundTripSerialization()
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
   def typeMapSerialization() =
     TypedMapSetup.roundTripSerialization()
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def lazyMapSerialization() =
+    lazyMapSetup.roundTripSerialization()
 
 }
