c8d2cb07e91eae97f190d1ba36925703714c109c
Filip Michalski <fmc@touk.pl>
2023-11-09 16:10:43 +0100

    Change def authenticationMethod() type from Auth[AuthCredentials, _] to EndpointInput[AuthCredentials]

--- security/src/main/scala/pl/touk/nussknacker/ui/security/basicauth/BasicAuthenticationResources.scala
+++ security/src/main/scala/pl/touk/nussknacker/ui/security/basicauth/BasicAuthenticationResources.scala
@@ -3,7 +3,6 @@ package pl.touk.nussknacker.ui.security.basicauth
 import akka.http.scaladsl.server.directives.{AuthenticationDirective, SecurityDirectives}
 import pl.touk.nussknacker.ui.security.api._
 import sttp.model.headers.WWWAuthenticateChallenge
-import sttp.tapir.EndpointInput.Auth
 import sttp.tapir._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -27,7 +26,7 @@ class BasicAuthenticationResources(realm: String, configuration: BasicAuthentica
       realm = realm
     )
 
-  override def authenticationMethod(): Auth[AuthCredentials, _] = {
+  override def authenticationMethod(): EndpointInput[AuthCredentials] = {
     auth.basic[AuthCredentials](WWWAuthenticateChallenge.basic.realm(realm))
   }
 
