cf39528be4e6aab9a95cca7c2fe7ff863dc3da65
Mateusz Słabek <mslb@touk.pl>
2023-11-16 23:48:07 +0100

    extract additional validation logic

--- scenario-api/src/main/scala/pl/touk/nussknacker/engine/graph/node.scala
+++ scenario-api/src/main/scala/pl/touk/nussknacker/engine/graph/node.scala
@@ -317,6 +317,10 @@ object node {
 
   val IdFieldName = "$id"
 
+  // TODO: record vs fields prefix? with $ or without?
+  def recordKeyFieldName(index: Int)   = s"fields-$index-key"
+  def recordValueFieldName(index: Int) = s"fields-$index-value"
+
   // TODO: after migration to cats > 1.0.0 shapeless cast on node subclasses won't compile outside package :|
 
   import shapeless.syntax.typeable._
