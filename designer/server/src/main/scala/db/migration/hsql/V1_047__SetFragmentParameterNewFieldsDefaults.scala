2bbc22fc5e9ec6ad8c6faff503599acd4c200d8d
Adrian Matwiejuk <amt@touk.pl>
2023-11-16 15:20:37 +0100

    DB migration

---
+++ designer/server/src/main/scala/db/migration/hsql/V1_047__SetFragmentParameterNewFieldsDefaults.scala
@@ -0,0 +1,8 @@
+package db.migration.hsql
+
+import db.migration.V1_047__SetFragmentParameterNewFieldsDefaultsDefinition
+import slick.jdbc.{HsqldbProfile, JdbcProfile}
+
+class V1_047__SetFragmentParameterNewFieldsDefaults extends V1_047__SetFragmentParameterNewFieldsDefaultsDefinition {
+  override protected lazy val profile: JdbcProfile = HsqldbProfile
+}
---
+++ designer/server/src/test/scala/db/migration/V1_047__SetFragmentParameterNewFieldsDefaults.scala
@@ -0,0 +1,186 @@
+package db.migration
+
+import io.circe.Json
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.engine.api.CirceUtil
+
+class V1_047__SetFragmentParameterNewFieldsDefaults extends AnyFunSuite with Matchers {
+
+  private def parse(str: String): Json = CirceUtil.decodeJsonUnsafe[Json](str, "Failed to decode")
+
+  private val oldFragment: Json = parse(
+    s"""{
+       |  "metaData" : {
+       |    "id" : "migration_test",
+       |    "additionalFields" : {
+       |      "description" : null,
+       |      "properties" : {
+       |        "docsUrl" : ""
+       |      },
+       |      "metaDataType" : "FragmentSpecificData"
+       |    }
+       |  },
+       |  "nodes" : [
+       |    {
+       |      "id" : "input",
+       |      "parameters" : [
+       |        {
+       |          "name" : "paramString",
+       |          "typ" : {
+       |            "refClazzName" : "java.lang.String"
+       |          }
+       |        },
+       |        {
+       |          "name" : "paramBool",
+       |          "typ" : {
+       |            "refClazzName" : "java.lang.Boolean"
+       |          }
+       |        }
+       |      ],
+       |      "additionalFields" : {
+       |        "description" : "desc",
+       |        "layoutData" : {
+       |          "x" : 0,
+       |          "y" : 0
+       |        }
+       |      },
+       |      "type" : "FragmentInputDefinition"
+       |    },
+       |    {
+       |      "id" : "variable",
+       |      "varName" : "varName",
+       |      "value" : {
+       |        "language" : "spel",
+       |        "expression" : "#paramString"
+       |      },
+       |      "additionalFields" : {
+       |        "description" : null,
+       |        "layoutData" : {
+       |          "x" : 0,
+       |          "y" : 180
+       |        }
+       |      },
+       |      "type" : "Variable"
+       |    },
+       |    {
+       |      "id" : "dead-end",
+       |      "ref" : {
+       |        "typ" : "dead-end",
+       |        "parameters" : [
+       |        ]
+       |      },
+       |      "endResult" : null,
+       |      "isDisabled" : null,
+       |      "additionalFields" : {
+       |        "description" : null,
+       |        "layoutData" : {
+       |          "x" : 0,
+       |          "y" : 360
+       |        }
+       |      },
+       |      "type" : "Sink"
+       |    }
+       |  ],
+       |  "additionalBranches" : [
+       |  ]
+       |}""".stripMargin
+  )
+
+  private val newFragment: Json = parse(
+    """{
+      |  "metaData" : {
+      |    "id" : "migration_test",
+      |    "additionalFields" : {
+      |      "description" : null,
+      |      "properties" : {
+      |        "docsUrl" : ""
+      |      },
+      |      "metaDataType" : "FragmentSpecificData"
+      |    }
+      |  },
+      |  "nodes" : [
+      |    {
+      |      "id" : "input",
+      |      "parameters" : [
+      |        {
+      |          "name" : "paramString",
+      |          "typ" : {
+      |            "refClazzName" : "java.lang.String"
+      |          },
+      |          "required" : false,
+      |          "initialValue" : null,
+      |          "hintText" : null,
+      |          "inputConfig" : {
+      |            "inputMode" : "InputModeAny",
+      |            "fixedValuesList" : null
+      |          }
+      |        },
+      |        {
+      |          "name" : "paramBool",
+      |          "typ" : {
+      |            "refClazzName" : "java.lang.Boolean"
+      |          },
+      |          "required" : false,
+      |          "initialValue" : null,
+      |          "hintText" : null,
+      |          "inputConfig" : {
+      |            "inputMode" : "InputModeAny",
+      |            "fixedValuesList" : null
+      |          }
+      |        }
+      |      ],
+      |      "additionalFields" : {
+      |        "description" : "desc",
+      |        "layoutData" : {
+      |          "x" : 0,
+      |          "y" : 0
+      |        }
+      |      },
+      |      "type" : "FragmentInputDefinition"
+      |    },
+      |    {
+      |      "id" : "variable",
+      |      "varName" : "varName",
+      |      "value" : {
+      |        "language" : "spel",
+      |        "expression" : "#paramString"
+      |      },
+      |      "additionalFields" : {
+      |        "description" : null,
+      |        "layoutData" : {
+      |          "x" : 0,
+      |          "y" : 180
+      |        }
+      |      },
+      |      "type" : "Variable"
+      |    },
+      |    {
+      |      "id" : "dead-end",
+      |      "ref" : {
+      |        "typ" : "dead-end",
+      |        "parameters" : [
+      |        ]
+      |      },
+      |      "endResult" : null,
+      |      "isDisabled" : null,
+      |      "additionalFields" : {
+      |        "description" : null,
+      |        "layoutData" : {
+      |          "x" : 0,
+      |          "y" : 360
+      |        }
+      |      },
+      |      "type" : "Sink"
+      |    }
+      |  ],
+      |  "additionalBranches" : [
+      |  ]
+      |}""".stripMargin
+  )
+
+  test("set absent fragment parameter fields to new defaults") {
+    V1_047__SetFragmentParameterNewFieldsDefaultsDefinition.updateProcessJson(oldFragment) shouldBe Some(newFragment)
+  }
+
+}
