81616ee99e67ccb26fdc78f70d8b5699246f40f3
Julian Wielga <j@touk.pl>
2023-12-01 14:17:15 +0100

    test tweak

--- designer/client/cypress/e2e/tableEditor.cy.ts
+++ designer/client/cypress/e2e/tableEditor.cy.ts
@@ -14,6 +14,7 @@ describe("Table editor", () => {
     });
 
     it("should display rich table editor", () => {
+        cy.viewport("macbook-15");
         cy.visitNewProcess(seed, "table");
 
         cy.getNode("decisionTableCustomNode").dblclick();
