fcebf8f6c8c1099e981eef7176b5f67b3b77825e
Julian Wielga <j@touk.pl>
2023-12-01 13:18:52 +0100

    test tweak

--- designer/client/cypress/e2e/tableEditor.cy.ts
+++ designer/client/cypress/e2e/tableEditor.cy.ts
@@ -22,20 +22,21 @@ describe("Table editor", () => {
         cy.get("[data-testid='table-container']").should("be.visible").as("table");
         snapshot();
 
-        cy.get("@table").click(100, 50);
+        const delay = 100;
+        cy.get("@table").click(100, delay);
         cy.get("[value='java.lang.Double']").click();
         cy.get("@table").click(500, 15);
-        cy.realType("some name{enter}", { delay: 50 });
+        cy.realType("some name{enter}", { delay });
 
         cy.get("@table").click(100, 125);
-        cy.realType("hello world{enter}", { delay: 50 });
+        cy.realType("hello world{enter}", { delay });
         cy.get("@table").click(550, 25).click(550, 25);
         cy.get("@table").click(350, 125).click(350, 125);
-        cy.realType("foo", { delay: 50 });
+        cy.realType("foo", { delay });
         cy.focused().tab();
-        cy.wait(50).realType("bar{enter}", { delay: 50 });
-        cy.wait(50).realType("{enter}", { delay: 50 });
-        cy.wait(50).realType("xxx{enter}", { delay: 50 });
+        cy.wait(delay).realType("bar{enter}", { delay });
+        cy.wait(delay).realType("{enter}", { delay });
+        cy.wait(delay).realType("xxx{enter}", { delay });
 
         cy.get("[title='Switch to expression mode']").should("be.enabled").click();
         cy.get("@table").should("not.exist");
