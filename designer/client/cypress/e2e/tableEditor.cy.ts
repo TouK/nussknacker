c887b9b4cbe35ad355be63c7aba03c1af0116aa0
Julian Wielga <j@touk.pl>
2023-12-03 17:34:34 +0100

    test tweak

--- designer/client/cypress/e2e/tableEditor.cy.ts
+++ designer/client/cypress/e2e/tableEditor.cy.ts
@@ -26,18 +26,19 @@ describe("Table editor", () => {
         cy.get("@table").click(100, 50);
         cy.get("[value='java.lang.Double']").click();
         cy.get("@table").click(500, 15);
-        cy.realType("some name{enter}");
+        cy.realType("some name", { delay: 50 });
+        cy.realPress("Enter", { pressDelay: 50 });
 
         cy.get("@table").click(100, 125);
-        cy.realType("hello world");
-        cy.realPress("Enter");
+        cy.realType("hello world", { delay: 50 });
+        cy.realPress("Enter", { pressDelay: 50 });
         cy.get("@table").click(550, 25).click(550, 25);
         cy.get("@table").click(350, 125).click(350, 125);
-        cy.realType("foo").realPress("Tab");
-        cy.realType("bar").realPress("Enter");
-        cy.realPress("Enter");
+        cy.realType("foo", { delay: 50 }).realPress("Tab", { pressDelay: 50 });
+        cy.realType("bar", { delay: 50 }).realPress("Enter", { pressDelay: 50 });
+        cy.realPress("Enter", { pressDelay: 50 });
         cy.get("#portal textarea").should("be.visible");
-        cy.realType("xxx");
+        cy.realType("xxx", { delay: 50 });
 
         cy.get("[title='Switch to expression mode']").should("be.enabled").click();
         cy.get("@table").should("not.exist");
