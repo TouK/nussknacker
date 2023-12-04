4677a915a1c98e51458aba05a24768522f3b8957
Julian Wielga <j@touk.pl>
2023-12-04 21:43:50 +0100

    test tweak

--- designer/client/cypress/e2e/tableEditor.cy.ts
+++ designer/client/cypress/e2e/tableEditor.cy.ts
@@ -25,7 +25,7 @@ describe("Table editor", () => {
 
         cy.get("@table").click(100, 50);
         cy.get("[value='java.lang.Double']").click();
-        cy.get("@table").click(500, 15);
+        cy.get("@table").click(520, 22);
         cy.realType("some name", { delay: 50 });
         cy.realPress("Enter", { pressDelay: 50 });
 
