2caa34c60eb38979cc9666aaaeda2559ce35a34d
Julian Wielga <j@touk.pl>
2023-12-05 11:53:13 +0100

    test cleanup

--- designer/client/cypress/e2e/tableEditor.cy.ts
+++ designer/client/cypress/e2e/tableEditor.cy.ts
@@ -25,23 +25,22 @@ describe("Table editor", () => {
 
         cy.get("@table").click(100, 50);
         cy.get("[value='java.lang.Double']").click();
-        cy.get("@table").realClick({ x: 500, y: 18 });
-        cy.realType("some name", { delay: 50 });
+        cy.get("@table").click(500, 18);
+        cy.realType("some name");
         snapshot();
-        cy.realPress("Enter", { pressDelay: 50 });
+        cy.realPress("Enter");
 
         cy.get("@table").click(100, 125);
         cy.get("#portal textarea").should("be.visible");
-        cy.realType("hello world", { delay: 50 });
-        cy.realPress("Enter", { pressDelay: 50 });
+        cy.realType("hello world");
+        cy.realPress("Enter");
         cy.get("@table").click(550, 25).click(550, 25);
         cy.get("@table").click(350, 125).click(350, 125);
         cy.get("#portal textarea").should("be.visible");
-        cy.realType("foo", { delay: 50 }).realPress("Tab", { pressDelay: 50 });
-        cy.realType("bar", { delay: 50 }).realPress("Enter", { pressDelay: 50 });
-        cy.realPress("Enter", { pressDelay: 50 });
-        cy.realPress("Escape", { pressDelay: 50 });
-        cy.realType("xxx", { delay: 50 });
+        cy.realType("foo").realPress("Tab");
+        cy.realType("bar").realPress("Enter");
+        cy.realPress("Enter");
+        cy.realType("xxx");
 
         cy.get("[title='Switch to expression mode']").should("be.enabled").click();
         cy.get("@table").should("not.exist");
