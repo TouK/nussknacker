8aab2b88ee0d2fb03cc33509b75aa9285821c5c7
Szymon Bogusz <sbg@touk.pl>
2023-12-08 16:26:06 +0100

    Basic creation of scenario and deploying it and matching on created notifications

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/notifications/NotificationApiTest.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/notifications/NotificationApiTest.scala
@@ -1,9 +1,9 @@
 package pl.touk.nussknacker.ui.notifications
 
 import io.restassured.RestAssured.`given`
-import io.restassured.module.scala.RestAssuredSupport.AddThenToResponse
 import org.hamcrest.Matchers.equalTo
 import org.scalatest.freespec.AnyFreeSpecLike
+import pl.touk.nussknacker.engine.api.process.ProcessName
 import pl.touk.nussknacker.test.{
   NuRestAssureExtensions,
   NuRestAssureMatchers,
@@ -11,6 +11,9 @@ import pl.touk.nussknacker.test.{
   RestAssuredVerboseLogging
 }
 import pl.touk.nussknacker.ui.api.helpers.{NuItTest, NuScenarioConfigurationHelper, WithMockableDeploymentManager}
+import io.restassured.module.scala.RestAssuredSupport.AddThenToResponse
+import org.scalatestplus.mockito.MockitoSugar.mock
+import pl.touk.nussknacker.engine.api.deployment.DeploymentManager
 
 class NotificationApiTest
     extends AnyFreeSpecLike
@@ -25,6 +28,7 @@ class NotificationApiTest
   "The endpoint for getting notifications when" - {
     "authenticated should" - {
       "return empty list if no notifications are present" in {
+
         given()
           .auth()
           .basic("admin", "admin")
@@ -36,18 +40,56 @@ class NotificationApiTest
             equalTo("[]")
           )
       }
-//      "return a list of notifications" in {
-//        val notificationAfterExecution =
-//          given()
-//            .auth()
-//            .basic("admin", "admin")
-//            .when()
-//            .get(s"$nuDesignerHttpAddress/api/notifications")
-//            .Then()
-//            .statusCode(200)
-//            .extract()
-//            .body()
-//      }
+      "return a list of notifications" in {
+
+        given()
+          .auth()
+          .basic("admin", "admin")
+          .when()
+          .get(s"$nuDesignerHttpAddress/api/notifications")
+          .Then()
+          .statusCode(200)
+          .body(
+            equalTo("[]")
+          )
+
+        val canceledProcessName = ProcessName("process-execution-canceled")
+        createDeployedCanceledProcess(canceledProcessName)
+        given()
+          .auth()
+          .basic("admin", "admin")
+          .when()
+          .get(s"$nuDesignerHttpAddress/api/notifications")
+          .Then()
+          .statusCode(200)
+          .body(
+            matchJsonWithRegexValues(
+              s"""[{
+                   |  "id": "^\\\\w{8}-\\\\w{4}-\\\\w{4}-\\\\w{4}-\\\\w{12}$$",
+                   |  "scenarioName": "$canceledProcessName",
+                   |  "message": "Deployment finished",
+                   |  "type": null,
+                   |  "toRefresh": [ "versions", "activity", "state" ]
+                   |  },
+                   |  {
+                   |  "id": "^\\\\w{8}-\\\\w{4}-\\\\w{4}-\\\\w{4}-\\\\w{12}$$",
+                   |  "scenarioName": "$canceledProcessName",
+                   |  "message": "Cancel finished",
+                   |  "type": null,
+                   |  "toRefresh": [ "versions", "activity", "state" ]
+                   |}]""".stripMargin
+            )
+          )
+//        createDeployedProcess(canceledProcessName)
+        given()
+          .auth()
+          .basic("admin", "admin")
+          .when()
+          .get(s"$nuDesignerHttpAddress/api/notifications")
+          .Then()
+          .statusCode(200)
+
+      }
     }
     "not authenticated should" - {
       "forbid access" in {
