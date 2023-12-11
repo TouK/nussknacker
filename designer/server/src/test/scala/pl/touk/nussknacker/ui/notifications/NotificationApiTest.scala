31a5707555a36a25b82eb7819f2bd905e0fb2052
Szymon Bogusz <sbg@touk.pl>
2023-12-11 15:01:24 +0100

    Revert "Removed NotificationServiceTest"
    This reverts commit 543654a62564f798d8e8dee9018c923ef377163c.

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/notifications/NotificationApiTest.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/notifications/NotificationApiTest.scala
@@ -100,20 +100,6 @@ class NotificationApiTest
             )
           )
       }
-      "return 405 when invalid HTTP method is passed" in {
-        given()
-          .auth()
-          .basic("admin", "admin")
-          .when()
-          .put(s"$nuDesignerHttpAddress/api/notifications")
-          .Then()
-          .statusCode(405)
-          .body(
-            equalTo(
-              s"HTTP method not allowed, supported methods: GET"
-            )
-          )
-      }
     }
 
     "not authenticated should" - {
