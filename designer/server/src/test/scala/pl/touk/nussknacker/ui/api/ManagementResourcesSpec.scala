299081cf9b489d7cd3d96386a3ee0732cf6ed2a3
Arek Burdach <arek.burdach@gmail.com>
2023-12-11 18:55:02 +0100

    test fix

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/ManagementResourcesSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/ManagementResourcesSpec.scala
@@ -340,7 +340,6 @@ class ManagementResourcesSpec
         .downField("nodeResults")
         .downField("endsuffix")
         .downArray
-        .downField("context")
         .downField("variables")
 
       ctx
