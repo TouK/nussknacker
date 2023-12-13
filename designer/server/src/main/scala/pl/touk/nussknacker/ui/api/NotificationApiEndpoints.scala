920336a695ece0f4014bdf11e40a02aaf2b707eb
Szymon Bogusz <sbg@touk.pl>
2023-12-13 13:30:12 +0100

    Optional parameter "after" can now be parsed as it was possible in Akka based implementation Example DateTime value is shown in OpenApi

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/NotificationApiEndpoints.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/NotificationApiEndpoints.scala
@@ -6,16 +6,19 @@ import pl.touk.nussknacker.security.AuthCredentials
 import pl.touk.nussknacker.ui.notifications.Notification
 import sttp.model.StatusCode.Ok
 import sttp.tapir.json.circe.jsonBody
-import sttp.tapir.{EndpointInput, statusCode}
+import sttp.tapir.{EndpointInput, query, statusCode}
+
+import java.time.Instant
 
 class NotificationApiEndpoints(auth: EndpointInput[AuthCredentials]) extends BaseEndpointDefinitions {
 
-  lazy val notificationEndpoint: SecuredEndpoint[Unit, Unit, List[Notification], Any] =
+  lazy val notificationEndpoint: SecuredEndpoint[Option[Instant], Unit, List[Notification], Any] =
     baseNuApiEndpoint
       .summary("Endpoint to display notifications")
       .tag("Notifications")
       .get
       .in("notifications")
+      .in(query[Option[Instant]]("after").example(Some(Instant.now())))
       .out(
         statusCode(Ok).and(
           jsonBody[List[Notification]]
