69ad4122626b28c270c64b9613a7d9369b8d47a4
ForrestFairy <szymon.bogusz55@gmail.com>
2023-12-15 14:55:10 +0100

    [NU-1228] Add documentaiton for Notification OpenApi  (#5250)
    * Created simple example of notification returned from notification endpoint
    
    * Added information to Changelog.md that OpenApi is available for Notification Endpoint
    
    ---------
    
    Co-authored-by: Szymon Bogusz <sbg@touk.pl>

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/NotificationApiEndpoints.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/NotificationApiEndpoints.scala
@@ -1,10 +1,12 @@
 package pl.touk.nussknacker.ui.api
 
+import pl.touk.nussknacker.engine.api.process.ProcessName
 import pl.touk.nussknacker.restmodel.BaseEndpointDefinitions
 import pl.touk.nussknacker.restmodel.BaseEndpointDefinitions.SecuredEndpoint
 import pl.touk.nussknacker.security.AuthCredentials
-import pl.touk.nussknacker.ui.notifications.Notification
+import pl.touk.nussknacker.ui.notifications.{DataToRefresh, Notification}
 import sttp.model.StatusCode.Ok
+import sttp.tapir.EndpointIO.Example
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.{EndpointInput, statusCode}
 
@@ -18,7 +20,24 @@ class NotificationApiEndpoints(auth: EndpointInput[AuthCredentials]) extends Bas
       .in("notifications")
       .out(
         statusCode(Ok).and(
-          jsonBody[List[Notification]]
+          jsonBody[List[Notification]].example(
+            Example.of(
+              summary = Some("Display simple deployment notification"),
+              value = List(
+                Notification(
+                  id = "0351c45a-2c4c-4ffd-8848-ae6c2f281ef1",
+                  scenarioName = Some(ProcessName("scenario1")),
+                  message = "Deployment finished",
+                  `type` = None,
+                  toRefresh = List(
+                    DataToRefresh(DataToRefresh.versions.id),
+                    DataToRefresh(DataToRefresh.activity.id),
+                    DataToRefresh(DataToRefresh.state.id)
+                  )
+                )
+              )
+            )
+          )
         )
       )
       .withSecurity(auth)
