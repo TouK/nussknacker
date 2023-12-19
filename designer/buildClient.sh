22c709687a39c0700f5bab006b7579be8de035a0
Mateusz Piękos <mpk@touk.pl>
2023-12-19 11:27:39 +0100

    testing pipeline

--- designer/buildClient.sh
+++ designer/buildClient.sh
@@ -3,6 +3,8 @@
 set -e
 
 cd "$(dirname -- "$0")"
+echo "----NPM VERSION"
+npm --version
 cd client && npm ci && npm run build && cd -
 cp -r client/.federated-types/nussknackerUi submodules/types/@remote
 cd submodules && npm ci && CI=true npm run build && cd -
