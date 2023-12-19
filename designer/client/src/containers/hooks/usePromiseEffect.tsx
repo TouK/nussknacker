7d8a8e41678a6d235589e486e1a8693b3117ae74
Julian Wielga <j@touk.pl>
2023-12-19 12:51:03 +0100

    NU-1042 fixed history behaviour for node windows

---
+++ designer/client/src/containers/hooks/usePromiseEffect.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+
+export function usePromiseEffect(callback: () => Promise<void>) {
+    const running = useRef(false);
+    useEffect(() => {
+        // avoid running twice for same callback (react 18)
+        if (!running.current) {
+            running.current = true;
+            Promise.resolve()
+                .then(callback)
+                .finally(() => {
+                    running.current = false;
+                });
+        }
+    }, [callback]);
+}
