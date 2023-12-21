2a611308dc60f3fa2dcd53ff53b39e44d379f9cd
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-12-21 11:45:36 +0100

    wip

--- components/decisiontable/src/main/scala/pl/touk/nussknacker/decisiontable/DecisionTable.scala
+++ components/decisiontable/src/main/scala/pl/touk/nussknacker/decisiontable/DecisionTable.scala
@@ -1,5 +1,6 @@
 package pl.touk.nussknacker.decisiontable
 
+import pl.touk.nussknacker.decisiontable.TypedTabularData.{Column, ColumnWithValues}
 import pl.touk.nussknacker.engine.api._
 import pl.touk.nussknacker.engine.api.context.{ContextTransformation, OutputVar}
 import pl.touk.nussknacker.engine.api.editor.{SimpleEditor, SimpleEditorType}
@@ -9,9 +10,8 @@ object DecisionTable extends CustomStreamTransformer {
 
   @MethodToInvoke
   def invoke(
-      @ParamName("Basic Decision Table") @SimpleEditor(`type` =
-        SimpleEditorType.TYPED_TABULAR_DATA_EDITOR
-      ) table: Table[Any],
+      @ParamName("Basic Decision Table")
+      @SimpleEditor(`type` = SimpleEditorType.TYPED_TABULAR_DATA_EDITOR) table: Table[TypedTabularData],
       @ParamName("Expression") expression: LazyParameter[java.lang.Boolean],
       @OutputVariableName outputVariable: String
   )(implicit nodeId: NodeId): ContextTransformation = {
@@ -22,6 +22,15 @@ object DecisionTable extends CustomStreamTransformer {
       }
   }
 
+}
+
+// todo: check + smart constructor
+final case class TypedTabularData(columns: List[ColumnWithValues])
+
+object TypedTabularData {
+  final case class ColumnWithValues(name: String, aType: Class[_], values: List[Any])
+
+  lazy val empty: TypedTabularData = TypedTabularData(List.empty)
 }
 
 final case class Table[T](rows: T)
