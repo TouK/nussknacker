f79cd81f60b14743c0d24905d3fc37853f9ea78e
JulianWielga <j@touk.pl>
2024-01-01 17:33:26 -0500

    [NU-1042] node windows history fixed (#5256)
    * move most of query params code to one place
    
    * cypress tests for problems
    
    * NU-1042 fixed history behaviour for node windows
    
    * fixed props type narrowing
    
    * Updated snapshots (#5260)
    
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>
    
    * fixed fragment preview
    
    * review cleanup
    
    * merge query params when needed
    
    * test fix attempt
    
    * Revert "test fix attempt"
    
    This reverts commit 26ad7ad16063f79f6b8eda43e5dab52d27b72327.
    
    * test fix attempt
    
    * Revert "test fix attempt"
    
    This reverts commit dd477e3bd85ff9e31986e4805bff28d8b7f82a0c.
    
    * test fix attempt
    
    * fixed doubled node window problem
    
    * test fix attempt
    
    * test fix attempt
    
    * test fix attempt
    
    * test fix attempt
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>

--- designer/client/src/containers/hooks/useSearchQuery.tsx
+++ designer/client/src/containers/hooks/useSearchQuery.tsx
@@ -1,26 +1,74 @@
-import { defaults } from "lodash";
+import { isEqual, omitBy, uniq, without } from "lodash";
 import * as queryString from "query-string";
-import { ParseOptions } from "query-string";
-import { useCallback, useMemo } from "react";
-import { defaultArrayFormat, setAndPreserveLocationParams } from "../../common/VisualizationUrl";
-import { UnknownRecord } from "../../types/common";
-import { useLocation, useNavigate } from "react-router-dom";
+import { ParsedQuery, ParseOptions } from "query-string";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { ensureArray } from "../../common/arrayUtils";
 
-export function useSearchQuery<T extends UnknownRecord>(options?: ParseOptions): [T, (v: T) => void] {
-    const navigate = useNavigate();
+type QueryRecord = ParsedQuery<string | boolean | number>;
+
+const DEFAULT_ARRAY_FORMAT: ParseOptions["arrayFormat"] = "comma";
+
+function parseQuery(searchString = window.location.search): QueryRecord {
+    return queryString.parse(searchString, {
+        arrayFormat: DEFAULT_ARRAY_FORMAT,
+        parseNumbers: true,
+        parseBooleans: true,
+        decode: true,
+    });
+}
+
+function stringifyQuery(params: QueryRecord): string {
+    const resultParams = omitBy(params, (value) => value === undefined || isEqual(value, []));
+    return queryString.stringify(resultParams, {
+        arrayFormat: DEFAULT_ARRAY_FORMAT,
+        encode: true,
+    });
+}
+
+export function useSearchQuery<T extends QueryRecord>(): [T, (v: T) => void] {
     const location = useLocation();
 
-    const query = useMemo(() => {
-        const parsedQuery = queryString.parse(location.search, defaults(options, { arrayFormat: defaultArrayFormat, parseBooleans: true }));
-        return parsedQuery as T;
-    }, [location.search, options]);
+    const [queryState, setQueryState] = useState(location.search);
 
-    const updateQuery = useCallback(
-        (value: T) => {
-            navigate({ search: setAndPreserveLocationParams(value) }, { replace: true });
-        },
-        [navigate],
+    const parsed = useMemo(() => parseQuery(queryState) as T, [queryState]);
+
+    const updateQuery = useCallback((value: T) => {
+        setQueryState(() => setAndPreserveLocationParams(value));
+    }, []);
+
+    useEffect(() => {
+        replaceSearchQuery(parsed);
+    }, [parsed]);
+
+    return [parsed, updateQuery];
+}
+
+export function replaceSearchQuery(value: QueryRecord): void;
+export function replaceSearchQuery(update: (current: QueryRecord) => QueryRecord): void;
+export function replaceSearchQuery(callback: ((current: QueryRecord) => QueryRecord) | QueryRecord): void {
+    const { history, location } = window;
+    const currentState = history.state || {};
+    const currentURL = new URL(location.href);
+    const params = typeof callback === "function" ? callback(parseQuery()) : callback;
+    currentURL.search = stringifyQuery(params);
+    history.replaceState(currentState, "", currentURL.href);
+}
+
+export function parseWindowsQueryParams<P extends Record<string, string | string[]>>(append: P, remove?: P): Record<string, string[]> {
+    const query = parseQuery();
+    const keys = uniq(Object.keys({ ...append, ...remove }));
+    return Object.fromEntries(
+        keys.map((key) => {
+            const current = ensureArray(query[key]).map(decodeURIComponent);
+            const withAdded = uniq(current.concat(append?.[key]));
+            const cleaned = without(withAdded, ...ensureArray(remove?.[key])).filter(Boolean);
+            return [key, cleaned];
+        }),
     );
+}
 
-    return [query, updateQuery];
+export function setAndPreserveLocationParams<T extends Record<string, unknown>>(params: T): string {
+    const currentParams = parseQuery();
+    return stringifyQuery({ ...currentParams, ...params });
 }
--- designer/client/src/windowManager/useWindows.ts
+++ designer/client/src/windowManager/useWindows.ts
@@ -1,27 +1,11 @@
 import { useWindowManager, WindowId, WindowType } from "@touk/window-manager";
-import { defaults, uniq, without } from "lodash";
-import * as queryString from "query-string";
+import { defaults } from "lodash";
 import { useCallback, useMemo } from "react";
-import { ensureArray } from "../common/arrayUtils";
 import { useUserSettings } from "../common/userSettings";
-import { defaultArrayFormat } from "../common/VisualizationUrl";
 import { ConfirmDialogData } from "../components/modals/GenericConfirmDialog";
 import { NodeType, Process } from "../types";
 import { WindowKind } from "./WindowKind";
 import { InfoDialogData } from "../components/modals/GenericInfoDialog";
-
-export function parseWindowsQueryParams<P extends Record<string, string | string[]>>(append: P, remove?: P): Record<string, string[]> {
-    const query = queryString.parse(window.location.search, { arrayFormat: defaultArrayFormat });
-    const keys = uniq(Object.keys({ ...append, ...remove }));
-    return Object.fromEntries(
-        keys.map((key) => {
-            const current = ensureArray(query[key]).map(decodeURIComponent);
-            const withAdded = uniq(current.concat(append?.[key]));
-            const cleaned = without(withAdded, ...ensureArray(remove?.[key])).filter(Boolean);
-            return [key, cleaned];
-        }),
-    );
-}
 
 export function useWindows(parent?: WindowId) {
     const { open: _open, closeAll } = useWindowManager(parent);
@@ -31,7 +15,7 @@ export function useWindows(parent?: WindowId) {
     const open = useCallback(
         async <M = never>(windowData: Partial<WindowType<WindowKind, M>> = {}) => {
             const isModal = windowData.isModal === undefined ? !forceDisableModals : windowData.isModal && !forceDisableModals;
-            await _open({ isResizable: false, ...windowData, isModal });
+            return await _open({ isResizable: false, ...windowData, isModal });
         },
         [forceDisableModals, _open],
     );
@@ -39,6 +23,7 @@ export function useWindows(parent?: WindowId) {
     const openNodeWindow = useCallback(
         (node: NodeType, process: Process, readonly?: boolean) =>
             open({
+                id: node.id,
                 title: node.id,
                 isResizable: true,
                 kind: readonly ? WindowKind.viewNode : WindowKind.editNode,
