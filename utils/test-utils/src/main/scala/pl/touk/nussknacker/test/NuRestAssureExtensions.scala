4ac0b59e6400a141670ad336b518ebee3aa6a59c
Łukasz Bigorajski <lbg@touk.pl>
2024-01-09 11:23:24 +0100

    [NU-1230] Refactor process activity API tests

--- utils/test-utils/src/main/scala/pl/touk/nussknacker/test/NuRestAssureExtensions.scala
+++ utils/test-utils/src/main/scala/pl/touk/nussknacker/test/NuRestAssureExtensions.scala
@@ -1,8 +1,13 @@
 package pl.touk.nussknacker.test
 
+import io.restassured.builder.MultiPartSpecBuilder
+import io.restassured.http.ContentType
 import io.restassured.response.ValidatableResponse
 import io.restassured.specification.RequestSpecification
+import org.hamcrest.core.IsEqual
 import pl.touk.nussknacker.test.NuRestAssureMatchers.equalsJson
+
+import java.nio.charset.StandardCharsets
 
 trait NuRestAssureExtensions {
 
@@ -50,6 +55,35 @@ trait NuRestAssureExtensions {
 
   }
 
+  implicit class PlainBody[T <: RequestSpecification](requestSpecification: T) {
+
+    def plainBody(body: String): RequestSpecification = {
+      requestSpecification
+        .contentType(ContentType.TEXT.withCharset(StandardCharsets.UTF_8))
+        .body(body)
+    }
+
+  }
+
+  implicit class MultiPartBody[T <: RequestSpecification](requestSpecification: T) {
+    private val doubleQuote = '"'
+
+    def multiPartBody(fileContent: String, fileName: String): RequestSpecification = {
+      requestSpecification
+        .multiPart(
+          new MultiPartSpecBuilder(fileContent)
+            // https://github.com/rest-assured/rest-assured/issues/866#issuecomment-617127889
+            .header(
+              "Content-Disposition",
+              s"form-data; name=${doubleQuote}attachment${doubleQuote}; filename=${doubleQuote}${fileName}${doubleQuote}"
+            )
+            .build()
+        )
+        .contentType("multipart/form-data")
+    }
+
+  }
+
   implicit class EqualsJsonBody[T <: ValidatableResponse](validatableResponse: T) {
 
     def equalsJsonBody(json: String): ValidatableResponse = {
@@ -57,6 +91,28 @@ trait NuRestAssureExtensions {
         .body(
           equalsJson(json)
         )
+    }
+
+  }
+
+  implicit class EqualsPlainBody[T <: ValidatableResponse](validatableResponse: T) {
+
+    def equalsPlainBody(body: String): ValidatableResponse = {
+      validatableResponse
+        .body(
+          new IsEqual(body)
+        )
+    }
+
+  }
+
+  implicit class ExtractLong[T <: ValidatableResponse](validatableResponse: T) {
+
+    def extractLong(jsonPath: String): Long = {
+      validatableResponse
+        .extract()
+        .jsonPath()
+        .getLong(jsonPath)
     }
 
   }
