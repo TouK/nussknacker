6511900527038797af0b4926884f700dde529ba6
Szymon Bogusz <sbg@touk.pl>
2024-01-10 08:28:49 +0100

    Rebase

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/services/NodesApiHttpService.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/services/NodesApiHttpService.scala
@@ -2,10 +2,8 @@ package pl.touk.nussknacker.ui.services
 
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.{Decoder, Json}
 import pl.touk.nussknacker.engine.ModelData
-import pl.touk.nussknacker.engine.api.displayedgraph.{DisplayableProcess, ProcessProperties}
-import pl.touk.nussknacker.engine.api.process.{ProcessIdWithName, VersionId}
+import pl.touk.nussknacker.engine.api.process.ProcessIdWithName
 import pl.touk.nussknacker.ui.additionalInfo.AdditionalInfoProviders
 import pl.touk.nussknacker.ui.api.NodesApiEndpoints.Dtos.TypingResultDto.{toTypingResult, typingResultToDto}
 import pl.touk.nussknacker.ui.api.NodesApiEndpoints.Dtos.{
@@ -14,7 +12,7 @@ import pl.touk.nussknacker.ui.api.NodesApiEndpoints.Dtos.{
   ParameterDto,
   ParametersValidationResultDto
 }
-import pl.touk.nussknacker.ui.api.{NodeValidationRequest, NodesApiEndpoints, NodesResources, ParametersValidationResult}
+import pl.touk.nussknacker.ui.api.NodesApiEndpoints
 import pl.touk.nussknacker.ui.process.ProcessService.GetScenarioWithDetailsOptions
 import pl.touk.nussknacker.ui.process.{ProcessCategoryService, ProcessService}
 import pl.touk.nussknacker.ui.process.processingtypedata.ProcessingTypeDataProvider
@@ -27,7 +25,6 @@ import scala.concurrent.{ExecutionContext, Future}
 class NodesApiHttpService(
     config: Config,
     authenticator: AuthenticationResources,
-    getProcessCategoryService: () => ProcessCategoryService,
     typeToConfig: ProcessingTypeDataProvider[ModelData, _],
     typeToProcessValidator: ProcessingTypeDataProvider[UIProcessValidator, _],
     typeToNodeValidator: ProcessingTypeDataProvider[NodeValidator, _],
@@ -35,7 +32,7 @@ class NodesApiHttpService(
     typeToParametersValidator: ProcessingTypeDataProvider[ParametersValidator, _],
     protected val processService: ProcessService
 )(implicit executionContext: ExecutionContext)
-    extends BaseHttpService(config, getProcessCategoryService, authenticator)
+    extends BaseHttpService(config, authenticator)
     with LazyLogging {
 
   private val nodesApiEndpoints = new NodesApiEndpoints(authenticator.authenticationMethod())
@@ -76,12 +73,15 @@ class NodesApiHttpService(
         processService
           .getProcessId(processName)
           .flatMap { processId =>
+            println(processId)
+            println(ProcessIdWithName(processId, processName))
             processService
               .getLatestProcessWithDetails(
                 ProcessIdWithName(processId, processName),
                 GetScenarioWithDetailsOptions.detailsOnly
               )(user)
               .flatMap { process =>
+                println(process)
                 val nodeValidator = typeToNodeValidator.forTypeUnsafe(process.processingType)(user)
                 nodeValidationRequestDto.toRequest match {
                   case Some(nodeData) =>
