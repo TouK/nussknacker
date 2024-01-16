a4430ba626070289c2f7e568cb4cff2e8a6fcd7d
Pawel Czajka <pcz@touk.pl>
2024-01-16 13:18:43 +0100

    copied test

--- components/sql/src/test/scala/pl/touk/nussknacker/sql/service/DatabaseLookupEnricherTest.scala
+++ components/sql/src/test/scala/pl/touk/nussknacker/sql/service/DatabaseLookupEnricherTest.scala
@@ -1,25 +1,38 @@
 package pl.touk.nussknacker.sql.service
 
 import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigValueFactory.fromAnyRef
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import pl.touk.nussknacker.engine.api.context.ValidationContext
 import pl.touk.nussknacker.engine.api.context.transformation.OutputVariableNameValue
 import pl.touk.nussknacker.engine.api.definition.{FixedExpressionValue, FixedValuesParameterEditor}
 import pl.touk.nussknacker.engine.api.typed.TypedMap
 import pl.touk.nussknacker.engine.api.NodeId
 import pl.touk.nussknacker.engine.api.component.ComponentDefinition
+import pl.touk.nussknacker.engine.api.process.SourceFactory
 import pl.touk.nussknacker.engine.build.ScenarioBuilder
+import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
 import pl.touk.nussknacker.engine.flink.test.FlinkSpec
+import pl.touk.nussknacker.engine.flink.util.source.EmitWatermarkAfterEachElementCollectionSource
+import pl.touk.nussknacker.engine.flink.util.transformer.FlinkBaseComponentProvider
 import pl.touk.nussknacker.engine.graph.expression.Expression
+import pl.touk.nussknacker.engine.process.helpers.ConfigCreatorWithCollectingListener
 import pl.touk.nussknacker.engine.process.runner.UnitTestsFlinkRunner
 import pl.touk.nussknacker.engine.testing.LocalModelData
+import pl.touk.nussknacker.engine.testmode.{ResultsCollectingListener, ResultsCollectingListenerHolder, TestProcess}
 import pl.touk.nussknacker.engine.util.test.TestScenarioRunner
 import pl.touk.nussknacker.sql.db.pool.DBPoolConfig
 import pl.touk.nussknacker.sql.db.query.ResultSetStrategy
 import pl.touk.nussknacker.sql.db.schema.{JdbcMetaDataProvider, MetaDataProviderFactory, TableDefinition}
 import pl.touk.nussknacker.sql.utils.BaseHsqlQueryEnricherTest
+import pl.touk.nussknacker.engine.spel.Implicits._
 
 import java.io.{ByteArrayOutputStream, FileOutputStream, ObjectOutputStream}
 import scala.concurrent.Await
+
+case class TestRecord(id: String = "1", timeHours: Int = 0, eId: Int = 1, str: String = "a") {
+  def timestamp: Long = timeHours * 3600L * 1000
+}
 
 // TODO_PAWEL kolejnosc i czy sie odpalaja wszystkie before all
 class DatabaseLookupEnricherTest extends BaseHsqlQueryEnricherTest with FlinkSpec {
@@ -41,6 +54,21 @@ class DatabaseLookupEnricherTest extends BaseHsqlQueryEnricherTest with FlinkSpe
 
   override val service = new DatabaseLookupEnricher(hsqlDbPoolConfig, provider(hsqlDbPoolConfig))
 
+  private val sinkId                    = "end"
+  private val resultVariableName        = "resultVar"
+  private val forEachOutputVariableName = "forEachVar"
+  private val forEachNodeResultId       = "for-each-result"
+
+  private def aProcessWithForEachNode(elements: String, resultExpression: String = s"#$forEachOutputVariableName") =
+    ScenarioBuilder
+      .streaming("forEachProcess")
+      .parallelism(1)
+      .stateOnDisk(true)
+      .source("start", "start")
+      .customNode("for-each", forEachOutputVariableName, "for-each", "Elements" -> elements)
+      .buildSimpleVariable(forEachNodeResultId, "resultVar", resultExpression)
+      .emptySink(sinkId, "dead-end")
+
   test("Database lookup enricher should be serializable") {
     val fos = new ByteArrayOutputStream()
     val oos = new ObjectOutputStream(fos);
@@ -49,13 +77,63 @@ class DatabaseLookupEnricherTest extends BaseHsqlQueryEnricherTest with FlinkSpe
     oos.close()
   }
 
-  test("Test on flink") {
-    import pl.touk.nussknacker.engine.flink.util.test.FlinkTestScenarioRunner._
-    val testScenarioRunner = TestScenarioRunner
-      .flinkBased(config, flinkMiniCluster)
-      .withExtraComponents(List(ComponentDefinition("dbLookup", service)))
-      .build()
+  private def initializeListener = ResultsCollectingListenerHolder.registerRun
 
+  private def modelData(
+      list: List[TestRecord] = List(),
+      collectingListener: ResultsCollectingListener
+  ): LocalModelData = {
+    val modelConfig = ConfigFactory
+      .empty()
+      .withValue("useTypingResultTypeInformation", fromAnyRef(true))
+    val sourceComponent = ComponentDefinition(
+      "start",
+      SourceFactory.noParam[TestRecord](
+        EmitWatermarkAfterEachElementCollectionSource
+          .create[TestRecord](list, _.timestamp, java.time.Duration.ofHours(1))(TypeInformation.of(classOf[TestRecord]))
+      )
+    )
+    LocalModelData(
+      modelConfig,
+      sourceComponent :: FlinkBaseComponentProvider.Components,
+      configCreator = new ConfigCreatorWithCollectingListener(collectingListener),
+    )
+  }
+
+  private def collectTestResults[T](
+      model: LocalModelData,
+      testProcess: CanonicalProcess,
+      collectingListener: ResultsCollectingListener
+  ): TestProcess.TestResults = {
+    runProcess(model, testProcess)
+    collectingListener.results
+  }
+
+  private def runProcess(model: LocalModelData, testProcess: CanonicalProcess): Unit = {
+    val stoppableEnv = flinkMiniCluster.createExecutionEnvironment()
+    UnitTestsFlinkRunner.registerInEnvironmentWithModel(stoppableEnv, model)(testProcess)
+    stoppableEnv.executeAndWaitForFinished(testProcess.name.value)()
+  }
+
+  test("should produce results for each element in list") {
+    val collectingListener = initializeListener
+    val model              = modelData(List(TestRecord()), collectingListener)
+
+    val testProcess =
+      aProcessWithForEachNode(elements = "{'one', 'other'}", resultExpression = s"#$forEachOutputVariableName + '_1'")
+
+    val results = collectTestResults[String](model, testProcess, collectingListener)
+    extractResultValues(results) shouldBe List("one_1", "other_1")
+  }
+
+  private def extractResultValues(results: TestProcess.TestResults): List[String] = results
+    .nodeResults(sinkId)
+    .map(_.get[String](resultVariableName).get)
+
+  test("Test on flink") {
+
+    val env       = flinkMiniCluster.createExecutionEnvironment()
+    val modelData = LocalModelData(ConfigFactory.empty(), List(ComponentDefinition("dbLookup", service)))
     val scenario =
       ScenarioBuilder
         .streaming("test")
@@ -71,6 +149,12 @@ class DatabaseLookupEnricherTest extends BaseHsqlQueryEnricherTest with FlinkSpe
           "Key value"  -> Expression.spel("#input")
         )
         .processorEnd("end", "invocationCollector", "value" -> Expression.spel("#person"))
+
+    import pl.touk.nussknacker.engine.flink.util.test.FlinkTestScenarioRunner._
+    val testScenarioRunner = TestScenarioRunner
+      .flinkBased(config, flinkMiniCluster)
+      .withExtraComponents(List(ComponentDefinition("dbLookup", service)))
+      .build()
 
     val sth = testScenarioRunner.runWithData(scenario, List(1))
     val a   = 5
