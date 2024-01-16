d7219cb3d4fbbbc34029b8e57ce2c73950498086
Łukasz Bigorajski <lbg@touk.pl>
2024-01-16 14:58:45 +0100

    review fixes -2

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/ScenarioActivitySpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/ScenarioActivitySpec.scala
@@ -1,13 +1,10 @@
 package pl.touk.nussknacker.ui.api
 
 import io.restassured.RestAssured.`given`
-import io.restassured.http.ContentType
 import io.restassured.module.scala.RestAssuredSupport.AddThenToResponse
-import io.restassured.response.{Response, ValidatableResponse}
+import io.restassured.response.ValidatableResponse
 import org.scalatest.freespec.AnyFreeSpecLike
-import pl.touk.nussknacker.engine.api.process.ProcessName
 import pl.touk.nussknacker.engine.build.ScenarioBuilder
-import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
 import pl.touk.nussknacker.test.{NuRestAssureExtensions, NuRestAssureMatchers, RestAssuredVerboseLogging}
 import pl.touk.nussknacker.ui.api.helpers.TestCategories.Category1
 import pl.touk.nussknacker.ui.api.helpers.{
@@ -17,7 +14,6 @@ import pl.touk.nussknacker.ui.api.helpers.{
   WithMockableDeploymentManager
 }
 
-import java.nio.charset.StandardCharsets
 import java.util.UUID
 
 class ScenarioActivitySpec
@@ -29,15 +25,16 @@ class ScenarioActivitySpec
     with NuRestAssureMatchers
     with RestAssuredVerboseLogging {
   import ScenarioActivitySpecAsserts._
+  import TestProcessingTypes._
 
-  val scenarioName      = ProcessName(UUID.randomUUID().toString)
-  val commentContent    = "test message"
-  val wrongScenarioName = ProcessName("wrongProcessName")
-  val fileContent       = "very important content"
-  val fileName          = "important_file.txt"
+  private val exampleScenarioName = UUID.randomUUID().toString
+  private val commentContent      = "test message"
+  private val wrongScenarioName   = "wrongProcessName"
+  private val fileContent         = "very important content"
+  private val fileName            = "important_file.txt"
 
-  val scenario: CanonicalProcess = ScenarioBuilder
-    .streaming(scenarioName.value)
+  private val exampleScenario = ScenarioBuilder
+    .streaming(exampleScenarioName)
     .source("sourceId", "barSource")
     .emptySink("sinkId", "barSink")
 
@@ -46,11 +43,11 @@ class ScenarioActivitySpec
       "return empty comments and attachment for existing process without them" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity")
           .Then()
           .statusCode(200)
           .equalsJsonBody(
@@ -67,7 +64,7 @@ class ScenarioActivitySpec
         given()
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${wrongScenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$wrongScenarioName/activity")
           .Then()
           .statusCode(404)
           .equalsPlainBody(s"No scenario $wrongScenarioName found")
@@ -77,10 +74,9 @@ class ScenarioActivitySpec
     "not authenticated should" - {
       "forbid access" in {
         given()
-          .contentType(ContentType.JSON.withCharset(StandardCharsets.UTF_8))
           .noAuth()
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity")
           .Then()
           .statusCode(401)
           .equalsPlainBody("The resource requires authentication, which was not supplied with the request")
@@ -93,26 +89,26 @@ class ScenarioActivitySpec
       "add comment in existing scenario" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .plainBody(commentContent)
           .basicAuth("writer", "writer")
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/comments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/1/activity/comments")
           .Then()
           .statusCode(200)
-          .verifyCommentExists(scenarioName, commentContent)
+          .verifyCommentExists(scenarioName = exampleScenarioName, commentContent = commentContent)
       }
 
       "return 404 for no existing scenario" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .plainBody(commentContent)
           .basicAuth("writer", "writer")
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${wrongScenarioName.value}/1/activity/comments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$wrongScenarioName/1/activity/comments")
           .Then()
           .statusCode(404)
           .equalsPlainBody(s"No scenario $wrongScenarioName found")
@@ -125,7 +121,7 @@ class ScenarioActivitySpec
           .plainBody(commentContent)
           .noAuth()
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/comments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/1/activity/comments")
           .Then()
           .statusCode(401)
           .equalsPlainBody("The resource requires authentication, which was not supplied with the request")
@@ -134,12 +130,12 @@ class ScenarioActivitySpec
       "forbid access for insufficient privileges" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .plainBody(commentContent)
           .basicAuth("reader", "reader")
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/comments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/1/activity/comments")
           .Then()
           .statusCode(403)
           .equalsPlainBody("The supplied authentication is not authorized to access this resource")
@@ -152,32 +148,32 @@ class ScenarioActivitySpec
       "remove comment in existing scenario" in {
         val commentId = given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
-            createComment(scenarioName, commentContent)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
+            createComment(scenarioName = exampleScenarioName, commentContent = commentContent)
           }
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity")
           .Then()
           .extractLong("comments[0].id")
 
         given()
           .basicAuth("writer", "writer")
           .when()
-          .delete(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity/comments/$commentId")
+          .delete(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity/comments/$commentId")
           .Then()
           .statusCode(200)
-          .verifyEmptyCommentsAndAttachments(scenarioName)
+          .verifyEmptyCommentsAndAttachments(exampleScenarioName)
       }
 
       "return 500 for no existing comment" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .basicAuth("writer", "writer")
           .when()
-          .delete(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity/comments/1")
+          .delete(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity/comments/1")
           .Then()
           .statusCode(500)
           .equalsPlainBody("Unable to delete comment with id: 1")
@@ -187,7 +183,7 @@ class ScenarioActivitySpec
         given()
           .basicAuth("writer", "writer")
           .when()
-          .delete(s"$nuDesignerHttpAddress/api/processes/${wrongScenarioName.value}/activity/comments/1")
+          .delete(s"$nuDesignerHttpAddress/api/processes/$wrongScenarioName/activity/comments/1")
           .Then()
           .statusCode(404)
           .equalsPlainBody(s"No scenario $wrongScenarioName found")
@@ -199,7 +195,7 @@ class ScenarioActivitySpec
         given()
           .noAuth()
           .when()
-          .delete(s"$nuDesignerHttpAddress/api/processes/${scenario.name}/activity/comments/1")
+          .delete(s"$nuDesignerHttpAddress/api/processes/${exampleScenario.name}/activity/comments/1")
           .Then()
           .statusCode(401)
           .equalsPlainBody("The resource requires authentication, which was not supplied with the request")
@@ -208,11 +204,11 @@ class ScenarioActivitySpec
       "forbid access for insufficient privileges" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .basicAuth("reader", "reader")
           .when()
-          .delete(s"$nuDesignerHttpAddress/api/processes/${scenario.name}/activity/comments/1")
+          .delete(s"$nuDesignerHttpAddress/api/processes/${exampleScenario.name}/activity/comments/1")
           .Then()
           .statusCode(403)
           .equalsPlainBody("The supplied authentication is not authorized to access this resource")
@@ -225,15 +221,15 @@ class ScenarioActivitySpec
       "add attachment to existing scenario" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .multiPartBody(fileContent = fileContent, fileName = fileName)
           .basicAuth("writer", "writer")
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/attachments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/1/activity/attachments")
           .Then()
           .statusCode(200)
-          .verifyAttachmentsExists(scenarioName)
+          .verifyAttachmentsExists(exampleScenarioName)
       }
 
       "handle attachments with the same name" in {
@@ -241,13 +237,13 @@ class ScenarioActivitySpec
         val fileContent2 = "very important content2"
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
-            createAttachment(scenarioName, fileContent = fileContent1, fileName = fileName)
-            createAttachment(scenarioName, fileContent = fileContent2, fileName = fileName)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
+            createAttachment(scenarioName = exampleScenarioName, fileContent = fileContent1, fileName = fileName)
+            createAttachment(scenarioName = exampleScenarioName, fileContent = fileContent2, fileName = fileName)
           }
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity")
           .Then()
           .body(
             matchJsonWithRegexValues(
@@ -281,7 +277,7 @@ class ScenarioActivitySpec
           .multiPartBody(fileContent = fileContent, fileName = fileName)
           .basicAuth("writer", "writer")
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${wrongScenarioName.value}/1/activity/attachments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$wrongScenarioName/1/activity/attachments")
           .Then()
           .statusCode(404)
           .equalsPlainBody(s"No scenario $wrongScenarioName found")
@@ -294,7 +290,7 @@ class ScenarioActivitySpec
           .multiPartBody(fileContent = "test", fileName = "test.xml")
           .noAuth()
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/attachments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/1/activity/attachments")
           .Then()
           .statusCode(401)
           .equalsPlainBody("The resource requires authentication, which was not supplied with the request")
@@ -303,12 +299,12 @@ class ScenarioActivitySpec
       "forbid access for insufficient privileges" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .multiPartBody(fileContent = "test", fileName = "test.xml")
           .basicAuth("reader", "reader")
           .when()
-          .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/attachments")
+          .post(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/1/activity/attachments")
           .Then()
           .statusCode(403)
           .equalsPlainBody("The supplied authentication is not authorized to access this resource")
@@ -321,19 +317,19 @@ class ScenarioActivitySpec
       "download existing attachment" in {
         val attachmentId = given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
-            createAttachment(scenarioName, fileContent)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
+            createAttachment(scenarioName = exampleScenarioName, fileContent = fileContent)
           }
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity")
           .Then()
           .extractLong("attachments[0].id")
 
         given()
           .basicAuth("writer", "writer")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity/attachments/$attachmentId")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity/attachments/$attachmentId")
           .Then()
           .statusCode(200)
           .equalsPlainBody(fileContent)
@@ -342,11 +338,11 @@ class ScenarioActivitySpec
       "return empty body for no existing attachment" in {
         given()
           .applicationState {
-            createSavedScenario(scenario, category = Category1, TestProcessingTypes.Streaming)
+            createSavedScenario(exampleScenario, category = Category1, Streaming)
           }
           .basicAuth("writer", "writer")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity/attachments/1")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity/attachments/1")
           .Then()
           .statusCode(200)
           .equalsPlainBody("")
@@ -356,7 +352,7 @@ class ScenarioActivitySpec
         given()
           .basicAuth("writer", "writer")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${wrongScenarioName.value}/activity/attachments/1")
+          .get(s"$nuDesignerHttpAddress/api/processes/$wrongScenarioName/activity/attachments/1")
           .Then()
           .statusCode(404)
           .equalsPlainBody(s"No scenario $wrongScenarioName found")
@@ -368,7 +364,7 @@ class ScenarioActivitySpec
         given()
           .noAuth()
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity/attachments/1")
+          .get(s"$nuDesignerHttpAddress/api/processes/$exampleScenarioName/activity/attachments/1")
           .Then()
           .statusCode(401)
           .equalsPlainBody("The resource requires authentication, which was not supplied with the request")
@@ -376,37 +372,37 @@ class ScenarioActivitySpec
     }
   }
 
-  private def createComment(scenarioName: ProcessName, commentContent: String): ValidatableResponse = {
+  private def createComment(scenarioName: String, commentContent: String): Unit = {
     given()
       .plainBody(commentContent)
       .basicAuth("writer", "writer")
       .when()
-      .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/comments")
-      .Then()
+      .post(s"$nuDesignerHttpAddress/api/processes/$scenarioName/1/activity/comments")
   }
 
   private def createAttachment(
-      scenarioName: ProcessName,
+      scenarioName: String,
       fileContent: String,
       fileName: String = "important_file.txt"
-  ): Response = {
+  ): Unit = {
     given()
       .multiPartBody(fileContent = fileContent, fileName = fileName)
       .basicAuth("writer", "writer")
       .when()
-      .post(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/1/activity/attachments")
+      .post(s"$nuDesignerHttpAddress/api/processes/$scenarioName/1/activity/attachments")
   }
 
   object ScenarioActivitySpecAsserts {
 
     implicit class VerifyCommentExists[T <: ValidatableResponse](validatableResponse: T) {
 
-      def verifyCommentExists(scenarioName: ProcessName, commentContent: String): ValidatableResponse = {
+      def verifyCommentExists(scenarioName: String, commentContent: String): ValidatableResponse = {
         given()
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$scenarioName/activity")
           .Then()
+          .statusCode(200)
           .body(
             matchJsonWithRegexValues(
               s"""
@@ -431,11 +427,11 @@ class ScenarioActivitySpec
 
     implicit class VerifyEmptyCommentsAndAttachments[T <: ValidatableResponse](validatableResponse: T) {
 
-      def verifyEmptyCommentsAndAttachments(scenarioName: ProcessName): ValidatableResponse = {
+      def verifyEmptyCommentsAndAttachments(scenarioName: String): ValidatableResponse = {
         given()
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$scenarioName/activity")
           .Then()
           .equalsJsonBody(
             s"""
@@ -451,11 +447,11 @@ class ScenarioActivitySpec
 
     implicit class VerifyAttachmentsExists[T <: ValidatableResponse](validatableResponse: T) {
 
-      def verifyAttachmentsExists(scenarioName: ProcessName): ValidatableResponse = {
+      def verifyAttachmentsExists(scenarioName: String): ValidatableResponse = {
         given()
           .basicAuth("reader", "reader")
           .when()
-          .get(s"$nuDesignerHttpAddress/api/processes/${scenarioName.value}/activity")
+          .get(s"$nuDesignerHttpAddress/api/processes/$scenarioName/activity")
           .Then()
           .body(
             matchJsonWithRegexValues(
