045bf7b574a0f7876f8387640bf0003cb752be72
Mateusz Słabek <mslb@touk.pl>
2024-01-22 09:24:41 +0100

    validate on level of CanonicalProcess and cleanup

---
+++ interpreter/src/main/scala/pl/touk/nussknacker/engine/compile/FragmentValidator.scala
@@ -0,0 +1,34 @@
+package pl.touk.nussknacker.engine.compile
+
+import cats.data.Validated.{invalidNel, valid}
+import cats.data.ValidatedNel
+import cats.implicits._
+import pl.touk.nussknacker.engine.api.context.ProcessCompilationError
+import pl.touk.nussknacker.engine.api.context.ProcessCompilationError.DuplicateFragmentOutputNamesInFragment
+import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
+import pl.touk.nussknacker.engine.graph.node.FragmentOutputDefinition
+
+object FragmentValidator {
+
+  def validateUniqueFragmentOutputNames(
+      process: CanonicalProcess
+  ): ValidatedNel[ProcessCompilationError, Unit] = {
+    if (process.metaData.isFragment) {
+      val nodes               = process.collectAllNodes
+      val fragmentOutputNodes = nodes.collect { case fo: FragmentOutputDefinition => fo }
+      val duplicatedOutputNamesWithNodeIds = fragmentOutputNodes
+        .groupBy(_.outputName)
+        .collect {
+          case (name, nodes) if nodes.size > 1 => name -> nodes.map(_.id).toSet
+        }
+      duplicatedOutputNamesWithNodeIds
+        .map(n => invalidNel(DuplicateFragmentOutputNamesInFragment(n._1, n._2)))
+        .toList
+        .sequence
+        .map(_ => ())
+    } else {
+      valid(())
+    }
+  }
+
+}
