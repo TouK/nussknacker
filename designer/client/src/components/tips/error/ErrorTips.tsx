37299562677317a13b9c6c40c0f79c2196691ed1
Mateusz Słabek <mslb@touk.pl>
2024-01-24 14:54:58 +0100

    FE review

--- designer/client/src/components/tips/error/ErrorTips.tsx
+++ designer/client/src/components/tips/error/ErrorTips.tsx
@@ -1,8 +1,6 @@
 import React, { useMemo } from "react";
 import { concat, isEmpty } from "lodash";
 import { Props } from "./Errors";
-import { NodeValidationError } from "../../../types";
-import { v4 as uuid4 } from "uuid";
 import NodeErrorsLinkSection from "./NodeErrorsLinkSection";
 import i18next from "i18next";
 
@@ -11,17 +9,13 @@ export const ErrorTips = ({ errors, showDetails, scenario }: Props) => {
 
     const invalidNodeIds = Object.keys(invalidNodes);
 
-    const renderGlobalErrorSimpleMessage = (error: NodeValidationError) => (
-        <span key={uuid4()} title={error.description}>
-            {error.message + (error.fieldName ? `(${error.fieldName})` : "")}
-        </span>
-    );
-
     const globalErrorsLinkSections = useMemo(
         () =>
             globalErrors.map((error, index) =>
                 isEmpty(error.nodeIds) ? (
-                    renderGlobalErrorSimpleMessage(error.error)
+                    <span key={index} title={error.error.description}>
+                        {error.error.message}
+                    </span>
                 ) : (
                     <NodeErrorsLinkSection
                         key={index}
