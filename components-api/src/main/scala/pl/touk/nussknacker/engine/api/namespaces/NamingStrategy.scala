f370c298b3a449737e7872eeac5a198aa796b17e
Mateusz Słabek <mslb@touk.pl>
2024-02-12 16:14:37 +0100

    cleanup

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/namespaces/NamingStrategy.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/namespaces/NamingStrategy.scala
@@ -1,28 +1,30 @@
 package pl.touk.nussknacker.engine.api.namespaces
 
 import com.typesafe.config.Config
+import pl.touk.nussknacker.engine.api.namespaces.NamingStrategy.namespaceSeparator
 
-case class NamingStrategy(namespace: Option[String]) {
+final case class NamingStrategy(namespace: Option[String]) {
 
-  def prepareName(name: String): String = {
-    namespace match {
-      case Some(value) => s"${value}_$name"
-      case None        => name
-    }
+  private val namespacePattern = namespace.map(ns => s"^$ns$namespaceSeparator(.*)".r)
+
+  def prepareName(name: String): String = namespace match {
+    case Some(value) => s"$value$namespaceSeparator$name"
+    case None        => name
   }
 
-  def decodeName(name: String): Option[String] = {
-    namespace match {
-      case Some(ns) =>
-        val pattern = s"${ns}_(.*)".r
-        name match {
-          case pattern(originalName) => Some(originalName)
-          case _                     => None
-        }
-      case None => Some(name)
-    }
+  def decodeName(name: String): Option[String] = namespacePattern match {
+    case Some(pattern) =>
+      name match {
+        case pattern(originalName) => Some(originalName)
+        case _                     => None
+      }
+    case None => Some(name)
   }
 
+}
+
+object NamingStrategy {
+  private val namespaceSeparator = "_"
 }
 
 object NamingStrategyProvider {
