90da2498be1323967b054fd2a1fdfd3dcf95876b
MK Software <mk_software_pl@protonmail.com>
2024-02-12 12:09:13 +0100

    wip

--- engine/common/components-tests/src/test/scala/pl/touk/nussknacker/engine/common/components/DecisionTableSpec.scala
+++ engine/common/components-tests/src/test/scala/pl/touk/nussknacker/engine/common/components/DecisionTableSpec.scala
@@ -1,15 +1,18 @@
 package pl.touk.nussknacker.engine.common.components
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.engine.api.component.{ComponentDefinition, ComponentProvider, NussknackerVersion}
 import pl.touk.nussknacker.engine.api.process.ProcessObjectDependencies
 import pl.touk.nussknacker.engine.build.ScenarioBuilder
 import pl.touk.nussknacker.engine.graph.expression.Expression
 import pl.touk.nussknacker.engine.lite.util.test.LiteTestScenarioRunner._
 import pl.touk.nussknacker.engine.spel
+import pl.touk.nussknacker.engine.util.config.DocsConfig
 import pl.touk.nussknacker.engine.util.test.TestScenarioRunner
 import pl.touk.nussknacker.test.ValidatedValuesDetailedMessage
+
 import scala.jdk.CollectionConverters._
 import java.util.{List => JList}
 import java.util.{Map => JMap}
@@ -99,3 +102,27 @@ class DecisionTableSpec extends AnyFunSuite with Matchers with ValidatedValuesDe
 }
 
 private final case class TestMessage(id: String, value: Int)
+
+class DecisionTableComponentProvider extends ComponentProvider {
+
+  override val providerName: String = "decisionTable"
+
+  override def resolveConfigForExecution(config: Config): Config = config
+
+  override def isCompatible(version: NussknackerVersion): Boolean = true
+
+  override val isAutoLoaded: Boolean = true
+
+  override def create(config: Config, dependencies: ProcessObjectDependencies): List[ComponentDefinition] = {
+    val docsConfig = DocsConfig(config)
+    import docsConfig._
+
+    List {
+      ComponentDefinition(
+        name = "decision-table",
+        component = DecisionTable
+      ).withRelativeDocs("BasicNodes#decisiontable")
+    }
+  }
+
+}
