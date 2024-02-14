3ef2b9a9b635a5e263fc2ceb46810fc06bcb887e
Marcel Philipiak <ramcel@localhost.localdomain>
2024-02-14 14:38:33 +0100

    started tests for CustomActionValidator

---
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/validation/CustomActionValidator.scala
@@ -0,0 +1,33 @@
+package pl.touk.nussknacker.ui.validation
+
+import pl.touk.nussknacker.engine.api.NodeId
+import pl.touk.nussknacker.engine.api.deployment.{CustomAction, CustomActionRequest}
+import pl.touk.nussknacker.engine.graph.expression.Expression
+
+object CustomActionValidator {
+
+  def validateCustomActionParams(request: CustomActionRequest, customAction: CustomAction): Unit = {
+    implicit val nodeId: NodeId = NodeId(customAction.name)
+    val requestParamsMap        = request.params
+    val customActionParams      = customAction.parameters
+
+    if (requestParamsMap.keys.size != customActionParams.size) {
+      throw new Exception("Different count of custom action parameters than provided in request for: " + request.name)
+    }
+
+    requestParamsMap.foreach { case (k, v) =>
+      customActionParams.find(_.name == k) match {
+        case Some(param) =>
+          param.validators.foreach { validators =>
+            if (validators.nonEmpty) {
+              validators.foreach(
+                _.isValid(paramName = k, expression = Expression.spel("None"), value = Some(v), label = None)
+              )
+            }
+          }
+        case None => throw new Exception("No such parameter should be defined for this action: " + customAction.name)
+      }
+    }
+  }
+
+}
