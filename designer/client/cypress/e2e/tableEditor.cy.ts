d0d9cfdb6ef6bc64d386f24a4ac1fc3434e271e7
Julian Wielga <j@touk.pl>
2024-02-14 02:14:32 +0100

    fixed test

--- designer/client/cypress/e2e/tableEditor.cy.ts
+++ designer/client/cypress/e2e/tableEditor.cy.ts
@@ -16,6 +16,11 @@ describe("Table editor", () => {
     it("should display rich table editor", () => {
         cy.viewport("macbook-15");
         cy.visitNewProcess(seed, "table");
+        cy.intercept("POST", "/api/nodes/*/validation", (request) => {
+            if (request.body.nodeData.service?.parameters.find((p) => p.name === "Expression")) {
+                request.alias = "validation";
+            }
+        });
 
         cy.getNode("decisionTableCustomNode").dblclick();
         cy.get("[data-testid=window]").should("be.visible").as("modal");
@@ -24,7 +29,7 @@ describe("Table editor", () => {
         snapshot();
 
         cy.get("@table").click(100, 50);
-        cy.get("[value='java.lang.Double']").click();
+        cy.get("[role='menuitem']").contains("Double").click();
         cy.get("@table").click(550, 18);
         cy.realType("some name");
         cy.realPress("Enter");
@@ -43,6 +48,35 @@ describe("Table editor", () => {
         cy.realType("xxx");
         snapshot();
 
+        cy.get("[title=Expression]").next().find(".ace_editor").as("expr");
+
+        cy.get("@expr").click().type("{selectall}#ROW.").contains(/.$/);
+        cy.get(".ace_autocomplete")
+            .should("be.visible")
+            .matchImage({
+                maxDiffThreshold: 0.0025,
+                screenshotConfig: { padding: [25, 1, 1] },
+            });
+        cy.get("@expr").type("B");
+        cy.wait("@validation").its("response.statusCode").should("eq", 200);
+        cy.contains(`Bad expression type, expected: Boolean, found: String`).should("be.visible");
+
+        cy.get("@table").click(350, 50);
+        cy.get("[role='menuitem']").contains("Boolean").click();
+        cy.wait("@validation").its("response.statusCode").should("eq", 200);
+        cy.contains(`Bad expression type, expected: Boolean, found: String`).should("not.be.visible");
+
+        cy.get("@expr").click().type(`{selectall}#ROW["{ctrl} `);
+        cy.get(".ace_autocomplete")
+            .should("be.visible")
+            .matchImage({
+                maxDiffThreshold: 0.0025,
+                screenshotConfig: { padding: [25, 1, 1] },
+            });
+        cy.get("@expr").type(`{leftarrow}{leftarrow}some name`);
+        cy.wait("@validation").its("response.statusCode").should("eq", 200);
+        cy.contains(`Bad expression type, expected: Boolean, found: Double`).should("be.visible");
+
         cy.get("@table").rightclick(15, 125);
         cy.contains(/^remove row$/i).click();
         cy.get("@table").rightclick(480, 15);
@@ -54,5 +88,8 @@ describe("Table editor", () => {
         cy.get("@table").rightclick(480, 90);
         cy.contains(/^remove column$/i).click();
         snapshot();
+
+        cy.wait("@validation").its("response.statusCode").should("eq", 200);
+        cy.contains(`There is no property 'some name' in type`).should("be.visible");
     });
 });
