2376a601c0b1c877c1f185b234649af3669bfa95
MK Software <mk_software_pl@protonmail.com>
2024-02-19 14:18:44 +0100

    improvements

--- common-api/src/main/scala/pl/touk/nussknacker/engine/api/definition/TabularTypedData.scala
+++ common-api/src/main/scala/pl/touk/nussknacker/engine/api/definition/TabularTypedData.scala
@@ -1,8 +1,12 @@
 package pl.touk.nussknacker.engine.api.definition
 
 import io.circe
-import io.circe.{Decoder, Encoder, Json}
+import io.circe.Decoder.Result
+import io.circe.DecodingFailure.Reason.CustomReason
+import io.circe.syntax.EncoderOps
+import io.circe._
 import pl.touk.nussknacker.engine.api.CirceUtil._
+import pl.touk.nussknacker.engine.api.definition.TabularTypedData.Cell.RawValue
 import pl.touk.nussknacker.engine.api.definition.TabularTypedData.{Column, Row}
 
 import scala.util.Try
@@ -20,136 +24,193 @@ object TabularTypedData {
     final case class Definition(name: String, aType: Class[_])
   }
 
-  final case class Cell(definition: Column.Definition, value: Any)
+  final case class Cell(definition: Column.Definition, rawValue: RawValue) {
+
+    def value: Any = rawValue match {
+      case RawValue(null) => null
+      case RawValue(v)    => fromStringToSupportedClassInstance(v, definition.aType).get
+    }
+
+  }
+
+  object Cell {
+    final case class RawValue(value: String)
+  }
+
   final case class Row(cells: Vector[Cell])
 
-  def create(columns: Vector[Column.Definition], rows: Vector[Vector[Any]]): Try[TabularTypedData] = Try {
+  type HumanReadableErrorMessage = String
+
+  lazy val empty: TabularTypedData = TabularTypedData(Vector.empty)
+
+  def create(
+      columns: Vector[Column.Definition],
+      rows: Vector[Vector[RawValue]]
+  ): Either[HumanReadableErrorMessage, TabularTypedData] = {
+    for {
+      _    <- validateColumnNamesUniqueness(columns)
+      _    <- validateCellsCount(columns, rows)
+      data <- Right(createTabularTypedData(columns, rows))
+      _    <- validateCellValuesType(data)
+    } yield data
+  }
+
+  private def createTabularTypedData(columns: Vector[Column.Definition], rows: Vector[Vector[RawValue]]) = {
     TabularTypedData {
       columns.zipWithIndex
         .map { case (column, idx) =>
-          val valuesOfColumn = rows.map { row =>
-            row.lift(idx) match {
-              case Some(value) => value
-              case None        => throw new IllegalArgumentException("More columns than rows")
-            }
-          }
+          val valuesOfColumn = rows.map { row => row(idx) }
           Column(column, valuesOfColumn.map(Cell(column, _)))
         }
     }
   }
 
-  lazy val empty: TabularTypedData = TabularTypedData(Vector.empty)
+  private def validateColumnNamesUniqueness(
+      columns: Vector[Column.Definition]
+  ): Either[HumanReadableErrorMessage, Unit] = {
+    val originColumnNames = columns.map(_.name)
+    val duplicates        = originColumnNames.diff(originColumnNames.distinct)
+    if (duplicates.isEmpty) Right(())
+    else Left(s"Column names should be unique. Duplicates: ${duplicates.distinct.mkString(",")}")
+  }
+
+  private def validateCellsCount(columns: Vector[Column.Definition], rows: Vector[Vector[Any]]) = {
+    rows.foldLeft(Right(()): Either[HumanReadableErrorMessage, Unit]) {
+      case (r @ Right(()), row) if row.size == columns.size => r
+      case (Right(()), _)   => Left("All rows should have the same number of cells as there are columns")
+      case (l @ Left(_), _) => l
+    }
+  }
+
+  private def validateCellValuesType(data: TabularTypedData): Either[HumanReadableErrorMessage, Unit] = {
+    data.rows.flatMap(_.cells).foldLeft(Right(()): Either[HumanReadableErrorMessage, Unit]) {
+      case (r @ Right(()), cell) if cell.rawValue == RawValue(null) => r
+      case (r @ Right(()), cell) if doesCellValueLookOK(cell)       => r
+      case (Right(()), cell) =>
+        Left(
+          s"Column has a ${cell.definition.aType.getCanonicalName} type but the value ${cell.rawValue.value} cannot be converted to it."
+        )
+      case (l @ Left(_), _) => l
+    }
+  }
+
+  private def doesCellValueLookOK(cell: Cell) = {
+    fromStringToSupportedClassInstance(cell.rawValue.value, cell.definition.aType).isSuccess
+  }
+
+  private def fromStringToSupportedClassInstance(value: String, toType: Class[_]): Try[Any] = Try {
+    toType match {
+      case t if classOf[java.lang.String] == t        => value
+      case t if classOf[java.lang.Boolean] == t       => java.lang.Boolean.valueOf(value)
+      case t if classOf[java.lang.Integer] == t       => java.lang.Integer.valueOf(value)
+      case t if classOf[java.lang.Float] == t         => java.lang.Float.valueOf(value)
+      case t if classOf[java.lang.Double] == t        => java.lang.Double.valueOf(value)
+      case t if classOf[java.math.BigInteger] == t    => new java.math.BigInteger(value)
+      case t if classOf[java.math.BigDecimal] == t    => new java.math.BigDecimal(value)
+      case t if classOf[java.time.LocalDate] == t     => java.time.LocalDate.parse(value)
+      case t if classOf[java.time.LocalDateTime] == t => java.time.LocalDateTime.parse(value)
+      case t => throw new IllegalArgumentException(s"Type ${t.getCanonicalName} is not supported")
+    }
+  }
 
   implicit class Stringify(data: TabularTypedData) {
 
-    def stringify: String = Coders.encoder(data).noSpaces
+    def stringify: String = Coders.TabularTypedDataEncoder(data).noSpaces
   }
 
   def fromString(value: String): Either[circe.Error, TabularTypedData] = {
     for {
       json <- io.circe.parser.parse(value)
-      data <- TabularTypedData.Coders.decoder.decodeJson(json)
+      data <- Coders.TabularTypedDataDecoder.decodeJson(json)
     } yield data
   }
 
-  object Coders {
+}
 
-    /* example:
+/* example:
+{
+  "columns": [
     {
-      "columns": [
-        {
-          "name": "some name",
-          "type": "java.lang.Double"
-        },
-        {
-          "name": "B",
-          "type": "java.lang.String"
-        },
-        {
-          "name": "C",
-          "type": "java.lang.String"
-        }
-      ],
-      "rows": [
-        [
-          null,
-          null,
-          "test"
-        ],
-        [
-          1,
-          "foo",
-          "bar"
-        ],
-        [
-          null,
-          null,
-          "xxx"
-        ]
-      ]
+      "name": "some name",
+      "type": "java.lang.Double"
+    },
+    {
+      "name": "B",
+      "type": "java.lang.String"
+    },
+    {
+      "name": "C",
+      "type": "java.lang.String"
     }
-     */
-    implicit val encoder: Encoder[TabularTypedData] = Encoder.instance { data =>
-      Json.obj(
-        "columns" -> Json.arr(
-          data.columns
-            .map { c =>
-              Json.obj(
-                "name" -> Json.fromString(c.definition.name),
-                "type" -> Json.fromString(c.definition.aType.getCanonicalName)
-              )
-            }: _*
-        ),
-        "rows" -> Json.arr(
-          data.rows
-            .map(_.cells.map(_.value))
-            .map { rowValues =>
-              Json.arr(
-                rowValues.map {
-                  case null          => Json.Null
-                  case v: String     => Json.fromString(v)
-                  case v: Int        => Json.fromInt(v)
-                  case v: Long       => Json.fromLong(v)
-                  case v: Double     => Json.fromDoubleOrNull(v)
-                  case v: Float      => Json.fromFloatOrNull(v)
-                  case v: BigDecimal => Json.fromBigDecimal(v)
-                  case v: BigInt     => Json.fromBigInt(v)
-                  case v: Boolean    => Json.fromBoolean(v)
-                  case c             => throw new IllegalArgumentException(s"Unexpected type: ${c.getClass.getName}")
-                }: _*
-              )
-            }: _*
-        )
-      )
+  ],
+  "rows": [
+    [
+      null,
+      null,
+      "test"
+    ],
+    [
+      "1",
+      "foo",
+      "bar"
+    ],
+    [
+      null,
+      null,
+      "xxx"
+    ]
+  ]
+}
+ */
+private object Coders {
+
+  object TabularTypedDataEncoder extends Encoder[TabularTypedData] {
+    override def apply(data: TabularTypedData): Json = dataEncoder(data)
+
+    private implicit lazy val dataEncoder: Encoder[TabularTypedData] =
+      Encoder.forProduct2("columns", "rows")(data => (data.columns, data.rows))
+
+    private implicit lazy val columnEncoder: Encoder[Column] =
+      Encoder.forProduct2("name", "type")(column => (column.definition.name, column.definition.aType))
+
+    private implicit lazy val rowEncoder: Encoder[Row] = Encoder.instance { row =>
+      row.cells.map(_.rawValue).asJson
     }
 
-    // todo: are we sure keys are unique
-    implicit val decoder: Decoder[TabularTypedData] = {
-      implicit val classDecoder: Decoder[Class[_]] = Decoder.decodeString.emapTry[Class[_]] { str =>
-        Try(Class.forName(str))
-      }
-      implicit val columnDecoder: Decoder[Column.Definition] =
-        Decoder.forProduct2("name", "type")(Column.Definition.apply)
-      implicit val valueDecoder: Decoder[Any] = Decoder.decodeJson.emapTry { value =>
-        Try { // todo: validate if type is declared one (and try to cast)
-          value.asNull.map(_ => null).getOrElse {
-            value.asString.getOrElse {
-              value.asBoolean.getOrElse {
-                value.asNumber.flatMap(_.toBigDecimal).getOrElse {
-                  throw new IllegalArgumentException("Value unexpected type")
-                }
-              }
-            }
-          }
-        }
-      }
-      Decoder.instance { c =>
-        for {
-          columns <- c.downField("columns").as[Vector[Column.Definition]]
-          rows    <- c.downField("rows").as[Vector[Vector[Any]]]
-        } yield TabularTypedData.create(columns, rows).get
-      }
+    private implicit lazy val aTypeEncoder: Encoder[Class[_]] =
+      Encoder.encodeString.contramap(_.getCanonicalName)
+
+    private implicit lazy val rawValueEncoder: Encoder[RawValue] = Encoder.encodeJson.contramap {
+      case RawValue(null)    => Json.Null
+      case RawValue(nonNull) => Json.fromString(nonNull)
     }
+
+  }
+
+  object TabularTypedDataDecoder extends Decoder[TabularTypedData] {
+
+    override def apply(c: HCursor): Result[TabularTypedData] = {
+      for {
+        columns <- c.downField("columns").as[Vector[Column.Definition]]
+        rows    <- c.downField("rows").as[Vector[Vector[RawValue]]]
+        data <- TabularTypedData
+          .create(columns, rows)
+          .left
+          .map(message => DecodingFailure(CustomReason(message), List.empty))
+      } yield data
+    }
+
+    private implicit val classDecoder: Decoder[Class[_]] =
+      Decoder.decodeString.emapTry[Class[_]] { str => Try(Class.forName(str)) }
+
+    private implicit val columnDecoder: Decoder[Column.Definition] =
+      Decoder.forProduct2("name", "type")(Column.Definition.apply)
+
+    private implicit val rawValueDecoder: Decoder[RawValue] =
+      Decoder[Option[String]].map {
+        case Some(value) => RawValue(value)
+        case None        => RawValue(null)
+      }
 
   }
 
--- engine/common/components-tests/src/test/scala/pl/touk/nussknacker/engine/common/components/DecisionTableSpec.scala
+++ engine/common/components-tests/src/test/scala/pl/touk/nussknacker/engine/common/components/DecisionTableSpec.scala
@@ -1,58 +1,70 @@
 package pl.touk.nussknacker.engine.common.components
 
-import org.scalatest.funsuite.AnyFunSuite
+import cats.data.ValidatedNel
+import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 import pl.touk.nussknacker.engine.api.component.ComponentDefinition
-import pl.touk.nussknacker.engine.build.ScenarioBuilder
-import pl.touk.nussknacker.engine.graph.expression.Expression
+import pl.touk.nussknacker.engine.api.context.ProcessCompilationError
+import pl.touk.nussknacker.engine.build.{GraphBuilder, ScenarioBuilder}
+import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
+import pl.touk.nussknacker.engine.flink.test.FlinkSpec
+import pl.touk.nussknacker.engine.flink.util.test.FlinkTestScenarioRunner
 import pl.touk.nussknacker.engine.lite.util.test.LiteTestScenarioRunner._
+import pl.touk.nussknacker.engine.flink.util.test.FlinkTestScenarioRunner._
+import pl.touk.nussknacker.engine.graph.expression.Expression
+import pl.touk.nussknacker.engine.lite.util.test.LiteTestScenarioRunner
 import pl.touk.nussknacker.engine.spel
-import pl.touk.nussknacker.engine.util.test.TestScenarioRunner
+import pl.touk.nussknacker.engine.util.test.{RunListResult, TestScenarioRunner}
 import pl.touk.nussknacker.test.ValidatedValuesDetailedMessage
 
 import java.util.{List => JList, Map => JMap}
 import scala.jdk.CollectionConverters._
+import scala.reflect.ClassTag
 
-class DecisionTableSpec extends AnyFunSuite with Matchers with ValidatedValuesDetailedMessage {
+trait DecisionTableSpec extends AnyFreeSpec with Matchers with ValidatedValuesDetailedMessage {
 
   import spel.Implicits._
 
-  private val testScenarioRunner = TestScenarioRunner
-    .liteBased()
-    .withExtraComponents(
-      ComponentDefinition("decision-table", DecisionTable) :: Nil
-    )
-    .build()
+  "Decision Table component should" - {
+    "filter and return decision table's rows filtered by the expression" in {
+      val scenarioWithDecisionTable = ScenarioBuilder
+        .requestResponse("test scenario")
+        .source("request", TestScenarioRunner.testDataSource)
+        .enricher(
+          "decision-table",
+          "dtResult",
+          "decision-table",
+          "Basic Decision Table" -> decisionTableJson,
+          "Expression"           -> "#ROW['B'] == 'foo' && #ROW['C'] != null",
+        )
+        .end("end", "value" -> "#dtResult")
 
-  test("simple test") { // todo: change name
-    val scenarioWithDecisionTable = ScenarioBuilder
-      .requestResponse("test scenario")
-      .source("request", TestScenarioRunner.testDataSource)
-      .enricher(
-        "decision-table",
-        "dtResult",
-        "decision-table",
-        "Basic Decision Table" -> decisionTableJson,
-        "Expression"           -> "#ROW['B'] == 'foo' && #ROW['C'] != null",
+      val validatedResult = runScenario[TestMessage, JList[JMap[String, Any]]](scenarioWithDecisionTable)(
+        withData = List(TestMessage("1", 100))
       )
-      .emptySink("response", TestScenarioRunner.testResultSink, "value" -> "#dtResult")
 
-    val validatedResult = testScenarioRunner.runWithData[TestMessage, JList[JMap[String, Any]]](
-      scenario = scenarioWithDecisionTable,
-      data = List(TestMessage("1", 100))
-    )
-
-    val resultList = validatedResult.validValue.successes
-    resultList should be(oneElementList {
-      List(
-        Map(
-          "somename" -> 1,
-          "B"        -> "foo",
-          "C"        -> "bar"
+      val resultList = validatedResult.validValue.successes
+      resultList should be(oneElementList {
+        List(
+          Map(
+            "somename" -> 1.0,
+            "B"        -> "foo",
+            "C"        -> "bar"
+          ).asJava
         ).asJava
-      ).asJava
-    })
+      })
+    }
   }
+
+  protected def testScenarioRunner: TestScenarioRunner
+
+  protected def runScenario[DATA: ClassTag, RESULT](scenario: CanonicalProcess)(
+      withData: Iterable[DATA]
+  ): ValidatedNel[ProcessCompilationError, RunListResult[RESULT]]
+
+  protected def addEndNode(
+      builder: GraphBuilder[CanonicalProcess]
+  )(id: String, params: Seq[(String, Expression)]): CanonicalProcess
 
   private lazy val decisionTableJson = Expression.tabularDataDefinition {
     s"""{
@@ -77,7 +89,7 @@ class DecisionTableSpec extends AnyFunSuite with Matchers with ValidatedValuesDe
        |      "test"
        |    ],
        |    [
-       |      1,
+       |      "1",
        |      "foo",
        |      "bar"
        |    ],
@@ -91,6 +103,60 @@ class DecisionTableSpec extends AnyFunSuite with Matchers with ValidatedValuesDe
   }
 
   private def oneElementList[T](obj: T) = List(obj)
+
+  private implicit class AddEndNodeExt(builder: GraphBuilder[CanonicalProcess]) {
+    def end(id: String, params: (String, Expression)*): CanonicalProcess =
+      addEndNode(builder)(id, params)
+  }
+
 }
 
 private final case class TestMessage(id: String, value: Int)
+
+class FlinkEngineRunDecisionTableSpec extends DecisionTableSpec with FlinkSpec {
+
+  override protected lazy val testScenarioRunner: FlinkTestScenarioRunner =
+    TestScenarioRunner
+      .flinkBased(config, flinkMiniCluster)
+      .withExtraComponents(
+        ComponentDefinition("decision-table", DecisionTable) :: Nil
+      )
+      .build()
+
+  override protected def runScenario[DATA: ClassTag, RESULT](
+      scenario: CanonicalProcess
+  )(withData: Iterable[DATA]): ValidatedNel[ProcessCompilationError, RunListResult[RESULT]] = {
+    testScenarioRunner.runWithData(scenario, withData.toList)
+  }
+
+  override protected def addEndNode(
+      builder: GraphBuilder[CanonicalProcess]
+  )(id: String, params: Seq[(String, Expression)]): CanonicalProcess = {
+    builder.processorEnd(id, TestScenarioRunner.testResultService, params: _*)
+  }
+
+}
+
+class LiteEngineRunDecisionTableSpec extends DecisionTableSpec {
+
+  override protected lazy val testScenarioRunner: LiteTestScenarioRunner =
+    TestScenarioRunner
+      .liteBased()
+      .withExtraComponents(
+        ComponentDefinition("decision-table", DecisionTable) :: Nil
+      )
+      .build()
+
+  override protected def runScenario[DATA: ClassTag, RESULT](
+      scenario: CanonicalProcess
+  )(withData: Iterable[DATA]): ValidatedNel[ProcessCompilationError, RunListResult[RESULT]] = {
+    testScenarioRunner.runWithData(scenario, withData.toList)
+  }
+
+  override protected def addEndNode(
+      builder: GraphBuilder[CanonicalProcess]
+  )(id: String, params: Seq[(String, Expression)]): CanonicalProcess = {
+    builder.emptySink(id, TestScenarioRunner.testResultSink, params: _*)
+  }
+
+}
