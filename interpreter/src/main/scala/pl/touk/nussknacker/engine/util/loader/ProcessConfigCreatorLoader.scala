6426f75613f4fec91e38938aaf485285a1d6a55d
Arek Burdach <arek.burdach@gmail.com>
2024-02-22 10:51:04 +0100

    Tests improvements: FlinkTestScenarioRunner showing job failure details + work around for Idea classloader issue (#5602)

--- interpreter/src/main/scala/pl/touk/nussknacker/engine/util/loader/ProcessConfigCreatorLoader.scala
+++ interpreter/src/main/scala/pl/touk/nussknacker/engine/util/loader/ProcessConfigCreatorLoader.scala
@@ -3,7 +3,8 @@ package pl.touk.nussknacker.engine.util.loader
 import pl.touk.nussknacker.engine.api.conversion.ProcessConfigCreatorMapping
 import pl.touk.nussknacker.engine.api.process.ProcessConfigCreator
 
-object ProcessConfigCreatorLoader extends LoadClassFromClassLoader {
+class ProcessConfigCreatorLoader(shouldIncludeConfigCreator: ProcessConfigCreator => Boolean)
+    extends LoadClassFromClassLoader {
   type JPCC = pl.touk.nussknacker.engine.javaapi.process.ProcessConfigCreator
   type SPCC = ProcessConfigCreator
 
@@ -11,7 +12,7 @@ object ProcessConfigCreatorLoader extends LoadClassFromClassLoader {
   override val prettyClassName: String = "ProcessConfigCreator"
 
   override def loadAll(classLoader: ClassLoader): List[SPCC] = {
-    ScalaServiceLoader.load[SPCC](classLoader) ++
+    ScalaServiceLoader.load[SPCC](classLoader).filter(shouldIncludeConfigCreator) ++
       ScalaServiceLoader
         .load[JPCC](classLoader)
         .map(ProcessConfigCreatorMapping.toProcessConfigCreator)
