3b198cc64fafbdb7fceeaa778acadd851ad7c237
Dawid Poliszak <dpl@touk.pl>
2024-02-22 10:44:19 +0100

    NU-1431 update type

--- designer/submodules/packages/components/src/settings/useNkSettingsQuery.tsx
+++ designer/submodules/packages/components/src/settings/useNkSettingsQuery.tsx
@@ -2,10 +2,9 @@ import HttpService from "nussknackerUi/HttpService";
 import { useContext } from "react";
 import { useQuery } from "react-query";
 import { UseQueryResult } from "react-query/types/react/types";
-import { Promisable } from "type-fest";
 import { NkApiContext } from "./nkApiProvider";
 
-export function useNkSettingsQuery(): UseQueryResult<Promisable<ReturnType<(typeof HttpService)["fetchSettingsWithAuth"]>>> {
+export function useNkSettingsQuery(): UseQueryResult<Awaited<ReturnType<(typeof HttpService)["fetchSettingsWithAuth"]>>> {
     const api = useContext(NkApiContext);
     return useQuery({
         queryKey: "settings",
