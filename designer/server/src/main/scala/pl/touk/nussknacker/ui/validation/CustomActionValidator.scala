687341407468985b9a0ab8ff4ab3c0cac86ae419
Marcel Philipiak <ramcel@localhost.localdomain>
2024-02-26 20:00:01 +0100

    Started writing validation endpoint

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/validation/CustomActionValidator.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/validation/CustomActionValidator.scala
@@ -3,16 +3,18 @@ package pl.touk.nussknacker.ui.validation
 import pl.touk.nussknacker.engine.api.NodeId
 import pl.touk.nussknacker.engine.deployment.{CustomActionDefinition, CustomActionRequest}
 import pl.touk.nussknacker.engine.graph.expression.Expression
+import pl.touk.nussknacker.ui.process.deployment.ValidationError
 
 object CustomActionValidator {
 
   def validateCustomActionParams(request: CustomActionRequest, customAction: CustomActionDefinition): Unit = {
+
     implicit val nodeId: NodeId = NodeId(customAction.name.value)
     val requestParamsMap        = request.params
     val customActionParams      = customAction.parameters
 
     if (requestParamsMap.keys.size != customActionParams.size) {
-      throw new Exception("Different count of custom action parameters than provided in request for: " + request.name)
+      throw ValidationError("Different count of custom action parameters than provided in request for: " + request.name)
     }
 
     requestParamsMap.foreach { case (k, v) =>
@@ -25,7 +27,7 @@ object CustomActionValidator {
               )
             }
           }
-        case None => throw new Exception("No such parameter should be defined for this action: " + customAction.name)
+        case None => throw ValidationError("No such parameter should be defined for this action: " + customAction.name)
       }
     }
   }
