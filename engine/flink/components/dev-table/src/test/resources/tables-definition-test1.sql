64c6e8d6c206630be9f79b1d1fdbf7ae26033554
Mateusz Słabek <mslb@touk.pl>
2024-02-28 12:48:10 +0100

    test sql file reader

--- engine/flink/components/dev-table/src/test/resources/tables-definition-test1.sql
+++ engine/flink/components/dev-table/src/test/resources/tables-definition-test1.sql
@@ -1,6 +1,17 @@
-CREATE TABLE Orders (
-    customerName STRING,
-    order_number INT
+CREATE TABLE TestTable1
+(
+    someString  STRING,
+    someVarChar VARCHAR(150),
+    someInt     INT
 ) WITH (
-      'connector' = 'kafka'
+      'connector' = 'datagen'
+);
+
+CREATE TABLE TestTable2
+(
+    someString2  STRING,
+    someVarChar2 VARCHAR(150),
+    someInt2     INT
+) WITH (
+      'connector' = 'datagen'
 );
--- engine/flink/components/dev-table/src/test/scala/pl/touk/nussknacker/engine/flink/table/SqlFromFileReaderTest.scala
+++ engine/flink/components/dev-table/src/test/scala/pl/touk/nussknacker/engine/flink/table/SqlFromFileReaderTest.scala
@@ -8,11 +8,28 @@ class SqlFromFileReaderTest extends AnyFunSuite with Matchers {
 
   test("read sql statements from resource") {
     val resourceName = "tables-definition-test1.sql"
-    val statementFromFile =
-      "CREATE TABLE Orders (\n    customerName STRING,\n    order_number INT\n) WITH (\n      'connector' = 'kafka'\n)"
+    val statement1FromFile =
+      """|CREATE TABLE TestTable1
+         |(
+         |    someString  STRING,
+         |    someVarChar VARCHAR(150),
+         |    someInt     INT
+         |) WITH (
+         |      'connector' = 'datagen'
+         |)""".stripMargin
+    val statement2FromFile =
+      s"""|CREATE TABLE TestTable2
+          |(
+          |    someString2  STRING,
+          |    someVarChar2 VARCHAR(150),
+          |    someInt2     INT
+          |) WITH (
+          |      'connector' = 'datagen'
+          |)""".stripMargin
     val statements = SqlFromResourceReader.readFileFromResources(resourceName)
-    inside(statements) { case s :: Nil =>
-      s shouldBe statementFromFile
+    inside(statements) { case s1 :: s2 :: Nil =>
+      s1 shouldBe statement1FromFile
+      s2 shouldBe statement2FromFile
     }
   }
 
