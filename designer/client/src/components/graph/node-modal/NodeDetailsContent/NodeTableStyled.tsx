df446f549e08edc4455ded45920538652f7d30e3
Maciej Cichanowicz <30436981+Elmacioro@users.noreply.github.com>
2024-03-08 11:44:13 +0100

    Modify TestValue textArea height calculation (#5690)

--- designer/client/src/components/graph/node-modal/NodeDetailsContent/NodeTableStyled.tsx
+++ designer/client/src/components/graph/node-modal/NodeDetailsContent/NodeTableStyled.tsx
@@ -1,6 +1,7 @@
 import { css, styled } from "@mui/material";
 import { customCheckbox } from "./CustomCheckbox";
 
+export const HIDDEN_TEXTAREA_PIXEL_HEIGHT = 100;
 export const NodeTableStyled = styled("div")(
     ({ theme }) => css`
         font-size: 11px;
@@ -49,7 +50,7 @@ export const NodeTableStyled = styled("div")(
             }
             &.partly-hidden {
                 textarea {
-                    height: 100px !important;
+                    height: ${HIDDEN_TEXTAREA_PIXEL_HEIGHT}px !important;
                 }
             }
             &.node-value-type-select {
