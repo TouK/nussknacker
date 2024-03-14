c0fa670222043f04a93ace4a58b0e1a0e328d91d
Adrian Matwiejuk <amt@touk.pl>
2024-03-14 10:07:44 +0100

    url fix

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/integration/DictsFlowTest.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/integration/DictsFlowTest.scala
@@ -52,7 +52,7 @@ class DictsFlowTest
     val response1 = httpClient.send(
       quickRequest
         .get(
-          uri"$nuDesignerHttpAddress/api/dicts/${Streaming.stringify}/dict/$DictId/entry?label=${"Black"
+          uri"$nuDesignerHttpAddress/api/dicts/${Streaming.stringify}/$DictId/entry?label=${"Black"
               .take(3)}"
         )
         .auth
