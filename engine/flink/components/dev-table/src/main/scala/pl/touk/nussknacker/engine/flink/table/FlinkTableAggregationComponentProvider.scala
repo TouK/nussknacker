b8e8d37cdfd8ec96039c5fba37d58a75b48f2f64
Mateusz Słabek <mslb@touk.pl>
2024-03-18 23:02:15 +0100

    hardcoded aggregation PoC

---
+++ engine/flink/components/dev-table/src/main/scala/pl/touk/nussknacker/engine/flink/table/FlinkTableAggregationComponentProvider.scala
@@ -0,0 +1,28 @@
+package pl.touk.nussknacker.engine.flink.table
+
+import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
+import pl.touk.nussknacker.engine.api.component.{ComponentDefinition, ComponentProvider, NussknackerVersion}
+import pl.touk.nussknacker.engine.api.process.ProcessObjectDependencies
+import pl.touk.nussknacker.engine.flink.table.aggregate.TableAggregationFactory
+
+object FlinkTableAggregationComponentProvider extends ComponentProvider with LazyLogging {
+
+  override def providerName: String = "tableAggregate"
+
+  override def resolveConfigForExecution(config: Config): Config = config
+
+  override def create(config: Config, dependencies: ProcessObjectDependencies): List[ComponentDefinition] = {
+    components
+  }
+
+  override def isCompatible(version: NussknackerVersion): Boolean = true
+
+  override def isAutoLoaded: Boolean = true
+
+  val components: List[ComponentDefinition] = ComponentDefinition(
+    "aggregate",
+    new TableAggregationFactory()
+  ) :: Nil
+
+}
