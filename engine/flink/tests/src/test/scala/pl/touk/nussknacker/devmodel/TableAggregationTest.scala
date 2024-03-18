9440ce859f1e43c6e8eacc7c21e0c14f9a013afa
Mateusz Słabek <mslb@touk.pl>
2024-03-18 23:02:15 +0100

    hardcoded aggregation PoC

---
+++ engine/flink/tests/src/test/scala/pl/touk/nussknacker/devmodel/TableAggregationTest.scala
@@ -0,0 +1,62 @@
+package pl.touk.nussknacker.devmodel
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.Inside
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.devmodel.TableAggregationTest.TestRecord
+import pl.touk.nussknacker.engine.build.ScenarioBuilder
+import pl.touk.nussknacker.engine.flink.table.FlinkTableAggregationComponentProvider
+import pl.touk.nussknacker.engine.flink.table.aggregate.TableAggregationFactory
+import pl.touk.nussknacker.engine.flink.test.FlinkSpec
+import pl.touk.nussknacker.engine.util.test.TestScenarioRunner
+import pl.touk.nussknacker.test.ValidatedValuesDetailedMessage.convertValidatedToValuable
+
+class TableAggregationTest extends AnyFunSuite with FlinkSpec with Matchers with Inside {
+
+  import pl.touk.nussknacker.engine.flink.util.test.FlinkTestScenarioRunner._
+  import pl.touk.nussknacker.engine.spel.Implicits._
+
+  import scala.jdk.CollectionConverters._
+
+  test("should do sum aggregations") {
+    val runner = TestScenarioRunner
+      .flinkBased(ConfigFactory.empty(), flinkMiniCluster)
+      .withExtraComponents(FlinkTableAggregationComponentProvider.components)
+      .build()
+
+    val scenario = ScenarioBuilder
+      .streaming("test")
+      .source("start", TestScenarioRunner.testDataSource)
+      .customNode(
+        "aggregate",
+        "agg",
+        "aggregate",
+        TableAggregationFactory.groupByParamName.value            -> "#input.someKey",
+        TableAggregationFactory.aggregateByParamName.value        -> "#input.someAmount",
+        TableAggregationFactory.aggregatorFunctionParamName.value -> "'sum'",
+      )
+      .processorEnd("end", TestScenarioRunner.testResultService, "value" -> "#agg")
+
+    val result = runner.runWithData(
+      scenario,
+      List(
+        TestRecord("A", 1),
+        TestRecord("B", 2),
+        TestRecord("A", 1),
+        TestRecord("B", 2),
+      )
+    )
+    print(result)
+
+    result.validValue.successes.toSet shouldBe Set(
+      Map("groupBy" -> "B", "aggregatedSum" -> 4).asJava,
+      Map("groupBy" -> "A", "aggregatedSum" -> 2).asJava,
+    )
+  }
+
+}
+
+object TableAggregationTest {
+  case class TestRecord(someKey: String, someAmount: Int)
+}
