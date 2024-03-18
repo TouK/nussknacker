1c2404ea21b9d5f7110d91b39450c39b63c9973f
Julian Wielga <j@touk.pl>
2024-03-19 00:37:47 +0100

    debug test

--- designer/client/cypress/e2e/process.cy.ts
+++ designer/client/cypress/e2e/process.cy.ts
@@ -125,32 +125,6 @@ describe("Process", () => {
             cy.get("[data-testid=graphPage]").matchImage(screenshotOptions);
         });
 
-        it("should have node search toolbar", () => {
-            cy.get("[data-testid=search-panel]").should("be.visible");
-            cy.get("[data-testid=search-panel]")
-                .contains(/^search$/i)
-                .click();
-            cy.get("[title='toggle left panel']").click();
-            cy.get("[data-testid=search-panel]").should("be.not.visible");
-            cy.get("#nk-graph-main").click();
-            cy.realPress("F");
-            // cy.get("[data-testid=search-panel] input").should("be.visible").should("be.focused");
-            // cy.realType("en");
-            // cy.get("[data-testid=search-panel]").contains(/sms/i).click();
-            // cy.getNode("enricher")
-            //     .parent()
-            //     .matchImage({ screenshotConfig: { padding: 16 } });
-            // cy.get("[data-testid=search-panel]")
-            //     .scrollIntoView()
-            //     .matchImage({ diffConfig: { threshold: 0.08 } });
-            // cy.get("[data-testid=search-panel]")
-            //     .contains(/source/i)
-            //     .dblclick();
-            // cy.get("[data-testid=window]")
-            //     .contains(/^source$/i)
-            //     .should("be.visible");
-        });
-
         it("should allow drag component and drop on edge", () => {
             cy.contains(/^custom$/)
                 .should("be.visible")
