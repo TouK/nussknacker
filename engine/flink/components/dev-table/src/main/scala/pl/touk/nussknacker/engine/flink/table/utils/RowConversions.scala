b2ab24790086a2f2b79d2fc23ea9d4b58ef80ea1
Mateusz Słabek <mslb@touk.pl>
2024-03-19 16:32:30 +0100

    [NU-1473] Table API sink (#5653)
    add sql based table api sink

--- engine/flink/components/dev-table/src/main/scala/pl/touk/nussknacker/engine/flink/table/utils/RowConversions.scala
+++ engine/flink/components/dev-table/src/main/scala/pl/touk/nussknacker/engine/flink/table/utils/RowConversions.scala
@@ -1,6 +1,9 @@
 package pl.touk.nussknacker.engine.flink.table.utils
 
+import org.apache.flink.table.api.Expressions.$
+import org.apache.flink.table.api.{ApiExpression, DataTypes, Schema}
 import org.apache.flink.types.Row
+import pl.touk.nussknacker.engine.flink.table.ColumnDefinition
 
 object RowConversions {
 
@@ -11,5 +14,35 @@ object RowConversions {
     val fields     = fieldNames.map(n => n -> row.getField(n)).toMap
     new java.util.HashMap[String, Any](fields.asJava)
   }
+
+  def mapToRowUnsafe(map: java.util.Map[String, Any], columns: List[ColumnDefinition]): Row = {
+    val row = Row.withNames()
+    columns.foreach(c => row.setField(c.columnName, map.get(c.columnName)))
+    row
+  }
+
+}
+
+object NestedRowConversions {
+
+  import scala.jdk.CollectionConverters._
+
+  private val NestedRowColumnDefaultFlinkGivenName = "f0"
+
+  def columnsToSingleRowFlinkSchema(columns: List[ColumnDefinition]): Schema = {
+    val fields: java.util.List[DataTypes.Field] =
+      columns.map(c => DataTypes.FIELD(c.columnName, c.flinkDataType)).asJava
+    val row = DataTypes.ROW(fields)
+    Schema
+      .newBuilder()
+      .column(
+        NestedRowColumnDefaultFlinkGivenName,
+        row
+      )
+      .build()
+  }
+
+  def flatteningSelectExpressions(columns: List[ColumnDefinition]): List[ApiExpression] =
+    columns.map(c => $(NestedRowColumnDefaultFlinkGivenName).get(c.columnName).as(c.columnName))
 
 }
