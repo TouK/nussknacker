b1f900c0cc0dabf9ba7124d2f7a9d4b4e92326b3
MK Software <mk_software_pl@protonmail.com>
2024-03-19 14:25:34 +0100

    fix

--- utils/flink-components-testkit/src/main/scala/pl/touk/nussknacker/engine/flink/util/test/TestResultService.scala
+++ utils/flink-components-testkit/src/main/scala/pl/touk/nussknacker/engine/flink/util/test/TestResultService.scala
@@ -1,24 +1,14 @@
 package pl.touk.nussknacker.engine.flink.util.test
 
-import pl.touk.nussknacker.engine.api._
 import org.apache.flink.api.common.functions.FlatMapFunction
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
-import org.apache.flink.util.Collector
+import pl.touk.nussknacker.engine.api._
 import pl.touk.nussknacker.engine.api.component.ComponentDefinition
 import pl.touk.nussknacker.engine.api.process.{Sink, SinkFactory}
-import pl.touk.nussknacker.engine.api._
-import pl.touk.nussknacker.engine.api.context.ValidationContext
-import pl.touk.nussknacker.engine.api.context.transformation.{
-  DefinedEagerParameter,
-  NodeDependencyValue,
-  SingleInputDynamicComponent
-}
-import pl.touk.nussknacker.engine.api.definition.{NodeDependency, ParameterWithExtractor}
 import pl.touk.nussknacker.engine.flink.api.process.{BasicFlinkSink, FlinkLazyParameterFunctionHelper}
 import pl.touk.nussknacker.engine.util.test.TestScenarioRunner
 
 import java.util.concurrent.atomic.AtomicReference
-import java.util.function.BinaryOperator
 import scala.concurrent.{ExecutionContext, Future}
 
 class TestResultService extends Service {
