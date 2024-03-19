ea25982c9a7b9784be221416512900580175621a
Arek Burdach <arek.burdach@gmail.com>
2024-03-19 16:42:52 +0100

    review: type checked synchronization of categories and processing types

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/UserApiHttpServiceCategoryUsedMoreThanOnceConfigSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/UserApiHttpServiceCategoryUsedMoreThanOnceConfigSpec.scala
@@ -1,37 +1,23 @@
 package pl.touk.nussknacker.ui.api
 
-import com.typesafe.config.{Config, ConfigFactory}
 import io.restassured.RestAssured.given
 import io.restassured.module.scala.RestAssuredSupport.AddThenToResponse
 import org.scalatest.freespec.AnyFreeSpecLike
-import pl.touk.nussknacker.engine.util.config.ScalaMajorVersionConfig
 import pl.touk.nussknacker.test.base.it.NuItTest
-import pl.touk.nussknacker.test.config.{
-  WithDesignerConfig,
-  WithSimplifiedConfigRestAssuredUsersExtensions,
-  WithSimplifiedDesignerConfig
-}
-import pl.touk.nussknacker.test.{
-  NuRestAssureExtensions,
-  NuRestAssureMatchers,
-  PatientScalaFutures,
-  RestAssuredVerboseLogging
-}
+import pl.touk.nussknacker.test.config.WithCategoryUsedMoreThanOnceDesignerConfig
+import pl.touk.nussknacker.test.config.WithCategoryUsedMoreThanOnceDesignerConfig.TestCategory
+import pl.touk.nussknacker.test.{NuRestAssureExtensions, NuRestAssureMatchers, PatientScalaFutures, RestAssuredVerboseLogging}
 
-class UserApiHttpServiceCategoryUsedMoreThanOnceSetupSpec
+class UserApiHttpServiceCategoryUsedMoreThanOnceConfigSpec
     extends AnyFreeSpecLike
     with NuItTest
-    with WithDesignerConfig
+    with WithCategoryUsedMoreThanOnceDesignerConfig
     with NuRestAssureExtensions
     with NuRestAssureMatchers
     with RestAssuredVerboseLogging
     with PatientScalaFutures {
 
-  override def designerConfig: Config = ScalaMajorVersionConfig.configWithScalaMajorVersion(
-    ConfigFactory.parseResources("config/business-cases/category-used-more-than-once-designer.conf")
-  )
-
-  "In designer setup with multiple processing types using the same category" - {
+  "In designer configured with multiple processing types using the same category" - {
     "The endpoint for getting user info should" - {
       "return not duplicated categories" in {
         given()
@@ -45,7 +31,7 @@ class UserApiHttpServiceCategoryUsedMoreThanOnceSetupSpec
                |  "id": "admin",
                |  "username": "admin",
                |  "isAdmin": true,
-               |  "categories": ["Category1"],
+               |  "categories": ["${TestCategory.Category1}"],
                |  "categoryPermissions": {},
                |  "globalPermissions": []
                |}""".stripMargin)
