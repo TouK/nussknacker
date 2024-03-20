0ac9bc43f10459877b3f88e503023c4c0178296c
Piotr Rudnicki <prn@touk.pl>
2024-03-20 16:25:00 +0100

    refactor after merge

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/MigrationApiHttpService.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/MigrationApiHttpService.scala
@@ -1,21 +1,19 @@
 package pl.touk.nussknacker.ui.services
 
 import cats.data.EitherT
-import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import pl.touk.nussknacker.ui.NuDesignerError
-import pl.touk.nussknacker.ui.api.MigrationApiEndpoints
+import pl.touk.nussknacker.ui.api.{BaseHttpService, MigrationApiEndpoints}
 import pl.touk.nussknacker.ui.migrations.MigrationService
 import pl.touk.nussknacker.ui.security.api.AuthenticationResources
 
 import scala.concurrent.ExecutionContext
 
 class MigrationApiHttpService(
-    config: Config,
     authenticator: AuthenticationResources,
     migrationService: MigrationService
 )(implicit val ec: ExecutionContext)
-    extends BaseHttpService(config, authenticator)
+    extends BaseHttpService(authenticator)
     with LazyLogging {
 
   private val remoteEnvironmentApiEndpoints = new MigrationApiEndpoints(authenticator.authenticationMethod())
