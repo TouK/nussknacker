274bd5c85125ae53f0c2fccace7ec454066e0aa0
Mateusz Słabek <mslb@touk.pl>
2024-03-22 22:38:04 +0100

    add some typing to parameters

---
+++ engine/flink/components/dev-table/src/test/scala/pl/touk/nussknacker/engine/flink/table/aggregate/TableAggregationParametersTest.scala
@@ -0,0 +1,68 @@
+package pl.touk.nussknacker.engine.flink.table.aggregate
+
+import org.scalatest.Inside.inside
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.engine.api.{NodeId, VariableConstants}
+import pl.touk.nussknacker.engine.api.context.ProcessCompilationError.CustomNodeError
+import pl.touk.nussknacker.engine.api.context.ValidationContext
+import pl.touk.nussknacker.engine.api.context.transformation.{
+  DefinedEagerParameter,
+  DefinedLazyParameter,
+  OutputVariableNameValue
+}
+import pl.touk.nussknacker.engine.api.typed.typing.Typed
+
+class TableAggregationParametersTest extends AnyFunSuite with Matchers {
+
+  implicit val nodeId: NodeId = NodeId("id")
+  val aggregationComponent    = new TableAggregation()
+
+  // TODO: after adding more aggregators do a table based test for these cases here
+  test("should return errors for mismatch between aggregate function and aggregated-by value") {
+    val definition =
+      aggregationComponent.contextTransformation(ValidationContext(), List(OutputVariableNameValue("out")))
+
+    val result = definition(
+      aggregationComponent.TransformationStep(
+        parameters = List(
+          TableAggregation.groupByParamName            -> DefinedLazyParameter(Typed[String]),
+          TableAggregation.aggregateByParamName        -> DefinedLazyParameter(Typed[String]),
+          TableAggregation.aggregatorFunctionParamName -> DefinedEagerParameter("Sum", Typed[String]),
+        ),
+        state = None
+      )
+    )
+    inside(result.errors) { case CustomNodeError(_, _, Some(TableAggregation.aggregateByParamName)) :: Nil =>
+    }
+  }
+
+  // TODO: after adding more aggregators do a table based test for these cases here
+  test("should returns correct types for valid parameters") {
+    val outVariableName = "out"
+    val definition =
+      aggregationComponent.contextTransformation(ValidationContext(), List(OutputVariableNameValue(outVariableName)))
+
+    val result = definition(
+      aggregationComponent.TransformationStep(
+        parameters = List(
+          TableAggregation.groupByParamName            -> DefinedLazyParameter(Typed[String]),
+          TableAggregation.aggregateByParamName        -> DefinedLazyParameter(Typed[Int]),
+          TableAggregation.aggregatorFunctionParamName -> DefinedEagerParameter("Sum", Typed[String]),
+        ),
+        state = None
+      )
+    )
+
+    inside(result) { case aggregationComponent.FinalResults(finalContext, Nil, _) =>
+      finalContext shouldBe ValidationContext(
+        Map(
+          outVariableName                   -> Typed[Number],
+          VariableConstants.KeyVariableName -> Typed[String]
+        )
+      )
+    }
+
+  }
+
+}
