ef173374d3bf2910cb2c78f8cd9cabb4e42abf6f
ForrestFairy <szymon.bogusz55@gmail.com>
2024-04-08 11:35:10 +0200

    [NU-1296] Ad-hoc tests with ververica (#5611)
    * With a new default parameter "isTest" in a couple of places ad-hoc test works without adding "flink-dropwizard-metrics-deps" to classpath
    
    * Removed 'isTest' param as we can use already present ComponentUseCase
    
    * One place was hardcoded to give dummy TestMetrics and this caused the test to fail, now it also matches on the ComponentUseCase
    Also some cleaning
    
    * Ad-hoc test goes through as everything gets dummy metrics in FlinkEngineRuntimeContextImpl
    Need to find a way to parse ComponentUseCase, so it still will be serializable on Flink
    
    * MetricsSpec passes with this change
    
    * Work in progress, works everywhere except for FlinkProcessRegistrar
    
    * FlinkProcessRegistrar now also works - there is no serialization error, parts of compilerData have to be initialized before using it in function
    
    * Getting MetricsProviderForScenario for FlinkEngineRuntimeContextImpl and SourceMetricsFunction is moved to MetricsProviderForFlink
    Also renamed FlinkEngineRuntimeContextImpl method to 'apply' as it is used to create new instance of it
    
    * Name changed as we create MetricProvider, not just get it from somewhere
    
    ---------
    
    Co-authored-by: Szymon Bogusz <sbg@touk.pl>

---
+++ engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/compiler/MetricsProviderForFlink.scala
@@ -0,0 +1,19 @@
+package pl.touk.nussknacker.engine.process.compiler
+
+import org.apache.flink.api.common.functions.RuntimeContext
+import pl.touk.nussknacker.engine.api.process.ComponentUseCase
+import pl.touk.nussknacker.engine.util.metrics.{MetricsProviderForScenario, NoOpMetricsProviderForScenario}
+
+object MetricsProviderForFlink {
+
+  def createMetricsProvider(
+      componentUseCase: ComponentUseCase,
+      runtimeContext: RuntimeContext
+  ): MetricsProviderForScenario = {
+    componentUseCase match {
+      case ComponentUseCase.TestRuntime => NoOpMetricsProviderForScenario
+      case _                            => new FlinkMetricsProviderForScenario(runtimeContext)
+    }
+  }
+
+}
--- engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/registrar/SourceMetricsFunction.scala
+++ engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/registrar/SourceMetricsFunction.scala
@@ -3,16 +3,18 @@ package pl.touk.nussknacker.engine.process.registrar
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.util.Collector
-import pl.touk.nussknacker.engine.process.compiler.FlinkMetricsProviderForScenario
+import pl.touk.nussknacker.engine.api.process.ComponentUseCase
+import pl.touk.nussknacker.engine.process.compiler.MetricsProviderForFlink.createMetricsProvider
 import pl.touk.nussknacker.engine.util.metrics.common.OneSourceMetrics
 
-private[registrar] class SourceMetricsFunction[T](sourceId: String) extends ProcessFunction[T, T] {
+private[registrar] class SourceMetricsFunction[T](sourceId: String, componentUseCase: ComponentUseCase)
+    extends ProcessFunction[T, T] {
 
   @transient private var metrics: OneSourceMetrics = _
 
   override def open(parameters: Configuration): Unit = {
     metrics = new OneSourceMetrics(sourceId)
-    val metricsProvider = new FlinkMetricsProviderForScenario(getRuntimeContext)
+    val metricsProvider = createMetricsProvider(componentUseCase, getRuntimeContext)
     metrics.registerOwnMetrics(metricsProvider)
   }
 
