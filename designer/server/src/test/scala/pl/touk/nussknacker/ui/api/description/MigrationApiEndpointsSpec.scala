7d4c10fb6988ec2feac8055c7be11d800254b739
Piotr Rudnicki <prn@touk.pl>
2024-04-09 10:12:33 +0200

    change to AnyFreeSpecLike in reflection based tests

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/description/MigrationApiEndpointsSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/description/MigrationApiEndpointsSpec.scala
@@ -1,22 +1,25 @@
 package pl.touk.nussknacker.ui.api.description
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 import pl.touk.nussknacker.test.utils.domain.ReflectionBasedUtils.findSubclassesOf
 import pl.touk.nussknacker.test.utils.domain.migration.MigrationUtils.allSubclassesContainVersionField
 import pl.touk.nussknacker.ui.api.description.MigrationApiEndpoints.Dtos.MigrateScenarioRequestDto
 
-class MigrationApiEndpointsSpec extends AnyFlatSpec with Matchers {
+class MigrationApiEndpointsSpec extends AnyFreeSpecLike with Matchers {
 
-  it should "all subclasses of MigrateScenarioRequestDto contain version field" in {
-    val subClasses = findSubclassesOf[MigrateScenarioRequestDto](
-      classOf[MigrateScenarioRequestDto],
-      "pl.touk.nussknacker.ui.api.description"
-    )
-    val actual   = allSubclassesContainVersionField(subClasses)
-    val expected = subClasses.map(clazz => (clazz.getName, true))
+  "all subclasses of MigrateScenarioRequestDto" - {
+    "contain version field" in {
+      val subClasses = findSubclassesOf[MigrateScenarioRequestDto](
+        classOf[MigrateScenarioRequestDto],
+        "pl.touk.nussknacker.ui.api.description"
+      )
+      val actual   = allSubclassesContainVersionField(subClasses)
+      val expected = subClasses.map(clazz => (clazz.getName, true))
 
-    actual shouldBe expected
+      actual shouldBe expected
+    }
+
   }
 
 }
--- designer/server/src/test/scala/pl/touk/nussknacker/ui/migrations/MigrateScenarioRequestSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/migrations/MigrateScenarioRequestSpec.scala
@@ -1,21 +1,24 @@
 package pl.touk.nussknacker.ui.migrations
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 import pl.touk.nussknacker.test.utils.domain.ReflectionBasedUtils.findSubclassesOf
 import pl.touk.nussknacker.test.utils.domain.migration.MigrationUtils.allSubclassesContainVersionField
 
-class MigrateScenarioRequestSpec extends AnyFlatSpec with Matchers {
+class MigrateScenarioRequestSpec extends AnyFreeSpecLike with Matchers {
 
-  it should "all subclasses of MigrateScenarioRequest contain version field" in {
-    val subClasses = findSubclassesOf[MigrateScenarioRequest](
-      classOf[MigrateScenarioRequest],
-      "pl.touk.nussknacker.ui.migrations"
-    )
-    val actual   = allSubclassesContainVersionField(subClasses)
-    val expected = subClasses.map(clazz => (clazz.getName, true))
+  "all subclasses of MigrateScenarioRequestDto" - {
+    "contain version field" in {
+      val subClasses = findSubclassesOf[MigrateScenarioRequest](
+        classOf[MigrateScenarioRequest],
+        "pl.touk.nussknacker.ui.api.description"
+      )
+      val actual   = allSubclassesContainVersionField(subClasses)
+      val expected = subClasses.map(clazz => (clazz.getName, true))
 
-    actual shouldBe expected
+      actual shouldBe expected
+    }
+
   }
 
 }
