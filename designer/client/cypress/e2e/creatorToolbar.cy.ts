eca610561e0d90479c662b2a5f1f23dd09bdf962
Julian Wielga <j@touk.pl>
2024-04-10 14:38:01 +0200

    test fix

--- designer/client/cypress/e2e/creatorToolbar.cy.ts
+++ designer/client/cypress/e2e/creatorToolbar.cy.ts
@@ -1,3 +1,6 @@
+const width = 1440;
+const height = 900;
+
 describe("Creator toolbar", () => {
     const seed = "creator";
 
@@ -10,13 +13,27 @@ describe("Creator toolbar", () => {
     });
 
     beforeEach(() => {
-        cy.viewport("macbook-15");
+        cy.viewport(width, height);
         cy.visitNewProcess(seed).as("processName");
         cy.contains(/^Creator panel.*sources/i).as("toolbar");
     });
 
     it("should allow filtering", () => {
-        cy.get("@toolbar").should("be.visible").matchImage();
+        cy.get("@toolbar")
+            .should("be.visible")
+            .then((el) => {
+                cy.matchImage({
+                    // manual clip to fix wrong clipping of big invisible part
+                    screenshotConfig: {
+                        clip: {
+                            x: el.offset().left,
+                            y: el.offset().top,
+                            width: el.width(),
+                            height: Math.min(height, el.height()) - el.offset().top,
+                        },
+                    },
+                });
+            });
         cy.get("@toolbar").find("input").type("var");
         cy.get("@toolbar").matchImage();
     });
