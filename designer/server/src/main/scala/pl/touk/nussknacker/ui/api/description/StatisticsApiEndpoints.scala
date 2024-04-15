e10ab62e6e12e2b4467c95b393b356b38467e866
Łukasz Bigorajski <72501021+lukasz-bigorajski@users.noreply.github.com>
2024-04-15 13:16:16 +0200

    [NU-1524] Add endpoint to provide statistics URLs (#5843)

---
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/description/StatisticsApiEndpoints.scala
@@ -0,0 +1,93 @@
+package pl.touk.nussknacker.ui.api.description
+
+import derevo.circe.{decoder, encoder}
+import derevo.derive
+import pl.touk.nussknacker.restmodel.BaseEndpointDefinitions
+import pl.touk.nussknacker.restmodel.BaseEndpointDefinitions.SecuredEndpoint
+import pl.touk.nussknacker.security.AuthCredentials
+import pl.touk.nussknacker.ui.api.TapirCodecs
+import pl.touk.nussknacker.ui.api.description.StatisticsApiEndpoints.Dtos
+import sttp.model.StatusCode.{InternalServerError, Ok}
+import sttp.tapir.EndpointIO.Example
+import sttp.tapir.derevo.schema
+import sttp.tapir.json.circe.jsonBody
+import sttp.tapir._
+
+import java.net.{URI, URL}
+
+class StatisticsApiEndpoints(auth: EndpointInput[AuthCredentials]) extends BaseEndpointDefinitions {
+
+  import StatisticsApiEndpoints.Dtos._
+  import StatisticsApiEndpoints.Dtos.StatisticError._
+
+  lazy val statisticUsageEndpoint: SecuredEndpoint[Unit, StatisticError, StatisticUrlResponseDto, Any] =
+    baseNuApiEndpoint
+      .summary("Statistics URL service")
+      .tag("Statistics")
+      .get
+      .in("statistic" / "usage")
+      .out(
+        statusCode(Ok).and(
+          jsonBody[StatisticUrlResponseDto]
+            .example(
+              Example.of(
+                summary = Some("List of statistics URLs"),
+                value = StatisticUrlResponseDto(
+                  List(
+                    URI
+                      .create(
+                        "https://stats.nussknacker.io/?fingerprint=development&s_a=0&s_dm_c=0&s_dm_e=0&s_dm_f=" +
+                          "1&s_dm_l=0&s_f=0&s_pm_b=0&s_pm_rr=0&s_pm_s=1&s_s=1&source=sources&version=1.15.0-SNAPSHOT"
+                      )
+                      .toURL
+                  )
+                )
+              )
+            )
+        )
+      )
+      .errorOut(
+        oneOf[StatisticError](
+          oneOfVariantFromMatchType(
+            InternalServerError,
+            plainBody[StatisticError]
+              .example(
+                Example.of(
+                  summary = Some("Statistics generation failed."),
+                  value = StatisticError.InvalidURL
+                )
+              )
+          )
+        )
+      )
+      .withSecurity(auth)
+
+}
+
+object StatisticsApiEndpoints {
+
+  object Dtos {
+    import TapirCodecs.URLCodec._
+
+    @derive(encoder, decoder, schema)
+    final case class StatisticUrlResponseDto private (urls: List[URL])
+
+    sealed trait StatisticError
+
+    object StatisticError {
+      final case object InvalidURL extends StatisticError
+
+      private def deserializationException =
+        (ignored: Any) => throw new IllegalStateException("Deserializing errors is not supported.")
+
+      implicit val errorCodec: Codec[String, StatisticError, CodecFormat.TextPlain] = {
+        Codec.string.map(
+          Mapping.from[String, StatisticError](deserializationException)(_ => "Statistics generation failed.")
+        )
+      }
+
+    }
+
+  }
+
+}
