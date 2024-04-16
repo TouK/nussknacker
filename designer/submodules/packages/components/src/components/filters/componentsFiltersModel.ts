f633a75f42d263e7b3b7912d7afaf3195d9b01e0
Dawid Poliszak <dpl@touk.pl>
2024-04-16 10:12:09 +0200

    [Nu-1065] processing mode info on components table (#5853)
    * Nu-1065 processing mode info on components table
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dzuming <Dzuming@users.noreply.github.com>

--- designer/submodules/packages/components/src/components/filters/componentsFiltersModel.ts
+++ designer/submodules/packages/components/src/components/filters/componentsFiltersModel.ts
@@ -1,7 +1,10 @@
+import { ProcessingMode } from "../../scenarios/list/processingMode";
+
 export interface ComponentsFiltersModel {
     NAME?: string;
     GROUP?: string[];
     CATEGORY?: string[];
     SHOW_ARCHIVED?: boolean;
     USAGES?: number[];
+    PROCESSING_MODE: ProcessingMode[];
 }
