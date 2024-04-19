6ed8eba4acac63b39adabd34098de0c797f6ebd4
Adrian Matwiejuk <amt@touk.pl>
2024-04-19 10:47:18 +0200

    gracefully stop deployment actors (and wait for free names) on deployment manager close

--- engine/flink/management/periodic/src/main/scala/pl/touk/nussknacker/engine/management/periodic/Utils.scala
+++ engine/flink/management/periodic/src/main/scala/pl/touk/nussknacker/engine/management/periodic/Utils.scala
@@ -1,13 +1,72 @@
 package pl.touk.nussknacker.engine.management.periodic
 
+import akka.actor.{ActorNotFound, ActorPath, ActorRef, ActorSystem}
+import akka.pattern.{AskTimeoutException, gracefulStop}
 import com.typesafe.scalalogging.LazyLogging
 
+import java.util.concurrent.TimeoutException
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+
 object Utils extends LazyLogging {
+
+  private val GracefulStopTimeout         = 5 seconds
+  private val ActorResolutionTimeout      = 5 seconds
+  private val TotalActorResolutionTimeout = 16 seconds
 
   def runSafely(action: => Unit): Unit = try {
     action
   } catch {
     case t: Throwable => logger.error("Error occurred, but skipping it", t)
+  }
+
+  def gracefulStopActor(actorRef: ActorRef, actorSystem: ActorSystem): Unit = {
+    logger.info(s"Gracefully stopping $actorRef")
+
+    try {
+      Await.result(
+        gracefulStop(actorRef, GracefulStopTimeout),
+        GracefulStopTimeout + (1 second)
+      )
+    } catch {
+      case _: AskTimeoutException | _: TimeoutException =>
+        throw new IllegalStateException(s"Failed to gracefully stop actor $actorRef within timeout")
+    }
+
+    logger.info(s"Gracefully stopped $actorRef, waiting for it's name to be freed")
+
+    try {
+      Await.result(
+        waitUntilActorNameIsFree(actorRef.path, actorSystem),
+        TotalActorResolutionTimeout
+      )
+    } catch {
+      case _: TimeoutException =>
+        throw new IllegalStateException(s"Failed to free actor path ${actorRef.path} within timeout")
+    }
+
+    logger.info(s"$actorRef is stopped and it's name is free")
+  }
+
+  private def waitUntilActorNameIsFree(actorPath: ActorPath, actorSystem: ActorSystem): Future[Unit] = {
+    import actorSystem.dispatcher
+
+    def waitLoop(): Future[Unit] = {
+      val actorResolutionFuture =
+        actorSystem.actorSelection(actorPath).resolveOne(ActorResolutionTimeout).map(_ => false)
+
+      actorResolutionFuture
+        .recover { case _: ActorNotFound => true }
+        .flatMap {
+          case true  => Future.successful(())
+          case false =>
+            // name is still taken, retry until it's free
+            Thread.sleep(500)
+            waitLoop()
+        }
+    }
+
+    waitLoop()
   }
 
 }
---
+++ engine/flink/management/periodic/src/test/scala/pl/touk/nussknacker/engine/management/periodic/UtilsSpec.scala
@@ -0,0 +1,43 @@
+package pl.touk.nussknacker.engine.management.periodic
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.TestKit
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class UtilsSpec extends TestKit(ActorSystem("UtilsSpec")) with AnyWordSpecLike with Matchers with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "Utils" should {
+
+    "gracefully stop actor and free actor path" in {
+      class TestActor extends Actor {
+        override def receive: Receive = { case _ =>
+          ()
+        }
+      }
+      val actorName = "actorName"
+
+      val actorRef = system.actorOf(Props(new TestActor), actorName)
+
+      Utils.gracefulStopActor(actorRef, system)
+
+      // with normal system.stop(actorRef) or akka.pattern.gracefulStop this throws "actor name is not unique"
+      system.actorOf(Props(new TestActor), actorName)
+    }
+
+    "ignore exceptions inside runSafely block" in {
+      Utils.runSafely {
+        throw new RuntimeException("dummy")
+      }
+
+      1 shouldBe 1
+    }
+
+  }
+
+}
