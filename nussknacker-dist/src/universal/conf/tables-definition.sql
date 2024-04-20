35426e9c1f728ec9dd4955d31d027f8ca97490af
Mateusz Słabek <mslb@touk.pl>
2024-04-20 02:11:16 +0200

    add primitive type support

--- nussknacker-dist/src/universal/conf/tables-definition.sql
+++ nussknacker-dist/src/universal/conf/tables-definition.sql
@@ -1,8 +1,8 @@
 CREATE TABLE transactions
 (
     client_id STRING,
-    amount INT,
-    datetime DATE
+    amount    INT,
+    datetime  DATE
 ) WITH (
     'connector' = 'datagen'
 );
@@ -10,8 +10,31 @@ CREATE TABLE transactions
 CREATE TABLE transactions_summary
 (
     client_id STRING,
-    amount INT,
-    datetime DATE
+    amount    INT,
+    datetime  DATE
+) WITH (
+      'connector' = 'datagen'
+);
+
+CREATE TABLE all_types_generated
+(
+    `string`        STRING,
+    `boolean`       BOOLEAN,
+    `tinyInt`       TINYINT,
+    `smallInt`      SMALLINT,
+    `int`           INT,
+    `bigint`        BIGINT,
+    `float`         FLOAT,
+    `double`        DOUBLE,
+    `decimal`       DECIMAL,
+    `date`          DATE,
+    `time`          TIME,
+    `timestamp`     TIMESTAMP,
+    `timestampLtz`  TIMESTAMP_LTZ,
+    `row`           ROW<colStr STRING, colInt INT>,
+    `array`         ARRAY<STRING>,
+    `map`           MAP<STRING, STRING>,
+    `multiset`      MULTISET<STRING>
 ) WITH (
       'connector' = 'datagen'
 );
