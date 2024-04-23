fe85fb87403da8054df1f75ea7ed487f14244615
Arek Burdach <arek.burdach@gmail.com>
2024-04-23 15:11:36 +0200

    [NU-1611] Test showing that currently generated OpenAPI schema is invalid - examples don't match it (#5900)

--- utils/test-utils/src/main/scala/pl/touk/nussknacker/test/NuScalaTestAssertions.scala
+++ utils/test-utils/src/main/scala/pl/touk/nussknacker/test/NuScalaTestAssertions.scala
@@ -1,13 +1,13 @@
 package pl.touk.nussknacker.test
 
-import org.everit.json.schema.Schema
-import org.json.JSONObject
+import com.networknt.schema.{InputFormat, JsonSchema}
+import io.circe.Json
 import org.scalactic.source
 import org.scalatest.matchers.{MatchResult, Matcher}
 import org.scalatest.{Assertion, Assertions}
 
+import scala.jdk.CollectionConverters._
 import scala.reflect.ClassTag
-import scala.util.{Success, Try}
 
 trait NuScalaTestAssertions extends Assertions {
 
@@ -24,17 +24,21 @@ trait NuScalaTestAssertions extends Assertions {
     }
   }
 
-  class ValidateJsonMatcher(json: JSONObject) extends Matcher[Schema] {
+  class ValidateJsonMatcher(json: Json) extends Matcher[JsonSchema] {
 
-    override def apply(left: Schema): MatchResult =
+    override def apply(left: JsonSchema): MatchResult = {
+      val errors = left.validate(json.spaces2, InputFormat.JSON).asScala
       MatchResult(
-        Try(left.validate(json)) == Success(()),
-        s"JSON $json cannot be validated by schema $left",
-        s"JSON $json can be validated by schema $left"
+        errors.isEmpty,
+        errors
+          .map(_.getMessage)
+          .mkString(s"Validation of given JSON\n$json against given schema returned errors:\n", ",\n", ""),
+        s"Validation of given JSON\n$json against given schema returned no errors."
       )
+    }
 
   }
 
-  def validateJson(json: JSONObject): Matcher[Schema] = new ValidateJsonMatcher(json)
+  def validateJson(json: Json): Matcher[JsonSchema] = new ValidateJsonMatcher(json)
 
 }
