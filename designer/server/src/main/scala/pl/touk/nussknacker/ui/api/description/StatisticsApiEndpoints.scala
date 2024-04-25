2fb37a9c38336f2d5d4e6a6f080967868e5ad694
Łukasz Bigorajski <lbg@touk.pl>
2024-04-25 17:06:09 +0200

    [NU-1520] Add register statistics endpoint

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/description/StatisticsApiEndpoints.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/description/StatisticsApiEndpoints.scala
@@ -2,13 +2,16 @@ package pl.touk.nussknacker.ui.api.description
 
 import derevo.circe.{decoder, encoder}
 import derevo.derive
+import enumeratum.EnumEntry.UpperSnakecase
+import enumeratum.{CirceEnum, Enum, EnumEntry}
 import pl.touk.nussknacker.restmodel.BaseEndpointDefinitions
 import pl.touk.nussknacker.restmodel.BaseEndpointDefinitions.SecuredEndpoint
 import pl.touk.nussknacker.security.AuthCredentials
 import pl.touk.nussknacker.ui.api.TapirCodecs
-import sttp.model.StatusCode.{InternalServerError, Ok}
+import sttp.model.StatusCode.{InternalServerError, NoContent, Ok}
 import sttp.tapir.EndpointIO.Example
 import sttp.tapir._
+import sttp.tapir.codec.enumeratum._
 import sttp.tapir.derevo.schema
 import sttp.tapir.json.circe.jsonBody
 
@@ -16,7 +19,6 @@ import java.net.{URI, URL}
 
 class StatisticsApiEndpoints(auth: EndpointInput[AuthCredentials]) extends BaseEndpointDefinitions {
 
-  import StatisticsApiEndpoints.Dtos.StatisticError._
   import StatisticsApiEndpoints.Dtos._
 
   lazy val statisticUsageEndpoint: SecuredEndpoint[Unit, StatisticError, StatisticUrlResponseDto, Any] =
@@ -63,6 +65,15 @@ class StatisticsApiEndpoints(auth: EndpointInput[AuthCredentials]) extends BaseE
       )
       .withSecurity(auth)
 
+  lazy val registerStatisticsEndpoint: SecuredEndpoint[RegisterStatisticsRequestDto, Unit, Unit, Any] =
+    baseNuApiEndpoint
+      .summary("Register statistics service")
+      .tag("Statistics")
+      .post
+      .in("statistic" and jsonBody[RegisterStatisticsRequestDto])
+      .out(statusCode(NoContent))
+      .withSecurity(auth)
+
 }
 
 object StatisticsApiEndpoints {
@@ -72,6 +83,67 @@ object StatisticsApiEndpoints {
 
     @derive(encoder, decoder, schema)
     final case class StatisticUrlResponseDto private (urls: List[URL])
+
+    @derive(encoder, decoder, schema)
+    final case class RegisterStatisticsRequestDto private (statistics: List[StatisticDto])
+
+    @derive(encoder, decoder, schema)
+    final case class StatisticDto private (name: StatisticName, value: Long)
+
+    sealed trait StatisticName extends EnumEntry with UpperSnakecase
+
+    object StatisticName extends Enum[StatisticName] with CirceEnum[StatisticName] {
+      case object ScenarioFilter                         extends StatisticName
+      case object SearchScenario                         extends StatisticName
+      case object ComponentsList                         extends StatisticName
+      case object SearchComponents                       extends StatisticName
+      case object SearchComponentsWithGroup              extends StatisticName
+      case object SearchComponentsWithProcessingMode     extends StatisticName
+      case object SearchComponentsWithCategory           extends StatisticName
+      case object SearchComponentsWithMultipleCategories extends StatisticName
+      case object SearchComponentsWithUsages             extends StatisticName
+      case object SearchComponentsUsagesList             extends StatisticName
+      case object SearchComponentsUsagesListWithStatus   extends StatisticName
+      case object SearchComponentsUsagesListWithCategory extends StatisticName
+      case object SearchComponentsUsagesListWithAuthor   extends StatisticName
+      case object SearchComponentsUsagesListWithOther    extends StatisticName
+      case object GoToScenarioFromComponentsUsagesList   extends StatisticName
+      case object GlobalMetrics                          extends StatisticName
+      case object ActionDeploy                           extends StatisticName
+      case object ActionMetrics                          extends StatisticName
+      case object ViewZoomIn                             extends StatisticName
+      case object ViewReset                              extends StatisticName
+      case object EditUndo                               extends StatisticName
+      case object EditRedo                               extends StatisticName
+      case object EditCopy                               extends StatisticName
+      case object EditPaste                              extends StatisticName
+      case object EditDelete                             extends StatisticName
+      case object EditLayout                             extends StatisticName
+      case object ScenarioProperties                     extends StatisticName
+      case object ScenarioCompare                        extends StatisticName
+      case object ScenarioMigrate                        extends StatisticName
+      case object ScenarioImport                         extends StatisticName
+      case object ScenarioJson                           extends StatisticName
+      case object ScenarioPdf                            extends StatisticName
+      case object ScenarioArchive                        extends StatisticName
+      case object TestGenerated                          extends StatisticName
+      case object TestAdhoc                              extends StatisticName
+      case object TestFromFile                           extends StatisticName
+      case object TestGenerateFile                       extends StatisticName
+      case object TestHide                               extends StatisticName
+      case object MoreScenarioDetails                    extends StatisticName
+      case object RollUpPanel                            extends StatisticName
+      case object ExpandPanel                            extends StatisticName
+      case object MovePanel                              extends StatisticName
+      case object SearchNodesInScenario                  extends StatisticName
+      case object SearchComponentsInScenario             extends StatisticName
+      case object SwitchToOlderVersion                   extends StatisticName
+      case object SwitchToNewerVersion                   extends StatisticName
+      case object FiredKeyStroke                         extends StatisticName
+      case object NodeDocumentation                      extends StatisticName
+
+      override def values = findValues
+    }
 
     sealed trait StatisticError
     final case object CannotGenerateStatisticError extends StatisticError
