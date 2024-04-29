8a2d322fd0ae2284485426a1e596a25d77f27215
Dawid Poliszak <dpl@touk.pl>
2024-04-29 12:04:19 +0200

    NU-1641 fix scenario name in scenario details

--- designer/client/webpack.config.ts
+++ designer/client/webpack.config.ts
@@ -149,7 +149,7 @@ const config: Configuration = {
             //see ./config.ts
             base: isProd ? "__publicPath__/static/" : "/",
             filename: "main.html",
-            favicon: "src/assets/img/favicon.svg",
+            favicon: "src/assets/img/nuLogoIcon.svg",
             meta: {
                 viewport: "user-scalable = no",
             },
