1859daed722ec208ea4c1113be58fed423594ab9
Mateusz Słabek <mslb@touk.pl>
2024-05-07 15:38:15 +0200

    wip

--- nussknacker-dist/src/universal/conf/tables-definition.sql
+++ nussknacker-dist/src/universal/conf/tables-definition.sql
@@ -1,40 +1,45 @@
 CREATE TABLE transactions
 (
+    datetime  TIMESTAMP,
     client_id STRING,
-    amount    INT,
-    datetime  DATE
+    amount    DECIMAL(15, 2),
+    `date`    STRING
 ) WITH (
-    'connector' = 'datagen'
+      'connector' = 'datagen',
+      'number-of-rows' = '1000'
 );
 
 CREATE TABLE transactions_summary
 (
     client_id STRING,
-    amount    INT,
-    datetime  DATE
+    amount    DECIMAL(15, 2),
+    `date`    STRING
 ) WITH (
-      'connector' = 'datagen'
+      'connector' = 'blackhole'
 );
 
 CREATE TABLE all_types_generated
 (
-    `string`        STRING,
-    `boolean`       BOOLEAN,
-    `tinyInt`       TINYINT,
-    `smallInt`      SMALLINT,
-    `int`           INT,
-    `bigint`        BIGINT,
-    `float`         FLOAT,
-    `double`        DOUBLE,
-    `decimal`       DECIMAL,
-    `date`          DATE,
-    `time`          TIME,
-    `timestamp`     TIMESTAMP,
-    `timestampLtz`  TIMESTAMP_LTZ,
-    `row`           ROW<colStr STRING, colInt INT>,
-    `array`         ARRAY<STRING>,
-    `map`           MAP<STRING, STRING>,
-    `multiset`      MULTISET<STRING>
+    `string`       STRING,
+    `boolean`      BOOLEAN,
+    `tinyInt`      TINYINT,
+    `smallInt`     SMALLINT,
+    `int`          INT,
+    `bigint`       BIGINT,
+    `float`        FLOAT,
+    `double` DOUBLE,
+    `decimal`      DECIMAL,
+    `date`         DATE,
+    `time`         TIME,
+    `timestamp`    TIMESTAMP,
+    `timestampLtz` TIMESTAMP_LTZ,
+    `row`          ROW<colStr STRING,
+    colInt         INT>,
+    `array`        ARRAY<STRING>,
+    `map`          MAP<STRING,
+    STRING>,
+    `multiset`     MULTISET<STRING>
 ) WITH (
-      'connector' = 'datagen'
+      'connector' = 'datagen',
+      'number-of-rows' = '1000'
 );
