2a3c5c4c746238de34e0a900f12eddce93680f30
Łukasz Jędrzejewski <ljd@touk.pl>
2024-05-09 13:48:41 +0200

    Review

--- utils/components-utils/src/main/scala/pl/touk/nussknacker/engine/util/parameters/TestingParametersSupport.scala
+++ utils/components-utils/src/main/scala/pl/touk/nussknacker/engine/util/parameters/TestingParametersSupport.scala
@@ -9,15 +9,15 @@ object TestingParametersSupport {
   def joinWithDelimiter(key1: String, key2: String) = s"$key1$delimiter$key2"
 
   // Used to un-flat map with concat name eg. { a.b -> _ } => { a -> { b -> _ } }. Reverse kinda joinWithDelimiter
-  def unflattenParameters(flatMap: Map[ParameterName, AnyRef]): Map[String, AnyRef] = {
-    unflattenParametersInternal(flatMap.map { case (ParameterName(name), value) => name -> value })
+  def unflattenParameters(flatParameters: Map[ParameterName, AnyRef]): Map[String, AnyRef] = {
+    unflattenParametersInternal(flatParameters.map { case (ParameterName(name), value) => name -> value })
   }
 
-  private def unflattenParametersInternal(flatMap: Map[String, AnyRef]): Map[String, AnyRef] = {
-    flatMap
+  private def unflattenParametersInternal(flatParameters: Map[String, AnyRef]): Map[String, AnyRef] = {
+    flatParameters
       .foldLeft(Map.empty[String, AnyRef]) { case (result, (key, value)) =>
         if (key.contains(delimiter)) {
-          unflattenParameter(key, flatMap, result)
+          unflattenParameter(key, flatParameters, result)
         } else {
           result + (key -> value)
         }
@@ -26,7 +26,7 @@ object TestingParametersSupport {
 
   private def unflattenParameter(
       key: String,
-      flatMap: Map[String, AnyRef],
+      flatParameters: Map[String, AnyRef],
       result: Map[String, AnyRef]
   ): Map[String, AnyRef] = {
     val parentKey = key.takeWhile(_ != delimiter)
@@ -35,7 +35,7 @@ object TestingParametersSupport {
         // parentKey was already processed because some of the previous parameters starts with the parentKey
         result
       case None =>
-        val childParameters = flatMap.collect {
+        val childParameters = flatParameters.collect {
           case (key, value) if key.startsWith(parentKey) =>
             val childKey = key.stripPrefix(s"$parentKey$delimiter")
             childKey -> value
