c3fa583e1b6dcf2c14eab8f118fbb50cd2bfabbf
Dawid Poliszak <dpl@touk.pl>
2024-05-23 09:38:48 +0200

    NU-974 remove redundant screenshot

--- designer/client/cypress/e2e/fragment.cy.ts
+++ designer/client/cypress/e2e/fragment.cy.ts
@@ -299,9 +299,6 @@ describe("Fragment", () => {
 
         cy.get("[data-testid=window]").find("section").scrollTo("top");
         cy.get("[data-testid=window]").find('[data-testid="content-size"]').matchImage();
-
-        cy.get("[data-testid=window]").find("section").scrollTo("bottom");
-        cy.get("[data-testid=window]").find('[id="nk-graph-fragment"]').matchImage();
     });
 
     it("should open properties", () => {
