5e96ff397bf5a68112eee530a5ad7be5d6f16926
Julian Wielga <j@touk.pl>
2024-05-24 22:49:59 +0200

    fix and cleanup of graph/paper styles

---
+++ designer/client/src/components/graph/styledDiv.tsx
@@ -0,0 +1,5 @@
+import { styled } from "@mui/material";
+import { PropsOf } from "@emotion/react";
+
+export type StyledDivProps = PropsOf<typeof StyledDiv>;
+export const StyledDiv = styled("div")({});
