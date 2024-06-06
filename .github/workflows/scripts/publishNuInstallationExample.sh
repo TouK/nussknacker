7020ab906f4bc5fcf9711c307c50c9527cc98cec
MK Software <mk_software_pl@protonmail.com>
2024-06-06 13:00:03 +0200

    wip

--- .github/workflows/scripts/publishNuInstallationExample.sh
+++ .github/workflows/scripts/publishNuInstallationExample.sh
@@ -5,6 +5,11 @@ if [ -z "$ACCESS_TOKEN" ]; then
     exit 1
 fi
 
+if [ -z "$NUSSKNACKER_VERSION" ]; then
+    echo "NUSSKNACKER_VERSION variable has to be defined"
+    exit 1
+fi
+
 rm -rf nu-installation-example-repo
 git clone "https://$ACCESS_TOKEN@github.com/TouK/nussknacker-installation-example.git" nu-installation-example-repo
 
@@ -18,6 +23,6 @@ git config user.email "actions@github.com"
 git config user.name "GitHub Actions"
 git add .
 # todo: version in the commit message
-git commit -m "Push $FOLDER_TO_PUSH from source repository"
-git push -f origin master
-# todo: tag
+git commit -m "Push $NUSSKNACKER_VERSION from source repository"
+git tag "$NUSSKNACKER_VERSION"
+git push -f origin master --tags
