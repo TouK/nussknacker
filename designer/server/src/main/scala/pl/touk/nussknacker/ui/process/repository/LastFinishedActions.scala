85a8c98d19b7964b5e3339e5a419f4f3cff44a5f
Adrian Matwiejuk <amt@touk.pl>
2024-06-07 11:26:05 +0200

    crosscompile fix

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/repository/LastFinishedActions.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/repository/LastFinishedActions.scala
@@ -1,18 +1,18 @@
 package pl.touk.nussknacker.ui.process.repository
 
 import pl.touk.nussknacker.engine.api.deployment.{ProcessAction, ProcessActionState, ScenarioActionName}
+import pl.touk.nussknacker.engine.util.SeqUtil.maxByOption
 
 case class LastFinishedActions(
     private val lastFinishedActions: Seq[ProcessAction]
 ) {
-  val lastFinishedAction: Option[ProcessAction] = lastFinishedActions.maxByOption(_.createdAt)
+
+  val lastFinishedAction: Option[ProcessAction] = maxByOption(lastFinishedActions)(_.createdAt)
 
   val lastFinishedStateAction: Option[ProcessAction] =
-    lastFinishedActions
-      .filter { action =>
-        ScenarioActionName.StateActions.contains(action.actionName)
-      }
-      .maxByOption(_.createdAt)
+    maxByOption(
+      lastFinishedActions.filter { action => ScenarioActionName.StateActions.contains(action.actionName) }
+    )(_.createdAt)
 
   // For last deploy action we are interested in Deploys that are Finished (not ExecutionFinished) and that are not Cancelled
   // so that the presence of such an action means that the process is currently deployed
