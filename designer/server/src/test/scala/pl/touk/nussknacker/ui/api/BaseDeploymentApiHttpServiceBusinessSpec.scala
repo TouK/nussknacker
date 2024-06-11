9861f60a0fdaeb03a5ba1229d04b002fd14ef849
Arek Burdach <arek.burdach@gmail.com>
2024-06-11 15:28:00 +0200

    tests fix

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/BaseDeploymentApiHttpServiceBusinessSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/BaseDeploymentApiHttpServiceBusinessSpec.scala
@@ -129,7 +129,7 @@ trait BaseDeploymentApiHttpServiceBusinessSpec extends WithFlinkContainersDeploy
       requestedDeploymentId: DeploymentId,
       expectedStatusName: DeploymentStatusName
   ): Unit = {
-    eventually(Timeout(Span(60, Seconds)), Interval(Span(2, Seconds))) {
+    eventually(Timeout(Span(120, Seconds)), Interval(Span(3, Seconds))) {
       checkDeploymentStatusNameMatches(requestedDeploymentId, expectedStatusName)
     }
   }
