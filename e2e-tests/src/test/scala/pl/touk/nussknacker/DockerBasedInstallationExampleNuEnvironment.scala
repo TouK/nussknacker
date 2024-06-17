5bcd5777bd38d316c533559eac1ab4c37c153932
MK Software <mk_software_pl@protonmail.com>
2024-06-17 12:10:29 +0200

    wip

--- e2e-tests/src/test/scala/pl/touk/nussknacker/DockerBasedInstallationExampleNuEnvironment.scala
+++ e2e-tests/src/test/scala/pl/touk/nussknacker/DockerBasedInstallationExampleNuEnvironment.scala
@@ -3,30 +3,23 @@ package pl.touk.nussknacker
 import better.files._
 import com.dimafeng.testcontainers._
 import com.typesafe.scalalogging.LazyLogging
-import org.scalatest.Suite
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
 import pl.touk.nussknacker.ContainerExt._
+import pl.touk.nussknacker.DockerBasedInstallationExampleNuEnvironment.JSON
 import ujson.Value
 
 import java.io.{File => JFile}
 
-// todo: singleton container
-trait NuDockerBasedInstallationExample extends ForAllTestContainer with LazyLogging {
+trait DockerBasedInstallationExampleNuEnvironment
+    extends ForAllTestContainer
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with LazyLogging {
   this: Suite =>
 
-  override val container: DockerComposeContainer = new DockerComposeContainer(
-    composeFiles = Seq(
-      new JFile("examples/installation/docker-compose.yml"),
-      new JFile(Resource.getUrl("spec-setup/docker-compose.override.yml").toURI)
-    ),
-    logConsumers = Seq(
-      ServiceLogConsumer("spec-setup", new Slf4jLogConsumer(logger.underlying))
-    ),
-    waitingFor = Some(
-      WaitingForService("spec-setup", new LogMessageWaitStrategy().withRegEx("^Setup done!.*"))
-    )
-  )
+  override val container: DockerComposeContainer = DockerBasedInstallationExampleNuEnvironment.singletonContainer
 
   private lazy val specSetupService = unsafeContainerByServiceName("spec-setup")
 
@@ -43,14 +36,12 @@ trait NuDockerBasedInstallationExample extends ForAllTestContainer with LazyLogg
     )
   }
 
-  type JSON = Value
-
   def sendMessageToKafka(topic: String, message: JSON): Unit = {
     val escapedMessage = message.render().replaceAll("\"", "\\\\\"")
     specSetupService.executeBash(s"""/app/scripts/utils/kafka/send-to-topic.sh "$topic" "$escapedMessage" """)
   }
 
-  def readMessagesFromKafka(topic: String): List[JSON] = {
+  def readAllMessagesFromKafka(topic: String): List[JSON] = {
     val stdout = specSetupService.executeBashAndReadStdout(s"""/app/scripts/utils/kafka/read-from-topic.sh "$topic" """)
     stdout
       .split("\n")
@@ -58,8 +49,32 @@ trait NuDockerBasedInstallationExample extends ForAllTestContainer with LazyLogg
       .map(ujson.read(_))
   }
 
+  def purgeKafkaTopic(topic: String): Unit = {
+    specSetupService.executeBash(s"""/app/scripts/utils/kafka/purge-topic.sh "$topic" """)
+  }
+
   private def unsafeContainerByServiceName(name: String) = container
     .getContainerByServiceName(name)
     .getOrElse(throw new IllegalStateException(s"'$name' service not available!"))
+
+}
+
+object DockerBasedInstallationExampleNuEnvironment extends LazyLogging {
+
+  type JSON = Value
+
+  val singletonContainer = new DockerComposeContainer(
+    composeFiles = Seq(
+      new JFile("examples/installation/docker-compose.yml"),
+      new JFile(Resource.getUrl("spec-setup/spec-setup.override.yml").toURI),
+      new JFile(Resource.getUrl("spec-setup/debuggable-nu-designer.override.yml").toURI)
+    ),
+    logConsumers = Seq(
+      ServiceLogConsumer("spec-setup", new Slf4jLogConsumer(logger.underlying))
+    ),
+    waitingFor = Some(
+      WaitingForService("spec-setup", new LogMessageWaitStrategy().withRegEx("^Setup done!.*"))
+    )
+  )
 
 }
---
+++ e2e-tests/src/test/scala/pl/touk/nussknacker/InstallationExampleSpecNuEnvironment.scala
@@ -0,0 +1,54 @@
+package pl.touk.nussknacker
+
+import better.files._
+import org.scalatest.freespec.AnyFreeSpecLike
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.test.VeryPatientScalaFutures
+
+class InstallationExampleSpecNuEnvironment
+    extends AnyFreeSpecLike
+    with DockerBasedInstallationExampleNuEnvironment
+    with Matchers
+    with VeryPatientScalaFutures {
+
+  override def afterStart(): Unit = {
+    super.afterStart()
+    val scenarioName = "InstallationExampleDetectLargeTransactions"
+    loadFlinkStreamingScenarioFromResource(
+      scenarioName = scenarioName,
+      scenarioJsonFile = File(getClass.getResource(s"/scenarios/$scenarioName.json"))
+    )
+    deployAndWaitForRunningState(scenarioName)
+  }
+
+  "Make sure that the installation example is functional" in {
+    val smallAmountTransactions = List(
+      transactionJson(amount = 1),
+      transactionJson(amount = 2),
+      transactionJson(amount = 3),
+    )
+    val largeAmountTransactions = List(
+      transactionJson(amount = 100),
+      transactionJson(amount = 1000),
+      transactionJson(amount = 10000),
+    )
+
+    (smallAmountTransactions ::: largeAmountTransactions).foreach { transaction =>
+      sendMessageToKafka("transactions", transaction)
+    }
+
+    eventually {
+      val processedTransactions = readAllMessagesFromKafka("processedEvents")
+      processedTransactions should equal(largeAmountTransactions)
+    }
+  }
+
+  override protected def afterEach(): Unit = {
+    purgeKafkaTopic("transactions")
+    purgeKafkaTopic("processedEvents")
+    super.afterEach()
+  }
+
+  private def transactionJson(amount: Int) =
+    ujson.read(s"""{ "clientId": "100", "amount":$amount, "isLast":false }""")
+}
