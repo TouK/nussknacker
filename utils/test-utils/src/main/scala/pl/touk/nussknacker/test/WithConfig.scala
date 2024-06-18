85de74dce34ac8e0b597b7d74bcec310e64dfec5
Łukasz Ciołecki <ciolecki.lukasz@gmail.com>
2024-06-18 10:19:24 +0200

    Improvement: Load only test resource file config context at WithConfig

--- utils/test-utils/src/main/scala/pl/touk/nussknacker/test/WithConfig.scala
+++ utils/test-utils/src/main/scala/pl/touk/nussknacker/test/WithConfig.scala
@@ -1,10 +1,19 @@
 package pl.touk.nussknacker.test
 
 import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.commons.io.IOUtils
+
+import java.nio.charset.StandardCharsets
 
 trait WithConfig {
 
-  protected lazy val config: Config = resolveConfig(ConfigFactory.load())
+  protected val configFilename: String = "application.conf"
+
+  protected lazy val config: Config = {
+    val resource = IOUtils.resourceToString(s"/$configFilename", StandardCharsets.UTF_8)
+    val config   = ConfigFactory.parseString(resource)
+    resolveConfig(config.resolve())
+  }
 
   protected def resolveConfig(config: Config): Config = config
 }
