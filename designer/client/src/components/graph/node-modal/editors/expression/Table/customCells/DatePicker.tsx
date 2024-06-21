633ab9269678600960fccdac33e2fb09a8756abf
Dawid Poliszak <dpl@touk.pl>
2024-06-21 10:22:40 +0200

    NU-1710 fix date picker visibility on table editor

--- designer/client/src/components/graph/node-modal/editors/expression/Table/customCells/DatePicker.tsx
+++ designer/client/src/components/graph/node-modal/editors/expression/Table/customCells/DatePicker.tsx
@@ -4,6 +4,7 @@ import moment from "moment";
 import { DTPicker } from "../../../../../../common/DTPicker";
 import { SupportedType } from "../TableEditor";
 import { CustomCell, EditableGridCell } from "@glideapps/glide-data-grid";
+import { useWindowSize } from "rooks";
 
 export type DatePickerCellData = { date: string; kind: "date-picker-cell"; format: SupportedType };
 export type DatePickerCell = CustomCell<DatePickerCellData>;
@@ -16,15 +17,38 @@ const MOMENT_FORMAT = `${DATE_FORMAT} ${TIME_FORMAT}`;
 const DATE_TIME_DISPLAY_FORMAT = `${DATE_FORMAT}T${TIME_FORMAT}`;
 
 export const DatePicker: ProvideEditorComponent<DatePickerCell> = (props) => {
+    const { innerHeight } = useWindowSize();
     const { value, onChange, target } = props;
 
     const formattedValue = useMemo(() => (value.data?.date ? moment(value.data?.date, MOMENT_FORMAT) : null), [value.data?.date]);
+
+    const datePickerTopPosition = useMemo(() => {
+        const datePickerHeight = 245.5;
+        const dateTimePickerHeight = 275.5;
+        const pickerHeight = value.data.format === "java.time.LocalDateTime" ? dateTimePickerHeight : datePickerHeight;
+        const pickerPadding = 10;
+
+        const availablePositions = {
+            bottom: target.y + target.height + pickerPadding,
+            top: target.y - target.height + pickerPadding - pickerHeight,
+        };
+
+        return innerHeight <= target.y + pickerHeight + target.height + pickerPadding * 2
+            ? availablePositions.top
+            : availablePositions.bottom;
+    }, [innerHeight, target.height, target.y, value.data.format]);
+
     return (
         <DTPicker
             inputProps={{
                 style: { minWidth: target.width, minHeight: target.height, padding: 0 },
                 value: value.data.date,
+                autoFocus: true,
+                onFocus: (event) => {
+                    event.target.select();
+                },
             }}
+            sx={{ ".rdtPicker": { top: datePickerTopPosition } }}
             timeFormat={value.data.format === "java.time.LocalDateTime" ? TIME_FORMAT : null}
             dateFormat={DATE_FORMAT}
             open={true}
