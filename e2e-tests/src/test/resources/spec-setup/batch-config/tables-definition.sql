e8d5e833a435ca250164e2b02ffc3a28f67f8922
Mateusz Słabek <mslb@touk.pl>
2024-06-28 11:56:59 +0200

    clean up tests

--- e2e-tests/src/test/resources/spec-setup/batch-config/tables-definition.sql
+++ e2e-tests/src/test/resources/spec-setup/batch-config/tables-definition.sql
@@ -3,7 +3,7 @@ CREATE TABLE transactions (
     client_id STRING,
     amount    DECIMAL(15, 2),
     `date`    STRING
-) PARTITIONED BY (`date`) WITH (
+) WITH (
       'connector' = 'filesystem',
       'path' = 'file:///opt/flink/data/transactions-data/transactions',
       'format' = 'csv'
