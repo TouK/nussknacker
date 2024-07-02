fe844a8c73471b7002b4f68ed0af796ca70c1803
mkl <158037696+mk-software-pl@users.noreply.github.com>
2024-07-02 13:57:16 +0200

    Revert "[NU-1792] The "Old" API should be used to deploy and check deployment…"
    This reverts commit 281479048b032de8ac444c364c8d5974c10fd92f.

--- e2e-tests/src/test/resources/spec-setup/scripts/utils/nu/deploy-scenario-and-wait-for-running-state.sh
+++ e2e-tests/src/test/resources/spec-setup/scripts/utils/nu/deploy-scenario-and-wait-for-running-state.sh
@@ -12,24 +12,32 @@ TIMEOUT_SECONDS=${2:-60}
 WAIT_INTERVAL=5
 
 function deployScenario() {
-  if [ "$#" -ne 1 ]; then
-      echo "Error: One parameter required: 1) scenario name"
+  if [ "$#" -ne 2 ]; then
+      echo "Error: Two parameters required: 1) scenario name, 2) deployment UUID"
       exit 11
   fi
 
   set -e
 
   local SCENARIO_NAME=$1
+  local DEPLOYMENT_ID=$2
+
+  local REQUEST_BODY="{
+    \"scenarioName\": \"$SCENARIO_NAME\",
+    \"nodesDeploymentData\": {},
+    \"comment\": \"\"
+  }"
 
   local RESPONSE
   RESPONSE=$(curl -s -L -w "\n%{http_code}" -u admin:admin \
-    -X POST "http://nginx:8080/api/processManagement/deploy/$SCENARIO_NAME"
+    -X PUT "http://nginx:8080/api/deployments/$DEPLOYMENT_ID" \
+    -H "Content-Type: application/json" -d "$REQUEST_BODY"
   )
 
   local HTTP_STATUS
   HTTP_STATUS=$(echo "$RESPONSE" | tail -n 1)
 
-  if [ "$HTTP_STATUS" != "200" ]; then
+  if [ "$HTTP_STATUS" != "202" ]; then
     local RESPONSE_BODY
     RESPONSE_BODY=$(echo "$RESPONSE" | sed \$d)
     echo -e "Error: Cannot run scenario $SCENARIO_NAME deployment.\nHTTP status: $HTTP_STATUS, response body: $RESPONSE_BODY"
@@ -41,17 +49,17 @@ function deployScenario() {
 
 function checkDeploymentStatus() {
   if [ "$#" -ne 1 ]; then
-    echo "Error: One parameter required: 1) scenario name"
+    echo "Error: One parameter required: 1) deployment UUID"
     exit 21
   fi
 
   set -e
 
-  local SCENARIO_NAME=$1
+  local DEPLOYMENT_ID=$1
 
   local RESPONSE
   RESPONSE=$(curl -s -L -w "\n%{http_code}" -u admin:admin \
-    -X GET "http://nginx:8080/api/processes/$SCENARIO_NAME/status"
+    -X GET "http://nginx:8080/api/deployments/$DEPLOYMENT_ID/status"
   )
 
   local HTTP_STATUS
@@ -60,12 +68,12 @@ function checkDeploymentStatus() {
   RESPONSE_BODY=$(echo "$RESPONSE" | sed \$d)
 
   if [ "$HTTP_STATUS" != "200" ]; then
-    echo -e "Error: Cannot check scenario $SCENARIO_NAME deployment status.\nHTTP status: $HTTP_STATUS, response body: $RESPONSE_BODY"
+    echo -e "Error: Cannot check deployment $DEPLOYMENT_ID status.\nHTTP status: $HTTP_STATUS, response body: $RESPONSE_BODY"
     exit 22
   fi
 
   local SCENARIO_STATUS
-  SCENARIO_STATUS=$(echo "$RESPONSE_BODY" | jq -r '.status.name')
+  SCENARIO_STATUS=$(echo "$RESPONSE_BODY" | jq -r '.name')
   echo "$SCENARIO_STATUS"
 }
 
@@ -74,10 +82,11 @@ echo "Deploying scenario $SCENARIO_NAME ..."
 START_TIME=$(date +%s)
 END_TIME=$((START_TIME + TIMEOUT_SECONDS))
 
-deployScenario "$SCENARIO_NAME"
+DEPLOYMENT_ID=$(uuidgen)
+deployScenario "$SCENARIO_NAME" "$DEPLOYMENT_ID"
 
 while true; do
-  DEPLOYMENT_STATUS=$(checkDeploymentStatus "$SCENARIO_NAME")
+  DEPLOYMENT_STATUS=$(checkDeploymentStatus "$DEPLOYMENT_ID")
 
   if [ "$DEPLOYMENT_STATUS" == "RUNNING" ]; then
     break
