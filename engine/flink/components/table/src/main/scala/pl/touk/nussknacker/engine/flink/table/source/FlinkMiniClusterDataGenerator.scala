16cd15afe2709de9511a47cfcb36081e724bc97e
Mateusz Słabek <mslb@touk.pl>
2024-07-04 10:19:15 +0200

    refactor into smaller methods

--- engine/flink/components/table/src/main/scala/pl/touk/nussknacker/engine/flink/table/source/FlinkMiniClusterDataGenerator.scala
+++ engine/flink/components/table/src/main/scala/pl/touk/nussknacker/engine/flink/table/source/FlinkMiniClusterDataGenerator.scala
@@ -1,5 +1,6 @@
 package pl.touk.nussknacker.engine.flink.table.source
 
+import cats.implicits._
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.parser.parse
 import org.apache.commons.io.FileUtils
@@ -9,71 +10,80 @@ import pl.touk.nussknacker.engine.api.test.{TestData, TestRecord}
 import pl.touk.nussknacker.engine.flink.table.source.FlinkMiniClusterDataGenerator._
 import pl.touk.nussknacker.engine.util.ThreadUtils
 
+import java.net.URL
 import java.nio.charset.StandardCharsets
 import java.nio.file.{DirectoryStream, Files, Path}
 import scala.jdk.CollectionConverters._
 import scala.util.{Failure, Success, Try}
-import cats.implicits._
 
 class FlinkMiniClusterDataGenerator(flinkTableSchema: Schema) extends LazyLogging {
 
-  def generateTestData(size: Int): TestData = {
-    val classLoader = getClass.getClassLoader
-
+  // TODO: check the minicluster releases memory properly and if not, refactor to reuse one minicluster per all usages
+  def generateTestData(amountOfRecordsToGenerate: Int): TestData =
     // setting context classloader because Flink in multiple places relies on it and without this temporary override it doesnt have
     // the necessary classes
-    ThreadUtils.withThisAsContextClassLoader(classLoader) {
-      // TODO: check if this miniCluster env releases memory properly and if not, refactor to reuse one minicluster per all usages
-      val tableEnv = TableEnvironment.create(
-        EnvironmentSettings.newInstance.withConfiguration(miniClusterTestingEnvConfig).build()
-      )
-
-      tableEnv.createTable(
-        dataGenerationInputInternalTableName,
-        TableDescriptor
-          .forConnector("datagen")
-          .option("number-of-rows", size.toString)
-          .schema(flinkTableSchema)
-          .build()
-      )
-
-      val tempDir    = Files.createTempDirectory("tableSourceDataDump-")
-      val tempDirUrl = tempDir.toUri.toURL
-      logger.debug(s"Created temporary directory for dumping test data at: '$tempDirUrl'")
-
-      val result = Try {
-        tableEnv.createTable(
-          dataGenerationOutputFileInternalTableName,
-          TableDescriptor
-            .forConnector("filesystem")
-            .option("path", tempDirUrl.toString)
-            .format("json")
-            .schema(flinkTableSchema)
-            .build()
+    ThreadUtils.withThisAsContextClassLoader(getClass.getClassLoader) {
+      val env = TableEnvironment.create(miniClusterTestingEnvConfig)
+      createGeneratorTable(env, amountOfRecordsToGenerate)
+      val (tempDirForOutputTable, tempDirUrl) = createTempDirectoryForTestResults()
+      createOutputFileTable(env, tempDirUrl)
+      val generatedRowsTry = Try {
+        insertDataAndAwait(
+          env = env,
+          inputTableName = dataGenerationInputInternalTableName,
+          outputTableName = dataGenerationOutputFileInternalTableName
         )
-
-        val inputTable = tableEnv.from(dataGenerationInputInternalTableName)
-        inputTable.insertInto(dataGenerationOutputFileInternalTableName).execute().await()
-
-        val outputFiles: List[Path] = {
-          val dirStream: DirectoryStream[Path] = Files.newDirectoryStream(tempDir)
-          val outputFiles                      = dirStream.asScala.toList
-          dirStream.close()
-          outputFiles
-        }
-
-        val records = outputFiles
-          .flatMap(f => FileUtils.readLines(f.toFile, StandardCharsets.UTF_8).asScala)
-          .map(parse)
-          .sequence
-          .getOrElse(throw new IllegalStateException("Couldn't parse record from test data dump"))
-
-        TestData(records.map(TestRecord(_)))
+        readRecordsFromFilesUnderPath(tempDirForOutputTable)
       }
-      tryToDeleteDirectoryWithLogging(tempDir)
-      result.get
+      tryToDeleteDirectoryWithLogging(tempDirForOutputTable)
+      val rows = generatedRowsTry.get
+      TestData(rows.map(TestRecord(_)))
     }
+
+  private def readRecordsFromFilesUnderPath(path: Path) = {
+    val filesUnderPath: List[Path] = {
+      val dirStream: DirectoryStream[Path] = Files.newDirectoryStream(path)
+      val outputFiles                      = dirStream.asScala.toList
+      dirStream.close()
+      outputFiles
+    }
+    filesUnderPath
+      .flatMap(f => FileUtils.readLines(f.toFile, StandardCharsets.UTF_8).asScala)
+      .map(parse)
+      .sequence
+      .getOrElse(throw new IllegalStateException("Couldn't parse record from test data dump"))
   }
+
+  private def insertDataAndAwait(env: TableEnvironment, inputTableName: String, outputTableName: String): Unit = {
+    val inputTable = env.from(inputTableName)
+    inputTable.insertInto(outputTableName).execute().await()
+  }
+
+  private def createTempDirectoryForTestResults(): (Path, URL) = {
+    val tempDir    = Files.createTempDirectory(tempTestDataOutputTablePrefix)
+    val tempDirUrl = tempDir.toUri.toURL
+    logger.debug(s"Created temporary directory for dumping test data at: '$tempDirUrl'")
+    tempDir -> tempDirUrl
+  }
+
+  private def createGeneratorTable(env: TableEnvironment, amountOfRecordsToGenerate: Int): Unit = env.createTable(
+    dataGenerationInputInternalTableName,
+    TableDescriptor
+      .forConnector("datagen")
+      .option("number-of-rows", amountOfRecordsToGenerate.toString)
+      .schema(flinkTableSchema)
+      .build()
+  )
+
+  private def createOutputFileTable(env: TableEnvironment, tempDirUrl: URL): Unit = env.createTable(
+    dataGenerationOutputFileInternalTableName,
+    TableDescriptor
+      .forConnector("filesystem")
+      .option("path", tempDirUrl.toString)
+      .format("json")
+      .schema(flinkTableSchema)
+      .build()
+  )
 
   private def tryToDeleteDirectoryWithLogging(dirPath: Path): Unit = Try {
     Files
@@ -96,6 +106,7 @@ object FlinkMiniClusterDataGenerator {
   private val dataGenerationInputInternalTableName      = "testDataInputTable"
   private val dataGenerationOutputFileInternalTableName = "testDataOutputTable"
   private val miniClusterTestingEnvParallelism          = Int.box(1)
+  private val tempTestDataOutputTablePrefix             = "tableSourceDataDump-"
 
   // TODO: how to get path of jar cleaner? Through config?
   private val classPathUrlsForMiniClusterTestingEnv = List(
@@ -115,7 +126,7 @@ object FlinkMiniClusterDataGenerator {
       classPathUrlsForMiniClusterTestingEnv.map(_.toString).asJava
     )
     conf.set(CoreOptions.DEFAULT_PARALLELISM, miniClusterTestingEnvParallelism)
-    conf
+    EnvironmentSettings.newInstance().withConfiguration(conf).build()
   }
 
 }
