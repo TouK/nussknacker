cc3b4f052d3f9ad905dc73448c4caa7601e825c6
Julian Wielga <j@touk.pl>
2024-07-06 21:49:38 +0200

    introduce markdown field

--- designer/client/src/components/graph/node-modal/editors/expression/ace.ts
+++ designer/client/src/components/graph/node-modal/editors/expression/ace.ts
@@ -1,5 +1,6 @@
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/ext-searchbox";
+import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/mode-jsx";
 import AceEditor from "react-ace";
