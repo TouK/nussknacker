a226f97a24d5e16ec627c5e2ee3851066babf863
Mateusz Słabek <mslb@touk.pl>
2024-07-08 19:51:48 +0200

    adapt TestDataPreparer to work on lists

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/testmode/TestDataPreparer.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/testmode/TestDataPreparer.scala
@@ -19,6 +19,7 @@ import pl.touk.nussknacker.engine.definition.globalvariables.ExpressionConfigDef
 import pl.touk.nussknacker.engine.expression.ExpressionEvaluator
 import pl.touk.nussknacker.engine.graph.expression.Expression
 import pl.touk.nussknacker.engine.variables.GlobalVariablesPreparer
+import scala.reflect.runtime.universe._
 
 class TestDataPreparer(
     classloader: ClassLoader,
@@ -35,32 +36,41 @@ class TestDataPreparer(
   private lazy val expressionCompiler: ExpressionCompiler =
     ExpressionCompiler.withoutOptimization(classloader, dictRegistry, expressionConfig, classDefinitionSet)
 
-  def prepareRecordForTest[T](source: Source, record: ScenarioTestRecord): T = {
-    ???
-//    implicit val implicitNodeId: NodeId = record.sourceId
-//    (source, record) match {
-//      case (s: SourceTestSupport[T @unchecked], jsonRecord: ScenarioTestJsonRecord) =>
-//        s.testRecordParser.parse(jsonRecord.record)
-//      case (s: TestWithParametersSupport[T @unchecked], parametersRecord: ScenarioTestParametersRecord) =>
-//        val parameterTypingResults = s.testParametersDefinition.collect { param =>
-//          parametersRecord.parameterExpressions.get(param.name) match {
-//            case Some(expression)          => evaluateExpression(expression, param).map(e => param.name -> e)
-//            case None if !param.isOptional => UnknownProperty(param.name).invalidNel
-//          }
-//        }
-//        parameterTypingResults.sequence match {
-//          case Valid(evaluatedParams) => s.parametersToTestData(evaluatedParams.toMap)
-//          case Invalid(errors)        => throw new IllegalArgumentException(errors.toList.mkString(", "))
-//        }
-//      case (other, _: ScenarioTestJsonRecord) =>
-//        throw new IllegalArgumentException(
-//          s"Source ${other.getClass} cannot be stubbed - it doesn't provide test data parser"
-//        )
-//      case (other, _: ScenarioTestParametersRecord) =>
-//        throw new IllegalArgumentException(
-//          s"Source ${other.getClass} cannot be stubbed - it doesn't provide test with parameters"
-//        )
-//    }
+  def prepareRecordForTest[T, R <: ScenarioTestRecord: TypeTag](source: Source, records: List[R]): List[T] = {
+    typeOf[R] match {
+      case r if r =:= typeOf[List[ScenarioTestJsonRecord]] =>
+        val jsonRecords = records.asInstanceOf[List[ScenarioTestJsonRecord]].map(_.record)
+        source match {
+          case s: SourceTestSupport[T @unchecked] => s.testRecordParser.parse(jsonRecords)
+          case other =>
+            throw new IllegalArgumentException(
+              s"Source ${other.getClass} cannot be stubbed - it doesn't provide test data parser"
+            )
+        }
+      case r if r =:= typeOf[List[ScenarioTestParametersRecord]] =>
+        val parametersRecords = records.asInstanceOf[List[ScenarioTestParametersRecord]]
+        source match {
+          case s: TestWithParametersSupport[T @unchecked] =>
+            parametersRecords.map { record =>
+              implicit val implicitNodeId: NodeId = record.sourceId
+              val parameterTypingResults = s.testParametersDefinition.collect { param =>
+                record.parameterExpressions.get(param.name) match {
+                  case Some(expression)          => evaluateExpression(expression, param).map(e => param.name -> e)
+                  case None if !param.isOptional => UnknownProperty(param.name).invalidNel
+                }
+              }
+              parameterTypingResults.sequence match {
+                case Valid(evaluatedParams) => s.parametersToTestData(evaluatedParams.toMap)
+                case Invalid(errors)        => throw new IllegalArgumentException(errors.toList.mkString(", "))
+              }
+            }
+          case other =>
+            throw new IllegalArgumentException(
+              s"Source ${other.getClass} cannot be stubbed - it doesn't provide test with parameters"
+            )
+        }
+      case _ => throw new IllegalStateException("TODO error message") // TODO local
+    }
   }
 
   private def evaluateExpression(expression: Expression, parameter: Parameter)(
