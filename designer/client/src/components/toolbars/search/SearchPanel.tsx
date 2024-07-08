3e4d87501d36cef31e7a6dd22606b757c44164b5
Grzegorz Skrobisz <gsk@touk.pl>
2024-07-08 14:55:20 +0200

    revert listener for esc

--- designer/client/src/components/toolbars/search/SearchPanel.tsx
+++ designer/client/src/components/toolbars/search/SearchPanel.tsx
@@ -1,7 +1,6 @@
 import { isEmpty } from "lodash";
 import React, { ReactElement, useCallback, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useDocumentListeners } from "../../../containers/useDocumentListeners";
 import { SearchIcon } from "../../table/SearchFilter";
 import { Focusable } from "../../themed/InputWithIcon";
 import { ToolbarPanelProps } from "../../toolbarComponents/DefaultToolbarPanel";
@@ -16,23 +15,6 @@ export function SearchPanel(props: ToolbarPanelProps): ReactElement {
     const clearFilter = useCallback(() => setFilter(""), []);
 
     const searchRef = useRef<Focusable>();
-
-    useDocumentListeners({
-        keydown: (e) => {
-            switch (e.key.toUpperCase()) {
-                case "ESCAPE": {
-                    e.preventDefault();
-                    e.stopImmediatePropagation();
-                    clearFilter();
-                    const target = e.composedPath().shift();
-                    if (target instanceof HTMLElement) {
-                        target.blur();
-                    }
-                    break;
-                }
-            }
-        },
-    });
 
     return (
         <ToolbarWrapper {...props} title={t("panels.search.title", "Search")} onExpand={() => searchRef.current?.focus()}>
