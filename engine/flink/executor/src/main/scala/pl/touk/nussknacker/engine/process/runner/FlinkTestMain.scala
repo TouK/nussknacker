TouK/nussknacker

commit 892d9104a3f5ee3546bafe22c7402be2d833ea6e

Improvements: Fixes and changes on TestScenarioRunner (#4901)

engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/runner/FlinkTestMain.scala
@@ -12,7 +12,13 @@ import pl.touk.nussknacker.engine.process.ExecutionConfigPreparer
 import pl.touk.nussknacker.engine.process.compiler.TestFlinkProcessCompiler
 import pl.touk.nussknacker.engine.process.registrar.FlinkProcessRegistrar
 import pl.touk.nussknacker.engine.testmode.TestProcess.TestResults
-import pl.touk.nussknacker.engine.testmode.{ResultsCollectingListener, ResultsCollectingListenerHolder}
+import pl.touk.nussknacker.engine.testmode.{
+  ResultsCollectingListener,
+  ResultsCollectingListenerHolder,
+  TestServiceInvocationCollector
+}
+
+import scala.util.Using
 
 object FlinkTestMain extends FlinkRunner {
 
@@ -41,18 +47,16 @@ class FlinkTestMain(
     val configuration: Configuration
 ) extends FlinkStubbedRunner {
 
-  def runTest[T](variableEncoder: Any => T): TestResults[T] = {
-    val env                = createEnv
-    val collectingListener = ResultsCollectingListenerHolder.registerRun(variableEncoder)
-    try {
-      val registrar: FlinkProcessRegistrar = prepareRegistrar(collectingListener, scenarioTestData)
-      registrar.register(env, process, processVersion, deploymentData, Option(collectingListener.runId))
+  def runTest[T](variableEncoder: Any => T): TestResults[T] =
+    Using.resource(ResultsCollectingListenerHolder.registerRun(variableEncoder)) { collectingListener =>
+      val resultCollector = new TestServiceInvocationCollector(collectingListener.runId)
+      val registrar       = prepareRegistrar(collectingListener, scenarioTestData)
+      val env             = createEnv
+
+      registrar.register(env, process, processVersion, deploymentData, resultCollector)
       execute(env, SavepointRestoreSettings.none())
       collectingListener.results
-    } finally {
-      collectingListener.clean()
     }
-  }
 
   protected def prepareRegistrar[T](
       collectingListener: ResultsCollectingListener,

build.sbt
@@ -621,18 +621,18 @@ lazy val flinkDeploymentManager = (project in flink("management"))
     IntegrationTest / parallelExecution             := false,
     libraryDependencies ++= {
       Seq(
-        "org.typelevel"                 %% "cats-core"                        % catsV                % "provided",
-        "org.apache.flink"               % "flink-streaming-java"             % flinkV               % flinkScope
+        "org.typelevel"          %% "cats-core"                      % catsV                % "provided",
+        "org.apache.flink"        % "flink-streaming-java"           % flinkV               % flinkScope
           excludeAll (
             ExclusionRule("log4j", "log4j"),
             ExclusionRule("org.slf4j", "slf4j-log4j12"),
             ExclusionRule("com.esotericsoftware", "kryo-shaded"),
           ),
-        "org.apache.flink"               % "flink-statebackend-rocksdb"       % flinkV               % flinkScope,
-        "com.softwaremill.retry"        %% "retry"                            % "0.3.6",
-        "com.dimafeng"                  %% "testcontainers-scala-scalatest"   % testContainersScalaV % "it,test",
-        "com.dimafeng"                  %% "testcontainers-scala-kafka"       % testContainersScalaV % "it,test",
-        "com.github.tomakehurst"         % "wiremock-jre8"                    % wireMockV            % Test
+        "org.apache.flink"        % "flink-statebackend-rocksdb"     % flinkV               % flinkScope,
+        "com.softwaremill.retry" %% "retry"                          % "0.3.6",
+        "com.dimafeng"           %% "testcontainers-scala-scalatest" % testContainersScalaV % "it,test",
+        "com.dimafeng"           %% "testcontainers-scala-kafka"     % testContainersScalaV % "it,test",
+        "com.github.tomakehurst"  % "wiremock-jre8"                  % wireMockV            % Test
       ) ++ flinkLibScalaDeps(scalaVersion.value, Some(flinkScope))
     },
     // override scala-collection-compat from com.softwaremill.retry:retry
@@ -994,7 +994,7 @@ lazy val flinkComponentsTestkit = (project in utils("flink-components-testkit"))
       )
     }
   )
-  .dependsOn(componentsTestkit, flinkExecutor, flinkTestUtils)
+  .dependsOn(componentsTestkit, flinkExecutor, flinkTestUtils, defaultHelpers % "test")
 
 //this should be only added in scope test - 'module % "test"'
 lazy val liteComponentsTestkit = (project in utils("lite-components-testkit"))
@@ -1007,7 +1007,8 @@ lazy val liteComponentsTestkit = (project in utils("lite-components-testkit"))
     requestResponseRuntime,
     liteEngineRuntime,
     liteKafkaComponents,
-    liteRequestResponseComponents
+    liteRequestResponseComponents,
+    defaultHelpers % "test"
   )
 
 lazy val commonUtils = (project in utils("utils"))
@@ -1271,8 +1272,8 @@ lazy val liteEngineKafkaIntegrationTest: Project = (project in lite("integration
       )
       .value,
     libraryDependencies ++= Seq(
-      "com.dimafeng"                  %% "testcontainers-scala-scalatest"   % testContainersScalaV % "it",
-      "com.dimafeng"                  %% "testcontainers-scala-kafka"       % testContainersScalaV % "it"
+      "com.dimafeng" %% "testcontainers-scala-scalatest" % testContainersScalaV % "it",
+      "com.dimafeng" %% "testcontainers-scala-kafka"     % testContainersScalaV % "it"
     )
   )
   .dependsOn(
@@ -1442,16 +1443,16 @@ lazy val componentsApi = (project in file("components-api"))
     name := "nussknacker-components-api",
     libraryDependencies ++= {
       Seq(
-        "org.apache.commons"          % "commons-text"            % flinkCommonsTextV,
-        "org.typelevel"              %% "cats-core"               % catsV,
-        "com.typesafe.scala-logging" %% "scala-logging"           % scalaLoggingV,
-        "com.typesafe"                % "config"                  % configV,
-        "com.vdurmont"                % "semver4j"                % "3.1.0",
-        "javax.validation"            % "validation-api"          % javaxValidationApiV,
-        "org.scala-lang.modules"     %% "scala-collection-compat" % scalaCollectionsCompatV,
-        "com.iheart"                 %% "ficus"                   % ficusV,
-        "org.springframework"         % "spring-core"             % springV,
-        "com.google.code.findbugs"    % "jsr305"                  % findBugsV,
+        "org.apache.commons"             % "commons-text"                     % flinkCommonsTextV,
+        "org.typelevel"                 %% "cats-core"                        % catsV,
+        "com.typesafe.scala-logging"    %% "scala-logging"                    % scalaLoggingV,
+        "com.typesafe"                   % "config"                           % configV,
+        "com.vdurmont"                   % "semver4j"                         % "3.1.0",
+        "javax.validation"               % "validation-api"                   % javaxValidationApiV,
+        "org.scala-lang.modules"        %% "scala-collection-compat"          % scalaCollectionsCompatV,
+        "com.iheart"                    %% "ficus"                            % ficusV,
+        "org.springframework"            % "spring-core"                      % springV,
+        "com.google.code.findbugs"       % "jsr305"                           % findBugsV,
         "com.softwaremill.sttp.client3" %% "async-http-client-backend-future" % sttpV
       )
     }
@@ -1524,10 +1525,10 @@ lazy val security = (project in file("security"))
       "com.typesafe.scala-logging" %% "scala-logging"     % scalaLoggingV,
       "com.auth0"                   % "jwks-rsa"          % "0.22.0", // a tool library for reading a remote JWK store, not an Auth0 service dependency
 
-      "com.softwaremill.sttp.tapir"   %% "tapir-core"                       % tapirV,
-      "com.softwaremill.sttp.tapir"   %% "tapir-json-circe"                 % tapirV,
-      "com.dimafeng"                  %% "testcontainers-scala-scalatest"   % testContainersScalaV % "it,test",
-      "com.github.dasniko"             % "testcontainers-keycloak"          % "2.5.0"              % "it,test" excludeAll (
+      "com.softwaremill.sttp.tapir" %% "tapir-core"                     % tapirV,
+      "com.softwaremill.sttp.tapir" %% "tapir-json-circe"               % tapirV,
+      "com.dimafeng"                %% "testcontainers-scala-scalatest" % testContainersScalaV % "it,test",
+      "com.github.dasniko"           % "testcontainers-keycloak"        % "2.5.0"              % "it,test" excludeAll (
         // we're using testcontainers-scala which requires a proper junit4 dependency
         ExclusionRule("io.quarkus", "quarkus-junit4-mock")
       )
@@ -1568,9 +1569,9 @@ lazy val processReports = (project in file("designer/processReports"))
     name := "nussknacker-process-reports",
     libraryDependencies ++= {
       Seq(
-        "com.dimafeng"                  %% "testcontainers-scala-scalatest"   % testContainersScalaV % "it,test",
-        "com.dimafeng"                  %% "testcontainers-scala-influxdb"    % testContainersScalaV % "it,test",
-        "org.influxdb"                   % "influxdb-java"                    % "2.23"               % "it,test"
+        "com.dimafeng" %% "testcontainers-scala-scalatest" % testContainersScalaV % "it,test",
+        "com.dimafeng" %% "testcontainers-scala-influxdb"  % testContainersScalaV % "it,test",
+        "org.influxdb"  % "influxdb-java"                  % "2.23"               % "it,test"
       )
     }
   )
@@ -1582,9 +1583,9 @@ lazy val httpUtils = (project in utils("http-utils"))
     name := "nussknacker-http-utils",
     libraryDependencies ++= {
       Seq(
-        "com.softwaremill.sttp.client3" %% "core"                             % sttpV,
-        "com.softwaremill.sttp.client3" %% "json-common"                      % sttpV,
-        "com.softwaremill.sttp.client3" %% "circe"                            % sttpV,
+        "com.softwaremill.sttp.client3" %% "core"        % sttpV,
+        "com.softwaremill.sttp.client3" %% "json-common" % sttpV,
+        "com.softwaremill.sttp.client3" %% "circe"       % sttpV,
       )
     }
   )
@@ -1602,13 +1603,13 @@ lazy val openapiComponents = (project in component("openapi"))
   .settings(
     name := "nussknacker-openapi",
     libraryDependencies ++= Seq(
-      "io.swagger.core.v3"             % "swagger-integration"              % swaggerIntegrationV excludeAll (
+      "io.swagger.core.v3" % "swagger-integration"          % swaggerIntegrationV excludeAll (
         ExclusionRule(organization = "jakarta.activation"),
         ExclusionRule(organization = "jakarta.validation")
       ),
-      "io.netty"                       % "netty-transport-native-epoll"     % nettyV,
-      "org.apache.flink"               % "flink-streaming-java"             % flinkV     % Provided,
-      "org.scalatest"                 %% "scalatest"                        % scalaTestV % "it,test"
+      "io.netty"           % "netty-transport-native-epoll" % nettyV,
+      "org.apache.flink"   % "flink-streaming-java"         % flinkV     % Provided,
+      "org.scalatest"     %% "scalatest"                    % scalaTestV % "it,test"
     ),
   )
   .dependsOn(

engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/registrar/FlinkProcessRegistrar.scala
@@ -32,14 +32,14 @@ import pl.touk.nussknacker.engine.process.{CheckpointConfig, ExecutionConfigPrep
 import pl.touk.nussknacker.engine.resultcollector.{ProductionServiceInvocationCollector, ResultCollector}
 import pl.touk.nussknacker.engine.splittedgraph.end.BranchEnd
 import pl.touk.nussknacker.engine.splittedgraph.{SplittedNodesCollector, splittednode}
-import pl.touk.nussknacker.engine.testmode.{SinkInvocationCollector, TestRunId, TestServiceInvocationCollector}
+import pl.touk.nussknacker.engine.testmode.TestServiceInvocationCollector
+import pl.touk.nussknacker.engine.util.Implicits.RichScalaMap
 import pl.touk.nussknacker.engine.util.loader.ScalaServiceLoader
 import pl.touk.nussknacker.engine.util.{MetaDataExtractor, ThreadUtils}
 import shapeless.syntax.typeable.typeableOps
 
 import java.util.concurrent.TimeUnit
 import scala.language.implicitConversions
-import pl.touk.nussknacker.engine.util.Implicits.RichScalaMap
 
 /*
   This is main class where we translate Nussknacker model to Flink job.
@@ -63,22 +63,26 @@ class FlinkProcessRegistrar(
       env: StreamExecutionEnvironment,
       process: CanonicalProcess,
       processVersion: ProcessVersion,
+      deploymentData: DeploymentData
+  ): Unit =
+    register(env, process, processVersion, deploymentData, ProductionServiceInvocationCollector)
+
+  def register(
+      env: StreamExecutionEnvironment,
+      process: CanonicalProcess,
+      processVersion: ProcessVersion,
       deploymentData: DeploymentData,
-      testRunId: Option[TestRunId] = None
+      resultCollector: ResultCollector
   ): Unit = {
     usingRightClassloader(env) { userClassLoader =>
-      // TODO: move creation outside Registrar, together with refactoring SinkInvocationCollector...
-      val collector =
-        testRunId.map(new TestServiceInvocationCollector(_)).getOrElse(ProductionServiceInvocationCollector)
-
-      val processCompilation = compileProcess(process, processVersion, collector)
+      val processCompilation = compileProcess(process, processVersion, resultCollector)
       val processWithDeps    = processCompilation(UsedNodes.empty, userClassLoader)
 
       streamExecutionEnvPreparer.preRegistration(env, processWithDeps, deploymentData)
       val typeInformationDetection = TypeInformationDetectionUtils.forExecutionConfig(env.getConfig, userClassLoader)
 
       val partCompilation = FlinkProcessRegistrar.partCompilation[FlinkProcessCompilerData](processCompilation) _
-      register(env, partCompilation, processWithDeps, testRunId, typeInformationDetection)
+      register(env, partCompilation, processWithDeps, resultCollector, typeInformationDetection)
       streamExecutionEnvPreparer.postRegistration(env, processWithDeps, deploymentData)
     }
   }
@@ -111,7 +115,7 @@ class FlinkProcessRegistrar(
       env: StreamExecutionEnvironment,
       compiledProcessWithDeps: Option[ProcessPart] => ClassLoader => FlinkProcessCompilerData,
       processWithDeps: FlinkProcessCompilerData,
-      testRunId: Option[TestRunId],
+      resultCollector: ResultCollector,
       typeInformationDetection: TypeInformationDetection
   ): Unit = {
 
@@ -265,12 +269,10 @@ class FlinkProcessRegistrar(
       val customNodeContext = nodeContext(nodeComponentInfoFrom(part), Left(contextBefore))
       val withValuePrepared = sink.prepareValue(afterInterpretation, customNodeContext)
       // TODO: maybe this logic should be moved to compiler instead?
-      val withSinkAdded = testRunId match {
-        case None =>
-          sink.registerSink(withValuePrepared, nodeContext(nodeComponentInfoFrom(part), Left(contextBefore)))
-        case Some(runId) =>
+      val withSinkAdded = resultCollector match {
+        case testResultCollector: TestServiceInvocationCollector =>
           val typ            = part.node.data.ref.typ
-          val collectingSink = SinkInvocationCollector(runId, part.id, typ)
+          val collectingSink = testResultCollector.createSinkInvocationCollector(part.id, typ)
           withValuePrepared
             .map(
               (ds: ValueWithContext[sink.Value]) => ds.map(sink.prepareTestValue),
@@ -278,6 +280,8 @@ class FlinkProcessRegistrar(
             )
             // FIXME: ...
             .addSink(new CollectingSinkFunction[AnyRef](compiledProcessWithDeps(None), collectingSink, part.id))
+        case _ =>
+          sink.registerSink(withValuePrepared, nodeContext(nodeComponentInfoFrom(part), Left(contextBefore)))
       }
 
       withSinkAdded.name(operatorName(metaData, part.node, "sink"))

engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/runner/FlinkVerificationMain.scala
@@ -9,7 +9,7 @@ import pl.touk.nussknacker.engine.deployment.DeploymentData
 import pl.touk.nussknacker.engine.process.ExecutionConfigPreparer
 import pl.touk.nussknacker.engine.process.compiler.VerificationFlinkProcessCompiler
 import pl.touk.nussknacker.engine.process.registrar.FlinkProcessRegistrar
-import pl.touk.nussknacker.engine.testmode.TestRunId
+import pl.touk.nussknacker.engine.testmode.{TestRunId, TestServiceInvocationCollector}
 
 object FlinkVerificationMain extends FlinkRunner {
 
@@ -36,9 +36,11 @@ class FlinkVerificationMain(
 ) extends FlinkStubbedRunner {
 
   def runTest(): Unit = {
-    val env       = createEnv
-    val registrar = prepareRegistrar()
-    registrar.register(env, process, processVersion, deploymentData, Option(TestRunId("dummy")))
+    val resultCollector = new TestServiceInvocationCollector(TestRunId("dummy"))
+    val registrar       = prepareRegistrar()
+    val env             = createEnv
+
+    registrar.register(env, process, processVersion, deploymentData, resultCollector)
     execute(env, SavepointRestoreSettings.forPath(savepointPath, true))
   }
 

engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/runner/TestFlinkRunner.scala
@@ -11,8 +11,8 @@ import pl.touk.nussknacker.engine.deployment.DeploymentData
 import pl.touk.nussknacker.engine.process.ExecutionConfigPreparer
 import pl.touk.nussknacker.engine.process.compiler.FlinkProcessCompiler
 import pl.touk.nussknacker.engine.process.registrar.FlinkProcessRegistrar
+import pl.touk.nussknacker.engine.resultcollector.{ProductionServiceInvocationCollector, ResultCollector}
 import pl.touk.nussknacker.engine.testing.LocalModelData
-import pl.touk.nussknacker.engine.testmode.TestRunId
 
 object TestFlinkRunner {
 
@@ -20,11 +20,11 @@ object TestFlinkRunner {
       scenario: CanonicalProcess,
       deploymentData: DeploymentData = DeploymentData.empty,
       version: ProcessVersion = ProcessVersion.empty,
-      testRunId: Option[TestRunId] = None
+      resultCollector: ResultCollector = ProductionServiceInvocationCollector
   ): Unit = {
     val registrar =
       FlinkProcessRegistrar(new FlinkProcessCompiler(modelData), ExecutionConfigPreparer.unOptimizedChain(modelData))
-    registrar.register(env, scenario, version, deploymentData, testRunId)
+    registrar.register(env, scenario, version, deploymentData, resultCollector)
   }
 
   def registerInEnvironment(
@@ -35,10 +35,10 @@ object TestFlinkRunner {
       scenario: CanonicalProcess,
       deploymentData: DeploymentData = DeploymentData.empty,
       version: ProcessVersion = ProcessVersion.empty,
-      testRunId: Option[TestRunId] = None
+      resultCollector: ResultCollector = ProductionServiceInvocationCollector
   ): Unit = {
     val modelData = LocalModelData(config, configCreator)
-    registerInEnvironmentWithModel(env, modelData)(scenario, deploymentData, version, testRunId)
+    registerInEnvironmentWithModel(env, modelData)(scenario, deploymentData, version, resultCollector)
   }
 
 }

engine/lite/runtime/src/main/scala/pl/touk/nussknacker/engine/lite/TestRunner.scala
@@ -48,13 +48,14 @@ class InterpreterTestRunner[F[_]: InterpreterShape: CapabilityTransformer: Effec
     // in tests we don't send metrics anywhere
     val testContext                        = LiteEngineRuntimeContextPreparer.noOp.prepare(testJobData(process))
     val componentUseCase: ComponentUseCase = ComponentUseCase.TestRuntime
+    val testServiceInvocationCollector     = new TestServiceInvocationCollector(collectingListener.runId)
 
     // FIXME: validation??
     val scenarioInterpreter = ScenarioInterpreterFactory.createInterpreter[F, Input, Res](
       process,
       modelData,
       additionalListeners = List(collectingListener),
-      new TestServiceInvocationCollector(collectingListener.runId),
+      testServiceInvocationCollector,
       componentUseCase
     )(SynchronousExecutionContext.ctx, implicitly[InterpreterShape[F]], implicitly[CapabilityTransformer[F]]) match {
       case Valid(interpreter) => interpreter
@@ -81,7 +82,7 @@ class InterpreterTestRunner[F[_]: InterpreterShape: CapabilityTransformer: Effec
 
       val results = implicitly[EffectUnwrapper[F]].apply(scenarioInterpreter.invoke(inputs))
 
-      collectSinkResults(collectingListener.runId, results)
+      collectSinkResults(testServiceInvocationCollector, results)
       collectingListener.results
     } finally {
       collectingListener.clean()
@@ -96,11 +97,16 @@ class InterpreterTestRunner[F[_]: InterpreterShape: CapabilityTransformer: Effec
     JobData(process.metaData, processVersion)
   }
 
-  private def collectSinkResults(runId: TestRunId, results: ResultType[EndResult[Res]]): Unit = {
+  private def collectSinkResults(
+      testServiceInvocationCollector: TestServiceInvocationCollector,
+      results: ResultType[EndResult[Res]]
+  ): Unit = {
     val successfulResults = results.value
     successfulResults.foreach { result =>
       val node = result.nodeId.id
-      SinkInvocationCollector(runId, node, node).collect(result.context, result.result)
+      testServiceInvocationCollector
+        .createSinkInvocationCollector(node, node)
+        .collect(result.context, result.result)
     }
   }
 

interpreter/src/main/scala/pl/touk/nussknacker/engine/testmode/TestServiceInvocationCollector.scala
@@ -1,10 +1,9 @@
 package pl.touk.nussknacker.engine.testmode
 
 import cats.Monad
-import pl.touk.nussknacker.engine.api.{Context, ContextId}
-import pl.touk.nussknacker.engine.api.test.InvocationCollectors.{CollectableAction, ToCollect, TransmissionNames}
 import cats.implicits._
-import pl.touk.nussknacker.engine.api.NodeId
+import pl.touk.nussknacker.engine.api.test.InvocationCollectors.{CollectableAction, ToCollect, TransmissionNames}
+import pl.touk.nussknacker.engine.api.{Context, ContextId, NodeId}
 import pl.touk.nussknacker.engine.resultcollector.ResultCollector
 
 import scala.language.higherKinds
@@ -39,10 +38,13 @@ class TestServiceInvocationCollector(testRunId: TestRunId) extends ResultCollect
     }
   }
 
+  def createSinkInvocationCollector(nodeId: String, ref: String): SinkInvocationCollector =
+    new SinkInvocationCollector(testRunId, nodeId, ref)
+
 }
 
 //TODO: this should be somehow expressed via ResultCollector/TestServiceInvocationCollector
-case class SinkInvocationCollector(runId: TestRunId, nodeId: String, ref: String) {
+final class SinkInvocationCollector(runId: TestRunId, nodeId: String, ref: String) extends Serializable {
 
   def collect(context: Context, result: Any): Unit = {
     ResultsCollectingListenerHolder.updateResults(

utils/components-testkit/src/main/scala/pl/touk/nussknacker/engine/util/test/ModelWithTestExtensions.scala
@@ -4,7 +4,12 @@ import com.typesafe.config.Config
 import com.typesafe.config.ConfigValueFactory.fromAnyRef
 import pl.touk.nussknacker.engine.ModelData
 import pl.touk.nussknacker.engine.api.component.ComponentDefinition
-import pl.touk.nussknacker.engine.api.process.EmptyProcessConfigCreator
+import pl.touk.nussknacker.engine.api.process.{
+  EmptyProcessConfigCreator,
+  ExpressionConfig,
+  ProcessObjectDependencies,
+  WithCategories
+}
 import pl.touk.nussknacker.engine.testing.LocalModelData
 
 object ModelWithTestExtensions {
@@ -21,7 +26,18 @@ object ModelWithTestExtensions {
       s"components.${TestComponentsProvider.name}.${TestComponentsProvider.testRunIdConfig}",
       fromAnyRef(testExtensionsHolder.runId.id)
     )
-    val model = LocalModelData(configWithRunId, new EmptyProcessConfigCreator)
+
+    val configCreator = new EmptyProcessConfigCreator {
+      override def expressionConfig(processObjectDependencies: ProcessObjectDependencies) = {
+        val globalProcessVariables = globalVariables.map { case (key, value) =>
+          key -> WithCategories.anyCategory(value)
+        }
+
+        ExpressionConfig(globalProcessVariables, List.empty)
+      }
+    }
+
+    val model = LocalModelData(configWithRunId, configCreator)
     try {
       action(model)
     } finally {

commit e3505e43c1344df3222a3d8ab2476324fb014123

[NU-1683] Fixes memory leak in test mechanism introduced in #4901

engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/runner/FlinkTestMain.scala
@@ -46,8 +46,9 @@ class FlinkTestMain(
     val configuration: Configuration
 ) extends FlinkStubbedRunner {
 
-  def runTest: TestResults[Json] =
-    Using.resource(ResultsCollectingListenerHolder.registerTestEngineListener) { collectingListener =>
+  def runTest: TestResults[Json] = {
+    val collectingListener = ResultsCollectingListenerHolder.registerTestEngineListener
+    try {
       val resultCollector = new TestServiceInvocationCollector(collectingListener)
       val registrar       = prepareRegistrar(collectingListener, scenarioTestData)
       val env             = createEnv
@@ -55,7 +56,10 @@ class FlinkTestMain(
       registrar.register(env, process, processVersion, deploymentData, resultCollector)
       execute(env, SavepointRestoreSettings.none())
       collectingListener.results
+    } finally {
+      collectingListener.clean()
     }
+  }
 
   protected def prepareRegistrar(
       collectingListener: ResultsCollectingListener[Json],

scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/testmode/ResultsCollectingListener.scala
@@ -24,6 +24,10 @@ case class ResultsCollectingListener[T](holderClass: String, runId: TestRunId, v
 
   def results: TestResults[T] = ResultsCollectingListenerHolder.resultsForId(runId)
 
+  // Warning! close can't clean resources because listener is passed into each scenario subpart and it will be closed few times
+  // We have to use dedicated clean() method when we are sure that we consumed results instead.
+  override final def close(): Unit = {}
+
   def clean(): Unit = ResultsCollectingListenerHolder.cleanResult(runId)
 
   override def nodeEntered(nodeId: String, context: Context, processMetaData: MetaData): Unit = {