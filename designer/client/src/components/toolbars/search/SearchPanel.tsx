cb051577ad75835635e4e406b32ff24dd9bb8e36
gskrobisz <gsk@touk.pl>
2024-07-09 11:31:52 +0200

    Fix search nodes panel (do not catch ctrl f) (#6322)

--- designer/client/src/components/toolbars/search/SearchPanel.tsx
+++ designer/client/src/components/toolbars/search/SearchPanel.tsx
@@ -1,58 +1,20 @@
 import { isEmpty } from "lodash";
 import React, { ReactElement, useCallback, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleToolbar } from "../../../actions/nk/toolbars";
-import { useDocumentListeners } from "../../../containers/useDocumentListeners";
-import { getToolbarsConfigId } from "../../../reducers/selectors/toolbars";
-import { useSidePanel } from "../../sidePanels/SidePanel";
 import { SearchIcon } from "../../table/SearchFilter";
 import { Focusable } from "../../themed/InputWithIcon";
 import { ToolbarPanelProps } from "../../toolbarComponents/DefaultToolbarPanel";
 import { ToolbarWrapper } from "../../toolbarComponents/toolbarWrapper/ToolbarWrapper";
 import { SearchResults } from "./SearchResults";
 import { SearchInputWithIcon } from "../../themed/SearchInput";
-import { EventTrackingSelector, getEventTrackingProps, EventTrackingType, useEventTracking } from "../../../containers/event-tracking";
+import { EventTrackingSelector, getEventTrackingProps } from "../../../containers/event-tracking";
 
 export function SearchPanel(props: ToolbarPanelProps): ReactElement {
     const { t } = useTranslation();
-    const dispatch = useDispatch();
-    const toolbarsConfigId = useSelector(getToolbarsConfigId);
-    const { trackEvent } = useEventTracking();
     const [filter, setFilter] = useState<string>("");
     const clearFilter = useCallback(() => setFilter(""), []);
 
     const searchRef = useRef<Focusable>();
-    const sidePanel = useSidePanel();
-
-    useDocumentListeners({
-        keydown: (e) => {
-            switch (e.key.toUpperCase()) {
-                case "ESCAPE": {
-                    e.preventDefault();
-                    e.stopImmediatePropagation();
-                    clearFilter();
-                    const target = e.composedPath().shift();
-                    if (target instanceof HTMLElement) {
-                        target.blur();
-                    }
-                    break;
-                }
-                case "F": {
-                    if (!e.ctrlKey && !e.metaKey) return;
-                    e.preventDefault();
-                    e.stopPropagation();
-                    if (!sidePanel.isOpened) {
-                        sidePanel.onToggle();
-                    }
-                    dispatch(toggleToolbar(props.id, toolbarsConfigId, false));
-                    searchRef.current.focus();
-                    trackEvent({ selector: EventTrackingSelector.FocusSearchNodeField, event: EventTrackingType.Keyboard });
-                    break;
-                }
-            }
-        },
-    });
 
     return (
         <ToolbarWrapper {...props} title={t("panels.search.title", "Search")} onExpand={() => searchRef.current?.focus()}>
@@ -66,7 +28,7 @@ export function SearchPanel(props: ToolbarPanelProps): ReactElement {
             >
                 <SearchIcon isEmpty={isEmpty(filter)} />
             </SearchInputWithIcon>
-            <SearchResults filterValues={filter.toLowerCase().split(/\s/).filter(Boolean)} />
+            <SearchResults filterValues={[filter.toLowerCase()].filter(Boolean)} />
         </ToolbarWrapper>
     );
 }
