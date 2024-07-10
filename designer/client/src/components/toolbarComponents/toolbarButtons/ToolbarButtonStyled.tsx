211ecd78533597a8398981d61b0d9eec464a3954
JulianWielga <j@touk.pl>
2024-07-10 15:55:26 +0200

    quickfix for LinkButton/ToolbarButton icon in safari (#6357)

--- designer/client/src/components/toolbarComponents/toolbarButtons/ToolbarButtonStyled.tsx
+++ designer/client/src/components/toolbarComponents/toolbarButtons/ToolbarButtonStyled.tsx
@@ -2,11 +2,14 @@ import { styled } from "@mui/material";
 import { PANEL_BUTTON_SIZE } from "../../../stylesheets/variables";
 
 export const Icon = styled("div")({
-    flex: 1,
-    lineHeight: 0,
-    display: "flex",
-    flexDirection: "column",
-    width: PANEL_BUTTON_SIZE / 2,
+    // TODO: flatten nested usages 
+    "&, &>*": {
+        flex: 1,
+        lineHeight: 0,
+        display: "flex",
+        flexDirection: "column",
+        width: PANEL_BUTTON_SIZE / 2,
+    },
 });
 
 export const ToolbarButtonWrapper = styled("div")(() => ({
