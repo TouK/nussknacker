0969b8cc9b940c2a59528253db6407cc6b7cf72a
gskrobisz <gsk@touk.pl>
2024-07-10 17:49:18 +0200

    Fix search nodes panel (do not catch ctrl f) (#6322) (#6358)

--- designer/client/cypress/e2e/process2.cy.ts
+++ designer/client/cypress/e2e/process2.cy.ts
@@ -21,14 +21,8 @@ describe("Process view", () => {
 
     it("should have node search toolbar", () => {
         cy.get("[data-testid=search-panel]").should("be.visible");
-        cy.get("[data-testid=search-panel]")
-            .contains(/^search$/i)
-            .click();
-        cy.get("[title='toggle left panel']").click();
-        cy.get("[data-testid=search-panel]").should("be.not.visible");
-        cy.get("#nk-graph-main").click();
-        cy.realPress(["Meta", "F"]);
-        cy.get("[data-testid=search-panel] input").should("be.visible").should("be.focused");
+        cy.get("[data-testid=search-panel]").contains(/^search$/i);
+        cy.get("[data-testid=search-panel] input").click();
         cy.realType("en");
         cy.get("[data-testid=search-panel]").contains(/sms/i).click();
         cy.getNode("enricher")
@@ -46,9 +40,7 @@ describe("Process view", () => {
         cy.get("[data-testid=window]")
             .contains(/^cancel$/i)
             .click();
-        cy.get("#nk-graph-main").click();
-        cy.realPress(["Meta", "F"]);
-        cy.get("[data-testid=search-panel] input").should("be.visible").should("be.focused");
+        cy.get("[data-testid=search-panel] input").click().clear();
         cy.realType("source");
         cy.wait(750); //wait for animation
         cy.getNode("enricher")
