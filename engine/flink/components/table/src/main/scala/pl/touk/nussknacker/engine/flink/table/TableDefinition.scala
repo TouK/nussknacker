3f655e46db8070be1d822601487463091e570df8
Mateusz Słabek <mslb@touk.pl>
2024-07-11 12:39:52 +0200

    [NU-1665] Table source test data generation (#6195)
    add data generation support for table source

--- engine/flink/components/table/src/main/scala/pl/touk/nussknacker/engine/flink/table/TableDefinition.scala
+++ engine/flink/components/table/src/main/scala/pl/touk/nussknacker/engine/flink/table/TableDefinition.scala
@@ -1,7 +1,18 @@
 package pl.touk.nussknacker.engine.flink.table
 
+import org.apache.flink.table.api.{DataTypes, Schema}
 import org.apache.flink.table.types.DataType
 import pl.touk.nussknacker.engine.api.typed.typing.TypingResult
 
-final case class TableDefinition(tableName: String, typingResult: TypingResult, columns: List[ColumnDefinition])
+import scala.jdk.CollectionConverters._
+
+final case class TableDefinition(tableName: String, typingResult: TypingResult, columns: List[ColumnDefinition]) {
+
+  def toFlinkSchema: Schema = {
+    val cols = columns.map(c => DataTypes.FIELD(c.columnName, c.flinkDataType)).asJava
+    Schema.newBuilder().fromRowDataType(DataTypes.ROW(cols)).build()
+  }
+
+}
+
 final case class ColumnDefinition(columnName: String, typingResult: TypingResult, flinkDataType: DataType)
