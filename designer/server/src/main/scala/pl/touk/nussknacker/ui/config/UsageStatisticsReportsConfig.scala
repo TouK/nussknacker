2f72c4ab70eaafc700a7ffd8d43d0b8e43300c67
Łukasz Bigorajski <lbg@touk.pl>
2024-07-17 09:30:39 +0200

    Adjust default config and add docs

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/config/UsageStatisticsReportsConfig.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/config/UsageStatisticsReportsConfig.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.ui.config
 
 final case class UsageStatisticsReportsConfig(
     enabled: Boolean,
-    errorReportsEnabled: Boolean = true,
+    errorReportsEnabled: Boolean,
     // unique identifier for Designer installation
     fingerprint: Option[String],
     // source from which Nussknacker was downloaded
