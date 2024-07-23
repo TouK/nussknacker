6cf173d0f4467035db952b5f72af439d4fbc8a31
Julian Wielga <j@touk.pl>
2024-07-23 16:42:54 +0200

    debug container update

--- designer/client/src/containers/Debug.tsx
+++ designer/client/src/containers/Debug.tsx
@@ -1,11 +1,23 @@
 /* eslint-disable i18next/no-literal-string */
 import React from "react";
-import Inspector from "react-inspector";
+import Inspector, { ObjectLabel, ObjectName } from "react-inspector";
 
-export function Debug({ data }: { data: any }): JSX.Element {
+const defaultNodeRenderer = ({ depth, name, data, isNonenumerable, expanded }) => {
+    return depth === 0 ? (
+        <ObjectName name={name} />
+    ) : expanded ? (
+        <>
+            <ObjectName name={name} />:
+        </>
+    ) : (
+        <ObjectLabel name={name} data={data} isNonenumerable={isNonenumerable} />
+    );
+};
+
+export function Debug({ data, name }: { data: any; name?: string }): JSX.Element {
     return (
         <div style={{ zoom: 2 }}>
-            <Inspector expandLevel={1} theme="chromeDark" data={data} />
+            <Inspector expandLevel={1} theme="chromeDark" data={data} name={name} nodeRenderer={defaultNodeRenderer} />
         </div>
     );
 }
