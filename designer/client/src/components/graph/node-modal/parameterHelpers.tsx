735e6ababd16840f1c4f25bb6bd1cf0ed9b2255b
Julian Wielga <j@touk.pl>
2024-07-23 17:27:47 +0200

    replace aggregator & aggregateBy with row with editors

--- designer/client/src/components/graph/node-modal/parameterHelpers.tsx
+++ designer/client/src/components/graph/node-modal/parameterHelpers.tsx
@@ -1,4 +1,13 @@
-import { UIParameter } from "../../../types";
+import { Parameter, UIParameter } from "../../../types";
+import { useMemo } from "react";
+
+export function useParameter(parameters: Parameter[], paramName: string): [Parameter, string] {
+    const index = useMemo(() => parameters?.findIndex((param) => param.name === paramName), [paramName, parameters]);
+    const parameter = useMemo(() => parameters[index], [index, parameters]);
+    const path = useMemo(() => `parameters[${index}].expression.expression`, [index]);
+
+    return [parameter, path];
+}
 
 export function findParamDefinitionByName(definitions: UIParameter[], paramName: string): UIParameter {
     return definitions?.find((param) => param.name === paramName);
