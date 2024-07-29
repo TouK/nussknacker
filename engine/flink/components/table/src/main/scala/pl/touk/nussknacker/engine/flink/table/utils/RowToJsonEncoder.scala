c1ce4b9ac6cc90599f170981d31a747499a893ff
Arek Burdach <arek.burdach@gmail.com>
2024-07-29 20:04:59 +0200

    Tests fixes + rowtojsonencoders

---
+++ engine/flink/components/table/src/main/scala/pl/touk/nussknacker/engine/flink/table/utils/RowToJsonEncoder.scala
@@ -0,0 +1,28 @@
+package pl.touk.nussknacker.engine.flink.table.utils
+
+import io.circe.Json
+import org.apache.flink.types.Row
+import org.everit.json.schema.{ObjectSchema, Schema}
+import pl.touk.nussknacker.engine.util.json.{EncodeOutput, ToJsonBasedOnSchemaEncoder, ToJsonEncoder}
+
+import scala.jdk.CollectionConverters._
+
+class RowToJsonEncoder extends ToJsonEncoder {
+
+  override def encoder(encode: Any => Json): PartialFunction[Any, Json] = { case row: Row =>
+    val fieldNames = row.getFieldNames(true).asScala
+    encode(fieldNames.map(n => n -> encode(row.getField(n))).toMap)
+  }
+
+}
+
+class RowToJsonBasedOnSchemaEncoder extends ToJsonBasedOnSchemaEncoder {
+
+  override def encoder(
+      encode: ((Any, Schema, Option[String])) => EncodeOutput
+  ): PartialFunction[(Any, Schema, Option[String]), EncodeOutput] = { case (row: Row, schema, fieldName) =>
+    val fieldNames = row.getFieldNames(true).asScala
+    encode(fieldNames.map(n => n -> row.getField(n)).toMap, schema, fieldName)
+  }
+
+}
