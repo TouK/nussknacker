4a17203099c161592fe2e1c24e1fa9544aeaa8e7
Piotr Fałdrowicz <pfl@touk.pl>
2024-07-30 16:19:44 +0200

    language of parameter's expression is based on editor type

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/component/parameter/defaults/ConfigParameterDefaultValueDeterminer.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/component/parameter/defaults/ConfigParameterDefaultValueDeterminer.scala
@@ -2,11 +2,9 @@ package pl.touk.nussknacker.engine.definition.component.parameter.defaults
 
 import pl.touk.nussknacker.engine.graph.expression.Expression
 
-object ConfigParameterDefaultValueDeterminer extends ParameterDefaultValueDeterminer {
+object ConfigParameterDefaultValueDeterminer extends ParameterDefaultValueDeterminer with SimpleParameterEditorHelper {
 
-  override def determineParameterDefaultValue(parameters: DefaultValueDeterminerParameters): Option[Expression] = {
-    // TODO: make language configurable as well
-    parameters.parameterConfig.defaultValue.map(Expression.spel)
-  }
+  override def determineParameterDefaultValue(parameters: DefaultValueDeterminerParameters): Option[Expression] =
+    calculateDefaultValue(parameters.determinedEditor, parameters.parameterConfig.defaultValue)
 
 }
