96019fa58dae42b38819cac164a41555ad4d1e41
ForrestFairy <szymon.bogusz55@gmail.com>
2024-08-06 09:53:06 +0200

    Update StatisticUrlConfig.scala
    Better explanation for decreased urlBytesSizeLimit

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/statistics/StatisticUrlConfig.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/statistics/StatisticUrlConfig.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.ui.statistics
 
 final case class StatisticUrlConfig(
     // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cloudfront-limits.html#limits-general
-    // 300 bytes less for 256 key size + 33% increase from encryption
+    // 300 bytes less: 256 bytes for secret key encrypted with rsa + 16 for padding in aes
     urlBytesSizeLimit: Int = 6700,
     nuStatsUrl: String = "https://stats.nussknacker.io/?",
     queryParamsSeparator: String = "&",
