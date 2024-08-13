ff65b0d7eb98261a8ad9ed19f5c0e438819eea98
Szymon Bogusz <sbg@touk.pl>
2024-08-13 15:38:57 +0200

    Removed commented test And removed one todo from StatisticsSpec as for test purposes we will still provide different key than the default one

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/statistics/StatisticsSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/statistics/StatisticsSpec.scala
@@ -9,7 +9,7 @@ import java.net.URL
 class StatisticsSpec extends AnyFunSuite with Matchers {
   private val cfg = StatisticUrlConfig(maybePublicEncryptionKey = None)
 
-  test("should split parameters into 2 URLS") {
+  test("should split parameters into a list of URLS") {
     val twoThousandCharsParam = (1 to 2000).map(_ => "x").mkString
     val sut = new Statistics.NonEmpty(
       new Fingerprint("t"),
@@ -54,8 +54,6 @@ class StatisticsSpec extends AnyFunSuite with Matchers {
   }
 
   test("should return encrypted URL if not set otherwise") {
-//    TODO: switch once logstash is ready
-//    val cfg = StatisticUrlConfig()
     val cfg = StatisticUrlConfig(
       maybePublicEncryptionKey = Some(PublicEncryptionKey(StatisticEncryptionSupport.publicKeyForTest))
     )
@@ -72,22 +70,5 @@ class StatisticsSpec extends AnyFunSuite with Matchers {
         StatisticEncryptionSupport.decode(url.toString)
       } shouldBe Some(Map("f" -> "a+b", "v" -> "1.6.5-a%26b%3Dc", "fingerprint" -> "t", "req_id" -> "req_id"))
   }
-
-  // jak klucz jest niepoprawny to powinniśmy się wywalić, bo w docelowym rozwiązaniu nie przewidujemy, że w ogóle będziemy wysyłać niezaszyfrowanych parametrów
-//  test("should return query params if public key is wrong") {
-//    val cfg = StatisticUrlConfig(maybePublicEncryptionKey = PublicEncryptionKey(Some("abc")))
-//    val sut = new Statistics.NonEmpty(
-//      new Fingerprint("t"),
-//      new RequestId("req_id"),
-//      Map("f" -> "a b", "v" -> "1.6.5-a&b=c")
-//    )
-//    sut
-//      .prepareURLs(cfg)
-//      .getOrElse(List.empty)
-//      .headOption
-//      .map { url =>
-//        QueryParamsHelper.extractFromURLString(url.toString)
-//      } shouldBe Some(Map("f" -> "a+b", "v" -> "1.6.5-a%26b%3Dc", "fingerprint" -> "t", "req_id" -> "req_id"))
-//  }
 
 }
