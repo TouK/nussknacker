9f88dc8bef5d07f7a991e4834eda5d42f8db08e7
Dawid Poliszak <dpl@touk.pl>
2024-08-13 15:06:19 +0200

    [Nu-1765] scenario details changes (#6563)
    * NU-1765 provide scenario details style
    
    Co-authored-by: Dzuming <Dzuming@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dzuming <Dzuming@users.noreply.github.com>

--- designer/client/src/components/toolbars/scenarioDetails/ScenarioDetailsComponents.tsx
+++ designer/client/src/components/toolbars/scenarioDetails/ScenarioDetailsComponents.tsx
@@ -4,31 +4,27 @@ export const PanelScenarioDetails = styled("div")(
     ({ theme }) => css`
         display: flex;
         flex-direction: column;
-        padding: ${theme.spacing(1.5, 1, 0.5)};
+        padding: ${theme.spacing(1.5, 2, 0.5)};
         gap: ${theme.spacing(1)};
     `,
 );
 
-export const PanelScenarioDetailsIcon = styled("div")`
-    display: inline-block;
-    width: 1rem;
-    height: 1rem;
-`;
+export const PanelScenarioDetailsIcon = styled("div")(({ theme }) => ({
+    display: "inline-block",
+    width: "1rem",
+    height: "1rem",
+    marginTop: theme.spacing(0.5),
+}));
 
 export const ScenarioDetailsItemWrapper = styled("div")(
     ({ theme }) => css`
         display: flex;
-        align-items: center;
+        align-items: flex-start;
         gap: ${theme.spacing(1)};
     `,
 );
 
-export const ProcessName = styled(Typography)`
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: 215px;
-    white-space: pre;
-`;
+export const ProcessName = styled(Typography)``;
 
 export const ProcessRename = styled(ProcessName)(({ theme }) => ({
     color: theme.palette.warning.main,
