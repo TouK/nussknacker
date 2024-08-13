80f277b963905e4c767bf9d6c27a22927cbf1145
Łukasz Bigorajski <lbg@touk.pl>
2024-08-13 11:33:29 +0200

    some adjustments

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/statistics/StatisticUrlConfig.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/statistics/StatisticUrlConfig.scala
@@ -9,12 +9,5 @@ final case class StatisticUrlConfig(
     nuStatsUrl: String = "https://stats.nussknacker.io/?",
     queryParamsSeparator: String = "&",
     emptyString: String = "",
-    publicEncryptionKey: PublicEncryptionKey = PublicEncryptionKey(
-      Some(
-        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjdvfEFH8jBF56UZmtQv" +
-          "UH1gUCnRkHTke/jnOEIjdSmSqoyWGJ9UKC/PgLYyqLqNRKq2eEmLr26tQRaIoOHLcdGlGZXIrRkWHAPH8QTGrDt4Qm/COB8BPS7oV2tATsUN7z" +
-          "JNWfVRNDzcunzDwAtZKs4SDTsFLAPrZ5CKMt5JK9Q7Xrzekl5PunzJEuIOmlWusFanIqCgs0d245NVRKhbkqq/JkoseEB4sDXFwNyO7sO51aLg" +
-          "DST+P/G+tPveQpMusbGBK48Syce93R6bTf0Bd8KGaJQWvJPjD6rbX+K3vSQHqgb+NjIyUAWWNHuFvRt2NQ5iHsuaBU0B/61W8y6oMxQIDAQAB"
-      )
-    )
+    maybePublicEncryptionKey: Option[PublicEncryptionKey] = None
 )
