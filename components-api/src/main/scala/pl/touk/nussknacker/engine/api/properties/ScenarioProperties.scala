8ac81bcfd56adcd6be76226c9157aa6cfcf8949a
Marcel Philipiak <ramcel@localhost.localdomain>
2024-08-19 23:49:42 +0200

    Remove ++ method

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/properties/ScenarioProperties.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/properties/ScenarioProperties.scala
@@ -6,14 +6,7 @@ import pl.touk.nussknacker.engine.api.component.SingleScenarioPropertyConfig
 @JsonCodec case class ScenarioProperties(
     propertiesConfig: Map[String, SingleScenarioPropertyConfig],
     docsUrl: Option[String] = None
-) {
-
-  // it will overwrite docsUrl, with the approach that the latest config is the proper one.
-  def ++(newConfig: ScenarioProperties): ScenarioProperties = {
-    this.copy(propertiesConfig = this.propertiesConfig ++ newConfig.propertiesConfig, newConfig.docsUrl)
-  }
-
-}
+)
 
 object ScenarioProperties {
   def empty(): ScenarioProperties = { ScenarioProperties(Map.empty, None) }
