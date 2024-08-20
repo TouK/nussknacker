7885cf430bdd9e098cdba0cf1a0cd894b314d382
Rafał Solarski <solarski.rafal@gmail.com>
2024-08-20 12:27:16 +0200

    Additional comment

--- engine/flink/metrics-deferred-reporter/src/main/java/pl/touk/nussknacker/engine/flink/metrics/MetricRemovalDeferredToNextReportMetricReporterFactory.java
+++ engine/flink/metrics-deferred-reporter/src/main/java/pl/touk/nussknacker/engine/flink/metrics/MetricRemovalDeferredToNextReportMetricReporterFactory.java
@@ -6,6 +6,10 @@ import org.apache.flink.metrics.reporter.MetricReporterFactory;
 import java.util.*;
 
 /*
+* Flink by design doesn't flush metrics after job finish but only unregisters them from shared MetricRegistry which can leads to metrics incompleteness or even no metrics for short living jobs.
+* To deal with it MetricRemovalDeferredToNextReportMetricReporter can be used to defer metrics removal to the next report call on MetricReporter
+* to ensure that metrics from closed jobs will be flushed with last known values.
+*
 * Usage:
 * 1. Add nussknacker-flink-metrics-deferred-reporter.jar to dir with reporter to wrap (e.g. plugins/metrics-influx) in jobmanager and taskmanager
 * 2. Point to MetricRemovalDeferredToNextReportMetricReporterFactory and wrapped reporter in flink config e.g:
