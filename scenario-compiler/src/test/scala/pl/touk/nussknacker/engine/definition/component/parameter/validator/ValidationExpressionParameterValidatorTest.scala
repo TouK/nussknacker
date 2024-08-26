17b72d1f37564bde0f4b8d0acb7c8356c801f547
Adrian Matwiejuk <amt@touk.pl>
2024-08-26 14:07:56 +0200

    TestSpelExpression context fix

--- scenario-compiler/src/test/scala/pl/touk/nussknacker/engine/definition/component/parameter/validator/ValidationExpressionParameterValidatorTest.scala
+++ scenario-compiler/src/test/scala/pl/touk/nussknacker/engine/definition/component/parameter/validator/ValidationExpressionParameterValidatorTest.scala
@@ -7,11 +7,13 @@ import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.expression.spel.support.StandardEvaluationContext
 import pl.touk.nussknacker.engine.api.parameter.ParameterName
 import pl.touk.nussknacker.engine.api.{Context, MetaData, NodeId, StreamMetaData}
+import pl.touk.nussknacker.engine.definition.component.parameter.validator.TestSpelExpression.expressionConfig
 import pl.touk.nussknacker.engine.definition.globalvariables.ExpressionConfigDefinition
 import pl.touk.nussknacker.engine.expression.ExpressionEvaluator
 import pl.touk.nussknacker.engine.expression.parse.CompiledExpression
 import pl.touk.nussknacker.engine.graph.expression.Expression
 import pl.touk.nussknacker.engine.graph.expression.Expression.Language
+import pl.touk.nussknacker.engine.spel.internal.EvaluationContextPreparer
 import pl.touk.nussknacker.engine.testing.ModelDefinitionBuilder
 import pl.touk.nussknacker.engine.variables.GlobalVariablesPreparer
 
@@ -20,16 +22,6 @@ import scala.jdk.CollectionConverters._
 class ValidationExpressionParameterValidatorTest extends AnyFunSuite with TableDrivenPropertyChecks with Matchers {
 
   private implicit val nodeId: NodeId = NodeId("someNode")
-
-  object Add {
-    def add(a: java.lang.Integer, b: java.lang.Integer): java.lang.Integer = a + b
-  }
-
-  private val expressionConfig: ExpressionConfigDefinition =
-    ModelDefinitionBuilder.empty
-      .withGlobalVariable("ADD", Add)
-      .build
-      .expressionConfig
 
   test("ValidationExpressionParameterValidator") {
     forAll(
@@ -49,7 +41,8 @@ class ValidationExpressionParameterValidatorTest extends AnyFunSuite with TableD
         ("#value.size() == 2 && #value[0] == 'foo'", "list", "{'foo', 'bar'}", Some(List("foo", "bar").asJava), true),
         ("#ADD.add(#value, 3) > 10", "param", "9", Some(9), true),
         ("#ADD.add(#value, 3) > 10", "param", "7", Some(7), false),
-//        ("#value == #meta.processName.toLowerCase", "param", "fooprocess", Some("fooprocess"), true),
+        ("#value == #meta.processName.toLowerCase", "param", "fooprocess", Some("fooprocess"), true),
+        ("#value == #meta.processName.toLowerCase", "param", "bar_baz", Some("bar_baz"), false),
       )
     ) { (validationExpression, paramName, inputExpression, value, isValid) =>
       ValidationExpressionParameterValidator(
@@ -72,10 +65,25 @@ private class TestSpelExpression(expression: String) extends CompiledExpression 
   override def original: String = expression
 
   override def evaluate[T](ctx: Context, globals: Map[String, Any]): T = {
-    val context = new StandardEvaluationContext()
-    ctx.variables.foreach { case (param, value) => context.setVariable(param, value) }
-    globals.foreach { case (name, value) => context.setVariable(name, value) }
-    new SpelExpressionParser().parseRaw(expression).getValue(context).asInstanceOf[T]
+    val evaluationContext = EvaluationContextPreparer
+      .default(getClass.getClassLoader, expressionConfig)
+      .prepareEvaluationContext(ctx, globals)
+
+    new SpelExpressionParser().parseRaw(expression).getValue(evaluationContext).asInstanceOf[T]
   }
+
+}
+
+object TestSpelExpression {
+
+  object Add {
+    def add(a: java.lang.Integer, b: java.lang.Integer): java.lang.Integer = a + b
+  }
+
+  val expressionConfig: ExpressionConfigDefinition =
+    ModelDefinitionBuilder.empty
+      .withGlobalVariable("ADD", Add)
+      .build
+      .expressionConfig
 
 }
