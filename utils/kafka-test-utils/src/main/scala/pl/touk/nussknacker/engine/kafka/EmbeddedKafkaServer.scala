21609059814f1f7d374c97ca3a8205e682167731
Arek Burdach <arek.burdach@gmail.com>
2024-08-27 23:09:33 +0200

    recreate work around

--- utils/kafka-test-utils/src/main/scala/pl/touk/nussknacker/engine/kafka/EmbeddedKafkaServer.scala
+++ utils/kafka-test-utils/src/main/scala/pl/touk/nussknacker/engine/kafka/EmbeddedKafkaServer.scala
@@ -33,12 +33,13 @@ object EmbeddedKafkaServer {
 
   def run(brokerPort: Int, controllerPort: Int, kafkaBrokerConfig: Map[String, String]): EmbeddedKafkaServer = {
     val tempDir     = Files.createTempDirectory("embeddedKafka").toFile
-    val kafkaConfig = prepareServerConfig(brokerPort, controllerPort, tempDir, kafkaBrokerConfig)
+    val clusterId   = Uuid.randomUuid()
+    val kafkaConfig = prepareServerConfig(brokerPort, controllerPort, tempDir, kafkaBrokerConfig, clusterId)
     val server = if (kRaftEnabled) {
-      prepareRaftStorage(tempDir, kafkaConfig)
+      prepareRaftStorage(tempDir, kafkaConfig, clusterId)
       new EmbeddedKafkaServer(
         None,
-        new KafkaRaftServer(kafkaConfig, time = Time.SYSTEM),
+        () => new KafkaRaftServer(kafkaConfig, time = Time.SYSTEM),
         s"$localhost:$brokerPort",
         tempDir
       )
@@ -46,7 +47,7 @@ object EmbeddedKafkaServer {
       val zk = createZookeeperServer(controllerPort)
       new EmbeddedKafkaServer(
         Some(zk),
-        new KafkaServer(kafkaConfig, time = Time.SYSTEM),
+        () => new KafkaServer(kafkaConfig, time = Time.SYSTEM),
         s"$localhost:$brokerPort",
         tempDir
       )
@@ -59,17 +60,21 @@ object EmbeddedKafkaServer {
       brokerPort: Int,
       controllerPort: Int,
       logDir: File,
-      kafkaBrokerConfig: Map[String, String]
+      kafkaBrokerConfig: Map[String, String],
+      clusterId: Uuid
   ) = {
     val properties = new Properties()
-    properties.setProperty("broker.id", "0")
     if (kRaftEnabled) {
+      properties.setProperty("node.id", "0")
       properties.setProperty("process.roles", "broker,controller")
       properties.setProperty("listeners", s"PLAINTEXT://$localhost:$brokerPort,CONTROLLER://$localhost:$controllerPort")
       properties.setProperty("listener.security.protocol.map", s"PLAINTEXT:PLAINTEXT,CONTROLLER:PLAINTEXT")
       properties.setProperty("controller.listener.names", s"CONTROLLER")
+      properties.setProperty("inter.broker.listener.name", "PLAINTEXT")
       properties.setProperty("controller.quorum.voters", s"0@$localhost:$controllerPort")
+      properties.setProperty("cluster.id", clusterId.toString)
     } else {
+      properties.setProperty("broker.id", "0")
       properties.setProperty("zookeeper.connect", s"$localhost:$controllerPort")
       properties.setProperty("listeners", s"PLAINTEXT://$localhost:$brokerPort")
     }
@@ -91,12 +96,11 @@ object EmbeddedKafkaServer {
     new server.KafkaConfig(properties)
   }
 
-  private def prepareRaftStorage(logDir: File, kafkaConfig: server.KafkaConfig) = {
-    val uuid = Uuid.randomUuid()
+  private def prepareRaftStorage(logDir: File, kafkaConfig: server.KafkaConfig, clusterId: Uuid) = {
     StorageTool.formatCommand(
       new PrintStream(NullOutputStream.NULL_OUTPUT_STREAM),
       Seq(logDir.getAbsolutePath),
-      StorageTool.buildMetadataProperties(uuid.toString, kafkaConfig),
+      StorageTool.buildMetadataProperties(clusterId.toString, kafkaConfig),
       MetadataVersion.MINIMUM_BOOTSTRAP_VERSION,
       ignoreFormatted = false
     )
@@ -114,10 +118,16 @@ object EmbeddedKafkaServer {
 
 class EmbeddedKafkaServer(
     zooKeeperServerOpt: Option[(NIOServerCnxnFactory, ZooKeeperServer, File)],
-    val kafkaServer: Server,
+    createKafkaServer: () => Server,
     val kafkaAddress: String,
     tempDir: File
 ) extends LazyLogging {
+
+  var kafkaServer: Server = createKafkaServer()
+
+  def recreateKafkaServer(): Unit = {
+    kafkaServer = createKafkaServer()
+  }
 
   def startup(): Unit = {
     zooKeeperServerOpt.foreach(t => t._1.startup(t._2))
