572a45667c8c29b8ab3f9487525ea4a1424b76ad
MK Software <mk_software_pl@protonmail.com>
2024-09-03 15:37:21 +0200

    wip

--- benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/e2e/BaseE2EBenchmark.scala
+++ benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/e2e/BaseE2EBenchmark.scala
@@ -5,13 +5,40 @@ import com.dimafeng.testcontainers.{DockerComposeContainer, ServiceLogConsumer, 
 import com.typesafe.scalalogging.LazyLogging
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.DockerHealthcheckWaitStrategy
-import pl.touk.nussknacker.engine.benchmarks.e2e.BaseE2EBenchmark.singletonContainer
+import pl.touk.nussknacker.engine.benchmarks.e2e.BaseE2EBenchmark.{JSON, singletonContainer}
+import pl.touk.nussknacker.test.containers.ContainerExt.toContainerExt
+import ujson.Value
 
 import java.io.{File => JFile}
 
 trait BaseE2EBenchmark {
 
   private val bootstrapSetupService = unsafeContainerByServiceName("bootstrap-setup")
+
+  def deployAndWaitForRunningState(scenarioName: String): Unit = {
+    bootstrapSetupService.executeBash(
+      s"""/app/utils/nu/deploy-scenario-and-wait-for-running-state.sh "$scenarioName" """
+    )
+  }
+
+  def sendMessageToKafka(topic: String, message: JSON): Unit = {
+    val escapedMessage = message.render().replaceAll("\"", "\\\\\"")
+    bootstrapSetupService.executeBash(s"""/app/utils/kafka/send-to-topic.sh "$topic" "$escapedMessage" """)
+  }
+
+  def sendMessagesToKafka(topic: String, messages: Iterable[JSON]): Unit = {
+    val escapedMessages = messages.map(_.render().replaceAll("\"", "\\\\\"")).mkString("\n")
+    bootstrapSetupService.executeBash(s"""/app/utils/kafka/send-to-topic.sh "$topic" "$escapedMessages" """)
+  }
+
+  def readAllMessagesFromKafka(topic: String): List[JSON] = {
+    bootstrapSetupService
+      .executeBashAndReadStdout(s"""/app/utils/kafka/read-from-topic.sh "$topic" """)
+      .split("\n")
+      .toList
+      .flatMap(str => Option.when(str.nonEmpty)(str))
+      .map(ujson.read(_))
+  }
 
   private def unsafeContainerByServiceName(name: String) = singletonContainer
     .getContainerByServiceName(name)
@@ -20,6 +47,8 @@ trait BaseE2EBenchmark {
 }
 
 object BaseE2EBenchmark extends LazyLogging {
+
+  type JSON = Value
 
   val singletonContainer: DockerComposeContainer = new DockerComposeContainer(
     composeFiles = Seq(
--- benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/e2e/Example.scala
+++ benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/e2e/Example.scala
@@ -3,13 +3,50 @@ package pl.touk.nussknacker.engine.benchmarks.e2e
 import cats.effect.{ExitCode, IO, IOApp}
 import com.typesafe.scalalogging.LazyLogging
 
+import java.time.{Duration, Instant}
+
 object Example extends IOApp with BaseE2EBenchmark with LazyLogging {
 
   override def run(args: List[String]): IO[ExitCode] =
     IO
       .delay {
-        logger.info("test")
+        val max = 100000
+        (1 to max).foreach(_ => sendTestMessages(100))
+        sendTestMessages(1, last = true)
+
+        deployAndWaitForRunningState("DetectLargeTransactions")
+
+        waitForLastMessage()
+
+        logger.info("DONE")
       }
       .map(_ => ExitCode.Success)
+
+  private def sendTestMessages(count: Int, last: Boolean = false): Unit = {
+    sendMessagesToKafka(
+      "transactions",
+      (1 to count).map { id =>
+        ujson.Obj(
+          "amount"   -> 100,
+          "clientId" -> s"$id",
+          "isLast"   -> last
+        )
+      }
+    )
+  }
+
+  private def waitForLastMessage(): Unit = {
+    def foundLastRequest = readAllMessagesFromKafka("alerts")
+      .exists(json => json.obj.get("message").exists(_.str == "Last request"))
+
+    val started = Instant.now()
+
+    while (!foundLastRequest) {
+      Thread.sleep(100)
+    }
+
+    val duration = Duration.between(started, Instant.now())
+    logger.info(s"Took: ${duration.toSeconds} sec")
+  }
 
 }
