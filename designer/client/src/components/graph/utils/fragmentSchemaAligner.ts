30c2fefe908d0a5c63ab2adcad271aa7821bb062
Dawid Poliszak <dpl@touk.pl>
2024-09-03 09:44:59 +0200

    [Nu-1792] properties save fix when fragment is in different group than fragments (#6725)
    * NU-1792 properties save fix when fragment is in a different group than fragments
    
    * NU-1792 format message
    
    (cherry picked from commit bb82d34d7f526da505fb6219c16e11b95d542f92)

--- designer/client/src/components/graph/utils/fragmentSchemaAligner.ts
+++ designer/client/src/components/graph/utils/fragmentSchemaAligner.ts
@@ -1,13 +1,19 @@
 import fp from "lodash/fp";
 import { NodeType, ProcessDefinitionData } from "../../../types";
 
+/*
+ * TODO: It's a workaround
+ * There may be situations where an open scenario has its fragment parameters edited by a different owner
+ * In such cases, we need to align the fragmentInput properties with the updated state of the fragment process definition data.
+ */
 export function alignFragmentWithSchema(processDefinitionData: ProcessDefinitionData, fragmentNode: NodeType) {
     const fragmentId = fragmentNode.ref.id;
     const fragmentSchema = processDefinitionData.componentGroups
-        .find((componentGroups) => {
-            return componentGroups.name === "fragments";
-        })
-        .components.find((obj) => obj.node.ref.id === fragmentId);
+        // This is a workaround. We cannot look for the fragment schema only in the fragments group,
+        // because fragmentInput can be moved to a different group.
+        // In this case, there was an error, which is why we need to iterate through all groups.
+        .flatMap((componentGroups) => componentGroups.components)
+        .find((obj) => obj?.node?.ref?.id === fragmentId);
     const fragmentSchemaParameters = fragmentSchema.node.ref.parameters;
     const mergedParameters = fragmentSchemaParameters.map(
         (param) => fragmentNode.ref.parameters.find((nodeParam) => nodeParam.name === param.name) || param,
