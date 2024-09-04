747ee82b62504f2d1560df916ac544acdcbc9db4
MK Software <mk_software_pl@protonmail.com>
2024-09-04 11:55:38 +0200

    wip

--- benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/e2e/FlinkSteamingScenarioBenchmark.scala
+++ benchmarks/src/test/scala/pl/touk/nussknacker/engine/benchmarks/e2e/FlinkSteamingScenarioBenchmark.scala
@@ -1,24 +1,31 @@
 package pl.touk.nussknacker.engine.benchmarks.e2e
 
+import better.files.File.root
 import cats.effect.{ExitCode, IO, IOApp}
 import com.typesafe.scalalogging.LazyLogging
 
 import java.time.{Duration, Instant}
+import scala.concurrent.duration._
 import scala.util.Random
 
 object FlinkSteamingScenarioBenchmark extends IOApp with BaseE2EBenchmark with LazyLogging {
 
   override def run(args: List[String]): IO[ExitCode] = {
     for {
-      messagesCount <- readBenchmarkMessagesCount(args)
-      _             <- log("Starting message generation...")
-      _             <- generateBenchmarkMessages(messagesCount)
-      _             <- log("Generation finished!")
-      _             <- log("Starting benchmark scenario...")
-      _             <- runScenarioAndStartProcessing()
-      _             <- log("Scenario started. Messages are processing...")
-      time          <- measureProcessingTime()
-      _             <- log(s"All messages processed. It took ${time.toSeconds} seconds")
+      messagesCount        <- readBenchmarkMessagesCount(args)
+      _                    <- log("Starting message generation...")
+      prepareDataDuration  <- measure(generateBenchmarkMessages(messagesCount))
+      _                    <- log("Generation finished!")
+      _                    <- log("Starting benchmark scenario...")
+      runScenarioDuration  <- measure(runScenarioAndStartProcessing())
+      _                    <- log("Scenario started. Messages are processing...")
+      verifyResultDuration <- measure(waitForProcessingFinish())
+      _                    <- log(s"All messages processed.")
+      _                    <- saveMeasurementsToFile(prepareDataDuration, runScenarioDuration, verifyResultDuration)
+      _                    <- log(s"Results:")
+      _                    <- log(s"- Preparing data: ${prepareDataDuration.duration.toSeconds} seconds")
+      _                    <- log(s"- Running scenario: ${runScenarioDuration.duration.toSeconds} seconds")
+      _                    <- log(s"- Verifying result: ${verifyResultDuration.duration.toSeconds} seconds")
     } yield ExitCode.Success
   }
 
@@ -49,17 +56,45 @@ object FlinkSteamingScenarioBenchmark extends IOApp with BaseE2EBenchmark with L
     deployAndWaitForRunningState("DetectLargeTransactions")
   }
 
-  private def measureProcessingTime() = IO.delay {
-    def foundLastRequest = readAllMessagesFromKafka("alerts")
-      .exists(json => json.obj.get("message").exists(_.str == "Last request"))
-
-    val started = Instant.now()
-
-    while (!foundLastRequest) {
-      Thread.sleep(100)
+  private def waitForProcessingFinish(): IO[Unit] = {
+    def foundLastRequest(): IO[Boolean] = IO.delay {
+      readAllMessagesFromKafka("alerts")
+        .exists(json => json.obj.get("message").exists(_.str == "Last request"))
     }
 
-    Duration.between(started, Instant.now())
+    foundLastRequest()
+      .flatMap {
+        case false =>
+          IO
+            .sleep(100 millis)
+            .flatMap(_ => waitForProcessingFinish())
+        case true =>
+          IO.pure(())
+      }
+  }
+
+  private def saveMeasurementsToFile(
+      prepareData: Measured[_],
+      runScenario: Measured[_],
+      verifyResult: Measured[_]
+  ): IO[Unit] = IO.delay {
+    def format(measured: Measured[_]) = "%.2f".format(measured.duration.toMillis.toDouble)
+
+    (root / "tmp" / "benchmarkResult.csv")
+      .createFileIfNotExists()
+      .clear()
+      .appendLine(s"prepareData,${format(prepareData)}")
+      .appendLine(s"runScenario,${format(runScenario)}")
+      .appendLine(s"verifyResult,${format(verifyResult)}")
+      .appendLine()
+  }
+
+  private def measure[T](action: IO[T]): IO[Measured[T]] = {
+    for {
+      started  <- IO.delay(Instant.now())
+      result   <- action
+      duration <- IO.delay(Duration.between(started, Instant.now()))
+    } yield Measured(result, duration)
   }
 
   private def sendTestMessages(batchId: Int, batchSize: Int): Unit = {
@@ -77,3 +112,5 @@ object FlinkSteamingScenarioBenchmark extends IOApp with BaseE2EBenchmark with L
 
   private def log(message: => String) = IO.delay(logger.info(message))
 }
+
+final case class Measured[T](result: T, duration: Duration)
