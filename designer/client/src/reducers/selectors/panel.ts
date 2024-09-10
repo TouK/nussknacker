7e18b882c79057ffbbc78fe0a24357a023993641
JulianWielga <j@touk.pl>
2024-09-10 15:27:55 +0200

    collapsed panels fixed (#6728)
    * collapsed panels fixed
    
    * Updated snapshots (#6829)
    
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: JulianWielga <JulianWielga@users.noreply.github.com>

--- designer/client/src/reducers/selectors/panel.ts
+++ designer/client/src/reducers/selectors/panel.ts
@@ -1,8 +1,5 @@
 import { createSelector } from "reselect";
+import { defaultState } from "../panel";
 import { getToolbars } from "./toolbars";
-import { Panels } from "../panel";
 
-export const panelsState = createSelector(getToolbars, (t) => t.panels || ({} as Panels));
-
-export const isLeftPanelOpened = createSelector(panelsState, (panels) => panels?.LEFT);
-export const isRightPanelOpened = createSelector(panelsState, (panels) => panels?.RIGHT);
+export const panelsState = createSelector(getToolbars, (t) => ({ ...defaultState, ...t.panels }));
