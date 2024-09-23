c076e093bf3977da1f41db4f4f0993c467c42838
Dawid Poliszak <dpl@touk.pl>
2024-09-23 09:12:29 +0200

    NU-1778 provide an option to open specific version

--- designer/client/src/components/toolbars/activities/ActivityItem.tsx
+++ designer/client/src/components/toolbars/activities/ActivityItem.tsx
@@ -5,12 +5,11 @@ import { formatDateTime } from "../../../common/DateUtils";
 import CommentContent from "../../comment/CommentContent";
 import { createSelector } from "reselect";
 import { getFeatureSettings } from "../../../reducers/selectors/settings";
-import { ActionMetadata } from "../../../http/HttpService";
 import { blend } from "@mui/system";
-import { blendLighten, getBorderColor } from "../../../containers/theme/helpers";
-import UrlIcon from "../../UrlIcon";
+import { blendLighten } from "../../../containers/theme/helpers";
 import { ItemActivity } from "./ActivitiesPanel";
 import { SearchHighlighter } from "../creator/SearchHighlighter";
+import ActivityItemHeader from "./ActivityItemHeader";
 
 const StyledActivityRoot = styled("div")(({ theme }) => ({
     padding: `${theme.spacing(1)} ${theme.spacing(1)} ${theme.spacing(2)}`,
@@ -30,54 +29,11 @@ const StyledActivityContent = styled("div")<{ isActiveFound: boolean; isFound: b
         : "none",
 }));
 
-const StyledActivityHeader = styled("div")<{ isHighlighted: boolean; isActive: boolean }>(({ theme, isHighlighted, isActive }) => ({
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0.5),
-    backgroundColor: isActive
-        ? blend(theme.palette.background.paper, theme.palette.primary.main, 0.2)
-        : isHighlighted
-        ? blend(theme.palette.background.paper, theme.palette.primary.main, 0.05)
-        : undefined,
-    border: (isActive || isHighlighted) && `1px solid ${getBorderColor(theme)}`,
-    borderRadius: theme.spacing(1),
-}));
 const StyledActivityBody = styled("div")(({ theme }) => ({
     margin: theme.spacing(1),
 }));
-const StyledHeaderIcon = styled(UrlIcon)(({ theme }) => ({
-    width: "16px",
-    height: "16px",
-    marginRight: theme.spacing(1),
-}));
-
-const StyledHeaderActionIcon = styled(UrlIcon)(() => ({
-    width: "16px",
-    height: "16px",
-    marginLeft: "auto",
-    cursor: "pointer",
-}));
 
 const getCommentSettings = createSelector(getFeatureSettings, (f) => f.commentSettings || {});
-
-const HeaderActivity = ({ activityAction }: { activityAction: ActionMetadata }) => {
-    switch (activityAction.id) {
-        case "compare": {
-            return (
-                <StyledHeaderActionIcon
-                    onClick={() => {
-                        alert(`action called: ${activityAction.id}`);
-                    }}
-                    key={activityAction.id}
-                    src={activityAction.icon}
-                />
-            );
-        }
-        default: {
-            return null;
-        }
-    }
-};
 
 export const ActivityItem = forwardRef(
     (
@@ -86,27 +42,17 @@ export const ActivityItem = forwardRef(
     ) => {
         const commentSettings = useSelector(getCommentSettings);
 
-        const isHighlighted = ["SCENARIO_DEPLOYED", "SCENARIO_CANCELED"].includes(activity.type);
         const version = `Version: ${activity.scenarioVersionId}`;
 
         return (
             <StyledActivityRoot ref={ref}>
                 <StyledActivityContent isActiveFound={activity.isActiveFound} isFound={activity.isFound}>
-                    <StyledActivityHeader isHighlighted={isHighlighted} isActive={isActiveItem}>
-                        <StyledHeaderIcon src={activity.activities.icon} />
-                        <Typography
-                            variant={"caption"}
-                            component={SearchHighlighter}
-                            highlights={[searchQuery]}
-                            sx={(theme) => ({ color: theme.palette.text.primary })}
-                        >
-                            {activity.activities.displayableName}
-                        </Typography>
-
-                        {activity.actions.map((activityAction) => (
-                            <HeaderActivity key={activityAction.id} activityAction={activityAction} />
-                        ))}
-                    </StyledActivityHeader>
+                    <ActivityItemHeader
+                        isActiveItem={isActiveItem}
+                        searchQuery={searchQuery}
+                        activity={activity}
+                        scenarioVersion={activity.scenarioVersionId}
+                    />
                     <StyledActivityBody>
                         <Box display={"flex"} alignItems={"center"} justifyContent={"flex-start"}>
                             <Typography component={SearchHighlighter} highlights={[searchQuery]} variant={"overline"}>
---
+++ designer/client/src/components/toolbars/activities/ActivityItemHeader.tsx
@@ -0,0 +1,149 @@
+import React, { PropsWithChildren, useCallback, useMemo } from "react";
+import { Button, styled, Typography } from "@mui/material";
+import { SearchHighlighter } from "../creator/SearchHighlighter";
+import { ActionMetadata } from "../../../http/HttpService";
+import UrlIcon from "../../UrlIcon";
+import { blend } from "@mui/system";
+import { getBorderColor } from "../../../containers/theme/helpers";
+import { unsavedProcessChanges } from "../../../common/DialogMessages";
+import { useDispatch, useSelector } from "react-redux";
+import { getScenario, isSaveDisabled } from "../../../reducers/selectors/graph";
+import { useWindows } from "../../../windowManager";
+import { displayScenarioVersion } from "../../../actions/nk";
+import { ItemActivity } from "./ActivitiesPanel";
+
+const StyledHeaderIcon = styled(UrlIcon)(({ theme }) => ({
+    width: "16px",
+    height: "16px",
+    marginRight: theme.spacing(1),
+}));
+
+const StyledHeaderActionIcon = styled(UrlIcon)(() => ({
+    width: "16px",
+    height: "16px",
+    marginLeft: "auto",
+    cursor: "pointer",
+}));
+
+const StyledActivityItemHeader = styled("div")<{ isHighlighted: boolean; isActive: boolean }>(({ theme, isHighlighted, isActive }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0.5),
+    backgroundColor: isActive
+        ? blend(theme.palette.background.paper, theme.palette.primary.main, 0.2)
+        : isHighlighted
+        ? blend(theme.palette.background.paper, theme.palette.primary.main, 0.05)
+        : undefined,
+    border: (isActive || isHighlighted) && `1px solid ${getBorderColor(theme)}`,
+    borderRadius: theme.spacing(1),
+}));
+
+const HeaderActivity = ({ activityAction }: { activityAction: ActionMetadata }) => {
+    switch (activityAction.id) {
+        case "compare": {
+            return (
+                <StyledHeaderActionIcon
+                    onClick={() => {
+                        alert(`action called: ${activityAction.id}`);
+                    }}
+                    key={activityAction.id}
+                    src={activityAction.icon}
+                />
+            );
+        }
+        default: {
+            return null;
+        }
+    }
+};
+
+interface Props {
+    activity: ItemActivity;
+    isActiveItem: boolean;
+    searchQuery: string;
+    scenarioVersion: number;
+}
+
+const WithOpenVersion = ({
+    scenarioVersion,
+    children,
+}: PropsWithChildren<{
+    scenarioVersion: number;
+}>) => {
+    const nothingToSave = useSelector(isSaveDisabled);
+    const scenario = useSelector(getScenario);
+    const { name } = scenario || {};
+    const dispatch = useDispatch();
+    const { confirm } = useWindows();
+
+    const doChangeVersion = useCallback(
+        (scenarioId: number) => {
+            console.log(scenarioId);
+            dispatch(displayScenarioVersion(name, scenarioId));
+        },
+        [dispatch, name],
+    );
+
+    const changeVersion = useCallback(
+        (scenarioId: number) =>
+            nothingToSave
+                ? doChangeVersion(scenarioId)
+                : confirm({
+                      text: unsavedProcessChanges(),
+                      onConfirmCallback: (confirmed) => confirmed && doChangeVersion(scenarioId),
+                      confirmText: "DISCARD",
+                      denyText: "CANCEL",
+                  }),
+        [confirm, doChangeVersion, nothingToSave],
+    );
+
+    return (
+        <Button
+            sx={{ textTransform: "initial", p: 0, m: 0 }}
+            onClick={() => {
+                changeVersion(scenarioVersion);
+            }}
+        >
+            {children}
+        </Button>
+    );
+};
+
+const ActivityItemHeader = ({ activity, isActiveItem, searchQuery }: Props) => {
+    const scenario = useSelector(getScenario);
+    const { processVersionId } = scenario || {};
+
+    const isHighlighted = ["SCENARIO_DEPLOYED", "SCENARIO_CANCELED"].includes(activity.type);
+    const openVersionEnable = activity.type === "SCENARIO_MODIFIED" && activity.scenarioVersionId !== processVersionId;
+
+    const getHeaderTitle = useMemo(() => {
+        const headerTitle = (
+            <Typography
+                variant={"caption"}
+                component={SearchHighlighter}
+                highlights={[searchQuery]}
+                sx={(theme) => ({ color: theme.palette.text.primary })}
+            >
+                {activity.activities.displayableName}
+            </Typography>
+        );
+
+        if (openVersionEnable) {
+            return <WithOpenVersion scenarioVersion={activity.scenarioVersionId}>{headerTitle}</WithOpenVersion>;
+        }
+
+        return headerTitle;
+    }, [activity.activities.displayableName, activity.scenarioVersionId, openVersionEnable, searchQuery]);
+
+    return (
+        <StyledActivityItemHeader isHighlighted={isHighlighted} isActive={isActiveItem}>
+            <StyledHeaderIcon src={activity.activities.icon} />
+            {getHeaderTitle}
+            {activity.actions.map((activityAction) => (
+                <HeaderActivity key={activityAction.id} activityAction={activityAction} />
+            ))}
+        </StyledActivityItemHeader>
+    );
+};
+
+export default ActivityItemHeader;
