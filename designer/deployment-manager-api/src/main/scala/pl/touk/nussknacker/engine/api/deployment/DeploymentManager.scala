909f49134d06d938d2c9d27a10eab0f9cac9ed28
Michał Goworko <m.goworko.pl@gmail.com>
2024-10-03 16:51:40 +0200

    TODO comment

--- designer/deployment-manager-api/src/main/scala/pl/touk/nussknacker/engine/api/deployment/DeploymentManager.scala
+++ designer/deployment-manager-api/src/main/scala/pl/touk/nussknacker/engine/api/deployment/DeploymentManager.scala
@@ -73,6 +73,16 @@ trait DeploymentManager extends AutoCloseable {
   protected final def notImplemented: Future[Nothing] =
     Future.failed(new NotImplementedError())
 
+  // todo NU-1772
+  //  In the current implementation:
+  //    - the DeploymentManager is a kind of plugin, and it may have its own data source (separate db, cache, etc.)
+  //    - the DeploymentManager may return (by implementing this method) custom ScenarioActivities, that are associated with operations performed internally by the manager
+  //  Why is it not the ideal solution:
+  //    - we have different data sources for ScenarioActivities, and merging data from two sources may be problematic, e.g. when paginating results
+  //  How can it be redesigned:
+  //    - we could do it the other way round - we could provide the hook, that would allow the manager to save its custom activities to the main ScenarioActivity repository
+  //    - only the ScenarioActivities repo would then be used, so it would require migrating old data from DeploymentManager-specific data source
+  //    - we may need to refactor PeriodicDeploymentManager data source first, that is why it was not implemented that way in the first place
   def managerSpecificScenarioActivities(processIdWithName: ProcessIdWithName): Future[List[ScenarioActivity]]
 
 }
