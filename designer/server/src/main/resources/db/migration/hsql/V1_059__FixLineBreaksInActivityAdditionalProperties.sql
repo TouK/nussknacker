53f324b7295b0dd9ac1bfe37d6c1f52fc58fc348
Michał Goworko <m.goworko.pl@gmail.com>
2024-10-24 13:24:44 +0200

    refactor

--- designer/server/src/main/resources/db/migration/hsql/V1_059__FixLineBreaksInActivityAdditionalProperties.sql
+++ designer/server/src/main/resources/db/migration/hsql/V1_059__FixLineBreaksInActivityAdditionalProperties.sql
@@ -1,3 +1,3 @@
-UPDATE scenario_activities
-SET additional_properties = REPLACE(additional_properties, CHAR(10), '\n')
-WHERE additional_properties LIKE '%' || CHAR(10) || '%';
+UPDATE "scenario_activities"
+SET "additional_properties" = REPLACE("additional_properties", CHAR(10), '\n')
+WHERE "additional_properties" LIKE '%' || CHAR(10) || '%';
--- designer/server/src/main/resources/db/migration/postgres/V1_059__FixLineBreaksInActivityAdditionalProperties.sql
+++ designer/server/src/main/resources/db/migration/postgres/V1_059__FixLineBreaksInActivityAdditionalProperties.sql
@@ -1,3 +1,3 @@
-UPDATE scenario_activities
-SET additional_properties = REPLACE(additional_properties, CHR(10), '\n')
-WHERE additional_properties LIKE '%' || CHR(10) || '%';
+UPDATE "scenario_activities"
+SET "additional_properties" = REPLACE("additional_properties", CHR(10), '\n')
+WHERE "additional_properties" LIKE '%' || CHR(10) || '%';
