41802557fce2068c5952ac240f803534c9f2a42c
Filip Michalski <fmc@touk.pl>
2024-11-04 15:48:02 +0100

    Add PoC httpApiService for StickyNotes - add slick migration

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/server/AkkaHttpBasedRouteProvider.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/server/AkkaHttpBasedRouteProvider.scala
@@ -407,6 +407,14 @@ class AkkaHttpBasedRouteProvider(
         scenarioService = processService,
       )
 
+      val stickyNotesApiHttpService = new StickyNotesApiHttpService(
+        authManager = authManager,
+        stickyNotesRepository = stickyNotesRepository,
+        scenarioService = processService,
+        scenarioAuthorizer = processAuthorizer,
+        dbioRunner
+      )
+
       val scenarioActivityApiHttpService = new ScenarioActivityApiHttpService(
         authManager = authManager,
         fetchScenarioActivityService = fetchScenarioActivityService,
@@ -599,6 +607,7 @@ class AkkaHttpBasedRouteProvider(
           scenarioActivityApiHttpService,
           scenarioLabelsApiHttpService,
           scenarioParametersHttpService,
+          stickyNotesApiHttpService,
           userApiHttpService,
           statisticsApiHttpService
         )
