460b354bbf63456c0afb275843fa8ba4d3efdc87
Filip Michalski <fmc@touk.pl>
2024-11-04 15:48:02 +0100

    Add PoC httpApiService for StickyNotes - add slick migration

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/api/description/stickynotes/StickyNoteEvent.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/api/description/stickynotes/StickyNoteEvent.scala
@@ -1,6 +1,14 @@
 package pl.touk.nussknacker.ui.api.description.stickynotes
 
+import io.circe.{Decoder, Encoder}
+
 object StickyNoteEvent extends Enumeration {
+  implicit val typeEncoder: Encoder[StickyNoteEvent.Value] = Encoder.encodeEnumeration(StickyNoteEvent)
+  implicit val typeDecoder: Decoder[StickyNoteEvent.Value] = Decoder.decodeEnumeration(StickyNoteEvent)
+
   type StickyNoteEvent = Value
-  val StickyNoteCreated, StickyNoteUpdated, StickyNoteDeleted = Value
+  val StickyNoteCreated: Value = Value("CREATED")
+  val StickyNoteUpdated: Value = Value("UPDATED")
+  val StickyNoteDeleted: Value = Value("DELETED")
+
 }
