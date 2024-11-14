9b0dc6140e96d6e4f360e30e9706d5dd678d3ac2
Filip Michalski <fmc@touk.pl>
2024-11-14 11:51:38 +0100

    Disable stickyNotes when scenario is not saved

--- designer/client/src/components/stickyNotes/StickyNotesPanel.tsx
+++ designer/client/src/components/stickyNotes/StickyNotesPanel.tsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { ToolbarPanelProps } from "../toolbarComponents/DefaultToolbarPanel";
 import { ToolbarWrapper } from "../toolbarComponents/toolbarWrapper/ToolbarWrapper";
 import Tool from "../toolbars/creator/Tool";
 import { StyledToolbox } from "../toolbars/creator/ToolBox";
 import { StickyNoteType } from "../../types/stickyNote";
+import { useSelector } from "react-redux";
+import { isPristine } from "../../reducers/selectors/graph";
 
 export function StickyNotesPanel(props: ToolbarPanelProps): JSX.Element {
     const { t } = useTranslation();
-
+    const pristine = useSelector(isPristine);
     const noteModel = { id: "StickyNote", type: StickyNoteType, isDisabled: false };
 
-    return (
-        <ToolbarWrapper {...props} title={t("panels.sticky-notes.title", "Sticky Notes Panel")}>
-            <StyledToolbox id="toolbox">
-                <Tool nodeModel={noteModel} label="sticky note" />
-            </StyledToolbox>
-        </ToolbarWrapper>
-    );
+    //One of stickyNotes simplifications is that we cannot add note to not saved scenario version
+    const stickyNoteTool = useMemo(() => {
+        return (
+            <ToolbarWrapper {...props} title={t("panels.sticky-notes.title", "Sticky Notes Panel")}>
+                <StyledToolbox id="toolbox">
+                    <Tool nodeModel={noteModel} label="sticky note" key={StickyNoteType} disabled={!pristine} />
+                </StyledToolbox>
+            </ToolbarWrapper>
+        );
+    }, [pristine]);
+    return <>{stickyNoteTool}</>;
 }
