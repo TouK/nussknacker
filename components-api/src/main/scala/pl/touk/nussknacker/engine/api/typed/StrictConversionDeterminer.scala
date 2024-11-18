b269fa6a90a9e54996e4ea346c7a3afaa2f820d5
Marcel Philipiak <mph@touk.pl>
2024-11-18 16:57:39 +0100

    feat: Add StrictConversionDeterminer for type conversion validation

---
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/StrictConversionDeterminer.scala
@@ -0,0 +1,39 @@
+package pl.touk.nussknacker.engine.api.typed
+
+import cats.data.Validated.condNel
+import cats.data.ValidatedNel
+import org.apache.commons.lang3.ClassUtils
+import pl.touk.nussknacker.engine.api.typed.supertype.NumberTypesPromotionStrategy.AllNumbers
+import pl.touk.nussknacker.engine.api.typed.typing.{SingleTypingResult, TypedClass, TypingResult}
+
+object StrictConversionDeterminer {
+
+  def canBeConvertedTo(givenType: TypingResult, superclassCandidate: TypingResult): ValidatedNel[String, Unit] = {
+  ImplicitConversionDeterminer.canBeConvertedTo(givenType, superclassCandidate)
+  }
+
+    def singleCanBeConvertedTo(
+      givenType: SingleTypingResult,
+      superclassCandidate: SingleTypingResult
+  ): ValidatedNel[String, Unit] = {
+    val givenClass     = givenType.runtimeObjType
+    val givenSuperclas = superclassCandidate.runtimeObjType
+
+    isStrictSubclass(givenClass, givenSuperclas)
+  }
+
+  def canBeStrictSubclassOf(givenType: TypingResult, superclassCandidate: TypingResult): ValidatedNel[String, Unit] = {
+    this.canBeConvertedTo(givenType, superclassCandidate)
+  }
+
+  override def isAssignable(from: Class[_], to: Class[_]): Boolean = {
+    (from, to) match {
+      case (f, t) if ClassUtils.isAssignable(f, t, true) => true
+      // Number double check by hand because lang3 can incorrectly throw false when dealing with java types
+      case (f, t) if AllNumbers.contains(f) && AllNumbers.contains(t) =>
+        AllNumbers.indexOf(f) >= AllNumbers.indexOf(t)
+      case _ => false
+    }
+  }
+
+}
