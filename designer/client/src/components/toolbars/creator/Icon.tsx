4e2e3990f4b441bbe87d882930a9f2bbf70525ca
Dawid Poliszak <dpl@touk.pl>
2024-11-19 12:58:03 +0100

    NU-1858 provide fallback component to the Icon

--- designer/client/src/components/toolbars/creator/Icon.tsx
+++ designer/client/src/components/toolbars/creator/Icon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PlaceholderIcon from "../../../components/common/error-boundary/images/placeholder-icon.svg";
 
 type IconProps = {
     className?: string;
@@ -6,6 +7,10 @@ type IconProps = {
 };
 
 export function Icon({ className, src }: IconProps) {
+    if (!src) {
+        return <PlaceholderIcon className={className} />;
+    }
+
     return (
         <svg className={className}>
             <use href={src} />
