9f35b1f30794c9436ac49a7c5a8366140d05e241
Filip Michalski <fmc@touk.pl>
2024-11-19 20:03:58 +0100

    Add stickyNotes length and count validation, add stickyNotes configuration, fix error method (or did i?)

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/server/AkkaHttpBasedRouteProvider.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/server/AkkaHttpBasedRouteProvider.scala
@@ -412,7 +412,8 @@ class AkkaHttpBasedRouteProvider(
         stickyNotesRepository = stickyNotesRepository,
         scenarioService = processService,
         scenarioAuthorizer = processAuthorizer,
-        dbioRunner
+        dbioRunner,
+        stickyNotesSettings = featureTogglesConfig.stickyNotesSettings
       )
 
       val scenarioActivityApiHttpService = new ScenarioActivityApiHttpService(
