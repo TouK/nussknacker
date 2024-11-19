b5360d088318160f416dbb5da520aac9814487f4
Julian Wielga <j@touk.pl>
2024-11-19 16:13:12 +0100

    allow RemoteModuleDialog buttons customization

--- designer/client/src/components/RemoteModuleDialog.tsx
+++ designer/client/src/components/RemoteModuleDialog.tsx
@@ -1,5 +1,6 @@
 import { ModuleUrl } from "@touk/federated-component";
 import { WindowContentProps } from "@touk/window-manager";
+import type { FooterButtonProps } from "@touk/window-manager/cjs/components/window/footer";
 import React, { useCallback, useMemo, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { WindowContent, WindowKind } from "../windowManager";
@@ -9,55 +10,49 @@ import { RemoteComponent } from "./RemoteComponent";
 export type RemoteModuleDialogProps = NonNullable<unknown>;
 export type RemoteModuleDialogRef = NonNullable<{
     closeAction?: () => Promise<void>;
-    confirmAction?: () => Promise<void>;
+    adjustButtons: (buttons: { closeButton: FooterButtonProps; confirmButton: FooterButtonProps }) => FooterButtonProps[];
 }>;
 
-export function RemoteModuleDialog<P extends NonNullable<unknown>>(
-    props: WindowContentProps<
-        WindowKind,
-        {
-            url: ModuleUrl;
-        } & P
-    >,
-): JSX.Element {
+export function RemoteModuleDialog<P extends NonNullable<unknown>>({
+    close,
+    ...props
+}: WindowContentProps<WindowKind, { url: ModuleUrl } & P>): JSX.Element {
     const {
         data: { meta: passProps },
     } = props;
 
     const ref = useRef<RemoteModuleDialogRef>();
 
-    const close = useCallback(() => {
-        Promise.all([ref.current?.closeAction?.()]).then(() => {
-            props.close();
-        });
-    }, [props]);
+    const closeAction = useCallback(async () => {
+        await Promise.all([ref.current?.closeAction?.()]);
+        close();
+    }, [close]);
 
     const { t } = useTranslation();
 
-    const buttons = useMemo(
-        () => [
-            {
-                title: t("dialog.button.cancel", "cancel"),
-                action: () => {
-                    close();
-                },
-                className: LoadingButtonTypes.secondaryButton,
-            },
-            {
-                title: t("dialog.button.ok", "OK"),
-                action: () => {
-                    Promise.all([ref.current?.confirmAction?.()]).then(() => {
-                        props.close();
-                    });
-                },
-                disabled: !ref.current?.confirmAction,
-            },
-        ],
-        [close, props, t],
+    const closeButton = useMemo<FooterButtonProps>(
+        () => ({
+            title: t("dialog.button.cancel", "cancel"),
+            action: closeAction,
+            className: LoadingButtonTypes.secondaryButton,
+        }),
+        [closeAction, t],
+    );
+
+    const confirmButton = useMemo<FooterButtonProps>(
+        () => ({
+            title: t("dialog.button.ok", "OK"),
+            action: closeAction,
+        }),
+        [closeAction, t],
     );
 
     return (
-        <WindowContent {...props} close={close} buttons={buttons}>
+        <WindowContent
+            {...props}
+            close={close}
+            buttons={ref.current?.adjustButtons({ closeButton, confirmButton }) || [closeButton, confirmButton]}
+        >
             <RemoteComponent<RemoteModuleDialogProps, RemoteModuleDialogRef> ref={ref} {...passProps} />
         </WindowContent>
     );
