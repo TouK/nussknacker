237f3b7dd8313741205047c9526c8396071336fc
Michał Goworko <m.goworko.pl@gmail.com>
2024-11-20 17:07:47 +0100

    fix cypress tests

--- designer/client/cypress/e2e/connectionError.cy.ts
+++ designer/client/cypress/e2e/connectionError.cy.ts
@@ -59,13 +59,13 @@ describe("Connection error", () => {
             cy.get("[model-id='filter']").dblclick();
             cy.wait("@validation");
 
-            cy.intercept("/api/notifications/*", { statusCode: 502 });
+            cy.intercept("/api/notifications", { statusCode: 502 });
             cy.contains(/Backend connection issue/).should("be.visible");
             cy.get("body").matchImage({
                 screenshotConfig,
             });
 
-            cy.intercept("/api/notifications/*", (req) => {
+            cy.intercept("/api/notifications", (req) => {
                 req.continue();
             });
 
