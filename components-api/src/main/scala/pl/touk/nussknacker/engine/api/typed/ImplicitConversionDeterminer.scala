07af990220b1dd15181b3e9f489b0225be979497
Marcel Philipiak <mph@touk.pl>
2024-11-21 18:32:50 +0100

    pr changes - delte test cases

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/ImplicitConversionDeterminer.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/ImplicitConversionDeterminer.scala
@@ -4,6 +4,13 @@ import cats.data.Validated._
 import cats.data.{NonEmptyList, Validated, ValidatedNel}
 import cats.implicits.{catsSyntaxValidatedId, _}
 import org.apache.commons.lang3.ClassUtils
+import pl.touk.nussknacker.engine.api.typed.StrictConversionDeterminer.{
+  canBeConvertedTo,
+  canNullBeConvertedTo,
+  isAssignable,
+  isStrictSubclass,
+  singleCanBeConvertedTo
+}
 import pl.touk.nussknacker.engine.api.typed.typing._
 
 /**
@@ -38,6 +45,50 @@ object ImplicitConversionDeterminer {
       case (given: TypedUnion, superclass: TypedUnion) =>
         canBeConvertedTo(given.possibleTypes, superclass.possibleTypes)
     }
+  }
+
+  def canBeStrictlyConvertedTo(
+      givenType: TypingResult,
+      superclassCandidate: TypingResult
+  ): ValidatedNel[String, Unit] = {
+    (givenType, superclassCandidate) match {
+      case (_, Unknown)       => ().validNel
+      case (Unknown, _)       => ().validNel
+      case (TypedNull, other) => canNullBeConvertedTo(other)
+      case (_, TypedNull)     => s"No type can be subclass of ${TypedNull.display}".invalidNel
+      case (given: SingleTypingResult, superclass: TypedUnion) =>
+        canBeConvertedTo(NonEmptyList.one(given), superclass.possibleTypes)
+      case (given: TypedUnion, superclass: SingleTypingResult) =>
+        canBeConvertedTo(given.possibleTypes, NonEmptyList.one(superclass))
+      case (given: SingleTypingResult, superclass: SingleTypingResult) =>
+        singleCanBeStrictlyConvertedTo(given, superclass)
+      case (given: TypedUnion, superclass: TypedUnion) =>
+        canBeConvertedTo(given.possibleTypes, superclass.possibleTypes)
+    }
+
+  }
+
+  def singleCanBeStrictlyConvertedTo(
+      givenType: SingleTypingResult,
+      superclassCandidate: SingleTypingResult
+  ): ValidatedNel[String, Unit] = {
+    val givenClass     = givenType.runtimeObjType
+    val givenSuperclas = superclassCandidate.runtimeObjType
+
+    isStrictSubclass(givenClass, givenSuperclas)
+  }
+
+  def isStrictSubclass(givenClass: TypedClass, givenSuperclass: TypedClass): Validated[NonEmptyList[String], Unit] = {
+    condNel(
+      givenClass == givenSuperclass,
+      (),
+      f"${givenClass.display} and ${givenSuperclass.display} are not the same"
+    ) orElse
+      condNel(
+        isAssignable(givenClass.klass, givenSuperclass.klass),
+        (),
+        s"${givenClass.klass} is not assignable from ${givenSuperclass.klass}"
+      )
   }
 
   def canBeConvertedTo(
