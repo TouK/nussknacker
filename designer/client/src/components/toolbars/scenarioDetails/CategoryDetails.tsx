b89a0f3c769ed786b2f7916150d3d8093b66b7b9
Dawid Poliszak <dpl@touk.pl>
2024-11-21 15:29:46 +0100

    [Nu-1890] hide categories from a scenarios list and more scenario details when only one category is available (#7183)
    * NU-1890 hide categories from a scenarios list and more scenario details when only one category is available

--- designer/client/src/components/toolbars/scenarioDetails/CategoryDetails.tsx
+++ designer/client/src/components/toolbars/scenarioDetails/CategoryDetails.tsx
@@ -1,34 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useProcessFormDataOptions } from "../../useProcessFormDataOptions";
-import HttpService, { ScenarioParametersCombination } from "../../../http/HttpService";
+import React from "react";
 import { Skeleton, Typography } from "@mui/material";
 import { Scenario } from "../../Process/types";
+import { useGetAllCombinations } from "../../useGetAllCombinations";
 import { useTranslation } from "react-i18next";
 
 export const CategoryDetails = ({ scenario }: { scenario: Scenario }) => {
     const { t } = useTranslation();
-    const [allCombinations, setAllCombinations] = useState<ScenarioParametersCombination[]>([]);
-    const [isAllCombinationsLoading, setIsAllCombinationsLoading] = useState<boolean>(false);
-
-    const { isCategoryFieldVisible } = useProcessFormDataOptions({
-        allCombinations,
-        value: {
-            processCategory: scenario.processCategory,
-            processingMode: scenario.processingMode,
-            processEngine: scenario.engineSetupName,
-        },
+    const { isAllCombinationsLoading, isCategoryFieldVisible } = useGetAllCombinations({
+        processCategory: scenario.processCategory,
+        processingMode: scenario.processingMode,
+        processEngine: scenario.engineSetupName,
     });
-
-    useEffect(() => {
-        setIsAllCombinationsLoading(true);
-        HttpService.fetchScenarioParametersCombinations()
-            .then((response) => {
-                setAllCombinations(response.data.combinations);
-            })
-            .finally(() => {
-                setIsAllCombinationsLoading(false);
-            });
-    }, []);
 
     return (
         <>
