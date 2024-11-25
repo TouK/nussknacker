d902da1225fb8896d217ec9a745d2214ba776861
Piotr Rudnicki <piotr.rudnicki94@protonmail.com>
2024-11-25 11:07:53 +0100

    [NU-7164] Add basic parser for fragment input definitions V2 (#7228)
    * Revert "Revert "[NU-7164] Add basic parser for fragment input definitions (#7167)" (#7202)"
    
    This reverts commit 383b4d578e586de18c96853093e7325e1fc7e9a9.
    
    * [NU-7164] Adjust fields parameter list
    
    * Revert "[NU-7164] Adjust fields parameter list"
    
    This reverts commit d4a7ccc8f308ef763925cf976be11715ec94ae07.
    
    * add dicts bugfix
    
    * adjust type
    
    * correct validation rules
    
    ---------
    
    Co-authored-by: Piotr Rudnicki <prn@touk.pl>

---
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/fragment/FragmentParameterTypingParser.scala
@@ -0,0 +1,45 @@
+package pl.touk.nussknacker.engine.definition.fragment
+
+import org.apache.commons.lang3.ClassUtils
+import pl.touk.nussknacker.engine.api.typed.typing.{Typed, TypingResult}
+import pl.touk.nussknacker.engine.definition.clazz.ClassDefinition
+
+import scala.util.Try
+
+class FragmentParameterTypingParser(classLoader: ClassLoader, classDefinitions: Set[ClassDefinition]) {
+
+  def parseClassNameToTypingResult(className: String): Try[TypingResult] = {
+    /*
+   TODO: Write this parser in a way that handles arbitrary depth expressions
+         One should not use regexes for doing so and rather build AST
+     */
+    def resolveInnerClass(simpleClassName: String): TypingResult =
+      classDefinitions
+        .find(classDefinition => classDefinition.clazzName.display == simpleClassName)
+        .fold(
+          // This is fallback - it may be removed and `ClassNotFound` exception may be thrown here after cleaning up the mess with `FragmentClazzRef` class
+          Typed(ClassUtils.getClass(classLoader, simpleClassName))
+        ) { classDefinition =>
+          classDefinition.clazzName
+        }
+
+    val mapPattern  = "Map\\[(.+),(.+)\\]".r
+    val listPattern = "List\\[(.+)\\]".r
+    val setPattern  = "Set\\[(.+)\\]".r
+
+    Try(className match {
+      case mapPattern(x, y) =>
+        val resolvedFirstTypeParam  = resolveInnerClass(x)
+        val resolvedSecondTypeParam = resolveInnerClass(y)
+        Typed.genericTypeClass[java.util.Map[_, _]](List(resolvedFirstTypeParam, resolvedSecondTypeParam))
+      case listPattern(x) =>
+        val resolvedTypeParam = resolveInnerClass(x)
+        Typed.genericTypeClass[java.util.List[_]](List(resolvedTypeParam))
+      case setPattern(x) =>
+        val resolvedTypeParam = resolveInnerClass(x)
+        Typed.genericTypeClass[java.util.Set[_]](List(resolvedTypeParam))
+      case simpleClassName => resolveInnerClass(simpleClassName)
+    })
+  }
+
+}
