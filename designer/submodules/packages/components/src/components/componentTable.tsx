1a4c2d0a64878c080cc90e3ea167ceb987631034
Marcel Philipiak <137457433+Diamekod0221@users.noreply.github.com>
2024-11-26 18:48:48 +0100

    Scale fields in components (#7213)
    Bump up mui versions to support dynamic user scaling of fields in components view with x-data-grid. Also changed flex to 4 to display the name as longer by default
    ---------
    
    Co-authored-by: Marcel Philipiak <mph@touk.pl>

--- designer/submodules/packages/components/src/components/componentTable.tsx
+++ designer/submodules/packages/components/src/components/componentTable.tsx
@@ -6,7 +6,7 @@ import { Columns, TableViewData, TableWrapper } from "./tableWrapper";
 import { ExternalLink, NuIcon } from "../common";
 import { filterRules } from "./filterRules";
 import { ComponentsFiltersModel } from "./filters";
-import { GridActionsCellItem } from "@mui/x-data-grid";
+import { GridActionsCellItem, GridColDef } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom";
 import { ProcessingModesCell } from "./cellRenderers/processingModesCell";
 
@@ -15,14 +15,15 @@ export function ComponentTable(props: TableViewData<ComponentType>): JSX.Element
     const { data = [], isLoading } = props;
     const { t } = useTranslation();
 
-    const columns = useMemo(
-        (): Columns<ComponentType> => [
+    const columns: (GridColDef & { display?: string })[] = useMemo(
+        (): (GridColDef & { display?: string })[] => [
             {
                 field: "name",
                 minWidth: 200,
                 cellClassName: "noPadding stretch",
                 headerName: t("table.title.NAME", "Name"),
-                flex: 1,
+                flex: 4,
+                display: "flex",
                 renderCell: (props) => <NameCell {...props} />,
                 sortComparator: (v1, v2) => v1.toString().toLowerCase().localeCompare(v2.toString().toLowerCase()),
                 hideable: false,
