fd3afd12258eed016f5115c3ae6f3be1f7cf4921
Michał Goworko <m.goworko.pl@gmail.com>
2024-11-28 16:20:33 +0100

    qs

--- engine/flink/management/periodic/src/test/scala/pl/touk/nussknacker/engine/management/periodic/flink/PeriodicDeploymentHandlerTest.scala
+++ engine/flink/management/periodic/src/test/scala/pl/touk/nussknacker/engine/management/periodic/flink/PeriodicDeploymentHandlerTest.scala
@@ -4,26 +4,23 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.engine.api.ProcessVersion
 import pl.touk.nussknacker.engine.api.deployment.periodic.model.{DeploymentWithRuntimeParams, RuntimeParams}
 import pl.touk.nussknacker.engine.api.process.{ProcessName, VersionId}
-import pl.touk.nussknacker.engine.api.{MetaData, ProcessVersion, StreamMetaData}
-import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
 import pl.touk.nussknacker.engine.common.periodic.PeriodicDeploymentHandler
 import pl.touk.nussknacker.engine.management.FlinkModelJarProvider
-import pl.touk.nussknacker.engine.management.periodic.flink.FlinkPeriodicDeploymentHandler
 import pl.touk.nussknacker.engine.modelconfig.InputConfigDuringExecution
 import pl.touk.nussknacker.test.PatientScalaFutures
 
 import java.nio.file.{Files, Path, Paths}
 import scala.concurrent.Future
 
-class JarManagerTest extends AnyFunSuite with Matchers with ScalaFutures with PatientScalaFutures {
+class PeriodicDeploymentHandlerTest extends AnyFunSuite with Matchers with ScalaFutures with PatientScalaFutures {
 
   private val processName      = "test"
   private val processVersionId = 5
   private val processVersion =
     ProcessVersion.empty.copy(processName = ProcessName(processName), versionId = VersionId(processVersionId))
-  private val process             = CanonicalProcess(MetaData("foo", StreamMetaData()), Nil)
   private val jarsDir             = Files.createTempDirectory("jars-dir")
   private val modelJarFileContent = "abc".getBytes
 
@@ -35,9 +32,9 @@ class JarManagerTest extends AnyFunSuite with Matchers with ScalaFutures with Pa
 
   private val currentModelUrls = List(currentModelJarFile.toURI.toURL)
 
-  private val jarManager = createJarManager(jarsDir = jarsDir)
+  private val periodicDeploymentHandler = createPeriodicDeploymentHandler(jarsDir = jarsDir)
 
-  private def createJarManager(
+  private def createPeriodicDeploymentHandler(
       jarsDir: Path,
       modelJarProvider: FlinkModelJarProvider = new FlinkModelJarProvider(currentModelUrls)
   ): PeriodicDeploymentHandler = {
@@ -51,7 +48,7 @@ class JarManagerTest extends AnyFunSuite with Matchers with ScalaFutures with Pa
   }
 
   test("prepareDeploymentWithJar - should copy to local dir") {
-    val result = jarManager.prepareDeploymentWithRuntimeParams(processVersion)
+    val result = periodicDeploymentHandler.prepareDeploymentWithRuntimeParams(processVersion)
 
     val copiedJarFileName = result.futureValue.runtimeParams.params("jarFileName")
     copiedJarFileName should fullyMatch regex s"^$processName-$processVersionId-\\d+\\.jar$$"
@@ -61,8 +58,8 @@ class JarManagerTest extends AnyFunSuite with Matchers with ScalaFutures with Pa
   }
 
   test("prepareDeploymentWithJar - should handle disappearing model JAR") {
-    val modelJarProvider = new FlinkModelJarProvider(currentModelUrls)
-    val jarManager       = createJarManager(jarsDir, modelJarProvider)
+    val modelJarProvider          = new FlinkModelJarProvider(currentModelUrls)
+    val periodicDeploymentHandler = createPeriodicDeploymentHandler(jarsDir, modelJarProvider)
 
     def verifyAndDeleteJar(result: Future[DeploymentWithRuntimeParams]): Unit = {
       val copiedJarFile = jarsDir.resolve(result.futureValue.runtimeParams.params("jarFileName"))
@@ -71,21 +68,21 @@ class JarManagerTest extends AnyFunSuite with Matchers with ScalaFutures with Pa
       Files.delete(copiedJarFile)
     }
 
-    verifyAndDeleteJar(jarManager.prepareDeploymentWithRuntimeParams(processVersion))
+    verifyAndDeleteJar(periodicDeploymentHandler.prepareDeploymentWithRuntimeParams(processVersion))
 
     modelJarProvider.getJobJar().delete() shouldBe true
 
-    verifyAndDeleteJar(jarManager.prepareDeploymentWithRuntimeParams(processVersion))
+    verifyAndDeleteJar(periodicDeploymentHandler.prepareDeploymentWithRuntimeParams(processVersion))
   }
 
   test("prepareDeploymentWithJar - should create jars dir if not exists") {
-    val tmpDir     = System.getProperty("java.io.tmpdir")
-    val jarsDir    = Paths.get(tmpDir, s"jars-dir-not-exists-${System.currentTimeMillis()}")
-    val jarManager = createJarManager(jarsDir = jarsDir)
+    val tmpDir                    = System.getProperty("java.io.tmpdir")
+    val jarsDir                   = Paths.get(tmpDir, s"jars-dir-not-exists-${System.currentTimeMillis()}")
+    val periodicDeploymentHandler = createPeriodicDeploymentHandler(jarsDir = jarsDir)
 
     Files.exists(jarsDir) shouldBe false
 
-    val result = jarManager.prepareDeploymentWithRuntimeParams(processVersion)
+    val result = periodicDeploymentHandler.prepareDeploymentWithRuntimeParams(processVersion)
 
     val copiedJarFileName = result.futureValue.runtimeParams.params("jarFileName")
     Files.exists(jarsDir) shouldBe true
@@ -97,13 +94,14 @@ class JarManagerTest extends AnyFunSuite with Matchers with ScalaFutures with Pa
     val jarPath     = jarsDir.resolve(jarFileName)
     Files.copy(currentModelJarFile.toPath, jarPath)
 
-    jarManager.cleanAfterDeployment(RuntimeParams(Map("jarFileName" -> jarFileName))).futureValue
+    periodicDeploymentHandler.cleanAfterDeployment(RuntimeParams(Map("jarFileName" -> jarFileName))).futureValue
 
     Files.exists(jarPath) shouldBe false
   }
 
   test("deleteJar - should handle not existing file") {
-    val result = jarManager.cleanAfterDeployment(RuntimeParams(Map("jarFileName" -> "unknown.jar"))).futureValue
+    val result =
+      periodicDeploymentHandler.cleanAfterDeployment(RuntimeParams(Map("jarFileName" -> "unknown.jar"))).futureValue
 
     result shouldBe (())
   }
