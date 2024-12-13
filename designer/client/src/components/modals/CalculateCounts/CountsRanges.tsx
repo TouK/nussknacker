69022164604475146c295dc079789526bd79bf34
Piotr Rudnicki <prn@touk.pl>
2024-12-13 16:19:45 +0100

    [Nu-7330] Use activities endpoint to compute ranges in counts window

--- designer/client/src/components/modals/CalculateCounts/CountsRanges.tsx
+++ designer/client/src/components/modals/CalculateCounts/CountsRanges.tsx
@@ -4,9 +4,9 @@ import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { getProcessName } from "../../../reducers/selectors/graph";
 import { CountsRangesButtons } from "./CountsRangesButtons";
-import { useDeployHistory } from "./useDeployHistory";
 import { predefinedRanges } from "./utils";
 import { StyledRangesWrapper } from "./CountsStyled";
+import { useActivityHistory } from "./useActivityHistory";
 
 interface RangesProps {
     label: string;
@@ -16,7 +16,7 @@ interface RangesProps {
 export function CountsRanges({ label, onChange }: RangesProps): JSX.Element {
     const { t } = useTranslation<string>();
     const processName = useSelector(getProcessName);
-    const deploys = useDeployHistory(processName);
+    const activities = useActivityHistory(processName);
     const dates = useMemo(() => predefinedRanges(t), [t]);
 
     return (
@@ -24,8 +24,8 @@ export function CountsRanges({ label, onChange }: RangesProps): JSX.Element {
             <p>{label}</p>
             <StyledRangesWrapper>
                 <CountsRangesButtons ranges={dates} onChange={onChange} />
-                <CountsRangesButtons ranges={deploys} onChange={onChange} limit={1}>
-                    {t("calculateCounts.deployments", "Previous deployments...")}
+                <CountsRangesButtons ranges={activities} onChange={onChange} limit={1}>
+                    {t("calculateCounts.activities", "Previous activities...")}
                 </CountsRangesButtons>
             </StyledRangesWrapper>
         </>
