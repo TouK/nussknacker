7d285f458a440945adbbd1f8ff8262fab5e549ce
MK Software <mk_software_pl@protonmail.com>
2024-12-15 10:51:45 +0100

    fix

--- utils/utils-internal/src/main/scala/pl/touk/nussknacker/engine/util/loader/ModelClassLoader.scala
+++ utils/utils-internal/src/main/scala/pl/touk/nussknacker/engine/util/loader/ModelClassLoader.scala
@@ -4,7 +4,6 @@ import com.typesafe.scalalogging.LazyLogging
 import pl.touk.nussknacker.engine.util.StringUtils._
 import pl.touk.nussknacker.engine.util.UrlUtils._
 
-import java.io.File
 import java.net.{URL, URLClassLoader}
 import java.nio.file.Path
 
@@ -31,31 +30,11 @@ object ModelClassLoader extends LazyLogging {
       workingDirectoryOpt: Option[Path],
       jarExtension: String = defaultJarExtension
   ): ModelClassLoader = {
-    val postProcessedURLs = validateExistence(
-      urls.map(_.convertToURL(workingDirectoryOpt)).flatMap(_.expandFiles(jarExtension))
-    )
+    val postProcessedURLs = urls.map(_.convertToURL(workingDirectoryOpt)).flatMap(_.expandFiles(jarExtension))
     ModelClassLoader(
       new URLClassLoader(postProcessedURLs.toArray, this.getClass.getClassLoader),
-      postProcessedURLs.toList
+      postProcessedURLs
     )
-  }
-
-  private def validateExistence(urls: Iterable[URL]): Iterable[URL] = {
-    urls.filterNot(url => doesExist(url)).toList match {
-      case Nil => urls
-      case notExisted =>
-        throw new IllegalArgumentException(s"The following URLs don't exist: [${notExisted.mkString(",")}]")
-    }
-  }
-
-  private def doesExist(url: URL): Boolean = {
-    url.getProtocol match {
-      case "file" =>
-        val file = new File(url.toURI)
-        file.exists() && file.isFile
-      case _ =>
-        false
-    }
   }
 
 }
