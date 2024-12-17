d8fb287809749a695921eadf37e779d750698639
Piotr Fałdrowicz <pfl@touk.pl>
2024-12-17 15:02:03 +0100

    switch to state

--- designer/client/src/components/sidePanels/SearchLabeledAutocomplete.tsx
+++ designer/client/src/components/sidePanels/SearchLabeledAutocomplete.tsx
@@ -2,12 +2,20 @@ import { Autocomplete, FormControl } from "@mui/material";
 import { nodeInput } from "../graph/node-modal/NodeDetailsContent/NodeTableStyled";
 import React from "react";
 
-export const SearchLabeledAutocomplete = ({ children, name, values }) => {
+export const SearchLabeledAutocomplete = ({ children, name, options, value, setFilterFields }) => {
+    function handleChange(_, value) {
+        setFilterFields((prev) => ({ ...prev, [name]: [value] }));
+    }
+
     return (
         <FormControl sx={{ display: "flex", flexDirection: "column", m: 0, gap: 1, width: "100%" }}>
             {children}
             <Autocomplete
-                options={values}
+                freeSolo
+                options={options}
+                value={value.join(",")}
+                onChange={handleChange}
+                onInputChange={handleChange}
                 className={nodeInput}
                 renderInput={(params) => (
                     <div ref={params.InputProps.ref}>
--- designer/client/src/components/sidePanels/SearchLabeledInput.tsx
+++ designer/client/src/components/sidePanels/SearchLabeledInput.tsx
@@ -2,11 +2,15 @@ import React, { PropsWithChildren } from "react";
 import { FormControl } from "@mui/material";
 import { nodeInput } from "../graph/node-modal/NodeDetailsContent/NodeTableStyled";
 
-export const SearchLabeledInput = ({ children, ...props }) => {
+export const SearchLabeledInput = ({ children, name, value, setFilterFields }) => {
+    function handleChange(event) {
+        setFilterFields((prev) => ({ ...prev, [name]: event.target.value.split(",") }));
+    }
+
     return (
         <FormControl sx={{ display: "flex", flexDirection: "column", m: 0, gap: 1, width: "100%" }}>
             {children}
-            <input {...props} className={nodeInput} />
+            <input name={name} value={value} className={nodeInput} onChange={handleChange} />
         </FormControl>
     );
 };
