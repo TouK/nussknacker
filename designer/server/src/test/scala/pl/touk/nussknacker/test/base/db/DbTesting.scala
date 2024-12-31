2c916bc3a52f74fb7158c8a0d6dc03798cc15e5d
Michał Goworko <m.goworko.pl@gmail.com>
2024-12-31 10:21:19 +0100

    table names in tests

--- designer/server/src/test/scala/pl/touk/nussknacker/test/base/db/DbTesting.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/test/base/db/DbTesting.scala
@@ -93,8 +93,8 @@ trait DbTesting extends BeforeAndAfterEach with BeforeAndAfterAll {
     session.prepareStatement("""delete from "environments"""").execute()
     session.prepareStatement("""delete from "processes"""").execute()
     session.prepareStatement("""delete from "fingerprints"""").execute()
-    session.prepareStatement("""delete from "periodic_processes"""").execute()
-    session.prepareStatement("""delete from "periodic_process_deployments"""").execute()
+    session.prepareStatement("""delete from "periodic_scenarios"""").execute()
+    session.prepareStatement("""delete from "periodic_scenario_deployments"""").execute()
   }
 
 }
