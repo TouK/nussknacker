1833c063df472fd03c852238850c319f7309209d
Filip Michalski <fmc@touk.pl>
2025-01-14 16:36:06 +0100

    Sticky notes  (#7277)
    * Create StickyNotes DB entity
    
    * Add PoC httpApiService for StickyNotes - add slick migration
    
    * Add put and delete endpoints, use value classes for ID in stickyNotes model
    
    * Add StickyNote PoC to FE
    
    * Resize and update StickyNotes
    
    * Handle note removal
    
    * Remove some unused fragments
    
    * Update openApi definition
    
    * Resize stickyNote without visual-lag
    
    * Disable stickyNotes when scenario is not saved
    
    * Show/hide tools on graph actions
    
    * Edit stickyNote on graph
    
    * Fix some suggestions made by rabbit
    
    * Update openApi definitions
    
    * Add white characters to textarea in stickyNote markdown editor
    
    * Allow focus to stay in markdown editor
    
    * Allow switch viewer to editor witgh left mouse click
    
    * Add stickyNotes length and count validation, add stickyNotes configuration, fix error method (or did i?)
    
    * Remove node specific code from StickyNotePreview, update openApi defs
    
    * Add some fixes, improve types, add max width and height for stickyNote
    
    * Add STICKY_NOTE_CONSTRAINTS with config values
    
    * Reuse common code, restore default color, remove duplicated update method
    
    * Restore CSS class, remove unused imports
    
    * Remove stickyNotePanel, add stickyNote to creatos panel
    
    * Update cypress test
    
    * Add Changelog and migrationGuide entries
    
    * Updated snapshots (#7275)
    
    Co-authored-by: philemone <13632953+philemone@users.noreply.github.com>
    
    * remove space
    
    * Remove DOMpurify and use XSS instead
    
    * Remove StickyNotePreview from ComponentPreview
    
    * Rename stickyNotes panel to sticky notes
    
    * Fix markdown preview
    
    * Change shape of stickyNote, fix 'changed' detection for notes
    
    * Add notifications to graph, prevent from making changes to old stickyNotes versions
    
    * Revert fix in error notification msg
    
    * Fix warn messages
    
    * Add strict type for color #xxxxxx, remove unnecessary generated noteId, add more optimistic tests
    
    * Update pointer, add cypress tests, remove todo from uncomplete method, remove unused dompurify from package
    
    * Add regex in sticky note tests for note id
    
    * Change remove tool color and margins, update cypress tests
    
    * update cypress tests
    
    * Add maxDiffThreshold to flaky cypress test
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: philemone <13632953+philemone@users.noreply.github.com>

--- designer/client/src/components/graph/GraphPartialsInTS/applyCellChanges.ts
+++ designer/client/src/components/graph/GraphPartialsInTS/applyCellChanges.ts
@@ -7,26 +7,32 @@ import NodeUtils from "../NodeUtils";
 import { isEdgeConnected } from "./EdgeUtils";
 import { updateChangedCells } from "./updateChangedCells";
 import { Theme } from "@mui/material";
+import { StickyNote } from "../../../common/StickyNote";
+import { makeStickyNoteElement, ModelWithTool } from "../EspNode/stickyNoteElements";
 
 export function applyCellChanges(
     paper: dia.Paper,
     scenarioGraph: ScenarioGraph,
+    stickyNotes: StickyNote[],
     processDefinitionData: ProcessDefinitionData,
     theme: Theme,
 ): void {
     const graph = paper.model;
 
     const nodeElements = NodeUtils.nodesFromScenarioGraph(scenarioGraph).map(makeElement(processDefinitionData, theme));
+    const stickyNotesModelsWithTools: ModelWithTool[] = stickyNotes.map(makeStickyNoteElement(processDefinitionData, theme));
+    const stickyNotesModels = stickyNotesModelsWithTools.map((a) => a.model);
 
     const edges = NodeUtils.edgesFromScenarioGraph(scenarioGraph);
     const indexed = flatMap(groupBy(edges, "from"), (edges) => edges.map((edge, i) => ({ ...edge, index: ++i })));
     const edgeElements = indexed.filter(isEdgeConnected).map((value) => makeLink(value, paper, theme));
 
-    const cells = [...nodeElements, ...edgeElements];
+    const cells = [...nodeElements, ...edgeElements, ...stickyNotesModels];
 
     const currentCells = graph.getCells();
     const currentIds = currentCells.map((c) => c.id);
     const newCells = cells.filter((cell) => !currentIds.includes(cell.id));
+    const newStickyNotesModelsWithTools = stickyNotesModelsWithTools.filter((s) => !currentIds.includes(s.model.id));
     const deletedCells = currentCells.filter((oldCell) => !cells.find((cell) => cell.id === oldCell.id));
     const changedCells = cells.filter((cell) => {
         const old = graph.getCell(cell.id);
@@ -36,4 +42,17 @@ export function applyCellChanges(
     graph.removeCells(deletedCells);
     updateChangedCells(graph, changedCells);
     graph.addCells(newCells);
+
+    newStickyNotesModelsWithTools.forEach((m) => {
+        try {
+            const view = m.model.findView(paper);
+            if (!view) {
+                console.warn(`View not found for stickyNote model: ${m.model.id}`);
+                return;
+            }
+            view.addTools(m.tools);
+        } catch (error) {
+            console.error(`Failed to add tools to stickyNote view:`, error);
+        }
+    });
 }
