a748090e141b9b807100d024b10492e7c3377230
Dawid Poliszak <dpl@touk.pl>
2025-01-17 08:57:26 +0100

    resolve processDefinitionData reference issue

--- designer/client/src/actions/nk/processDefinitionData.ts
+++ designer/client/src/actions/nk/processDefinitionData.ts
@@ -1,6 +1,7 @@
 import HttpService from "../../http/HttpService";
-import { ProcessDefinitionData } from "../../types";
+import { ProcessDefinitionData, TypingResult } from "../../types";
 import { ThunkAction } from "../reduxTypes";
+import { orderBy } from "lodash";
 
 export type ProcessDefinitionDataAction = {
     type: "PROCESS_DEFINITION_DATA";
@@ -9,10 +10,14 @@ export type ProcessDefinitionDataAction = {
 
 export type ProcessingType = string;
 
+const orderedProcessDefinitionDataClasses = (classes: TypingResult[]) => orderBy(classes, ["display", "refClazzName"]);
+
 export function fetchProcessDefinition(processingType: ProcessingType, isFragment?: boolean): ThunkAction<Promise<ProcessDefinitionData>> {
     return async (dispatch) => {
         const { data: processDefinitionData } = await HttpService.fetchProcessDefinitionData(processingType, isFragment);
 
+        // Since BE doesn't provide ordered classes, to correctly memoize data in redux and not create a new object reference on every state change we need to order classes
+        processDefinitionData.classes = orderedProcessDefinitionDataClasses(processDefinitionData.classes);
         dispatch({ type: "PROCESS_DEFINITION_DATA", processDefinitionData });
 
         return processDefinitionData;
