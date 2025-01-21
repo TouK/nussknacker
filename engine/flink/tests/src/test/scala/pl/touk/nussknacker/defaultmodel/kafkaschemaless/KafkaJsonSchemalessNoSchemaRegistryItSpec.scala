b6824fe359bb3fd6a6f421b3ee6b7b75413f12c1
Pawel Czajka <pcz@touk.pl>
2025-01-21 16:02:05 +0100

    some test change

---
+++ engine/flink/tests/src/test/scala/pl/touk/nussknacker/defaultmodel/kafkaschemaless/KafkaJsonSchemalessNoSchemaRegistryItSpec.scala
@@ -0,0 +1,20 @@
+package pl.touk.nussknacker.defaultmodel.kafkaschemaless
+
+import com.typesafe.config.Config
+import pl.touk.nussknacker.engine.flink.util.transformer.FlinkKafkaComponentProvider
+
+class KafkaJsonSchemalessNoSchemaRegistryItSpec extends BaseKafkaJsonSchemalessItSpec {
+
+  override def createFinkKafkaComponentProvider() = new FlinkKafkaComponentProvider()
+
+  override protected def maybeAddSchemaRegistryUrl(config: Config): Config = config
+
+  test("should round-trip json message without schema registry") {
+    shouldRoundTripJsonMessageWithoutProvidedSchema()
+  }
+
+  ignore("should round-trip plain message without schema registry") {
+    shouldRoundTripPlainMessageWithoutProvidedSchema()
+  }
+
+}
