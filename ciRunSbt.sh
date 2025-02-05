487ba187a1a50e9d1bee661aee6ace79b99ac6ac
Michał Goworko <m.goworko.pl@gmail.com>
2025-02-05 13:37:00 +0100

    Disable cross build

--- ciRunSbt.sh
+++ ciRunSbt.sh
@@ -3,7 +3,7 @@ set -e
 # When $NUSSKNACKER_SCALA_VERSION is not present, we check for $CROSS_BUILD. If it's true - we do cross build, otherwise we use default scala version
 if [[ -n "$NUSSKNACKER_SCALA_VERSION" ]]; then
    ARGS="++$NUSSKNACKER_SCALA_VERSION $*"
-elif [[ $CROSS_BUILD == 'true' ]]; then
+elif [[ $CROSS_BUILD == 'false' ]]; then
    #for crossbuild we prepend each arg with +...
    ARGS=`echo " $*" | sed "s/ \+/ \+/g"`
 else
