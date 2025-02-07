bc4ef14af9aec4ac9fda0cfb43ade564c936cfa4
Michał Goworko <m.goworko.pl@gmail.com>
2025-02-07 13:18:25 +0100

    rename constraint

--- designer/server/src/main/resources/db/migration/common/V1_062__ScenarioSchedulingTablesModifications.sql
+++ designer/server/src/main/resources/db/migration/common/V1_062__ScenarioSchedulingTablesModifications.sql
@@ -1,7 +1,7 @@
 ALTER TABLE "periodic_scenarios" RENAME TO "scheduled_scenarios";
 ALTER TABLE "scheduled_scenarios" ALTER COLUMN "input_config_during_execution" DROP NOT NULL;
 ALTER TABLE "scheduled_scenarios" ADD COLUMN "resolved_scenario_json" VARCHAR(10485760);
-ALTER TABLE "scheduled_scenarios" ADD CONSTRAINT check_scheduled_scenarios
+ALTER TABLE "scheduled_scenarios" ADD CONSTRAINT scheduled_scenarios_check_config_and_json_present_for_active
 CHECK (
   ("active" = TRUE AND "input_config_during_execution" IS NOT NULL AND "resolved_scenario_json" IS NOT NULL)
     OR
