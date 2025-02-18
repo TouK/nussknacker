9ee738a2f70f2c7b7025ee04ed5266b300cc166b
deamondev <piotr.rudnicki94@protonmail.com>
2025-02-18 12:21:56 +0100

    [Nu-7573] refactor FE

--- designer/client/src/components/graph/node-modal/fragment-input-definition/settings/variants/DefaultVariant.tsx
+++ designer/client/src/components/graph/node-modal/fragment-input-definition/settings/variants/DefaultVariant.tsx
@@ -6,7 +6,6 @@ import { DefaultParameterVariant, onChangeType } from "../../item";
 import { NodeValidationError, VariableTypes } from "../../../../../../types";
 import { TextAreaNode } from "../../../../../FormElements";
 import InitialValue from "./fields/InitialValue";
-import { ValidationsFields } from "./fields/validation";
 import { getValidationErrorsForField } from "../../../editors/Validators";
 import { nodeInput } from "../../../NodeDetailsContent/NodeTableStyled";
 
@@ -49,14 +48,6 @@ export const DefaultVariant = ({ item, onChange, path, variableTypes, readOnly, 
                     className={nodeInput}
                 />
             </FormControl>
-            <ValidationsFields
-                path={path}
-                onChange={onChange}
-                item={item}
-                variableTypes={variableTypes}
-                readOnly={readOnly}
-                errors={errors}
-            />
         </SettingsWrapper>
     );
 };
