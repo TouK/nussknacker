7f7803b35287c0069a6eb90b49939f84945c19ba
Arek Burdach <arek.burdach@gmail.com>
2025-02-25 11:56:50 +0100

    [NU-2048] Fix for invalid classloader used for runtime/default-logback.xml in K8sDeploymentManager

--- engine/lite/k8sDeploymentManager/src/main/scala/pl/touk/nussknacker/k8s/manager/K8sDeploymentManager.scala
+++ engine/lite/k8sDeploymentManager/src/main/scala/pl/touk/nussknacker/k8s/manager/K8sDeploymentManager.scala
@@ -100,7 +100,8 @@ class K8sDeploymentManager(
     inputConfig.copy(config = withOverrides).serialized
   }
 
-  private lazy val defaultLogbackConfig = Using.resource(Source.fromResource("runtime/default-logback.xml"))(_.mkString)
+  private val defaultLogbackConfig =
+    Using.resource(Source.fromResource("runtime/default-logback.xml", getClass.getClassLoader))(_.mkString)
 
   private def logbackConfig: String = config.logbackConfigPath
     .map(path => Using.resource(Source.fromFile(path))(_.mkString))
