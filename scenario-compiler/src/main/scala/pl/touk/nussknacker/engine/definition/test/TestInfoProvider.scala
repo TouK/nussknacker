6129a443e32fba1a2aee3d73a5c960c77bb72473
Łukasz Jędrzejewski <ljd@touk.pl>
2025-02-25 12:34:17 +0100

    Remove message

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/test/TestInfoProvider.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/test/TestInfoProvider.scala
@@ -5,6 +5,11 @@ import pl.touk.nussknacker.engine.api.definition.Parameter
 import pl.touk.nussknacker.engine.api.test.ScenarioTestData
 import pl.touk.nussknacker.engine.api.{MetaData, ProcessVersion}
 import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
+import pl.touk.nussknacker.engine.definition.test.TestInfoProvider.ScenarioTestDataGenerationError.TestDataPreparationError
+import pl.touk.nussknacker.engine.definition.test.TestInfoProvider.SourceTestDataGenerationError.{
+  ScenarioTestDataGenerationError,
+  TestDataPreparationError
+}
 import pl.touk.nussknacker.engine.definition.test.TestInfoProvider.{
   ScenarioTestDataGenerationError,
   SourceTestDataGenerationError,
@@ -39,56 +44,30 @@ trait TestInfoProvider {
 
 object TestInfoProvider {
 
-  sealed trait TestDataError {
-    def message: String
-  }
+  sealed trait TestDataError
 
   sealed trait SourceTestDataGenerationError extends TestDataError
 
   object SourceTestDataGenerationError {
-
     final case class SourceCompilationError(nodeId: String, errors: List[ProcessCompilationError])
-        extends SourceTestDataGenerationError {
-      override def message: String = s"Source node can't be compiled. Problems: ${errors.mkString(", ")}"
-    }
-
-    final case class UnsupportedSourceError(nodeId: String) extends SourceTestDataGenerationError {
-      override def message: String = s"Source '$nodeId' doesn't support records preview"
-    }
-
-    final case object NoDataGenerated extends SourceTestDataGenerationError {
-      override def message: String = "No test data was generated"
-    }
-
+        extends SourceTestDataGenerationError
+    final case class UnsupportedSourceError(nodeId: String) extends SourceTestDataGenerationError
+    final case object NoDataGenerated                       extends SourceTestDataGenerationError
   }
 
   sealed trait ScenarioTestDataGenerationError extends TestDataError
 
   object ScenarioTestDataGenerationError {
+    final case object NoDataGenerated extends ScenarioTestDataGenerationError
 
-    final case object NoDataGenerated extends ScenarioTestDataGenerationError {
-      override def message: String = "No test data was generated"
-    }
-
-    final case object NoSourcesWithTestDataGeneration extends ScenarioTestDataGenerationError {
-      override def message: String = "Scenario doesn't have any valid source supporting test data generation"
-    }
-
+    final case object NoSourcesWithTestDataGeneration extends ScenarioTestDataGenerationError
   }
 
   sealed trait TestDataPreparationError extends TestDataError
 
   object TestDataPreparationError {
-
-    final case class MissingSource(sourceId: String, recordIndex: Int) extends TestDataPreparationError {
-      override def message: String = s"Record ${recordIndex + 1} - scenario does not have source id: '$sourceId'"
-    }
-
-    final case class MultipleSourcesRequired(recordIndex: Int) extends TestDataPreparationError {
-      override def message: String =
-        s"Record ${recordIndex + 1} - scenario has multiple sources but got record without source id"
-    }
-
+    final case class MissingSource(sourceId: String, recordIndex: Int) extends TestDataPreparationError
+    final case class MultipleSourcesRequired(recordIndex: Int)         extends TestDataPreparationError
   }
 
 }
