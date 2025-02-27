d0b83761975472e786d03d0541749d85667893b3
Julian Wielga <j@touk.pl>
2025-02-27 13:28:22 +0100

    fragment creator

--- designer/client/src/reducers/selectors/getComponentGroups.ts
+++ designer/client/src/reducers/selectors/getComponentGroups.ts
@@ -4,7 +4,7 @@ import { stickyNoteComponentGroup } from "../../components/toolbars/creator/Stic
 import { ComponentGroup } from "../../types";
 import { RootState } from "../index";
 import { getCreator } from "./getCreator";
-import { isPristine } from "./graph";
+import { isFragment, isPristine } from "./graph";
 import { getProcessDefinitionData, getStickyNotesSettings } from "./settings";
 
 function addUniqueElement<T extends { name: string }>(array: T[], newElement: T): T[] {
@@ -19,7 +19,7 @@ function addUniqueElement<T extends { name: string }>(array: T[], newElement: T)
 export const appendAdditionalCreators = (groups: ComponentGroup[], additionalCreators: string[]) => {
     if (!additionalCreators.length) return groups;
     const newElement = {
-        name: "debug ㊙️",
+        name: "cloud addons",
         components: additionalCreators.map(getCreator),
     };
     return addUniqueElement(groups, newElement);
@@ -30,12 +30,63 @@ function appendStickyNotes(groups: ComponentGroup[], stickyNotesSettings: Sticky
     return groups.concat(stickyNoteComponentGroup(pristine));
 }
 
+function replaceOrAdd<T>(collection: T[] = [], predicate: (item: T) => boolean, replaceOrAddFn: (item?: T) => T): T[] {
+    const index = collection.findIndex(predicate);
+    if (index === -1) return [...collection, replaceOrAddFn()];
+
+    const currentItem = collection[index];
+    return [...collection.slice(0, index), replaceOrAddFn(currentItem), ...collection.slice(index + 1)];
+}
+
+function appendFragmentCreator(groups: ComponentGroup[], isFragment?: boolean) {
+    if (isFragment) return groups;
+    const groupName = "fragments";
+
+    const fragmentCreator = {
+        label: "new fragment",
+        componentId: "fragment-.template",
+        node: {
+            id: "",
+            ref: {
+                id: ".template",
+                parameters: [],
+                outputVariableNames: {
+                    output: "output",
+                },
+            },
+            additionalFields: null,
+            isDisabled: null,
+            fragmentParams: null,
+            type: "FragmentInput",
+            branchParametersTemplate: [],
+        },
+        branchParametersTemplate: [],
+    };
+
+    return replaceOrAdd<ComponentGroup>(
+        groups,
+        ({ name }) => name === groupName,
+        (
+            { components, ...group } = {
+                name: groupName,
+                components: [],
+            },
+        ) => ({
+            ...group,
+            components: [...components, fragmentCreator as any],
+        }),
+    );
+}
+
 export const getComponentGroups = createSelector(
     getProcessDefinitionData,
     getStickyNotesSettings,
     isPristine,
-    ({ componentGroups }, stickyNotesSettings, pristine) => {
-        return appendStickyNotes(componentGroups, stickyNotesSettings, pristine);
+    isFragment,
+    ({ componentGroups }, stickyNotesSettings, pristine, isFragment) => {
+        const withStickyNotes = appendStickyNotes(componentGroups, stickyNotesSettings, pristine);
+        const groups = appendFragmentCreator(withStickyNotes, isFragment);
+        return groups;
     },
 );
 
