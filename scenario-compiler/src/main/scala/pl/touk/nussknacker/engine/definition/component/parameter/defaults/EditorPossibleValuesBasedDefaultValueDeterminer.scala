b2affdeea83d411c49c582ca7e213f05c9eeeb64
Mateusz Kopeć <mateuszkp96@gmail.com>
2025-02-28 15:06:17 +0100

    Determine default value for sql parameter editor

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/component/parameter/defaults/EditorPossibleValuesBasedDefaultValueDeterminer.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/component/parameter/defaults/EditorPossibleValuesBasedDefaultValueDeterminer.scala
@@ -5,6 +5,7 @@ import pl.touk.nussknacker.engine.api.definition.{
   DualParameterEditor,
   FixedValuesParameterEditor,
   SpelTemplateParameterEditor,
+  SqlParameterEditor,
   TabularTypedDataEditor
 }
 import pl.touk.nussknacker.engine.graph.expression.{Expression, TabularTypedData}
@@ -22,7 +23,7 @@ protected object EditorPossibleValuesBasedDefaultValueDeterminer extends Paramet
           Some(Expression.spel(firstValue.expression))
         case TabularTypedDataEditor =>
           Some(Expression.tabularDataDefinition(TabularTypedData.empty.stringify))
-        case SpelTemplateParameterEditor =>
+        case SpelTemplateParameterEditor | SqlParameterEditor =>
           Some(Expression.spelTemplate(""))
         case DictParameterEditor(_) => Some(Expression(Language.DictKeyWithLabel, ""))
         case _                      => None
