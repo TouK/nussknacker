7ed67c7e0f3a12b67b7f0c0601337698947befee
mgoworko <37329559+mgoworko@users.noreply.github.com>
2025-03-04 20:49:37 +0100

    Custom HTTP service SPI (#7600)

---
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/repository/ScenarioVersionMetadata.scala
@@ -0,0 +1,12 @@
+package pl.touk.nussknacker.ui.process.repository
+
+import pl.touk.nussknacker.engine.api.deployment.UserName
+import pl.touk.nussknacker.engine.api.process.VersionId
+
+import java.time.Instant
+
+final case class ScenarioVersionMetadata(
+    versionId: VersionId,
+    createdAt: Instant,
+    createdByUser: UserName,
+)
