c6aca034ebb82aa66c1fdb9363bc54cd6eeeb8e3
Łukasz Bigorajski <lbg@touk.pl>
2025-03-12 14:49:20 +0100

    [NU-1862] SpEL to SpelTemplate migration

---
+++ defaultModel/src/test/scala/pl/touk/nussknacker/defaultModel/migrations/SpelToSpelTemplateNodeMigrationSpec.scala
@@ -0,0 +1,86 @@
+package pl.touk.nussknacker.defaultModel.migrations
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.defaultmodel.migrations.SpelToSpelTemplateNodeMigration
+import pl.touk.nussknacker.engine.api.{MetaData, StreamMetaData}
+import pl.touk.nussknacker.engine.api.parameter.ParameterName
+import pl.touk.nussknacker.engine.graph.evaluatedparam.Parameter
+import pl.touk.nussknacker.engine.graph.node.{
+  CustomNode,
+  Enricher,
+  Join,
+  Processor,
+  Sink,
+  Source,
+  UserDefinedAdditionalNodeFields
+}
+import pl.touk.nussknacker.engine.graph.service.ServiceRef
+import pl.touk.nussknacker.engine.graph.sink.SinkRef
+import pl.touk.nussknacker.engine.graph.source.SourceRef
+import pl.touk.nussknacker.engine.spel.SpelExtension.SpelExpresion
+
+class SpelToSpelTemplateNodeMigrationSpec extends AnyFunSuite with Matchers {
+
+  private val paramsBeforeMigration = List(
+    Parameter(ParameterName("name1"), "11".spel),
+    Parameter(ParameterName("name2"), "'John'".spel),
+  )
+
+  private val paramsAfterMigration = List(
+    Parameter(ParameterName("name1"), "11".spel),
+    Parameter(ParameterName("name2"), "John".spelTemplate),
+  )
+
+  private val additionalFields = Some(UserDefinedAdditionalNodeFields(Some("desc"), None))
+  private val metaData         = MetaData("test", StreamMetaData(Some(1)))
+
+  test("should migrate custom node") {
+    val node = CustomNode("id", Some("out1"), "typ1", paramsBeforeMigration, additionalFields)
+
+    val migrated = SpelToSpelTemplateNodeMigration.migrateNode(metaData)(node)
+
+    migrated shouldBe CustomNode("id", Some("out1"), "typ1", paramsAfterMigration, additionalFields)
+  }
+
+  test("should migrate enricher") {
+    val node = Enricher("id", ServiceRef("id", paramsBeforeMigration), "out1", additionalFields)
+
+    val migrated = SpelToSpelTemplateNodeMigration.migrateNode(metaData)(node)
+
+    migrated shouldBe Enricher("id", ServiceRef("id", paramsAfterMigration), "out1", additionalFields)
+  }
+
+  test("should migrate join") {
+    val node = Join("id", Some("out1"), "typ1", paramsBeforeMigration, Nil, additionalFields)
+
+    val migrated = SpelToSpelTemplateNodeMigration.migrateNode(metaData)(node)
+
+    migrated shouldBe Join("id", Some("out1"), "typ1", paramsAfterMigration, Nil, additionalFields)
+  }
+
+  test("should migrate processor") {
+    val node = Processor("id", ServiceRef("id", paramsBeforeMigration), Some(false), additionalFields)
+
+    val migrated = SpelToSpelTemplateNodeMigration.migrateNode(metaData)(node)
+
+    migrated shouldBe Processor("id", ServiceRef("id", paramsAfterMigration), Some(false), additionalFields)
+  }
+
+  test("should migrate sink") {
+    val node = Sink("id", SinkRef("id", paramsBeforeMigration), None, Some(false), additionalFields)
+
+    val migrated = SpelToSpelTemplateNodeMigration.migrateNode(metaData)(node)
+
+    migrated shouldBe Sink("id", SinkRef("id", paramsAfterMigration), None, Some(false), additionalFields)
+  }
+
+  test("should migrate source") {
+    val node = Source("id", SourceRef("id", paramsBeforeMigration), additionalFields)
+
+    val migrated = SpelToSpelTemplateNodeMigration.migrateNode(metaData)(node)
+
+    migrated shouldBe Source("id", SourceRef("id", paramsAfterMigration), additionalFields)
+  }
+
+}
--- docs/Changelog.md
+++ docs/Changelog.md
@@ -112,6 +112,7 @@
 * [#7614](https://github.com/TouK/nussknacker/pull/7614) SpelTemplate as a main text editor
   * Added a new parameter editor type: SpelParameterEditor which works the same as RawParameterEditor.
   * Added a list of editors to the UIParameter.
+  * Added migration of scenario nodes from a string defined in SpEL to a SpelTemplate string. 
 * [#7616](https://github.com/TouK/nussknacker/pull/7616) (K8s DM) Fix for: k8s object name sanitizing strategy sometimes generated invalid object names, in other cases, 
   it generated names with unnecessary characters appended
 * [#7615](https://github.com/TouK/nussknacker/pull/7615) Updated Flink dependency to 1.19.2
