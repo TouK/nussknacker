a958f98ed02a7487992591ca7f327b7d1a29f8d0
Julian Wielga <j@touk.pl>
2025-03-16 21:53:54 +0100

    expose userSettings

--- designer/client/src/components/toolbarSettings/DEV_TOOLBARS.ts
+++ designer/client/src/components/toolbarSettings/DEV_TOOLBARS.ts
@@ -1,4 +1,5 @@
 import { isDev } from "../../devHelpers";
 import { ToolbarConfig } from "./types";
 
-export const DEV_TOOLBARS: ToolbarConfig[] = isDev ? [{ id: "user-settings-panel" }] : [];
+export const getDevToolbars = (userSettingsVisible?: boolean): ToolbarConfig[] =>
+    isDev || userSettingsVisible ? [{ id: "user-settings-panel" }] : [];
