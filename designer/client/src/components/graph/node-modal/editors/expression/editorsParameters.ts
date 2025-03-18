597cbf2483e359c5f35ffae54930379660b781cd
Dawid Poliszak <dpl@touk.pl>
2025-03-18 15:05:55 +0100

    NU-1863 change editor displayName

--- designer/client/src/components/graph/node-modal/editors/expression/editorsParameters.ts
+++ designer/client/src/components/graph/node-modal/editors/expression/editorsParameters.ts
@@ -15,14 +15,14 @@ export const editorsParameters: Record<EditorType, { displayName: string; langua
         language: ExpressionLang.SpEL,
     },
     [EditorType.FIXED_VALUES_WITH_RADIO_PARAMETER_EDITOR]: {
-        displayName: "Radio",
+        displayName: "Fixed Values",
         language: ExpressionLang.SpEL,
     },
     [EditorType.JSON_PARAMETER_EDITOR]: { displayName: "Json", language: ExpressionLang.JSON },
     [EditorType.PERIOD_EDITOR]: { displayName: "Period", language: ExpressionLang.SpEL },
     [EditorType.SPEL_PARAMETER_EDITOR]: { displayName: "Expression", language: ExpressionLang.SpEL },
     [EditorType.STRING_PARAMETER_EDITOR]: { displayName: "Text", language: ExpressionLang.String },
-    [EditorType.TEXTAREA_PARAMETER_EDITOR]: { displayName: "Textarea", language: ExpressionLang.SpEL },
+    [EditorType.TEXTAREA_PARAMETER_EDITOR]: { displayName: "Text", language: ExpressionLang.SpEL },
     [EditorType.TIME]: { displayName: "Time", language: ExpressionLang.SpEL },
     [EditorType.SQL_PARAMETER_EDITOR]: { displayName: "SQL", language: ExpressionLang.SQL },
     [EditorType.SPEL_TEMPLATE_PARAMETER_EDITOR]: {
