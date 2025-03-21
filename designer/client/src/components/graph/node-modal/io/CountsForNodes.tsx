729c73944a5caf358355a5a94a16a3726dd7edc3
Julian Wielga <j@touk.pl>
2025-03-21 13:27:14 +0100

    visual tweaks

--- designer/client/src/components/graph/node-modal/io/CountsForNodes.tsx
+++ designer/client/src/components/graph/node-modal/io/CountsForNodes.tsx
@@ -2,8 +2,13 @@ import { Chip, Stack, styled } from "@mui/material";
 import React from "react";
 
 const Path = styled(Chip)(({ theme }) => ({
+    background: theme.palette.background.default,
     borderRadius: theme.spacing(0.8),
     height: "2em",
+}));
+
+const Count = styled("span")(({ theme }) => ({
+    fontWeight: "bold",
 }));
 
 type NodeCount = {
@@ -13,23 +18,28 @@ type NodeCount = {
 
 export function CountsForNodes({ nodes, input }: { nodes: NodeCount[]; input?: boolean }) {
     return (
-        <Stack spacing={0.5} direction="row">
-            {nodes.map(({ id, count = -1 }) => (
+        <Stack spacing={0.5} useFlexGap direction="row" sx={{ flexWrap: "wrap" }}>
+            {nodes.map(({ id, count }) => (
                 <Path
                     key={id}
                     label={
                         <Stack direction={input ? "row-reverse" : "row"} spacing={0.5}>
-                            {count ? <strong>{Math.abs(count)}</strong> : null}
+                            {count ? (
+                                <Count
+                                    sx={(theme) => ({
+                                        color: count >= 0 ? theme.palette.success.main : theme.palette.error.main,
+                                    })}
+                                >
+                                    {Math.abs(count)}
+                                </Count>
+                            ) : null}
                             <span>→</span>
                             <span>{id}</span>
                         </Stack>
                     }
                     sx={(theme) => {
-                        const background =
-                            count > 0 ? theme.palette.primary.dark : count < 0 ? theme.palette.error.main : theme.palette.action.disabled;
                         return {
-                            background,
-                            color: theme.palette.getContrastText(background),
+                            color: count > 0 ? "inherit" : theme.palette.action.disabled,
                         };
                     }}
                 />
