9c77108a99ebe9cbab0cf1baf9e25fe7434df468
Szymon Bogusz <sbg@touk.pl>
2025-03-21 12:37:26 +0100

    Added dummy helpers for cypress, to remove errors

---
+++ engine/flink/management/dev-model/src/main/scala/pl/touk/nussknacker/engine/management/sample/helper/DummyUuidHelper.scala
@@ -0,0 +1,7 @@
+package pl.touk.nussknacker.engine.management.sample.helper
+
+import pl.touk.nussknacker.engine.api.HideToString
+
+object DummyUuidHelper extends HideToString {
+  def uuid(): String = "be7663f5-80c8-4f82-8cd5-db4da3edc35e"
+}
