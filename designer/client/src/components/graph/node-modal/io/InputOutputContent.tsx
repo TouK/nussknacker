b55d52e78e30d8ffd32bdf5bd3a8750f41702cbf
Julian Wielga <j@touk.pl>
2025-03-22 14:00:26 +0100

    node window toolbar buttons styles

--- designer/client/src/components/graph/node-modal/io/InputOutputContent.tsx
+++ designer/client/src/components/graph/node-modal/io/InputOutputContent.tsx
@@ -1,5 +1,5 @@
 import { PropsOf } from "@emotion/react/dist/emotion-react.cjs";
-import { Stack } from "@mui/material";
+import { alpha, Stack } from "@mui/material";
 import React, { forwardRef } from "react";
 import { ButtonsVariant, ToolbarButtons } from "../../../toolbarComponents/toolbarButtons";
 import AdhocTestingButton from "../../../toolbars/test/buttons/AdhocTestingButton";
@@ -12,7 +12,27 @@ export const InputOutputContent = forwardRef<HTMLDivElement, PropsOf<typeof Styl
         <InputOutputLayout>
             <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
                 <Stack sx={{ overflow: "hidden" }}>
-                    <ToolbarButtons variant={ButtonsVariant.horizontal} sx={{ padding: 1 }}>
+                    <ToolbarButtons
+                        variant={ButtonsVariant.horizontal}
+                        sx={(theme) => ({
+                            padding: 1,
+                            justifyContent: "flex-start",
+                            ".toolbarButton-Root": {
+                                background: alpha(theme.palette.background.default, 0.4),
+                                paddingX: 1,
+                                paddingY: 0.25,
+                            },
+                            ".toolbarButton-Icon": {
+                                "&, &>*": {
+                                    height: ".8em",
+                                    width: ".8em",
+                                },
+                            },
+                            ".toolbarButton-Label": {
+                                fontSize: ".6em",
+                            },
+                        })}
+                    >
                         <AdhocTestingButton type={CustomButtonTypes.adhocTesting} />
                     </ToolbarButtons>
                     <StyledContent {...props} />
