a47615f14e210a4d5bb8ffae127178ae87476f00
Dawid Poliszak <dpl@touk.pl>
2025-03-31 13:33:02 +0200

    NU-2096 handle multiline in a user content

--- designer/client/src/components/aiAssistant/components/UserMessage.tsx
+++ designer/client/src/components/aiAssistant/components/UserMessage.tsx
@@ -5,7 +5,6 @@ import React from "react";
 export const UserMessage = () => {
     const { content, ...rest } = useMessage();
 
-    console.log(rest);
     return (
         <Box sx={{ display: "flex", justifyContent: "flex-end", width: "100%" }}>
             <Box
@@ -19,7 +18,9 @@ export const UserMessage = () => {
                 }}
             >
                 {content.map((part, index) => (
-                    <Typography key={index}>{part.text}</Typography>
+                    <Typography key={index} sx={{ whiteSpace: "pre-line" }}>
+                        {part.text}
+                    </Typography>
                 ))}
             </Box>
         </Box>
