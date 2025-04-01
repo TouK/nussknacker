8acf1dc5e98536869f567e9ca2147717c9c3e5bb
Julian Wielga <j@touk.pl>
2025-04-01 22:44:57 +0200

    test fix

--- designer/client/cypress/e2e/activities.cy.ts
+++ designer/client/cypress/e2e/activities.cy.ts
@@ -17,7 +17,8 @@ const addAttachmentActivity = (path: string) => {
 
 const findActivity = (query: string) => {
     cy.contains("Activities").scrollIntoView();
-    cy.get('input[placeholder="type here to find past event"]').should("be.visible").clear().type(query, { delay: 25 });
+    cy.get('input[placeholder="type here to find past event"]').should("be.visible").clear().type(query);
+    cy.wait(500);
 };
 
 const makeScreenshot = () => {
