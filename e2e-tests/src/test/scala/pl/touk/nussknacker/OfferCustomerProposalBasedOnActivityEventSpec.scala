64bd0a0cd4d30968278a879c240d2a3fe7156288
Michał Goworko <m.goworko.pl@gmail.com>
2025-04-02 11:25:37 +0200

    adjust tests

--- e2e-tests/src/test/scala/pl/touk/nussknacker/OfferCustomerProposalBasedOnActivityEventSpec.scala
+++ e2e-tests/src/test/scala/pl/touk/nussknacker/OfferCustomerProposalBasedOnActivityEventSpec.scala
@@ -2,17 +2,36 @@ package pl.touk.nussknacker
 
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
-import pl.touk.nussknacker.test.VeryPatientScalaFutures
+import pl.touk.nussknacker.utils.BasicScenarioInformationFetching
+import pl.touk.nussknacker.utils.BasicScenarioInformationFetching.{
+  ScenarioBasicInformation,
+  ScenarioState,
+  ScenarioStatus
+}
 
 class OfferCustomerProposalBasedOnActivityEventSpec
     extends AnyFreeSpecLike
     with BaseE2ESpec
     with Matchers
-    with VeryPatientScalaFutures {
+    with BasicScenarioInformationFetching {
 
-  // The OfferCustomerProposalBasedOnActivityEvent is a highly dynamic scenario, with output depending on events representing customers actions
-  // We currently only check, that the scenario is compiled and running (which is done by the underlying nussknacker-example-scenarios-library Docker image)
-  "OfferCustomerProposalBasedOnActivityEventSpec" in {}
+  // The OfferCustomerProposalBasedOnActivityEvent is a highly dynamic scenario,
+  // with output depending on events representing customers actions.
+  // We currently only check, that the scenario is compiled and running)
+  "OfferCustomerProposalBasedOnActivityEvent scenario is running" in {
+    val scenarios = fetchScenariosBasicInformation()
+    scenarios should contain(
+      ScenarioBasicInformation(
+        name = "OfferCustomerProposalBasedOnActivityEvent",
+        isArchived = false,
+        isFragment = false,
+        processingType = "streaming",
+        processCategory = "Default",
+        labels = List.empty,
+        state = ScenarioState(ScenarioStatus("RUNNING"))
+      )
+    )
+  }
 
   override protected def afterEach(): Unit = {
     client.purgeKafkaTopic("CustomerEvents")
--- e2e-tests/src/test/scala/pl/touk/nussknacker/RtmClientNearPosSpec.scala
+++ e2e-tests/src/test/scala/pl/touk/nussknacker/RtmClientNearPosSpec.scala
@@ -3,12 +3,37 @@ package pl.touk.nussknacker
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 import pl.touk.nussknacker.test.VeryPatientScalaFutures
+import pl.touk.nussknacker.utils.BasicScenarioInformationFetching
+import pl.touk.nussknacker.utils.BasicScenarioInformationFetching.{
+  ScenarioBasicInformation,
+  ScenarioState,
+  ScenarioStatus
+}
 
-class RtmClientNearPosSpec extends AnyFreeSpecLike with BaseE2ESpec with Matchers with VeryPatientScalaFutures {
+class RtmClientNearPosSpec
+    extends AnyFreeSpecLike
+    with BaseE2ESpec
+    with Matchers
+    with VeryPatientScalaFutures
+    with BasicScenarioInformationFetching {
 
-  // The RTMClientNearPOS is a highly dynamic scenario, with output depending on the current weekday and time
-  // We currently only check, that the scenario is compiled and running (which is done by the underlying nussknacker-example-scenarios-library Docker image)
-  "RtmClientNearPosSpec" in {}
+  // The RTMClientNearPOS is a highly dynamic scenario,
+  // with output depending on the current weekday, time and randomly generated coordinates.
+  // We currently only check, that the scenario is compiled and running)
+  "RtmClientNearPos is running" in {
+    val scenarios = fetchScenariosBasicInformation()
+    scenarios should contain(
+      ScenarioBasicInformation(
+        name = "RTMClientNearPOS",
+        isArchived = false,
+        isFragment = false,
+        processingType = "streaming",
+        processCategory = "Default",
+        labels = List.empty,
+        state = ScenarioState(ScenarioStatus("RUNNING"))
+      )
+    )
+  }
 
   override protected def afterEach(): Unit = {
     client.purgeKafkaTopic("GeoLocations")
