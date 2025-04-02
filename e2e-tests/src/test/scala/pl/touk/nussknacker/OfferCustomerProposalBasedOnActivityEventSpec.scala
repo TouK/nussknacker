bb57d0aa7b99964fcea00b472b78854fe8c613e7
Michał Goworko <m.goworko.pl@gmail.com>
2025-04-02 15:50:27 +0200

    review changes

--- e2e-tests/src/test/scala/pl/touk/nussknacker/OfferCustomerProposalBasedOnActivityEventSpec.scala
+++ e2e-tests/src/test/scala/pl/touk/nussknacker/OfferCustomerProposalBasedOnActivityEventSpec.scala
@@ -2,22 +2,22 @@ package pl.touk.nussknacker
 
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.test.VeryPatientScalaFutures
 import pl.touk.nussknacker.utils.BasicScenarioInformationFetching
 import pl.touk.nussknacker.utils.BasicScenarioInformationFetching.{
   ScenarioBasicInformation,
   ScenarioState,
   ScenarioStatus
 }
+import ujson.Str
 
 class OfferCustomerProposalBasedOnActivityEventSpec
     extends AnyFreeSpecLike
     with BaseE2ESpec
     with Matchers
+    with VeryPatientScalaFutures
     with BasicScenarioInformationFetching {
 
-  // The OfferCustomerProposalBasedOnActivityEvent is a highly dynamic scenario,
-  // with output depending on events representing customers actions.
-  // We currently only check, that the scenario is compiled and running)
   "OfferCustomerProposalBasedOnActivityEvent scenario is running" in {
     val scenarios = fetchScenariosBasicInformation()
     scenarios should contain(
@@ -31,6 +31,43 @@ class OfferCustomerProposalBasedOnActivityEventSpec
         state = ScenarioState(ScenarioStatus("RUNNING"))
       )
     )
+  }
+
+  "Generate offers only for of-age customer and ClientBrowseOffers event" in {
+    val events = List(
+      ujson.Obj(
+        "customerId" -> "1",
+        "eventType"  -> "ClientBrowseOffers"
+      ),
+      ujson.Obj(
+        "customerId"    -> "2",
+        "assignedOffer" -> "ClientBrowseOffers"
+      )
+    )
+
+    events.foreach { customer =>
+      client.sendMessageToKafka("CustomerEvents", customer)
+    }
+
+    eventually {
+      val offers = client.readAllMessagesFromKafka("OfferProposalsBasedOnCustomerEvents")
+
+      offers.length shouldBe 1
+
+      val offer = offers.head.obj.toMap
+      offer("customerId") shouldBe Str("1")
+      offer("profileId") shouldBe Str("111")
+
+      // Assert, that fields with dynamic values are present
+      offer.keys should contain allOf (
+        "offerName",
+        "preparedMessageReadyToSend",
+        "clientMsisdn",
+        "dueDate",
+        "clientName",
+        "offerDescription",
+      )
+    }
   }
 
   override protected def afterEach(): Unit = {
