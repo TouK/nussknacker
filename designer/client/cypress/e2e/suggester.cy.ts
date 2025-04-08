d74b1cf0a5b0d55548c0325db7e1f0c2f7ecba8e
ForrestFairy <szymon.bogusz55@gmail.com>
2025-04-08 18:13:39 +0200

    Default value for event generator (#7700)
    Added default value for event generator, so it won't turn red immediately

--- designer/client/cypress/e2e/suggester.cy.ts
+++ designer/client/cypress/e2e/suggester.cy.ts
@@ -38,7 +38,7 @@ describe("Expression suggester", () => {
         cy.visitNewProcess(seed, "variables");
         cy.get("[model-id=kafka-string]").trigger("dblclick");
         cy.get("[data-testid=window]").as("modal");
-        cy.get("[title=Value]").next().find(".ace_editor").click().type("{selectall}#DATE.parseDat");
+        cy.get("[title=Value]").next().find(".ace_editor").click().type("{selectall}#DATE_FORMAT.parseLocalDat");
         // We wait for validation result to be sure that red message below the form field will be visible
         cy.contains("Failed to parse expression").should("exist");
         cy.get(".ace_autocomplete").should("be.visible");
