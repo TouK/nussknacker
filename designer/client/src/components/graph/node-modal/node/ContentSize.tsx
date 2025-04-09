d3120a9436db61a06127e7372f964abcb72d2b9e
Julian Wielga <j@touk.pl>
2025-04-09 13:10:50 +0200

    cleanup and explanations

--- designer/client/src/components/graph/node-modal/node/ContentSize.tsx
+++ designer/client/src/components/graph/node-modal/node/ContentSize.tsx
@@ -3,6 +3,8 @@ import { styled } from "@mui/material";
 import type { PropsWithChildren } from "react";
 import React from "react";
 
+import { DEFAULT_WINDOW_WIDTH } from "../../../../windowManager/useWindows";
+
 const ModalContentDark = ({
     className,
     ...props
@@ -16,7 +18,9 @@ export const ContentSize = styled(ModalContentDark)({
     padding: "1.0em 0.5em 1.5em",
     display: "flex",
     ">div": {
-        width: 720,
+        // minimal content size for dynamic width windows to expand to. below that size horizontal scrollbar appears.
+        // 100 is safe margin for DEFAULT_WINDOW_WIDTH sized windows for some scrollbars etc.
+        width: DEFAULT_WINDOW_WIDTH - 100,
         flex: 1,
     },
 });
