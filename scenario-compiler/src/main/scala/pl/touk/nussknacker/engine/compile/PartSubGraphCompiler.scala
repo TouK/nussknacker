c074fb70a9f2d34986d59c69483760c5a65b4a82
Rafał Solarski <solarski.rafal@gmail.com>
2025-04-23 09:57:53 +0200

    Enricher output mocking in tests - wip

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/compile/PartSubGraphCompiler.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/compile/PartSubGraphCompiler.scala
@@ -210,15 +210,19 @@ class PartSubGraphCompiler(nodeCompiler: NodeCompiler) {
           (ref, next) => compiledgraph.node.Processor(id, ref, next, isDisabled.contains(true))
         )
 
-      case enricher @ Enricher(id, _, output, _) =>
+      case enricher @ Enricher(id, _, output, _, mockExpression) =>
         val NodeCompilationResult(typingInfo, parameters, newCtx, validatedServiceRef, _) =
           nodeCompiler.compileEnricher(enricher, ctx, outputVar = OutputVar.enricher(output))
+        //tddo: compile expression
 
-        CompilationResult.map3(
+        //todo: type expression here and check if it match output variable type
+
+        CompilationResult.map4(
           toCompilationResult(validatedServiceRef, typingInfo, parameters),
           CompilationResult(newCtx),
-          compile(next, newCtx.getOrElse(ctx))
-        )((ref, _, next) => compiledgraph.node.Enricher(id, ref, output, next))
+          compile(next, newCtx.getOrElse(ctx)),
+
+        )((ref, _, next) => compiledgraph.node.Enricher(id, ref, output, next, ))
 
       // here we don't do anything, in subgraphcompiler it's just pass through, we can't add input context here because it contains output variable context (not input)
       case CustomNode(id, _, nodeType, _, _) =>
