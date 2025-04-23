882d90a935f998b20b96b9282e8d56dcc71366b0
Łukasz Bigorajski <lbg@touk.pl>
2025-04-23 16:17:06 +0200

    [NU-2125] Add json template editor to appropriate components
    # Conflicts:
    #	scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/language/json/JsonTemplateParser.scala

---
+++ components-api/src/main/java/pl/touk/nussknacker/engine/api/ExpressionLanguage.java
@@ -0,0 +1,10 @@
+package pl.touk.nussknacker.engine.api;
+
+public enum ExpressionLanguage {
+    SPEL,
+    SPEL_TEMPLATE,
+    DICT_KEY_WITH_LABEL,
+    TABULAR_DATA_DEFINITION,
+    JSON,
+    JSON_TEMPLATE,
+}
