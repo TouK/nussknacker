5cb0b9f319a4c0e2ce73b6dfb0e238ad9306ab55
deamondev <piotr.rudnicki94@protonmail.com>
2025-04-24 11:43:54 +0200

    [Nu-7983] Add test for unbalanced brackets

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/fragment/FragmentParameterTypingParser.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/definition/fragment/FragmentParameterTypingParser.scala
@@ -45,7 +45,7 @@ class FragmentParameterTypingParser(classLoader: ClassLoader, classDefinitions: 
 
   def parseClassNameToTypingResult(className: String): Try[TypingResult] = {
 
-    Try(parse(className, typParser(_)) match {
+    Try(parse(className, implicit ctx => typParser(ctx) ~ End) match {
       case Parsed.Success(typingResult, _) => typingResult
       case Parsed.Failure(label, index, extra) =>
         throw new IllegalArgumentException(s"Parsing failed at $index: $label\n${extra.trace().longMsg}")
