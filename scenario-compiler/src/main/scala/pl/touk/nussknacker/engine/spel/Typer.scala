7e1e616c4d0062ab0d51120b5c79f2dd626767a4
Pawel Czajka <pcz@touk.pl>
2025-04-25 14:48:39 +0200

    shorten in another place

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/spel/Typer.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/spel/Typer.scala
@@ -716,28 +716,11 @@ private[spel] class Typer(
         if tc.runtimeObjType.canBeLooselyAssignedTo(Typed[java.util.Collection[_]]) ||
           tc.runtimeObjType.klass.isArray =>
       valid(tc.runtimeObjType.params.headOption.getOrElse(Unknown))
-    // it would have been caught by the next case, but since IndexedRecord does not have type parameters we
+    // it would have been caught by the next case, but since IndexedRecord and Row does not have type parameters we
     // extract iterative type in different way
     case tc: TypedObjectTypingResult
-        if AssignabilityUtil.isAssignableToLoadableClass(
-          tc.runtimeObjType.klass,
-          "org.apache.avro.generic.IndexedRecord"
-        ) =>
-      valid(
-        Typed.record(
-          Map(
-            "key"   -> Typed.genericTypeClass(classOf[String], List()),
-            "value" -> superTypeOfTypes(tc.fields.values)
-          )
-        )
-      )
-    // it would have been caught by the next case, but since flink row does not have type parameters we
-    // extract iterative type in different way
-    case tc: TypedObjectTypingResult
-        if AssignabilityUtil.isAssignableToLoadableClass(
-          tc.runtimeObjType.klass,
-          "org.apache.flink.types.Row"
-        ) =>
+        if List("org.apache.avro.generic.IndexedRecord", "org.apache.flink.types.Row")
+          .exists(className => AssignabilityUtil.isAssignableToLoadableClass(tc.runtimeObjType.klass, className)) =>
       valid(
         Typed.record(
           Map(
