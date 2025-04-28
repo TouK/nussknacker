a97109ce48372ea9ab770565a6e60f81fdc19103
Piotr Rudnicki <piotr.rudnicki94@protonmail.com>
2025-04-28 11:02:40 +0200

    [Nu-7983] Improve FragmentParameterTypingParser (#7985)
    * [Nu-7983] Improve FragmentParameterTypingParser
    
    * [Nu-7983] Add test for unbalanced brackets
    
    * [Nu-7983] Remove redundant implicit dependency
    
    * [Nu-7983] Add even more test
    
    * [Nu-7983] Optimize imports
    
    * [Nu-7983] Simple rename
    
    * [Nu-7983] Reorder parsers in code
    
    * [Nu-7983] Add one more test

---
+++ scenario-compiler/src/test/scala/pl/touk/nussknacker/engine/definition/fragment/FragmentParameterTypingParserSpec.scala
@@ -0,0 +1,47 @@
+package pl.touk.nussknacker.engine.definition.fragment
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.engine.api.typed.typing.TypedClass
+import pl.touk.nussknacker.engine.definition.clazz.{ClassDefinitionSet, ClassDefinitionTestUtils}
+
+import scala.util.{Failure, Success}
+
+class FragmentParameterTypingParserSpec extends AnyFunSuite with Matchers {
+  private val classLoader = getClass.getClassLoader
+
+  private val classDefinitionExtractor = ClassDefinitionTestUtils.DefaultExtractor
+
+  private val clazzDefinitions: ClassDefinitionSet = ClassDefinitionSet(
+    Set(
+      classDefinitionExtractor.extract(classOf[String]),
+      classDefinitionExtractor.extract(classOf[Integer]),
+    )
+  )
+
+  private val fragmentParameterTypingParser = new FragmentParameterTypingParser(classLoader, clazzDefinitions)
+
+  test("should parse nested generic Map type into TypedClass") {
+    val triedTypingResult =
+      fragmentParameterTypingParser.parseClassNameToTypingResult("Map[List[Integer], Map[String, List[Integer]]]")
+
+    triedTypingResult match {
+      case Failure(ex) => fail(s"Unexpected failure: $ex")
+      case Success(tc: TypedClass) =>
+        tc.display shouldBe "Map[List[Integer],Map[String,List[Integer]]]"
+      case Success(tr) =>
+        fail(s"Expected a result type which is TypedClass but got instead: ${tr.getClass.getSimpleName}")
+    }
+  }
+
+  test("should fail to parse a class name when it contains a non-existing type") {
+    val triedTypingResult =
+      fragmentParameterTypingParser.parseClassNameToTypingResult("Map[String, NotExistingType]")
+
+    triedTypingResult match {
+      case Failure(ex) => ex shouldBe a[ClassNotFoundException]
+      case Success(tr) => fail(s"Expected failure due to non-existing type, but got success with: $tr")
+    }
+  }
+
+}
