510dc33156ec65145692a07bd766cf42ad6b9ec9
Pawel Czajka <pcz@touk.pl>
2025-04-28 09:41:47 +0200

    compilation fix

--- components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/TypeConversionHandler.scala
+++ components-api/src/main/scala/pl/touk/nussknacker/engine/api/typed/TypeConversionHandler.scala
@@ -12,7 +12,7 @@ import java.nio.charset.Charset
 import java.time._
 import java.time.chrono.{ChronoLocalDate, ChronoLocalDateTime}
 import java.util.{Currency, UUID}
-import scala.collection.compat.toOptionCompanionExtension
+import scala.collection.compat._
 import scala.reflect.{classTag, ClassTag}
 import scala.util.Try
 
