6e5e626416f502d29caa980424066bd32306a33d
Rafał Solarski <solarski.rafal@gmail.com>
2025-04-30 14:34:50 +0200

    Showing mockExpression validation error next to the exact parameter on form.

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/compile/nodecompilation/NodeCompiler.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/compile/nodecompilation/NodeCompiler.scala
@@ -20,6 +20,7 @@ import pl.touk.nussknacker.engine.canonize.MissingSinkHandler
 import pl.touk.nussknacker.engine.compile._
 import pl.touk.nussknacker.engine.compile.nodecompilation.NodeCompiler.{
   EnricherCompilationResult,
+  MockExpressionParameterName,
   NodeCompilationResult
 }
 import pl.touk.nussknacker.engine.compiledgraph.{CompiledParameter, TypedParameter}
@@ -45,6 +46,8 @@ import shapeless.Typeable
 import shapeless.syntax.typeable._
 
 object NodeCompiler {
+
+  val MockExpressionParameterName: ParameterName = ParameterName("mockExpression")
 
   case class NodeCompilationResult[T](
       expressionTypingInfo: Map[String, ExpressionTypingInfo],
@@ -412,7 +415,7 @@ class NodeCompiler(
       implicit nodeId: NodeId
   ): ValidatedNel[ProcessCompilationError, CompiledExpression] = {
     expressionCompiler
-      .compile(expression, None, ctx, expectedType)
+      .compile(expression, Some(MockExpressionParameterName), ctx, expectedType)
       .map(_.expression)
   }
 
