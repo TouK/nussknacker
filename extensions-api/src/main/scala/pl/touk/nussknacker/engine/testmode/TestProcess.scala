5fab9d14eb4e73e72ca591e11b42e5781e26602f
Michał Goworko <m.goworko.pl@gmail.com>
2025-05-13 12:52:41 +0200

    qs

--- extensions-api/src/main/scala/pl/touk/nussknacker/engine/testmode/TestProcess.scala
+++ extensions-api/src/main/scala/pl/touk/nussknacker/engine/testmode/TestProcess.scala
@@ -126,7 +126,9 @@ object TestProcess {
   }
 
   case class ResultContext[T](id: String, variables: Map[String, T]) {
+
     def variableTyped[U <: T](name: String): Option[U] = variables.get(name).map(_.asInstanceOf[U])
+
   }
 
 }
