e1b069ca1a1c063f350b946f368b83788e10f978
Julian Wielga <j@touk.pl>
2025-05-15 20:39:46 +0200

    optimize scenario state

--- designer/client/src/reducers/selectors/scenarioState.ts
+++ designer/client/src/reducers/selectors/scenarioState.ts
@@ -1,4 +1,10 @@
+import { createSelector } from "reselect";
+
 import type { ProcessStateType } from "../../components/Process/types";
 import type { RootState } from "../index";
 
 export const getProcessState = (state: RootState): ProcessStateType | null => state.scenarioState;
+
+export const getIsRunning = createSelector(getProcessState, (state) => {
+    return ["RUNNING", "SCHEDULED"].includes(state?.status?.name);
+});
