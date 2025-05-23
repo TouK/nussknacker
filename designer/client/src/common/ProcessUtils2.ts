a1ec55ed641cff447543090d643640b441236d65
Julian Wielga <j@touk.pl>
2025-05-23 09:45:35 +0200

    extract functions

--- designer/client/src/common/ProcessUtils2.ts
+++ designer/client/src/common/ProcessUtils2.ts
@@ -8,57 +8,74 @@ import { getScenario, isProcessRenamed } from "../reducers/selectors/graph";
 import type { NodeResults, ScenarioGraph, ValidationErrors, ValidationResult } from "../types";
 
 // easy extractable selectors-like
+const nothingToSave = (state: RootState): boolean => {
+    const scenario: Scenario = getScenario(state);
+    const savedProcessState: Scenario = getHistoryPast(state)?.[0]?.scenario || scenario;
+
+    /**
+     * It's a fix of https://touk-jira.atlassian.net/browse/NU-2194
+     * When node is added from a toolbar, branchParametersTemplate are initially added to the node, but when we perform a scenario save, node has no branchParametersTemplate
+     * Let's ignore branchParametersTemplate in a button save state checking
+     */
+    const omitBranchParametersTemplate = (details: ScenarioGraph) => {
+        if (!details.nodes?.length) {
+            return details;
+        }
+
+        return {
+            ...details,
+            nodes: details.nodes.map((node) => omit(node, ["branchParametersTemplate"])),
+        };
+    };
+    const processRenamed = isProcessRenamed(state);
+
+    if (processRenamed) {
+        return false;
+    }
+
+    if (isEmpty(scenario)) {
+        return true;
+    }
+
+    const labelsFor = (scenario: Scenario): string[] => {
+        return scenario.labels ? scenario.labels.slice().sort((a, b) => a.localeCompare(b)) : [];
+    };
+
+    const isGraphUpdated = isEqual(
+        omitBranchParametersTemplate(scenario.scenarioGraph),
+        omitBranchParametersTemplate(savedProcessState.scenarioGraph),
+    );
+    const areScenarioLabelsUpdated = isEqual(labelsFor(scenario), labelsFor(savedProcessState));
+
+    return !savedProcessState || (isGraphUpdated && areScenarioLabelsUpdated);
+};
+
+const canExport = (state: RootState): boolean => {
+    const scenario = getScenario(state);
+    return isEmpty(scenario) ? false : !isEmpty(scenario.scenarioGraph.nodes);
+};
+
+const isValidationResultPresent = (scenario: Scenario) => {
+    return Boolean(scenario.validationResult);
+};
+
+const getValidationResult = (scenario: Scenario): ValidationResult =>
+    scenario?.validationResult || {
+        validationErrors: [],
+        validationWarnings: [],
+        nodeResults: {},
+        errors: {
+            globalErrors: [],
+            processPropertiesErrors: [],
+            invalidNodes: {},
+        },
+    };
+
 export class ProcessUtils2 {
-    nothingToSave = (state: RootState): boolean => {
-        const scenario: Scenario = getScenario(state);
-        const savedProcessState: Scenario = getHistoryPast(state)?.[0]?.scenario || scenario;
-
-        /**
-         * It's a fix of https://touk-jira.atlassian.net/browse/NU-2194
-         * When node is added from a toolbar, branchParametersTemplate are initially added to the node, but when we perform a scenario save, node has no branchParametersTemplate
-         * Let's ignore branchParametersTemplate in a button save state checking
-         */
-        const omitBranchParametersTemplate = (details: ScenarioGraph) => {
-            if (!details.nodes?.length) {
-                return details;
-            }
-
-            return {
-                ...details,
-                nodes: details.nodes.map((node) => omit(node, ["branchParametersTemplate"])),
-            };
-        };
-        const processRenamed = isProcessRenamed(state);
-
-        if (processRenamed) {
-            return false;
-        }
-
-        if (isEmpty(scenario)) {
-            return true;
-        }
-
-        const labelsFor = (scenario: Scenario): string[] => {
-            return scenario.labels ? scenario.labels.slice().sort((a, b) => a.localeCompare(b)) : [];
-        };
-
-        const isGraphUpdated = isEqual(
-            omitBranchParametersTemplate(scenario.scenarioGraph),
-            omitBranchParametersTemplate(savedProcessState.scenarioGraph),
-        );
-        const areScenarioLabelsUpdated = isEqual(labelsFor(scenario), labelsFor(savedProcessState));
-
-        return !savedProcessState || (isGraphUpdated && areScenarioLabelsUpdated);
-    };
-
-    canExport = (state: RootState): boolean => {
-        const scenario = getScenario(state);
-        return isEmpty(scenario) ? false : !isEmpty(scenario.scenarioGraph.nodes);
-    };
-
-    isValidationResultPresent = (scenario: Scenario) => {
-        return Boolean(scenario.validationResult);
-    };
+    nothingToSave = nothingToSave;
+    canExport = canExport;
+    isValidationResultPresent = isValidationResultPresent;
+    getValidationResult = getValidationResult;
 
     //fixme maybe return hasErrors flag from backend?
     hasNeitherErrorsNorWarnings = (scenario: Scenario) => {
@@ -74,18 +91,6 @@ export class ProcessUtils2 {
                 (result.processPropertiesErrors || []).length == 0)
         );
     };
-
-    getValidationResult = (scenario: Scenario): ValidationResult =>
-        scenario?.validationResult || {
-            validationErrors: [],
-            validationWarnings: [],
-            nodeResults: {},
-            errors: {
-                globalErrors: [],
-                processPropertiesErrors: [],
-                invalidNodes: {},
-            },
-        };
 
     hasNoWarnings = (scenario: Scenario) => {
         const warnings = this.getValidationResult(scenario).warnings;
