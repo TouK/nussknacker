42f91756154157c9ecc57c67cb76b9bd956b4ae5
Piotr Rudnicki <piotr.rudnicki94@protonmail.com>
2025-05-28 13:40:03 +0200

    [Nu-8156] Improve UI logging while migrating scenarios

--- designer/client/src/http/HttpService.ts
+++ designer/client/src/http/HttpService.ts
@@ -941,7 +941,13 @@ class HttpService {
                     }),
                 ),
             )
-            .catch((error) => this.#addError(i18next.t("notification.error.failedToMigrate", "Failed to migrate"), error, true));
+            .catch((error) =>
+                this.#addError(
+                    i18next.t("notification.error.failedToMigrate", "Failed to migrate: {{ cause }}", { cause: error.response.data }),
+                    error,
+                    true,
+                ),
+            );
     }
 
     fetchOAuth2AccessToken<T>(provider: string, authorizeCode: string | string[], redirectUri: string | null) {
