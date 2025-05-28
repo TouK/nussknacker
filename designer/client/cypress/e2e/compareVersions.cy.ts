190e8a3a78d540fb5e580ccde632ab9c2b8dc7db
Dawid Poliszak <dpl@touk.pl>
2025-05-28 12:37:43 +0200

    NU-2197 fix tests

--- designer/client/cypress/e2e/compareVersions.cy.ts
+++ designer/client/cypress/e2e/compareVersions.cy.ts
@@ -60,7 +60,7 @@ describe("Compare versions", () => {
         cy.contains(/^apply/i)
             .should("be.enabled")
             .click();
-        cy.get("@window").should("not.exist");
+        cy.get("[data-testid=window]").should("not.exist");
         cy.verifySaveIndicator();
         cy.contains(/^save$/i).click();
         cy.contains(/^ok$/i).click();
