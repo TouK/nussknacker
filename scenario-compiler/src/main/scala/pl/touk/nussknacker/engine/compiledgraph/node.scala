0098905ceef3649ecb2ab886de511c3013728770
Łukasz Jędrzejewski <ljd@touk.pl>
2025-05-30 11:37:02 +0200

    revert formatting

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/compiledgraph/node.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/compiledgraph/node.scala
@@ -30,13 +30,7 @@ object node {
 
   case class EndingProcessor(id: String, service: ServiceRef, isDisabled: Boolean) extends Node
 
-  case class Enricher(
-      id: String,
-      service: ServiceRef,
-      output: String,
-      next: Option[Next],
-      mockedOutput: Option[CompiledExpression]
-  ) extends Node
+  case class Enricher(id: String, service: ServiceRef, output: String, next: Option[Next], mockedOutput: Option[CompiledExpression]) extends Node
 
   case class Filter(
       id: String,
