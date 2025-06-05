28e210c324cb65136c91242f122118d894e8ee72
Arek Burdach <arek.burdach@gmail.com>
2025-06-05 14:46:48 +0200

    dropNullValues + test fixes

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/PreliminaryScenarioTestData.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/PreliminaryScenarioTestData.scala
@@ -1,19 +1,24 @@
 package pl.touk.nussknacker.ui.process.test
 
 import cats.data.NonEmptyList
-import io.circe.Json
+import io.circe.{Decoder, Encoder, Json}
 import io.circe.generic.JsonCodec
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import pl.touk.nussknacker.engine.api.test.ScenarioTestJsonRecord
 
 case class PreliminaryScenarioTestData(testRecords: NonEmptyList[PreliminaryScenarioTestRecord])
 
-@JsonCodec final case class PreliminaryScenarioTestRecord(
+final case class PreliminaryScenarioTestRecord(
     sourceId: String,
     record: Json,
     timestamp: Option[Long] = None
 )
 
 object PreliminaryScenarioTestRecord {
+
+  implicit val encoder: Encoder[PreliminaryScenarioTestRecord] =
+    deriveEncoder[PreliminaryScenarioTestRecord].mapJson(_.dropNullValues)
+  implicit val decoder: Decoder[PreliminaryScenarioTestRecord] = deriveDecoder[PreliminaryScenarioTestRecord]
 
   def apply(ScenarioTestJsonRecord: ScenarioTestJsonRecord): PreliminaryScenarioTestRecord = {
     PreliminaryScenarioTestRecord(
