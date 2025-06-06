f6b22574e26e186b5eb817a863b6e0450855e44b
Michał Goworko <m.goworko.pl@gmail.com>
2025-06-06 15:09:06 +0200

    qs

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/api/livedata/ScenarioLiveDataApiHttpServiceSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/api/livedata/ScenarioLiveDataApiHttpServiceSpec.scala
@@ -1,82 +1,109 @@
-package pl.touk.nussknacker.ui.api.livedata
-
-import io.restassured.RestAssured.given
-import io.restassured.module.scala.RestAssuredSupport.AddThenToResponse
-import org.apache.pekko.http.scaladsl.model.StatusCodes
-import org.scalatest.freespec.AnyFreeSpecLike
-import pl.touk.nussknacker.development.manager.MockableDeploymentManagerProvider.MockableDeploymentManager
-import pl.touk.nussknacker.engine.api.deployment.NoLiveDataPreviewSupport
-import pl.touk.nussknacker.engine.build.ScenarioBuilder
-import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
-import pl.touk.nussknacker.test.{
-  NuRestAssureMatchers,
-  PatientScalaFutures,
-  RestAssuredVerboseLoggingIfValidationFails,
-  WithTestHttpClient
-}
-import pl.touk.nussknacker.test.base.it.{NuItTest, WithSimplifiedConfigScenarioHelper}
-import pl.touk.nussknacker.test.config.{
-  WithBusinessCaseRestAssuredUsersExtensions,
-  WithMockableDeploymentManager,
-  WithSimplifiedDesignerConfig
-}
-
-import java.time.Instant
-
-class ScenarioLiveDataApiHttpServiceSpec
-    extends AnyFreeSpecLike
-    with NuItTest
-    with WithTestHttpClient
-    with WithSimplifiedDesignerConfig
-    with WithSimplifiedConfigScenarioHelper
-    with WithMockableDeploymentManager
-    with WithBusinessCaseRestAssuredUsersExtensions
-    with NuRestAssureMatchers
-    with RestAssuredVerboseLoggingIfValidationFails
-    with PatientScalaFutures {
-
-  import pl.touk.nussknacker.engine.spel.SpelExtension._
-
-  private val exampleScenario: CanonicalProcess =
-    ScenarioBuilder
-      .streaming("scenario_2")
-      .source(
-        "Event Generator",
-        "event-generator",
-        "count"    -> "1".spel,
-        "value"    -> "1".spel,
-        "schedule" -> "T(java.time.Duration).parse('PT1M')".spel,
-      )
-      .emptySink("end", "dead-end")
-
-  private val mockedInstant = Instant.ofEpochSecond(1748382500)
-
-  "The endpoint for live data should" - {
+//package pl.touk.nussknacker.ui.api.livedata
+//
+//import io.circe.Json
+//import io.circe.syntax.EncoderOps
+//import io.restassured.RestAssured.given
+//import io.restassured.module.scala.RestAssuredSupport.AddThenToResponse
+//import org.apache.pekko.http.scaladsl.model.StatusCodes
+//import org.scalatest.freespec.AnyFreeSpecLike
+//import pl.touk.nussknacker.development.manager.MockableDeploymentManagerProvider.MockableDeploymentManager
+//import pl.touk.nussknacker.engine.api.NodeId
+//import pl.touk.nussknacker.engine.api.deployment.{LiveDataPreviewSupported, NoLiveDataPreviewSupport}
+//import pl.touk.nussknacker.engine.api.deployment.LiveDataPreviewSupported._
+//import pl.touk.nussknacker.engine.api.process.ProcessIdWithName
+//import pl.touk.nussknacker.engine.build.ScenarioBuilder
+//import pl.touk.nussknacker.engine.canonicalgraph.CanonicalProcess
+//import pl.touk.nussknacker.test.{
+//  NuRestAssureMatchers,
+//  PatientScalaFutures,
+//  RestAssuredVerboseLoggingIfValidationFails,
+//  WithTestHttpClient
+//}
+//import pl.touk.nussknacker.test.base.it.{NuItTest, WithSimplifiedConfigScenarioHelper}
+//import pl.touk.nussknacker.test.config.{
+//  WithBusinessCaseRestAssuredUsersExtensions,
+//  WithMockableDeploymentManager,
+//  WithSimplifiedDesignerConfig
+//}
+//
+//import java.time.Instant
+//import scala.concurrent.Future
+//
+//class ScenarioLiveDataApiHttpServiceSpec
+//  extends AnyFreeSpecLike
+//    with NuItTest
+//    with WithTestHttpClient
+//    with WithSimplifiedDesignerConfig
+//    with WithSimplifiedConfigScenarioHelper
+//    with WithMockableDeploymentManager
+//    with WithBusinessCaseRestAssuredUsersExtensions
+//    with NuRestAssureMatchers
+//    with RestAssuredVerboseLoggingIfValidationFails
+//    with PatientScalaFutures {
+//
+//  import pl.touk.nussknacker.engine.spel.SpelExtension._
+//
+//  private val exampleScenario: CanonicalProcess =
+//    ScenarioBuilder
+//      .streaming("scenario_2")
+//      .source(
+//        "Event Generator",
+//        "event-generator",
+//        "count"    -> "1".spel,
+//        "value"    -> "1".spel,
+//        "schedule" -> "T(java.time.Duration).parse('PT1M')".spel,
+//      )
+//      .emptySink("end", "dead-end")
+//
+//  private val mockedInstant = Instant.ofEpochSecond(1748382500)
+//
+//  "The endpoint for live data should" - {
 //    "return present, but empty live data" in {
-//      CollectedLiveData(mockedInstant, Map.empty, Map.empty, Map.empty, Map.empty)
-//      LiveDataCollectingListenerHolder.createListenerFor(exampleScenario.name, 10, 60)
+//      val mockedResults = LiveData(mockedInstant, Map.empty, Map.empty, Map.empty, Map.empty)
 //      given()
 //        .applicationState {
 //          createSavedScenario(exampleScenario)
-//          MockableDeploymentManager.configureLiveDataPreviewSupport(LiveDataPreviewStoredInDesignerJvm)
+//          MockableDeploymentManager.configureLiveDataPreviewSupport(
+//            new LiveDataPreviewSupported {
+//              override def getLiveData(
+//                                        processIdWithName: ProcessIdWithName
+//                                      ): Future[Either[LiveDataError, LiveData]] = Future.successful(Right(mockedResults))
+//            }
+//          )
 //        }
 //        .when()
 //        .basicAuthAllPermUser()
 //        .get(s"$nuDesignerHttpAddress/api/liveData/${exampleScenario.name}")
 //        .Then()
 //        .statusCode(StatusCodes.OK.intValue)
-//        .matchJsonWithRegexValuesBody(
+//        .equalsJsonBody(
 //          s"""{
-//             |    "timestamp": "${regexes.zuluDateRegex}",
-//             |    "nodeTransitions": [],
+//             |  "timestamp": "2025-05-27T21:48:20Z",
+//             |  "results": {
+//             |    "nodeResults": null,
+//             |    "nodeTransitionResults": [],
 //             |    "invocationResults": {},
 //             |    "externalInvocationResults": {},
+//             |    "exceptions": [],
 //             |    "exceptionsByNodeId": {}
+//             |  },
+//             |  "counts": {
+//             |    "Event Generator": {
+//             |      "all": 0,
+//             |      "errors": 0,
+//             |      "fragmentCounts": {}
+//             |    },
+//             |    "end": {
+//             |      "all": 0,
+//             |      "errors": 0,
+//             |      "fragmentCounts": {}
+//             |    }
+//             |  }
 //             |}""".stripMargin
 //        )
 //    }
 //    "return present data" in {
-//      val mockedResults = CollectedLiveData(
+//      val mockedResults = LiveData(
 //        timestamp = mockedInstant,
 //        nodeTransitions = Map(
 //          NodeTransition("start", Some("variable")) -> LiveDataForNodeTransition(
@@ -130,8 +157,8 @@ class ScenarioLiveDataApiHttpServiceSpec
 //          MockableDeploymentManager.configureLiveDataPreviewSupport(
 //            new LiveDataPreviewSupported {
 //              override def getLiveData(
-//                  processIdWithName: ProcessIdWithName
-//              ): Future[Either[LiveDataError, LiveData]] = Future.successful(Right(mockedResults))
+//                                        processIdWithName: ProcessIdWithName
+//                                      ): Future[Either[LiveDataError, LiveData]] = Future.successful(Right(mockedResults))
 //            }
 //          )
 //        }
@@ -142,65 +169,100 @@ class ScenarioLiveDataApiHttpServiceSpec
 //        .statusCode(StatusCodes.OK.intValue)
 //        .equalsJsonBody(
 //          s"""{
-//             |    "timestamp": "2025-05-27T21:48:20Z",
-//             |    "nodeTransitions": [
-//             |        {
-//             |            "sourceNodeId": "start",
-//             |            "destinationNodeId": "variable",
-//             |            "samples": [
-//             |                {
-//             |                    "contextId": "",
-//             |                    "timestamp": "2025-05-27T21:48:20Z",
-//             |                    "variables": {
-//             |                        "v1": {
-//             |                            "a": "aaa",
-//             |                            "b": 1
-//             |                        }
-//             |                    }
-//             |                }
-//             |            ],
-//             |            "totalCount": 101,
-//             |            "currentThroughput": 0.9811
-//             |        }
+//             |  "timestamp": "2025-05-27T21:48:20Z",
+//             |  "results": {
+//             |    "nodeResults": null,
+//             |    "nodeTransitionResults": [
+//             |      {
+//             |        "sourceNodeId": "start",
+//             |        "destinationNodeId": "variable",
+//             |        "results": [
+//             |          {
+//             |            "id": "",
+//             |            "timestamp": "2025-05-27T21:48:20Z",
+//             |            "variables": {
+//             |              "v1": {
+//             |                "a": "aaa",
+//             |                "b": 1
+//             |              }
+//             |            }
+//             |          }
+//             |        ],
+//             |        "totalCount": 101,
+//             |        "currentThroughput": 0.9811
+//             |      }
 //             |    ],
 //             |    "invocationResults": {
-//             |        "start": [
-//             |            {
-//             |                "contextId": "mocked-context-id",
-//             |                "timestamp": "2025-05-27T21:48:20Z",
-//             |                "name": "var",
-//             |                "value": {
-//             |                    "pretty": 1
-//             |                }
-//             |            }
-//             |        ]
+//             |      "start": [
+//             |        {
+//             |          "contextId": "mocked-context-id",
+//             |          "timestamp": "2025-05-27T21:48:20Z",
+//             |          "name": "var",
+//             |          "value": {
+//             |            "pretty": 1
+//             |          }
+//             |        }
+//             |      ]
 //             |    },
 //             |    "externalInvocationResults": {
-//             |        "start": [
-//             |            {
-//             |                "contextId": "mocked-context-id",
-//             |                "timestamp": "2025-05-27T21:48:20Z",
-//             |                "name": "var",
-//             |                "value": {
-//             |                    "pretty": 1
-//             |                }
-//             |            }
-//             |        ]
+//             |      "start": [
+//             |        {
+//             |          "contextId": "mocked-context-id",
+//             |          "timestamp": "2025-05-27T21:48:20Z",
+//             |          "name": "var",
+//             |          "value": {
+//             |            "pretty": 1
+//             |          }
+//             |        }
+//             |      ]
 //             |    },
-//             |    "exceptionsByNodeId": {
-//             |        "start": [
-//             |            {
-//             |                "contextId": "mocked-context-id",
-//             |                "timestamp": "2025-05-27T21:48:20Z",
-//             |                "variables": {
-//             |                    "var1": {
-//             |                        "pretty": "abc"
-//             |                    }
-//             |                },
-//             |                "errorMessage": "Something bad happened"
+//             |    "exceptions": [
+//             |      {
+//             |        "context": {
+//             |          "id": "mocked-context-id",
+//             |          "timestamp": "2025-05-27T21:48:20Z",
+//             |          "variables": {
+//             |            "var1": {
+//             |              "pretty": "abc"
 //             |            }
-//             |        ]
+//             |          }
+//             |        },
+//             |        "nodeId": "start",
+//             |        "throwable": "Something bad happened"
+//             |      }
+//             |    ],
+//             |    "exceptionsByNodeId": {
+//             |      "start": [
+//             |        {
+//             |          "context": {
+//             |            "id": "mocked-context-id",
+//             |            "timestamp": "2025-05-27T21:48:20Z",
+//             |            "variables": {
+//             |              "var1": {
+//             |                "pretty": "abc"
+//             |              }
+//             |            }
+//             |          },
+//             |          "nodeId": "start",
+//             |          "throwable": "Something bad happened"
+//             |        }
+//             |      ]
 //             |    }
+//             |  },
+//             |  "counts": {
+//             |    "Event Generator": {
+//             |      "all": 0,
+//             |      "errors": 0,
+//             |      "fragmentCounts": {
+//             |      }
+//             |    },
+//             |    "end": {
+//             |      "all": 0,
+//             |      "errors": 0,
+//             |      "fragmentCounts": {
+//             |      }
+//             |    }
+//             |  }
 //             |}""".stripMargin
 //        )
 //    }
@@ -211,8 +273,8 @@ class ScenarioLiveDataApiHttpServiceSpec
 //          MockableDeploymentManager.configureLiveDataPreviewSupport(
 //            new LiveDataPreviewSupported {
 //              override def getLiveData(
-//                  processIdWithName: ProcessIdWithName
-//              ): Future[Either[LiveDataError, LiveData]] =
+//                                        processIdWithName: ProcessIdWithName
+//                                      ): Future[Either[LiveDataError, LiveData]] =
 //                Future.successful(Left(LiveDataError.NoLiveDataAvailableForScenario))
 //            }
 //          )
@@ -223,18 +285,18 @@ class ScenarioLiveDataApiHttpServiceSpec
 //        .Then()
 //        .statusCode(StatusCodes.NoContent.intValue)
 //    }
-    "return live data not supported error" in {
-      given()
-        .applicationState {
-          createSavedScenario(exampleScenario)
-          MockableDeploymentManager.configureLiveDataPreviewSupport(NoLiveDataPreviewSupport)
-        }
-        .when()
-        .basicAuthAllPermUser()
-        .get(s"$nuDesignerHttpAddress/api/liveData/${exampleScenario.name}")
-        .Then()
-        .statusCode(StatusCodes.NotImplemented.intValue)
-    }
-  }
-
-}
+//    "return live data not supported error" in {
+//      given()
+//        .applicationState {
+//          createSavedScenario(exampleScenario)
+//          MockableDeploymentManager.configureLiveDataPreviewSupport(NoLiveDataPreviewSupport)
+//        }
+//        .when()
+//        .basicAuthAllPermUser()
+//        .get(s"$nuDesignerHttpAddress/api/liveData/${exampleScenario.name}")
+//        .Then()
+//        .statusCode(StatusCodes.NotImplemented.intValue)
+//    }
+//  }
+//
+//}
