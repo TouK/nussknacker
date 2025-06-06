817d4879bddbae58629d3bd8bca9af3da9cd18df
Dawid Poliszak <dpl@touk.pl>
2025-06-06 13:53:45 +0200

    NU-2244 don't show archived scenarios in breadcrumbs

--- designer/client/src/reducers/scenarios.ts
+++ designer/client/src/reducers/scenarios.ts
@@ -39,3 +39,6 @@ export function fetchScenarios(): ThunkAction {
 
 const getScenarios = (state: RootState) => state.scenarios;
 export const getScenariosNames = createSelector(getScenarios, (scenarios) => scenarios.map((scenario) => scenario.name));
+export const getActiveScenariosNames = createSelector(getScenarios, (scenarios) =>
+    scenarios.filter((scenario) => !scenario.isArchived).map((scenario) => scenario.name),
+);
