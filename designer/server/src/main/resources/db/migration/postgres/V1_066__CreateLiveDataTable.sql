1591d15b8cf27ed5885b37dd95461035f1848650
Michał Goworko <m.goworko.pl@gmail.com>
2025-06-08 22:04:35 +0200

    qs

--- designer/server/src/main/resources/db/migration/postgres/V1_066__CreateLiveDataTable.sql
+++ designer/server/src/main/resources/db/migration/postgres/V1_066__CreateLiveDataTable.sql
@@ -3,8 +3,7 @@ CREATE TABLE "live_data"
     "scenario_id"  BIGINT NOT NULL,
     "collector_id" VARCHAR NOT NULL,
     "live_data"    TEXT,
-    "updated_at"   BIGINT  NOT NULL,
-
+    "updated_at"   BIGINT  NOT NULL
 );
 
 ALTER TABLE "live_data" ADD CONSTRAINT pk_scenario_id_collector_id PRIMARY KEY ("scenario_id", "collector_id")
