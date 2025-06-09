878ab9459d76c6e75532f8c9698f4403f8413de0
Rafał Solarski <solarski.rafal@gmail.com>
2025-06-09 08:38:47 +0200

    Jdk 17 bump

--- nussknacker-dist/src/universal/bin/nussknacker-entrypoint.sh
+++ nussknacker-dist/src/universal/bin/nussknacker-entrypoint.sh
@@ -37,11 +37,33 @@ if [ "$USAGE_REPORTS_SOURCE" == "" ]; then
   export USAGE_REPORTS_SOURCE="docker"
 fi
 
+MODULES_OPEN_OPTS="--add-exports=java.base/sun.net.util=ALL-UNNAMED \
+--add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED \
+--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
+--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
+--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
+--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
+--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
+--add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED \
+--add-opens=java.base/java.lang=ALL-UNNAMED \
+--add-opens=java.base/java.net=ALL-UNNAMED \
+--add-opens=java.base/java.io=ALL-UNNAMED \
+--add-opens=java.base/java.nio=ALL-UNNAMED \
+--add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+--add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+--add-opens=java.base/java.text=ALL-UNNAMED \
+--add-opens=java.base/java.time=ALL-UNNAMED \
+--add-opens=java.base/java.util=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED"
+
 mkdir -p ${STORAGE_DIR}/db
 
 echo "Starting Nussknacker:"
 
 exec java $JDK_JAVA_OPTIONS $JAVA_DEBUG_OPTS $JAVA_PROMETHEUS_OPTS \
+          $MODULES_OPEN_OPTS \
           -Dlogback.configurationFile="$LOGBACK_FILE" \
           -Dnussknacker.config.locations="$CONFIG_FILE" -Dconfig.override_with_env_vars=true \
           -cp "$CLASSPATH" "pl.touk.nussknacker.ui.NussknackerApp"
