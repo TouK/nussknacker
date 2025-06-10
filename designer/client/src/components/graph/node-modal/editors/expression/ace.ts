1653550e01d6a4984eee9c04e58d0523faf00298
Dawid Poliszak <dpl@touk.pl>
2025-06-10 12:05:05 +0200

    NU-2243 fix ace editor tooltip position

--- designer/client/src/components/graph/node-modal/editors/expression/ace.ts
+++ designer/client/src/components/graph/node-modal/editors/expression/ace.ts
@@ -4,6 +4,7 @@ import "ace-builds/src-noconflict/ext-searchbox";
 import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/mode-jsx";
+import ace from "ace-builds";
 import AceEditor from "react-ace";
 
 import "../../../../../brace/mode/spel";
@@ -13,3 +14,21 @@ import "../../../../../brace/mode/sql";
 import "../../../../../brace/theme/nussknacker";
 
 export default AceEditor;
+
+/**
+ * There is a problem with Ace editor tooltip positioning in the modal, because of modal transform property.
+ * To avoid it, we need to set the parent node of the tooltip to document.body, to keep correct positioning for fixed position element
+ */
+const Tooltip = ace.require("ace/tooltip").Tooltip;
+const originalInit = Tooltip.prototype.$init;
+Tooltip.prototype.$init = function () {
+    this.$parentNode = document.body;
+
+    originalInit.call(this);
+};
+
+Tooltip.prototype.setHtml = function (html) {
+    if (this.getElement()) {
+        this.getElement().innerHTML = html;
+    }
+};
