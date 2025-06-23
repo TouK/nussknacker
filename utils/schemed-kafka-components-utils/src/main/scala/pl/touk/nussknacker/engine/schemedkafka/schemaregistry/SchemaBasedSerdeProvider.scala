9a3a9c5186d8b6e1d704ff321304e798b516572d
Arek Burdach <arek.burdach@gmail.com>
2025-06-23 20:26:27 +0200

    KafkaSchemaBasedKeyValueDeserializationSchemaFactory and KafkaSchemaBasedDeserializationSchemaFactory flatten

--- utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/SchemaBasedSerdeProvider.scala
+++ utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/SchemaBasedSerdeProvider.scala
@@ -3,13 +3,13 @@ package pl.touk.nussknacker.engine.schemedkafka.schemaregistry
 import pl.touk.nussknacker.engine.schemedkafka.schema.SchemaValidator
 import pl.touk.nussknacker.engine.schemedkafka.schemaregistry.universal.UniversalToJsonFormatterFactory
 import pl.touk.nussknacker.engine.schemedkafka.serialization.{
-  KafkaSchemaBasedDeserializationSchemaFactory,
+  KafkaSchemaBasedKeyValueDeserializationSchemaFactory,
   KafkaSchemaBasedSerializationSchemaFactory
 }
 
 case class SchemaBasedSerdeProvider(
     serializationSchemaFactory: KafkaSchemaBasedSerializationSchemaFactory,
-    deserializationSchemaFactory: KafkaSchemaBasedDeserializationSchemaFactory,
+    deserializationSchemaFactory: KafkaSchemaBasedKeyValueDeserializationSchemaFactory,
     recordFormatterFactory: UniversalToJsonFormatterFactory,
     schemaValidator: SchemaValidator
 )
