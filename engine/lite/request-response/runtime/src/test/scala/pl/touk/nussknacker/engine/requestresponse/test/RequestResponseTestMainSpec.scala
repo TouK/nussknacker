251d1a2e15c4746a8a0238385c49f29e10a6655f
Rafał Solarski <solarski.rafal@gmail.com>
2025-06-24 18:21:55 +0200

    review fix - stable interpretation results sorting in ScenarioInterpreterFactory

--- engine/lite/request-response/runtime/src/test/scala/pl/touk/nussknacker/engine/requestresponse/test/RequestResponseTestMainSpec.scala
+++ engine/lite/request-response/runtime/src/test/scala/pl/touk/nussknacker/engine/requestresponse/test/RequestResponseTestMainSpec.scala
@@ -199,13 +199,7 @@ class RequestResponseTestMainSpec extends AnyFunSuite with Matchers with BeforeA
     val endNodeIdInvocationResult = results.externalInvocationResults("endNodeIID").loneElement
     endNodeIdInvocationResult.contextId shouldBe contextIdGenForNodeId(process, "collect1").nextContextId()
 
-    // todo: we should discuss in Pull Request whether the order of split-union-collect nodes result in RR should be more stable - look InvokerCompiler.partInvoker
-    endNodeIdInvocationResult.value.hcursor
-      .get[List[String]]("pretty")
-      .getOrElse(throw new IllegalStateException("Lack of pretty field")) should contain theSameElementsAs List(
-      "bb",
-      "aa"
-    )
+    endNodeIdInvocationResult.value shouldBe variable(List("aa", "bb"))
   }
 
   private def createTestRecord(field1: String, field2: String) = {
