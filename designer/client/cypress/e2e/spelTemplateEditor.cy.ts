04851397e9d873f7dfee4c21cba909319d2a9b5e
Dawid Poliszak <dpl@touk.pl>
2025-06-24 16:12:18 +0200

    [NU-2245] add SpEL template based snippet (#8266)
    * NU-2245 Add spelTemplate based snippet
    
    * NU-2245 provide json template snippet test
    
    * NU-2245 provide spel template snippet test

---
+++ designer/client/cypress/e2e/spelTemplateEditor.cy.ts
@@ -0,0 +1,28 @@
+describe("SpEL template editor", () => {
+    const seed = "spelTemplate";
+
+    before(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    after(() => {
+        cy.deleteAllTestProcesses({ filter: seed });
+    });
+
+    it("should insert '#{ # }' snippet when '#{' provided", () => {
+        cy.visitNewProcess(seed, "spelTemplateEditor", "Category1");
+        cy.getNode("Delay").should("be.visible").trigger("dblclick");
+        cy.get("[title='key']").next().find(".ace_editor").should("be.visible").parent().as("editor");
+
+        cy.get("@editor").click();
+
+        cy.realType("#{");
+
+        cy.get("@editor").within(() => {
+            cy.window().then((win) => {
+                const aceEditor = (win as any).ace.edit(Cypress.$(".ace_editor")[0]);
+                expect(aceEditor.getValue()).to.eq("#{ # }");
+            });
+        });
+    });
+});
