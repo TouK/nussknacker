ff5a7853d204b6ff158a70867701be927ea1ec23
Piotr Przybylski <ppr@touk.pl>
2025-07-09 22:33:22 +0200

    docs, constants

--- docs/Changelog.md
+++ docs/Changelog.md
@@ -214,6 +214,7 @@ description: Stay informed with detailed changelogs covering new features, impro
     * currently supported for Kafka Sinks only.
 * [#8209](https://github.com/TouK/nussknacker/pull/8209) Support for jdk 1.17. Scala 2.12.10 -> 2.12.20 and 2.13.15 -> 2.13.16 migration. Using the own flink-scala library also for 2.12 builds.  
 * [8304](https://github.com/TouK/nussknacker/pull/8304) Added functionality of setting component group in ComponentDefinition using `withComponentGroup`.
+* [#8319](https://github.com/TouK/nussknacker/pull/8319) Set default Kafka serializer/deserializer in `KafkaUtils` as class names to be compatible with Flink's `KafkaSource`
 
 ## 1.18
 
--- utils/kafka-utils/src/main/scala/pl/touk/nussknacker/engine/kafka/KafkaUtils.scala
+++ utils/kafka-utils/src/main/scala/pl/touk/nussknacker/engine/kafka/KafkaUtils.scala
@@ -1,10 +1,10 @@
 package pl.touk.nussknacker.engine.kafka
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.kafka.clients.KafkaClient
+import org.apache.kafka.clients.{CommonClientConfigs, KafkaClient}
 import org.apache.kafka.clients.admin.{Admin, AdminClient}
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, KafkaConsumer}
-import org.apache.kafka.clients.producer.{Callback, Producer, ProducerRecord, RecordMetadata}
+import org.apache.kafka.clients.producer.{Callback, Producer, ProducerConfig, ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.{IsolationLevel, TopicPartition}
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer}
 import pl.touk.nussknacker.engine.api.process.TopicName
@@ -30,7 +30,7 @@ trait KafkaUtils extends LazyLogging {
   val defaultTimeoutMillis = 10000
 
   def setClientId(props: Properties, id: String): Unit = {
-    props.setProperty("client.id", sanitizeClientId(id))
+    props.setProperty(CommonClientConfigs.CLIENT_ID_CONFIG, sanitizeClientId(id))
   }
 
   def createKafkaAdminClient(kafkaConfig: KafkaConfig): Admin = {
@@ -82,8 +82,8 @@ trait KafkaUtils extends LazyLogging {
 
   def toProducerProperties(config: KafkaConfig, clientId: String): Properties = {
     val props: Properties = new Properties
-    props.put("key.serializer", classOf[ByteArraySerializer].getName)
-    props.put("value.serializer", classOf[ByteArraySerializer].getName)
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[ByteArraySerializer].getName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[ByteArraySerializer].getName)
     setClientId(props, clientId)
     withPropertiesFromConfig(props, config)
   }
@@ -102,8 +102,8 @@ trait KafkaUtils extends LazyLogging {
 
   def toConsumerProperties(config: KafkaConfig, groupId: Option[String]): Properties = {
     val props = new Properties()
-    props.put("value.deserializer", classOf[ByteArrayDeserializer].getName)
-    props.put("key.deserializer", classOf[ByteArrayDeserializer].getName)
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[ByteArrayDeserializer].getName)
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[ByteArrayDeserializer].getName)
     groupId.foreach(props.setProperty("group.id", _))
     withPropertiesFromConfig(props, config)
   }
