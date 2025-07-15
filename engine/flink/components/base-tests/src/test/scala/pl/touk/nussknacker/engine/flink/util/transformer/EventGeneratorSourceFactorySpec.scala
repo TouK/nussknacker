cc1716c1e7477688af9c6e5ffccb2a92dbf7e8ea
Arek Burdach <arek.burdach@gmail.com>
2025-07-15 16:19:42 +0200

    More types + class extraction settings

--- engine/flink/components/base-tests/src/test/scala/pl/touk/nussknacker/engine/flink/util/transformer/EventGeneratorSourceFactorySpec.scala
+++ engine/flink/components/base-tests/src/test/scala/pl/touk/nussknacker/engine/flink/util/transformer/EventGeneratorSourceFactorySpec.scala
@@ -4,6 +4,7 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.Inside
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import pl.touk.nussknacker.engine.api.typed.typing.Typed
 import pl.touk.nussknacker.engine.build.ScenarioBuilder
 import pl.touk.nussknacker.engine.flink.FlinkBaseUnboundedComponentProvider
 import pl.touk.nussknacker.engine.flink.test.FlinkSpec
@@ -15,7 +16,20 @@ import pl.touk.nussknacker.engine.testing.LocalModelData
 import pl.touk.nussknacker.engine.testmode.ResultsCollectingListenerHolder
 import pl.touk.nussknacker.test.PatientScalaFutures
 
-import java.time.Instant
+import java.nio.charset.{Charset, StandardCharsets}
+import java.time.{
+  Duration,
+  Instant,
+  LocalDate,
+  LocalDateTime,
+  LocalTime,
+  OffsetDateTime,
+  Period,
+  ZonedDateTime,
+  ZoneId,
+  ZoneOffset
+}
+import java.util.{Currency, Locale, UUID}
 import scala.jdk.CollectionConverters._
 
 class EventGeneratorSourceFactorySpec
@@ -112,7 +126,20 @@ class EventGeneratorSourceFactorySpec
           "count"    -> "1".spel,
           "value" ->
             """{
-              |  "instant": "#{ T(java.time.Instant).ofEpochMilli(123L) }"
+              |  "instant": "#{ T(java.time.Instant).ofEpochMilli(123L) }",
+              |  "offsetDateTime": "#{ T(java.time.OffsetDateTime).of(2025, 1, 1, 0, 0, 0, 0, T(java.time.ZoneOffset).UTC) }",
+              |  "zonedDateTime": "#{ T(java.time.ZonedDateTime).of(2025, 1, 1, 0, 0, 0, 0, T(java.time.ZoneOffset).UTC) }",
+              |  "localDateTime": "#{ T(java.time.LocalDateTime).of(2025, 1, 1, 0, 0, 0, 0) }",
+              |  "localDate": "#{ T(java.time.LocalDate).of(2025, 1, 1) }",
+              |  "localTime": "#{ T(java.time.LocalTime).of(12, 1) }",
+              |  "period": "#{ T(java.time.Period).ofDays(30) }",
+              |  "duration": "#{ T(java.time.Duration).ofHours(12) }",
+              |  "zoneOffset": "#{ T(java.time.ZoneOffset).of("+01:00") }",
+              |  "zoneId": "#{ T(java.time.ZoneId).of("Europe/Warsaw") }",
+              |  "locale": "#{ T(java.util.Locale).ENGLISH }",
+              |  "charset": "#{ T(java.nio.charset.StandardCharsets).UTF_8 }",
+              |  "currency": "#{ T(java.util.Currency).getInstance("USD") }",
+              |  "uuid": "#{ T(java.util.UUID).fromString("38a727ce-44d6-43ef-85b8-1fdde02108cf") }"
               |}""".stripMargin.jsonTemplate
         )
         // We adds some flink operator to enforce flink messages serialization
@@ -129,9 +156,23 @@ class EventGeneratorSourceFactorySpec
             emittedResults.size should be > 1
             emittedResults.head
           }
-          emittedRecord shouldBe Map(
-            "instant" -> Instant.ofEpochMilli(123L)
+          val expectedRecord = Map(
+            "instant"        -> Instant.ofEpochMilli(123L),
+            "offsetDateTime" -> OffsetDateTime.of(2025, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC),
+            "zonedDateTime"  -> ZonedDateTime.of(2025, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC),
+            "localDateTime"  -> LocalDateTime.of(2025, 1, 1, 0, 0, 0, 0),
+            "localDate"      -> LocalDate.of(2025, 1, 1),
+            "localTime"      -> LocalTime.of(12, 1),
+            "period"         -> Period.ofDays(30),
+            "duration"       -> Duration.ofHours(12),
+            "zoneOffset"     -> ZoneOffset.of("+01:00"),
+            "zoneId"         -> ZoneId.of("Europe/Warsaw"),
+            "locale"         -> Locale.ENGLISH,
+            "charset"        -> StandardCharsets.UTF_8,
+            "currency"       -> Currency.getInstance("USD"),
+            "uuid"           -> UUID.fromString("38a727ce-44d6-43ef-85b8-1fdde02108cf"),
           ).asJava
+          emittedRecord shouldBe expectedRecord
         }
       }
     }
