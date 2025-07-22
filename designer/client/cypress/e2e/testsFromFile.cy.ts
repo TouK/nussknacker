8f9484d4ba5821b42b1f7fc3ab116472d3f77a46
Dawid Poliszak <dpl@touk.pl>
2025-07-22 12:04:04 +0200

    wait for node validation instead

--- designer/client/cypress/e2e/testsFromFile.cy.ts
+++ designer/client/cypress/e2e/testsFromFile.cy.ts
@@ -21,9 +21,9 @@ describe("Process tests from file", () => {
             .should("exist")
             .selectFile("cypress/fixtures/transactionsTestData.txt", { force: true });
         cy.get("text[joint-selector='testResultsSummary']").eq(0).contains("1");
+        cy.intercept("POST", "/api/nodes/*/validation").as("validation");
         cy.get("[model-id='kafka']").should("be.visible").trigger("dblclick");
-        cy.get('[data-testid="window"] [role="progressbar"]').should("exist");
-        cy.get('[data-testid="window"] [role="progressbar"]').should("not.exist");
+        cy.wait("@validation");
         cy.get("[data-testid='window-frame']").matchImage();
     });
 
