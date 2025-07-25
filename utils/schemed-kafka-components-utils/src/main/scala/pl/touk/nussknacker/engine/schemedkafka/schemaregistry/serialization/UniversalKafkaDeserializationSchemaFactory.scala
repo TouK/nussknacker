1fe64bd4e6818137d9edc7f96d1de2676145b5b2
Marcin Stelmaszczyk <mst@touk.pl>
2025-07-25 08:55:41 +0200

    [NU-2298] Use 'FromJsonTypingResultBasedDecoder' when deserializing json based on data sample inferred type

--- utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/serialization/UniversalKafkaDeserializationSchemaFactory.scala
+++ utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/serialization/UniversalKafkaDeserializationSchemaFactory.scala
@@ -2,6 +2,7 @@ package pl.touk.nussknacker.engine.schemedkafka.schemaregistry.serialization
 
 import io.confluent.kafka.schemaregistry.ParsedSchema
 import org.apache.kafka.common.serialization.Deserializer
+import pl.touk.nussknacker.engine.api.typed.typing.TypingResult
 import pl.touk.nussknacker.engine.kafka.KafkaComponentsConfig
 import pl.touk.nussknacker.engine.schemedkafka.RuntimeSchemaData
 import pl.touk.nussknacker.engine.schemedkafka.schemaregistry.{
@@ -15,21 +16,23 @@ import pl.touk.nussknacker.engine.schemedkafka.serialization.KafkaSchemaBasedKey
 class UniversalKafkaDeserializationSchemaFactory(
     override protected val kafkaComponentsConfig: KafkaComponentsConfig,
     schemaRegistryClientFactory: SchemaRegistryClientFactory,
-    createSchemaIdFromMessageExtractor: SchemaRegistryClient => ChainedSchemaIdFromMessageExtractor
+    createSchemaIdFromMessageExtractor: SchemaRegistryClient => ChainedSchemaIdFromMessageExtractor,
 ) extends KafkaSchemaBasedKeyValueDeserializationSchemaFactory {
 
   override protected def createKeyDeserializer[K](
       schemaDataOpt: Option[RuntimeSchemaData[ParsedSchema]],
   ): Deserializer[K] =
-    createDeserializer[K](schemaDataOpt, isKey = true)
+    createDeserializer[K](schemaDataOpt, None, isKey = true)
 
   override protected def createValueDeserializer[V](
       schemaDataOpt: Option[RuntimeSchemaData[ParsedSchema]],
+      dataSampleTypingResult: Option[TypingResult]
   ): Deserializer[V] =
-    createDeserializer[V](schemaDataOpt, isKey = false)
+    createDeserializer[V](schemaDataOpt, dataSampleTypingResult, isKey = false)
 
   private final def createDeserializer[T](
       schemaDataOpt: Option[RuntimeSchemaData[ParsedSchema]],
+      dataSampleTypingResult: Option[TypingResult],
       isKey: Boolean
   ): Deserializer[T] = {
     val schemaRegistryClient = schemaRegistryClientFactory.create(kafkaComponentsConfig)
@@ -38,7 +41,8 @@ class UniversalKafkaDeserializationSchemaFactory(
       kafkaComponentsConfig,
       createSchemaIdFromMessageExtractor(schemaRegistryClient),
       schemaDataOpt,
-      isKey
+      isKey,
+      dataSampleTypingResult
     )
   }
 
