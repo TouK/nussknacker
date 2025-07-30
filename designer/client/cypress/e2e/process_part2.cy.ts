0b59364fbbaaa4420f8d4565dd830df76ffdb41d
Julian Wielga <j@touk.pl>
2025-07-30 16:10:58 +0200

    test fix

---
+++ designer/client/cypress/e2e/process_part2.cy.ts
@@ -0,0 +1,95 @@
+describe("Process", () => {
+    const seed = "process";
+
+    before(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    after(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    beforeEach(() => {
+        cy.mockWindowDate();
+    });
+
+    describe("with data", () => {
+        const screenshotOptions: Cypress.MatchImageOptions = {
+            screenshotConfig: {
+                blackout: ["[data-testid=SidePanel]"],
+            },
+        };
+
+        beforeEach(() => {
+            cy.mockWindowDate();
+            cy.visitNewProcess(seed, "testProcess");
+        });
+
+        it("should allow drag node", () => {
+            cy.get("[title='toggle left panel']").click();
+            cy.layoutScenario();
+            cy.dragNode("dynamicService", { x: 150, y: 150 });
+            cy.get("[data-testid=graphPage]").matchImage(screenshotOptions);
+        });
+
+        it("should allow drag component and drop on edge", () => {
+            cy.contains(/^custom$/)
+                .should("exist")
+                .scrollIntoView();
+            cy.layoutScenario();
+            cy.get("[data-testid='component:Customfilter']")
+                .should("be.visible")
+                .drag("#nk-graph-main", {
+                    target: {
+                        x: 580,
+                        y: 450,
+                    },
+                    force: true,
+                });
+            cy.get("[data-testid=graphPage]").matchImage(screenshotOptions);
+            //why save and test snapshot? mistake?
+            cy.verifySaveIndicator();
+            cy.contains(/^save$/i).click();
+            cy.get("[data-testid=window]").contains(/^ok$/i).click();
+            cy.get("[data-testid=window]").should("not.exist");
+            cy.get("#nk-graph-main").should("be.visible");
+            cy.get("[data-testid=graphPage]").matchImage(screenshotOptions);
+        });
+
+        it("should return 400 status code and show info about required comment", () => {
+            cy.viewport("macbook-15");
+            cy.contains(/^deploy$/i).click();
+            cy.intercept("POST", "/api/processManagement/deploy/*").as("deploy");
+            cy.contains(/^ok$/i).should("be.enabled").as("okButton");
+            cy.get("@okButton").click();
+            cy.wait("@deploy", { timeout: 20000 }).its("response.statusCode").should("eq", 400);
+            cy.contains(/^Comment is required.$/i).should("exist");
+        });
+
+        // This test is for  deploy scenario dialog snapshot comparing only (equal snapshot).
+        // For some reason cypress does not have a valid snapshot comparison inside another test case.
+        it("should make a deploy of the new version", () => {
+            cy.viewport("macbook-15");
+
+            cy.deployScenario(undefined, true);
+        });
+
+        it("should display some node details in modal", () => {
+            cy.get("[model-id=dynamicService]").should("be.visible").trigger("dblclick");
+            cy.get("[data-testid=window]").contains("Dynamicservice").should("be.visible");
+            cy.get("[data-testid=window]").should("be.visible").matchImage();
+            cy.get("[data-testid=window]")
+                .contains(/^cancel$/i)
+                .click();
+            cy.get("[model-id=boundedSource]").should("be.visible").trigger("dblclick");
+            cy.get("[data-testid=window]").contains("Boundedsource").should("be.visible");
+            cy.get("[data-testid=window]").should("be.visible").matchImage();
+            cy.get("[data-testid=window]")
+                .contains(/^cancel$/i)
+                .click();
+            cy.get("[model-id=sendSms]").should("be.visible").trigger("dblclick");
+            cy.get("[data-testid=window]").contains("Sendsms").should("be.visible");
+            cy.get("[data-testid=window]").should("be.visible").matchImage();
+        });
+    });
+});
---
+++ designer/client/cypress/e2e/process_part3.cy.ts
@@ -0,0 +1,196 @@
+describe("Process", () => {
+    const seed = "process";
+
+    before(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    after(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    beforeEach(() => {
+        cy.mockWindowDate();
+    });
+
+    it("should preserve condition on link move (switch)", () => {
+        cy.intercept("POST", "/api/*Validation/*", (req) => {
+            if (req.body.scenarioGraph.edges.length == 3) {
+                req.alias = "validation";
+            }
+        });
+        cy.visitNewProcess(seed, "switch");
+        cy.viewport(1500, 800);
+        cy.layoutScenario();
+
+        cy.getNode("switch")
+            .click()
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 16 } });
+
+        cy.contains(/^sinks$/)
+            .should("exist")
+            .scrollIntoView();
+        const x = 900;
+        const y = 630;
+        cy.get("[data-testid='component:Dead End']").should("be.visible").drag("#nk-graph-main", {
+            target: {
+                x,
+                y,
+            },
+            force: true,
+        });
+
+        cy.get(`[model-id$="false"] [end="target"].marker-arrowhead`).trigger("mousedown");
+        cy.get("#nk-graph-main")
+            .trigger("mousemove", x, y, {
+                clientX: x,
+                clientY: y,
+                moveThreshold: 5,
+            })
+            .trigger("mouseup", { force: true });
+
+        cy.wait("@validation");
+        cy.wait(500);
+
+        cy.getNode("switch")
+            .click()
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 16 } });
+
+        cy.get(`[model-id$="false"] .label`).dblclick();
+        cy.get("[data-testid=window]").should("be.visible");
+        cy.contains(/^Conditions:$/)
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 8 } });
+    });
+
+    it("should preserve condition on link move (filter)", () => {
+        cy.intercept("POST", "/api/*Validation/*", (req) => {
+            if (req.body.scenarioGraph.edges.length == 2) {
+                req.alias = "validation";
+            }
+        });
+        cy.visitNewProcess(seed, "filter");
+        cy.viewport(1500, 800);
+        cy.layoutScenario();
+
+        cy.get(`[model-id="dead-end(true)"]`).click().type("{backspace}");
+        cy.wait("@validation");
+
+        cy.getNode("filter")
+            .click()
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 16 } });
+
+        cy.contains(/^sinks$/)
+            .should("exist")
+            .scrollIntoView();
+        const x = 700;
+        const y = 600;
+        cy.get("[data-testid='component:Dead End']").should("be.visible").drag("#nk-graph-main", {
+            target: {
+                x,
+                y,
+            },
+            force: true,
+        });
+
+        cy.get(`[model-id$="false"] [end="target"].marker-arrowhead`).trigger("mousedown");
+        cy.get("#nk-graph-main")
+            .trigger("mousemove", x, y, {
+                clientX: x,
+                clientY: y,
+                moveThreshold: 5,
+            })
+            .trigger("mouseup", { force: true });
+
+        cy.wait("@validation");
+        cy.wait(500);
+
+        cy.getNode("filter")
+            .click()
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 16 } });
+    });
+
+    it("should validate process on nodes paste", () => {
+        cy.visitNewProcess(seed, "filter");
+        cy.viewport(1500, 800);
+        cy.layoutScenario();
+
+        const pasteNewNodeToScenario = () => {
+            cy.contains("svg", /filter/i).click();
+            cy.contains("button", "copy").click();
+            cy.contains("button", "paste").click();
+            cy.contains("Loose node: filter (copy 1)").should("be.visible");
+        };
+
+        const copyAndPasteWholeScenario = () => {
+            cy.realPress(["Meta", "A"]);
+            cy.contains("button", "copy").click();
+            cy.contains("button", "delete").click();
+            cy.contains("Loose node: filter (copy 1)").should("not.exist");
+            cy.contains("button", "paste").click();
+            cy.contains("Loose node: filter (copy 1)").should("be.visible");
+        };
+
+        pasteNewNodeToScenario();
+        copyAndPasteWholeScenario();
+
+        // Center diagram before the screen to have all nodes visible
+        cy.contains("button", "layout").click();
+        cy.wait(500);
+        cy.getNode("filter")
+            .click()
+            .parent()
+            .matchImage({ screenshotConfig: { padding: 16 } });
+    });
+
+    it("should zoom/restore node window with test data", () => {
+        cy.visitNewProcess(seed, "rrEmpty", "RequestResponse");
+        cy.viewport(1500, 800);
+        cy.layoutScenario();
+
+        cy.contains("button", "Test").should("be.enabled").click();
+        cy.get("[data-testid=window]").should("be.visible").find("#ace-editor").type("10");
+        cy.get("[data-testid=window]")
+            .contains(/^test$/i)
+            .should("be.enabled")
+            .click();
+        cy.getNode("request").dblclick();
+
+        cy.get("[data-testid=window]").matchImage();
+        cy.get("[data-testid=window]")
+            .should("contain.text", "Test case")
+            .then(($win) => {
+                const width = $win.width();
+                const height = $win.height();
+
+                // maximize (one way)
+                cy.wrap($win)
+                    .contains(/^source$/i)
+                    .dblclick();
+                // restore (second way)
+                cy.wrap($win).get("button[name=zoom]").click();
+
+                cy.wrap($win).should(($current) => {
+                    expect($current.width()).to.equal(width);
+                    expect($current.height()).to.equal(height);
+                });
+            });
+    });
+
+    it("should open more scenario details", () => {
+        cy.visitNewProcess(seed, "rrEmpty", "RequestResponse");
+        cy.contains(/^More details$/i).click();
+        cy.get("[data-testid=window]")
+            .should("be.visible")
+            .within(() => {
+                cy.contains(/^last modified$/i).should("be.visible");
+            })
+            .matchImage({
+                maxDiffThreshold: 0.02,
+            });
+    });
+});
