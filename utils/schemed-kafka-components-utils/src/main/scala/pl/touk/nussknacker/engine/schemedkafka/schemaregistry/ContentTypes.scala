92dd09b62141e4ae3b5e75deb956add853e2de51
Piotr Przybylski <piotrprz@gmail.com>
2025-08-04 11:27:30 +0200

    Dependency updates (#8352)

--- utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/ContentTypes.scala
+++ utils/schemed-kafka-components-utils/src/main/scala/pl/touk/nussknacker/engine/schemedkafka/schemaregistry/ContentTypes.scala
@@ -9,6 +9,8 @@ object ContentTypes extends Enumeration {
 }
 
 object ContentTypesSchemas {
-  val schemaForJson: OpenAPIJsonSchema  = OpenAPIJsonSchema("{}")
+  val schemaForJson: OpenAPIJsonSchema = OpenAPIJsonSchema("{}")
+  // note: "" deserializes to com.fasterxml.jackson.databind.node.MissingNode,
+  // which causes NPE when using Jackson 2.18.3+, because from this version on it decodes to null
   val schemaForPlain: OpenAPIJsonSchema = OpenAPIJsonSchema("")
 }
