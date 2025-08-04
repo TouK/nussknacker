68e0ca19eb584001cc71e3e06b332e26a036d8bc
Julian Wielga <j@touk.pl>
2025-08-04 23:49:07 +0200

    test check

--- designer/client/cypress/e2e/nodeAdding.cy.ts
+++ designer/client/cypress/e2e/nodeAdding.cy.ts
@@ -58,7 +58,6 @@ describe("Node adding", () => {
         cy.get("@toolbar").find("input").should("be.focused").type("sql");
         cy.contains(/event generator/i).should("not.exist");
         cy.contains(/sql source/i).should("be.visible");
-        return;
         cy.realPress("Enter");
 
         cy.get("[title='add source node']").should("not.exist");
