6385399b1c9c1e70e8a87354a93de5c826c74ce5
Julian Wielga <j@touk.pl>
2025-08-05 12:40:54 +0200

    test check

--- designer/client/cypress.config.ts
+++ designer/client/cypress.config.ts
@@ -1,5 +1,30 @@
 import { defineConfig } from "cypress";
 
+function setupWindowSize(browser: Cypress.Browser, launchOptions: Cypress.BeforeBrowserLaunchOptions) {
+    if (browser.isHeadless) {
+        const width = 1400;
+        const height = 1200;
+        if (browser.name === "chrome") {
+            launchOptions.args.push(`--window-size=${width},${height}`);
+        }
+        if (browser.name === "electron") {
+            launchOptions.preferences.width = width;
+            launchOptions.preferences.height = height;
+        }
+        if (browser.name === "firefox") {
+            launchOptions.args.push(`--width=${width}`);
+            launchOptions.args.push(`--height=${height}`);
+        }
+    } else {
+        if (browser.name === "electron") {
+            launchOptions.preferences.fullscreen = true;
+        } else if (browser.family === "chromium") {
+            launchOptions.args.push("--start-fullscreen");
+        }
+    }
+    return launchOptions;
+}
+
 export default defineConfig({
     env: {
         updateSnapshotsOnFail: false,
@@ -14,37 +39,17 @@ export default defineConfig({
     },
     defaultCommandTimeout: 30000,
     e2e: {
-        video: true,
         experimentalMemoryManagement: true,
         experimentalRunAllSpecs: true,
         numTestsKeptInMemory: 10,
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
-        setupNodeEvents(on, config) {
+        setupNodeEvents: (on, config) => {
             on("before:browser:launch", (browser, launchOptions) => {
-                const width = 1400;
-                const height = 1200;
-                if (browser.isHeadless) {
-                    if (browser.name === "chrome") {
-                        launchOptions.args.push(`--window-size=${width},${height}`);
-                    }
-                    if (browser.name === "electron") {
-                        launchOptions.preferences.width = width;
-                        launchOptions.preferences.height = height;
-                    }
-                    if (browser.name === "firefox") {
-                        launchOptions.args.push(`--width=${width}`);
-                        launchOptions.args.push(`--height=${height}`);
-                    }
-                } else {
-                    if (browser.name === "electron") {
-                        launchOptions.preferences.fullscreen = true;
-                    } else if (browser.family === "chromium") {
-                        launchOptions.args.push("--start-fullscreen");
-                    }
-                }
-
-                return launchOptions;
+                setupWindowSize(browser, launchOptions);
+            });
+            on("before:browser:launch", (browser) => {
+                config.video = browser.isHeadless;
             });
             return require("./cypress/plugins/index.js")(on, config);
         },
