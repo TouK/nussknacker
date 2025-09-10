8f89ca8a42868d4b00f0d41238a29fa06f12cb50
Julian Wielga <j@touk.pl>
2025-09-10 03:00:02 +0200

    test fix

--- designer/client/cypress/e2e/stickyNotes.cy.ts
+++ designer/client/cypress/e2e/stickyNotes.cy.ts
@@ -23,7 +23,7 @@ describe("Sticky notes", () => {
             .drag("#nk-graph-main", {
                 target: {
                     x: 600,
-                    y: 300,
+                    y: 400,
                 },
                 force: true,
             });
