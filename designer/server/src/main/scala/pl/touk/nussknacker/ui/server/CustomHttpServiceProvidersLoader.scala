4a03aa81ba3e76ae9c835562cc5cd868cf6ff834
Rafał Solarski <solarski.rafal@gmail.com>
2025-09-17 14:45:29 +0200

    DefinitionService facade exposed to custom http service api - wip

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/server/CustomHttpServiceProvidersLoader.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/server/CustomHttpServiceProvidersLoader.scala
@@ -3,8 +3,10 @@ package pl.touk.nussknacker.ui.server
 import cats.effect.IO
 import cats.effect.kernel.Resource
 import cats.implicits.toTraverseOps
+import pl.touk.nussknacker.engine.api.process.ProcessingType
 import pl.touk.nussknacker.engine.util.loader.ScalaServiceLoader
 import pl.touk.nussknacker.engine.util.multiplicity.{Empty, Many, Multiplicity, One}
+import pl.touk.nussknacker.restmodel.definition
 import pl.touk.nussknacker.ui.config.DesignerConfig
 import pl.touk.nussknacker.ui.customhttpservice.{
   CustomHttpServiceProviderFactory,
@@ -12,10 +14,19 @@ import pl.touk.nussknacker.ui.customhttpservice.{
   ProcessServiceBasedScenarioServiceAdapter,
   TapirCustomHttpServiceProvider
 }
-import pl.touk.nussknacker.ui.customhttpservice.services.NussknackerServicesForCustomHttpService
+import pl.touk.nussknacker.ui.customhttpservice.services.{
+  DefinitionsServiceForHttpService,
+  NussknackerServicesForCustomHttpService,
+  ProcessingTypeServicesProvider
+}
+import pl.touk.nussknacker.ui.definition.DefinitionsService
+import pl.touk.nussknacker.ui.definition.DefinitionsService.ComponentUiConfigMode.{BasicConfig, EnrichedWithUiConfig}
 import pl.touk.nussknacker.ui.factory.{DomainServices, InfrastructureServices}
+import pl.touk.nussknacker.ui.process.processingtype.{CombinedProcessingTypeData, ProcessingTypeServices}
+import pl.touk.nussknacker.ui.process.processingtype.provider.ProcessingTypeDataProvider
+import pl.touk.nussknacker.ui.security.api.LoggedUser
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object CustomHttpServiceProvidersLoader {
 
@@ -62,7 +73,8 @@ object CustomHttpServiceProvidersLoader {
   )(implicit executionContext: ExecutionContext): Resource[IO, CustomHttpServiceProviders] = {
     lazy val nussknackerServices = new NussknackerServicesForCustomHttpService(
       new ProcessServiceBasedScenarioServiceAdapter(domainServices.processService),
-      infrastructureServices.dbRef
+      infrastructureServices.dbRef,
+      new ProcessingTypeServicesProviderImpl(domainServices.processingTypeServicesProvider)
     )
     customHttpServiceProviderFactories
       .traverse { factory => factory.create(designerConfig.rawConfig, nussknackerServices).map(factory.name -> _) }
@@ -80,5 +92,34 @@ object CustomHttpServiceProvidersLoader {
       pekko: Map[String, PekkoCustomHttpServiceProvider],
       tapir: Map[String, TapirCustomHttpServiceProvider]
   )
+
+  private class ProcessingTypeServicesProviderImpl(
+      processingTypeServicesProvider: ProcessingTypeDataProvider[ProcessingTypeServices, CombinedProcessingTypeData]
+  ) extends ProcessingTypeServicesProvider {
+
+    // todo: rethink what can be memoized here
+    override def definitionService: DefinitionsServiceForHttpService = new DefinitionsServiceForHttpService {
+      override def prepareUIDefinitions(
+          processingType: ProcessingType,
+          forFragment: Boolean,
+          componentUiConfigMode: DefinitionsServiceForHttpService.ExternalComponentUiConfigMode
+      )(implicit user: LoggedUser): Future[definition.UIDefinitions] = {
+        val mappedToInternalConfigMode: DefinitionsService.ComponentUiConfigMode = componentUiConfigMode match {
+          case DefinitionsServiceForHttpService.ExternalComponentUiConfigMode.EnrichedWithUiConfig =>
+            EnrichedWithUiConfig
+          case DefinitionsServiceForHttpService.ExternalComponentUiConfigMode.BasicConfig => BasicConfig
+        }
+        processingTypeServicesProvider
+          .forProcessingTypeUnsafe(processingType)
+          .definitionService
+          .prepareUIDefinitions(
+            processingType,
+            forFragment,
+            mappedToInternalConfigMode
+          )
+      }
+    }
+
+  }
 
 }
