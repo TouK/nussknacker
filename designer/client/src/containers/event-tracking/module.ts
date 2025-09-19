051b1f6acc3698d515f19c846c212087f52fd0a6
Julian Wielga <j@touk.pl>
2025-09-19 16:48:49 +0200

    fix

--- designer/client/src/containers/event-tracking/module.ts
+++ designer/client/src/containers/event-tracking/module.ts
@@ -1,7 +1,5 @@
 // check federation.config.json
 
-export { useEventTracking } from "./use-event-tracking";
-export type { TrackEventParams } from "./use-event-tracking";
-export { mapToolbarButtonToStatisticsEvent, getEventTrackingProps, getEventStatisticName } from "./helpers";
-export { useRegisterTrackingEvents, EventTrackingSelector } from "./use-register-tracking-events";
-export type { EventTrackingSelectorType } from "./use-register-tracking-events";
+export * from "./use-event-tracking";
+export * from "./helpers";
+export * from "./use-register-tracking-events";
