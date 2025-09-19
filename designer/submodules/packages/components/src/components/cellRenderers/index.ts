2bb0629f0088350acc98fe01bd17fe191b3f64e3
Julian Wielga <j@touk.pl>
2025-09-19 10:14:41 +0200

    removed barrel files [submodules]

--- designer/submodules/packages/components/src/components/cellRenderers/index.ts
+++
@@ -1,5 +0,0 @@
-export { CategoriesCell } from "./categoriesCell";
-export { CellLink } from "./cellLink";
-export { FilterLinkCell } from "./filterLinkCell";
-export { NameCell } from "./nameCell";
-export { UsageCountCell } from "./usageCountCell";
