2bac5b66a76db7ab40f3dcd8e6e66220e25f9c74
Julian Wielga <j@touk.pl>
2025-09-26 22:38:11 +0200

    update PendingPromise

--- designer/client/src/init.js
+++ designer/client/src/init.js
@@ -1,4 +1,8 @@
 //config *has* to be loaded before other imports, see Warning at https://webpack.js.org/guides/public-path/#on-the-fly
 import "./config";
 import "./styles";
+import { PendingPromise } from "./common/PendingPromise";
+
+window["PendingPromise"] = PendingPromise;
+
 import("./bootstrap");
