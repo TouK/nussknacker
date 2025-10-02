294eb31eb409dd7bf7cfbe197e27989110894648
Julian Wielga <j@touk.pl>
2025-10-02 12:22:37 +0200

    filters responsive design

--- designer/submodules/packages/components/src/scenarios/filters/filtersPart.tsx
+++ designer/submodules/packages/components/src/scenarios/filters/filtersPart.tsx
@@ -1,4 +1,3 @@
-import { Divider, Stack } from "@mui/material";
 import { flatten, sortBy, uniq } from "lodash";
 import { EventTrackingSelector, getEventTrackingProps } from "nussknackerUi/eventTracking";
 import React, { useCallback, useMemo } from "react";
@@ -13,11 +12,10 @@ import { FilterMenu } from "./filterMenu";
 import { ProcessingModeStack } from "./processingModeStack";
 import { QuickFilter } from "./quickFilter";
 import type { ScenariosFiltersModel } from "./scenariosFiltersModel";
-import { SCENARIOS_FILTER } from "./scenariosFiltersModel";
-import { ScenariosFiltersModelType } from "./scenariosFiltersModel";
+import { SCENARIOS_FILTER, ScenariosFiltersModelType } from "./scenariosFiltersModel";
 import { SimpleOptionsStack } from "./simpleOptionsStack";
 import { SortOptionsStack } from "./sortOptionsStack";
-import { TypeOptionsStack, StatusOptionsStack } from "./typeOptionsStack";
+import { StatusOptionsStack, TypeOptionsStack } from "./typeOptionsStack";
 
 export function FiltersPart({ withSort, isLoading, data = [] }: { data: RowType[]; isLoading?: boolean; withSort?: boolean }): JSX.Element {
     const { t } = useTranslation();
@@ -82,85 +80,80 @@ export function FiltersPart({ withSort, isLoading, data = [] }: { data: RowType[
                 filter="NAME"
                 {...getEventTrackingProps({ selector: EventTrackingSelector.ScenariosByName })}
             >
-                <Stack direction="row" spacing={1} p={1} alignItems="center" divider={<Divider orientation="vertical" flexItem />}>
-                    <FilterMenu label={t("table.filter.STATUS", "Status")} count={getFilter(SCENARIOS_FILTER.STATUS, true).length}>
-                        <StatusOptionsStack
-                            options={filterableValues.status}
-                            withArchived={true}
-                            {...getEventTrackingProps({
-                                selector: EventTrackingSelector.ScenariosByStatus,
-                            })}
-                        />
-                    </FilterMenu>
-                    <FilterMenu
+                <FilterMenu label={t("table.filter.STATUS", "Status")} count={getFilter(SCENARIOS_FILTER.STATUS, true).length}>
+                    <StatusOptionsStack
+                        options={filterableValues.status}
+                        withArchived={true}
+                        {...getEventTrackingProps({
+                            selector: EventTrackingSelector.ScenariosByStatus,
+                        })}
+                    />
+                </FilterMenu>
+                <FilterMenu
+                    label={t("table.filter.PROCESSING_MODE", "PROCESSING MODE")}
+                    count={getFilter(SCENARIOS_FILTER.PROCESSING_MODE, true).length}
+                >
+                    <ProcessingModeStack
                         label={t("table.filter.PROCESSING_MODE", "PROCESSING MODE")}
-                        count={getFilter(SCENARIOS_FILTER.PROCESSING_MODE, true).length}
-                    >
-                        <ProcessingModeStack
-                            label={t("table.filter.PROCESSING_MODE", "PROCESSING MODE")}
-                            options={filterableValues.processingMode}
-                            value={getFilter(SCENARIOS_FILTER.PROCESSING_MODE, true)}
-                            onChange={setFilter(SCENARIOS_FILTER.PROCESSING_MODE)}
-                            {...getEventTrackingProps({
-                                selector: EventTrackingSelector.ScenariosByProcessingMode,
-                            })}
-                        />
-                    </FilterMenu>
-                    {withCategoriesVisible && (
-                        <FilterMenu
+                        options={filterableValues.processingMode}
+                        value={getFilter(SCENARIOS_FILTER.PROCESSING_MODE, true)}
+                        onChange={setFilter(SCENARIOS_FILTER.PROCESSING_MODE)}
+                        {...getEventTrackingProps({
+                            selector: EventTrackingSelector.ScenariosByProcessingMode,
+                        })}
+                    />
+                </FilterMenu>
+                {withCategoriesVisible && (
+                    <FilterMenu label={t("table.filter.CATEGORY", "Category")} count={getFilter(SCENARIOS_FILTER.CATEGORY, true).length}>
+                        <SimpleOptionsStack
                             label={t("table.filter.CATEGORY", "Category")}
-                            count={getFilter(SCENARIOS_FILTER.CATEGORY, true).length}
-                        >
-                            <SimpleOptionsStack
-                                label={t("table.filter.CATEGORY", "Category")}
-                                options={filterableValues.processCategory}
-                                value={getFilter(SCENARIOS_FILTER.CATEGORY, true)}
-                                onChange={setFilter(SCENARIOS_FILTER.CATEGORY)}
-                                {...getEventTrackingProps({
-                                    selector: EventTrackingSelector.ScenariosByCategory,
-                                })}
-                            />
-                        </FilterMenu>
-                    )}
-                    <FilterMenu label={t("table.filter.LABEL", "Label")} count={getFilter(SCENARIOS_FILTER.LABEL, true).length}>
-                        <SimpleOptionsStack
-                            label={t("table.filter.LABEL", "Label")}
-                            options={filterableValues.label}
-                            value={getFilter(SCENARIOS_FILTER.LABEL, true)}
-                            onChange={setFilter(SCENARIOS_FILTER.LABEL)}
+                            options={filterableValues.processCategory}
+                            value={getFilter(SCENARIOS_FILTER.CATEGORY, true)}
+                            onChange={setFilter(SCENARIOS_FILTER.CATEGORY)}
                             {...getEventTrackingProps({
-                                selector: EventTrackingSelector.ScenariosByLabel,
+                                selector: EventTrackingSelector.ScenariosByCategory,
                             })}
                         />
                     </FilterMenu>
-                    <FilterMenu label={t("table.filter.CREATED_BY", "Author")} count={getFilter(SCENARIOS_FILTER.CREATED_BY, true).length}>
-                        <SimpleOptionsStack
-                            label={t("table.filter.CREATED_BY", "Author")}
-                            options={filterableValues.author}
-                            value={getFilter(SCENARIOS_FILTER.CREATED_BY, true)}
-                            onChange={setFilter(SCENARIOS_FILTER.CREATED_BY)}
+                )}
+                <FilterMenu label={t("table.filter.LABEL", "Label")} count={getFilter(SCENARIOS_FILTER.LABEL, true).length}>
+                    <SimpleOptionsStack
+                        label={t("table.filter.LABEL", "Label")}
+                        options={filterableValues.label}
+                        value={getFilter(SCENARIOS_FILTER.LABEL, true)}
+                        onChange={setFilter(SCENARIOS_FILTER.LABEL)}
+                        {...getEventTrackingProps({
+                            selector: EventTrackingSelector.ScenariosByLabel,
+                        })}
+                    />
+                </FilterMenu>
+                <FilterMenu label={t("table.filter.CREATED_BY", "Author")} count={getFilter(SCENARIOS_FILTER.CREATED_BY, true).length}>
+                    <SimpleOptionsStack
+                        label={t("table.filter.CREATED_BY", "Author")}
+                        options={filterableValues.author}
+                        value={getFilter(SCENARIOS_FILTER.CREATED_BY, true)}
+                        onChange={setFilter(SCENARIOS_FILTER.CREATED_BY)}
+                        {...getEventTrackingProps({
+                            selector: EventTrackingSelector.ScenariosByAuthor,
+                        })}
+                    />
+                </FilterMenu>
+                <FilterMenu label={t("table.filter.TYPE", "Type")} count={getFilter(SCENARIOS_FILTER.TYPE, true).length}>
+                    <TypeOptionsStack />
+                </FilterMenu>
+                {withSort ? (
+                    <FilterMenu label={t("table.filter.SORT_BY", "Sort")}>
+                        <SortOptionsStack
+                            label={t("table.filter.SORT_BY", "Sort")}
+                            options={["createdAt", "modificationDate", "name"].map((name) => ({ name }))}
+                            value={getFilter(SCENARIOS_FILTER.SORT_BY, true)}
+                            onChange={setFilter(SCENARIOS_FILTER.SORT_BY)}
                             {...getEventTrackingProps({
-                                selector: EventTrackingSelector.ScenariosByAuthor,
+                                selector: EventTrackingSelector.ScenariosBySortOption,
                             })}
                         />
                     </FilterMenu>
-                    <FilterMenu label={t("table.filter.TYPE", "Type")} count={getFilter(SCENARIOS_FILTER.TYPE, true).length}>
-                        <TypeOptionsStack />
-                    </FilterMenu>
-                    {withSort ? (
-                        <FilterMenu label={t("table.filter.SORT_BY", "Sort")}>
-                            <SortOptionsStack
-                                label={t("table.filter.SORT_BY", "Sort")}
-                                options={["createdAt", "modificationDate", "name"].map((name) => ({ name }))}
-                                value={getFilter(SCENARIOS_FILTER.SORT_BY, true)}
-                                onChange={setFilter(SCENARIOS_FILTER.SORT_BY)}
-                                {...getEventTrackingProps({
-                                    selector: EventTrackingSelector.ScenariosBySortOption,
-                                })}
-                            />
-                        </FilterMenu>
-                    ) : null}
-                </Stack>
+                ) : null}
             </QuickFilter>
             <ActiveFilters
                 getLabel={getLabel}
