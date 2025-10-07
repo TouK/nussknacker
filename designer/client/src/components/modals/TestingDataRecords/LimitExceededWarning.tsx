84fd7ef9f1aa903f18563c884bc98852e7c60763
Dawid Poliszak <dpl@touk.pl>
2025-10-07 11:48:09 +0200

    [NU-2363] better showing records exceeded limit (#8660)
    * NU-2363 better showing records exceeded limit
    
    * NU-2363 better showing record exceeded limit info
    
    * NU-2363 append from live data validation improved
    
    * NU-2363 review comment resolve
    
    * NU-2363 fix test

---
+++ designer/client/src/components/modals/TestingDataRecords/LimitExceededWarning.tsx
@@ -0,0 +1,31 @@
+import { Alert } from "@mui/material";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+interface Props {
+    maxTestingRecords: number;
+}
+export const LimitExceededWarning = ({ maxTestingRecords }: Props) => {
+    const { t } = useTranslation();
+    const [warningVisible, setWarningVisible] = useState<boolean>(true);
+
+    if (!warningVisible) {
+        return null;
+    }
+
+    return (
+        <Alert
+            sx={{ width: "100%" }}
+            severity={"warning"}
+            onClose={() => {
+                setWarningVisible(false);
+            }}
+        >
+            {t(
+                "testingDialog.warning.dataRecordsLimitExceeded",
+                "The maximum number of {{maxTestingRecords}} Input data records has been exceeded.",
+                { maxTestingRecords },
+            )}
+        </Alert>
+    );
+};
