31243e6180492640fa2f4f08b58df6d9067b182c
Julian Wielga <j@touk.pl>
2025-10-07 14:01:29 +0200

    icons fix

--- designer/client/src/components/table/SearchFilter.tsx
+++ designer/client/src/components/table/SearchFilter.tsx
@@ -1,76 +1,52 @@
-import { css, cx } from "@emotion/css";
-import { useTheme } from "@mui/material";
+import { styled } from "@mui/material";
 import React from "react";
 
 import AdvancedSearchSvg from "../../assets/img/advanced-search.svg";
 import SearchSvg from "../../assets/img/search.svg";
 import DeleteSvg from "../../assets/img/toolbarButtons/delete.svg";
 
-const flex = css({
+const flex = {
     width: 0, // edge 18. why? because! 🙃
     flex: 1,
-});
+};
 
-export function AdvancedOptionsIcon(props: {
+const AdvancedSearchIcon = styled(AdvancedSearchSvg)<{ isActive?: boolean }>(({ theme, isActive }) => ({
+    ...flex,
+    cursor: "pointer",
+    transform: "scale(1.5)",
+    path: {
+        fill: "none",
+        stroke: isActive ? theme.palette.primary.main : theme.palette.text.secondary,
+    },
+}));
+
+export const AdvancedOptionsIcon = ({
+    collapseHandler,
+    isActive,
+}: {
     isActive?: boolean;
     collapseHandler: React.Dispatch<React.SetStateAction<boolean>>;
-}): JSX.Element {
-    const theme = useTheme();
+}) => (
+    <AdvancedSearchIcon
+        id="advanced-search-icon"
+        onClick={() => {
+            collapseHandler((p) => !p);
+        }}
+        isActive={isActive}
+    />
+);
 
-    const toggleCollapseHandler = () => {
-        props.collapseHandler((p) => !p);
-    };
+export const SearchIcon = styled(SearchSvg)<{ isEmpty?: boolean }>(({ isEmpty, theme }) => ({
+    ...flex,
+    transform: "scale(0.8)",
+    path: {
+        fill: isEmpty ? theme.palette.text.secondary : theme.palette.primary.main,
+    },
+}));
 
-    return (
-        <AdvancedSearchSvg
-            id="advanced-search-icon"
-            onClick={toggleCollapseHandler}
-            className={cx(
-                flex,
-                css({
-                    cursor: "pointer",
-                    transform: "scale(1.5)",
-                    ".icon-fill": {
-                        fill: "none",
-                        stroke: !props.isActive ? theme.palette.text.secondary : theme.palette.primary.main,
-                    },
-                }),
-            )}
-        />
-    );
-}
-
-export function SearchIcon(props: { isEmpty?: boolean }): JSX.Element {
-    const theme = useTheme();
-    return (
-        <SearchSvg
-            className={cx(
-                flex,
-                css({
-                    transform: "scale(0.8)",
-                    ".icon-fill": {
-                        fill: props.isEmpty ? theme.palette.text.secondary : theme.palette.primary.main,
-                    },
-                }),
-            )}
-        />
-    );
-}
-
-export function ClearIcon(): JSX.Element {
-    const theme = useTheme();
-
-    return (
-        <DeleteSvg
-            id="clear-icon"
-            className={cx(
-                flex,
-                css({
-                    path: {
-                        fill: theme.palette.text.secondary,
-                    },
-                }),
-            )}
-        />
-    );
-}
+export const ClearIcon = styled(DeleteSvg)(({ theme }) => ({
+    ...flex,
+    path: {
+        fill: theme.palette.text.secondary,
+    },
+}));
