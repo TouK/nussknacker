bf2bfc47a3d155aaef8c991ec73f972b8bb95b6a
Julian Wielga <j@touk.pl>
2025-10-17 22:26:26 +0200

    topics tab override

--- designer/client/src/containers/paths.ts
+++ designer/client/src/containers/paths.ts
@@ -2,4 +2,5 @@ export const RootPath = `/`;
 export const VisualizationBasePath = `/visualization`;
 export const MetricsBasePath = `/metrics`;
 export const ScenariosBasePath = `/scenarios`;
+export const TopicsBasePath = `/topics`;
 export const ArchivedPath = `${ScenariosBasePath}/?ARCHIVED=true`;
