88ffc2335cf9194f80ff405dd3aa9ce4ad59e7aa
Julian Wielga <j@touk.pl>
2025-10-25 16:03:07 +0200

    tests fix

--- designer/client/test/Editors/TimeEditor-test.tsx
+++ designer/client/test/Editors/TimeEditor-test.tsx
@@ -4,8 +4,8 @@ import { render, screen } from "@testing-library/react";
 import { jest } from "@jest/globals";
 import { TimeEditor } from "../../src/components/graph/node-modal/editors/expression/DateTimeEditor/TimeEditor";
 import { mockFieldErrors, mockFormatter, mockValueChange } from "./helpers";
-import { NuThemeProvider } from "../../src/containers/theme/nuThemeProvider";
 import { nodeInputWithError } from "../../src/components/graph/node-modal/NodeDetailsContent/NodeTableStyled";
+import { TestProviders } from "./TestProviders";
 
 jest.mock("react-i18next", () => ({
     useTranslation: () => ({
@@ -17,7 +17,7 @@ jest.mock("react-i18next", () => ({
 describe("TimeEditor.name", () => {
     it("should display validation error when the field is required", () => {
         render(
-            <NuThemeProvider>
+            <TestProviders>
                 <TimeEditor
                     momentFormat={"YYYY-MM-DD"}
                     readOnly={false}
@@ -30,7 +30,7 @@ describe("TimeEditor.name", () => {
                     formatter={mockFormatter}
                     showValidation={true}
                 />
-            </NuThemeProvider>,
+            </TestProviders>,
         );
 
         expect(screen.getByRole("textbox")).toHaveClass(nodeInputWithError);
