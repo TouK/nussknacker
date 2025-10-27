8c6a232a401810ecf282d04ac5b9c237ef8071c4
Julian Wielga <j@touk.pl>
2025-10-27 14:00:36 +0100

    fixed selector warning

--- designer/client/src/components/graph/node-modal/NodeDetailsContent/getNodeDetails.tsx
+++ designer/client/src/components/graph/node-modal/NodeDetailsContent/getNodeDetails.tsx
@@ -5,5 +5,5 @@ import type { RootState } from "../../../../reducers";
 
 const createDeepEqualSelector = createSelectorCreator(defaultMemoize, isEqual);
 
-const getNodesDetails = (state: RootState) => state.nodeDetails;
+export const getNodesDetails = (state: RootState) => state.nodeDetails;
 export const getNodeDetails = createDeepEqualSelector(getNodesDetails, (nodeDetails) => (nodeId: string) => nodeDetails[nodeId]);
--- designer/client/src/components/graph/node-modal/parametersList.tsx
+++ designer/client/src/components/graph/node-modal/parametersList.tsx
@@ -1,11 +1,11 @@
 import { Box, Skeleton } from "@mui/material";
-import { isEqual } from "lodash";
-import React, { Fragment } from "react";
+import React from "react";
 
 import { useAppSelector } from "../../../store/storeHelpers";
 import type { Parameter } from "../../../types/node";
-import { getDynamicParametersChanged } from "./NodeDetailsContent/selectors";
+import { getIsParameterStable } from "./NodeDetailsContent/selectors";
 import type { ParameterExpressionFieldProps } from "./ParameterExpressionField";
+import type { ParametersListFieldProps } from "./parametersListField";
 import { ParametersListField } from "./parametersListField";
 
 type ParametersListItemProps = Omit<ParameterExpressionFieldProps, "listFieldPath" | "parameter">;
@@ -20,25 +20,32 @@ export type ParametersListProps = ParametersListItemProps & {
     getListFieldPath: (index: number) => string;
 };
 
+function ListElement(props: ParametersListFieldProps) {
+    const { node, paramWithIndex } = props;
+    const isParameterStable = useAppSelector((state) => {
+        return getIsParameterStable(state, { node, param: paramWithIndex.param });
+    });
+
+    if (isParameterStable) {
+        return <ParametersListField {...props} paramWithIndex={paramWithIndex} />;
+    }
+
+    return (
+        <Box display={"flex"} justifyContent={"space-between"} mt={2} fontSize={14}>
+            <Box height={15} sx={{ flexBasis: "20%", mt: "9px", maxWidth: "20em" }}>
+                <Skeleton variant="rectangular" sx={{ maxWidth: "75px" }} />
+            </Box>
+            <Skeleton variant="rectangular" height={35} width={"100%"} sx={{ flexBasis: "60%", flex: 1 }} />
+        </Box>
+    );
+}
+
 export const ParametersList = (props: ParametersListProps) => {
     const { parameters = [], node } = props;
-    const dynamicParametersChanged = useAppSelector(getDynamicParametersChanged, isEqual)(node.id);
-
     return (
         <>
             {parameters.map((paramWithIndex) => (
-                <Fragment key={node.id + paramWithIndex.param.name + paramWithIndex.index}>
-                    {dynamicParametersChanged?.length > 0 && !dynamicParametersChanged.includes(paramWithIndex.param.name) ? (
-                        <Box display={"flex"} justifyContent={"space-between"} mt={2} fontSize={14}>
-                            <Box height={15} sx={{ flexBasis: "20%", mt: "9px", maxWidth: "20em" }}>
-                                <Skeleton variant="rectangular" sx={{ maxWidth: "75px" }} />
-                            </Box>
-                            <Skeleton variant="rectangular" height={35} width={"100%"} sx={{ flexBasis: "60%", flex: 1 }} />
-                        </Box>
-                    ) : (
-                        <ParametersListField {...props} paramWithIndex={paramWithIndex} />
-                    )}
-                </Fragment>
+                <ListElement key={node.id + paramWithIndex.param.name + paramWithIndex.index} paramWithIndex={paramWithIndex} {...props} />
             ))}
         </>
     );
