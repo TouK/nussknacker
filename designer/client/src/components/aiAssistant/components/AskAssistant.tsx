6931f33701f66a59ddcfd82f26a20007215184f8
Julian Wielga <j@touk.pl>
2025-11-08 13:50:17 +0100

    assistant question fix and tweak for fragments

--- designer/client/src/components/aiAssistant/components/AskAssistant.tsx
+++ designer/client/src/components/aiAssistant/components/AskAssistant.tsx
@@ -1,10 +1,11 @@
+import { useThread } from "@assistant-ui/react";
 import { Button } from "@mui/material";
 import type { PropsWithChildren } from "react";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { assistantAsk } from "../../../actions/assistantActions";
-import { getScenarioGraph } from "../../../reducers/selectors/graph";
+import { getScenarioGraph, isFragment } from "../../../reducers/selectors/graph";
 import { getProcessState } from "../../../reducers/selectors/scenarioState";
 import { getUserSettings } from "../../../reducers/selectors/userSettings";
 import { useAppDispatch, useAppSelector } from "../../../store/storeHelpers";
@@ -13,24 +14,46 @@ export type AskAssistantProps = PropsWithChildren<{ question: string; realPrompt
 
 function AskAssistant({ question, realPrompt = question, children }: AskAssistantProps) {
     const { t } = useTranslation();
+    const thread = useThread();
     const dispatch = useAppDispatch();
     const graph = useAppSelector(getScenarioGraph);
-    const { status } = useAppSelector(getProcessState);
+    const state = useAppSelector(getProcessState);
+    const fragment = useAppSelector(isFragment);
     const settings = useAppSelector(getUserSettings);
 
+    const contextData = useMemo(
+        () => JSON.stringify(fragment ? { scenario: graph, isFragment: true } : { scenario: graph, status: state?.status }),
+        [fragment, graph, state?.status],
+    );
+
+    const [askedOnce, setAskedOnce] = useState(false);
+    useEffect(() => {
+        if (thread.messages.length < 1) {
+            setAskedOnce(false);
+        }
+    }, [thread.messages.length]);
+
+    const contextSetup = useMemo(() => {
+        const context = askedOnce
+            ? [`This time I have this raw internal data: ${contextData}.`]
+            : [
+                  `Here you have some more raw internal data: ${contextData}.`,
+                  `Remember that I can't fully see that data. I only edit some of it directly, while other parts are displayed differently.`,
+                  `This data is provided so you can better understand the context, all my expressions, nodes and connections should be here.`,
+                  `I mainly edit expressions in leaf values that are sometimes wrapped in a more user-friendly layer.`,
+                  `Please answer briefly, without unnecessary repetitions.`,
+              ];
+        return context.join("\n");
+    }, [askedOnce, contextData]);
+
     const onClick = useCallback(() => {
-        dispatch(
-            assistantAsk(
-                question,
-                realPrompt && settings["assistant.includeScenarioData"]
-                    ? `${realPrompt}. (Here you have some more raw internal data: ${JSON.stringify({
-                          graph,
-                          status,
-                      })}. Remember that I can't fully see that data. I only edit some of it directly, while other parts are displayed differently. This data is provided so you can better understand the context, all my expressions, nodes and connections should be here. I mainly edit expressions that are sometimes wrapped in a more user-friendly layer.)`
-                    : realPrompt,
-            ),
-        );
-    }, [dispatch, question, settings, realPrompt, graph, status]);
+        let prompt = realPrompt;
+        if (prompt && settings["assistant.includeScenarioData"]) {
+            prompt = `${realPrompt}. (${contextSetup})`;
+        }
+        dispatch(assistantAsk(question, prompt));
+        setAskedOnce(true);
+    }, [dispatch, question, realPrompt, settings, contextSetup]);
 
     return (
         <Button
