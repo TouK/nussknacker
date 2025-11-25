2bba1f9455736ae4d63fab1fc5f7d15e916d52c8
Filip Michalski <fmc@touk.pl>
2025-11-25 13:56:28 +0100

    Move sth to trigger pipelines

--- utils/default-helpers/src/test/scala/pl/touk/nussknacker/engine/util/functions/GeoUtilsSpec.scala
+++ utils/default-helpers/src/test/scala/pl/touk/nussknacker/engine/util/functions/GeoUtilsSpec.scala
@@ -12,8 +12,8 @@ class GeoUtilsSpec extends AnyFunSuite with Matchers {
   test("calculates distance between the White House and the Eiffel Tower") {
     val whiteHouse  = (38.898, -77.037)
     val eiffelTower = (48.858, 2.294)
-
     val distanceOnWGS84Ellipsoid = 6177.45
+    
     // we assume up to 0.5% error
     geo.distanceInKm(whiteHouse._1, whiteHouse._2, eiffelTower._1, eiffelTower._2) should ===(
       distanceOnWGS84Ellipsoid +- distanceOnWGS84Ellipsoid * 0.005
