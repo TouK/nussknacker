700f69af91afb057f5b56a84856ab1bb0a2da805
Julian Wielga <j@touk.pl>
2025-11-27 20:19:32 +0100

    various type related fixes

--- designer/client/src/reducers/graph/utils.test.ts
+++ designer/client/src/reducers/graph/utils.test.ts
@@ -1,9 +1,8 @@
 import { prepareNewNodesWithLayout } from "./utils";
-import { nodesWithPositions, state } from "./utils.fixtures";
+import { scenarioGraph, nodesWithPositions } from "./utils.fixtures";
 
 describe("GraphUtils", () => {
     it("prepareNewNodesWithLayout should update union output expression parameter with an updated node name when new unique node ids created", () => {
-        const { scenarioGraph } = state.scenario;
         expect(prepareNewNodesWithLayout(scenarioGraph.nodes, nodesWithPositions, true)).toMatchSnapshot();
     });
 });
