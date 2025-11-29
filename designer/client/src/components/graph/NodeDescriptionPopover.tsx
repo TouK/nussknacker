2c5b3f88208833473b5e75ab99795c670ed44178
Julian Wielga <j@touk.pl>
2025-11-29 02:03:28 +0100

    test fix

--- designer/client/src/components/graph/NodeDescriptionPopover.tsx
+++ designer/client/src/components/graph/NodeDescriptionPopover.tsx
@@ -13,6 +13,34 @@ import { isStickyNoteElement } from "./GraphPartialsInTS/cellUtils";
 import { MarkdownStyled } from "./node-modal/MarkdownStyled";
 import { Events } from "./types";
 
+const watchForCover = (getElement: () => Element, callback: (covered: boolean) => void) => {
+    let lastState = false;
+
+    const observer = new MutationObserver(() => {
+        const element = getElement();
+        if (!element) return;
+
+        const rect = element.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const topElement = document.elementFromPoint(centerX, centerY);
+        const isCovered = topElement !== null && topElement !== element && !element.contains(topElement);
+
+        if (isCovered !== lastState) {
+            callback(isCovered);
+            lastState = isCovered;
+        }
+    });
+
+    observer.observe(document.body, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+    });
+
+    return () => observer.disconnect();
+};
+
 const useTimeout = <A extends Array<unknown>>(
     callback: (...args: A) => void,
     time: number,
@@ -162,6 +190,14 @@ export function NodeDescriptionPopover(props: NodeDescriptionPopoverProps) {
         innerSelector: "[joint-selector=testResultsGroup]",
         outerSelector: "[model-id]",
     });
+
+    useEffect(() => {
+        if (!open) return;
+        return watchForCover(
+            () => lastTarget.current,
+            () => leaveTimer.start(),
+        );
+    }, [leaveTimer, open]);
 
     useEffect(() => {
         const graph = graphRef.current;
