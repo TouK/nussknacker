61880b54f0eb9ff8e4ff4a31d5e214cd7f81be8e
Julian Wielga <j@touk.pl>
2025-12-02 19:18:56 +0100

    chip values list update

--- designer/client/src/components/graph/node-modal/aggregate/groupBy/valuesList.tsx
+++ designer/client/src/components/graph/node-modal/aggregate/groupBy/valuesList.tsx
@@ -1,19 +1,34 @@
-import { Stack } from "@mui/material";
+import { Chip, Stack } from "@mui/material";
+import type { StackProps } from "@mui/material/Stack/Stack";
 import type { UIEvent } from "react";
 import React, { useCallback } from "react";
 
 import { getTabindexedElements } from "../../editors/expression/AceWrapper";
-import { SpelChip } from "./spelChip";
-
-export type LastOf<T extends [...unknown[]]> = T extends [...unknown[], infer L] ? L : never;
 
 type ValuesListProps = {
     values: string[];
     onRemove?: (i: number) => void;
     onEdit?: (i: number) => void;
+    getLabel?: (value: string, i: number) => string;
+    isValid?: (value: string) => boolean;
+    ChipComponent?: typeof Chip;
 };
 
-export function ValuesList({ values, onRemove, onEdit }: ValuesListProps) {
+function isElementMatchingSelector(event: React.MouseEvent<unknown>, selectors: string) {
+    return event.nativeEvent.composedPath().some((t) => {
+        return (t as HTMLElement)?.matches?.(selectors);
+    });
+}
+
+export function ValuesList({
+    values,
+    onRemove,
+    onEdit,
+    ChipComponent = Chip,
+    getLabel = (value) => value,
+    isValid = () => true,
+    ...props
+}: ValuesListProps & StackProps) {
     const focusPrevious = useCallback((element: HTMLElement) => {
         const [, prev] = getTabindexedElements(element, element);
         prev.pop()?.focus();
@@ -21,19 +36,57 @@ export function ValuesList({ values, onRemove, onEdit }: ValuesListProps) {
 
     const onDelete = useCallback(
         (index: number) => {
+            if (!onRemove) return undefined;
+
             return ({ currentTarget }: UIEvent<HTMLElement>) => {
                 focusPrevious(currentTarget);
-                onRemove?.(index);
+                onRemove(index);
             };
         },
         [onRemove, focusPrevious],
     );
 
+    const onClick = useCallback(
+        (index: number) => {
+            if (!onEdit) return undefined;
+
+            return () => {
+                onEdit(index);
+            };
+        },
+        [onEdit],
+    );
+
     return (
-        <Stack direction="row" flexWrap="wrap" gap={0.75} m={0.75} tabIndex={-1}>
-            {values.map((value, index) => (
-                <SpelChip key={index} label={value} onDelete={onDelete(index)} onClick={onEdit ? () => onEdit(index) : null} />
-            ))}
+        <Stack
+            direction="row"
+            flexWrap="wrap"
+            gap={0.75}
+            m={0.6875}
+            tabIndex={-1}
+            onPointerDown={(event) => {
+                const strings = [];
+                if (onEdit) strings.push(".MuiChip-root");
+                if (onDelete) strings.push(".MuiChip-deleteIcon");
+                if (isElementMatchingSelector(event, strings.join(","))) {
+                    event.preventDefault();
+                }
+            }}
+            {...props}
+        >
+            {values.map((value, index) => {
+                return (
+                    <ChipComponent
+                        key={index}
+                        size="small"
+                        variant="outlined"
+                        color={isValid(value) ? "default" : "error"}
+                        label={getLabel(value, index)}
+                        onDelete={onDelete(index)}
+                        onClick={onClick(index)}
+                    />
+                );
+            })}
         </Stack>
     );
 }
