2f122206299d538774d1eb75974732456bbfbb01
Julian Wielga <j@touk.pl>
2025-12-03 17:22:42 +0100

    multi select editor test

---
+++ designer/client/cypress/e2e/multiselectEditor.cy.ts
@@ -0,0 +1,48 @@
+describe("Multiselect fixed editor", () => {
+    const seed = "multiselect";
+    const maxDiffThreshold = 0.0001;
+
+    before(() => {
+        cy.deleteAllTestProcesses({ filter: seed, force: true });
+    });
+
+    after(() => {
+        cy.deleteAllTestProcesses({ filter: seed });
+    });
+
+    it("should display wrong data", () => {
+        cy.visitNewProcess(seed, "multiselect_wrong");
+        cy.getNode("Service").should("be.visible").trigger("dblclick");
+        cy.get("[title='multiSelectParam']").next().should("be.visible").as("editor");
+        cy.get("@editor").matchImage({ maxDiffThreshold });
+
+        // force switch mode to display invalid data
+        cy.toggleUserFlag("editor.allowForceSwitch", true);
+        cy.get("@editor").contains("button", "multi select").click({ altKey: true });
+        cy.get("@editor").matchImage({ maxDiffThreshold });
+    });
+
+    it("should show chips with selected values", () => {
+        cy.visitNewProcess(seed, "multiselect_empty");
+        cy.getNode("Service").should("be.visible").trigger("dblclick");
+        cy.get("[title='multiSelectParam']").next().should("be.visible").as("editor");
+        cy.get("@editor").matchImage({ maxDiffThreshold });
+
+        cy.get("@editor").find("input").parent().as("select");
+        cy.get("@select").click();
+        cy.contains("option1").should("be.visible").click();
+        cy.contains("option2").should("be.visible").click();
+        cy.get("body").click(5, 5); //click outside;
+        cy.get("@editor").matchImage({ maxDiffThreshold });
+
+        cy.get("@editor").contains("button", "json").click();
+        cy.get("@editor").matchImage({ maxDiffThreshold });
+        cy.get("@editor").contains("button", "multi select").click();
+
+        cy.contains("[role='button']", "option1").should("be.visible").find('[data-testid="CancelIcon"]').click();
+        cy.contains("[role='button']", "option2").should("be.visible").find('[data-testid="CancelIcon"]').click();
+        cy.contains("option1").should("not.exist");
+        cy.contains("option2").should("not.exist");
+        cy.get("@editor").matchImage({ maxDiffThreshold });
+    });
+});
