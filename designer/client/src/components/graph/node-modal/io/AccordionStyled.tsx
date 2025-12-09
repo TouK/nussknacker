be66dc8a7718d6007f264fedf2bb0231a59f86e0
Julian Wielga <j@touk.pl>
2025-12-09 03:38:44 +0100

    fixed styles

--- designer/client/src/components/graph/node-modal/io/AccordionStyled.tsx
+++ designer/client/src/components/graph/node-modal/io/AccordionStyled.tsx
@@ -6,6 +6,13 @@ export const AccordionStyled = styled(Accordion, {
 })<AccordionProps & { animatedAppear: boolean }>(({ theme, expanded, animatedAppear }) => ({
     // gutter sized shadow to mask sticky elements in margin area
     boxShadow: expanded ? `0 0 0 ${theme.spacing(2)} var(--sidePanelBackground)` : null,
+    "&:first-of-type": {
+        boxShadow: expanded ? `0 ${theme.spacing(1)} 0 ${theme.spacing(1)} var(--sidePanelBackground)` : null,
+    },
+    "&:last-of-type": {
+        boxShadow: expanded ? `0 -${theme.spacing(1)} 0 ${theme.spacing(1)} var(--sidePanelBackground)` : null,
+    },
+    transitionProperty: "transform, box-shadow, margin",
     "&.highlight": {
         animation: [
             `${keyframes({
