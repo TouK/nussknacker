276f58a4aca2a837744cb7dadcafabb37e89cea6
Julian Wielga <j@touk.pl>
2025-12-11 15:24:34 +0100

    SettingsView tweaks

--- designer/client/src/containers/settings/SettingsView.tsx
+++ designer/client/src/containers/settings/SettingsView.tsx
@@ -22,11 +22,17 @@ function SettingsView() {
     const settings = useAppSelector(getUserSettingsMerged);
     const rawValues = useAppSelector(getUserSettingsValues);
     const [filter, setFilter] = useState("");
+    const searchStrings = useMemo(() => filter.toLowerCase().split(" "), [filter]);
 
     const values = useMemo(() => {
-        const filtered = Object.entries(settings).filter(([key]) => key.toLowerCase().includes(filter.toLowerCase()));
+        const filtered = Object.entries(settings).filter(([key]) => {
+            const value = key.toLowerCase();
+            return searchStrings.every((string) => {
+                return value.includes(string);
+            });
+        });
         return toNested(filtered);
-    }, [filter, settings]);
+    }, [searchStrings, settings]);
 
     const { t } = useTranslation();
 
@@ -64,6 +70,8 @@ function SettingsView() {
                     );
                 }}
                 flattenSingleChild
+                openIfOnly
+                searchStrings={searchStrings.flatMap((v) => v.split("."))}
             />
         </Stack>
     );
