1f24ae1bc7b421c4c430b0e47c9afff82cca7ce4
Julian Wielga <j@touk.pl>
2025-12-12 15:57:57 +0100

    test fix

--- designer/client/cypress/support/testing.ts
+++ designer/client/cypress/support/testing.ts
@@ -8,19 +8,19 @@ declare global {
 }
 
 const addTestRecord = (callback?: () => void) => {
-    cy.get('[data-testid="data-grid-canvas"]')
-        .should("exist")
-        .then(($canvas) => {
-            const rect = $canvas[0].getBoundingClientRect();
-            // Click near bottom right corner
-            const clickX = rect.width - 10;
-            const clickY = rect.height - 10;
+    cy.get('[data-testid="data-grid-canvas"]').should("exist");
+    cy.wait(500);
+    cy.get('[data-testid="data-grid-canvas"]').then(($canvas) => {
+        const rect = $canvas[0].getBoundingClientRect();
+        // Click near bottom right corner
+        const clickX = rect.width - 10;
+        const clickY = rect.height - 10;
 
-            cy.wrap($canvas).click(clickX, clickY, { force: true });
-            if (callback) {
-                callback();
-            }
-        });
+        cy.wrap($canvas).realClick({ x: clickX, y: clickY });
+        if (callback) {
+            callback();
+        }
+    });
 };
 
 Cypress.Commands.add("addTestRecord", addTestRecord);
