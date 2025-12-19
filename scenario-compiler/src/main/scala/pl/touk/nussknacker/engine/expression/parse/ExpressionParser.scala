56ae33848be987b2e818b29c183bb0d04d727720
Arek Burdach <arek.burdach@gmail.com>
2025-12-19 15:43:30 +0100

    [NU-2414] Switch TableSink usage of DiscardingSink to SinkV2 DiscardingSink (#8815)
    * [NU-2414] Switch TableSink usage of DiscardingSink to SinkV2 DiscardingSink
    * Expression type not aligned after conversion fix

--- scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/expression/parse/ExpressionParser.scala
+++ scenario-compiler/src/main/scala/pl/touk/nussknacker/engine/expression/parse/ExpressionParser.scala
@@ -1,10 +1,12 @@
 package pl.touk.nussknacker.engine.expression.parse
 
-import cats.data.{Validated, ValidatedNel}
 import cats.data.Validated.Valid
+import cats.data.ValidatedNel
 import pl.touk.nussknacker.engine.api.context.ValidationContext
 import pl.touk.nussknacker.engine.api.expression.ExpressionTypingInfo
 import pl.touk.nussknacker.engine.api.generics.ExpressionParseError
+import pl.touk.nussknacker.engine.api.typed.{AssignabilityDeterminer, ConversionNecessaryForTypeAssignment}
+import pl.touk.nussknacker.engine.api.typed.ConversionStrategy.Loose
 import pl.touk.nussknacker.engine.api.typed.typing.{TypedNull, TypingResult}
 import pl.touk.nussknacker.engine.expression.NullExpression
 import pl.touk.nussknacker.engine.graph.expression.Expression.Language
@@ -43,12 +45,13 @@ object ExpressionParser {
   def validateResultTypeMatchExpectedType(
       resultType: TypingResult,
       expectedType: TypingResult
-  ): ValidatedNel[ExpressionParseError, Unit] = {
-    Validated.condNel(
-      resultType.canBeLooselyAssignedTo(expectedType),
-      (),
-      BadTypeExpressionParseError(expectedType, resultType)
-    )
+  ): ValidatedNel[ExpressionParseError, ConversionNecessaryForTypeAssignment] = {
+    AssignabilityDeterminer
+      .isAssignable(resultType, expectedType)(Loose)
+      .leftMap { _ =>
+        BadTypeExpressionParseError(expectedType, resultType)
+      }
+      .toValidatedNel
   }
 
   private[engine] case class BadTypeExpressionParseError(expected: TypingResult, found: TypingResult)
