bbd723b733376e4d4b3fec8d8563d7b9044f4097
Arek Burdach <arek.burdach@gmail.com>
2025-12-29 11:59:16 +0100

    [NU-2414] Minor deprecated api cleanups

--- engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/registrar/StreamExecutionEnvPreparer.scala
+++ engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/registrar/StreamExecutionEnvPreparer.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.engine.process.registrar
 
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.flink.api.common.RuntimeExecutionMode
-import org.apache.flink.runtime.state.StateBackend
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.datastream.{DataStream, SingleOutputStreamOperator}
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.apache.flink.util.OutputTag
@@ -13,10 +13,7 @@ import pl.touk.nussknacker.engine.process.FlinkJobConfig.ExecutionMode
 import pl.touk.nussknacker.engine.process.FlinkJobConfig.ExecutionMode.ExecutionMode
 import pl.touk.nussknacker.engine.process.compiler.FlinkProcessCompilerData
 import pl.touk.nussknacker.engine.process.util.StateConfiguration
-import pl.touk.nussknacker.engine.process.util.StateConfiguration.RocksDBStateBackendConfig
 import pl.touk.nussknacker.engine.util.MetaDataExtractor
-
-import scala.annotation.nowarn
 
 /*
   This trait is meant to be the place to configure StreamExecutionEnvironment. Here (e.g. in DefaultStreamExecutionEnvPreparer)
@@ -72,13 +69,20 @@ class DefaultStreamExecutionEnvPreparer(
     (jobConfig.rocksDB, streamMetaData.spillStateToDisk) match {
       case (Some(config), Some(true)) if config.enable =>
         logger.info("Using RocksDB state backend")
-        env.configure(StateConfiguration.prepareRocksDBStateBackendFlinkConfiguration(config), null)
+        env.configure(prepareRocksDBStateBackendFlinkConfiguration(config), null)
       case (None, Some(true)) =>
         // TODO: handle non-configured rocksDB more transparently e.g. hide checkbox on FE?
         logger.warn("RocksDB not configured, cannot use spillStateToDisk")
       case _ =>
         logger.info("Using default state backend configured by cluster")
     }
+  }
+
+  // This method is protected for purpose of backward compatibility layer for flink 1.20
+  protected def prepareRocksDBStateBackendFlinkConfiguration(
+      config: StateConfiguration.RocksDBStateBackendConfig
+  ): Configuration = {
+    StateConfiguration.prepareRocksDBStateBackendFlinkConfiguration(config)
   }
 
   private def configureCheckpoints(env: StreamExecutionEnvironment, streamMetaData: StreamMetaData): Unit = {
--- engine/flink/executor/src/test/scala/pl/touk/nussknacker/engine/process/util/SerializersSpec.scala
+++ engine/flink/executor/src/test/scala/pl/touk/nussknacker/engine/process/util/SerializersSpec.scala
@@ -5,41 +5,52 @@ import org.apache.flink.runtime.types.FlinkScalaKryoInstantiator
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.time.{
+  Duration,
+  Instant,
+  LocalDate,
+  LocalDateTime,
+  LocalTime,
+  MonthDay,
+  OffsetDateTime,
+  OffsetTime,
+  Period,
+  Year,
+  YearMonth,
+  ZonedDateTime,
+  ZoneId,
+  ZoneOffset
+}
 import java.util.Collections
 import scala.jdk.CollectionConverters._
 
 class SerializersSpec extends AnyFlatSpec with Matchers {
 
   it should "serialize case objects" in {
-    val deserialized = serializeAndDeserialize(None)
-    deserialized shouldBe None
+    checkSerializeDeserializeRoundTrip(None)
   }
 
   it should "serialize usual case class" in {
-    val obj          = UsualCaseClass("a", 1)
-    val deserialized = serializeAndDeserialize(obj)
-    deserialized shouldBe obj
+    val obj = UsualCaseClass("a", 1)
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize case class without params" in {
-    val obj          = NoParams()
-    val deserialized = serializeAndDeserialize(obj)
-    deserialized shouldBe obj
+    val obj = NoParams()
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize case class with implicit param" in {
     implicit val b: Long = 5L
     val obj              = WithImplicitVal("a")
 
-    val deserialized = serializeAndDeserialize(obj)
-
-    deserialized shouldBe obj
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize inner case class" in {
     val obj = WrapperObj.createInner("abc")
 
-    val deserialized = serializeAndDeserialize(obj.asInstanceOf[Product])
+    val deserialized = checkSerializeDeserializeRoundTrip(obj)
 
     // deserialized shouldBe obj // it explodes here because of deserialized obj.$outer ???
     deserialized.getClass shouldBe obj.getClass
@@ -49,52 +60,65 @@ class SerializersSpec extends AnyFlatSpec with Matchers {
   it should "serialize inner case class 2" in {
     val obj = WrapperObj.InnerGenericCaseClass("a")
 
-    val deserialized = serializeAndDeserialize(obj.asInstanceOf[Product])
-
-    deserialized shouldBe obj
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize inner case class 3" in {
     val obj = WrapperObj.InnerGenericCaseClassWithTrait[String]("a")
 
-    val deserialized = serializeAndDeserialize(obj.asInstanceOf[Product])
-
-    deserialized shouldBe obj
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize case class with serial version uid" in {
-    val obj          = CaseClassWithSerialVersionUID("a")
-    val deserialized = serializeAndDeserialize(obj)
-    deserialized shouldBe obj
+    val obj = CaseClassWithSerialVersionUID("a")
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize nested case class with serial version uid" in {
-    val obj = ObjCaseClassWrapper.CaseClassWithSerialVersionUIDInObject("a")
-    val deserialized =
-      serializeAndDeserialize(obj)
+    val obj          = ObjCaseClassWrapper.CaseClassWithSerialVersionUIDInObject("a")
+    val deserialized = checkSerializeDeserializeRoundTrip(obj)
     deserialized.a shouldBe obj.a
     deserialized.someField shouldBe obj.someField
   }
 
   it should "serialize unmodifiableList" in {
     val obj = Collections.unmodifiableList(List("foo", "bar").asJava)
-    val deserialized =
-      serializeAndDeserialize(obj)
-    deserialized shouldBe obj
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
   it should "serialize unmodifiableMap" in {
     val obj = Collections.unmodifiableMap(Map("foo" -> 1, "bar" -> 2).asJava)
-    val deserialized =
-      serializeAndDeserialize(obj)
-    deserialized shouldBe obj
+    checkSerializeDeserializeRoundTrip(obj)
   }
 
-  def serializeAndDeserialize[T](obj: T): T = {
+  it should "serialize date/time classes" in {
+    checkSerializeDeserializeRoundTrip(Duration.ofMillis(123))
+    checkSerializeDeserializeRoundTrip(Instant.ofEpochMilli(123))
+    checkSerializeDeserializeRoundTrip(LocalDate.of(2026, 1, 1))
+    checkSerializeDeserializeRoundTrip(LocalTime.of(12, 13))
+    checkSerializeDeserializeRoundTrip(LocalDateTime.of(2026, 1, 1, 12, 13))
+    checkSerializeDeserializeRoundTrip(ZoneOffset.of("+01:00"))
+    checkSerializeDeserializeRoundTrip(ZoneId.of("GMT"))
+    checkSerializeDeserializeRoundTrip(OffsetTime.of(LocalTime.of(12, 13), ZoneOffset.of("+01:00")))
+    checkSerializeDeserializeRoundTrip(
+      OffsetDateTime.of(LocalDate.of(2026, 1, 1), LocalTime.of(12, 13), ZoneOffset.of("+01:00"))
+    )
+    checkSerializeDeserializeRoundTrip(
+      ZonedDateTime.of(LocalDate.of(2026, 1, 1), LocalTime.of(12, 13), ZoneId.of("GMT"))
+    )
+    checkSerializeDeserializeRoundTrip(Year.of(2026))
+    checkSerializeDeserializeRoundTrip(YearMonth.of(2026, 1))
+    checkSerializeDeserializeRoundTrip(MonthDay.of(1, 2))
+    checkSerializeDeserializeRoundTrip(Period.ofWeeks(12))
+  }
+
+  def checkSerializeDeserializeRoundTrip[T](obj: T): T = {
     val kryo = new FlinkScalaKryoInstantiator().newKryo()
     val out  = new Output(1024)
     kryo.writeObject(out, obj)
-    kryo.readObject(new Input(out.toBytes), obj.getClass.asInstanceOf[Class[T]])
+    val deserialized = kryo.readObject(new Input(out.toBytes), obj.getClass.asInstanceOf[Class[T]])
+    deserialized shouldBe obj
+    deserialized
   }
 
 }
