00b629682132df311fc41d1681c32ef6db484282
Julian Wielga <j@touk.pl>
2026-01-17 14:31:35 +0100

    styled drop target

--- designer/client/src/components/common/dndItems/DropTarget.tsx
+++ designer/client/src/components/common/dndItems/DropTarget.tsx
@@ -1,22 +1,25 @@
 import type { DragDropContextProps, DraggableChildrenFn, DroppableProps } from "@hello-pangea/dnd";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
-import type { ReactComponentLike } from "prop-types";
-import type { PropsWithChildren } from "react";
+import { styled } from "@mui/material";
+import type { ComponentProps, ElementType, PropsWithChildren } from "react";
 import React, { useCallback } from "react";
 
+interface TargetProps extends Pick<DragDropContextProps, "onDragEnd" | "onDragUpdate" | "onDragStart">, Omit<DroppableProps, "children"> {
+    CloneWrapper?: ElementType;
+    className?: string;
+}
+
 // TODO: get rid of renderClone to fix touch ux -> replace CloneWrapper with styles to fix clone translation
-export function DropTarget({
+function Target({
     children,
     renderClone,
     CloneWrapper = "div",
     onDragEnd,
     onDragStart,
     onDragUpdate,
+    className,
     ...props
-}: PropsWithChildren<
-    { CloneWrapper?: ReactComponentLike } & Pick<DragDropContextProps, "onDragEnd" | "onDragUpdate" | "onDragStart"> &
-        Omit<DroppableProps, "children">
->): React.JSX.Element {
+}: PropsWithChildren<TargetProps>) {
     const clone: DraggableChildrenFn = useCallback(
         (...args) => <CloneWrapper>{renderClone(...args)}</CloneWrapper>,
         [CloneWrapper, renderClone],
@@ -25,7 +28,7 @@ export function DropTarget({
         <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart} onDragUpdate={onDragUpdate}>
             <Droppable {...props} renderClone={clone}>
                 {(p) => (
-                    <div ref={p.innerRef}>
+                    <div ref={p.innerRef} className={className}>
                         <div {...p.droppableProps}>
                             {children}
                             {p.placeholder}
@@ -36,3 +39,6 @@ export function DropTarget({
         </DragDropContext>
     );
 }
+
+export type DropTargetProps = ComponentProps<typeof DropTarget>;
+export const DropTarget = styled(Target)({});
