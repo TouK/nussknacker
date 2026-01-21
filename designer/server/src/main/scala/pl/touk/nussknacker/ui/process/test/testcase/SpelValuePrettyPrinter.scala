458e6480e37ca2b2361cda622fd9c4e7a501ae3f
Łukasz Jędrzejewski <ljd@touk.pl>
2026-01-21 12:45:36 +0100

    Spel pretty print null, string, long

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/testcase/SpelValuePrettyPrinter.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/testcase/SpelValuePrettyPrinter.scala
@@ -7,12 +7,22 @@ object SpelValuePrettyPrinter {
 
   def prettyPrintValue(value: Any): String = {
     value match {
-      case array: Array[_]                         => array.map(prettyPrintValue).mkString("{", ", ", "}")
-      case map: java.util.Map[_, _] if map.isEmpty => "{:}"
+      case null =>
+        "null"
+      case s: String =>
+        s"'${s.replace("'", "''")}'"
+      case l: java.lang.Long =>
+        s"${l}L"
+      case array: Array[_] =>
+        array.map(prettyPrintValue).mkString("{", ", ", "}")
+      case map: java.util.Map[_, _] if map.isEmpty =>
+        "{:}"
       case map: java.util.Map[_, _] =>
         map.asScala.map { case (k, v) => prettyPrintValue(k) + ": " + prettyPrintValue(v) }.mkString("{", ", ", "}")
-      case collection: java.util.Collection[_] => collection.asScala.map(prettyPrintValue).mkString("{", ", ", "}")
-      case _                                   => Objects.toString(value)
+      case collection: java.util.Collection[_] =>
+        collection.asScala.map(prettyPrintValue).mkString("{", ", ", "}")
+      case _ =>
+        Objects.toString(value)
     }
   }
 
