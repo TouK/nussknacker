f1c0f9a2eaa13bfa8de7beb7f68c9d34941a6332
Łukasz Jędrzejewski <ljd@touk.pl>
2026-01-29 10:47:06 +0100

    http enricher test

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/process/test/testcase/SpelExpressionGeneratorSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/process/test/testcase/SpelExpressionGeneratorSpec.scala
@@ -8,26 +8,31 @@ class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
 
   test("should generate sample expression for String") {
     val result = SpelExpressionGenerator.generate(Typed[String])
+
     result shouldBe Some("'string'")
   }
 
   test("should generate sample expression for Integer") {
     val result = SpelExpressionGenerator.generate(Typed[Integer])
+
     result shouldBe Some("42")
   }
 
   test("should generate sample expression for Long") {
     val result = SpelExpressionGenerator.generate(Typed[java.lang.Long])
+
     result shouldBe Some("42")
   }
 
   test("should generate sample expression for Double") {
     val result = SpelExpressionGenerator.generate(Typed[java.lang.Double])
+
     result shouldBe Some("42.0")
   }
 
   test("should generate sample expression for Boolean") {
     val result = SpelExpressionGenerator.generate(Typed[java.lang.Boolean])
+
     result shouldBe Some("true")
   }
 
@@ -38,7 +43,9 @@ class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
         "age"  -> Typed[Integer]
       )
     )
+
     val result = SpelExpressionGenerator.generate(recordType)
+
     result shouldBe Some("{name: 'string', age: 42}")
   }
 
@@ -54,13 +61,17 @@ class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
         "count" -> Typed[Integer]
       )
     )
+
     val result = SpelExpressionGenerator.generate(nestedRecord)
+
     result shouldBe Some("{user: {name: 'string', active: true}, count: 42}")
   }
 
   test("should generate sample expression for List") {
     val listType = Typed.genericTypeClass(classOf[java.util.List[_]], List(Typed[String]))
-    val result   = SpelExpressionGenerator.generate(listType)
+
+    val result = SpelExpressionGenerator.generate(listType)
+
     result shouldBe Some("{'string'}")
   }
 
@@ -69,13 +80,53 @@ class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
       classOf[java.util.Map[_, _]],
       List(Typed[String], Typed[Integer])
     )
+
     val result = SpelExpressionGenerator.generate(mapType)
+
     result shouldBe Some("{'key': 42}")
   }
 
   test("should return null for Unknown type") {
     val result = SpelExpressionGenerator.generate(Unknown)
+
     result shouldBe Some("null")
+  }
+
+  test("should generate sample expression for complex HTTP request/response record") {
+    val headerRecordType = Typed.record(
+      Map(
+        "name"  -> Typed[String],
+        "value" -> Typed[String]
+      )
+    )
+    val requestType = Typed.record(
+      Map(
+        "body"    -> Typed.record(Map.empty[String, TypingResult]),
+        "headers" -> Typed.genericTypeClass(classOf[java.util.List[_]], List(headerRecordType)),
+        "method"  -> Typed[String],
+        "url"     -> Typed[String]
+      )
+    )
+    val responseType = Typed.record(
+      Map(
+        "body"       -> Unknown,
+        "headers"    -> Typed.genericTypeClass(classOf[java.util.List[_]], List(headerRecordType)),
+        "statusCode" -> Typed[Integer],
+        "statusText" -> Typed[String]
+      )
+    )
+    val httpRecordType = Typed.record(
+      Map(
+        "request"  -> requestType,
+        "response" -> responseType
+      )
+    )
+
+    val result = SpelExpressionGenerator.generate(httpRecordType)
+
+    result shouldBe Some(
+      "{request: {body: {}, headers: {{name: 'string', value: 'string'}}, method: 'string', url: 'string'}, response: {body: null, headers: {{name: 'string', value: 'string'}}, statusCode: 42, statusText: 'string'}}"
+    )
   }
 
 }
