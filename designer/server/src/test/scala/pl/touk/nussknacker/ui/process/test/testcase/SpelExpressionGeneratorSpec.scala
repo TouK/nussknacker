40d905a32c5a1dbd3a256777d7ff74888b22fdd5
Łukasz Jędrzejewski <ljd@touk.pl>
2026-01-29 10:39:53 +0100

    import

--- designer/server/src/test/scala/pl/touk/nussknacker/ui/process/test/testcase/SpelExpressionGeneratorSpec.scala
+++ designer/server/src/test/scala/pl/touk/nussknacker/ui/process/test/testcase/SpelExpressionGeneratorSpec.scala
@@ -2,40 +2,40 @@ package pl.touk.nussknacker.ui.process.test.testcase
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import pl.touk.nussknacker.engine.api.typed.typing
+import pl.touk.nussknacker.engine.api.typed.typing._
 
 class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
 
   test("should generate sample expression for String") {
-    val result = SpelExpressionGenerator.generate(typing.Typed[String])
+    val result = SpelExpressionGenerator.generate(Typed[String])
     result shouldBe Some("'string'")
   }
 
   test("should generate sample expression for Integer") {
-    val result = SpelExpressionGenerator.generate(typing.Typed[Integer])
+    val result = SpelExpressionGenerator.generate(Typed[Integer])
     result shouldBe Some("42")
   }
 
   test("should generate sample expression for Long") {
-    val result = SpelExpressionGenerator.generate(typing.Typed[java.lang.Long])
+    val result = SpelExpressionGenerator.generate(Typed[java.lang.Long])
     result shouldBe Some("42")
   }
 
   test("should generate sample expression for Double") {
-    val result = SpelExpressionGenerator.generate(typing.Typed[java.lang.Double])
+    val result = SpelExpressionGenerator.generate(Typed[java.lang.Double])
     result shouldBe Some("42.0")
   }
 
   test("should generate sample expression for Boolean") {
-    val result = SpelExpressionGenerator.generate(typing.Typed[java.lang.Boolean])
+    val result = SpelExpressionGenerator.generate(Typed[java.lang.Boolean])
     result shouldBe Some("true")
   }
 
   test("should generate sample expression for record type") {
-    val recordType = typing.Typed.record(
+    val recordType = Typed.record(
       Map(
-        "name" -> typing.Typed[String],
-        "age"  -> typing.Typed[Integer]
+        "name" -> Typed[String],
+        "age"  -> Typed[Integer]
       )
     )
     val result = SpelExpressionGenerator.generate(recordType)
@@ -43,15 +43,15 @@ class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
   }
 
   test("should generate sample expression for nested record") {
-    val nestedRecord = typing.Typed.record(
+    val nestedRecord = Typed.record(
       Map(
-        "user" -> typing.Typed.record(
+        "user" -> Typed.record(
           Map(
-            "name"   -> typing.Typed[String],
-            "active" -> typing.Typed[java.lang.Boolean]
+            "name"   -> Typed[String],
+            "active" -> Typed[java.lang.Boolean]
           )
         ),
-        "count" -> typing.Typed[Integer]
+        "count" -> Typed[Integer]
       )
     )
     val result = SpelExpressionGenerator.generate(nestedRecord)
@@ -59,22 +59,22 @@ class SpelExpressionGeneratorSpec extends AnyFunSuite with Matchers {
   }
 
   test("should generate sample expression for List") {
-    val listType = typing.Typed.genericTypeClass(classOf[java.util.List[_]], List(typing.Typed[String]))
+    val listType = Typed.genericTypeClass(classOf[java.util.List[_]], List(Typed[String]))
     val result   = SpelExpressionGenerator.generate(listType)
     result shouldBe Some("{'string'}")
   }
 
   test("should generate sample expression for Map") {
-    val mapType = typing.Typed.genericTypeClass(
+    val mapType = Typed.genericTypeClass(
       classOf[java.util.Map[_, _]],
-      List(typing.Typed[String], typing.Typed[Integer])
+      List(Typed[String], Typed[Integer])
     )
     val result = SpelExpressionGenerator.generate(mapType)
     result shouldBe Some("{'key': 42}")
   }
 
   test("should return null for Unknown type") {
-    val result = SpelExpressionGenerator.generate(typing.Unknown)
+    val result = SpelExpressionGenerator.generate(Unknown)
     result shouldBe Some("null")
   }
 
