8c91fbd2217b07fadf5f2ab543166b9e64770ef6
Julian Wielga <j@touk.pl>
2026-02-04 13:35:02 +0100

    useUserSettings

--- designer/client/src/containers/SnowSnow.tsx
+++ designer/client/src/containers/SnowSnow.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Snowfall from "react-snowfall";
 
+import { useUserSettings } from "../common/useUserSettings";
 import { ThemedStylesWrapper } from "../components/ThemedStylesWrapper";
-import { getUserSettings } from "../reducers/selectors/userSettings";
-import { useAppSelector } from "../store/storeHelpers";
 
 export const SNOW_SNOW_FLAG = "Let it snow!❄️🎄🎅🏼☃️";
 
 export function SnowSnow() {
-    const settings = useAppSelector(getUserSettings);
-    const isSnowing = settings[SNOW_SNOW_FLAG];
+    const [isSnowing] = useUserSettings(SNOW_SNOW_FLAG);
 
     if (!isSnowing) return null;
     return (
