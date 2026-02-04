c84de033fce19d7ffd37358830577345cea78487
Julian Wielga <j@touk.pl>
2026-02-04 15:31:46 +0100

    useUserSettings

--- designer/client/src/components/toolbars/useSurvey.tsx
+++ designer/client/src/components/toolbars/useSurvey.tsx
@@ -2,20 +2,19 @@ import { useCallback } from "react";
 
 import type { SurveySettings } from "../../actions/nk/assignSettings";
 import { userSettingSet } from "../../actions/nk/userSettings";
+import { useUserSettings } from "../../common/useUserSettings";
 import { getSurveySettings } from "../../reducers/selectors/settings";
-import { getUserSettings } from "../../reducers/selectors/userSettings";
 import type { Setting } from "../../reducers/userSettings";
 import { useAppDispatch, useAppSelector } from "../../store/storeHelpers";
 
 export function useSurvey(): [SurveySettings | false, () => void] {
     const survey = useAppSelector(getSurveySettings);
-    const userSettings = useAppSelector(getUserSettings);
     const dispatch = useAppDispatch();
 
     const settingsKey: Setting = `survey.${survey?.key}.closed`;
+    const [closed] = useUserSettings(settingsKey);
 
-    const showSurvey = !userSettings[settingsKey];
     const hideSurvey = useCallback(() => dispatch(userSettingSet(settingsKey, true)), [dispatch, settingsKey]);
 
-    return [showSurvey && survey, hideSurvey];
+    return [!closed && survey, hideSurvey];
 }
