1d7f10591c4513b8244a27b47446fcc47c6a806b
Michał Goworko <mgw@touk.pl>
2026-02-17 19:44:57 +0100

    qs

--- engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/scenariotesting/StubbedFragmentSourceDefinitionPreparer.scala
+++ engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/scenariotesting/StubbedFragmentSourceDefinitionPreparer.scala
@@ -37,7 +37,7 @@ class StubbedFragmentSourceDefinitionPreparer(
       implementationInvoker =
         (_: Params, _: NodeCompilationDependencies, _: Option[ComponentImplementationSpecificInvocationContext]) =>
           buildSource(inputParameters),
-      parameters = inputParameters,
+      parameters = List.empty,
       outputNames = List.empty,
       docsUrl = None,
       componentGroupName = None,
