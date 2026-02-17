fda464f7671afee1c2c9aa215d8965c5342c96d7
Filip Michalski <filemon.michalski@gmail.com>
2026-02-17 11:37:24 +0100

    Update Url to URL for websocket override keys (#8978)

--- designer/client/src/components/graph/node-modal/parameterHelpers.tsx
+++ designer/client/src/components/graph/node-modal/parameterHelpers.tsx
@@ -24,7 +24,7 @@ export const OverrideKeys = {
     AggregateEndSession: "custom-aggregate-session/endSessionCondition",
     HttpQueryParameters: "service-http/Query Parameters",
     HttpHeaders: "service-http/Headers",
-    WebSocketUrl: "source-websocket/Url",
+    WebSocketUrl: "source-websocket/URL",
 } as const;
 
 export type ParamKeys = (typeof OverrideKeys)[keyof typeof OverrideKeys] | (string & NonNullable<unknown>);
