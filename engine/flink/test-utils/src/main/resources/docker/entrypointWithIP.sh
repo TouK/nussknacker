51adcbdd6711f7bb554d18fca8ce106d0dc4923d
Piotr Przybylski <ppr@touk.pl>
2026-02-18 20:04:21 +0100

    Unify directory permission management for Docker mounts (#8983)

--- engine/flink/test-utils/src/main/resources/docker/entrypointWithIP.sh
+++ engine/flink/test-utils/src/main/resources/docker/entrypointWithIP.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-#TODO: get rid of it
 set -eu
+
 if [[ -n "${SAVEPOINT_DIR_PATH:-}" ]]; then
-  mkdir -p "$SAVEPOINT_DIR_PATH"
   chmod -R 777 "$SAVEPOINT_DIR_PATH"
 fi
-mkdir -p /output
-chmod -R 777 /output
+
+if [[ -d /output ]]; then
+  chmod -R 777 /output
+fi
 
 cat /config.overrides.yml >> $FLINK_HOME/conf/config.yaml
 cat /log4j-console.overrides.properties >> $FLINK_HOME/conf/log4j-console.properties
--- utils/test-utils/src/main/scala/pl/touk/nussknacker/test/containers/WithDockerContainers.scala
+++ utils/test-utils/src/main/scala/pl/touk/nussknacker/test/containers/WithDockerContainers.scala
@@ -4,6 +4,9 @@ import org.scalatest.Suite
 import org.testcontainers.containers.{BindMode, Network}
 import pl.touk.nussknacker.test.containers.LogLevelConfigurableScalaLoggingConsumer.LoggerLevel
 
+import java.nio.file.{Files, FileSystems, Path}
+import java.nio.file.attribute.PosixFilePermissions
+
 trait WithDockerContainers { self: Suite =>
 
   // dedicated method because withPrefix is mutable
@@ -14,6 +17,30 @@ trait WithDockerContainers { self: Suite =>
     ).withPrefix(prefix)
 
   protected val network: Network = Network.newNetwork
+
+  /**
+   * Creates a temporary directory with possibly insecure permissions.
+   * If your directory is writable you may need to also use `chmod` in Docker container.
+   *
+   * Instead of mounting prefer [[com.dimafeng.testcontainers.GenericContainer.copyFileFromContainer()]]
+   * and [[com.dimafeng.testcontainers.GenericContainer.copyFileToContainer()]].
+   */
+  protected def createMountableTempDirectory(prefix: String, mode: BindMode): Path = {
+    val posixPerms = mode match {
+      case BindMode.READ_WRITE => "rwxrwxrwx"
+      case BindMode.READ_ONLY  => "rwxr-xr-x"
+    }
+
+    val tempDirectoryAttributes = {
+      if (FileSystems.getDefault.supportedFileAttributeViews().contains("posix")) {
+        val allPermissions = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString(posixPerms))
+        List(allPermissions)
+      } else {
+        Nil // Windows
+      }
+    }
+    Files.createTempDirectory(prefix, tempDirectoryAttributes: _*)
+  }
 
 }
 
