0c873b18ccda0ea88dbb7f6ddb3ba3ddd9240c08
Dawid Poliszak <dpl@touk.pl>
2026-02-18 09:41:22 +0100

    [NU-2464] sort assertions order

--- designer/client/src/components/toolbars/assertionResults/assertionResults.tsx
+++ designer/client/src/components/toolbars/assertionResults/assertionResults.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useMemo } from "react";
 
+import { getScenarioGraph } from "../../../reducers/selectors/graph";
 import { getTestAssertionResults } from "../../../reducers/selectors/testing";
 import { useAppSelector } from "../../../store/storeHelpers";
 import { Expandable } from "../../common/Expandable";
@@ -11,11 +12,31 @@ import { AssertionResultsHeader } from "./assertionResultsHeader";
 
 const AssertionResults = () => {
     const testAssertionResults = useAppSelector(getTestAssertionResults);
+    const scenarioGraph = useAppSelector(getScenarioGraph);
+
+    const nodeOrderMap = useMemo(
+        () =>
+            scenarioGraph.nodes.reduce((acc, node, index) => {
+                acc[node.id] = index;
+                return acc;
+            }, {} as Record<string, number>),
+        [scenarioGraph.nodes],
+    );
+
+    const sortedNodeIds = useMemo(
+        () =>
+            Object.keys(testAssertionResults).sort((nodeIdA, nodeIdB) => {
+                const orderA = nodeOrderMap[nodeIdA] ?? Infinity;
+                const orderB = nodeOrderMap[nodeIdB] ?? Infinity;
+                return orderA - orderB;
+            }),
+        [testAssertionResults, nodeOrderMap],
+    );
     return (
         <ToolbarWrapper id={"assertion-results-panel"} title={"Assertions result"}>
             <Box py={1}>
                 <AssertionResultsHeader />
-                {Object.keys(testAssertionResults).map((nodeId) => (
+                {sortedNodeIds.map((nodeId) => (
                     <Expandable
                         key={nodeId}
                         expandableTitle={<AssertionResultTitle title={nodeId} assertionResults={testAssertionResults[nodeId]} />}
