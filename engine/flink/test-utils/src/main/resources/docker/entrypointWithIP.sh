7f3b25c2e93a721c94b7c4ad8ebfc23231af33aa
Piotr Przybylski <ppr@touk.pl>
2026-02-18 19:24:59 +0100

    Adjust configuration of test Flink cluster in `WithFlinkContainers` (#8983)
    * adjust temp path generation so that container always gets Linux-compatible paths
    * JobManager - total memory use is the same, more goes towards metaspace (400m -> 700m)
    * TaskManager - gets 100m more of memory, more goes towards metaspace (512m -> 700m)

--- engine/flink/test-utils/src/main/resources/docker/entrypointWithIP.sh
+++ engine/flink/test-utils/src/main/resources/docker/entrypointWithIP.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #TODO: get rid of it
-mkdir -p /tmp/$SAVEPOINT_DIR_NAME
-chmod -R 777 /tmp/$SAVEPOINT_DIR_NAME
+set -eu
+if [[ -n "${SAVEPOINT_DIR_PATH:-}" ]]; then
+  mkdir -p "$SAVEPOINT_DIR_PATH"
+  chmod -R 777 "$SAVEPOINT_DIR_PATH"
+fi
 mkdir -p /output
 chmod -R 777 /output
 
 cat /config.overrides.yml >> $FLINK_HOME/conf/config.yaml
 cat /log4j-console.overrides.properties >> $FLINK_HOME/conf/log4j-console.properties
 
-/docker-entrypoint.sh "$@"
+exec /docker-entrypoint.sh "$@"
