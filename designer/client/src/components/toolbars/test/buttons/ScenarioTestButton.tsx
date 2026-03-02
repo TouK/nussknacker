7b406625121ae685fc48616e34a0fe493a2f2e9e
Dawid Poliszak <dpl@touk.pl>
2026-03-02 11:38:12 +0100

    [NU-2471] add test case to ScenarioTestButton

--- designer/client/src/components/toolbars/test/buttons/ScenarioTestButton.tsx
+++ designer/client/src/components/toolbars/test/buttons/ScenarioTestButton.tsx
@@ -1,12 +1,15 @@
-import { alpha } from "@mui/material";
-import React, { useContext, useMemo } from "react";
+import { alpha, styled } from "@mui/material";
+import React, { useCallback, useContext, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+import { testScenarioWithTestCase } from "../../../../actions/nk/testingActions";
 import TestingIcon from "../../../../assets/img/toolbarButtons/test.svg";
-import { hasInputDataRecordsDefined } from "../../../../reducers/selectors/testCases";
+import { useUserSettings } from "../../../../common/useUserSettings";
+import type { TestCase } from "../../../../reducers/graph/testCase";
+import { getTestCase } from "../../../../reducers/selectors/testCases";
 import { getTestResultsLoading } from "../../../../reducers/selectors/testing";
 import { ToolbarsSide } from "../../../../reducers/toolbars";
-import { useAppSelector } from "../../../../store/storeHelpers";
+import { useAppDispatch, useAppSelector } from "../../../../store/storeHelpers";
 import { useTestingScenarioEnabled } from "../../../modals/TestingDataRecords/useTestingScenarioEnabled";
 import { ToolbarButton } from "../../../toolbarComponents/toolbarButtons/ToolbarButton";
 import { ButtonsVariant, ToolbarButtonsContext } from "../../../toolbarComponents/toolbarButtons/ToolbarButtons";
@@ -21,27 +24,42 @@ export type ScenarioTestButtonProps = {
     titleOverride?: string;
 };
 
-const RUN_ALL = "runAll";
-const RERUN_LAST_TEST = "rerunLastTest";
-
 type Preset = {
     value: string;
     label: string;
     isDisabled?: boolean;
 };
 
-function ScenarioTestButton(props: PropsOfButton<CustomButtonTypes.scenarioTest>) {
-    const { disabled, name, title, titleOverride, type } = props;
-    const { t } = useTranslation();
+const RUN_ALL = "runAll";
 
-    const presets: Preset[] = useMemo(() => {
+function ScenarioTestButton(props: PropsOfButton<CustomButtonTypes.scenarioTest>) {
+    const { disabled, title, titleOverride, type } = props;
+    const { t } = useTranslation();
+    const testCase = useAppSelector(getTestCase);
+
+    const testCasePresets = useMemo(() => {
+        if (!testCase) return [];
         return [
             {
-                label: t("testingForm.test.menu.label", "Run all"),
-                value: RUN_ALL,
+                label: testCase.name,
+                value: testCase.id,
             },
         ];
-    }, [t]);
+    }, [testCase]);
+
+    const runAllPreset: Preset = useMemo(
+        () => ({
+            label: t("testingForm.test.menu.runAll", "Run all"),
+            value: RUN_ALL,
+        }),
+        [t],
+    );
+
+    const [selectedPreset, setSelectedPreset] = useState<Preset | null>(testCasePresets[0] || runAllPreset);
+
+    const presets: Preset[] = useMemo(() => {
+        return [runAllPreset, ...testCasePresets];
+    }, [runAllPreset, testCasePresets]);
 
     const isLoading = useAppSelector(getTestResultsLoading);
 
@@ -64,55 +82,75 @@ function ScenarioTestButton(props: PropsOfButton<CustomButtonTypes.scenarioTest>
               )
             : title);
 
+    const [showMockFieldOnEnrichers] = useUserSettings("node.showMockFieldOnEnrichers");
+    const dispatch = useAppDispatch();
+    const handleRunTest = useCallback(
+        (testCase: TestCase) => {
+            return dispatch(testScenarioWithTestCase(testCase, showMockFieldOnEnrichers));
+        },
+        [dispatch, showMockFieldOnEnrichers],
+    );
+
     return (
-        <ToolbarButton
-            name={
-                testingDataRecordsDefined
-                    ? t("panels.actions.scenarioTest.button.nameAlt", "Rerun test")
-                    : name || t("panels.actions.scenarioTest.button.name", "Test")
-            }
+        <StyledScenarioTestButton
+            onClick={() => {
+                handleRunTest(testCase);
+            }}
+            name={selectedPreset.label}
             title={tooltip || t("panels.actions.scenarioTest.button.title", "run test")}
             icon={<TestingIcon />}
-            sx={(theme) => {
-                const normal = theme.palette.primary.main;
-                const highlight = theme.palette.primary.light;
-                const isHorizontal =
-                    variant === ButtonsVariant.xs &&
-                    [ToolbarsSide.CenterTop, ToolbarsSide.CenterBottom, ToolbarsSide.AboveNodeWindow].includes(side);
-                return {
-                    color: alpha(theme.palette.getContrastText(normal), 0.75),
-
-                    ".toolbarButton-Root": {
-                        backgroundColor: normal,
-                    },
-                    "&:hover": {
-                        color: theme.palette.getContrastText(highlight),
-                        ".toolbarButton-Root, .toolbarButton-MenuExpand": {
-                            backgroundColor: highlight,
-                        },
-                    },
-                    "button:focus-within": {
-                        color: theme.palette.getContrastText(highlight),
-                        outlineColor: theme.palette.background.paper,
-                        backgroundColor: highlight,
-                    },
-                    ".toolbarButton-Label": isHorizontal
-                        ? {
-                              fontSize: "1em",
-                              minWidth: "5em",
-                              display: "inline",
-                              textTransform: "initial",
-                          }
-                        : null,
-                };
-            }}
+            side={side}
+            variant={variant}
             isLoading={isLoading}
             disabled={!testingScenarioEnabled || isLoading}
             type={type}
             presets={presets}
-            selected={presetActionOnButtonClick}
+            selected={selectedPreset}
+            onPresetChange={(preset) => {
+                setSelectedPreset(preset);
+                if (preset.value === RUN_ALL) {
+                    //TODO: Implement me when backend ready
+                    return;
+                }
+
+                //TODO: Handle multiple test selection when backend ready
+                handleRunTest(testCase);
+            }}
         />
     );
 }
 
 export default ScenarioTestButton;
+
+const StyledScenarioTestButton = styled(ToolbarButton)<{ side: ToolbarsSide; variant: ButtonsVariant }>(({ theme, side, variant }) => {
+    const normal = theme.palette.primary.main;
+    const highlight = theme.palette.primary.light;
+    const isHorizontal =
+        variant === ButtonsVariant.xs && [ToolbarsSide.CenterTop, ToolbarsSide.CenterBottom, ToolbarsSide.AboveNodeWindow].includes(side);
+    return {
+        color: alpha(theme.palette.getContrastText(normal), 0.75),
+
+        ".toolbarButton-Root": {
+            backgroundColor: normal,
+        },
+        "&:hover": {
+            color: theme.palette.getContrastText(highlight),
+            ".toolbarButton-Root, .toolbarButton-MenuExpand": {
+                backgroundColor: highlight,
+            },
+        },
+        "button:focus-within": {
+            color: theme.palette.getContrastText(highlight),
+            outlineColor: theme.palette.background.paper,
+            backgroundColor: highlight,
+        },
+        ".toolbarButton-Label": isHorizontal
+            ? {
+                  fontSize: "1em",
+                  minWidth: "5em",
+                  display: "inline",
+                  textTransform: "initial",
+              }
+            : null,
+    };
+});
