20e7653ebb81b8131c53bcde88dc4517dc7ca362
Piotr Przybylski <ppr@touk.pl>
2026-03-02 21:30:04 +0100

    Allow ciRunSbt.sh to work with "set" command, disable docs generation in test jobs

--- ciRunSbt.sh
+++ ciRunSbt.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 set -e
+ARGS=()
 # When $NUSSKNACKER_SCALA_VERSION is not present, we check for $CROSS_BUILD. If it's true - we do cross build, otherwise we use default scala version
 if [[ -n "$NUSSKNACKER_SCALA_VERSION" ]]; then
-   ARGS="++$NUSSKNACKER_SCALA_VERSION $*"
+  # prepend each command chain with '++', 'set ...' resets this so we need to repeat '++' insertion
+  scala_version_inserted=0
+  for arg in "$@"; do
+    if [[ "$arg" != set* ]] && (( !scala_version_inserted )); then
+      ARGS+=("++$NUSSKNACKER_SCALA_VERSION")
+      scala_version_inserted=1
+    elif [[ "$arg" == set* ]]; then
+      scala_version_inserted=0
+    fi
+    ARGS+=("$arg")
+  done
 elif [[ $CROSS_BUILD == 'true' ]]; then
-   #for crossbuild we prepend each arg with +...
-   ARGS=`echo " $*" | sed "s/ \+/ \+/g"`
+  # for crossbuild we prepend each task with +...
+  for arg in "$@"; do
+    if [[ "$arg" == set* ]]; then
+      ARGS+=("$arg")
+    else
+      ARGS+=("+$arg")
+    fi
+  done
 else
-   ARGS="$*"
+  ARGS=("$@")
 fi
 echo "Executing: sbt $ARGS"
-exec sbt $ARGS
+exec sbt "${ARGS[@]}"
