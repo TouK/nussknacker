f8f7a70cd57f6604b0489019e0dc11639221c2a2
Piotr Przybylski <ppr@touk.pl>
2026-03-06 10:29:16 +0100

    try to fix cypress test

--- designer/client/cypress/e2e/deployAdvancedSection.cy.ts
+++ designer/client/cypress/e2e/deployAdvancedSection.cy.ts
@@ -18,9 +18,9 @@ describe("Deploy advanced section", () => {
         cy.contains(/^deploy$/i).click();
         cy.get("[data-testid=window]").matchImage();
 
-        cy.contains("log").should("be.visible").click();
+        cy.contains("[data-testid=window]", "log").should("be.visible").click();
         // a click on the window header is on a purpose because we don't want to have a focus on the expandable section
-        cy.get("[data-testid=window]").find("h3").click();
+        cy.get("[data-testid=window]").find("[data-testid=expandable-header]").click();
         cy.get("[data-testid=window]").matchImage();
     });
 });
