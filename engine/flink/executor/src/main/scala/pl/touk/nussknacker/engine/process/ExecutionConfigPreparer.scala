22106777dc0890a926563d487f3ee53090a0c77d
Piotr Przybylski <ppr@touk.pl>
2026-03-20 10:41:31 +0100

    TableJoinTest

--- engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/ExecutionConfigPreparer.scala
+++ engine/flink/executor/src/main/scala/pl/touk/nussknacker/engine/process/ExecutionConfigPreparer.scala
@@ -91,6 +91,7 @@ object ExecutionConfigPreparer extends LazyLogging {
         config: ExecutionConfig
     )(jobData: JobData, deploymentData: DeploymentData): Unit = {
       FlinkTypeInfoRegistrar.ensureTypeInfosAreRegistered()
+      FlinkTypeInfoRegistrar.ensureKryoTypesAreRegistered(config.getSerializerConfig)
       FlinkTypeInfoRegistrar.validateKryoTypeRegistrations()
       Serializers.registerSerializers(modelData.modelClassLoader, config)
       if (enableObjectReuse) {
