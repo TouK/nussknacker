4d72c6863ea2cabcb6347a7fa18bf1e599862146
Łukasz Jędrzejewski <ljd@touk.pl>
2026-03-26 16:37:44 +0100

    hasSize

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/testcase/AssertionVerifier.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/testcase/AssertionVerifier.scala
@@ -106,6 +106,14 @@ class AssertionVerifier(globalVariablesPreparer: GlobalVariablesPreparer) {
           AssertionResult.produceFailedComparisonAssertion(">=", expectedValue, actualValue)
         case AssertionOperator.LessThanOrEqual =>
           AssertionResult.produceFailedComparisonAssertion("<=", expectedValue, actualValue)
+        case AssertionOperator.HasSize =>
+          val actualSize = actualValue match {
+            case c: java.util.Collection[_] => c.size()
+            case m: java.util.Map[_, _]     => m.size()
+            case a: Array[_]                => a.length
+            case _                          => -1
+          }
+          AssertionResult.produceFailedHasSizeAssertion(expectedValue, actualSize)
       }
     }
   }
