0cf18228533b8934b84a79eda7049bd419737069
Łukasz Jędrzejewski <ljd@touk.pl>
2026-03-31 13:04:59 +0200

    Do not estimate size objects for original test results which are possibly unhandled by RamUsageEstimator

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/ResultsWithCounts.scala
+++ designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/ResultsWithCounts.scala
@@ -2,7 +2,7 @@ package pl.touk.nussknacker.ui.process.test
 
 import io.circe.Json
 import pl.touk.nussknacker.engine.api.NodeId
-import pl.touk.nussknacker.engine.testmode.TestProcess.TestResults
+import pl.touk.nussknacker.engine.testmode.TestProcess._
 import pl.touk.nussknacker.ui.process.test.testcase.AssertionResult
 import pl.touk.nussknacker.ui.processreport.NodeCount
 
@@ -10,7 +10,27 @@ import java.time.Instant
 
 final case class ResultsWithCounts(
     timestamp: Instant,
-    results: TestResults[Json],
+    results: JsonTestResults,
     counts: Map[NodeId, NodeCount],
     assertionsResults: Map[NodeId, List[AssertionResult]] = Map.empty
 )
+
+final case class JsonTestResults(
+    nodeResults: Map[NodeId, List[ResultContext[Json]]],
+    nodeTransitionResults: Map[NodeTransition, List[ResultContext[Json]]],
+    expressionEvaluationResults: Map[NodeId, List[ExpressionEvaluationResult[Json]]],
+    externalServiceInvocationResults: Map[NodeId, List[ExternalServiceInvocationResult[Json]]],
+    exceptions: List[ExceptionResult[Json]],
+)
+
+object JsonTestResults {
+
+  def from(testResults: TestResults[Json]): JsonTestResults = JsonTestResults(
+    nodeResults = testResults.nodeResults,
+    nodeTransitionResults = testResults.nodeTransitionResults,
+    expressionEvaluationResults = testResults.expressionEvaluationResults,
+    externalServiceInvocationResults = testResults.externalServiceInvocationResults,
+    exceptions = testResults.exceptions,
+  )
+
+}
