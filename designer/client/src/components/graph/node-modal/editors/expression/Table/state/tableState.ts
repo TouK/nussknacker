ce8cc0804c9e546dfa6bc0f67327d7600eb946f8
Julian Wielga <j@touk.pl>
2026-04-06 23:27:39 +0200

    potential bugfix

--- designer/client/src/components/graph/node-modal/editors/expression/Table/state/tableState.ts
+++ designer/client/src/components/graph/node-modal/editors/expression/Table/state/tableState.ts
@@ -58,7 +58,8 @@ export function useTableState(expressionObj: ExpressionObj): [TableData, Dispatc
 
         // There are two rerenders on init, with empty state and with the parsed state. Let's skip the first one,
         // Also when we add empty table editor, backend sends empty rows and columns, and we set initial table editor state in the ActionTypes.expand
-        if (expressionAndStateAreEqual || isFirstRender()) {
+        const firstRender = isFirstRender();
+        if (expressionAndStateAreEqual || firstRender) {
             return;
         }
 
