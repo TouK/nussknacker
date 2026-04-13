fc5179927c7b896415c18012946f9b397b4e791f
Łukasz Jędrzejewski <ljd@touk.pl>
2026-04-13 15:56:47 +0200

    Remove unused ScenarioWithTestCase.scala

--- designer/server/src/main/scala/pl/touk/nussknacker/ui/process/test/testcase/ScenarioWithTestCase.scala
+++
@@ -1,7 +0,0 @@
-package pl.touk.nussknacker.ui.process.test.testcase
-
-import io.circe.generic.JsonCodec
-import pl.touk.nussknacker.engine.api.graph.ScenarioGraph
-import pl.touk.nussknacker.engine.test.testcase.TestCase
-
-@JsonCodec final case class ScenarioWithTestCase(scenario: ScenarioGraph, testCase: TestCase)
