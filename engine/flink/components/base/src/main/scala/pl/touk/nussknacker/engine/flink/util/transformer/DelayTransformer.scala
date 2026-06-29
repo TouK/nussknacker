d84e9ceb95e931ee7303c6d20af01df252acf89e
Łukasz Ciołecki <lcl@touk.pl>
2026-06-29 11:24:42 +0200

    DelayTransformer.delayParamDeclaration as SpelParameterEditor for temporary

--- engine/flink/components/base/src/main/scala/pl/touk/nussknacker/engine/flink/util/transformer/DelayTransformer.scala
+++ engine/flink/components/base/src/main/scala/pl/touk/nussknacker/engine/flink/util/transformer/DelayTransformer.scala
@@ -74,12 +74,7 @@ class DelayTransformer(config: DelayConfig)
               "input fields. A negative duration is treated as no delay (the event is released immediately)."
           ),
           defaultValue = Some(s"T(java.time.Duration).parse('${Duration.ofMillis(100)}')".spel),
-          editors = List(
-            DurationParameterEditor(
-              List(ChronoUnit.DAYS, ChronoUnit.HOURS, ChronoUnit.MINUTES, ChronoUnit.SECONDS, ChronoUnit.MILLIS)
-            ),
-            SpelParameterEditor
-          ),
+          editors = List(SpelParameterEditor),
           validators = param.validators
         )
     )
