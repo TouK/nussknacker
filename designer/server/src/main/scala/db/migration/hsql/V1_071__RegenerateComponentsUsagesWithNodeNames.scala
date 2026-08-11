d80a91830cbd370cfba37347576920c8ee389365
Mateusz Kopeć <mateuszkp96@gmail.com>
2026-08-11 14:15:13 +0200

    Turn V1_071 RegenerateComponentsUsagesWithNodeNames migration into noop

--- designer/server/src/main/scala/db/migration/hsql/V1_071__RegenerateComponentsUsagesWithNodeNames.scala
+++ designer/server/src/main/scala/db/migration/hsql/V1_071__RegenerateComponentsUsagesWithNodeNames.scala
@@ -1,8 +1,12 @@
 package db.migration.hsql
 
-import db.migration.InvalidateComponentsUsagesCache
+import pl.touk.nussknacker.ui.db.migration.SlickMigration
+import slick.dbio.{DBIOAction, Effect, NoStream}
 import slick.jdbc.HsqldbProfile
 
-class V1_071__RegenerateComponentsUsagesWithNodeNames extends InvalidateComponentsUsagesCache {
+// Noop: regeneration was needed after introducing node ids into componentsUsages, but this branch doesn't have node ids yet (unlike staging), so there's nothing to regenerate.
+class V1_071__RegenerateComponentsUsagesWithNodeNames extends SlickMigration {
   override protected lazy val profile = createNuJdbcProfileFrom(HsqldbProfile)
+
+  override protected def migrateActions: DBIOAction[Any, NoStream, Effect] = DBIOAction.successful(())
 }
--- designer/server/src/main/scala/db/migration/postgres/V1_071__RegenerateComponentsUsagesWithNodeNames.scala
+++ designer/server/src/main/scala/db/migration/postgres/V1_071__RegenerateComponentsUsagesWithNodeNames.scala
@@ -1,8 +1,12 @@
 package db.migration.postgres
 
-import db.migration.InvalidateComponentsUsagesCache
+import pl.touk.nussknacker.ui.db.migration.SlickMigration
+import slick.dbio.{DBIOAction, Effect, NoStream}
 import slick.jdbc.PostgresProfile
 
-class V1_071__RegenerateComponentsUsagesWithNodeNames extends InvalidateComponentsUsagesCache {
+// Noop: regeneration was needed after introducing node ids into componentsUsages, but this branch doesn't have node ids yet (unlike staging), so there's nothing to regenerate.
+class V1_071__RegenerateComponentsUsagesWithNodeNames extends SlickMigration {
   override protected lazy val profile = createNuJdbcProfileFrom(PostgresProfile)
+
+  override protected def migrateActions: DBIOAction[Any, NoStream, Effect] = DBIOAction.successful(())
 }
